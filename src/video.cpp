5c1b18c70dbbfb456d9e10480923b83e11dea16b
Dave White <davewx7@gmail.com>
2003-09-26 10:58:11 +0000

    made the game always use software surfaces rather than hardware surfaces...
    ...under Windows, since Windows seems to have trouble with hardware surfaces

--- src/video.cpp
+++ src/video.cpp
@@ -64,7 +64,11 @@ namespace {
 
 	unsigned int get_flags(unsigned int flags)
 	{
+		//SDL under Windows doesn't seem to like hardware surfaces for
+		//some reason.
+#ifndef _WIN32
 		flags |= SDL_HWSURFACE | SDL_DOUBLEBUF;
+#endif
 		if((flags&SDL_FULLSCREEN) == 0)
 			flags |= SDL_RESIZABLE;
 
