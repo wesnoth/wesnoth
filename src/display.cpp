92864733b0ee838a59bb05b251ad62e43a2d4936
Dave White <davewx7@gmail.com>
2003-09-29 11:27:30 +0000

    fixed mouse flickering reported by zas

--- src/display.cpp
+++ src/display.cpp
@@ -404,9 +404,10 @@ void display::draw(bool update,bool force)
 	const int time_between_draws = 20;
 	const int current_time = SDL_GetTicks();
 	const int wait_time = lastDraw_ + time_between_draws - current_time;
-	if(wait_time > 0) {
-		SDL_Delay(wait_time);
-	}
+
+	//force a wait for 10 ms every frame.
+	//TODO: review whether this is the correct thing to do
+	SDL_Delay(maximum(10,wait_time));
 
 	if(update) {
 		lastDraw_ = SDL_GetTicks();
