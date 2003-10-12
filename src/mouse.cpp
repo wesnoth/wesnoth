d3c0258b0e9173c64c74459b8cf60f779c409ff2
Dave White <davewx7@gmail.com>
2003-10-12 21:31:04 +0000

    fixed compile error

---
+++ src/mouse.cpp
@@ -0,0 +1,45 @@
+/* $Id$ */
+/*
+   Copyright (C) 2003 by David White <davidnwhite@optusnet.com.au>
+   Part of the Battle for Wesnoth Project http://wesnoth.whitevine.net
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#include "mouse.hpp"
+
+namespace {
+
+int scrollamount_ = 0;
+
+}
+
+namespace gui {
+
+// Current scroll pending
+int scrollamount()	{ return scrollamount_; }
+
+// Reset scroll
+void scroll_reset()	{ scrollamount_ = 0; }
+
+// Scrolling UP or LEFT
+void scroll_dec() { --scrollamount_; }
+
+// Scrolling DOWN or RIGHT
+void scroll_inc()  { ++scrollamount_; }
+	
+// Reducing scroll amount
+void scroll_reduce() { 
+	if(scrollamount_ > 0) {
+		--scrollamount_;
+	} else {
+		++scrollamount_;
+	}
+}
+
+}
---
+++ src/mouse.hpp
@@ -0,0 +1,29 @@
+/* $Id$ */
+/*
+   Copyright (C) 2003 by David White <davidnwhite@optusnet.com.au>
+   Part of the Battle for Wesnoth Project http://wesnoth.whitevine.net
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+#ifndef MOUSE_H_INCLUDED
+#define MOUSE_H_INCLUDED
+
+namespace gui {
+
+void scroll_inc();
+void scroll_dec();
+
+void scroll_reduce();
+
+int scrollamount();
+
+void scroll_reset();
+
+}
+
+#endif
