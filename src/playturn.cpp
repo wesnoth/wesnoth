2835d161966c73340563d4dfff11a927bdbb4fc4
uid67456 <uid67456>
2004-01-23 22:28:57 +0000

    Jacob patch to fix bug...
    ...that caused clicking on the mini map to lose focus on your unit

--- src/playturn.cpp
+++ src/playturn.cpp
@@ -378,6 +378,7 @@ void turn_info::left_click(const SDL_MouseButtonEvent& event)
 	const gamemap::location& loc = gui_.minimap_location_on(event.x,event.y);
 	if(loc.valid()) {
 		gui_.scroll_to_tile(loc.x,loc.y,display::WARP);
+		return;
 	}
 
 	gamemap::location hex = gui_.hex_clicked_on(event.x,event.y);
