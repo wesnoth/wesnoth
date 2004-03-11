f1984b879a9582c4db97a42412fcbe9f4bf19f31
Isaac Clerencia Perez <isaac@users.sf.net>
2004-03-11 00:44:49 +0000

    Not sure about last changed, rolling back, may be it's to late to hack :P

--- src/playturn.cpp
+++ src/playturn.cpp
@@ -427,8 +427,9 @@ void turn_info::left_click(const SDL_MouseButtonEvent& event)
 
 	gamemap::location hex = gui_.hex_clicked_on(event.x,event.y);
 
-	//selected_hex_ cannot have an invisible unit
-	unit_map::iterator u = units_.find(selected_hex_);
+	unit_map::iterator u = find_visible_unit(units_,
+			selected_hex_, map_,
+			status_.get_time_of_day().lawful_bonus,teams_,current_team);
 
 	//if the unit is selected and then itself clicked on,
 	//any goto command is cancelled
