9b42139479a8098ab190a700f131b44a59f476f1
Dave White <davewx7@gmail.com>
2004-04-17 13:33:40 +0000

    reverted version to 0.7.3-CVS

--- src/game_config.cpp
+++ src/game_config.cpp
@@ -25,7 +25,7 @@ namespace game_config
 	int rest_heal_amount= 2;
 	int recall_cost = 20;
 	int kill_experience = 8;
-	const std::string version = "0.7.3";
+	const std::string version = "0.7.3-CVS";
 	bool debug = false;
 
 	std::string game_icon = "wesnoth-icon.png", game_title, game_logo, title_music;
