fcdb2f6d86c07ac0bcf1497e29f898aa0450e8a3
Philippe Plantier <ayin@anathas.org>
2004-04-20 18:09:11 +0000

    Fixed bug where keeps located on the border of the screen had broken graphics.
    Keeps (as previously villages) may not be automatically extended at
    the border of the map now.

--- src/map.cpp
+++ src/map.cpp
@@ -274,7 +274,7 @@ gamemap::TERRAIN gamemap::get_terrain(const gamemap::location& loc) const
 	TERRAIN used_terrain = 0;
 	int terrain_count = 0;
 	for(int i = 0; i != nitems; ++i) {
-		if(items[i] != used_terrain && !is_village(items[i])) {
+		if(items[i] != used_terrain && !is_village(items[i]) && !is_keep(items[i])) {
 			const int c = std::count(items+i+1,items+nitems,items[i]) + 1;
 			if(c > terrain_count) {
 				used_terrain = items[i];
