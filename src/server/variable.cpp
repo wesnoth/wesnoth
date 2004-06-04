26ac82e7f66967f95cb07f176c9272cf0e8dd195
Dave White <davewx7@gmail.com>
2004-06-04 02:57:00 +0000

    fixed crash in wesnothd

--- src/server/variable.cpp
+++ src/server/variable.cpp
@@ -1,6 +1,22 @@
 #include "../game_events.hpp"
 
+//this is an 'identity' implementation of variables, that just returns the name
+//of the variable itself. To be used in systems for which variables shouldn't be implemented
+namespace {
+std::map<std::string,std::string> variables;
+
+}
+
 namespace game_events {
-const std::string& get_variable_const(const std::string& str) { return str; }
+const std::string& get_variable_const(const std::string& str)
+{
+	const std::map<std::string,std::string>::const_iterator itor = variables.find(str);
+	if(itor != variables.end()) {
+		return itor->second;
+	} else {
+		variables[str] = "$" + str;
+		return get_variable_const(str);
+	}
+}
 }
 
