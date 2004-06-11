3adba6c23e179534de8df1a3347a3a07b72bedbf
Pauli Manninen <gakusho@users.sourceforge.net>
2004-06-11 21:45:55 +0000

    pumped version to 0.7.10-CVS
    ran autoreconf

--- src/game_config.cpp
+++ src/game_config.cpp
@@ -26,7 +26,7 @@ namespace game_config
 	int recall_cost = 20;
 	int kill_experience = 8;
 	int leadership_bonus = 25;
-	const std::string version = "0.7.9";
+	const std::string version = "0.7.10-CVS";
 	bool debug = false, editor = false;
 
 	std::string game_icon = "wesnoth-icon.png", game_title, game_logo, title_music;
