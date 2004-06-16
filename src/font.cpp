81cd3304bab54248df68b45188adde59ee6e8d31
Dave White <davewx7@gmail.com>
2004-06-16 14:40:15 +0000

    disabled some font settings to work around bug in SDL_ttf

--- src/font.cpp
+++ src/font.cpp
@@ -111,6 +111,18 @@ struct font_style_setter
 		}
 
 		old_style_ = TTF_GetFontStyle(font_);
+
+		//according to the SDL_ttf documentation, combinations of styles may cause
+		//SDL_ttf to segfault. We work around this here by disallowing combinations
+		//of styles
+		if((style&TTF_STYLE_UNDERLINE) != 0) {
+			style = TTF_STYLE_UNDERLINE;
+		} else if((style&TTF_STYLE_BOLD) != 0) {
+			style = TTF_STYLE_BOLD;
+		} else if((style&TTF_STYLE_ITALIC) != 0) {
+			style = TTF_STYLE_NORMAL; //TTF_STYLE_ITALIC;
+		} 
+
 		TTF_SetFontStyle(font_,style);
 	}
 
