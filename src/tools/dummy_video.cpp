5a6c5dfaf740ec873af41d75462896a643ced022
Philippe Plantier <ayin@anathas.org>
2004-06-28 18:20:02 +0000

    Re-enabling build of the "exploder" and "cutter" tools.
    Adding in the repository the file whose absence was making it fail ^^

---
+++ src/tools/dummy_video.cpp
@@ -0,0 +1,34 @@
+/* $Id$ */
+/*
+   Copyright (C) 2004 by Philippe Plantier <ayin@anathas.org>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#include "../video.hpp"
+
+SDL_Surface* CVideo::getSurface()
+{
+	return NULL;
+}
+
+void update_rect(const SDL_Rect& rect)
+{
+}
+
+SDL_Surface* display_format_alpha(SDL_Surface* surf)
+{
+	return NULL;
+}
+
+SDL_Surface* get_video_surface()
+{
+	return NULL;
+}
+
