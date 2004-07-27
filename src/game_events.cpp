7b560294d034fc94600f6b15f764ebbc9787f04d
Dave White <davewx7@gmail.com>
2004-07-27 22:49:57 +0000

    made 'not' tag work properly

--- src/game_events.cpp
+++ src/game_events.cpp
@@ -1247,7 +1247,7 @@ bool filter_loc(const gamemap::location& loc, const config& cfg)
 	//iterate over any [not] tags, and if any match, then the filter does not match
 	const config::child_list& negatives = cfg.get_children("not");
 	for(config::child_list::const_iterator i = negatives.begin(); i != negatives.end(); ++i) {
-		if(filter_loc(loc,**i)) {
+		if((cfg["x"] != "" || cfg["y"] != "") && filter_loc(loc,**i)) {
 			return false;
 		}
 	}
