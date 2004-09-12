2df0c805edab264a15b2841ded3f3f8c9ee5b8a6
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2004-09-12 08:47:06 +0000

    Missing forward declaration

--- src/game_events.hpp
+++ src/game_events.hpp
@@ -57,6 +57,7 @@ struct manager {
 
 void write_events(config& cfg);
 
+bool unit_matches_filter(const unit& u, const config& filter);
 bool unit_matches_filter(unit_map::const_iterator itor, const config& filter);
 
 //function to fire an event. Events may have up to two arguments, both of
