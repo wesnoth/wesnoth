681d4c38f8843ae2994550dcef6d40796c599fbe
Pauli Manninen <gakusho@users.sourceforge.net>
2004-10-04 07:30:51 +0000

    pumped version to 0.8.5
    ran autogen.sh

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -4,8 +4,8 @@
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #else
-# define VERSION "0.8.5-CVS"
-# define WESNOTH_DEFAULT_SERVER "devsrv.wesnoth.org:14999"
+# define VERSION "0.8.5"
+# define WESNOTH_DEFAULT_SERVER "devsrv.wesnoth.org"
 # define PACKAGE "wesnoth"
 # define LOCALEDIR "messages"
 #endif
