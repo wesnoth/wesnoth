5f5619c72231bff8f6e81b11b0bed7c16f0e1443
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2004-10-31 14:28:31 +0000

    Fix untranslatability in the damage-computation-thingy dialog box.

--- src/actions.cpp
+++ src/actions.cpp
@@ -330,7 +330,8 @@ battle_stats evaluate_battle_stats(
 		if(include_strings && resist != 0) {
 			std::stringstream str_resist;
 
-			str_resist << gettext(resist < 0 ? N_("attacker resistance vs") : N_("attacker vulnerability vs")) << " " << string_table[defend.type()]
+			str_resist << gettext(resist < 0 ? N_("attacker resistance vs") : N_("attacker vulnerability vs"))
+				<< " " << gettext(defend.type().c_str())
 				<< ", ,^" << (resist > 0 ? "+" : "") << resist << "%";
 			res.defend_calculations.push_back(str_resist.str());
 		}
@@ -448,8 +449,9 @@ battle_stats evaluate_battle_stats(
 	if(include_strings && resist != 0) {
 		std::stringstream str_resist;
 
-		str_resist << gettext(resist < 0 ? N_("defender resistance vs") : N_("defender vulnerability vs")) << " " << string_table[attack.type()]
-			       << ", ,^" << (resist > 0 ? "+" : "") << resist << "%";
+		str_resist << gettext(resist < 0 ? N_("defender resistance vs") : N_("defender vulnerability vs"))
+			<< " " << gettext(attack.type().c_str())
+			<< ", ,^" << (resist > 0 ? "+" : "") << resist << "%";
 		res.attack_calculations.push_back(str_resist.str());
 	}
 
