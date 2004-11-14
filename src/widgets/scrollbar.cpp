f4db137d87966bbf28b7945f67ec72321facee28
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2004-11-14 00:39:10 +0000

    A bit of copyright assignment for the files I have heavily modified.

--- src/widgets/scrollbar.cpp
+++ src/widgets/scrollbar.cpp
@@ -1,7 +1,8 @@
 /* $Id$*/
 /*
    Copyright (C) 2003 by David White <davidnwhite@optusnet.com.au>
-   Part of the Battle for Wesnoth Project http://wesnoth.whitevine.net
+                 2004 by Guillaume Melquiond <guillaume.melquiond@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License.
