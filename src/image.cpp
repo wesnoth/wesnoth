99a6dcb13d07af9b0562e3c9225e0466ee602ec3
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2005-02-08 20:31:55 +0000

    Fix zipios support so that images can be loaded from user campaigns.

--- src/image.cpp
+++ src/image.cpp
@@ -204,18 +204,20 @@ surface locator::load_image_file() const
 {
 	surface res;
 
-#ifdef USE_ZIPIOS
-	std::string const &s = read_file("images/" + val_.filename_);
-	if (!s.empty()) {
-		SDL_RWops* ops = SDL_RWFromMem((void*)s.c_str(), s.size());
-		res = IMG_Load_RW(ops, 0);
-		SDL_FreeRW(ops);
-	}
-#else
 	std::string const &location = get_binary_file_location("images", val_.filename_);
-	if (!location.empty())
+	if (!location.empty()) {
+#ifdef USE_ZIPIOS
+		std::string const &s = read_file(location);
+		if (!s.empty()) {
+			SDL_RWops* ops = SDL_RWFromMem((void*)s.c_str(), s.size());
+			res = IMG_Load_RW(ops, 0);
+			SDL_FreeRW(ops);
+		}
+#else
 		res = IMG_Load(location.c_str());
 #endif
+	}
+
 	if (res.null())
 		ERR_DP << "could not open image '" << val_.filename_ << "'\n";
 
