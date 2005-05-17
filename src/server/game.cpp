12f99889f462c584000a78602f50b899d9922c88
András Salamon <ott@gaon.net>
2005-05-17 21:00:18 +0000

    Stop "control garbagestring playername" working...
    ...if "garbagestring" isn't a valid side.  Before this change it
    really meant "control 1 playername" but bypassed the checks for side 1
    already having a controller, so it would make the game creator an
    observer: somewhat unexpected!

--- src/server/game.cpp
+++ src/server/game.cpp
@@ -217,6 +217,16 @@ const std::string& game::transfer_side_control(const config& cfg)
 
 	const std::string& side = cfg["side"];
 
+	std::set<std::string>::const_iterator k;
+	for(k = sides_taken_.begin(); k != sides_taken_.end(); ++k) {
+		if(*k == side) break;
+	}
+
+	if(k == sides_taken_.end()) {
+		static const std::string notfound = "Side not found";
+		return notfound;
+	}
+
 	for(std::map<network::connection,std::string>::const_iterator j = sides_.begin(); j != sides_.end(); ++j) {
 		if(j->second == side) {
 			static const std::string already = "This side is already controlled by a player";
@@ -224,7 +234,9 @@ const std::string& game::transfer_side_control(const config& cfg)
 		}
 	}
 
-	if(sides_.count(*i) == 1) {
+	// FIXME:
+	// what is so bad about a player controlling more than one side?
+	if(sides_.count(*i) > 0) {
 		static const std::string already = "Player already controls a side";
 		return already;
 	}
