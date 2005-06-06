4572f00bf8eaa3222048ff30f7535c2ae6f40b54
Isaac Clerencia Perez <isaac@users.sf.net>
2005-06-06 00:16:21 +0000

    Bump version to 0.9.2+cvs

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -4,8 +4,8 @@
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #else
-# define VERSION "0.9.2"
-# define WESNOTH_DEFAULT_SERVER "server.wesnoth.org"
+# define VERSION "0.9.2+cvs"
+# define WESNOTH_DEFAULT_SERVER "devsrv.wesnoth.org"
 # define PACKAGE "wesnoth"
 # ifndef LOCALEDIR
 #  define LOCALEDIR "translations"
