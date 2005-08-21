e9be8f5c14079bcc2e98f69b6901c0dc94b48059
Philippe Plantier <ayin@anathas.org>
2005-08-21 14:46:04 +0000

    Partially fixed bug #14124

--- src/multiplayer_wait.cpp
+++ src/multiplayer_wait.cpp
@@ -203,6 +203,8 @@ void wait::join_game(bool observe)
 		size_t faction_choice = 0;
 
 		if(allow_changes) {
+			events::event_context context;
+
 			const config* era = level_.child("era");
 			if(era == NULL)
 				throw network::error(_("Era not available"));
