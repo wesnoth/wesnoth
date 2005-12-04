85ef610b734b1671e868d50f9779eaf3b247e742
Jérémy Rosen <jeremy.rosen@enst-bretagne.fr>
2005-12-04 22:06:38 +0000

    deprecate animation directly in the attack, please use the [animation] tag

--- src/unit_types.cpp
+++ src/unit_types.cpp
@@ -151,6 +151,7 @@ attack_type::attack_type(const config& cfg)
 
 	if(animation_.empty()) {
 		animation_.push_back(unit_animation(cfg));
+		LOG_STREAM(err, config) << "the animation for " << id() << "is directly in the attack, pleas use [animation]\n" ;
 	}
 
 	id_ = cfg["name"];
