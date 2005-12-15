323dc294f0c7fcb3ca09897f1fd355d69672f9a3
John W. C. McNabb <mcn4bb@gmail.com>
2005-12-15 17:47:23 +0000

    cleanup of team color code

---
+++ src/color_range.cpp
@@ -0,0 +1,45 @@
+/* $Id: team.hpp 9124 2005-12-12 04:09:50Z darthfool $ */
+/*
+   Copyright (C) 2003 by David White <davidnwhite@verizon.net>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#include "color_range.hpp"
+#include "serialization/string_utils.hpp"
+
+#include <string>
+#include <vector>
+
+
+std::vector<Uint32> string2rgb(std::string s){
+  std::vector<Uint32> out;
+  std::vector<std::string> rgb_vec = utils::split(s);
+  std::vector<std::string>::iterator c=rgb_vec.begin();
+  int r,g,b;
+
+  while(c!=rgb_vec.end()){
+    r = (atoi(c->c_str()));
+    c++;
+    if(c != rgb_vec.end()){
+      g = (atoi(c->c_str()));
+    }else{
+      g=0;
+    }
+    c++;
+    if(c != rgb_vec.end()){
+      b=(atoi(c->c_str()));
+    }else{
+      b=0;
+    }
+    c++;
+     out.push_back((Uint32) ((r<<16 & 0x00FF0000) + (g<<8 & 0x0000FF00) + (b & 0x000000FF)));
+   }
+  return(out);
+}
---
+++ src/color_range.hpp
@@ -0,0 +1,49 @@
+/* $Id: team.hpp 9124 2005-12-12 04:09:50Z darthfool $ */
+/*
+   Copyright (C) 2003 by David White <davidnwhite@verizon.net>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+#ifndef COLOR_RANGE_H_INCLUDED
+#define COLOR_RANGE_H_INCLUDED
+
+#include <vector>
+#include "SDL_types.h"
+
+//convert comma seperated string into rgb values
+std::vector<Uint32> string2rgb(std::string s);
+
+class color_range
+{
+public:
+  color_range(Uint32 mid = 0x00808080 , Uint32 max = 0x00FFFFFF, Uint32 min = 0x00000000):mid_(mid),max_(max),min_(min){};
+  color_range(const std::vector<Uint32>& v)
+  {
+    mid_ = v.size() ? v[0] : 0x00808080;
+    max_ = v.size() ? v[1] : 0x00FFFFFF;
+    min_ = v.size() ? v[2] : 0x00000000;
+  };
+  Uint32 mid() const{return(mid_);};
+  Uint32 max() const{return(max_);};
+  Uint32 min() const{return(min_);};
+  bool operator<(const color_range& b) const
+  {
+    if(mid_ != b.mid()) return(mid_ < b.mid());
+    if(max_ != b.max()) return(max_ < b.max());
+    return(min_ < b.min());
+  }
+  bool operator==(const color_range& b) const
+  {
+    return(mid_ == b.mid() && max_ == b.max() && min_ == b.min());
+  }
+private:
+  Uint32 mid_ , max_ , min_;
+};
+
+#endif
