093f89743c3c50fe6d28d388049d859313d2a0d6
Rusty Russell <rusty@rustcorp.com.au>
2006-05-30 06:27:21 +0000

    Fix serial number only to have basename of file, not full path

--- src/upload_log.cpp
+++ src/upload_log.cpp
@@ -133,7 +133,7 @@ upload_log::~upload_log()
 		config_["version"] = VERSION;
 		config_["format_version"] = "1";
 		config_["id"] = preferences::upload_id();
-		config_["serial"] = lexical_cast<std::string>(time(NULL)) + filename_;
+		config_["serial"] = lexical_cast<std::string>(time(NULL)) + file_name(filename_);
 		std::ostream *out = ostream_file(filename_);
 		write(*out, config_);
 		delete out;
