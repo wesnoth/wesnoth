c9774b4b036dc502ebc2308e791db8d8d55d02ac
Dominic Bolin <dominic.bolin@exong.net>
2006-07-13 21:26:21 +0000

    Fix portraits not showing up. ;)

--- src/unit.cpp
+++ src/unit.cpp
@@ -1138,7 +1138,9 @@ void unit::read(const config& cfg)
 		cfg_["unit_description"] = custom_unit_desc;
 	}
 	
-	cfg_["profile"] = cfg["profile"];
+	if(cfg["profile"] != "") {
+		cfg_["profile"] = cfg["profile"];
+	}
 	
 	if(!type_set) {
 		for(config::const_child_itors range = cfg.child_range("attack");
