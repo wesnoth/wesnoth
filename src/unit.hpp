794aecf9899be558870f57c6e7772256346b3672
Dominic Bolin <dominic.bolin@exong.net>
2006-07-30 20:03:30 +0000

    Don't write [event]s in units.

--- src/unit.hpp
+++ src/unit.hpp
@@ -83,8 +83,6 @@ class unit
 		//information about the unit -- a detailed description of it
 		const std::string& unit_description() const;
 
-		const config::child_list wml_events() const;
-
 		int hitpoints() const;
 		int max_hitpoints() const;
 		int experience() const;
