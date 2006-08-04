e1c06cb893ec965a871e709c7abca2c64eb880f4
Karol Nowak <grzywacz@gmail.com>
2006-08-04 22:48:18 +0000

    Tiny gui: use simple_style for menus to save 6 pixels, as suggested by Sapient.

--- src/widgets/menu_style.cpp
+++ src/widgets/menu_style.cpp
@@ -41,8 +41,13 @@ menu::imgsel_style menu::slateborder_style("misc/selection", false,
 menu::imgsel_style menu::bluebg_style("misc/selection2", true,
 										   0x000000, 0x000000, 0x333333,
 										   0.35, 0.0, 0.3);
-menu::style &menu::default_style = menu::bluebg_style;
 menu::style menu::simple_style;
+
+#ifdef USE_TINY_GUI
+menu::style &menu::default_style = menu::simple_style;
+#else
+menu::style &menu::default_style = menu::bluebg_style;
+#endif
 menu *empty_menu = NULL;
 
 	//constructors
