eadbc5f5dedb5d6ae4207fc3b130ffbab3464b95
Benoît Timbert <benoit.timbert@free.fr>
2006-08-29 11:03:56 +0000

    Now all AIs fire a "ai turn" event.

--- src/ai.cpp
+++ src/ai.cpp
@@ -35,7 +35,9 @@
 class idle_ai : public ai_interface {
 public:
 	idle_ai(info& i) : ai_interface(i) {}
-	void play_turn() {}
+	void play_turn() {
+		game_events::fire("ai turn");
+	}
 };
 
 class sample_ai : public ai_interface {
@@ -43,6 +45,7 @@ public:
 	sample_ai(info& i) : ai_interface(i) {}
 
 	void play_turn() {
+		game_events::fire("ai turn");
 		do_attacks();
 		get_villages();
 		do_moves();
