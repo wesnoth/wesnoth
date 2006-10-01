b668ffaa560e38e715efa8dcc065bd15e26b774f
Serge Martin <edb@sigluy.net>
2006-10-01 10:21:05 +0000

    fix https://gna.org/bugs/index.php?6291

--- src/game.cpp
+++ src/game.cpp
@@ -873,9 +873,11 @@ void game_controller::download_campaigns()
 
 	try {
 		const network::manager net_manager;
-		const network::connection sock = network::connect(items.front(),lexical_cast_default<int>(items.back(),15003));
+		const network::connection sock = gui::network_connect_dialog(disp(), _("Connecting to Server..."),
+										items.front(), lexical_cast_default<int>(items.back(),15003) );
 		if(!sock) {
 			gui::show_error_message(disp(), _("Could not connect to host."));
+			preferences::set_campaign_server("");
 			return;
 		}
 
