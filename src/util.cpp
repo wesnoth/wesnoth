73ad2d3028c96b5d3386418f4de3fdfd89c20f4b
Mark de Wever <koraq@xs4all.nl>
2007-02-11 09:13:53 +0000

    moved the test for empty strings in lexical_cast_default...
    ...before attempting to convert the string as a small
    optimization. Didn't convert lexical_cast since an empty string is not
    allowed there.

--- src/util.cpp
+++ src/util.cpp
@@ -77,10 +77,14 @@ int lexical_cast<int, const char*>(const char* a)
 template<>
 int lexical_cast_default<int, const std::string&>(const std::string& a, int def)
 {
+	if(a.empty()) {
+		return def;
+	}
+
 	char* endptr;
 	int res = strtol(a.c_str(), &endptr, 10);
 
-	if (a.empty() || *endptr != '\0') {
+	if (*endptr != '\0') {
 		return def;
 	} else {
 		return res;
@@ -90,10 +94,14 @@ int lexical_cast_default<int, const std::string&>(const std::string& a, int def)
 template<>
 int lexical_cast_default<int, const char*>(const char* a, int def)
 {
+	if(*a == '\0') {
+		return def;
+	}
+
 	char* endptr;
 	int res = strtol(a, &endptr, 10);
 
-	if (*a == '\0' || *endptr != '\0') {
+	if (*endptr != '\0') {
 		return def;
 	} else {
 		return res;
@@ -103,10 +111,14 @@ int lexical_cast_default<int, const char*>(const char* a, int def)
 template<>
 double lexical_cast_default<double, const std::string&>(const std::string& a, double def)
 {
+	if(a.empty()) {
+		return def;
+	}
+
 	char* endptr;
 	double res = strtod(a.c_str(), &endptr);
 
-	if (a.empty() || *endptr != '\0') {
+	if (*endptr != '\0') {
 		return def;
 	} else {
 		return res;
