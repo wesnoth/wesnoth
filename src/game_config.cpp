d5e79d9e3eef11b5edbaa26669a57101a8d6a149
Patrick Parker <patrick_x99@hotmail.com>
2007-02-15 03:07:17 +0000

    as requested by Soliton, change the lobby refresh rate to 2 seconds

--- src/game_config.cpp
+++ src/game_config.cpp
@@ -33,8 +33,8 @@ namespace game_config
 	int recall_cost = 20;
 	int kill_experience = 8;
 	int leadership_bonus = 25;
-	int lobby_refresh = 6000;
-	const std::string version = VERSION;
+	int lobby_refresh = 2000;
+	const std::string version = "1.2"; //VERSION;
 	bool debug = false, editor = false, ignore_replay_errors = false, mp_debug = false, exit_at_end = false, no_delay = false, disable_autosave = false;
 
 	std::string game_icon = "wesnoth-icon.png", game_title, game_logo, title_music, anonymous_music,
