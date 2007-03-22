2312f2adbed4849785c7b7ece074388fc296c776
Patrick Parker <patrick_x99@hotmail.com>
2007-03-22 23:34:29 +0000

    don't draw the dialog-style edges for the credits screen

--- src/about.cpp
+++ src/about.cpp
@@ -222,8 +222,13 @@ void show_about(display &disp, std::string campaign)
 
 		SDL_BlitSurface(map_image,&middle_src,video.getSurface(),&middle_dest);
 		std::string style = "mainmenu";
-
-          gui::draw_dialog_frame(map_rect.x + map_rect.w * 3/32, map_rect.y + top_margin, map_rect.w * 13 / 16, map_rect.h - top_margin - bottom_margin,disp.video(),&style);
+		SDL_Rect frame = {
+			map_rect.x + map_rect.w * 3/32,
+			map_rect.y + top_margin,
+			map_rect.w * 13 / 16,
+			map_rect.h - top_margin - bottom_margin 
+		};
+        gui::draw_dialog_background(frame.x, frame.y, frame.w, frame.h,disp.video(),style);
 		// draw one screen full of text
 		const int line_spacing = 5;
 		int y = map_rect.y + top_margin - offset;
