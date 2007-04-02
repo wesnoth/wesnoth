4624461c1e71344218123d242c753ad3955e3d65
Ali El Gariani <alinkmaze@gmail.com>
2007-04-02 03:10:07 +0000

    In tiny gui, fix the max dialog width to 300 instead of 150 pixels.
    The attack dialog has now enough room to display the second half of
    its content (which can be useful :) ).

--- src/construct_dialog.cpp
+++ src/construct_dialog.cpp
@@ -62,7 +62,7 @@ const size_t dialog::top_padding = font::relative_size(10);
 const size_t dialog::bottom_padding = font::relative_size(10);
 
 #ifdef USE_TINY_GUI
-	const int dialog::max_menu_width = 150;
+	const int dialog::max_menu_width = 300;
 #else
 	const int dialog::max_menu_width = -1;
 #endif
