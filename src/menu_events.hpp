8751fd4b47e98a5a4830a3b54cce610b047efc34
Jörg Hinrichs <joerg.hinrichs@alice-dsl.de>
2007-04-10 17:56:26 +0000

    The final fix for making observer replays happen.
    This time for everybody's satisfaction.

--- src/menu_events.hpp
+++ src/menu_events.hpp
@@ -26,7 +26,7 @@
 class game_state;
 class gamestatus;
 
-enum LEVEL_RESULT { VICTORY, DEFEAT, QUIT, LEVEL_CONTINUE, LEVEL_CONTINUE_NO_SAVE };
+enum LEVEL_RESULT { VICTORY, DEFEAT, QUIT, LEVEL_CONTINUE, LEVEL_CONTINUE_NO_SAVE, OBSERVER_END };
 
 struct end_level_exception {
 	end_level_exception(LEVEL_RESULT res, bool bonus=true)
