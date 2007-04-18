b49596f994418bba93bacfd754b7b2f4ea392da5
Karol Nowak <grzywacz@gmail.com>
2007-04-18 21:23:31 +0000

    Cleanup of sdl wrappers

--- src/sdl_wrappers.c
+++ src/sdl_wrappers.c
@@ -45,13 +45,8 @@ static void *imghandler;
 /*
  * Handles for SDL routines we're wrapping
  */
-static SDL_Surface * (*Load)(const char *);
 static void (*FreeSurface)(SDL_Surface *);
-static SDL_Surface * (*ConvertSurface)(SDL_Surface *, SDL_PixelFormat *, Uint32);
 static SDL_Surface * (*CreateRGBSurface)(Uint32, int, int, int, Uint32, Uint32, Uint32, Uint32);
-static SDL_Surface * (*CreateRGBSurfaceFrom)(void *, int, int, int, int, Uint32, Uint32, Uint32, Uint32);
-static SDL_Surface * (*DisplayFormat)(SDL_Surface *);
-static SDL_Surface * (*DisplayFormatAlpha)(SDL_Surface *);
 
 /*
  * Initialization and cleanup functions
@@ -74,14 +69,8 @@ void init()
 		exit(1);
 	}
 
-	Load = dlsym(imghandler, "IMG_Load");
-
 	WRAP(FreeSurface);
-	WRAP(ConvertSurface);
 	WRAP(CreateRGBSurface);
-	WRAP(CreateRGBSurfaceFrom);
-	WRAP(DisplayFormat);
-	WRAP(DisplayFormatAlpha);
 
 	fprintf(stderr, "SDL wrappers loaded.\n");
 }
@@ -114,7 +103,7 @@ static unsigned int surface_size(const SDL_Surface *s)
 }
 
 /*
- * Actual wrappers for SDL surface manipulating functions start here
+ * Actual wrappers for SDL functions
  */
 void SDL_FreeSurface(SDL_Surface *s)
 {
@@ -130,50 +119,5 @@ SDL_Surface *SDL_CreateRGBSurface(Uint32 flags, int width, int height, int depth
 	allocated += surface_size(tmp);
 	return tmp;
 }
-
-/*
- * These, as it turns out, use SDL_CreateRGBSurface internally, so counting
- * memory allocated by "them" would cause it being counted twice...
- *
- * Commented out, at least for now.
- *
-SDL_Surface *SDL_ConvertSurface(SDL_Surface *src, SDL_PixelFormat *fmt, Uint32 flags)
-{
-	SDL_Surface *tmp = ConvertSurface(src, fmt, flags);
-	//allocated += surface_size(tmp);
-	return tmp;
-}
-
-SDL_Surface *IMG_Load(const char *s)
-{
-	SDL_Surface *tmp = (SDL_Surface *) Load(s);
-
-//	if(s)
-//		allocated += surface_size(tmp);
-
-	return tmp;
-}
-
-SDL_Surface *SDL_CreateRGBSurfaceFrom(void  *pixels,  int width, int height, int depth, int pitch, Uint32 Rmask, Uint32 Gmask, Uint32 Bmask, Uint32 Amask)
-{
-	SDL_Surface *tmp = CreateRGBSurfaceFrom(pixels, width, height, depth, pitch, Rmask, Gmask, Bmask, Amask);
-	// allocated += surface_size(tmp);
-	return tmp;
-}
-
-SDL_Surface *SDL_DisplayFormat(SDL_Surface *surface)
-{
-	SDL_Surface *tmp = DisplayFormat(surface);
-	//allocated += surface_size(tmp);
-	return tmp;
-}
-
-SDL_Surface *SDL_DisplayFormatAlpha(SDL_Surface *surface)
-{
-	SDL_Surface *tmp = DisplayFormatAlpha(surface);
-//	allocated += surface_size(tmp);
-	return tmp;
-}
-*/
 
 /* vim: set ts=4 sw=4: */
