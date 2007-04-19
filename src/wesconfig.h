608f637d8127e86e42196544275d5553c22cbd1c
Gunter Labes <soliton@wesnoth.org>
2007-04-19 23:19:01 +0000

    change MP server port from 14998 to 15000

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -5,7 +5,7 @@
 # include "config.h"
 #else
 # define VERSION "1.3.1+svn"
-# define WESNOTH_DEFAULT_SERVER "devsrv.wesnoth.org:14998"
+# define WESNOTH_DEFAULT_SERVER "devsrv.wesnoth.org:15000"
 # define PACKAGE "wesnoth"
 # ifndef LOCALEDIR
 #  define LOCALEDIR "translations"
