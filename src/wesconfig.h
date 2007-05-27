249d3039e9bf372061944b3e8ff6933654d0fe60
Mark de Wever <koraq@xs4all.nl>
2007-05-27 14:53:44 +0000

    bump minimum savegame version to 1.3.2
    since the automatic terrain translation is about to be removed

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -16,6 +16,6 @@
  * Some older savegames of Wesnoth can't be loaded anymore this
  * variable defines the minimum required version
  */
-#define MIN_SAVEGAME_VERSION "1.3.1"
+#define MIN_SAVEGAME_VERSION "1.3.2"
 
 #endif
