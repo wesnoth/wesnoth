857a13d5c541eef4082c5312ae016630c46248ca
Eric S. Raymond <esr@thyrsus.com>
2007-07-04 22:59:57 +0000

    Revert alink's commit 2007-06-30T03:10:14Z!alinkmaze@gmail.com, which introduced a visible bug.
    To see it, buld the editor from a revision after 2007-06-30T02:41:40Z!alinkmaze@gmail.com up to
    or including 2007-07-04T20:22:58Z!Tuukka.Hastrup@iki.fi.  Notice that the display is defective,
    with hexes not drawn on the right side.  These hexes will appear if
    you mouse over them.

--- src/display.cpp
+++ src/display.cpp
@@ -257,6 +257,8 @@ void display::get_rect_hex_bounds(SDL_Rect rect, gamemap::location &topleft, gam
 	const SDL_Rect& map_rect = map_area();
 	rect.x -= map_rect.x;
 	rect.y -= map_rect.y;
+	rect.w += map_rect.x;
+	rect.h += map_rect.y;
 
 	const int tile_width = hex_width();
 
