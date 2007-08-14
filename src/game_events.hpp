0a4559dc9e4ea3d78be889fc3889c8ad254bf01d
Hans Joachim Gurt <gurt@gmx.de>
2007-08-14 01:52:24 +0000

    Comments, doxygen

--- src/game_events.hpp
+++ src/game_events.hpp
@@ -30,25 +30,28 @@ class game_state;
 #include <map>
 
 
-//this file defines the game's events mechanism. Events might be units
-//moving or fighting, or when victory or defeat occurs. A scenario's
-//configuration file will define actions to take when certain events
-//occur. This module is responsible for making sure that when the events
-//occur, the actions take place.
+//! @file game_events.hpp 
+//! Define the game's events mechanism. 
 //
-//note that game events have nothing to do with SDL events, like mouse
-//movement, keyboard events, etc. See events.hpp for how they are handled.
+// Events might be units moving or fighting, or when victory or defeat occurs. 
+// A scenario's configuration file will define actions to take when certain events occur. 
+// This module is responsible for making sure that when the events occur, the actions take place.
+//
+// Note that game events have nothing to do with SDL events, 
+// like mouse movement, keyboard events, etc. 
+// See events.hpp for how they are handled.
+
 namespace game_events
 {
-//the game event manager loads the scenario configuration object, and
-//ensures that events are handled according to the scenario configuration
-//for its lifetime.
+// The game event manager loads the scenario configuration object, 
+// and ensures that events are handled according to the 
+// scenario configuration for its lifetime.
 //
-//thus, a manager object should be created when a scenario is played, and
-//destroyed at the end of the scenario.
+// Thus, a manager object should be created when a scenario is played, 
+// and destroyed at the end of the scenario.
 struct manager {
-	//note that references will be maintained, and must remain valid
-	//for the life of the object.
+	// Note that references will be maintained, 
+	// and must remain valid for the life of the object.
 	manager(const config& scenario_cfg, game_display& disp, gamemap& map,
 			soundsource::manager& sndsources, unit_map& units, std::vector<team>& teams,
 			game_state& state_of_game, gamestatus& status, const game_data& data);
@@ -72,8 +75,8 @@ void add_events(const config::child_list& cfgs,const std::string& id);
 
 bool unit_matches_filter(unit_map::const_iterator itor, const vconfig filter);
 
-//function to fire an event. Events may have up to two arguments, both of
-//which must be locations.
+//! Function to fire an event. 
+// Events may have up to two arguments, both of which must be locations.
 bool fire(const std::string& event,
           const entity_location& loc1=gamemap::location::null_location,
           const entity_location& loc2=gamemap::location::null_location,
@@ -88,9 +91,9 @@ bool conditional_passed(const unit_map* units,
                         const vconfig cond, bool backwards_compat=true);
 bool pump();
 
-//the count of game event mutations to the unit_map
+// The count of game event mutations to the unit_map
 Uint32 mutations();
 
-}
+} // end namespace game_events
 
 #endif
