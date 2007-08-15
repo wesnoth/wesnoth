080eb69f60526ac16334952f67f850e88e70aeb9
Mark de Wever <koraq@xs4all.nl>
2007-08-15 13:41:56 +0000

    Updated the headers to state GPL 2+ and a few changelog cleanups.

--- src/leader_list.hpp
+++ src/leader_list.hpp
@@ -4,7 +4,8 @@
    Part of the Battle for Wesnoth Project http://www.wesnoth.org
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License.
+   it under the terms of the GNU General Public License
+   or at your option any later version.
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY.
 
--- src/pathutils.hpp
+++ src/pathutils.hpp
@@ -4,7 +4,8 @@ Copyright (C) 2003 - 2007 by David White <dave@whitevine.net>
 Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2.
+it under the terms of the GNU General Public License version 2
+or at your option any later version.
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY.
 
