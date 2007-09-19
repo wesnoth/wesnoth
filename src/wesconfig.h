492fc5a11dc22aa6aed47b6cb7a0013fc5ba4218
Nils Kneuper <crazy-ivanovic@gmx.net>
2007-09-19 00:34:27 +0000

    bumped version to 1.3.8+svn and updated changelog

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -12,7 +12,7 @@
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #else
-# define VERSION "1.3.8"
+# define VERSION "1.3.8+svn"
 # define PACKAGE "wesnoth"
 # ifndef LOCALEDIR
 #  define LOCALEDIR "translations"
