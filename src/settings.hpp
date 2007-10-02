0f6274fdd182034c2522a4f4cdb414fd7ba2ba4f
Gunter Labes <soliton@wesnoth.org>
2007-10-02 18:17:05 +0000

    use spaces for alignment, not tabs

--- src/settings.hpp
+++ src/settings.hpp
@@ -34,15 +34,15 @@ namespace settings {
 	 *  The value is also range checked.
 	 *  When out of bounds, it will be set to the nearest bound.
 	 *
-	 *  @param value	string containing the number of turns
+	 *  @param value        string containing the number of turns
 	 *
-	 *  @returns		the number of turns
+	 *  @returns            the number of turns
 	 */
 	int get_turns(const std::string& value);
-	const int turns_min = 1;		//< minimum number of turns
-	const int turns_max = 100;		//< maximum number of turns
-	const int turns_default = 100;	//< default number of turns
-	const int turns_step = 1;		//< slider step size for turns
+	const int turns_min = 1;          //< minimum number of turns
+	const int turns_max = 100;        //< maximum number of turns
+	const int turns_default = 100;    //< default number of turns
+	const int turns_step = 1;         //< slider step size for turns
 
 	/**
 	 *  Gets the village gold.
@@ -50,9 +50,9 @@ namespace settings {
 	 *  The value is also range checked.
 	 *  When out of bounds, it will be set to the nearest bound.
 	 *
-	 *  @param value	string containing the village gold
+	 *  @param value        string containing the village gold
 	 *
-	 *  @returns		the village gold
+	 *  @returns            the village gold
 	 */
 	int get_village_gold(const std::string& value);
 
@@ -62,9 +62,9 @@ namespace settings {
 	 *  The value is also range checked.
 	 *  When out of bounds, it will be set to the nearest bound.
 	 *
-	 *  @param value	string containing the xp modifier
+	 *  @param value        string containing the xp modifier
 	 *
-	 *  @returns		the xp modifier
+	 *  @returns            the xp modifier
 	 */
 	int get_xp_modifier(const std::string& value);
 
@@ -72,9 +72,9 @@ namespace settings {
 	 * Returns whether the fog of war should be used.
 	 * If no valid value is supplied, it will return a default.
 	 *
-	 * @param value		string containing a boolean value
+	 * @param value         string containing a boolean value
 	 *
-	 * @returns			whether or not the fog of war should be used
+	 * @returns             whether or not the fog of war should be used
 	 */
 	bool use_fog(const std::string& value);
 
@@ -82,9 +82,9 @@ namespace settings {
 	 * Returns whether the random start time feature should be used.
 	 * If no valid value is supplied, it will return a default.
 	 *
-	 * @param value		string containing a boolean value
+	 * @param value         string containing a boolean value
 	 *
-	 * @returns			whether or not the random start time should be used
+	 * @returns             whether or not the random start time should be used
 	 */
 	bool use_random_start_time(const std::string& value);
 
@@ -92,9 +92,9 @@ namespace settings {
 	 * Returns whether the shroud should be used.
 	 * If no valid value is supplied, it will return a default.
 	 *
-	 * @param value		string containing a boolean value
+	 * @param value         string containing a boolean value
 	 *
-	 * @returns			whether or not the shroud should be used
+	 * @returns             whether or not the shroud should be used
 	 */
 	bool use_shroud(const std::string& value);
 
