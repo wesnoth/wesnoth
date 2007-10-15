749df8f549c6b4930b0911ac74ebabf473e1401f
Gunter Labes <soliton@wesnoth.org>
2007-10-15 17:35:41 +0000

    revert 2007-10-15T17:10:28Z!jeremy.rosen@enst-bretagne.fr
    clarify the comment to hopefully avoid this in the future..

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -22,10 +22,9 @@
 /**
  * Some older savegames of Wesnoth can't be loaded anymore,
  * this variable defines the minimum required version.
- *
- * according to backward compatibility policy, this should be 
- * last dev release -1
+ * It is only to be updated upon changes that break *all* saves/replays
+ * (break as in crash wesnoth not compatibility issues like stat changes)
  */
-#define MIN_SAVEGAME_VERSION "1.3.8"
+#define MIN_SAVEGAME_VERSION "1.3.2"
 
 #endif
