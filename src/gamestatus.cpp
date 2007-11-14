e0b1fc741517834c7c91e81b4bea019a14c3ed6b
Ignacio R. Morelle <shadowm@wesnoth.org>
2007-11-14 16:01:04 +0000

    Forgot that write_player() is a double overloaded fellow...

--- src/gamestatus.cpp
+++ src/gamestatus.cpp
@@ -553,6 +553,8 @@ static void write_player(config_writer &out, const player_info& player)
 		config new_cfg;
 		i->write(new_cfg);
 		out.write_child("unit",new_cfg);
+		cfg.add_child("unit",new_cfg);
+		DBG_NG << "added unit '" << new_cfg["description"] << "' to player '" << player.name << "'\n";
 	}
 
 	std::stringstream can_recruit;
