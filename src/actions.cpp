25953a199e80ad7a5f156de2528dcd5c0c53ad6f
Mark de Wever <koraq@xs4all.nl>
2007-12-12 21:04:23 +0000

    Add some comment.

--- src/actions.cpp
+++ src/actions.cpp
@@ -1812,6 +1812,8 @@ bool clear_shroud_loc(const gamemap& map, team& tm,
 		// We clear one past the edge of the board, so that the half-hexes
 		// at the edge can also be cleared of fog/shroud.
 		if (on_board_loc || map.on_board(adj[i])) {
+			// Both functions should be executed so don't use || which 
+			// uses short-cut evaluation.
 			const bool res = tm.clear_shroud(adj[i]) | tm.clear_fog(adj[i]);
 
 			if(res && cleared != NULL) {
