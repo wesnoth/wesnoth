fc064a8519f392eb96510104b715d15a3e468acf
Pauli Nieminen <paniemin@cc.hut.fi>
2007-12-29 20:20:15 +0000

    removed interpolation from vconfig data

--- src/game_events.cpp
+++ src/game_events.cpp
@@ -1715,7 +1715,6 @@ bool event_handler::handle_event_command(const queued_event& event_info,
                                   
 			assert(state_of_game != 0);
 			side_for_show = false;
-			side_for_raw = utils::interpolate_variables_into_string(side_for_raw,*state_of_game);
 
 			std::vector<std::string> side_for =
 				utils::split(side_for_raw, ',', utils::STRIP_SPACES | utils::REMOVE_EMPTY);
