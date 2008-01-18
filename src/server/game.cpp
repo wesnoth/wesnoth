a7a33df3984743685f99b3ded3d8dcbecf73e6fc
uso <uso>
2008-01-18 23:51:05 +0000

    fix the checks against player_info_->end()...
    ...which may be the cause for the server crashes

--- src/server/game.cpp
+++ src/server/game.cpp
@@ -718,11 +718,11 @@ bool game::filter_commands(const network::connection member, config& cfg) const 
 		{
 			std::stringstream msg;
 			msg << "Removing illegal command from: "
-				<< (player_info_->find(member) == player_info_->end()
+				<< (player_info_->find(member) != player_info_->end()
 					? player_info_->find(member)->second.name()
 					: "")
 				<< ". Current player is: "
-				<< (player_info_->find(current_player()) == player_info_->end()
+				<< (player_info_->find(current_player()) != player_info_->end()
 					? player_info_->find(current_player())->second.name()
 					: "")
 				<< ".\n";
