18cc2ce38ee5a531acf90e1ebe677160b423c2cc
Mark de Wever <koraq@xs4all.nl>
2008-01-19 15:06:52 +0000

    Skip replay no longer skips the story for players only for observers
    (bug #9538).

--- src/multiplayer.cpp
+++ src/multiplayer.cpp
@@ -311,7 +311,8 @@ static void enter_wait_mode(game_display& disp, const config& game_config, game_
 
 	switch (res) {
 	case mp::ui::PLAY:
-		play_game(disp, state, game_config, data, nolog, IO_CLIENT, preferences::skip_mp_replay());
+		play_game(disp, state, game_config, data, nolog, IO_CLIENT, 
+			preferences::skip_mp_replay() && observe);
 		recorder.clear();
 
 		break;
