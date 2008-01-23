de8a7e66c6ef04ea1c88ccbceea9af6d3b113551
Pauli Nieminen <paniemin@cc.hut.fi>
2008-01-23 19:48:33 +0000

    Added files and configure option for server monitor; nothing done yet

---
+++ src/server/monitor.cpp
@@ -0,0 +1,25 @@
+/*
+   Copyright (C) 2008 by Pauli Nieminen <paniemin@cc.hut.fi>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+//! Here is implementation for server monitoring thread
+//! Initial target is to have ti usefull in posix systems
+//! and later add support for windows if possible
+//! If system doesn't support monitoring it is not compilied
+
+#include "monitor.hpp"
+
+#ifdef SERVER_MONITOR
+#else // SERVER_MONITOR
+// We need to define monitor hooks anyway as NOP so code will compile
+#endif // SERVER_MONITOR
+
---
+++ src/server/monitor.hpp
@@ -0,0 +1,20 @@
+/*
+   Copyright (C) 2008 by Pauli Nieminen <paniemin@cc.hut.fi>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#ifndef SERVER_MONITOR_HPP_INCLUDED
+#define SERVER_MONITOR_HPP_INCLUDED
+
+
+#ifdef SERVER_MONITOR
+#endif //SERVER_MONITOR
+#endif // SERVER_MONITOR_HPP_INCLUDED
