4e59e47d988753563911771dae338560c21fda9b
Patrick Parker <patrick_x99@hotmail.com>
2008-02-20 02:38:55 +0000

    make the load game dialog prettier for SMALL_GUI

--- src/dialogs.cpp
+++ src/dialogs.cpp
@@ -674,21 +674,28 @@ std::string load_game_dialog(display& disp, const config& game_config, const gam
 
 	gui::dialog lmenu(disp,
 			  _("Load Game"),
-			  _("Choose the game to load"),
-			  gui::OK_CANCEL);
+			  _("Choose the game to load"), gui::NULL_DIALOG);
+	lmenu.set_basic_behavior(gui::OK_CANCEL);
 	load_game_filter_textbox* filter = new load_game_filter_textbox(disp.video(), items, lmenu);
 	save_preview_pane save_preview(disp.video(),game_config,&map_obj,data,games,summaries,*filter);
 	lmenu.set_textbox(filter);
 	lmenu.add_pane(&save_preview);
 	// create an option for whether the replay should be shown or not
-	if(show_replay != NULL)
-		lmenu.add_option(_("Show replay"), false);
+	if(show_replay != NULL) {
+		#ifdef USE_SMALL_GUI
+			lmenu.add_option(_("Show replay"), false, gui::dialog::BUTTON_STANDARD);
+		#else
+			lmenu.add_option(_("Show replay"), false);
+		#endif
+	}
+	lmenu.add_button(new gui::standard_dialog_button(disp.video(),_("OK"),0,false), gui::dialog::BUTTON_STANDARD);
+	lmenu.add_button(new gui::standard_dialog_button(disp.video(),_("Cancel"),1,true), gui::dialog::BUTTON_STANDARD);
 
 	delete_save save_deleter(disp,*filter,games,summaries);
 	gui::dialog_button_info delete_button(&save_deleter,_("Delete Save"));
 	#ifdef USE_SMALL_GUI
 		//placing the buttons in one line so that none is coverd by any of the others
-		lmenu.add_button(delete_button,gui::dialog::BUTTON_STANDARD);
+		lmenu.add_button(delete_button,gui::dialog::BUTTON_HELP);
 	#else
 		lmenu.add_button(delete_button);
 	#endif
