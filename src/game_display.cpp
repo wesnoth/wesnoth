c717c3cd86c8ed1b67d88cc6b37fb35615617e95
Mark de Wever <koraq@xs4all.nl>
2008-03-08 11:52:13 +0000

    Foo isn't a great variable name or save your changes before you commit ;-)

--- src/game_display.cpp
+++ src/game_display.cpp
@@ -668,10 +668,10 @@ void game_display::draw_bar(const std::string& image, int xpos, int ypos,
 	if(unfilled < height && alpha >= ftofxp(0.3)) {
 		const Uint32 colour = SDL_MapRGB(video().getSurface()->format,col.r,col.g,col.b);
 		const Uint8 r_alpha = minimum<unsigned>(unsigned(fxpmult(alpha,255)),255);
-		surface foo(SDL_CreateRGBSurface(SDL_SWSURFACE, bar_loc.w, height - unfilled, 32,  /*col.r << 16, col.g << 8, col.b, 0x44000000*/0xFF0000, 0xFF00, 0xFF, 0 ) );
+		surface filled_surf(SDL_CreateRGBSurface(SDL_SWSURFACE, bar_loc.w, height - unfilled, 32,  /*col.r << 16, col.g << 8, col.b, 0x44000000*/0xFF0000, 0xFF00, 0xFF, 0 ) );
 		SDL_Rect filled_area = {0, 0, bar_loc.w, height-unfilled};
-		fill_rect_alpha(filled_area, colour, r_alpha, foo);
-		drawing_buffer_add(LAYER_UNIT_FG, drawing_order, tblit(xpos + bar_loc.x, ypos + bar_loc.y + unfilled, foo));
+		fill_rect_alpha(filled_area, colour, r_alpha, filled_surf);
+		drawing_buffer_add(LAYER_UNIT_FG, drawing_order, tblit(xpos + bar_loc.x, ypos + bar_loc.y + unfilled, filled_surf));
 	} 
 }
 
