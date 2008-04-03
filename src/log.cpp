c337d1be31419453b0be5bc30cdfb947b945d188
Mark de Wever <koraq@xs4all.nl>
2008-04-03 06:59:52 +0000

    Add the log domains for the new widget library.

--- src/log.cpp
+++ src/log.cpp
@@ -58,7 +58,9 @@ logger err("error", 0), warn("warning", 1), info("info", 2), debug("debug", 3);
 log_domain general("general"), ai("ai"), config("config"), display("display"),
 	engine("engine"), network("network"), mp_server("server"),
 	filesystem("filesystem"), audio("audio"), notifs("notifs"),
-	replay("replay"), help("help");
+	replay("replay"), help("help"), gui("gui"), gui_parse("gui_parse"), 
+	gui_draw("gui_draw"), gui_event("gui_event");
+
 log_domain::log_domain(char const *name) : domain_(log_domains.size())
 {
 	logd d = { name, 0 };
--- src/log.hpp
+++ src/log.hpp
@@ -50,7 +50,7 @@ std::string get_timestamp(const time_t& t, const std::string& format="%Y%m%d %T 
 
 extern logger err, warn, info, debug;
 extern log_domain general, ai, config, display, engine, network, mp_server,
-	filesystem, audio, notifs, replay, help;
+	filesystem, audio, notifs, replay, help, gui, gui_parse, gui_draw, gui_event;
 
 class scope_logger
 {
