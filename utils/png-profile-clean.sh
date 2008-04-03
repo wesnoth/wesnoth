044d9b5fd996733d5a5979037a25153b7564ccc6
Eric S. Raymond <esr@thyrsus.com>
2008-04-03 22:18:17 +0000

    Obsolesced by optipng.

--- utils/png-profile-clean.sh
+++
@@ -1,6 +0,0 @@
-grep -ir profile ../images/| grep -v .svn | cut -f3 -d ' '> /tmp/wesnoth-profile-clean
-while read x; do
-convert -strip $x $x;
-done <  /tmp/wesnoth-profile-clean
-cat /tmp/wesnoth-profile-clean
-rm /tmp/wesnoth-profile-clean
