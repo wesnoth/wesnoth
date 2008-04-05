994255a79692755a38789d2939b8c9467d37350c
Nils Kneuper <crazy-ivanovic@gmx.net>
2008-04-05 20:04:28 +0000

    version bump to 1.4.1

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -12,7 +12,7 @@
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #else
-# define VERSION "1.4+svn"
+# define VERSION "1.4.1"
 # define PACKAGE "wesnoth"
 # ifndef LOCALEDIR
 #  define LOCALEDIR "translations"
