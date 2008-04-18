cdf9cd49e412cc38cb86bb7317ccd27ffbc8b14d
Gunter Labes <soliton@wesnoth.org>
2008-04-18 00:46:24 +0000

    add kickban as an alias to kban
    some error message improvements

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -588,7 +588,7 @@ void server::run() {
 
 				games_and_users_list_.root().remove_child("user",index);
 			} else {
-				ERR_SERVER << ip << "ERROR: Could not find user: "
+				ERR_SERVER << ip << "ERROR: Could not find user to remove: "
 					<< pl_it->second.name() << " in games_and_users_list_.\n";
 			}
 			// Was the player in the lobby or a game?
@@ -793,7 +793,7 @@ void server::process_query(const network::connection sock,
                            simple_wml::node& query) {
 	const player_map::const_iterator pl = players_.find(sock);
 	if (pl == players_.end()) {
-		DBG_SERVER << "ERROR: Could not find player with socket: " << sock << "\n";
+		DBG_SERVER << "ERROR: process_query(): Could not find player with socket: " << sock << "\n";
 		return;
 	}
 	const simple_wml::string_span& command(query["type"]);
@@ -883,7 +883,7 @@ std::string server::process_command(const std::string& query) {
 					<< stats.bytes_received << " bytes\n";
 			}
 		}
-	} else if (command == "ban" || command == "bans" || command == "kban") {
+	} else if (command == "ban" || command == "bans" || command == "kban" || command == "kickban") {
 		if (parameters == "") {
 			if (bans_.empty()) return "No bans set.";
 			out << "BAN LIST\n";
@@ -894,12 +894,13 @@ std::string server::process_command(const std::string& query) {
 			}
 		} else {
 			bool banned = false;
+			const bool kick = (command == "kban" || command == "kickban")
 			// if we find 3 '.' consider it an ip mask
 			if (std::count(parameters.begin(), parameters.end(), '.') == 3) {
 				banned = true;
 				out << "Set ban on '" << parameters << "'\n";
 				bans_.push_back(parameters);
-				if (command == "kban") {
+				if (kick) {
 					for (player_map::const_iterator pl = players_.begin();
 						pl != players_.end(); ++pl)
 					{
@@ -920,7 +921,7 @@ std::string server::process_command(const std::string& query) {
 							bans_.push_back(ip);
 							out << "Set ban on '" << ip << "'.\n";
 						}
-						if (command == "kban") {
+						if (kick) {
 							out << "Kicked " << pl->second.name() << ".\n";
 							network::queue_disconnect(pl->first);
 						}
@@ -1187,7 +1188,7 @@ void server::process_data_game(const network::connection sock,
 	}
 	if (itor == games_.end()) {
 		ERR_SERVER << "ERROR: Could not find game for player: "
-			<< pl->second.name() << ". (socket:" << sock << ")\n";
+			<< pl->second.name() << ". (socket: " << sock << ")\n";
 		return;
 	}
 
@@ -1533,7 +1534,7 @@ void server::delete_game(std::vector<game*>::iterator game_it) {
 				send_diff = true;
 			}
 		} else {
-			ERR_SERVER << "ERROR: Could not find user in players_. (socket: "
+			ERR_SERVER << "ERROR: delete_game(): Could not find user in players_. (socket: "
 				<< *user << ")\n";
 		}
 	}
