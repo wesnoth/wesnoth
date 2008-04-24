5b9e5f7b66b987b33da742096c7053c6757979d4
Gunter Labes <soliton@wesnoth.org>
2008-04-24 02:25:31 +0000

    update the portmap for the add-on manager

--- data/tools/wesnoth/campaignserver_client.py
+++ data/tools/wesnoth/campaignserver_client.py
@@ -3,7 +3,7 @@ import wesnoth.wmldata as wmldata
 
 class CampaignClient:
     # First port listed will be used as default.
-    portmap = (("15003", "1.3.x"), ("15004", "1.2.x"), ("15005", "1.4.x"))
+    portmap = (("15003", "1.5.x"), ("15004", "1.2.x"), ("15005", "1.4.x"))
     # Files with these suffixes will not be downloaded
     excluded = ("~", "-bak", ".pbl", ".exe", ".com", ".bat", ".scr", ".sh")
 
