6cf78fa6f973f6818ae1d0ff5e15c83515864282
Moritz Göbelbecker <goebelbe@informatik.uni-freiburg.de>
2008-05-10 15:45:14 +0000

    Fix revision.hpp when you build a specific target

--- cmake/revision.cmake
+++ cmake/revision.cmake
@@ -1,14 +1,14 @@
-if (EXISTS revision.hpp)
-  file(READ revision.hpp OLD_VERSION)
-endif (EXISTS revision.hpp)
+if (EXISTS revision)
+  file(READ revision OLD_VERSION)
+endif (EXISTS revision)
 
-execute_process(COMMAND ${SVNVERSION_EXECUTABLE} -n ${CMAKE_SOURCE_DIR}
+execute_process(COMMAND ${SVNVERSION_EXECUTABLE} -n ${SRC_DIR}
                 OUTPUT_VARIABLE SVN_VERSION)
 
 if(SVN_VERSION MATCHES [0-9]+.*)
 
   if(NOT OLD_VERSION MATCHES ".*\"${SVN_VERSION}\".*")
-    file(WRITE revision.hpp "#define REVISION \"${SVN_VERSION}\"\n")
+    file(WRITE revision "#define REVISION \"${SVN_VERSION}\"\n")
   endif(NOT OLD_VERSION MATCHES ".*${SVN_VERSION}.*")
 
 endif(SVN_VERSION MATCHES [0-9]+.*)
