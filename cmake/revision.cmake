a96f8f209251aca02a0b4df8648b04e8c0aee1ad
Moritz Göbelbecker <goebelbe@informatik.uni-freiburg.de>
2008-05-10 11:30:58 +0000

    Add support for revision.hpp

---
+++ cmake/revision.cmake
@@ -0,0 +1,14 @@
+if (EXISTS revision.hpp)
+  file(READ revision.hpp OLD_VERSION)
+endif (EXISTS revision.hpp)
+
+execute_process(COMMAND ${SVNVERSION_EXECUTABLE} -n ${CMAKE_SOURCE_DIR}
+                OUTPUT_VARIABLE SVN_VERSION)
+
+if(SVN_VERSION MATCHES [0-9]+.*)
+
+  if(NOT OLD_VERSION MATCHES ".*\"${SVN_VERSION}\".*")
+    file(WRITE revision.hpp "#define REVISION \"${SVN_VERSION}\"\n")
+  endif(NOT OLD_VERSION MATCHES ".*${SVN_VERSION}.*")
+
+endif(SVN_VERSION MATCHES [0-9]+.*)
