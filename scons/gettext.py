6660a387adac9b738f401366f5ecc8f5a2cc2305
Sergey Popov <loonycyborg@gmail.com>
2008-05-13 22:23:46 +0000

    SCons recipe: added support for initializing message catalogs.

--- scons/gettext.py
+++ scons/gettext.py
@@ -1,5 +1,6 @@
 # vi: syntax=python:et:ts=4
 from os.path import join
+import os
 from SCons.Builder import Builder
 from SCons.Script  import *
 
@@ -25,3 +26,20 @@ def generate(env):
         single_source = True
         )
     env["BUILDERS"]["MsgMerge"] = msgmerge
+
+    env["MSGINIT"] = WhereIs("msginit")
+    msginit = Builder(
+        action = "$MSGINIT -i $SOURCE -o $TARGET",
+        src_suffix = ".pot",
+        suffix = ".po",
+        single_source = True
+        )
+    env["BUILDERS"]["MsgInit"] = msginit
+
+    def MsgInitMerge(env, target, source):
+        if os.path.exists(target + ".po"):
+            return env.MsgMerge(target, source)
+        else:
+            return env.MsgInit(target, source)
+    from SCons.Script.SConscript import SConsEnvironment
+    SConsEnvironment.MsgInitMerge = MsgInitMerge
