da953ecbb4e766b800cf3b72636c05d627e6b7f5
Jérémy Rosen <jeremy.rosen@enst-bretagne.fr>
2008-05-18 15:18:25 +0000

    fix slight misplacement of halos on non-default zoom

--- src/unit_frame.cpp
+++ src/unit_frame.cpp
@@ -391,14 +391,14 @@ void unit_frame::redraw(const int frame_time,bool first_time,const gamemap::loca
 				break;
 		}
 		if(direction != gamemap::location::SOUTH_WEST && direction != gamemap::location::NORTH_WEST) {
-			*halo_id = halo::add(x+current_data.halo_x,
-					y+current_data.halo_y,
+			*halo_id = halo::add(x+current_data.halo_x* game_display::get_singleton()->get_zoom_factor(),
+					y+current_data.halo_y* game_display::get_singleton()->get_zoom_factor(),
 					current_data.halo,
 					gamemap::location(-1, -1),
 					orientation);
 		} else {
-			*halo_id = halo::add(x-current_data.halo_x,
-					y+current_data.halo_y,
+			*halo_id = halo::add(x-current_data.halo_x* game_display::get_singleton()->get_zoom_factor(),
+					y+current_data.halo_y* game_display::get_singleton()->get_zoom_factor(),
 					current_data.halo,
 					gamemap::location(-1, -1),
 					orientation);
