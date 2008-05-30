760274ff6e83fc5b8488da68a6ff0e888feb3bed
Piotr Cychowski <cycholka@gna.org>
2008-05-30 09:32:31 +0000

    Revert scrapping the win32 is_internal_directory code...
    ...after hitting completely unforeseen and highly annoying side effects.

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -557,11 +557,23 @@ std::string read_map(const std::string& name)
 
 static bool is_directory_internal(const std::string& fname)
 {
+#ifdef _WIN32
+	_finddata_t info;
+	const long handle = _findfirst((fname + "/*").c_str(),&info);
+	if(handle >= 0) {
+		_findclose(handle);
+		return true;
+	} else {
+		return false;
+	}
+
+#else
 	struct stat dir_stat;
 	if(::stat(fname.c_str(), &dir_stat) == -1) {
 		return false;
 	}
 	return S_ISDIR(dir_stat.st_mode);
+#endif
 }
 
 bool is_directory(const std::string& fname)
