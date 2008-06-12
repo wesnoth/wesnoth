fe5b3c5290c1ba844a7150098eb52b1117f5648c
Ali El Gariani <alinkmaze@gmail.com>
2008-06-12 10:39:51 +0000

    tiny simplification of a stat() call for win32
    (not tested but logically equivalent)

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -615,7 +615,7 @@ bool file_exists(const std::string& name)
 {
 #ifdef _WIN32
        struct stat st;
-       return (::stat(name.c_str(), &st) != 0) ? false : true;
+       return (::stat(name.c_str(), &st) == 0);
 #else     
 	struct stat st;
 	return (::stat(name.c_str(), &st) != -1);
