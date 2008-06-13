cd568bc6c3af68f6aa47537b36200e6ff0a15274
Pauli Nieminen <paniemin@cc.hut.fi>
2008-06-13 09:01:44 +0000

    Fixing compilation of unit_types.hpp with gcc 4.3

---
+++ src/disable_gcc_concept_checks.hpp
@@ -0,0 +1,27 @@
+/* $Id$ */
+/*
+   Copyright (C) 2008 by David White <dave@whitevine.net>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+
+#ifndef DISABLE_GCC_CONCEPT_CHECK_HPP_INCLUDED
+#define DISABLE_GCC_CONCEPT_CHECK_HPP_INCLUDED
+
+#ifdef _GLIBCXX_CONCEPT_CHECKS
+#undef _GLIBCXX_CONCEPT_CHECKS
+#endif
+#ifdef _GLIBCPP_CONCEPT_CHECKS
+#undef _GLIBCPP_CONCEPT_CHECKS
+#endif
+
+
+#endif
--- src/unit_types.hpp
+++ src/unit_types.hpp
@@ -14,6 +14,7 @@
 #ifndef UNIT_TYPES_H_INCLUDED
 #define UNIT_TYPES_H_INCLUDED
 
+#include "disable_gcc_concept_checks.hpp"
 #include "unit_animation.hpp"
 #include "config.hpp"
 #include "map.hpp"
