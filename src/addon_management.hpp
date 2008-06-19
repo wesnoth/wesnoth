53aa85b8a2a2c2c67b87fe12fc1e575a9243fc6d
Mark de Wever <koraq@xs4all.nl>
2008-06-19 19:21:48 +0000

    Update doxygen comment style.

--- src/addon_management.hpp
+++ src/addon_management.hpp
@@ -28,25 +28,34 @@ typedef std::pair< std::string, ADDON_TYPE > addon_list_item;
 
 void remove_local_addon(const std::string& addon);
 
-//! Gets the publish information for an add-on
-//! @param addon_name The add-on's main directory/file name.
-//! @param cfg A config object to store the add-on's properties.
+/**
+ * Gets the publish information for an add-on.
+ *
+ * @param addon_name              The add-on's main directory/file name.
+ * @param cfg                     A config object to store the add-on's
+ *                                properties.
+ */
 void get_addon_info(const std::string& addon_name, class config& cfg);
 
-//! Sets the publish information for an add-on
-//! @param addon_name The add-on's main directory/file name.
-//! @param cfg A config object from which the add-on's properties are copied.
+/**
+ * Sets the publish information for an add-on
+ *
+ * @param addon_name              The add-on's main directory/file name.
+ * @param cfg                     A config object from which the add-on's
+ *                                properties are copied.
+ */
 void set_addon_info(const std::string& addon_name, const class config& cfg);
 
-//! Returns a list of local add-ons that can be published.
+/** Returns a list of local add-ons that can be published. */
 std::vector<std::string> available_addons();
 
-//! Retrieves the names of all installed add-ons.
+/** Retrieves the names of all installed add-ons. */
 std::vector<std::string> installed_addons();
 
-//! Archives an add-on into a config object for campaignd transactions.
+/** Archives an add-on into a config object for campaignd transactions. */
 void archive_addon(const std::string& addon_name, class config& cfg);
-//! Unarchives an add-on from campaignd's retrieved config object.
+
+/** Unarchives an add-on from campaignd's retrieved config object. */
 void unarchive_addon(const class config& cfg);
 
 #endif /* !ADDON_MANAGEMENT_HPP_INCLUDED */
