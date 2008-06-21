2d49a41adb3c1e7b0f0dc033db74ff8b55fa765b
Mark de Wever <koraq@xs4all.nl>
2008-06-21 19:16:09 +0000

    Initialize all members.

--- src/video.cpp
+++ src/video.cpp
@@ -230,7 +230,7 @@ void update_whole_screen()
 {
 	update_all = true;
 }
-CVideo::CVideo() : bpp(0), fake_screen(false), help_string_(0), updatesLocked_(0)
+CVideo::CVideo() : mode_changed_(false), bpp(0), fake_screen(false), help_string_(0), updatesLocked_(0)
 {
 	const int res = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_NOPARACHUTE);
 
@@ -241,7 +241,7 @@ CVideo::CVideo() : bpp(0), fake_screen(false), help_string_(0), updatesLocked_(0
 }
 
 CVideo::CVideo( int x, int y, int bits_per_pixel, int flags)
-		 : bpp(0), fake_screen(false), help_string_(0), updatesLocked_(0)
+		 : mode_changed_(false), bpp(0), fake_screen(false), help_string_(0), updatesLocked_(0)
 {
 	const int res = SDL_Init( SDL_INIT_VIDEO | SDL_INIT_NOPARACHUTE);
 	if(res < 0) {
