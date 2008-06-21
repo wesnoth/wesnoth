3518581568206494ae901ea179f5b1a7f71cb4f3
Mark de Wever <koraq@xs4all.nl>
2008-06-21 20:52:02 +0000

    Initialize all members.

--- src/unit_types.hpp
+++ src/unit_types.hpp
@@ -327,7 +327,14 @@ public:
 
         private:
             unit_type_map_wrapper();
-            unit_type_map_wrapper(unit_type_map_wrapper& /*utf*/) {}
+            unit_type_map_wrapper(unit_type_map_wrapper& /*utf*/) :
+				types_(),
+				dummy_unit_map_(),
+				movement_types_(),
+				races_(), 
+				unit_traits_(),
+				unit_cfg_(0)
+			{}
 
             void set_unit_config(const config& unit_cfg) { unit_cfg_ = &unit_cfg; }
             void set_unit_traits(const config::child_list unit_traits) { unit_traits_ = unit_traits; }
