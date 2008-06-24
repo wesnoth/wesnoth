61a78b4f3f7e44269a63dc38fc5da140e5523ce1
Ignacio R. Morelle <shadowm@wesnoth.org>
2008-06-24 21:10:48 +0000

    Fix inexistent ~/.wesnoth/data/units dir warning on first run

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -420,6 +420,7 @@ std::string get_user_data_dir()
 		_mkdir("userdata/data/multiplayer");
 		_mkdir("userdata/data/maps");
 		_mkdir("userdata/data/maps/multiplayer");
+		_mkdir("userdata/data/units");
 		_mkdir("userdata/saves");
 		inited_dirs = true;
 	}
@@ -483,6 +484,7 @@ std::string get_user_data_dir()
 		mkdir((dir_path + "/data/multiplayer").c_str(),AccessMode);
 		mkdir((dir_path + "/data/maps").c_str(),AccessMode);
 		mkdir((dir_path + "/data/maps/multiplayer").c_str(),AccessMode);
+		mkdir((dir_path + "/data/units").c_str(),AccessMode);
 		mkdir((dir_path + "/saves").c_str(),AccessMode);
 		if(res == 0) {
 			dir = opendir(dir_path.c_str());
