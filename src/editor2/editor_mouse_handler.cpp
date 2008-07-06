f16329941d94600fdc0fb633e6ee92f9eadc2f4a
Tomasz Śniatowski <kailoran@gmail.com>
2008-07-06 16:24:04 +0100

    Add editor2's mouse handler (derived from mouse_handler_base)

---
+++ src/editor2/editor_mouse_handler.cpp
@@ -0,0 +1,40 @@
+/* $Id$ */
+/*
+   Copyright (C) 2008 by Tomasz Sniatowski <kailoran@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+//! @file action.cpp
+//! Editor action classes
+
+#include "editor_display.hpp"
+#include "editor_map.hpp"
+#include "editor_mouse_handler.hpp"
+#include "../foreach.hpp"
+
+namespace editor2 {
+
+editor_mouse_handler::editor_mouse_handler(editor_display* disp, editor_map& map)
+: mouse_handler_base(disp, map)
+{
+}
+
+void editor_mouse_handler::mouse_motion(int x, int y, const bool browse, bool update)
+{
+	if (mouse_handler_base::mouse_motion_default(x, y, update)) return;
+}
+
+void editor_mouse_handler::set_gui(editor_display* gui)
+{
+	gui_ = gui;
+}
+		
+} //end namespace editor2
---
+++ src/editor2/editor_mouse_handler.hpp
@@ -0,0 +1,34 @@
+/* $Id$ */
+/*
+   Copyright (C) 2008 by Tomasz Sniatowski <kailoran@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#ifndef EDITOR2_EDITOR_MOUSE_HANDLER_HPP
+#define EDITOR2_EDITOR_MOUSE_HANDLER_HPP
+
+#include "../mouse_handler_base.hpp"
+
+#include "editor_common.hpp"
+
+namespace editor2 {
+
+class editor_mouse_handler : public events::mouse_handler_base
+{
+public:
+	editor_mouse_handler(editor_display* disp, editor_map& map);
+	void mouse_motion(int x, int y, const bool browse, bool update);
+	void set_gui(editor_display* gui);
+};
+
+} //end namespace editor2
+
+#endif
