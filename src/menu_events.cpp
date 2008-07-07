5c63c02a364c191cc678a6abbf2d6f1f9a2090ef
Ali El Gariani <alinkmaze@gmail.com>
2008-07-07 22:33:15 +0000

    Fix bas use of implict "units/" prefix in some hardcoded image's url

--- src/menu_events.cpp
+++ src/menu_events.cpp
@@ -497,7 +497,7 @@ private:
 					str << IMAGE_PREFIX << leader->second.absolute_image();
 					leader_bools.push_back(true);
 				} else {
-					str << IMAGE_PREFIX << std::string("unknown-unit.png");
+					str << IMAGE_PREFIX << std::string("units/unknown-unit.png");
 					leader_bools.push_back(false);
 				}
 #ifndef LOW_MEM
