753dada0eebdcc35e7fd26cba8e6f4bcbcc9bc5b
Tomasz Śniatowski <kailoran@gmail.com>
2008-07-10 12:02:07 +0100

    use default gamemap::location ctor

--- src/mouse_handler_base.cpp
+++ src/mouse_handler_base.cpp
@@ -47,7 +47,7 @@ static bool command_active()
 
 mouse_handler_base::mouse_handler_base(gamemap& map)
 : minimap_scrolling_(false), dragging_(false), dragging_started_(false),
-last_hex_(-999,-999), show_menu_(false), map_(map)
+last_hex_(), show_menu_(false), map_(map)
 {
 }
 
