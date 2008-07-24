d0c07cb6dc4cadcd318ad46f5a83cc915f60adde
Greg Copeland <gtcopeland@earthlink.net>
2008-07-24 18:33:30 +0000

    Changed exception handling to cover restricted and unrestricted executions.

--- data/ais/ai_launcher.py
+++ data/ais/ai_launcher.py
@@ -10,34 +10,39 @@ import traceback
 
 print "launch has been imported!"
 
-def launch( script, restrict ):
+def launch( script, restrict, importOnly=False ):
     # Launch one of two possible environments
     # If restrict arg is True, run inside the available
     # restrictied python environment (safe.py). If restrict
-    # is False, then run without any type of restrictions.
-    if restrict:
-        print "restricted environment detected - running parse/safe"
-        try:
+    # is False, then run without any type of restrictions at
+    # all. If importOnly is True, only import but don't attempt
+    # to execute.
+    try:
+        if restrict:
+            print "restricted environment detected - running parse/safe"
             import safe
             import parse
 
             parse.paths = ""
             code, context = parse.parse( script )
-            safe.safe_exec( code, context, restrict )
+            if not importOnly:
+                safe.safe_exec( code, context, restrict )
 
-        except:
-            err = str( traceback.format_exc() )
-            raise
+        else:
+            print "unrestricted environment detected..."
+            __import__( script )
+            scrpt = sys.modules[ script ]
 
-    else:
-        print "unrestricted environment detected...running directly."
-        __import__( script )
-        scrpt = sys.modules[ script ]
+            # Call our entry points
+            if not importOnly:
+                print "Running script in unrestricted environment..."
+                scrpt.turn()
+                print "Script has completed execution."
 
-        # Call our entry points
-        scrpt.start()
-        scrpt.turn()
-        scrpt.end()
+    except:
+        err = str( traceback.format_exc() )
+        raise
+
 
     print "launch has completed."
 
