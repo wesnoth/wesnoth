6e7f6e4fd89e17b3a16d522645ca39dc70610272
Tomasz Śniatowski <kailoran@gmail.com>
2008-07-25 23:18:34 +0100

    use binary_paths_manager in start_editor to fix path-related issues

--- src/game.cpp
+++ src/game.cpp
@@ -2380,6 +2380,7 @@ editor2::EXIT_STATUS game_controller::start_editor()
     reset_game_cfg();
     defines_map_["EDITOR2"] = preproc_define();
     refresh_game_cfg();
+    const binary_paths_manager bin_paths_manager(game_config_);
 	return editor2::start(game_config_, video_);
 }
 #endif
