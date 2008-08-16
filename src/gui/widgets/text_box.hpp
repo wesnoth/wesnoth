72b02bfdd62af3dea283677686d48ea5faa07915
Mark de Wever <koraq@xs4all.nl>
2008-08-16 11:15:03 +0000

    Code and comment cleanup.

--- src/gui/widgets/text_box.hpp
+++ src/gui/widgets/text_box.hpp
@@ -17,17 +17,28 @@
 
 #include "gui/widgets/text.hpp"
 
-
 namespace gui2 {
 
-//! Class for text input history
+/** 
+ * Class for text input history.
+ *
+ * The history of text items can be stored in the preferences. This class
+ * handles that. Every item needs an id by which the history is loaded and
+ * saved.
+ */
 class ttext_history
 {
 public:
-	//! Gets history that matches id and enables or disables it.
+	/** 
+	 * Gets history that matches id.
+	 *
+	 * @param id                  The id of the history to look for.
+	 * @param enabled             The enabled state of the history.
+	 *
+	 * @returns                   The history object.
+	 */
 	static ttext_history get_history(const std::string& id, const bool enabled);
 
-	// Defaults to a disabled history
 	ttext_history() : 
 		history_(0), 
 		pos_(0), 
@@ -35,18 +46,49 @@ public:
 	{
 	}
 	
-	//! Push string into the history if it is non-empty and is not the same as the last item
-	//! updates position to end of history.
+	/**
+	 * Push string into the history.
+	 *
+	 * If the string is empty or the same as the last item in the history this
+	 * function is a nop.
+	 *
+	 * @param text                   The text to push in the history.
+	 */
 	void push(const std::string& text);
 	
-	//! One step up/down in history. Pushes text to the history if at the end.
+	/**
+	 * One step up in the history. 
+	 *
+	 * Pushes text to the history if at the end.
+	 *
+	 * @param text                The text to push in the history.
+	 *
+	 * @returns                   The current value of the history.
+	 */
 	std::string up(const std::string& text = "");
+	
+	/**
+	 * One step down in the history. 
+	 *
+	 * Pushes text to the history if at the end.
+	 *
+	 * @param text                The text to push in the history.
+	 *
+	 * @returns                   The current value of the history.
+	 */
 	std::string down(const std::string& text = "");
 	
-	//! Returns the value at the current history position. Returns "" if not enabled
-	//! or if at the end of the history.
+	/**
+	 * Gets the current history value.
+	 *
+	 * @returns                   If enabled return the current history
+	 *                            position, otherwise an empty string is
+	 *                            returned.
+	 */
 	std::string get_value() const;
 	
+	/***** ***** ***** setters / getters for members ***** ****** *****/
+
 	void set_enabled(bool enabled = true) { enabled_ = enabled; }
 	bool get_enabled() const { return enabled_; }
 	
@@ -57,18 +99,20 @@ private:
 		enabled_(enabled) 
 	{}
 	
+	/** The items in the history. */
 	std::vector<std::string>* history_;
+
+	/** The current position in the history. */
 	unsigned pos_;
 	
+	/** Is the history enabled. */
 	bool enabled_;
 };
 
-//! Class for a single line text area.
+/** Class for a single line text area. */
 class ttext_box : public ttext_
 {
-
 public:
-
 	ttext_box() :
 		ttext_(),
 		character_offset_(),
@@ -81,74 +125,133 @@ public:
 		set_wants_mouse_left_double_click(); 
 	}
 
+	/** Saves the text in the widget to the history. */
+	void save_to_history() { history_.push(get_text()); }
+
+	/***** ***** ***** setters / getters for members ***** ****** *****/
+
 	void set_history(const std::string& id) 
 		{ history_ = ttext_history::get_history(id, true); }
 	
-	void save_to_history() { history_.push(text()); }
-	void save_to_history(const std::string& text) { history_.push(text); }
-
 protected:
 	
-	// Gets the character position at the wanted place
-	// as if the mouse would click there.
-//	virtual unsigned get_position_at(tpoint& coordinate);
-
-//	void delete_selection();
-//	void copy();
-//	void paste();
-//	
-	//! Inherited from ttext_.
-	void insert_char(const Uint16 unicode);
-
-	//! Inherited from ttext_.
-	void delete_char(const bool before_cursor);
-
-	//! Inherited from ttext_.
-	void delete_selection();
-
-	//! Inherited from tcontrol.
-	void set_canvas_text();
-
-	//! Inherited from tcontrol..
+	/** Inherited from tcontrol. */
 	void set_size(const SDL_Rect& rect);
 
-	void goto_end_of_line(const bool select = false) { goto_end_of_data(select); }
-	void goto_start_of_line(const bool select = false) { goto_start_of_data(select); }
+	/** Inherited from tcontrol. */
+	void set_canvas_text();
 
-	//! Handles the selection in a mouse down or mouse move event.
-	void handle_mouse_selection(tevent_handler& event, const bool start_selection);
+	/** Inherited from ttext_. */
+	void mouse_move(tevent_handler& event);
 
 	/** Inherited from ttext_. */
 	void mouse_left_button_down(tevent_handler& event);
 
-	//! Inherited from twidget.
-	void mouse_move(tevent_handler& event);
-
-	//! Inherited from twidget.
+	/** Inherited from ttext_. */
 	void mouse_left_button_up(tevent_handler&);
 
-	//! Inherited from twidget.
+	/** Inherited from ttext_. */
 	void mouse_left_button_double_click(tevent_handler&);
+
+	/** Inherited from ttext_. */
+	void goto_end_of_line(const bool select = false) 
+		{ goto_end_of_data(select); }
+
+	/** Inherited from ttext_. */
+	void goto_start_of_line(const bool select = false) 
+		{ goto_start_of_data(select); }
+
+	/** Inherited from ttext_. */
+	void insert_char(const Uint16 unicode);
+
+	/** Inherited from ttext_. */
+	void delete_char(const bool before_cursor);
+
+	/** Inherited from ttext_. */
+	void delete_selection();
+
+	/** Handles the selection in a mouse down or mouse move event. */
+	void handle_mouse_selection(
+		tevent_handler& event, const bool start_selection);
 
 private:
 
-	void handle_key_up_arrow(SDLMod modifier, bool& handled);
-	void handle_key_down_arrow(SDLMod modifier, bool& handled);
-
-	// Clears the current line
-	void handle_key_clear_line(SDLMod modifier, bool& handled);
-
-	//! Contains the end offset of each character in the text area.
+	/** 
+	 * Contains the end offset of each character in the text area.
+	 *
+	 * This is used for placing the cursor at the proper offset and determine
+	 * the offset for a mouse click.
+	 */
 	std::vector<unsigned> character_offset_;
 
-	//! Inherited from ttext_.
-	void calculate_char_offset();
-
-	//! Gets the character at the wanted offset, everything beyond will
-	//! select the last character.
+	/**
+	 * Gets the character at the wanted offset.
+	 * 
+	 * If the click is beyond the text, the last character will be selected.
+	 * 
+	 * @param offset              The offset in the widget to test.
+	 *
+	 * @returns                   The character offset where the click occured.
+	 */
 	unsigned get_character_offset_at(const unsigned offset);
 
+	/** The history text for this widget. */
 	ttext_history history_;
+
+	/** 
+	 * The x offset in the widget where the text starts.
+	 * 
+	 * This value is needed for get_character_offset_at()
+	 */
+	unsigned text_x_offset_;
+
+	/**
+	 * The y offset in the widget where the text starts.
+	 *
+	 * Needed to determine whether a click is on the text.
+	 */
+	unsigned text_y_offset_;
+
+	/**
+	 * The height of the text itself.
+	 *
+	 * Needed to determine whether a click is on the text.
+	 */
+	unsigned text_height_;
+
+	/** Updates text_x_offset_ and text_y_offset_. */
+	void update_offsets();
+
+	/** Is the mouse in dragging mode, this affects selection in mouse move */
+	bool dragging_;
+
+	/** Inherited from ttext_. */
+	void calculate_char_offset();
+
+	/** 
+	 * Inherited from ttext_.
+	 *
+	 * Unmodified                 If there is a history, goes one item up in the
+	 *                            history, otherwise ignored.
+	 * Control                    Ignored.
+	 * Shift                      Ignored.
+	 * Alt                        Ignored.
+	 */
+	void handle_key_up_arrow(SDLMod modifier, bool& handled);
+
+	/** 
+	 * Inherited from ttext_.
+	 *
+	 * Unmodified                 If there is a history, goes one item down in the
+	 *                            history, otherwise ignored.
+	 * Control                    Ignored.
+	 * Shift                      Ignored.
+	 * Alt                        Ignored.
+	 */
+	void handle_key_down_arrow(SDLMod modifier, bool& handled);
+
+	/** Inherited from ttext_. */
+	void handle_key_clear_line(SDLMod modifier, bool& handled);
 
 	//! Inherited from tcontrol.
 	const std::string& get_control_type() const 
@@ -157,17 +260,7 @@ private:
 	//! Inherited from tcontrol.
 	void load_config_extra();
 	
-	unsigned text_x_offset_;
-	unsigned text_y_offset_;
-	unsigned text_height_;
-
-	// Updates text_x_offset_ and text_x_offset_.
-	void update_offsets();
-
-	bool dragging_;
 };
-
-
 
 } //namespace gui2
 
