7346fa5ca74cf6cc07f5dd4efb012888243589df
Pauli Nieminen <paniemin@cc.hut.fi>
2008-08-18 16:00:26 +0000

    Made minor improvements to install script

--- resources/tests.wesnoth.org/install_tests.sh
+++ resources/tests.wesnoth.org/install_tests.sh
@@ -28,6 +28,9 @@ rm Smarty-2.6.20.tar.gz
 #copy configure from template
 cp include/configuration.php.template include/configuration.php
 cp include/settup.php.template include/settup.php
+# Make files read only
+chmod a-w include/configuration.php include/settup.php
+chmod o-rx include/configuration.php include/settup.php
 
 #Make svn checkout
 svn co svn://svn.gna.org/svn/wesnoth/trunk/ trunk
@@ -43,15 +46,17 @@ cd ..
 crontab -l > crontab
 echo " 16 *   *   *   *    $CURRENT_PATH/autotester/run_unit_tests.sh" >> crontab
 crontab - < crontab
+rm crontab
 
 echo ""
 echo "******* NOTES *****"
 echo "You have to still do a few things before test are working"
 echo " 1. Add database connection information"
 echo "Edit include/settup.php file so that all relevant info to connect"
-echo "to mysql server is present there"
+echo "to mysql server is present there. Remember to make it write-protected."
 echo " 2. Configure website functionality"
-echo "Edit include/configuration.php way that you want website function"
+echo "Edit include/configuration.php way that you want website function."
+echo "Remember to make it write-protected."
 echo " 3. Configure apache"
 echo "edit paths in apache/test_website.conf and copy it to /etc/apache2/conf.d/"
 echo " 4. run unit tests to settup database"
