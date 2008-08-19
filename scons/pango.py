a059f780d7e877dafd1863058cade4fd7bd3493e
Sergey Popov <loonycyborg@gmail.com>
2008-08-19 22:01:21 +0000

    Added support for using windows GTK SDKs during cross-compile...
    ...targeting win32 to pango check.

--- scons/pango.py
+++ scons/pango.py
@@ -1,5 +1,5 @@
 # vi: syntax=python:et:ts=4
-import os
+import os, sys
 from os.path import join
 from os import environ
 from SCons.Util import AppendPath
@@ -11,9 +11,11 @@ def CheckPango(context, backend):
     if gtkdir:
         environ["PATH"] = AppendPath(environ["PATH"], join(gtkdir, "bin"))
         environ["PKG_CONFIG_PATH"] = AppendPath(environ.get("PKG_CONFIG_PATH", ""), join(gtkdir, "lib/pkgconfig"))
+        if sys.platform != "win32":
+            env["PKGCONFIG_FLAGS"] = "--define-variable=prefix=" + gtkdir
 
     try:
-        env.ParseConfig("pkg-config --libs --cflags pango" + backend)
+        env.ParseConfig("pkg-config --libs --cflags $PKGCONFIG_FLAGS pango" + backend)
         context.Result("yes")
         return True
     except OSError:
