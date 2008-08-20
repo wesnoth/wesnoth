88cb10df999c6ebf60e6d782f2b078438bf6d845
Pauli Nieminen <paniemin@cc.hut.fi>
2008-08-20 07:20:06 +0000

    Improved test website install script.
    (still missing prompt for database connection information)

--- resources/tests.wesnoth.org/install_tests.sh
+++ resources/tests.wesnoth.org/install_tests.sh
@@ -28,14 +28,9 @@ rm Smarty-2.6.20.tar.gz
 #copy configure from template
 cp include/configuration.php.template include/configuration.php
 cp include/settup.php.template include/settup.php
-# Make files read only
-chmod a-w include/configuration.php include/settup.php
-chmod o-rx include/configuration.php include/settup.php
 
 #Make svn checkout
 svn co svn://svn.gna.org/svn/wesnoth/trunk/ trunk
-#Use this instead of previous line if you want to copy existing svn tree
-#cp -R /path/to/svntree trunk
 
 #Configure scons build enviroment
 cd trunk
@@ -46,18 +41,19 @@ cd ..
 crontab -l > crontab
 echo " 16 *   *   *   *    $CURRENT_PATH/autotester/run_unit_tests.sh" >> crontab
 crontab - < crontab
-rm crontab
+
+mv apache/test_website.conf apache/test_website.conf.orig
+sed -e "s/\\(Alias [^ ]* \"\\).*\\(\"\\)/\\1${CURRENT_PATH}\\/htdocs\\/\\2/" -e "s/\\(^.*Directory \"\\).*\\(\".*$\\)/\\1${CURRENT_PATH}\\/htdocs\\/\\2/" apache/test_website.conf.orig > apache/test_website.conf
 
 echo ""
 echo "******* NOTES *****"
 echo "You have to still do a few things before test are working"
 echo " 1. Add database connection information"
 echo "Edit include/settup.php file so that all relevant info to connect"
-echo "to mysql server is present there. Remember to make it write-protected."
+echo "to mysql server is present there"
 echo " 2. Configure website functionality"
-echo "Edit include/configuration.php way that you want website function."
-echo "Remember to make it write-protected."
+echo "Edit include/configuration.php way that you want website function"
 echo " 3. Configure apache"
-echo "edit paths in apache/test_website.conf and copy it to /etc/apache2/conf.d/"
+echo "copy apache/test_website.conf to /etc/apache2/conf.d/"
 echo " 4. run unit tests to settup database"
 echo "autotester/run_unit_tests.sh"
