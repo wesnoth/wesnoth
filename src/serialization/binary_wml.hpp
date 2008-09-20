23f5b13eb1147e122f0ef128792701485bea8dc3
Mark de Wever <koraq@xs4all.nl>
2008-09-20 09:44:02 +0000

    Update doxygen comment style.

--- src/serialization/binary_wml.hpp
+++ src/serialization/binary_wml.hpp
@@ -23,7 +23,7 @@
 
 class config;
 
-//! Holds the schema by which config objects can be compressed and decompressed.
+/** Holds the schema by which config objects can be compressed and decompressed. */
 struct compression_schema
 {
 	compression_schema() : char_to_word(), word_to_char() { }
@@ -37,13 +37,17 @@ struct compression_schema
 
 //  Read and write compressed data. 
 
-//! Write compressed data. 
-//! The schema will be created and written with the data. 
-//! However if you are making successive writes (e.g. a network connection),
-//! you can re-use the same schema on the sending end, and the receiver can store 
-//! the schema, meaning that the entire schema won't have to be transmitted each time.
+/**
+ * Write compressed data. 
+ *
+ * The schema will be created and written with the data.  However if you are
+ * making successive writes (e.g. a network connection), you can re-use the
+ * same schema on the sending end, and the receiver can store the schema,
+ * meaning that the entire schema won't have to be transmitted each time.
+ */
 void write_compressed(std::ostream &out, config const &cfg, compression_schema &schema);
-//! Read compressed data. 
+
+/** Read compressed data. */
 void read_compressed(config &cfg, std::istream &in, compression_schema &schema); //throws config::error
 
 void write_compressed(std::ostream &out, config const &cfg);
