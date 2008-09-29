7d3fcffee6def689058417159ef15150000567b1
Alexander van Gessel <ai0867@gmail.com>
2008-09-29 09:38:09 +0100

    Add the help section to wmlgrammar.py.

--- data/tools/wesnoth/wmlgrammar.py
+++ data/tools/wesnoth/wmlgrammar.py
@@ -9,8 +9,8 @@ grammar = {
 # This is the top-level pseudo-tag that everything is a child of.
 # It should have no keys
 'WML' : (
-    [ 'about', 'game_config', 'textdomain', ],
-    [] ),
+    [ 'about', 'game_config', 'help', 'textdomain', ],
+    []),
 # Attempt to keep everything alphabetically ordered
 'about' : (
     [ 'entry', ],
@@ -27,12 +27,24 @@ grammar = {
 'game_config' : (
     [ 'color_palette', 'color_range', 'server', ],
     [ 'ally_ball_image', 'base_income', 'buttons_x', 'buttons_y', 'buttons_padding', 'cross_image', 'default_defeat_music', 'default_victory_music', 'defense_color_scale', 'ellipsis_image', 'enemy_ball_image', 'energy_image', 'flag_icon_image', 'flag_image', 'flag_rgb', 'footprint_prefix', 'footprint_teleport_enter', 'footprint_teleport_exit', 'grid_image', 'hp_bar_scaling', 'icon', 'kill_experience', 'level_image', 'lobby_music', 'lobby_refresh', 'logo', 'logo_x', 'logo_y', 'moved_ball_image', 'observer_image', 'partmoved_ball_image', 'poison_amount', 'recall_cost', 'rest_heal_amount', 'terrain_mask_image', 'tip_padding', 'tip_width', 'tip_x', 'title', 'title_music', 'tod_bright_image', 'unmoved_ball_image', 'unreachable_image', 'village_income', 'wesnothd_name', 'xp_bar_scaling', ]),
+'help' : (
+    [ 'section', 'topic', 'toplevel' ],
+    []),
+'section' : (
+    [],
+    [ 'generator', 'id', 'sections_generator', 'sort_sections', 'sort_topics', 'title', 'topics', ]),
 'server' : (
     [],
     [ 'address', 'name', ]),
 'textdomain' : (
     [],
     [ 'name', ]),
+'topic' : (
+    [],
+    [ 'generator', 'id', 'text', 'title', ]),
+'toplevel' : (
+    [],
+    [ 'sections', 'topics', ]),
 
 }
 
