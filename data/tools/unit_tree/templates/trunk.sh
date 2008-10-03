b191839336aea4e1361ba7fb9efae34e0ebf633a
Eric S. Raymond <esr@thyrsus.com>
2008-10-03 16:13:00 +0000

    Remove the old Perl unit-tree code...
    ...now that we have wmlunits working and maintained.

--- data/tools/unit_tree/templates/trunk.sh
+++
@@ -1,5 +0,0 @@
-svn co http://svn.gna.org/svn/wesnoth/trunk/data/units data/units
-svn co -N http://svn.gna.org/svn/wesnoth/trunk/data data
-svn co http://svn.gna.org/svn/wesnoth/trunk/data/factions data/factions
-perl units.pl
-
