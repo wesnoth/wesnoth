e48fe86ee6c4b3643dd29eb18d28136c5d68305b
Gunter Labes <soliton@wesnoth.org>
2008-10-04 17:28:12 +0000

    connect to 1.4 by default

--- data/tools/wesnoth/campaignserver_client.py
+++ data/tools/wesnoth/campaignserver_client.py
@@ -3,7 +3,7 @@ import wesnoth.wmldata as wmldata
 
 class CampaignClient:
     # First port listed will be used as default.
-    portmap = (("15003", "1.5.x"), ("15004", "1.2.x"), ("15005", "1.4.x"))
+    portmap = (("15005", "1.4.x"), ("15003", "1.5.x"), ("15004", "1.2.x"))
 
     def __init__(self, address = None):
         """
