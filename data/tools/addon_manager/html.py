72111578453f4136092350be1b5e442264544740
Elias Pschernig <elias@pschernig.at>
2008-10-12 13:05:11 +0000

    addon_manager:
    - Table sorting now displays nice arrows (using the "jquery"
      javascript library for this now).
    
    - Auto-create --tar folder.

--- data/tools/addon_manager/html.py
+++ data/tools/addon_manager/html.py
@@ -1,3 +1,4 @@
+# encoding: utf8
 import time, os, glob, sys
 
 def output(path, url, data):
@@ -13,7 +14,19 @@ def output(path, url, data):
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link rel=stylesheet href="style.css" type="text/css">
-<script src="sorttable.js"></script>
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="tablesorter.js"></script>
+<script type="text/javascript">
+$(document).ready(function() 
+{ 
+    $("#campaigns").tablesorter(
+    {
+        headers: { 0: { sorter: false} }
+    }
+    ); 
+} 
+); 
+</script>
 </head>
 <body>""")
 
@@ -26,24 +39,25 @@ def output(path, url, data):
 <div class="topnav">
 <a href="index.html">Wesnoth Addons</a>
 </div>
+<div class="main">
 <p>To install an add-on please go to the title screen of Battle for Wesnoth. Select "Add-ons" from the menu and click "OK" to connect to add-ons.wesnoth.org.
 Select the add-on you want to install from the list and click "OK". The download will commence immediately. Wesnoth will then automatically install and load the add-on so you can use it.</p>
 <p>Note: Hover over the icons to see the description of the add-on.</p>
-<br/>""")
+""")
 
-    os.system("cp -u data/tools/addon_manager/style.css " + path)
-    os.system("cp -u data/tools/addon_manager/sorttable.js " + path)
+    for name in ["style.css", "jquery.js", "tablesorter.js",
+        "asc.gif", "bg.gif", "desc.gif"]:
+        os.system("cp -u data/tools/addon_manager/" + name + " " + path)
 
     campaigns = data.get_or_create_sub("campaigns")
-    w("<table class=\"sortable\" id=\"campaigns\">")
+    w("<table class=\"tablesorter\" id=\"campaigns\">")
+    w("<thead>")
     w("<tr>")
-    w("<th class=\"sorttable_nosort\"></th>")
-    w("<th>Addon</th>")
-    w("<th>Size</th>")
-    w("<th>Traffic</th>")
-    w("<th>Date</th>")
-    w("<th>Notes</th>")
+    for header in ["Icon", "Addon", "Size", "Traffic", "Date", "Notes"]:
+        w("<th>%s&nbsp;&nbsp;&nbsp;</th>" % header)
     w("</tr>")
+    w("</thead>")
+    w("<tbody>")
     for campaign in campaigns.get_all("campaign"):
         v = campaign.get_text_val
         w("<tr>")
@@ -84,19 +98,21 @@ Select the add-on you want to install from the list and click "OK". The download
         else:
             w("</td>")
         downloads = int(v("downloads", "0"))
-        w("<td sorttable_customkey=\"%d\"><b>%d</b> down<br/>" % (downloads,
-            downloads))
+        w("<td><b>%d</b> down<br/>" % (downloads))
         w("%s up</td>" % v("uploads", "unknown"))
         timestamp = int(v("timestamp", "0"))
         t = time.localtime(timestamp)
-        w("<td sorttable_customkey=\"%d\">%s</td>" % (timestamp,
+        w("<td><span class=\"hidden\">%d</span>%s</td>" % (timestamp,
             time.strftime("%b %d %Y", t)))
         w("<td>%s</td>" % v("translate", ""))
         w("</tr>")
+    w("</tbody>")
     w("</table>")
 
     w("""\
+</div>
 <div id="footer">
+<p><a href="http://www.wesnoth.org/wiki/Site_Map">Site map</a></p>
 <p><a href="http://www.wesnoth.org/wiki/Wesnoth:Copyrights">Copyright</a> &copy; 2003-2008 The Battle for Wesnoth</p>
 <p>Supported by <a href="http://www.jexiste.fr/">Jexiste</a></p>
 </div>
