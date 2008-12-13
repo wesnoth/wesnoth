3409bc090ef4f67e710fb95be3688816d47514e9
Gunter Labes <soliton@wesnoth.org>
2008-12-13 21:25:17 +0000

    fixed white space errors and simplified again

--- src/ai.cpp
+++ src/ai.cpp
@@ -369,20 +369,20 @@ bool ai::recruit_usage(const std::string& usage)
 	if (found) {
 		LOG_AI << "No available units to recruit that come under the price.\n";
 	} else if (usage != "")	{
-	  //FIXME: This message should be suppressed when WML author
-	  //chooses the default recruitment pattern.
-		const std::string warning1 = "At difficulty level " + 
+		//FIXME: This message should be suppressed when WML author
+		//chooses the default recruitment pattern.
+		const std::string warning = "At difficulty level " +
 			state_.sog().difficulty + ", trying to recruit a:" + 
 			usage + " but no unit of that type (usage=) is"
 			" available. Check the recruit and [ai]"
 			" recruitment_pattern keys for team '" + 
-	                current_team().name() + "' (";  
-         	const std::string warning2 = ")"
-	                " against the usage key of the"
+			current_team().name() + "' (" +
+			lexical_cast<std::string>(info_.team_num) + ")"
+			" against the usage key of the"
 			" units in question! Removing invalid"
 			" recruitment_pattern entry and continuing...\n";
-		WRN_AI << warning1 << info_.team_num << warning2;
-		lg::wml_error << warning1 << info_.team_num << warning2;
+		WRN_AI << warning;
+		lg::wml_error << warning;
 		return current_team().remove_recruitment_pattern_entry(usage);
 	}
 	return false;
