0282f6745f2d7c9be9d13f7862ed1d8014674184
Tomasz Śniatowski <kailoran@gmail.com>
2008-12-27 23:21:29 +0100

    Add missing header with $Id and license info and set svn properties.
    The files were split from string_utils.?pp so I'm copying the
    copyright notices from there.

--- src/formula_string_utils.cpp
+++ src/formula_string_utils.cpp
@@ -1,3 +1,18 @@
+/* $Id$ */
+/*
+   Copyright (C) 2003 by David White <dave@whitevine.net>
+   Copyright (C) 2005 - 2008 by Guillaume Melquiond <guillaume.melquiond@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
 #include "formula_string_utils.hpp"
 #include "log.hpp"
 #include "formula.hpp"
