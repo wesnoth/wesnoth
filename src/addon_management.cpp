9d1a3a94dadeb9c7709d47c982123a51c4fc6b06
Ignacio R. Morelle <shadowm@wesnoth.org>
2009-01-12 20:33:24 +0000

    Fix a small flaw in which we are putting a std::ostringstream's
    streambuf address into stderr, instead of putting its real contents.

--- src/addon_management.cpp
+++ src/addon_management.cpp
@@ -84,7 +84,7 @@ bool remove_local_addon(const std::string& addon, std::string* log)
 	if(log != NULL) {
 		*log = messages.str();
 	}
-	ERR_CFG << "removal of add-on " << addon << " failed:\n" << messages;
+	ERR_CFG << "removal of add-on " << addon << " failed:\n" << messages.str();
 
 	return ret;
 }
