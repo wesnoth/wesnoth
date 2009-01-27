a98c998c4f565e4259d6acc9890f35e9008cc550
Ignacio R. Morelle <shadowm@wesnoth.org>
2009-01-27 01:20:03 +0000

    Indented license header in pathfind.cpp, pathutils.hpp.

--- src/pathutils.hpp
+++ src/pathutils.hpp
@@ -1,15 +1,15 @@
 /* $Id$ */
 /*
-Copyright (C) 2003 - 2009 by David White <dave@whitevine.net>
-Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+   Copyright (C) 2003 - 2009 by David White <dave@whitevine.net>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2
-or at your option any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
 
-See the COPYING file for more details.
+   See the COPYING file for more details.
 */
 
 /** @file pathutils.hpp */
