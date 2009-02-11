124dfbc6a07dad1206fd87629061e287452d4932
Ignacio R. Morelle <shadowm@wesnoth.org>
2009-02-11 23:04:59 +0000

    Indent license headings.

--- src/astarnode.hpp
+++ src/astarnode.hpp
@@ -1,16 +1,16 @@
 /* $Id$ */
 /*
-Copyright (C) 2003 by David White <dave@whitevine.net>
-Copyright (C) 2005 - 2009 by Guillaume Melquiond <guillaume.melquiond@gmail.com>
-Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+   Copyright (C) 2003 by David White <dave@whitevine.net>
+                 2005 - 2009 by Guillaume Melquiond <guillaume.melquiond@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2
-or at your option any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
 
-See the COPYING file for more details.
+   See the COPYING file for more details.
 */
 
 #ifndef ASTARNODE_H_INCLUDED
