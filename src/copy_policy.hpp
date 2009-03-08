38f4f2fc030b4fa75b36471836d61b5b54b21006
Mark de Wever <koraq@xs4all.nl>
2009-03-08 18:45:02 +0000

    Doxygen fixes.

--- src/copy_policy.hpp
+++ src/copy_policy.hpp
@@ -13,7 +13,7 @@
 */
 
 /**
- * @file copy_policies.hpp.
+ * @file copy_policy.hpp.
  * Defines the copy policies for classes.
  *
  * When a class can have multiple copy policies these templates can be used.
