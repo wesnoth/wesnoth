8d75f9f2992d08336edc79fffd28ab0649005d52
Ignacio R. Morelle <shadowm@wesnoth.org>
2009-03-13 00:34:34 +0000

    Shut up some "deprecated WML" messages for 1.6 as requested by Ivanovic.

--- src/ai_move.cpp
+++ src/ai_move.cpp
@@ -525,7 +525,8 @@ std::pair<map_location,map_location> ai::choose_move(std::vector<target>& target
 			if(tg->type == target::VILLAGE) {
 				if(current_team().ai_parameters().has_attribute("scout_village_targetting")) {
 					rating *= lexical_cast_default<int>(current_team().ai_parameters()["scout_village_targetting"],3);
-					lg::wml_error << "[ai] the 'scout_village_targetting' attribute is deprecated, support will be removed in version 1.7.0; use 'scout_village_targeting' instead\n";
+					// TODO: re-enable next line after forking 1.6
+					//lg::wml_error << "[ai] the 'scout_village_targetting' attribute is deprecated, support will be removed in version 1.7.0; use 'scout_village_targeting' instead\n";
 				}
 				else {
 					rating *= lexical_cast_default<int>(current_team().ai_parameters()["scout_village_targeting"],3);
@@ -560,7 +561,8 @@ std::pair<map_location,map_location> ai::choose_move(std::vector<target>& target
 	bool simple_targeting = false;
 	if(current_team().ai_parameters().has_attribute("simple_targetting")) {
 		simple_targeting = utils::string_bool(current_team().ai_parameters()["simple_targetting"]);
-		lg::wml_error << "[ai] the 'simple_targetting' attribute is deprecated, support will be removed in version 1.7.0; use 'simple_targeting' instead\n";
+		// TODO: re-enable next line after forking 1.6
+ 		//lg::wml_error << "[ai] the 'simple_targetting' attribute is deprecated, support will be removed in version 1.7.0; use 'simple_targeting' instead\n";
 	}
 	else {
 		simple_targeting = utils::string_bool(current_team().ai_parameters()["simple_targeting"]);
