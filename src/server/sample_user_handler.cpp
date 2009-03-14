d76af99c2a9cbfb74302340ad079b18b617c4c7e
Thomas Baumhauer <thomas.baumhauer@gmail.com>
2009-03-14 18:17:22 +0000

    same fix as in 2009-03-14T18:15:31Z!thomas.baumhauer@gmail.com for the sampel user handler

--- src/server/sample_user_handler.cpp
+++ src/server/sample_user_handler.cpp
@@ -85,6 +85,7 @@ bool suh::user_is_moderator(const std::string& name) {
 }
 
 void suh::set_is_moderator(const std::string& name, const bool& is_moderator) {
+	if(!user_exists(name)) return;
 	users_[name].is_moderator = is_moderator;
 }
 
