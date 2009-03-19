8c3b441752a627e2a40fe6e9f0745645e7f9796d
Thomas Baumhauer <thomas.baumhauer@gmail.com>
2009-03-19 15:03:33 +0000

    fix unregistered nick error: remove untranslatable space

--- src/multiplayer.cpp
+++ src/multiplayer.cpp
@@ -357,8 +357,8 @@ static server_type open_connection(game_display& disp, const std::string& origin
 					} else if((*error)["error_code"] == MP_NAME_RESERVED_ERROR) {
 						error_message = vgettext("The nick '$nick' is reserved and cannot be used by players.", i18n_symbols);
 					} else if((*error)["error_code"] == MP_NAME_UNREGISTERED_ERROR) {
-						error_message = vgettext("The nick '$nick' is not registered on this server.", i18n_symbols) +
-								+ " " + _(" This server disallows unregistered nicks.");
+						error_message = vgettext("The nick '$nick' is not registered on this server.", i18n_symbols)
+								+ _(" This server disallows unregistered nicks.");
 					} else if((*error)["error_code"] == MP_PASSWORD_REQUEST) {
 						error_message = vgettext("The nick '$nick' is registered on this server.", i18n_symbols);
 					} else if((*error)["error_code"] == MP_PASSWORD_REQUEST_FOR_LOGGED_IN_NAME) {
