a0b356fa5b4a3afa34b6e9c27cbec465c0db08a8
Daniel Franke <dfoxfranke@gmail.com>
2009-03-22 04:09:56 +0000

    Fix 'Trying to open file with empty name' stdout spam in MP lobby

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -680,7 +680,9 @@ void write_file(const std::string& fname, const std::string& data)
 
 std::string read_map(const std::string& name)
 {
-	std::string res = read_file(get_wml_location("maps/" + name));
+        std::string res;
+        std::string map_location = get_wml_location("maps/" + name);
+        if(!map_location.empty()) res = read_file(map_location);
 
 	if (res.empty()) {
 		res = read_file(get_user_data_dir() + "/editor/maps/" + name);
