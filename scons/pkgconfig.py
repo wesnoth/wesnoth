36b0a414beb4698ecce3fd28488833d6a376a0e2
Sergey Popov <loonycyborg@gmail.com>
2009-03-22 22:31:40 +0000

    Refactoring.

--- scons/pkgconfig.py
+++ scons/pkgconfig.py
@@ -2,15 +2,21 @@
 
 import os
 
-def CheckPKG(context, name):
-    env = context.env
-    context.Message( 'Checking for %s... ' % name )
+def run_pkg_config(env, name):
     try:
         env["ENV"]["PKG_CONFIG_PATH"] = os.environ.get("PKG_CONFIG_PATH")
         env.ParseConfig("pkg-config --libs --cflags $PKGCONFIG_FLAGS '" + name + "'")
-        context.Result("yes")
         return True
     except OSError:
+        return False
+
+def CheckPKG(context, name):
+    env = context.env
+    context.Message( 'Checking for %s... ' % name )
+    if run_pkg_config(env, name):
+        context.Result("yes")
+        return True
+    else:
         context.Result("no")
         return False
 
