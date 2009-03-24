ad60d79bb4c7c978620e5e8075c56621b0a82662
Nils Kneuper <crazy-ivanovic@gmx.net>
2009-03-24 14:18:45 +0000

    add the searching for sendfile/round to the cmake recipe
    now a file for the defines is created automatically...

---
+++ config.h.cmake
@@ -0,0 +1,8 @@
+/*this file is used to automatically set some defines and is used in the CMake recipe*/
+#cmakedefine HAVE_STDLIB_H 1
+#cmakedefine HAVE_UNISTD_H 1
+#cmakedefine HAVE_POLL_H 1
+#cmakedefine HAVE_SYS_POLL_H 1
+#cmakedefine HAVE_SYS_SELECT_H 1
+#cmakedefine HAVE_SENDFILE 1
+#cmakedefine HAVE_ROUND 1
