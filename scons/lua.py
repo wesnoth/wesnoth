01de0050e6e75b73be040025c3f03e2d6ac8db5b
Sergey Popov <loonycyborg@gmail.com>
2009-03-27 13:48:45 +0000

    Added some diagnostic output to lua and pkg-config checks.
    (written to build/config.log)

--- scons/lua.py
+++ scons/lua.py
@@ -16,11 +16,12 @@ def CheckLua(context, require_version):
         env.Append(LIBPATH = ["$luadir"], CPPPATH = ["$luadir/include"], LIBS = "lua" + version)
         found = True
     else:
-        found = run_pkg_config(env, "lua >= " + require_version) or run_pkg_config(env, "lua" + version + " >= " + require_version)
+        found = run_pkg_config(context, "lua >= " + require_version) or run_pkg_config(context, "lua" + version + " >= " + require_version)
         if not found:
             try:
                 prefix, include = find_include([env["prefix"]], "lualib.h", "", not env["host"])[0]
                 found = True
+                context.Log("Found Lua header " + include + ".\n")
                 env.Append(LIBPATH = [join(prefix, "lib")], CPPPATH = [join(prefix, "include")], LIBS = ["lua"])
             except IndexError:
                 pass
--- scons/pkgconfig.py
+++ scons/pkgconfig.py
@@ -2,18 +2,21 @@
 
 import os
 
-def run_pkg_config(env, name):
+def run_pkg_config(context, name):
+    env = context.env
     try:
         env["ENV"]["PKG_CONFIG_PATH"] = os.environ.get("PKG_CONFIG_PATH")
         env.ParseConfig("pkg-config --libs --cflags --silence-errors $PKGCONFIG_FLAGS \"" + name + "\"")
+        context.Log("Found '" + name + "' with pkg-config.\n")
         return True
     except OSError:
+        context.Log("Failed to find '" + name + "' with pkg-config.\n")
         return False
 
 def CheckPKG(context, name):
     env = context.env
     context.Message( 'Checking for %s... ' % name )
-    if run_pkg_config(env, name):
+    if run_pkg_config(context, name):
         context.Result("yes")
         return True
     else:
