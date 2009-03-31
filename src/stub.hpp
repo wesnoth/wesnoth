5a3463fa2e115d18787017e0499231456d35dba0
Ignacio R. Morelle <shadowm@wesnoth.org>
2009-03-31 16:05:26 +0000

    Little header file that defines a macro for stub code.

---
+++ src/stub.hpp
@@ -0,0 +1,11 @@
+/* $Id$ */
+/*
+   By Ignacio R. Morelle <shadowm2006@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This source code file is not copyrighted.
+*/
+
+#include <boost/current_function.hpp>
+#define STUB() \
+	std::cerr << "Ouch[stub]: " << BOOST_CURRENT_FUNCTION << " [at " << __FILE__ << ":" << __LINE__ << "]\n"
