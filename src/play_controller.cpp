9d334a0ba9e3775e6c40b8349bab133c2c28dd51
Jörg Hinrichs <joerg.hinrichs@alice-dsl.de>
2009-04-01 23:49:26 +0000

    Fix engine problem (loading a savegame from turn 1...
    ...will give all subsequent teams income where it should not do so).

--- src/play_controller.cpp
+++ src/play_controller.cpp
@@ -438,7 +438,9 @@ void play_controller::init_side(const unsigned int team_index, bool /*is_replay*
 	// Healing/income happen if it's not the first turn of processing,
 	// or if we are loading a game, and this is not the player it started with.
 	// FIXME: This does not work correct if loading a save of turn 1 (turn_refresh should be false but is true for every subsequent side)
-	const bool turn_refresh = status_.turn() > start_turn_ || (loading_game_ && team_index != (first_player_ - 1));
+	const bool turn_refresh = ( (status_.turn() > start_turn_) || (loading_game_ && team_index != (first_player_ - 1)) ) 
+								&& (start_turn_ > 1);
+
 
 	if(turn_refresh) {
 		for(unit_map::iterator i = units_.begin(); i != units_.end(); ++i) {
