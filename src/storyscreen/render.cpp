d30d55ae0104b67a6d7f28110949a1056968cc98
Ignacio R. Morelle <shadowm@wesnoth.org>
2009-04-04 23:47:49 +0000

    Add stub files for the storyscreen rendering code.

---
+++ src/storyscreen/render.cpp
@@ -0,0 +1,30 @@
+/* $Id$ */
+/*
+   Copyright (C) 2009 by Ignacio R. Morelle <shadowm2006@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+/**
+ * @file storyscreen/render.cpp
+ * Storyscreen pages renderer.
+ * @todo Translate relevant parts to GUI2.
+ */
+
+#include "global.hpp"
+#include "asserts.hpp"
+#include "foreach.hpp"
+#include "storyscreen/page.hpp"
+#include "storyscreen/render.hpp"
+
+#include "video.hpp"
+
+// TODO: remove when completed
+#include "stub.hpp"
---
+++ src/storyscreen/render.hpp
@@ -0,0 +1,24 @@
+/* $Id$ */
+/*
+   Copyright (C) 2009 by Ignacio R. Morelle <shadowm2006@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+/**
+ * @file storyscreen/render.hpp
+ * Storyscreen pages rendering interface.
+ * @todo Translate relevant parts to GUI2.
+ */
+
+#ifndef STORYSCREEN_RENDER_HPP_INCLUDED
+#define STORYSCREEN_RENDER_HPP_INCLUDED
+
+#endif /* !STORYSCREEN_RENDER_HPP_INCLUDED */
