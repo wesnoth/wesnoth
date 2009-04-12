685fbf29a46048d92b891e9c4d6694c519fb0754
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2009-04-12 08:39:07 +0000

    Made --dummy-locales option actually...
    ...depend on support for dummy locales. Removed deprecated option
    --dummylocales.

--- src/game.cpp
+++ src/game.cpp
@@ -401,11 +401,12 @@ game_controller::game_controller(int argc, char** argv) :
 		} else if(val == "-e" || val == "--editor") {
 			jump_to_editor_ = true;
 #endif
-		} else if(val == "--dummylocales") {
-			std::cerr << "--dummylocales is deprecated use --dummy-locales instead.\n";
-			game_config::use_dummylocales = true;
 		} else if(val == "--dummy-locales") {
+#ifdef USE_DUMMYLOCALES
 			game_config::use_dummylocales = true;
+#else
+			std::cerr << "Option --dummy-locales ignored: support was not compiled in.\n";
+#endif
 		} else if(val[0] == '-') {
 			std::cerr << "unknown option: " << val << std::endl;
 			throw config::error("unknown option");
