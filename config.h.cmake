8c50425a76aa7e81f96dc55c064c592791990e13
Nils Kneuper <crazy-ivanovic@gmx.net>
2009-04-24 08:41:27 +0000

    port autotools change for BSD to cmake recipe:
    Conditionalized sendfile support on the presence of sys/sendfile.h,
    since FreeBSD version is slightly different.

--- config.h.cmake
+++ config.h.cmake
@@ -4,5 +4,6 @@
 #cmakedefine HAVE_POLL_H 1
 #cmakedefine HAVE_SYS_POLL_H 1
 #cmakedefine HAVE_SYS_SELECT_H 1
+#cmakedefine HAVE_SYS_SENDFILE_H 1
 #cmakedefine HAVE_SENDFILE 1
 #cmakedefine HAVE_ROUND 1
