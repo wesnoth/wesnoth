0feafc99be4cefac5ba3ae79545ab94c868ceb96
Gunter Labes <soliton@wesnoth.org>
2009-05-08 13:58:45 +0000

    don't treat positional parameters as the port (-p should be used)

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -2508,8 +2508,6 @@ int main(int argc, char** argv) {
 		} else if (val[0] == '-') {
 			ERR_SERVER << "unknown option: " << val << "\n";
 			return 0;
-		} else {
-			port = atoi(argv[arg]);
 		}
 	}
 
