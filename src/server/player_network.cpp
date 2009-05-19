7347e99e74f6a800305a9a1f3bfe7b9abd8f48cd
Tomasz Śniatowski <kailoran@gmail.com>
2009-05-19 22:49:27 +0100

    add/modify header copyright info for new files

--- src/server/player_network.cpp
+++ src/server/player_network.cpp
@@ -1,3 +1,18 @@
+/* $Id$ */
+/*
+   Copyright (C) 2003 - 2009 by David White <dave@whitevine.net>
+   Copyright (C) 2009 by Tomasz Sniatowski <kailoran@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
 #include "player_network.hpp"
 
 namespace wesnothd {
