01a541b1a22919e3e6084dfa10ee1bc5bd22c95b
Thomas Baumhauer <thomas.baumhauer@gmail.com>
2009-05-22 13:49:11 +0000

    added copyright/license info to server/mail.*

--- src/server/mail.cpp
+++ src/server/mail.cpp
@@ -1,3 +1,17 @@
+/* $Id$ */
+/*
+   Copyright (C) 2008 - 2009 by Thomas Baumhauer <thomas.baumhauer@NOSPAMgmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
 #include "mail.hpp"
 
 #include <iostream>
