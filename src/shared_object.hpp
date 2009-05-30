e614510b8329696f83ab37016aa85f378b0d356d
Chris Hopman <cjhopman@gmail.com>
2009-05-30 18:41:00 +0000

    add copyright notice

--- src/shared_object.hpp
+++ src/shared_object.hpp
@@ -1,3 +1,16 @@
+/*
+   Copyright (C) 2009 by Chris Hopman <cjhopman@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
 #include <climits>
 #include <cassert>
 #include <boost/multi_index_container.hpp>
