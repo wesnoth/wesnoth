2d1d06924271eabc5f5edc79f5519b909704a0b5
Gunter Labes <soliton@wesnoth.org>
2009-06-15 08:11:01 +0000

    fixed bug #13689: Error message contains unnecessary `^'

--- src/game.cpp
+++ src/game.cpp
@@ -1372,7 +1372,7 @@ void game_controller::reset_translations()
 	} catch(game::error& e) {
 		ERR_CONFIG << "Error loading game configuration files\n";
 		gui2::show_error_message(disp().video(), _("Error loading game configuration files: '") +
-			font::nullify_markup(e.message) + _("' (The game will now exit)"));
+			e.message + _("' (The game will now exit)"));
 		throw;
 	}
 
