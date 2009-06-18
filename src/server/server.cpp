b09e43b5b23ede1f68f23c74ccdb542dd120b433
Gunter Labes <soliton@wesnoth.org>
2009-06-18 10:03:09 +0000

    removed the useless ghost user number

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -529,8 +529,7 @@ void server::dump_stats(const time_t& now) {
 	LOG_SERVER << "Statistics:"
 		<< "\tnumber_of_games = " << games_.size()
 		<< "\tnumber_of_users = " << players_.size()
-		 << "\tnumber_of_ghost_users = " << ghost_players_.size()
-		<< "\trooms_users = " << rooms_.lobby().size() << "\n";
+		<< "\tlobby_users = " << rooms_.lobby().size() << "\n";
 }
 
 void server::clean_user_handler(const time_t& now) {
@@ -1250,7 +1249,6 @@ std::string server::process_command(const std::string& query, const std::string&
 	} else if (command == "stats") {
 		out << "Number of games = " << games_.size()
 			<< "\nTotal number of users = " << players_.size()
-			<< "\nNumber of ghost users = " << ghost_players_.size()
 			<< "\nNumber of users in the lobby = " << rooms_.lobby().size();
 		return out.str();
 	} else if (command == "metrics") {
