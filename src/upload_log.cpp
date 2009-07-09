d95588dd6f47f392e0aa7d0dc0d71eb804f2c57f
Eugen Jiresch <e0204097@student.tuwien.ac.at>
2009-07-09 13:47:34 +0000

    removed failing assertion about player_info/teams...
    ...in upload_log.cpp, proper solution is work in progress (player_info
    removal)

--- src/upload_log.cpp
+++ src/upload_log.cpp
@@ -262,8 +262,7 @@ void upload_log::start(game_state &state, const team &team,
 	}
 
 	/** @todo FIXME: Assumes first player is "us"; is that valid? */
-	player_info &player = state.players.begin()->second;
-	assert(player.available_units.size() == team.recall_list().size());
+	//player_info &player = state.players.begin()->second; //FIXME: fix assumptions about the first player and corresponding team
 	for (std::vector<unit>::const_iterator it = team.recall_list().begin();
 		 it != team.recall_list().end();
 		 ++it) {
