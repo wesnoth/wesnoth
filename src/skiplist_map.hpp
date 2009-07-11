b701a14e5e1b4ba67d57b74971d3213451f3576f
Chris Hopman <cjhopman@gmail.com>
2009-07-11 09:01:44 +0000

    add skiplist copyright notices

--- src/skiplist_map.hpp
+++ src/skiplist_map.hpp
@@ -1,3 +1,16 @@
+/*
+   Copyright (C) 2009 by Chris Hopman <cjhopman@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
 #ifndef SKIPLIST_MAP_HPP_INCLUDED
 #define SKIPLIST_MAP_HPP_INCLUDED
 
