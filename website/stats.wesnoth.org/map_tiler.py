6964003f6a92d263d14e08d3c957f00d558b1f66
Gregory Shikhman <cornmander@cornmander.com>
2009-07-19 03:11:00 +0000

    Added zoom level support to the map tiler and to the google map.
    Still need to create a tileserver on the web backend so that
    non-existent tile locations can correctly be served up with black
    tiles

--- website/stats.wesnoth.org/map_tiler.py
+++ website/stats.wesnoth.org/map_tiler.py
@@ -22,11 +22,16 @@ for infile in sys.argv[1:]:
 		width = im.size[0]
 		height = im.size[1]
 		filename_base = infile[0:len(infile)-3]
+		#generate fine zoom level, level 4
 		for i in range(0,width/256):
 			for j in range(0,height/256):
-				tile = Image.new(im.mode,(256,256))
 				tile_src = im.crop((i*256,j*256,(i+1)*256,(j+1)*256))
-				tile.paste(tile_src,(0,0,256,256))
-				tile.save("%s%d_%d.png" % (filename_base,i,j),"PNG")
+				tile_src.save("%s%d_%d_4.png" % (filename_base,i,j),"PNG")
+		#generate coarse zoom level, level 3
+		for i in range(0,width/1024):
+			for j in range(0,height/1024):
+				tile_src = im.crop((i*1024,j*1024,(i+1)*1024,(j+1)*1024))
+				tile_src = tile_src.resize((256,256))
+				tile_src.save("%s%d_%d_3.png" % (filename_base,i,j),"PNG")
 	except IOError:
 		print IOError
