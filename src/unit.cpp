4143d6f5d86eaa569974b31668c5a068f3f46380
Alexander van Gessel <ai0867@gmail.com>
2009-07-31 14:31:18 +0100

    Remove [stoned]->[petrified] compatibility hack. Do not bump min_savegame.

--- src/unit.cpp
+++ src/unit.cpp
@@ -2669,15 +2669,7 @@ void unit::add_modification(const std::string& type, const config& mod, bool no_
 					const std::string &remove = effect["remove"];
 
 					if(add.empty() == false) {
-				 		// FIXME: 1.6 compatibility hack
-					  	// Remove after 1.7.2
-					  	// (and bump min_savegame))
-						if (add == "stoned") {
-							set_state("petrified","yes");;
-							lg::wml_error << "Usage of 'stoned' is deprecated use 'petrify' instead, "
-								"support will be removed in 1.7.2.\n";
-					  	} else
-							set_state(add,"yes");
+						set_state(add,"yes");
 					}
 
 					if(remove.empty() == false) {
