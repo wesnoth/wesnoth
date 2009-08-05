5b6db98aecbec31ea37a0ca3c33c20037fc96fea
Tomasz Śniatowski <kailoran@gmail.com>
2009-08-05 22:14:58 +0100

    log typeid of widgets that are not controls in lifetime tracking

--- src/gui/widgets/widget.cpp
+++ src/gui/widgets/widget.cpp
@@ -50,7 +50,9 @@ void twidget::set_id(const std::string& id)
 {
 	DBG_GUI_LF << "set id of " << (void*)this << " to '" << id << "' "
 		<< "(was '" << id_ << "'). Widget type: " <<
-		(dynamic_cast<tcontrol*>(this) ? dynamic_cast<tcontrol*>(this)->get_control_type() : "?")
+		(dynamic_cast<tcontrol*>(this) ?
+			dynamic_cast<tcontrol*>(this)->get_control_type()
+			: typeid(twidget).name())
 		<< "\n";
 	id_ = id;
 }
