802c46b79716de9cb24c9557014a5d2bf8ce05c9
Gregory Shikhman <cornmander@cornmander.com>
2009-08-16 05:23:35 +0000

    Refactored the menu into a separate header file...
    ...that can be imported into other files. Added a footer. Added a link
    to the stats in the main menu. Removed a stub file that I ended up not
    using.

--- website/stats.wesnoth.org/map_decoder.py
+++
@@ -1,8 +0,0 @@
-import os,sys
-
-for infile in sys.argv[1:]:
-	map = open(infile,"r")
-	lines = map.readlines()
-	rows = len(lines) - 3
-	columns = lines[3].count(',')+1
-	print "%d rows x %d columns" % (rows,columns)
