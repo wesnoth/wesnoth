ef0d925e4f7ad181b97c727f7fb3f10a9f13f9a8
Gunter Labes <soliton@wesnoth.org>
2009-09-27 18:00:52 +0000

    put the output() call into the try block as well

--- src/server/game.cpp
+++ src/server/game.cpp
@@ -1234,19 +1234,19 @@ void game::save_replay() {
 	name << level_["name"] << " Turn " << current_turn();
 
 	std::stringstream replay_data;
-	replay_data << "campaign_type=\"multiplayer\"\n"
-	<< "difficulty=\"NORMAL\"\n"
-	<< "label=\"" << name.str() << "\"\n"
-	<< "mp_game_title=\"" << name_ << "\"\n"
-	<< "random_seed=\"" << level_["random_seed"] << "\"\n"
-	<< "version=\"" << level_["version"] << "\"\n"
-	<< "[replay]\n" << replay_commands << "[/replay]\n"
-	<< "[replay_start]\n" << level_.output() << "[/replay_start]\n";
-
-	name << " (" << id_ << ").gz";
-
-	std::string replay_data_str = replay_data.str();
 	try {
+		replay_data << "campaign_type=\"multiplayer\"\n"
+		<< "difficulty=\"NORMAL\"\n"
+		<< "label=\"" << name.str() << "\"\n"
+		<< "mp_game_title=\"" << name_ << "\"\n"
+		<< "random_seed=\"" << level_["random_seed"] << "\"\n"
+		<< "version=\"" << level_["version"] << "\"\n"
+		<< "[replay]\n" << replay_commands << "[/replay]\n"
+		<< "[replay_start]\n" << level_.output() << "[/replay_start]\n";
+
+		name << " (" << id_ << ").gz";
+
+		std::string replay_data_str = replay_data.str();
 		simple_wml::document replay(replay_data_str.c_str(), simple_wml::INIT_STATIC);
 
 		std::string filename(name.str());
