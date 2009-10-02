6295c88501a6e243b76414ac648bdf3d6c2f1a34
Mark de Wever <koraq@xs4all.nl>
2009-10-02 19:25:13 +0000

    Pass parameter by const ref instead of const value.
    Issue found by cppcheck.

--- src/team.hpp
+++ src/team.hpp
@@ -146,7 +146,7 @@ public:
 	void set_action_bonus_count(const int count) { action_bonus_count_ = count; }
 	std::vector<unit>& recall_list() {return recall_list_;}
 	const std::vector<unit>& recall_list() const {return recall_list_;}
-	void set_current_player(const std::string player)
+	void set_current_player(const std::string& player)
 		{ info_.current_player = player; }
 
 	int average_recruit_price() const;
