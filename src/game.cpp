c4f28eca1a0a4c47fd0169100828d96df5434399
Gunter Labes <soliton@wesnoth.org>
2009-10-20 18:06:16 +0000

    consistently return 2 on commandline argument errors

--- src/game.cpp
+++ src/game.cpp
@@ -1854,7 +1854,7 @@ static int process_command_args(int argc, char** argv) {
 			size_t p = val.find('=');
 			if (p == std::string::npos) {
 				std::cerr << "unknown option: " << val << '\n';
-				return 0;
+				return 2;
 			}
 			std::string s = val.substr(6, p - 6);
 			int severity;
@@ -1864,14 +1864,14 @@ static int process_command_args(int argc, char** argv) {
 			else if (s == "debug") severity = 3;
 			else {
 				std::cerr << "unknown debug level: " << s << '\n';
-				return 0;
+				return 2;
 			}
 			while (p != std::string::npos) {
 				size_t q = val.find(',', p + 1);
 				s = val.substr(p + 1, q == std::string::npos ? q : q - (p + 1));
 				if (!lg::set_log_domain_severity(s, severity)) {
 					std::cerr << "unknown debug domain: " << s << '\n';
-					return 0;
+					return 2;
 				}
 				p = q;
 			}
