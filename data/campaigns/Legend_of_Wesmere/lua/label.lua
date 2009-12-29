a014b208d83c35b0253c7ccf44ae59fd876891c6
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2009-12-29 20:14:06 +0000

    Fixed labels not being positioned with respect to the map excerpt.

---
+++ data/campaigns/Legend_of_Wesmere/lua/label.lua
@@ -0,0 +1,20 @@
+local labels = {}
+local wml_label
+
+wml_label = wesnoth.register_wml_action("label",
+  function(cfg)
+    table.insert(labels, cfg.__parsed)
+    wml_label(cfg)
+  end)
+
+wesnoth.register_wml_action("shift_labels",
+  function(cfg)
+    for k, v in ipairs(labels) do
+      wml_label { x = v.x, y = v.y }
+    end
+    for k, v in ipairs(labels) do
+      v.x = v.x + cfg.x
+      v.y = v.y + cfg.y
+      wml_label(v)
+    end
+  end)
