c5f07a75408b77de99ab4fa1b477a9011bb27188
Iurii Chernyi <terraninfo@terraninfo.net>
2010-01-29 21:31:56 +0000

    rename log domain 'ai/composite/engine/cpp' to 'ai/engine/cpp'

--- src/ai/composite/engine_default.cpp
+++ src/ai/composite/engine_default.cpp
@@ -25,10 +25,10 @@
 
 namespace ai {
 
-static lg::log_domain log_ai_composite_engine_cpp("ai/composite/engine/cpp");
-#define DBG_AI_COMPOSITE_ENGINE_CPP LOG_STREAM(debug, log_ai_composite_engine_cpp)
-#define LOG_AI_COMPOSITE_ENGINE_CPP LOG_STREAM(info, log_ai_composite_engine_cpp)
-#define ERR_AI_COMPOSITE_ENGINE_CPP LOG_STREAM(err, log_ai_composite_engine_cpp)
+static lg::log_domain log_ai_engine_cpp("ai/engine/cpp");
+#define DBG_AI_ENGINE_CPP LOG_STREAM(debug, log_ai_engine_cpp)
+#define LOG_AI_ENGINE_CPP LOG_STREAM(info, log_ai_engine_cpp)
+#define ERR_AI_ENGINE_CPP LOG_STREAM(err, log_ai_engine_cpp)
 
 engine_cpp::engine_cpp( readonly_context &context, const config &cfg )
 	: engine(context,cfg)
@@ -46,14 +46,14 @@ void engine_cpp::do_parse_aspect_from_config( const config &cfg, const std::stri
 	const std::string aspect_factory_key = id+"*"+cfg["name"];//@note: hack which combines aspect id and name to get the std::string key of the aspect factory
 	aspect_factory::factory_map::iterator f = aspect_factory::get_list().find(aspect_factory_key);
 	if (f == aspect_factory::get_list().end()){
-		ERR_AI_COMPOSITE_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNKNOWN aspect["<<aspect_factory_key<<"]" << std::endl;
-		DBG_AI_COMPOSITE_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
+		ERR_AI_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNKNOWN aspect["<<aspect_factory_key<<"]" << std::endl;
+		DBG_AI_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
 		return;
 	}
 	aspect_ptr new_aspect = f->second->get_new_instance(ai_,cfg,id);
 	if (!new_aspect) {
-		ERR_AI_COMPOSITE_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNABLE TO CREATE aspect, key=["<<aspect_factory_key<<"]"<< std::endl;
-		DBG_AI_COMPOSITE_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
+		ERR_AI_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNABLE TO CREATE aspect, key=["<<aspect_factory_key<<"]"<< std::endl;
+		DBG_AI_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
 		return;
 	}
 	*b = new_aspect;
@@ -63,14 +63,14 @@ void engine_cpp::do_parse_aspect_from_config( const config &cfg, const std::stri
 void engine_cpp::do_parse_candidate_action_from_config( rca_context &context, const config &cfg, std::back_insert_iterator<std::vector< candidate_action_ptr > > b ){
 	candidate_action_factory::factory_map::iterator f = candidate_action_factory::get_list().find(cfg["name"]);
 	if (f == candidate_action_factory::get_list().end()){
-		ERR_AI_COMPOSITE_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNKNOWN candidate_action["<<cfg["name"]<<"]"<< std::endl;
-		DBG_AI_COMPOSITE_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
+		ERR_AI_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNKNOWN candidate_action["<<cfg["name"]<<"]"<< std::endl;
+		DBG_AI_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
 		return;
 	}
 	candidate_action_ptr new_candidate_action = f->second->get_new_instance(context,cfg);
 	if (!new_candidate_action) {
-		ERR_AI_COMPOSITE_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNABLE TO CREATE candidate_action["<<cfg["name"]<<"]"<< std::endl;
-		DBG_AI_COMPOSITE_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
+		ERR_AI_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNABLE TO CREATE candidate_action["<<cfg["name"]<<"]"<< std::endl;
+		DBG_AI_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
 		return;
 	}
 	*b = new_candidate_action;
@@ -81,14 +81,14 @@ void engine_cpp::do_parse_stage_from_config( ai_context &context, const config &
 {
 	stage_factory::factory_map::iterator f = stage_factory::get_list().find(cfg["name"]);
 	if (f == stage_factory::get_list().end()){
-		ERR_AI_COMPOSITE_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNKNOWN stage["<<cfg["name"]<<"]"<< std::endl;
-		DBG_AI_COMPOSITE_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
+		ERR_AI_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNKNOWN stage["<<cfg["name"]<<"]"<< std::endl;
+		DBG_AI_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
 		return;
 	}
 	stage_ptr new_stage = f->second->get_new_instance(context,cfg);
 	if (!new_stage) {
-		ERR_AI_COMPOSITE_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNABLE TO CREATE stage["<<cfg["name"]<<"]"<< std::endl;
-		DBG_AI_COMPOSITE_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
+		ERR_AI_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNABLE TO CREATE stage["<<cfg["name"]<<"]"<< std::endl;
+		DBG_AI_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
 		return;
 	}
 	*b = new_stage;
@@ -99,14 +99,14 @@ void engine_cpp::do_parse_goal_from_config(const config &cfg, std::back_insert_i
 {
 	goal_factory::factory_map::iterator f = goal_factory::get_list().find(cfg["name"]);
 	if (f == goal_factory::get_list().end()){
-		ERR_AI_COMPOSITE_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNKNOWN goal["<<cfg["name"]<<"]"<< std::endl;
-		DBG_AI_COMPOSITE_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
+		ERR_AI_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNKNOWN goal["<<cfg["name"]<<"]"<< std::endl;
+		DBG_AI_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
 		return;
 	}
 	goal_ptr new_goal = f->second->get_new_instance(ai_,cfg);
 	if (!new_goal) {
-		ERR_AI_COMPOSITE_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNABLE TO CREATE goal["<<cfg["name"]<<"]"<< std::endl;
-		DBG_AI_COMPOSITE_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
+		ERR_AI_ENGINE_CPP << "side "<<ai_.get_side()<< " : UNABLE TO CREATE goal["<<cfg["name"]<<"]"<< std::endl;
+		DBG_AI_ENGINE_CPP << "config snippet contains: " << std::endl << cfg << std::endl;
 		return;
 	}
 	*b = new_goal;
