ac87556f79f6f7cee7babb8932701dc2653918bf
Jörg Hinrichs <joerg.hinrichs@alice-dsl.de>
2010-02-20 23:11:34 +0000

    Don't show the turn dialog once the scenario has ended.
    This should apply to both single player and multiplayer.

--- src/playsingle_controller.cpp
+++ src/playsingle_controller.cpp
@@ -652,7 +652,7 @@ void playsingle_controller::before_human_turn(bool save)
 }
 
 void playsingle_controller::show_turn_dialog(){
-	if(preferences::turn_dialog()) {
+	if(preferences::turn_dialog() && (level_result_ == NONE) ) {
 		std::string message = _("It is now $name|'s turn");
 		utils::string_map symbols;
 		symbols["name"] = teams_[player_number_ - 1].current_player();
