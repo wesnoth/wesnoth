2392e8ac80e737623957285d0e319a4f8c137e57
Timotei Dolean <timotei21@gmail.com>
2010-05-26 11:40:25 +0000

    eclipse plugin: showMessageBox now can receive a style mask

--- utils/java/eclipse_plugin/src/wesnoth_eclipse_plugin/utils/GUIUtils.java
+++ utils/java/eclipse_plugin/src/wesnoth_eclipse_plugin/utils/GUIUtils.java
@@ -3,6 +3,7 @@
  */
 package wesnoth_eclipse_plugin.utils;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.ui.IWorkbenchWindow;
 
@@ -15,6 +16,16 @@ public class GUIUtils
 	 */
 	public static void showMessageBox(final IWorkbenchWindow window,final String message)
 	{
+		showMessageBox(window, message,SWT.DEFAULT);
+	}
+
+	/**
+	 * Shows a message box with the specified message (thread-safe)
+	 * @param window the window where to show the message box
+	 * @param message the message to print
+	 */
+	public static void showMessageBox(final IWorkbenchWindow window,final String message,final int style)
+	{
 		if (window  == null || window.getShell() == null)
 			return;
 		try
@@ -23,7 +34,7 @@ public class GUIUtils
 				@Override
 				public void run()
 				{
-					MessageBox box = new MessageBox(window.getShell());
+					MessageBox box = new MessageBox(window.getShell(), style);
 					box.setMessage(message);
 					box.open();
 				}
