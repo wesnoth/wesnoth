d9ba561934756e2be2fe5d73e9af960ef4d7eb2a
Mark de Wever <koraq@xs4all.nl>
2010-05-27 20:05:53 +0000

    Add a virtual destructor.

--- src/arrow.hpp
+++ src/arrow.hpp
@@ -32,6 +32,9 @@
  */
 class arrow_observer {
   public:
+
+	virtual ~arrow_observer() {}
+
     virtual void arrow_changed(const arrow & a) = 0;
 
     virtual void arrow_deleted(const arrow & a) = 0;
