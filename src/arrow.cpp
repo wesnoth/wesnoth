7e05b5b7571f9ce64d83aa4963b7ef082adfb8f9
Gabriel Morin <gabrielmorin@gmail.com>
2010-05-27 19:58:42 +0000

    Copyright and licensing for arrow.cpp/hpp

--- src/arrow.cpp
+++ src/arrow.cpp
@@ -1,3 +1,21 @@
+/* $Id$ */
+/*
+   Copyright (C) 2010 by Gabriel Morin <gabrielmorin (at) gmail (dot) org>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2
+   or at your option any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+/**
+ * @file arrow.cpp
+ * Method bodies for the arrow class.
+ */
 
 #include "arrow.hpp"
 
