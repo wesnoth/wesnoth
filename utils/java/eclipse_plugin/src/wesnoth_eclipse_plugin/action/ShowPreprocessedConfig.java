548712c03c5fee8daac3816fa6f8a2232536180e
Timotei Dolean <timotei21@gmail.com>
2010-05-27 19:15:18 +0000

    eclipse plugin: move all context menus in a dedicated group
    eclipse plugin: add new dummy context menus

---
+++ utils/java/eclipse_plugin/src/wesnoth_eclipse_plugin/action/ShowPreprocessedConfig.java
@@ -0,0 +1,25 @@
+package wesnoth_eclipse_plugin.action;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class ShowPreprocessedConfig implements IObjectActionDelegate
+{
+	public ShowPreprocessedConfig()	{ }
+
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart){
+	}
+
+	@Override
+	public void run(IAction action)
+	{
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection){
+	}
+}
---
+++ utils/java/eclipse_plugin/src/wesnoth_eclipse_plugin/action/WesnothProjectReport.java
@@ -0,0 +1,25 @@
+package wesnoth_eclipse_plugin.action;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class WesnothProjectReport implements IObjectActionDelegate
+{
+	public WesnothProjectReport(){ }
+
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart){
+	}
+
+	@Override
+	public void run(IAction action)
+	{
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection){
+	}
+}
