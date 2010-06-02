724f447fa92f7c936226b560302aa547e0937247
Jody Northup <upthorn@gmail.com>
2010-06-02 14:42:51 +0000

    encapsulated functions related to namespace parsing...
    ...namespace parsing now basically accurate to spec (inaccuracies
    reflect pending changes to spec).

--- src/persist_context.cpp
+++ src/persist_context.cpp
@@ -21,26 +21,8 @@
 #include "resources.hpp"
 #include "serialization/binary_or_text.hpp"
 
-static bool is_valid_namespace(const std::string &name_space) {
-	return (name_space.find_first_not_of("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_") > name_space.length());
-}
-
-static std::string get_namespace_ancestry(const std::string &name_space) {
-	return name_space.substr(0,name_space.find_last_of("."));
-}
-static std::string get_namespace_leaf(const std::string &name_space) {
-	return name_space.substr(name_space.find_last_of(".") + 1);
-}
-static std::string get_namespace_root(const std::string &name_space) {
-	return name_space.substr(0,name_space.find_first_of("."));
-}
-
 static std::string get_persist_cfg_name(const std::string &name_space) {
-	// TODO: get namespace base name
-	if (is_valid_namespace(get_namespace_root(name_space)))
-		return (get_dir(get_user_data_dir() + "/persist/") + get_namespace_root(name_space) + ".cfg");
-	else
-		return "";
+	return (get_dir(get_user_data_dir() + "/persist/") + name_space + ".cfg");
 }
 
 static bool load_persist_data(const std::string &name_space, config &cfg, const bool create_if_missing = true)
@@ -54,7 +36,6 @@ static bool load_persist_data(const std::string &name_space, config &cfg, const 
 		scoped_istream file_stream = istream_file(cfg_name);
 		if (file_stream->fail()) {
 			if (create_if_missing) {
-				// TODO: create proper file structure.
 				success = true;
 			}
 		} else {
@@ -105,11 +86,11 @@ config pack_scalar(const std::string &name, const t_string &val)
 
 void persist_context::load() {
 	if (parent_ == NULL)
-		load_persist_data(namespace_,cfg_,false);
+		load_persist_data(namespace_.root(),cfg_,false);
 	for (config::all_children_iterator i = cfg_.ordered_begin(); i != cfg_.ordered_end(); i++) {
 		if (i->key != "variables") {
 			if (children_.find(i->key) == children_.end()) {
-				persist_context *child = new persist_context(i->key);
+				persist_context *child = new persist_context(namespace_.namespace_ + "." + i->key,this);
 				children_[i->key] = child;
 			}
 			children_[i->key]->parent_ = this;
@@ -118,11 +99,9 @@ void persist_context::load() {
 	}
 }
 
-void persist_context::init(const std::string &name_space) {
-	if (name_space.find_last_of(".") < name_space.length()) {
-		std::string ancestry = get_namespace_ancestry(name_space);
-		std::string parent_name = get_namespace_leaf(ancestry);
-		parent_ = new persist_context(ancestry,*this);
+void persist_context::init() {
+	if ((parent_ == NULL) && !namespace_.lineage().empty()) {
+		parent_ = new persist_context(namespace_.lineage(),*this);
 	}
 	load();
 	if (!cfg_.child("variables"))
@@ -130,32 +109,43 @@ void persist_context::init(const std::string &name_space) {
 }
 
 persist_context::persist_context(const std::string &name_space) : 
-namespace_(get_namespace_leaf(name_space)), 
+namespace_(name_space), 
 cfg_(),
 parent_(NULL),
 children_(),
-valid_(is_valid_namespace(namespace_)),
+valid_(namespace_.valid()),
 dirty_(false),
 collected_(false)
 {
-	init(name_space);
+	init();
 }
 
 persist_context::persist_context(const std::string &name_space, persist_context &child) : 
-namespace_(get_namespace_leaf(name_space)), 
+namespace_(name_space), 
 cfg_(),
 parent_(NULL),
 children_(),
-valid_(is_valid_namespace(namespace_)),
+valid_(namespace_.valid()),
 dirty_(false),
 collected_(false)
 {
-	children_[child.namespace_] = &child;
-	init(name_space);
-	if (!cfg_.child(child.namespace_))
-		cfg_.add_child(child.namespace_);
+	children_[child.namespace_.node()] = &child;
+	init();
+	if (!cfg_.child(child.namespace_.node()))
+		cfg_.add_child(child.namespace_.node());
 }
 
+persist_context::persist_context(const std::string &name_space, persist_context *parent) : 
+namespace_(name_space), 
+cfg_(),
+parent_(parent),
+children_(),
+valid_(namespace_.valid()),
+dirty_(false),
+collected_(false)
+{
+	init();
+}
 persist_context::~persist_context() {
 	collected_ = true;
 	if (parent_ != NULL) {
@@ -170,9 +160,9 @@ persist_context::~persist_context() {
 
 bool persist_context::clear_var(std::string &global)
 {
-	if (cfg_.empty()) {
-		load_persist_data(namespace_,cfg_);
-	}
+//	if (cfg_.empty()) {
+//		load_persist_data(namespace_.root(),cfg_);
+//	}
 
 	config &cfg = cfg_.child("variables");
 	bool exists = cfg.has_attribute(global);
@@ -209,9 +199,9 @@ bool persist_context::clear_var(std::string &global)
 config persist_context::get_var(const std::string &global)
 {
 	config ret;
-	if (cfg_.empty()) {
-		load_persist_data(namespace_,cfg_,false);
-	}
+//	if (cfg_.empty()) {
+//		load_persist_data(namespace_,cfg_,false);
+//	}
 
 	config &cfg = cfg_.child("variables");
 	if (cfg.child(global)) {
@@ -225,9 +215,9 @@ void persist_context::update_configs() {
 	for (child_map::iterator i = children_.begin(); i != children_.end(); i++) {
 		if (i->second->dirty()) {
 			i->second->update_configs();
-			cfg_.clear_children(i->second->namespace_);
-			cfg_.remove_attribute(i->second->namespace_);
-			cfg_.add_child(i->second->namespace_,i->second->cfg_);
+			cfg_.clear_children(i->second->namespace_.node());
+			cfg_.remove_attribute(i->second->namespace_.node());
+			cfg_.add_child(i->second->namespace_.node(),i->second->cfg_);
 			i->second->dirty_ = false;
 		}
 	}
@@ -236,13 +226,13 @@ bool persist_context::save_context() {
 	if (parent_ != NULL)
 		return parent_->save_context();
 	update_configs();
-	return save_persist_data(namespace_,cfg_);
+	return save_persist_data(namespace_.root(),cfg_);
 }
 bool persist_context::set_var(const std::string &global,const config &val)
 {
-	if (cfg_.empty()) {
-		load_persist_data(namespace_,cfg_);
-	}
+//	if (cfg_.empty()) {
+//		load_persist_data(namespace_,cfg_);
+//	}
 
 	config &cfg = cfg_.child("variables");
 	if (val.has_attribute(global)) {
--- src/persist_context.hpp
+++ src/persist_context.hpp
@@ -23,10 +23,57 @@ static lg::log_domain log_persist("engine/persistence");
 config pack_scalar(const std::string &,const t_string &);
 class persist_context {
 private:
+	struct name_space {
+		std::string namespace_;
+		std::string root_;
+		std::string node_;
+		std::string lineage_;
+		bool valid_;
+		bool valid() const {
+			return valid_;
+		}
+		std::string lineage() const {
+			return lineage_;
+		}
+		std::string node() const {
+			return node_;
+		}
+		std::string root() const {
+			return root_;
+		}
+		void parse() {
+			while (namespace_.find_first_of("^") < namespace_.size()) {
+				std::string infix = namespace_.substr(namespace_.find_first_of("^"));
+				size_t end = infix.find_first_not_of("^");
+				if (!((end >= infix.length()) || (infix[end] == '.'))) {
+					//TODO: Throw a WML error
+					namespace_ = "";
+					break;
+				}
+				infix = infix.substr(0,end);
+				std::string suffix = namespace_.substr(namespace_.find_first_of("^") + infix.length());
+				while (!infix.empty())
+				{
+					std::string body = namespace_.substr(0,namespace_.find_first_of("^"));
+					body = body.substr(0,body.find_last_of("."));
+					infix = infix.substr(1);
+					namespace_ = body + infix + suffix;
+				}
+			}
+		}
+		name_space(const std::string &ns) : namespace_(ns) {
+			parse();
+			valid_ = ((namespace_.find_first_not_of("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_.") > namespace_.length()) && !namespace_.empty());
+			root_ = namespace_.substr(0,namespace_.find_first_of("."));
+			node_ = namespace_.substr(namespace_.find_last_of(".") + 1);
+			if (namespace_.find_last_of(".") <= namespace_.length())
+				lineage_ = namespace_.substr(0,namespace_.find_last_of("."));
+		}
+	};
 	// TODO: transaction support (needed for MP)
 	typedef std::map<std::string,persist_context*> child_map;
 	// TODO: parent and child members (needed for namespace embeddeding)
-	std::string namespace_;
+	name_space namespace_;
 	config cfg_;
 	persist_context *parent_;
 	child_map children_;
@@ -34,12 +81,13 @@ private:
 	bool dirty_;
 	bool collected_;
 	void load();
-	void init(const std::string &name_space);
+	void init();
 	bool save_context();
 	void update_configs();
 public:
 	persist_context(const std::string &);
 	persist_context(const std::string &, persist_context &);
+	persist_context(const std::string &, persist_context *);
 	~persist_context();
 	bool clear_var(std::string &);
 	config get_var(const std::string &);
