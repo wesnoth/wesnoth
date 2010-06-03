595927f72ec9a1fd5061ecdc806267db326c101e
Alexander van Gessel <ai0867@gmail.com>
2010-06-03 18:12:00 +0100

    Remove PYTHON preprocessor define from a test

--- src/tests/test_config_cache.cpp
+++ src/tests/test_config_cache.cpp
@@ -38,10 +38,6 @@ static preproc_map setup_test_preproc_map()
 	if (game_config::small_gui)
 		defines_map["SMALL_GUI"] = preproc_define();
 
-#ifdef HAVE_PYTHON
-	defines_map["PYTHON"] = preproc_define();
-#endif
-
 #if defined(__APPLE__)
 	defines_map["APPLE"] = preproc_define();
 #endif
