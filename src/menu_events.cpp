2f0f761a80007b4fc940e63e55d74397d3e65429
Steven Panek <Majora700@gmail.com>
2010-07-03 01:28:31 +0000

    Converted more apostrophes; Americanized a spelling; ran pofix.

--- src/menu_events.cpp
+++ src/menu_events.cpp
@@ -2253,7 +2253,7 @@ class chat_command_handler : public map_command_handler<chat_command_handler>
 			register_alias("emote", "me");
 			register_command("whisper", &chat_command_handler::do_whisper,
 				_("Sends a private message. "
-				"You can't send messages to players that don't control "
+				"You can’t send messages to players that don't control "
 				"a side in a running game you are in."), _("<nick> <message>"));
 			register_alias("whisper", "msg");
 			register_alias("whisper", "m");
