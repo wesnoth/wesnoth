363df78f9f88c56c7d7dda4dcbc16a555faa47d0
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2010-07-23 10:19:38 +0000

    Avoided loop between setup_user_data_dir and set_preferences_dir.

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -541,8 +541,6 @@ void set_preferences_dir(std::string path)
 
 static void setup_user_data_dir()
 {
-	if (game_config::preferences_dir.empty())
-		set_preferences_dir(std::string());
 #ifdef _WIN32
 	_mkdir((game_config::preferences_dir).c_str());
 	_mkdir((game_config::preferences_dir + "/editor").c_str());
@@ -591,8 +589,12 @@ const std::string& get_user_data_dir()
 	// if the user deletes a dir while we are running?
 	if (user_data_dir.empty())
 	{
-		setup_user_data_dir();
-		user_data_dir = game_config::preferences_dir;
+		if (game_config::preferences_dir.empty())
+			set_preferences_dir(std::string());
+		else {
+			user_data_dir = game_config::preferences_dir;
+			setup_user_data_dir();
+		}
 	}
 	return user_data_dir;
 }
