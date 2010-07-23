2a954305d8836204ad2fb3ba592c2f43ae2d0ad4
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2010-07-23 10:19:46 +0000

    Moved cache directory to an XDG-compliant location.

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -612,7 +612,21 @@ const std::string &get_cache_dir()
 {
 	if (cache_dir.empty())
 	{
+#ifdef _X11
+		char const *xdg_cache = getenv("XDG_CACHE_HOME");
+		if (!xdg_cache || xdg_cache[0] == '\0') {
+			xdg_cache = getenv("HOME");
+			if (!xdg_cache) {
+				cache_dir = get_dir(get_user_data_dir() + "/cache");
+				return cache_dir;
+			}
+			cache_dir = xdg_cache;
+			cache_dir += "/.cache";
+		} else cache_dir = xdg_cache;
+		cache_dir = get_dir(cache_dir + "/wesnoth");
+#else
 		cache_dir = get_dir(get_user_data_dir() + "/cache");
+#endif
 	}
 	return cache_dir;
 }
