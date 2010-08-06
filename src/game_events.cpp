c8b1a1c1decaaa94919e291f2ea931ef776a75d9
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2010-08-06 20:25:03 +0000

    Added missing 'colour'.

--- src/game_events.cpp
+++ src/game_events.cpp
@@ -945,6 +945,8 @@ WML_HANDLER_FUNCTION(store_side, /*event_info*/, cfg)
 	state_of_game->get_variable(var_name+".team_name") = teams[team_index].team_name();
 	state_of_game->get_variable(var_name+".user_team_name") = teams[team_index].user_team_name();
 	state_of_game->get_variable(var_name+".color") = teams[team_index].map_color_to();
+	///@deprecated 1.9.2 'colour'
+	state_of_game->get_variable(var_name+".colour") = teams[team_index].map_color_to();
 
 	state_of_game->get_variable(var_name+".gold") = teams[team_index].gold();
 }
