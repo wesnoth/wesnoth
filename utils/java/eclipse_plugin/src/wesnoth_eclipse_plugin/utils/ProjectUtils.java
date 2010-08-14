e871c4110901bfa4a9a074fdf397f5c51b2573cf
Timotei Dolean <timotei21@gmail.com>
2010-08-14 08:51:02 +0000

    eclipse plugin: disable the xtext nature until...
    ...it's really used. this should fix the 'no heap space' problems

--- utils/java/eclipse_plugin/src/wesnoth_eclipse_plugin/utils/ProjectUtils.java
+++ utils/java/eclipse_plugin/src/wesnoth_eclipse_plugin/utils/ProjectUtils.java
@@ -127,8 +127,8 @@ public class ProjectUtils
 		monitor.subTask("Configuring project...");
 		// add wesnoth nature
 		IProjectDescription tmpDescription = handle.getDescription();
-		tmpDescription.setNatureIds(new String[] { Constants.NATURE_WESNOTH,
-				Constants.NATURE_XTEXT });
+		tmpDescription.setNatureIds(new String[] { Constants.NATURE_WESNOTH /*,
+				Constants.NATURE_XTEXT */});
 		handle.setDescription(tmpDescription, monitor);
 		monitor.worked(5);
 
