6d16ec5461e53d106214419eb59964dcd65a1f35
Guillermo Biset <billybiset@gmail.com>
2010-08-21 21:49:43 +0000

    Added a method comment to ana_network_manager::disconnect.

--- src/network_manager_ana.hpp
+++ src/network_manager_ana.hpp
@@ -477,7 +477,11 @@ class ana_network_manager : public ana::listener_handler,
         /** Retrieve download statistics on a given component. */
         network::statistics get_receive_stats(network::connection handle);
 
-        /** */
+        /**
+         * Disconnect a given client.
+         *
+         * @param handle : The ID of the component (should be a client) to be disconnected.
+         */
         bool disconnect( network::connection handle);
 
     private:
