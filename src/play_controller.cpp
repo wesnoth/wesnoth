de03f3010dd4055cc607e0990577f330485b61e4
Mark de Wever <koraq@xs4all.nl>
2010-08-27 20:35:15 +0000

    Strip trailing whitespace.

--- src/play_controller.cpp
+++ src/play_controller.cpp
@@ -662,7 +662,7 @@ void play_controller::finish_side_turn(){
 		if (uit->side() == player_number_)
 			uit->end_turn();
 	}
-	
+
 	const std::string turn_num = str_cast(turn());
 	const std::string side_num = str_cast(player_number_);
 	game_events::fire("side turn end");
@@ -688,7 +688,7 @@ void play_controller::finish_turn()
 	const std::string side_num = str_cast(player_number_);
 	game_events::fire("turn end");
 	game_events::fire("turn " + turn_num + " end");
-	
+
 	LOG_NG << "turn event..." << (recorder.is_skipping() ? "skipping" : "no skip") << '\n';
 	update_locker lock_display(gui_->video(),recorder.is_skipping());
 	gamestate_.get_variable("turn_number") = int(turn());
