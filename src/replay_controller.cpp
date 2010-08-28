0d75d88b6900bdeda2da747d8ab0823154f9e170
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2010-08-28 05:45:08 +0000

    Fixed era events not being replayed.

--- src/replay_controller.cpp
+++ src/replay_controller.cpp
@@ -170,6 +170,11 @@ void replay_controller::reset_replay(){
 	statistics::fresh_stats();
 	set_victory_when_enemies_defeated(level_["victory_when_enemies_defeated"].to_bool(true));
 
+	// Add era events for MP game.
+	if (const config &era_cfg = level_.child("era")) {
+		game_events::add_events(era_cfg.child_range("event"), "era_events");
+	}
+
 	fire_prestart(true);
 	init_gui();
 	fire_start(true);
