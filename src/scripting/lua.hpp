f95ea39cf3ca0c024150806f2addcee427080c56
Guillaume Melquiond <guillaume.melquiond@gmail.com>
2010-08-29 20:50:51 +0000

    Reverted 2010-08-29T13:24:06Z!guillaume.melquiond@gmail.com,
    ...since the animation cache is no longer meant to be filled from WML.

--- src/scripting/lua.hpp
+++ src/scripting/lua.hpp
@@ -38,7 +38,6 @@ public:
 	void initialize();
 	void save_game(config &);
 	void load_game();
-	config get_animation(std::string const &);
 	void set_wml_action(std::string const &, game_events::action_handler);
 	bool run_wml_action(std::string const &, vconfig const &,
 		game_events::queued_event const &);
