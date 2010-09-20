9139cee9750f62fd142e7a6cad266e9dfc8f0990
Mark de Wever <koraq@xs4all.nl>
2010-09-20 19:29:44 +0000

    Enable preferences hotkey.

--- src/gui/dialogs/title_screen.cpp
+++ src/gui/dialogs/title_screen.cpp
@@ -204,6 +204,12 @@ void ttitle_screen::post_build(CVideo& video, twindow& window)
 					  &hotkey
 					, boost::ref(window)
 					, gui::SHOW_HELP));
+
+	window.register_hotkey(hotkey::HOTKEY_PREFERENCES
+				, boost::bind(
+					  &hotkey
+					, boost::ref(window)
+					, gui::EDIT_PREFERENCES));
 }
 
 void ttitle_screen::pre_show(CVideo& video, twindow& window)
