980c48d4dc1bf2b97dbef2dad97837ef5a0a34b3
Mark de Wever <koraq@xs4all.nl>
2010-09-27 20:52:39 +0000

    Remove some leading whitespace.

--- src/gui/dialogs/title_screen.cpp
+++ src/gui/dialogs/title_screen.cpp
@@ -54,7 +54,7 @@ void show_language_dialog(CVideo& video, twindow& window)
 {
 	window.set_retval(static_cast<twindow::tretval>(gui::CHANGE_LANGUAGE));
 	return;
- 	//NOTE following code doesn't redraw the titlescreen, reload tips and images
+	//NOTE following code doesn't redraw the titlescreen, reload tips and images
 	tlanguage_selection dlg;
 	dlg.show(video);
 	if(dlg.get_retval() == twindow::OK) {
