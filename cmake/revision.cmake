706edf12b72130529ab3ad6f82c738333d8ef5b4
Mark de Wever <koraq@xs4all.nl>
2010-10-02 11:02:23 +0000

    Properly updates cmake revision numbers.
    Rewrote the code and should properly work with out-of-tree builds and
    also no longer causing unneeded recompilations if the version number
    didn't change. Fixes bug #16483, backported from 2010-10-02T09:50:39Z!koraq@xs4all.nl.

--- cmake/revision.cmake
+++ cmake/revision.cmake
@@ -1,6 +1,6 @@
-if (EXISTS revision-stamp)
-  file(READ revision-stamp OLD_VERSION)
-endif (EXISTS revision-stamp)
+if (EXISTS revision.hpp)
+  file(READ revision.hpp OLD_VERSION)
+endif (EXISTS revision.hpp)
 
 execute_process(COMMAND ${SVNVERSION_EXECUTABLE} -n ${SRC_DIR}
                 OUTPUT_VARIABLE SVN_VERSION)
@@ -8,7 +8,7 @@ execute_process(COMMAND ${SVNVERSION_EXECUTABLE} -n ${SRC_DIR}
 if(SVN_VERSION MATCHES [0-9]+.*)
 
   if(NOT OLD_VERSION MATCHES ".*\"${SVN_VERSION}\".*")
-    file(WRITE revision-stamp "#define REVISION \"${SVN_VERSION}\"\n")
+    file(WRITE revision.hpp "#define REVISION \"${SVN_VERSION}\"\n")
   endif(NOT OLD_VERSION MATCHES ".*\"${SVN_VERSION}\".*")
 
 endif(SVN_VERSION MATCHES [0-9]+.*)
