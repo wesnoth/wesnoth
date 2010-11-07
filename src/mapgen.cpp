31b7926b063689732b7fac47f169aedc6c3bea12
Ignacio R. Morelle <shadowm@wesnoth.org>
2010-11-07 21:35:01 +0000

    Remove a comment relevant to 2010-11-07T21:33:28Z!shadowm@wesnoth.org

--- src/mapgen.cpp
+++ src/mapgen.cpp
@@ -1003,7 +1003,6 @@ std::string default_generate_map(size_t width, size_t height, size_t island_size
 			}
 		}
 		if(best_ranking == 0) {
-			//FIXME: Make this error message translatable (not possible atm due to string freeze)
 			ERR_NG << "No castle location found, aborting.\n";
 			std::string error = _("No valid castle location found. Too many or too few mountain hexes? (please check the 'max hill size' parameter)");
 			throw mapgen_exception(error);
