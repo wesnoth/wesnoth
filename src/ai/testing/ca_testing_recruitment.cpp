2c962b79ae2f39e8c370970a7d50ad1ab54ae8c1
Mark de Wever <koraq@xs4all.nl>
2010-11-25 20:20:56 +0000

    Fix a compiler warning.

--- src/ai/testing/ca_testing_recruitment.cpp
+++ src/ai/testing/ca_testing_recruitment.cpp
@@ -382,7 +382,8 @@ static void update_recruit_qualities(fake_team &t, std::vector<potential_recruit
 		recruit.set_quality(score);
 	}
 }
-int analyze_recruit_combat(fake_team &t, const unit_map &units, std::vector<potential_recruit> &extra_units)
+
+static int analyze_recruit_combat(fake_team &t, const unit_map &units, std::vector<potential_recruit> &extra_units)
 {
 	const int hitpoints_const = 100;
 	int score = 0;
