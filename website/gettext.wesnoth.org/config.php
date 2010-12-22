6a9eb5b7ff18eb23fbe4c59a55e8eed2c21515c9
Nils Kneuper <crazy-ivanovic@gmx.net>
2010-12-22 11:47:16 +0000

    add the new textdomain to g.w.o

--- website/gettext.wesnoth.org/config.php
+++ website/gettext.wesnoth.org/config.php
@@ -21,9 +21,9 @@ $extrabbasedir="/usr/src/svn-checkouts/wescamp-i18n/branches/$wescampbranchversi
 
 //$packages = file_get_contents($basedir . "/po/DOMAINS");
 //$packages = substr($packages, 0, strlen($packages)-1);
-$corepackages = "wesnoth wesnoth-lib wesnoth-editor wesnoth-units wesnoth-multiplayer wesnoth-tutorial";
+$corepackages = "wesnoth wesnoth-lib wesnoth-editor wesnoth-help wesnoth-units wesnoth-multiplayer wesnoth-tutorial";
 //$packages = trim(system("grep ^SUBDIRS " . $basedir . "/po/Makefile.am | cut -d= -f2"));
-$packages = "wesnoth wesnoth-lib wesnoth-editor wesnoth-units wesnoth-multiplayer wesnoth-test wesnoth-anl wesnoth-tutorial wesnoth-manpages wesnoth-manual wesnoth-aoi wesnoth-did wesnoth-dm wesnoth-dw wesnoth-ei wesnoth-httt wesnoth-l wesnoth-low wesnoth-nr wesnoth-sof wesnoth-sotbe wesnoth-tb wesnoth-thot wesnoth-trow wesnoth-tsg wesnoth-utbs";
+$packages = "wesnoth wesnoth-lib wesnoth-editor wesnoth-help wesnoth-units wesnoth-multiplayer wesnoth-test wesnoth-anl wesnoth-tutorial wesnoth-manpages wesnoth-manual wesnoth-aoi wesnoth-did wesnoth-dm wesnoth-dw wesnoth-ei wesnoth-httt wesnoth-l wesnoth-low wesnoth-nr wesnoth-sof wesnoth-sotbe wesnoth-tb wesnoth-thot wesnoth-trow wesnoth-tsg wesnoth-utbs";
 
 $ignore_langs = "sr@ijekavianlatin sr@latin sr@ijekavian";
 
