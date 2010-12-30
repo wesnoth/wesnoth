cc933c026a4fc9d73838e782875f1cc3ace1ff75
Timotei Dolean <timotei21@gmail.com>
2010-12-30 18:56:56 +0000

    eclipse plugin: unharcode script's version

--- utils/java/plugin_feature/signing.cmd
+++ utils/java/plugin_feature/signing.cmd
@@ -5,15 +5,17 @@ REM cn = common name
 REM ou = organizational unit
 REM o = organization
 REM c = Country
+SET PLUGIN_VERSION=1.0.1
+SET PLUGIN_WML_VERSIOn=1.1.1
 echo generating key...
 keytool.exe -genkey -dname "cn=Battle for Wesnoth, ou=Wesnoth, o=Wesnoth" -alias wesnoth -keypass %1 -keystore keystore -storepass %2 -validity 1095
 echo signing feature
-jarsigner.exe -keystore keystore -storepass %2 -keypass %1 ../update_site/features/Wesnoth_Eclipse_Plugin_1.0.1.jar wesnoth
-echo signing org.wesnoth.wml_1.1.1.jar
-jarsigner.exe -keystore keystore -storepass %2 -keypass %1 ../update_site/plugins/org.wesnoth.wml_1.1.1.jar wesnoth
-echo signing org.wesnoth.wml.ui_1.1.1.jar
-jarsigner.exe -keystore keystore -storepass %2 -keypass %1 ../update_site/plugins/org.wesnoth.wml.ui_1.1.1.jar wesnoth
-echo signing org.wesnoth_1.0.1.jar
-jarsigner.exe -keystore keystore -storepass %2 -keypass %1 ../update_site/plugins/org.wesnoth_1.0.1.jar wesnoth
+jarsigner.exe -keystore keystore -storepass %2 -keypass %1 ../update_site/features/Wesnoth_Eclipse_Plugin_%PLUGIN_VERSION%.jar wesnoth
+echo signing org.wesnoth.wml_%PLUGIN_WML_VERSION%.jar
+jarsigner.exe -keystore keystore -storepass %2 -keypass %1 ../update_site/plugins/org.wesnoth.wml_%PLUGIN_VWML_ERSION%.jar wesnoth
+echo signing org.wesnoth.wml.ui_%PLUGIN_WML_VERSION%.jar
+jarsigner.exe -keystore keystore -storepass %2 -keypass %1 ../update_site/plugins/org.wesnoth.wml.ui_%PLUGIN_WML_VERSION%.jar wesnoth
+echo signing org.wesnoth_%PLUGIN_VERSION%.jar
+jarsigner.exe -keystore keystore -storepass %2 -keypass %1 ../update_site/plugins/org.wesnoth_%PLUGIN_VERSION%.jar wesnoth
 echo finished.
 @pause
