cfc293b8677cbfa15d841fab46705887d7f97764
Fabian Müller <fabianmueller5@gmx.de>
2011-02-12 20:47:43 +0000

    Stripped the input_message.

--- src/gui/dialogs/image_message/input_message.hpp
+++ src/gui/dialogs/image_message/input_message.hpp
@@ -13,111 +13,38 @@
    See the COPYING file for more details.
 */
 
-#ifndef GUI_DIALOGS_WML_MESSAGE_HPP_INCLUDED
-#define GUI_DIALOGS_WML_MESSAGE_HPP_INCLUDED
+#ifndef GUI_DIALOGS_INPUT_MESSAGE_HPP_INCLUDED
+#define GUI_DIALOGS_INPUT_MESSAGE_HPP_INCLUDED
 
-#include "gui/dialogs/dialog.hpp"
-//TODO get rid of this here
-#include "unit.hpp"
+#include "gui/dialogs/image_message/image_message.hpp"
+
 
 namespace gui2 {
 
 /**
- * Base class for the wml generated messages.
+ * image messages offering a text input dialog.
  *
  * We have a separate sub class for left and right images.
  */
-class twml_message_
-	: public tdialog
+class tinput_message_
+	: public timage_message_
 {
 public:
-	twml_message_(const std::string& title, const std::string& message,
-			const std::string& portrait, const bool mirror)
+	tinput_message_(const std::string& title, const std::string& message,
+			const std::string& portrait, const bool mirror,
+			const std::string& caption, std::string* text, const unsigned maximum_length)
 		: title_(title)
 		, image_("")
 		, message_(message)
 		, portrait_(portrait)
 		, mirror_(mirror)
-		, has_input_(false)
-		, input_caption_("")
-		, input_text_(NULL)
-		, input_maximum_lenght_(0)
-		, option_list_()
-		, chosen_option_(NULL)
+		, input_caption_(caption)
+		, input_text_(text)
+		, input_maximum_lenght_(maximum_length)
 	{
 	}
 
-	/**
-	 * Sets the input text variables.
-	 *
-	 * @param caption             The caption for the label.
-	 * @param text                The initial text, after showing the final
-	 *                            text.
-	 * @param maximum_length      The maximum length of the text.
-	 */
-	void set_input(const std::string& caption,
-			std::string* text, const unsigned maximum_length);
-
-	/**
-	 * Sets the option list.
-	 *
-	 * @param option_list			Vector of options to choice from.
-	 * @param chosen_option			Number of the option that was chosen.
-	 */
-	void set_option_list(
-			const std::vector<std::string>& option_list, int* chosen_option);
-
-	/**
-	 * Sets the unit list.
-	 *
-	 * @param unit_list          TODO
-	 * @param chosen_unit        TODO
-	 */
-	void set_unit_list(
-			const std::vector<unit>& unit_list, std::string* chosen_unit);
-
-	/**
-	 * Sets the type list
-	 *
-	 * @param type_list 		TODO
-	 * @param type_id			TODO
-	 * @param side_num			TODO
-	 * @param gold				TODO
-	 */
-	void set_type_list(
-				std::vector<const unit_type*> type_list, std::string* type_id, int side_num, int gold);
 private:
-
-
-	/** Handler for changed unit selection. */
-	void update_unit_list(twindow& window);
-
-	/** Handler for the profile button event. */
-	void profile_pressed();
-
-	/** Handler for the help button event */
-	void help_pressed();
-
-	/** The title for the dialog. */
-	std::string title_;
-
-	/**
-	 * The image which is shown in the dialog.
-	 *
-	 * This image can be an icon or portrait or any other image.
-	 */
-	std::string image_;
-
-	/** The message to show to the user. */
-	std::string message_;
-	/** Filename of the portrait. */
-	std::string portrait_;
-
-	/** Mirror the portrait? */
-	bool mirror_;
-
-	/** Do we need to show an input box? */
-	bool has_input_;
 
 	/** The caption to show for the input text. */
 	std::string input_caption_;
@@ -128,27 +55,6 @@ private:
 	/** The maximum length of the input text. */
 	unsigned input_maximum_lenght_;
 
-	/** The unit id. */
-	std::string* unit_id_;
-
-	/** The list of units the player can choose. */
-	std::vector<unit> unit_list_;
-
-	/** The list of unit_types the player can choose. */
-	//std::vector<const unit_type*> type_list_;
-
-	/** The list of options the player can choose. */
-	std::vector<std::string> option_list_;
-
-	/** The chosen option. */
-	int *chosen_option_;
-
-	/** The chosen unit. */
-	unit *chosen_unit_;
-
-	/** Gold */
-	int gold_;
-
 	/** Inherited from tdialog. */
 	void pre_show(CVideo& video, twindow& window);
 
@@ -157,12 +63,12 @@ private:
 };
 
 /** Shows a dialog with the portrait on the left side. */
-class twml_message_left : public twml_message_
+class tinput_message_left : public tinput_message_
 {
 public:
-	twml_message_left(const std::string& title, const std::string& message,
+	tinput_message_left(const std::string& title, const std::string& message,
 			const std::string& portrait, const bool mirror)
-		: twml_message_(title, message, portrait, mirror)
+		: tinput_message_(title, message, portrait, mirror)
 	{
 	}
 
@@ -173,12 +79,12 @@ private:
 };
 
 /** Shows a dialog with the portrait on the right side. */
-class twml_message_right : public twml_message_
+class tinput_message_right : public tinput_message_
 {
 public:
-	twml_message_right(const std::string& title, const std::string& message,
+	tinput_message_right(const std::string& title, const std::string& message,
 			const std::string& portrait, const bool mirror)
-		: twml_message_(title, message, portrait, mirror)
+		: tinput_message_(title, message, portrait, mirror)
 	{
 	}
 
@@ -200,42 +106,22 @@ private:
  *  @param portrait               Filename of the portrait.
  *  @param mirror                 Does the portrait need to be mirrored?
  *
- *  @param has_input              Do we need to show the input box.
  *  @param input_caption          The caption for the optional input text
  *                                box. If this value != "" there is an input
  *                                and the input text parameter is mandatory.
  *  @param input_text             Pointer to the initial text value will be
  *                                set to the result.
  *  @param maximum_length         The maximum length of the text.
- *
- *  @param option_list            A list of options to select in the dialog.
- *  @param chosen_option          Pointer to the initially chosen option.
- *                                Will be set to the chosen_option when the
- *                                dialog closes.
  */
-int show_recruit_message(const bool left_side
-		, CVideo& video
-	    , std::vector<const unit_type*> type_list
-	    , std::string* type_id
-	    , int side_num
-	    , int gold);
-
-int show_wml_message(const bool left_side
+int show_input_message(const bool left_side
 		, CVideo& video
 		, const std::string& title
 		, const std::string& message
 		, const std::string& portrait
 		, const bool mirror
-		, const bool has_input
 		, const std::string& input_caption
 		, std::string* input_text
-	    , const unsigned maximum_length
-	    , const bool has_unit
-	    , std::string* unit_id
-	    , const std::vector<unit>& unit_list
-		, const std::vector<std::string>& option_list
-		, int* chosen_option);
-
+	    , const unsigned maximum_length);
 
 } // namespace gui2
 
