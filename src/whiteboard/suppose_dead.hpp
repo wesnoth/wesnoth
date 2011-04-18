925b9c281ac1ac955eb3533b23bc1b149bb2400a
Gabriel Morin <gabrielmorin@gmail.com>
2011-04-18 20:16:54 +0000

    Added headers to the new files from patch #2640.
    (They should normally have been added for the patch to have been
    considered commit-ready.)

--- src/whiteboard/suppose_dead.hpp
+++ src/whiteboard/suppose_dead.hpp
@@ -1,3 +1,22 @@
+/* $Id$ */
+/*
+ Copyright (C) 2011 by Tommy Schmitz
+ Part of the Battle for Wesnoth Project http://www.wesnoth.org
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY.
+
+ See the COPYING file for more details.
+ */
+
+/**
+ * @file
+ */
+
 #ifndef WB_SUPPOSE_DEAD_HPP_
 #define WB_SUPPOSE_DEAD_HPP_
 
