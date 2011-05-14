d3de08c360864244e3dfbd70b8464791de3ea78b
Timotei Dolean <timotei21@gmail.com>
2011-05-14 06:52:04 +0000

    eclipse plugin: Tweak a bit the build script

--- utils/java/org.wesnoth.build/build.cmd
+++ utils/java/org.wesnoth.build/build.cmd
@@ -1,20 +1,19 @@
 @echo off
 
-set ECLIPSEHOME=D:\bin\eclipse\
-set ECLIPSEBUILD=D:\bin\eclipse_build\eclipse\
-set ECLIPSEDELTAPACK=d:\work\java\eclipse\3.6.2\eclipse\
+set ECLIPSEBIN=d:\work\java\eclipse\3.6.2\eclipse\
+set BUILDDIR=d:\tmp\eclipse_build
 
-set BUILDDIR=d:\tmp 
+mkdir %BUILDDIR%
 
-REM get path to equinox jar inside ECLIPSEHOME folder
-for /f "delims= tokens=1" %%c in ('dir /B /S /OD %ECLIPSEHOME%\plugins\org.eclipse.equinox.launcher_*.jar') do set EQUINOXJAR=%%c
+REM get path to equinox jar inside ECLIPSEBIN folder
+for /f "delims= tokens=1" %%c in ('dir /B /S /OD %ECLIPSEBIN%\plugins\org.eclipse.equinox.launcher_*.jar') do set EQUINOXJAR=%%c
 echo "Found equinox jar: " %EQUINOXJAR%
 
 REM find pde build folder
-for /f "delims= tokens=1" %%c in ('dir /B /S /OD %ECLIPSEHOME%\plugins\org.eclipse.pde.build_*') do set PDEBUILD_DIR=%%c
+for /f "delims= tokens=1" %%c in ('dir /B /S /OD %ECLIPSEBIN%\plugins\org.eclipse.pde.build_*') do set PDEBUILD_DIR=%%c
 echo "Found pde folder: " %PDEBUILD_DIR%
 
-java -cp %EQUINOXJAR% org.eclipse.core.launcher.Main -data workspace -application org.eclipse.ant.core.antRunner -DbuildDirectory=%BUILDDIR% -Dbase=%ECLIPSEBUILD% -DbaseLocation=%ECLIPSEBUILD% -Ddeltapack=%ECLIPSEDELTAPACK%  -Declipse.pdebuild.scripts=%PDEBUILD_DIR%\scripts -Declipse.pdebuild.templates=%PDEBUILD_DIR%\templates -buildfile build.xml
+java -cp %EQUINOXJAR% org.eclipse.core.launcher.Main -data workspace -application org.eclipse.ant.core.antRunner -DbuildDirectory=%BUILDDIR% -Dbase=%ECLIPSEBIN% -DbaseLocation=%ECLIPSEBIN% -Ddeltapack=%ECLIPSEBIN%  -Declipse.pdebuild.scripts=%PDEBUILD_DIR%\scripts -Declipse.pdebuild.templates=%PDEBUILD_DIR%\templates -buildfile build.xml
 
 IF EXIST ../org.wesnoth/build.xml rm ../org.wesnoth/build.xml
 IF EXIST ../org.wesnoth.wml/build.xml rm ../org.wesnoth.wml/build.xml
