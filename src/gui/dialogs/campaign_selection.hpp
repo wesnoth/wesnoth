48a1498ced18a0c7a1af07e45960a8d843cf75d4
Mark de Wever <koraq@xs4all.nl>
2011-05-22 17:56:21 +0000

    Make a constructor explicit.

--- src/gui/dialogs/campaign_selection.hpp
+++ src/gui/dialogs/campaign_selection.hpp
@@ -26,7 +26,7 @@ class tcampaign_selection
 	: public tdialog
 {
 public:
-	tcampaign_selection(const std::vector<config>& campaigns)
+	explicit tcampaign_selection(const std::vector<config>& campaigns)
 		: campaigns_(campaigns)
 		, choice_(-1)
 
