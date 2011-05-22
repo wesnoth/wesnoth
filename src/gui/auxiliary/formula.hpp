85f6a002aa347475ad0878a1642386e878f8dace
Mark de Wever <koraq@xs4all.nl>
2011-05-22 17:56:08 +0000

    Make a constructor explicit.

--- src/gui/auxiliary/formula.hpp
+++ src/gui/auxiliary/formula.hpp
@@ -57,7 +57,7 @@ public:
 	 *                            be converted to the type T.
 	 * @param value               The default value for the object.
 	 */
-	tformula<T>(const std::string& str, const T value = T());
+	explicit tformula<T>(const std::string& str, const T value = T());
 
 	/**
 	 * Returns the value, can only be used if the data is no formula.
