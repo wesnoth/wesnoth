1b309b09922516d3061ff783daed6788d4e2c97f
Timotei Dolean <timotei21@gmail.com>
2011-06-20 15:32:55 +0000

    eclipse plugin: Add a blank page for project properties.

---
+++ utils/java/org.wesnoth/src/org/wesnoth/propertypages/WesnothProjectPage.java
@@ -0,0 +1,20 @@
+package org.wesnoth.propertypages;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+public class WesnothProjectPage extends PropertyPage implements IWorkbenchPropertyPage
+{
+
+    public WesnothProjectPage()
+    {
+    }
+
+    @Override
+    protected Control createContents( Composite parent )
+    {
+        return null;
+    }
+}
