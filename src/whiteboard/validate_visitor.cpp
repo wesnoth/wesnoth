fa3b97ff7d404c06f762d5cb5f79986537241ef1
Tommy Schmitz <tschmitz.wesnoth@gmail.com>
2011-07-14 05:39:31 +0000

    Oops.
    That was unnecessary because set_route() does it already.

--- src/whiteboard/validate_visitor.cpp
+++ src/whiteboard/validate_visitor.cpp
@@ -137,7 +137,6 @@ validate_visitor::VALIDITY validate_visitor::evaluate_move_validity(move_ptr m_p
 			{
 				//new valid path differs from the previous one, replace
 				m.set_route(new_route);
-				m.calculate_move_cost();
 
 				//send updated path to allies
 				resources::whiteboard->queue_net_cmd(side_actions_->make_net_cmd_replace(arg_itor_,m_ptr));
