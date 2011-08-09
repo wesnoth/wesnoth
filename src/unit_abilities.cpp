0d75659eaa10aafca67bbad5576096a26483437c
Anonymissimus <anonymissimus@arcor.de>
2011-08-09 21:44:24 +0000

    Check for multiply= >=0 and divide= >0 in weapon specials.
    (fix for bug #18441)

--- src/unit_abilities.cpp
+++ src/unit_abilities.cpp
@@ -854,21 +854,28 @@ effect::effect(const unit_ability_list& list, int def, bool backstab) :
 			}
 		}
 		if (const config::attribute_value *v = cfg.get("multiply")) {
-			int multiply = int(v->to_double() * 100);
-			std::map<std::string,individual_effect>::iterator mul_effect = values_mul.find(effect_id);
-			if(mul_effect == values_mul.end() || multiply > mul_effect->second.value) {
-				values_mul[effect_id].set(MUL,multiply,i->first,i->second);
+			if(*v < 0) {
+				ERR_NG << "multiplication by a negative number with multiply= in weapon special " << effect_id << "\n";
+			}
+			else {
+				int multiply = int(v->to_double() * 100);
+				std::map<std::string,individual_effect>::iterator mul_effect = values_mul.find(effect_id);
+				if(mul_effect == values_mul.end() || multiply > mul_effect->second.value) {
+					values_mul[effect_id].set(MUL,multiply,i->first,i->second);
+				}
 			}
 		}
 		if (const config::attribute_value *v = cfg.get("divide")) {
-			if (*v == 0) {
-				ERR_NG << "division by zero with division= in weapon special " << effect_id << "\n";
-				return;
+			if (*v <= 0) {
+				ERR_NG << "division by zero or negative number with divide= in weapon special " << effect_id << "\n";
 			}
-			int divide = int(v->to_double() * 100);
-			std::map<std::string,individual_effect>::iterator div_effect = values_div.find(effect_id);
-			if(div_effect == values_div.end() || divide > div_effect->second.value) {
-				values_div[effect_id].set(DIV,divide,i->first,i->second);
+			else
+			{
+				int divide = int(v->to_double() * 100);
+				std::map<std::string,individual_effect>::iterator div_effect = values_div.find(effect_id);
+				if(div_effect == values_div.end() || divide > div_effect->second.value) {
+					values_div[effect_id].set(DIV,divide,i->first,i->second);
+				}
 			}
 		}
 	}
