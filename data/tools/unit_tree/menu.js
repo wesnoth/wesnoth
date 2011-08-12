0cb1ca0505fb94f92fdf4c5c9d268f2615e39160
Elias Pschernig <elias@pschernig.at>
2011-08-12 20:52:51 +0000

    [units.wesnoth.org] Fixed some CSS problems, collapsed the sidebar menus.

---
+++ data/tools/unit_tree/menu.js
@@ -0,0 +1,26 @@
+var all = new Array();
+
+function toggle_menu(event, id, what) {
+    var e = document.getElementById(id);
+    var show = what;
+    
+    /* Since we have Javascript, disable the CSS menu trigger. */
+    event.className = "";
+   
+    if (show == 2) {
+        if (e.style.display == 'block') show = 0;
+    }
+    if (show == 0) {
+        e.style.display = 'none';
+    }
+    else {
+        e.style.display = 'block';
+        all[id] = 1;
+        for (mid in all) {
+            if (mid != id) {
+                e = document.getElementById(mid);
+                e.style.display = 'none';
+            }
+        }
+    }
+}
