f8978538f00b0f698a4497a488063ed99a38d16b
Mark de Wever <koraq@xs4all.nl>
2011-08-14 17:59:33 +0000

    Initialize all members.

--- src/serialization/schema_validator.cpp
+++ src/serialization/schema_validator.cpp
@@ -115,8 +115,14 @@ schema_validator::schema_validator()
 schema_validator::~schema_validator(){}
 
 schema_validator::schema_validator(const std::string & config_file_name)
-	:create_exceptions_(false),stack_(),counter_(),cache_(){
-	config_read_ = read_config_file(config_file_name);
+	: config_read_(read_config_file(config_file_name))
+	, create_exceptions_(false)
+	, root_()
+	, stack_()
+	, counter_()
+	, cache_()
+	, types_()
+{
 	if (! config_read_) {
 		ERR_VL << "Schema file "<< config_file_name << " was not read.\n";
 		throw abstract_validator::error("Schema file "+ config_file_name
