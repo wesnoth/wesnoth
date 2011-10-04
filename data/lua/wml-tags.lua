84f1a90ebc6859d534f49a55e4ad10344461204c
Anonymissimus <anonymissimus@arcor.de>
2011-10-04 12:54:19 +0000

    add a comment

--- data/lua/wml-tags.lua
+++ data/lua/wml-tags.lua
@@ -65,6 +65,8 @@ function wml_actions.gold(cfg)
 	end
 end
 
+--note: This tag can't easily (without deprecation) be extended to store an array,
+--since the gold is stored in a scalar variable, not a container (there's no key).
 function wml_actions.store_gold(cfg)
 	local team = wesnoth.get_sides(cfg)[1]
 	if team then wesnoth.set_variable(cfg.variable or "gold", team.gold) end
