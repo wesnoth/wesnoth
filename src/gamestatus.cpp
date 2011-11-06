645b8475252bd86beb22296d82992f171ee599dd
Anonymissimus <anonymissimus@arcor.de>
2011-11-06 02:00:24 +0000

    deprecated ancient [scenario]objectives= attribute

--- src/gamestatus.cpp
+++ src/gamestatus.cpp
@@ -783,8 +783,10 @@ protected:
 		// If this team has no objectives, set its objectives
 		// to the level-global "objectives"
 		const config::attribute_value objectives = level_["objectives"];
-		if (!objectives.blank() && t_->objectives().empty())
-			t_->set_objectives(objectives.t_str(), false);
+		if (!objectives.blank()) {
+			WRN_NG << "[scenario]objectives= is deprecated, use [event][objectives]\n";
+			if(t_->objectives().empty()) t_->set_objectives(objectives.t_str(), false);
+		}
 	}
 
 
