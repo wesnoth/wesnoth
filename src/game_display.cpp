c784e97387fe81bdd95f193dc13de0b0ce26b84c
Anonymissimus <anonymissimus@arcor.de>
2011-12-06 14:58:07 +0000

    don't use 0 instead of NULL
    (actually it took me some time to realize that this is
    no side number or team index)

--- src/game_display.cpp
+++ src/game_display.cpp
@@ -1304,7 +1304,7 @@ void game_display::set_team(size_t teamindex, bool show_everything)
 	}
 	else
 	{
-		labels().set_team(0);
+		labels().set_team(NULL);
 		viewpoint_ = NULL;
 	}
 	labels().recalculate_labels();
