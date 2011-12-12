da80e7cf0b4291023aca860f170d3c0d5b6fd168
Ali El Gariani <alinkmaze@gmail.com>
2011-12-12 03:01:57 +0000

    Make Lua get_time_of_day() handles off-map correctly and consistently
    (same way regardless of the illumination parameter, and also as all
    the rest of the game does)

--- src/scripting/lua.cpp
+++ src/scripting/lua.cpp
@@ -1724,7 +1724,7 @@ static int intf_get_time_of_day(lua_State *L)
 
 	const time_of_day& tod = consider_illuminates ?
 		resources::tod_manager->get_illuminated_time_of_day(loc, for_turn) :
-		resources::tod_manager->get_time_of_day(loc, for_turn);
+		resources::tod_manager->get_time_of_day_with_areas(loc, for_turn);
 
 	lua_newtable(L);
 	lua_pushstring(L, tod.id.c_str());
