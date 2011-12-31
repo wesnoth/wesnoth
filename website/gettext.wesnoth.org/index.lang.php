7b1d5f74a57c1a770a568def2a0077870031b97d
Ignacio R. Morelle <shadowm@wesnoth.org>
2011-12-31 23:37:21 +0000

    gettext.wesnoth.org: New Year copyright update

--- website/gettext.wesnoth.org/index.lang.php
+++ website/gettext.wesnoth.org/index.lang.php
@@ -265,7 +265,7 @@ foreach($stats as $stat){
 <div> <br/> </div>
 <div id="footer">
 <div id="footnote">
-&copy; 2003&ndash;2011 The Battle for Wesnoth<br/>
+&copy; 2003&ndash;2012 The Battle for Wesnoth<br/>
 <br/>
 <a href="http://validator.w3.org/check?uri=referer"><img
 src="http://www.w3.org/Icons/valid-xhtml10"
