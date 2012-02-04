19b4cd90766a7048bf365710849fc32278377690
Iurii Chernyi <terraninfo@terraninfo.net>
2012-02-04 14:22:49 +0000

    reapply Mordante's patch allowing to throw some exceptions 'through' lua

--- src/lua/ldo.c
+++ src/lua/ldo.c
@@ -12,6 +12,12 @@
 #define ldo_c
 #define LUA_CORE
 
+#include "../lua_jailbreak_exception.hpp"
+#include <exception>
+#if !defined(__cplusplus)
+#error "Exception support requires a C++ compiler."
+#endif
+
 #include "lua.h"
 
 #include "lapi.h"
@@ -46,14 +52,27 @@
 ** C++ code, with _longjmp/_setjmp when asked to use them, and with
 ** longjmp/setjmp otherwise.
 */
-#if !defined(LUAI_THROW)
-
-#if defined(__cplusplus) && !defined(LUA_USE_LONGJMP)
+#if defined(__cplusplus)
 /* C++ exceptions */
-#define LUAI_THROW(L,c)		throw(c)
+#define LUAI_THROW(L,c) throw(c)
 #define LUAI_TRY(L,c,a) \
-	try { a } catch(...) { if ((c)->status == 0) (c)->status = -1; }
-#define luai_jmpbuf		int  /* dummy variable */
+        try { \
+                try { \
+                        a \
+                } catch(const tlua_jailbreak_exception &e) { \
+                        e.store(); \
+                        throw; \
+                } catch(const std::exception &e) { \
+                        lua_pushstring(L, e.what()); \
+                        luaG_errormsg(L); \
+                        throw; \
+                } \
+        } catch(...) { \
+                if((c)->status == 0) \
+                        (c)->status = -1;\
+        }
+#define luai_jmpbuf     int  /* dummy variable */
+
 
 #elif defined(LUA_USE_ULONGJMP)
 /* in Unix, try _longjmp/_setjmp (more efficient) */
@@ -66,8 +85,6 @@
 #define LUAI_THROW(L,c)		longjmp((c)->b, 1)
 #define LUAI_TRY(L,c,a)		if (setjmp((c)->b) == 0) { a }
 #define luai_jmpbuf		jmp_buf
-
-#endif
 
 #endif
 
