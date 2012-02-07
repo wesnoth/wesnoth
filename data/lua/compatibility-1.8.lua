fb982ed4004d521c1e9802cbb6cacde7c38ded22
Anonymissimus <anonymissimus@arcor.de>
2012-02-07 23:27:51 +0000

    Implement scheduled deprecation warnings for the functions...
    ...in compatibility-1.8.lua.
    
    Except for wesnoth.fire, which should better stay I think.

--- data/lua/compatibility-1.8.lua
+++ data/lua/compatibility-1.8.lua
@@ -1,17 +1,20 @@
 --! #textdomain wesnoth
 
--- This file provides an implementation of all the Lua functions removed
--- from the engine between 1.8 and 1.10.
+-- This file may provide an implementation of Lua functions removed from the engine.
 
-function wesnoth.get_side(i)
+local helper = wesnoth.require "lua/helper.lua"
+
+local get_side = function(i)
 	return wesnoth.sides[i]
 end
+wesnoth.get_side = helper.deprecate("wesnoth.get_side is deprecated, use wesnoth.sides instead", get_side)
 
-function wesnoth.get_side_count()
+local get_side_count = function()
 	return #wesnoth.sides
 end
+wesnoth.get_side_count = helper.deprecate("wesnoth.get_side_count is deprecated, use #wesnoth.sides instead", get_side_count)
 
-function wesnoth.get_unit_type_ids()
+local get_unit_type_ids = function()
 	local t = {}
 	for k,v in pairs(wesnoth.unit_types) do
 		table.insert(t, k)
@@ -19,17 +22,23 @@ function wesnoth.get_unit_type_ids()
 	table.sort(t)
 	return t
 end
+wesnoth.get_unit_type_ids = helper.deprecate("wesnoth.get_unit_type_ids is deprecated, use wesnoth.unit_types instead", get_unit_type_ids)
 
-function wesnoth.get_unit_type(t)
+local get_unit_type = function(t)
 	return wesnoth.unit_types[t]
 end
+wesnoth.get_unit_type = helper.deprecate("wesnoth.get_unit_type is deprecated, use wesnoth.unit_types instead", get_unit_type)
 
-function wesnoth.register_wml_action(name, handler)
+local register_wml_action = function(name, handler)
 	local old = wesnoth.wml_actions[name]
 	wesnoth.wml_actions[name] = handler
 	return old
 end
+wesnoth.register_wml_action = helper.deprecate("wesnoth.register_wml_action is deprecated, use wesnoth.wml_actions instead", register_wml_action)
 
+-- Calling wesnoth.fire isn't the same as calling wesnoth.wml_actions[name] due to the passed vconfig userdata
+-- which also provides "constness" of the passed wml object from the point of view of the caller.
+-- So please don't remove since it's not deprecated.
 function wesnoth.fire(name, cfg)
 	wesnoth.wml_actions[name](wesnoth.tovconfig(cfg or {}))
 end
