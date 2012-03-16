18b5571d875e61d6633392ba1a082e2fd80f8a5d
Ignacio R. Morelle <shadowm@wesnoth.org>
2012-03-16 18:33:34 +0000

    editor: Fix several source file header inconsistencies

--- src/editor/palette/common_palette.hpp
+++ src/editor/palette/common_palette.hpp
@@ -1,15 +1,16 @@
+/* $Id$ */
 /*
-  Copyright (C) 2003 - 2012 by David White <dave@whitevine.net>
-  Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+   Copyright (C) 2003 - 2012 by David White <dave@whitevine.net>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
 
-  See the COPYING file for more details.
+   See the COPYING file for more details.
 */
 
 #ifndef COMMON_PALETTES_H_INCLUDED
