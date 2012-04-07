44f842d58c1ef33279d59db356ba9ea9f64da1f8
Mark de Wever <koraq@xs4all.nl>
2012-04-07 13:55:54 +0000

    Removing a warning about relative paths.
    The issue was spotted by Ivanovic.

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -753,18 +753,7 @@ std::istream *istream_file(const std::string &fname)
 		s->clear(std::ios_base::failbit);
 		return s;
 	}
-#ifndef _WIN32
-	// TODO: Should also be done for Windows; but *nix systems should
-	// already be sufficient to catch most offenders.
-	if (fname[0] != '/') {
-		WRN_FS << "Trying to open file with relative path: '" << fname << "'.\n";
-#if 0
-		std::ifstream *s = new std::ifstream();
-		s->clear(std::ios_base::failbit);
-		return s;
-#endif
-	}
-#endif
+
 	std::ifstream *s = new std::ifstream(fname.c_str(),std::ios_base::binary);
 	if (s->is_open())
 		return s;
