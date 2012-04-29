3822677a8a06f9cba4fba385fc5edbfc3da3e0ef
Mark de Wever <koraq@xs4all.nl>
2012-04-29 14:15:51 +0000

    Add a copyright header.
    The file like formatter.hpp was introduced in 2008-03-03T05:40:29Z!davewx7@gmail.com. So used the
    copyright statement of formatter.hpp.

--- src/map_utils.hpp
+++ src/map_utils.hpp
@@ -1,3 +1,16 @@
+/* $Id$ */
+/*
+   Copyright (C) 2007 - 2012 by David White <dave.net>
+   Part of the Silver Tree Project
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by or later.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
 #ifndef MAP_UTILS_HPP_INCLUDED
 #define MAP_UTILS_HPP_INCLUDED
 
