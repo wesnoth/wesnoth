aa51d1c6da9cadba2b0d85a89dfe3acf2c9f4669
Mark de Wever <koraq@xs4all.nl>
2012-04-29 14:15:54 +0000

    Adds emulation for C++11's std::map::at().
    It is used to test some new listbox code, but can be generally useful.
    
    A non-official reference can be found here:
    http://en.cppreference.com/w/cpp/container/map/at

--- src/map_utils.hpp
+++ src/map_utils.hpp
@@ -15,6 +15,7 @@
 #define MAP_UTILS_HPP_INCLUDED
 
 #include <map>
+#include <stdexcept>
 
 template<typename K, typename V>
 const V& map_get_value_default(const std::map<K,V>& m, const K& key, const V& val) {
@@ -24,6 +25,41 @@ const V& map_get_value_default(const std::map<K,V>& m, const K& key, const V& va
 	} else {
 		return val;
 	}
+}
+
+/**
+ * Emulation for C++11's std::map::at().
+ *
+ * Acts like return map[key], but can be used on const map, and if the key
+ * doesn't exist will throw an exception instead of adding the key.
+ *
+ * A non-official reference can be found here:
+ * http://en.cppreference.com/w/cpp/container/map/at
+ *
+ * @note Didn't use template<class K, class V> since that has a problem when
+ * deducting the type when the key is a std::string and the type send is a
+ * charater string, e.g. "foo". Letting the map deduct the K and V types works.
+ *
+ * @throw std::out_of_range       When the key is not in the map.
+ *
+ * @param map                     The map search into.
+ * @param key                     The key to search for.
+ *
+ * @returns                       A copy of the value of key. @note C++11 uses
+ *                                a reference, but it's not possible to create
+ *                                a reference from an iterator.
+ */
+template<class M>
+inline typename M::mapped_type at(
+		  const M& map
+		, const typename M::key_type& key)
+{
+	typename M::const_iterator itor = map.find(key);
+	if(itor == map.end()) {
+		throw std::out_of_range("Key »" + key + "« doesn't exist.");
+	}
+
+	return itor->second;
 }
 
 #endif
