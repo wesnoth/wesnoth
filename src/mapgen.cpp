f8fc1597ef1ff1ad4b0d2fa206b5f039a6a4ce2e
Fabian Müller <fabianmueller5@gmx.de>
2012-05-28 17:53:13 +0000

    Add additional attributes usage and border_size to a generated [map].
    These are currently ignored (in case of a generated map) but that
    might change in the future.

--- src/mapgen.cpp
+++ src/mapgen.cpp
@@ -43,6 +43,8 @@ config map_generator::create_scenario(const std::vector<std::string>& args)
 {
 	config res;
 	res["data"] = create_map(args);
+	res["usage"] = "map";
+	res["border_size"] = 1;
 	return res;
 }
 
