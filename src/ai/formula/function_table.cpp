9663f578ae19410019ba17e6ab4d40b6f9aa2501
Mark de Wever <koraq@xs4all.nl>
2012-07-29 19:08:18 +0000

    Remove an unused variable.
    Issue found by cppcheck.

--- src/ai/formula/function_table.cpp
+++ src/ai/formula/function_table.cpp
@@ -758,7 +758,6 @@ private:
 		std::vector<variant> vars;
 		std::vector<variant> terrain;
 		std::vector<variant> unit_t;
-		std::vector<variant> max_hp;
 		std::vector<variant> score_list;
 
 		map_location att_loc = convert_variant<location_callable>(args()[0]->evaluate(variables,add_debug_info(fdb,0,"aki_eval:attacker_current_location")))->loc();
