a264bb36ef84b5bf2580e28136902a60530df109
Mark de Wever <koraq@xs4all.nl>
2012-07-29 19:08:16 +0000

    Remove an unused variable.
    Issue found by cppcheck.

--- src/ai/default/ai.cpp
+++ src/ai/default/ai.cpp
@@ -553,7 +553,7 @@ void ai_default_recruitment_stage::analyze_potential_recruit_movements()
 
 	const unsigned int max_targets = 5;
 
-	const move_map srcdst, dstsrc;
+	const move_map dstsrc;
 	std::vector<target> targets = find_targets(dstsrc);
 	if(targets.size() > max_targets) {
 		std::sort(targets.begin(),targets.end(),target_comparer_distance(start));
