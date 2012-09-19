9f971e386da6f1653791a92da7626902fc8de99d
J. Tyne <jt_coding@verizon.net>
2012-09-19 23:39:49 +0000

    English convention: space after the colon.

--- src/sdl_utils.cpp
+++ src/sdl_utils.cpp
@@ -932,7 +932,7 @@ surface mask_surface(const surface &surf, const surface &mask, bool* empty_resul
 		if(!filename.empty()) ss << filename << ": ";
 		ss << nsurf->w << "x" << nsurf->h << "\n";
 		std::cerr << ss.str();
-		std::cerr << "It will not be masked, please use :"<< nmask->w << "x" << nmask->h << "\n";
+		std::cerr << "It will not be masked, please use: "<< nmask->w << "x" << nmask->h << "\n";
 		return nsurf;
 	}
 
@@ -1105,8 +1105,8 @@ surface light_surface(const surface &surf, const surface &lightmap, bool optimiz
 		// (different height is not a real problem)
 		// This function is used on all hexes and usually only for that
 		// so better keep it simple and efficient for the normal case
-		std::cerr << "Detected an image with bad dimensions :" << nsurf->w << "x" << nsurf->h << "\n";
-		std::cerr << "It will not be lighted, please use :"<< lightmap->w << "x" << lightmap->h << "\n";
+		std::cerr << "Detected an image with bad dimensions: " << nsurf->w << "x" << nsurf->h << "\n";
+		std::cerr << "It will not be lighted, please use: "<< lightmap->w << "x" << lightmap->h << "\n";
 		return nsurf;
 	}
 	{
