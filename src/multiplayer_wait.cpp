36f05a020d61d3aad7cb45e0e6198dfa1862b854
Mark de Wever <koraq@xs4all.nl>
2012-10-07 09:17:44 +0000

    Remove a useless cast.
    Detected by the upcoming gcc-4.8.

--- src/multiplayer_wait.cpp
+++ src/multiplayer_wait.cpp
@@ -484,7 +484,7 @@ void wait::generate_menu()
 
 			if (RCcolor.empty())
 				RCcolor = sd["side"].str();
-			leader_image = utg.image() + std::string("~RC(") + std::string(utg.flag_rgb() + ">" + RCcolor + ")");
+			leader_image = utg.image() + std::string("~RC(") + utg.flag_rgb() + ">" + RCcolor + ")";
 #endif
 		} else {
 			leader_image = leader_list_manager::random_enemy_picture;
