553fcec86f75d9ef7862d44e19460aba232b7676
Alexander van Gessel <ai0867@gmail.com>
2012-11-12 16:46:56 +0100

    Remove unnecessary lexical_cast

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -902,9 +902,9 @@ file_tree_checksum::file_tree_checksum(const config& cfg) :
 
 void file_tree_checksum::write(config& cfg) const
 {
-	cfg["nfiles"] = lexical_cast<std::string>(nfiles);
-	cfg["size"] = lexical_cast<std::string>(sum_size);
-	cfg["modified"] = lexical_cast<std::string>(modified);
+	cfg["nfiles"] = nfiles;
+	cfg["size"] = sum_size;
+	cfg["modified"] = modified;
 }
 
 bool file_tree_checksum::operator==(const file_tree_checksum &rhs) const
