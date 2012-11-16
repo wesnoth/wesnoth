64cb928533990ebde1d71c2f2f1d25a12b651d29
J. Tyne <jt_coding@verizon.net>
2012-11-16 22:53:27 +0000

    Apply gabba's patch for bug #20625.

--- src/whiteboard/manager.cpp
+++ src/whiteboard/manager.cpp
@@ -1060,13 +1060,14 @@ void manager::contextual_bump_down_action()
 
 bool manager::has_actions() const
 {
-	assert(!wait_for_side_init_);
+	assert(resources::teams);
 	return wb::has_actions();
 }
 
 bool manager::unit_has_actions(unit const* unit) const
 {
 	assert(unit != NULL);
+	assert(resources::teams);
 	return viewer_actions()->unit_has_actions(*unit);
 }
 
