7b0a48025478f6f36805158c5c32e9e6e48a1268
J. Tyne <jt_coding@verizon.net>
2013-01-05 14:53:05 +0000

    Make Lua's wesnoth.transform_unit() function preserve hit points.
    This is actually simpler to implement and is more consistent with the
    [transform_unit] tag.  Plus, the wiki documentation for
    wesnoth.transform_unit() does not state that the hit points definitely
    will be changed (it's a bit vague on that point), so this is
    consistent with the existing documentation.

--- data/lua/wml-tags.lua
+++ data/lua/wml-tags.lua
@@ -915,25 +915,24 @@ function wml_actions.transform_unit(cfg)
 	local transform_to = cfg.transform_to
 
 	for index, unit in ipairs(wesnoth.get_units(cfg)) do
-		local hitpoints = unit.hitpoints
 
 		if transform_to then
 			wesnoth.transform_unit( unit, transform_to )
 		else
+			local hitpoints = unit.hitpoints
 			local experience = unit.experience
-			unit.experience = unit.max_experience
 			local status = helper.get_child( unit.__cfg, "status" )
 
+			unit.experience = unit.max_experience
 			wml_actions.store_unit { { "filter", { id = unit.id } }, variable = "Lua_store_unit", kill = true }
 			wml_actions.unstore_unit { variable = "Lua_store_unit", find_vacant = false, advance = true, fire_event = false }
 			wesnoth.set_variable ( "Lua_store_unit")
 
+			unit.hitpoints = hitpoints
 			unit.experience = experience
 
 			for key, value in pairs(status) do unit.status[key] = value end
 		end
-
-		unit.hitpoints = hitpoints
 
 		if unit.status.not_living then unit.status.poisoned = nil end
 	end
