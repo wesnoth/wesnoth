68fcaf664a9fd1abd51b0f0ed158e6bac5dcae38
Mark de Wever <koraq@xs4all.nl>
2013-01-09 19:57:01 +0000

    Add copyright headers.

--- src/server/simple_wml.cpp
+++ src/server/simple_wml.cpp
@@ -1,3 +1,18 @@
+/* $Id$ */
+/*
+   Copyright (C) 2008 - 2013
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License 2
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
 #include <iostream>
 #include <sstream>
 
