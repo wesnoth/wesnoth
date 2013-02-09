280105c1f1fb9cf10cf7e3f636591f06d14e3896
Mark de Wever <koraq@xs4all.nl>
2013-02-09 16:10:49 +0000

    Replace BOOST_FOREACH with FOREACH.

--- src/gui/auxiliary/window_builder/listbox.cpp
+++ src/gui/auxiliary/window_builder/listbox.cpp
@@ -30,9 +30,8 @@
 #include "gui/widgets/pane.hpp"
 #include "gui/widgets/viewport.hpp"
 #include "gui/widgets/settings.hpp"
+#include "utils/foreach.tpp"
 #include "wml_exception.hpp"
-
-#include <boost/foreach.hpp>
 
 namespace gui2 {
 
@@ -70,12 +69,12 @@ tbuilder_listbox::tbuilder_listbox(const config& cfg)
 		return;
 	}
 
-	BOOST_FOREACH(const config& row, data.child_range("row")) {
+	FOREACH(const AUTO& row, data.child_range("row")) {
 		unsigned col = 0;
 
-		BOOST_FOREACH(const config& c, row.child_range("column")) {
+		FOREACH(const AUTO& c, row.child_range("column")) {
 			list_data.push_back(string_map());
-			BOOST_FOREACH(const config::attribute& i, c.attribute_range()) {
+			FOREACH(const AUTO& i, c.attribute_range()) {
 				list_data.back()[i.first] = i.second;
 			}
 			++col;
