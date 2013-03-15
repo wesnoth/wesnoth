43f617179ec1ec6e770cdabee28959d534bf35f5
Mark de Wever <koraq@xs4all.nl>
2013-03-15 20:28:52 +0000

    Adds autorevision based revision numbers.
    This commit adds the support for CMake.

--- src/game_config.cpp
+++ src/game_config.cpp
@@ -27,6 +27,9 @@
 #ifdef HAVE_REVISION
 #include "revision.hpp"
 #endif /* HAVE_REVISION */
+#ifdef LOAD_REVISION
+#include "revision.h"
+#endif
 
 #include <boost/foreach.hpp>
 
@@ -51,6 +54,10 @@ namespace game_config
 	std::string default_terrain;
 #ifdef REVISION
 	const std::string revision = VERSION " (" REVISION ")";
+#elif defined(VCS_SHORT_HASH) && defined(VCS_WC_MODIFIED)
+	const std::string revision
+		= std::string(VCS_SHORT_HASH)
+		+ (VCS_WC_MODIFIED ? "-Modified" : "-Clean");
 #else
 	const std::string revision = VERSION;
 #endif
