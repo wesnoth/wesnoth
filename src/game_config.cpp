d54cd48f813c91f0076b8f93ad507e52fa9fc44e
Sergey Popov <loonycyborg@gmail.com>
2013-03-17 00:38:08 +0000

    Made the version string from autorevision...
    ...also include base version from wesconfig.h

--- src/game_config.cpp
+++ src/game_config.cpp
@@ -53,8 +53,8 @@ namespace game_config
 	const std::string revision = VERSION " (" REVISION ")";
 #elif defined(VCS_SHORT_HASH) && defined(VCS_WC_MODIFIED)
 	const std::string revision
-		= std::string(VCS_SHORT_HASH)
-		+ (VCS_WC_MODIFIED ? "-Modified" : "-Clean");
+		= std::string(VERSION) + " ("
+		+ VCS_SHORT_HASH + (VCS_WC_MODIFIED ? "-Modified" : "-Clean") + ")";
 #else
 	const std::string revision = VERSION;
 #endif
