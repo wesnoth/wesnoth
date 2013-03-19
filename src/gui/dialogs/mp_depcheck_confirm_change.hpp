8ad7a8d2ef171160dc75cbba6e3b79e57ffda02d
Boldizsár Lipka <lipkab@zoho.com>
2013-03-19 06:56:34 +0000

    Update my email.

--- src/gui/dialogs/mp_depcheck_confirm_change.hpp
+++ src/gui/dialogs/mp_depcheck_confirm_change.hpp
@@ -1,6 +1,6 @@
 /* $Id: mp_game_settings.cpp 55412 2012 - 2013-09-25 17:42:18Z lipk $ */
 /*
-   Copyright (C) 2012 - 2013 by Boldizsár Lipka <lipka.boldizsar@gmail.com>
+   Copyright (C) 2012 - 2013 by Boldizsár Lipka <lipkab@zoho.com>
    Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
