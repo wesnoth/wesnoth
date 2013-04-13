c6da4fcbd017e428170304aaf88f1a73a00b060e
upthorn <upthorn@gmail.com>
2013-04-13 05:08:38 -0700

    Refactor persist_context to correct subnamespace crash and avoid holding stale config references in general.

--- src/persist_context.hpp
+++ src/persist_context.hpp
@@ -99,80 +99,14 @@ public:
 		}
 	};
 protected:
-	struct node {
-		typedef std::map<std::string,node*> child_map;
-
-		std::string name_;
-		persist_context *root_;
-		node *parent_;
-		child_map children_;
-		config &cfg_;
-
-		node(std::string name, persist_context *root, config & cfg, node *parent = NULL)
-			: name_(name)
-			, root_(root)
-			, parent_(parent)
-			, children_()
-			, cfg_(cfg)
-		{
-		}
-
-		~node() {
-			for (child_map::iterator i = children_.begin(); i != children_.end(); ++i)
-				delete (i->second);
-		}
-		config &cfg() { return cfg_; }
-		node &add_child(const std::string &name) {
-			children_[name] = new node(name,root_,cfg_.child_or_add(name),this);
-			return *(children_[name]);
-		}
-		bool remove_child(const std::string &name) {
-			bool ret = false;
-			if (children_.find(name) != children_.end()) {
-				cfg_.clear_children(name);
-				cfg_.remove_attribute(name);
-				if (cfg_.child("variables").empty()) {
-					cfg_.clear_children("variables");
-					cfg_.remove_attribute("variables");
-				}
-				delete children_[name];
-				children_.erase(name);
-				ret = true;
-			}
-			return ret;
-		}
-		node &child(const name_space &name) {
-			if (name) {
-				if (children_.find(name.root_) == children_.end())
-					add_child(name.root_);
-				node &chld = *children_[name.root_];
-				return chld.child(name.next());
-			}
-			else return *this;
-		}
-		void init () {
-			for (config::all_children_iterator i = cfg_.ordered_begin(); i != cfg_.ordered_end(); ++i) {
-				if (i->key != "variables") {
-					child(i->key).init();
-				}
-			}
-			if (!cfg_.child("variables"))
-				cfg_.add_child("variables");
-		}
-	};
-
 	config cfg_;
 	name_space namespace_;
-	node root_node_;
-	node *active_;
 	bool valid_;
 	bool in_transaction_;
 
 	persist_context()
 		: cfg_()
 		, namespace_()
-		, root_node_("",this,cfg_)
-		, active_(&root_node_)
 		, valid_(false)
 		, in_transaction_(false)
 	{};
@@ -180,11 +114,35 @@ protected:
 	persist_context(const std::string &name_space)
 		: cfg_()
 		, namespace_(name_space,true)
-		, root_node_(namespace_.root_,this,cfg_)
-		, active_(&root_node_)
 		, valid_(namespace_.valid())
 		, in_transaction_(false)
 	{};
+
+	config *get_node(config &cfg, name_space &ns, bool force = false) {
+		name_space next = ns.next();
+		if (next) {
+			if (force)
+				return get_node(cfg.child_or_add(next.root_), next, true);
+			else if (cfg.has_child(next.root_))
+				return get_node(cfg.child(next.root_), next);
+			else
+				return NULL;
+		}
+		else 
+			return &cfg;
+	}
+
+	const config *get_node(const config &cfg, const name_space &ns) const {
+		name_space next = ns.next();
+		if (next) {
+			if (cfg.has_child(next.root_))
+				return get_node(cfg.child(next.root_), next);
+			else
+				return NULL;
+		}
+		else 
+			return &cfg;
+	}
 
 public:
 	virtual bool clear_var(const std::string &, bool immediate = false) = 0;
@@ -231,7 +189,6 @@ public:
 		if (!in_transaction_)
 			return false;
 		load();
-		root_node_.init();
 		in_transaction_ = false;
 		return true;
 	}
