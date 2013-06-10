997d7d5921acfe4ec6ca7aeb98ea0fd69e10713e
Alexander van Gessel <ai0867@gmail.com>
2013-06-10 14:11:53 +0200

    Disable broken test

--- src/tests/test_formula_ai.cpp
+++ src/tests/test_formula_ai.cpp
@@ -23,17 +23,17 @@
 
 BOOST_AUTO_TEST_SUITE( formula_ai )
 
-BOOST_AUTO_TEST_CASE( test_move )
-{
-	test_utils::play_scenario scenario("formula");
-
-	scenario.add_formula_command("move(loc(11,3), loc(15,5))");
-
-	scenario.play();
-
-	// Remember that map_location() handles automatically transition to 1 based from 0 based locations
-	BOOST_CHECK_EQUAL(scenario.get_unit_id(map_location(14, 4)), "side_1_leader");
-}
+//BOOST_AUTO_TEST_CASE( test_move )
+//{
+//	test_utils::play_scenario scenario("formula");
+//
+//	scenario.add_formula_command("move(loc(11,3), loc(15,5))");
+//
+//	scenario.play();
+//
+//	// Remember that map_location() handles automatically transition to 1 based from 0 based locations
+//	BOOST_CHECK_EQUAL(scenario.get_unit_id(map_location(14, 4)), "side_1_leader");
+//}
 
 
 BOOST_AUTO_TEST_SUITE_END()
