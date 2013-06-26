c8fe76751892f03e68294dbd96b28dba739977f6
Pierre T <ptalbot@mopong.net>
2013-06-26 18:51:22 +0200

    Add a class that represents a some network (metadata, binary data).

---
+++ src/campaign_server/server/network_data.hpp
@@ -0,0 +1,63 @@
+/*
+   Copyright (C) 2013 by Pierre Talbot <ptalbot@mopong.net>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#ifndef SERVER_NETWORK_DATA_HPP
+#define SERVER_NETWORK_DATA_HPP
+
+#include <string>
+#include <array>
+#include <vector>
+#include <boost/asio.hpp>
+
+#include "config.hpp"
+
+class network_data
+{
+public:
+   typedef config metadata_type;
+   typedef std::string binary_data_type;
+protected:
+   config metadata;
+   binary_data_type binary_data;
+
+public:
+   network_data(const network_data&) = delete;
+   network_data& operator=(const network_data&) = delete;
+
+   network_data(metadata_type&& metadata, binary_data_type&& binary_data)
+   : metadata(std::move(metadata))
+   , binary_data(std::move(binary_data))
+   {}
+
+   network_data() = default;
+
+   const metadata_type& get_metadata() const
+   {
+      return metadata;
+   }
+   const binary_data_type& get_binary() const
+   {
+      return binary_data;
+   }
+   metadata_type& get_metadata()
+   {
+      return metadata;
+   }
+   binary_data_type& get_binary()
+   {
+      return binary_data;
+   }  
+};
+
+#endif // SERVER_NETWORK_DATA_HPP
