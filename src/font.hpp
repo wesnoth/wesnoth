886599c2cee06de1760c93667ff7508afc58eebd
Mark de Wever <koraq@xs4all.nl>
2013-07-03 21:12:31 +0200

    Fix a compiler warning.

--- src/font.hpp
+++ src/font.hpp
@@ -215,16 +215,4 @@ void cache_mode(CACHE mode);
 
 }
 
-namespace {
-void function_to_hide_unused_variable_warning(){
-	(void) font::SIZE_TINY;
-	(void) font::SIZE_SMALL;
-
-	(void) font::SIZE_15;
-	(void) font::SIZE_PLUS;
-	(void) font::SIZE_LARGE;
-	(void) font::SIZE_XLARGE;
-}
-}
-
 #endif
