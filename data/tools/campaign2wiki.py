05addd327480780795dcc55df2509bf878256170
Thibault Févry <ThibaultFevry@gmail.com>
2013-07-13 22:53:50 +0200

    Added wiki_output, basic features now work with some campaigns.

--- data/tools/campaign2wiki.py
+++ data/tools/campaign2wiki.py
@@ -6,8 +6,19 @@ A script that autogenerates some information about campaigns for the
 CampaignInformation wiki page. The script is a WIP.
 """
 
-import glob, os.path, argparse
+#TODO: Make it work with all campaigns (see blacklist) and investigate
+#wmlparser2.py issues. More information. Output in wiki format. Cleanup.
+
+from __future__ import with_statement   # For python < 2.6
+import glob, os.path, sys
+try:
+    import argparse
+except ImportError:
+    print('Please install argparse by running "easy_install argparse"')
+    sys.exit(1)
+
 import wesnoth.wmlparser2 as wmlparser2
+
 
 class Campaign:
     """
@@ -23,9 +34,14 @@ class Campaign:
         for campaign in self.parser.get_all(tag = 'campaign'):
             self.name = campaign.get_text_val("name")
             self.id = campaign.get_text_val("id")
+            self.description = campaign.get_text_val("description")
+            # Difficulty levels are separated by commas, so there are
+            # count(',')+1 difficulty levels.
+            self.levels = campaign.get_text_val("difficulties").count(',') + 1
         self.credits_link = "http://wiki.wesnoth.org/Credits#" + self.id
         self.scenarios = self.build_scenario_list(path)
         self.total_scenarios = len(self.scenarios)
+        print self.description
         # Calculate how many scenarios a player will usually go through
         self.normal_scenarios = max([int(i.number[:2]) for i in self.scenarios ])
 
@@ -48,6 +64,20 @@ class Scenario:
             self.turns = scenario.get_text_val("turns")
             self.number = scenario.get_text_val("id", val=0)[:scenario.get_text_val("id", val=0).find('_')]      
 
+def wiki_output(campaign):
+    """
+    Takes a campaign instance and outputs information in wiki format
+    """
+    output = """== {0} ==
+{1}
+Total scenarios : {2}
+Scenarios you will go through : {3}
+Difficulty levels : {4} 
+* [{5} Credits]
+""".format(campaign.name, campaign.description, campaign.total_scenarios, campaign.normal_scenarios, campaign.levels, campaign.credits_link)
+    return output
+
+
 if __name__ == "__main__":
     # Possible arguments
     arg_parser = argparse.ArgumentParser(description='campaign2wiki is a script\
@@ -55,7 +85,7 @@ which generates information about campaigns for the wiki.')
     arg_parser.add_argument('-d', '--data', default='data/',
 dest='data_dir', help="The location of wesnoth data directory")
     arg_parser.add_argument('-o', '--output', default='/tmp/CampaignWML',
-dest='output-path', help="The location of the output file.")
+dest='output_path', help="The location of the output file.")
     arg_parser.add_argument('-w', '--wesnoth', default='./wesnoth',
 dest='wesnoth', help='The wesnoth executable location')
     args = arg_parser.parse_args()
@@ -63,9 +93,13 @@ dest='wesnoth', help='The wesnoth executable location')
     # TODO:Blacklist : investigate each one of these and see where the issues are
     blacklist = ['Delfadors_Memoirs', 'Eastern_Invasion', 'Legend_of_Wesmere',
 'Liberty', 'Northern_Rebirth', 'Sceptre_of_Fire', 'Son_Of_The_Black_Eye',
-'The_Rise_Of_Wesnoth', 'tutorial']
+'The_South_Guard', 'The_Rise_Of_Wesnoth', 'Two_Brothers', 'tutorial']
     blacklist = ['data/campaigns/' + i for i in blacklist]
-
+    output = ''
     for campaign in glob.glob(os.path.join(args.data_dir, 'campaigns/*')):
-        if campaign not in blacklist: a = Campaign(campaign, args.wesnoth)
-        print 'Campaign name : %s, id : %s, number of scenarios : %s, number of played scenarios : %s' % (a.name, a.id, a.total_scenarios, a.normal_scenarios)
+        if campaign not in blacklist: 
+            a = Campaign(campaign, args.wesnoth)
+            output += wiki_output(a)
+
+    with open(args.output_path, "w") as wiki_format:
+        wiki_format.write(output)
