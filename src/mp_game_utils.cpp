c1320fd9cda5c5c05ba077be9390277ebd5f714e
Andrius Silinskas <silinskas.andrius@gmail.com>
2013-08-19 11:46:08 +0100

    Reload game config when MP save game is selected.
    This guarantees that global game config has all the necessary
    information for the game.

--- src/mp_game_utils.cpp
+++ src/mp_game_utils.cpp
@@ -47,13 +47,18 @@ config initial_level_config(game_display& disp, const mp_game_settings& params,
 	config level;
 
 	if (params.saved_game) {
-		try{
+		try {
 			savegame::loadgame load(disp,
 				resources::config_manager->game_config(), state);
 			load.load_multiplayer_game();
 			load.fill_mplevel_config(level);
+
+			resources::config_manager->
+				load_game_config_for_game(state.classification());
 		}
 		catch (load_game_cancelled_exception){
+			return config();
+		} catch(config::error&) {
 			return config();
 		}
 	} else {
