276e1b8709112e6d824b5eb116ef6b5474691f48
mattsc <mattsc000@gmail.com>
2013-10-22 12:34:44 -0700

    Guardian MAIs: get rid of guardian_type= key
    The previous value of guardian_type= now goes directly into ai_type=

--- data/ai/micro_ais/ais/micro_ais_wml_tags.lua
+++ data/ai/micro_ais/ais/micro_ais_wml_tags.lua
@@ -286,33 +286,39 @@ function wesnoth.wml_actions.micro_ai(cfg)
         end
 
     --------- Micro AI Guardian - BCA AIs -----------------------------------
-    elseif (cfg.ai_type == 'guardian_unit') then
-        -- id= key is required also for CA deletion, for all guardians
+    elseif (cfg.ai_type == 'stationed_guardian') then
+        -- id= key is required also for CA deletion; needs to be checked separately
         if (not cfg.id) then
-            H.wml_error("[micro_ai] tag (guardian_unit) is missing required parameter: id")
+            H.wml_error("[micro_ai] tag (stationed_guardian) is missing required parameter: id")
         end
+        required_keys = { "id", "distance", "station_x", "station_y", "guard_x", "guard_y" }
+        CA_parms = { { ca_id = 'mai_guardian_stationed', score = cfg.ca_score or 300000, sticky = true } }
 
-        if (cfg.guardian_type == 'stationed_guardian') then
-            required_keys = { "id", "distance", "station_x", "station_y", "guard_x", "guard_y" }
-            CA_parms = { { ca_id = 'mai_guardian_stationed', score = cfg.ca_score or 300000, sticky = true } }
-
-        elseif (cfg.guardian_type == 'zone_guardian') then
-            required_keys = { "id", "filter_location" }
-            optional_keys = { "filter_location_enemy", "station_x", "station_y" }
-            CA_parms = { { ca_id = 'mai_guardian_zone', score = cfg.ca_score or 300000, sticky = true } }
-
-        elseif (cfg.guardian_type == 'return_guardian') then
-            required_keys = { "id", "return_x", "return_y" }
-            CA_parms = { { ca_id = 'mai_guardian_return', score = cfg.ca_score or 100010, sticky = true } }
-
-        elseif (cfg.guardian_type == 'coward') then
-            required_keys = { "id", "distance" }
-            optional_keys = { "seek_x", "seek_y","avoid_x","avoid_y" }
-            CA_parms = { { ca_id = 'mai_guardian_coward', score = cfg.ca_score or 300000, sticky = true } }
-
-        else
-            H.wml_error("[micro_ai] tag (guardian) guardian_type= key is missing or has unknown value")
+    elseif (cfg.ai_type == 'zone_guardian') then
+        -- id= key is required also for CA deletion; needs to be checked separately
+        if (not cfg.id) then
+            H.wml_error("[micro_ai] tag (zone_guardian) is missing required parameter: id")
         end
+        required_keys = { "id", "filter_location" }
+        optional_keys = { "filter_location_enemy", "station_x", "station_y" }
+        CA_parms = { { ca_id = 'mai_guardian_zone', score = cfg.ca_score or 300000, sticky = true } }
+
+    elseif (cfg.ai_type == 'return_guardian') then
+        -- id= key is required also for CA deletion; needs to be checked separately
+        if (not cfg.id) then
+            H.wml_error("[micro_ai] tag (return_guardian) is missing required parameter: id")
+        end
+        required_keys = { "id", "return_x", "return_y" }
+        CA_parms = { { ca_id = 'mai_guardian_return', score = cfg.ca_score or 100010, sticky = true } }
+
+    elseif (cfg.ai_type == 'coward') then
+        -- id= key is required also for CA deletion; needs to be checked separately
+        if (not cfg.id) then
+            H.wml_error("[micro_ai] tag (coward) is missing required parameter: id")
+        end
+        required_keys = { "id", "distance" }
+        optional_keys = { "seek_x", "seek_y","avoid_x","avoid_y" }
+        CA_parms = { { ca_id = 'mai_guardian_coward', score = cfg.ca_score or 300000, sticky = true } }
 
     --------- Micro AI Animals  - side-wide and BCA AIs ------------------------------------
     elseif (cfg.ai_type == 'big_animals') then
