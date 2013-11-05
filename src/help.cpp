7feeeb33c4d219660eb1ae022a5743a49db6f031
Alexander van Gessel <ai0867@gmail.com>
2013-11-05 15:23:04 +0100

    Fix advance references to units that have variations
    Reported by bumbadadabum

--- src/help.cpp
+++ src/help.cpp
@@ -1372,7 +1372,8 @@ public:
 					std::string lang_unit = type->type_name();
 					std::string ref_id;
 					if (description_type(*type) == FULL_DESCRIPTION) {
-						ref_id = unit_prefix + type->id();
+						const std::string section_prefix = type->variations().empty() ? "" : "..";
+						ref_id = section_prefix + unit_prefix + type->id();
 					} else {
 						ref_id = unknown_unit_topic;
 						lang_unit += " (?)";
