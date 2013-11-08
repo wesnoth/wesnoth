edaaad33b78aad860b3ecfb75ea5273344882206
gfgtdf <f492343@rmqkr.net>
2013-11-08 21:18:35 +0100

    add file scripting/lua_types.cpp to makelist.

--- src/scripting/lua_types.hpp
+++ src/scripting/lua_types.hpp
@@ -1,8 +1,20 @@
+/*
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
 
 typedef void* luatype;
 
 // i dont want to cast to void* each time ....
-// a drawback is thats these are now normal static variables wich are initialised at initialisation time (you shoudnt use these at/before initialisation time).
+// a drawback is, that these are now normal static variables wich are initialised at initialisation time (so you shoudn't use these at/before initialisation time).
 
 
 
