ba69f8ef39e9ddfda054fcd39cbd741f33fdd2cd
JaMiT <jt_coding@zoho.com>
2013-11-16 01:52:24 -0600

    Alphabetize

--- src/unit_types.cpp
+++ src/unit_types.cpp
@@ -1070,7 +1070,7 @@ const config & unit_type::build_unit_cfg() const
 	// Remove "pure" unit_type attributes (attributes that do not get directly
 	// copied to units; some do get copied, but under different keys).
 	static char const *unit_type_attrs[] = { "attacks", "base_ids", "die_sound",
-		"healed_sound", "experience", "flies", "hide_help", "hitpoints",
+		"experience", "flies", "healed_sound", "hide_help", "hitpoints",
 		"id", "ignore_race_traits", "inherit", "movement", "movement_type",
 		"name", "num_traits", "variation_id", "variation_name" };
 	BOOST_FOREACH(const char *attr, unit_type_attrs) {
