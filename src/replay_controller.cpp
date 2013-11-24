f2e7cfae4333e6c2f0f0b5eab17763b45a878bc2
mattsc <mattsc000@gmail.com>
2013-11-24 15:37:16 -0800

    Reset rng random_calls counter when resetting replays
    If this is not done, random_calls continues from where it was in the
    previous play through the replay, which results in everything being out
    of sync.

--- src/replay_controller.cpp
+++ src/replay_controller.cpp
@@ -276,6 +276,7 @@ void replay_controller::reset_replay(){
 
 	gui_->labels().read(level_);
 
+	resources::gamedata->rng().seed_random(level_["random_seed"], level_["random_calls"]);
 	statistics::fresh_stats();
 	set_victory_when_enemies_defeated(level_["victory_when_enemies_defeated"].to_bool(true));
 
