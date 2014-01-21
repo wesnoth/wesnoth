d9aa29a2f2562f1975196b42714f7690c5e36af5
Alexander van Gessel <ai0867@gmail.com>
2014-01-21 21:41:58 +0100

    Make handle_external_event's declaration unconditional
    The function is always implemented, so it should always be declared.

--- src/clipboard.hpp
+++ src/clipboard.hpp
@@ -39,8 +39,6 @@ void copy_to_clipboard(const std::string& text, const bool mouse);
  */
 std::string copy_from_clipboard(const bool mouse);
 
-#if defined(_X11) && !defined(__APPLE__)
 void handle_system_event(const SDL_Event& ev);
-#endif
 
 #endif
