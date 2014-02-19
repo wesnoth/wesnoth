f689cc8412316e19930a83668c490d576fee61e8
Chris Beck <render787@gmail.com>
2014-02-19 10:16:02 -0500

    create next_scenario_settings member of end_level_data
    it is read and written during serialization

--- src/game_end_exceptions.hpp
+++ src/game_end_exceptions.hpp
@@ -26,7 +26,7 @@
 
 #include <string>
 
-class config;
+#include "config.hpp"
 
 enum LEVEL_RESULT {
 	NONE,
@@ -107,6 +107,8 @@ struct end_level_data
 	int carryover_percentage;          /**< How much gold is carried over to next scenario. */
 	bool carryover_add;                /**< Add or replace next scenario's minimum starting gold. */
 	transient_end_level transient;
+
+	config next_scenario_settings;
 
 	void write(config& cfg) const;
 
