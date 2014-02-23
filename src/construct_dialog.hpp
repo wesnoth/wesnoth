adf0869fd27b9cedf0c3278c7b2142e51d3f80e9
JaMiT <jt_coding@zoho.com>
2014-02-23 12:00:43 -0600

    Fixes compilation with CLang 3.2 and libc++.
    This libc++ is the standard c++ library of the LLVM project.
    Backport of 16b4210fa9bf6ab811bb4b4620667fc1aeaec17f.
    Fixes bug #21303.

--- src/construct_dialog.hpp
+++ src/construct_dialog.hpp
@@ -190,11 +190,11 @@ public:
 		int x, y;
 		SDL_Rect interior, message, textbox;
 		unsigned int menu_width;
-		std::map<preview_pane *const, SDL_Rect > panes;
+		std::map<preview_pane *, SDL_Rect > panes;
 		int label_x, label_y;
 		int menu_x, menu_y, menu_height;
 		int image_x, image_y, caption_x, caption_y;
-		std::map<dialog_button *const, std::pair<int,int> > buttons;
+		std::map<dialog_button *, std::pair<int,int> > buttons;
 		//use get_frame().get_layout() to check frame dimensions
 	};
 	typedef dialog_frame::style style;
