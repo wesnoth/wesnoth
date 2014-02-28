1c528a7a37f68cf0b1ac2cc7ad7b828435d3e476
Alexander van Gessel <ai0867@gmail.com>
2014-02-28 20:59:22 +0100

    Make 'none' a special case for [unit_type]ellipse (bug #21724)

--- src/unit.cpp
+++ src/unit.cpp
@@ -2015,19 +2015,21 @@ void unit::redraw_unit()
 			ellipse="misc/ellipse";
 		}
 
-		// check if the unit has a ZoC or can recruit
-		const char* const nozoc = emit_zoc_ ? "" : "nozoc-";
-		const char* const leader = can_recruit() ? "leader-" : "";
-		const char* const selected = disp.selected_hex() == loc_ ? "selected-" : "";
+		if(ellipse != "none") {
+			// check if the unit has a ZoC or can recruit
+			const char* const nozoc = emit_zoc_ ? "" : "nozoc-";
+			const char* const leader = can_recruit() ? "leader-" : "";
+			const char* const selected = disp.selected_hex() == loc_ ? "selected-" : "";
 
-		// Load the ellipse parts recolored to match team color
-		char buf[100];
-		std::string tc=team::get_side_color_index(side_);
+			// Load the ellipse parts recolored to match team color
+			char buf[100];
+			std::string tc=team::get_side_color_index(side_);
 
-		snprintf(buf,sizeof(buf),"%s-%s%s%stop.png~RC(ellipse_red>%s)",ellipse.c_str(),leader,nozoc,selected,tc.c_str());
-		ellipse_back.assign(image::get_image(image::locator(buf), image::SCALED_TO_ZOOM));
-		snprintf(buf,sizeof(buf),"%s-%s%s%sbottom.png~RC(ellipse_red>%s)",ellipse.c_str(),leader,nozoc,selected,tc.c_str());
-		ellipse_front.assign(image::get_image(image::locator(buf), image::SCALED_TO_ZOOM));
+			snprintf(buf,sizeof(buf),"%s-%s%s%stop.png~RC(ellipse_red>%s)",ellipse.c_str(),leader,nozoc,selected,tc.c_str());
+			ellipse_back.assign(image::get_image(image::locator(buf), image::SCALED_TO_ZOOM));
+			snprintf(buf,sizeof(buf),"%s-%s%s%sbottom.png~RC(ellipse_red>%s)",ellipse.c_str(),leader,nozoc,selected,tc.c_str());
+			ellipse_front.assign(image::get_image(image::locator(buf), image::SCALED_TO_ZOOM));
+		}
 	}
 
 	if (ellipse_back != NULL) {
