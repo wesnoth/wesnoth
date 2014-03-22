9351a252493d8f6871a88211e24e530f652ab766
aquileia <sk.aquileia@gmail.com>
2014-03-22 20:55:58 +0100

    Fix a wrongly passed parameter in text_box.cpp

--- src/gui/widgets/text_box.cpp
+++ src/gui/widgets/text_box.cpp
@@ -208,7 +208,8 @@ void ttext_box::delete_selection()
 		start -= len;
 	}
 
-	set_value(utils::u8erase(get_value(), start, len));
+	utf8_string tmp = get_value();
+	set_value(utils::u8erase(tmp, start, len));
 	set_cursor(start, false);
 }
 
