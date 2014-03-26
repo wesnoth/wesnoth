ad18df295f3d318ec4bb4e2f7bbac95d7a897905
gfgtdf <f492343@rmqkr.net>
2014-03-26 22:31:01 +0100

    use synced_context in undo and rest
    in undos we have to do it differently in different actions becasue in moves we just show the move (ro save performance i guess) but in other actions (recall/recruit) we also fire the events again.
    
    "Rest" means: lua_ai, disband, fire_event (right click menus), update_shroud manualy.
    
    this commit is part of pr 121.

--- src/dialogs.cpp
+++ src/dialogs.cpp
@@ -136,8 +136,9 @@ gui::dialog_button_action::RESULT delete_recall_unit::button_pressed(int menu_se
 		assert(dismissed_unit != recall_list.end());
 
 		// Record the dismissal, then delete the unit.
-		recorder.add_disband(dismissed_unit->id());
-		recall_list.erase(dismissed_unit);
+		synced_context::run_in_synced_context("disband", replay_helper::get_disband(dismissed_unit->id()));
+		//recorder.add_disband(dismissed_unit->id());
+		//recall_list.erase(dismissed_unit);
 
 		return gui::DELETE_ITEM;
 	} else {
