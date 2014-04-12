8902e649d44fc3dd78c75631e3fa6f2c6c464075
mattsc <mattsc000@gmail.com>
2014-04-12 08:34:23 -0700

    Swarm Micro AI: code cleanup

--- data/ai/micro_ais/cas/ca_swarm_scatter.lua
+++ data/ai/micro_ais/cas/ca_swarm_scatter.lua
@@ -4,7 +4,7 @@ local AH = wesnoth.require "ai/lua/ai_helper.lua"
 local function get_enemies(cfg)
     local scatter_distance = cfg.scatter_distance or 3
     local enemies = wesnoth.get_units {
-        { "filter_side", {{"enemy_of", {side = wesnoth.current.side} }} },
+        { "filter_side", { { "enemy_of", { side = wesnoth.current.side } } } },
         { "filter_location",
             { radius = scatter_distance, { "filter", { side = wesnoth.current.side } } }
         }
@@ -31,9 +31,9 @@ function ca_swarm_scatter:execution(ai, cfg)
 
     -- In this case we simply maximize the distance from all these close enemies
     -- but only for units that are within 'vision_distance' of one of those enemies
-    for i,unit in ipairs(units) do
+    for _,unit in ipairs(units) do
         local unit_enemies = {}
-        for i,e in ipairs(enemies) do
+        for _,e in ipairs(enemies) do
             if (H.distance_between(unit.x, unit.y, e.x, e.y) <= vision_distance) then
                 table.insert(unit_enemies, e)
             end
@@ -42,7 +42,7 @@ function ca_swarm_scatter:execution(ai, cfg)
         if unit_enemies[1] then
             local best_hex = AH.find_best_move(unit, function(x, y)
                 local rating = 0
-                for i,e in ipairs(unit_enemies) do
+                for _,e in ipairs(unit_enemies) do
                     rating = rating + H.distance_between(x, y, e.x, e.y)
                 end
                 return rating
