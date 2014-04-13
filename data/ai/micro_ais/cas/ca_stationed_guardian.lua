41687b5569b55e2e8f7994136d05598150bfff7e
mattsc <mattsc000@gmail.com>
2014-04-12 21:56:42 -0700

    Guardian Micro AIs: code cleanup

--- data/ai/micro_ais/cas/ca_stationed_guardian.lua
+++ data/ai/micro_ais/cas/ca_stationed_guardian.lua
@@ -18,97 +18,84 @@ function ca_stationed_guardian:evaluation(ai, cfg)
 end
 
 function ca_stationed_guardian:execution(ai, cfg)
-    -- (s_x,s_y): coordinates where guardian is stationed; tries to move here if there is nobody to attack
-    -- (g_x,g_y): location that the guardian guards
+    -- (s_x, s_y): coordinates where guardian is stationed; tries to move here if there is nobody to attack
+    -- (g_x, g_y): location that the guardian guards
 
     local guardian = get_guardian(cfg)
 
-    -- find if there are enemies within 'distance'
     local enemies = wesnoth.get_units {
-        { "filter_side", {{"enemy_of", {side = wesnoth.current.side} }} },
-        { "filter_location", {x = guardian.x, y = guardian.y, radius = cfg.distance} }
+        { "filter_side", { { "enemy_of", { side = wesnoth.current.side } } } },
+        { "filter_location", { x = guardian.x, y = guardian.y, radius = cfg.distance } }
     }
 
-    -- if no enemies are within 'distance': keep guardian from doing anything and exit
+    -- If no enemies are within cfg.distance: keep guardian from doing anything and exit
     if not enemies[1] then
-        --print("No enemies close -> sleeping:",guardian.id)
         AH.checked_stopunit_moves(ai, guardian)
         return
     end
 
     -- Otherwise, guardian will either attack or move toward station
-    --print("Guardian unit waking up",guardian.id)
-    -- enemies must be within 'distance' of guard, (s_x,s_y) *and* (g_x,g_y)
-    -- simultaneous for guard to attack
-    local target = {}
-    local min_dist = 9999
-    for i,e in ipairs(enemies) do
-        local ds = H.distance_between(cfg.station_x, cfg.station_y, e.x, e.y)
-        local dg = H.distance_between(cfg.guard_x, cfg.guard_y, e.x, e.y)
+    -- Enemies must be within cfg.distance of guardian, (s_x, s_y) *and* (g_x, g_y)
+    -- simultaneously for guardian to attack
+    local target, min_dist = {}, 9e99
+    for _,e in ipairs(enemies) do
+        local dist_s = H.distance_between(cfg.station_x, cfg.station_y, e.x, e.y)
+        local dist_g = H.distance_between(cfg.guard_x, cfg.guard_y, e.x, e.y)
 
-        -- If valid target found, save the one with the shortest distance from (g_x,g_y)
-        if (ds <= cfg.distance) and (dg <= cfg.distance) and (dg < min_dist) then
-            --print("target:", e.id, ds, dg)
-            target = e
-            min_dist = dg
+        -- If valid target found, save the one with the shortest distance from (g_x, g_y)
+        if (dist_s <= cfg.distance) and (dist_g <= cfg.distance) and (dist_g < min_dist) then
+            target, min_dist = e, dist_g
         end
     end
 
     -- If a valid target was found, guardian attacks this target, or moves toward it
-    if (min_dist ~= 9999) then
-        --print ("Go for enemy unit:", target.id)
-
-        -- Find tiles adjacent to the target, and save the one that our guardian
-        -- can reach with the highest defense rating
+    if (min_dist < 9e99) then
+        -- Find tiles adjacent to the target
+        -- Save the one with the highest defense rating that guardian can reach
         local best_defense, attack_loc = -9e99, {}
         for x,y in H.adjacent_tiles(target.x, target.y) do
-            -- only consider unoccupied hexes
-            local occ_hex = wesnoth.get_units { x=x, y=y, { "not", { id = guardian.id } } }[1]
+            -- Only consider unoccupied hexes
+            local occ_hex = wesnoth.get_units { x = x, y = y, { "not", { id = guardian.id } } }[1]
             if not occ_hex then
-                -- defense rating of the hex
                 local defense = 100 - wesnoth.unit_defense(guardian, wesnoth.get_terrain(x, y))
-                --print(x,y,defense)
                 local nh = AH.next_hop(guardian, x, y)
-                -- if this is best defense rating and guardian can reach it, save this location
-                if (nh[1] == x) and (nh[2] == y) and (defense > best_defense) then
-                    best_defense, attack_loc = defense, {x, y}
+                if nh then
+                    if (nh[1] == x) and (nh[2] == y) and (defense > best_defense) then
+                        best_defense, attack_loc = defense, {x, y}
+                    end
                 end
             end
         end
 
         -- If a valid hex was found: move there and attack
         if (best_defense ~= -9e99) then
-            --print("Attack at:",attack_loc[1],attack_loc[2],best_defense)
             AH.movefull_stopunit(ai, guardian, attack_loc)
             if (not guardian) or (not guardian.valid) then return end
             if (not target) or (not target.valid) then return end
+
             AH.checked_attack(ai, guardian, target)
-        else  -- otherwise move toward that enemy
-            --print("Cannot reach target, moving toward it")
+        else  -- Otherwise move toward that enemy
             local reach = wesnoth.find_reach(guardian)
 
             -- Go through all hexes the guardian can reach, find closest to target
-            local nh = {}  -- cannot use next_hop here since target hex is occupied by enemy
-            local min_dist = 9999
-            for i,r in ipairs(reach) do
-                -- only consider unoccupied hexes
-                local occ_hex = wesnoth.get_units { x=r[1], y=r[2], { "not", { id = guardian.id } } }[1]
+            -- Cannot use next_hop here since target hex is occupied by enemy
+            local nh, min_dist = {}, 9e99
+            for _,r in ipairs(reach) do
+                -- Only consider unoccupied hexes
+                local occ_hex = wesnoth.get_units { x = r[1], y = r[2], { "not", { id = guardian.id } } }[1]
                 if not occ_hex then
-                    local d = H.distance_between(r[1], r[2], target.x, target.y)
-                    if d < min_dist then
-                        min_dist = d
-                        nh = {r[1], r[2]}
+                    local dist = H.distance_between(r[1], r[2], target.x, target.y)
+                    if (dist < min_dist) then
+                        min_dist, nh = d, { r[1], r[2] }
                     end
                 end
             end
 
-            -- Finally, execute the move toward the target
             AH.movefull_stopunit(ai, guardian, nh)
         end
 
-    -- If no enemy within the target zone, move toward station position
+    -- If no enemy is within the target zone, move toward station position
     else
-        --print "Move toward station"
         local nh = AH.next_hop(guardian, cfg.station_x, cfg.station_y)
         AH.movefull_stopunit(ai, guardian, nh)
     end
--- data/ai/micro_ais/cas/ca_zone_guardian.lua
+++ data/ai/micro_ais/cas/ca_zone_guardian.lua
@@ -21,85 +21,70 @@ end
 function ca_zone_guardian:execution(ai, cfg)
     local guardian = get_guardian(cfg)
     local reach = wesnoth.find_reach(guardian)
-    local zone_enemy = cfg.filter_location_enemy or cfg.filter_location
-    -- enemy units within reach
 
+    local zone_enemy = cfg.filter_location_enemy or cfg.filter_location
     local enemies = wesnoth.get_units {
-            { "filter_side", {{"enemy_of", {side = wesnoth.current.side} }} },
+            { "filter_side", { { "enemy_of", { side = wesnoth.current.side } } } },
             { "filter_location", zone_enemy }
         }
     if enemies[1] then
-
-        local target = {}
-        local min_dist = 9999
-        for i,e in ipairs(enemies) do
-            local dg = H.distance_between(guardian.x, guardian.y, e.x, e.y)
-
-            -- If valid target found, save the one with the shortest distance from guardian
-            if (dg < min_dist) then
-                --print("target:", e.id, ds, dg)
-                target = e
-                min_dist = dg
+        local target, min_dist = {}, 9e99
+        for _,e in ipairs(enemies) do
+            local dist = H.distance_between(guardian.x, guardian.y, e.x, e.y)
+            if (dist < min_dist) then
+                target, min_dist = e, dist
             end
         end
 
         -- If a valid target was found, guardian attacks this target, or moves toward it
-        if (min_dist ~= 9999) then
-            --print ("Go for enemy unit:", target.id)
-
-            -- Find tiles adjacent to the target, and save the one that our guardian
-            -- can reach with the highest defense rating
+        if (min_dist < 9e99) then
+            -- Find tiles adjacent to the target
+            -- Save the one with the highest defense rating that guardian can reach
             local best_defense, attack_loc = -9e99, {}
             for x,y in H.adjacent_tiles(target.x, target.y) do
-                -- only consider unoccupied hexes
-                local occ_hex = wesnoth.get_units { x=x, y=y, { "not", { id = guardian.id } } }[1]
+                -- Only consider unoccupied hexes
+                local occ_hex = wesnoth.get_units { x = x, y = y, { "not", { id = guardian.id } } }[1]
                 if not occ_hex then
-                    -- defense rating of the hex
                     local defense = 100 - wesnoth.unit_defense(guardian, wesnoth.get_terrain(x, y))
-                    --print(x,y,defense)
                     local nh = AH.next_hop(guardian, x, y)
-                    -- if this is best defense rating and guardian can reach it, save this location
                     if nh then
                         if (nh[1] == x) and (nh[2] == y) and (defense > best_defense) then
-                            best_defense, attack_loc = defense, {x, y}
+                            best_defense, attack_loc = defense, { x, y }
                         end
                     end
                 end
             end
 
             -- If a valid hex was found: move there and attack
-            if (best_defense ~= -9e99) then
-                --print("Attack at:",attack_loc[1],attack_loc[2],best_defense)
+            if (best_defense > -9e99) then
                 AH.movefull_stopunit(ai, guardian, attack_loc)
                 if (not guardian) or (not guardian.valid) then return end
+                if (not target) or (not target.valid) then return end
+
                 AH.checked_attack(ai, guardian, target)
-            else  -- otherwise move toward that enemy
-                --print("Cannot reach target, moving toward it")
+            else  -- Otherwise move toward that enemy
                 local reach = wesnoth.find_reach(guardian)
 
                 -- Go through all hexes the guardian can reach, find closest to target
-                local nh = {}  -- cannot use next_hop here since target hex is occupied by enemy
-                local min_dist = 9999
-                for i,r in ipairs(reach) do
-                    -- only consider unoccupied hexes
-                    local occ_hex = wesnoth.get_units { x=r[1], y=r[2], { "not", { id = guardian.id } } }[1]
+                -- Cannot use next_hop here since target hex is occupied by enemy
+                local nh, min_dist = {}, 9e99
+                for _,r in ipairs(reach) do
+                    -- Only consider unoccupied hexes
+                    local occ_hex = wesnoth.get_units { x = r[1], y = r[2], { "not", { id = guardian.id } } }[1]
                     if not occ_hex then
-                        local d = H.distance_between(r[1], r[2], target.x, target.y)
-                        if d < min_dist then
-                            min_dist = d
-                            nh = {r[1], r[2]}
+                        local dist = H.distance_between(r[1], r[2], target.x, target.y)
+                        if (dist < min_dist) then
+                            min_dist, nh = dist, { r[1], r[2] }
                         end
                     end
                 end
 
-                -- Finally, execute the move toward the target
                 AH.movefull_stopunit(ai, guardian, nh)
             end
         end
 
-    -- If no enemy around or within the zone, move toward "random" position which are mainy the borders
+    -- If no enemy around or within the zone, move toward station or zone
     else
-        --print "Move toward newpos"
         local newpos
         -- If cfg.station_x/y are given, move toward that location
         if cfg.station_x and cfg.station_y then
@@ -125,8 +110,6 @@ function ca_zone_guardian:execution(ai, cfg)
 
             local locs = locs_map:to_pairs()
 
-            -- If possible locations were found, move guardian toward a random one,
-            -- otherwise the guardian stays where it is
             if (#locs > 0) then
                 local newind = math.random(#locs)
                 newpos = { locs[newind][1], locs[newind][2] }
