68d92864f9b16cf78ca8b7ac41ee34ffd16c61e3
Chris Beck <render787@gmail.com>
2014-04-13 16:25:57 -0400

    provide a desktop notification when game is ready to start
    this is played at the same time as the turn bell, it is effectively
    the same as a chat message which will cause a desktop popup on
    systems that we support this for.
    
    note: we don't check notification preference, because this should
    be done in the show_notification function, not the caller.
    however, it seems that is only done with DBUS notifications? why
    not others?

--- src/multiplayer_connect.cpp
+++ src/multiplayer_connect.cpp
@@ -559,6 +559,7 @@ void connect::process_network_data(const config& data,
 	if (!was_able_to_start && engine_.can_start_game()) {
 		DBG_MP << "play party full sound" << std::endl;
 		sound::play_UI_sound(game_config::sounds::party_full_bell);
+		game_display::get_singleton()->send_notification(_("Wesnoth"), _ ("Ready to start!"));
 	}
 }
 
