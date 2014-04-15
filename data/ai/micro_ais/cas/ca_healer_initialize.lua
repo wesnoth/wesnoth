cce0dcba9de5c4d49790d52c7c9839ee147ca566
mattsc <mattsc000@gmail.com>
2014-04-15 14:59:29 -0700

    Healer Support Micro AI: code cleanup

--- data/ai/micro_ais/cas/ca_healer_initialize.lua
+++ data/ai/micro_ais/cas/ca_healer_initialize.lua
@@ -3,26 +3,21 @@ local W = H.set_wml_action_metatable {}
 
 local ca_healer_initialize = {}
 
------- Initialize healer support at beginning of turn -----------
--- Set variables and aspects correctly at the beginning of the turn
--- This will be blacklisted after first execution each turn
 function ca_healer_initialize:evaluation(ai)
+    -- Set variables and aspects so that healers are excluded from attacks at beginning of turn
+    -- This will be blacklisted after first execution each turn
+
     local score = 999990
     return score
 end
 
 function ca_healer_initialize:execution(ai, cfg, self)
-    --print(' Initializing healer_support at beginning of Turn ' .. wesnoth.current.turn)
-
-    -- First, modify the attacks aspect to exclude healers
-    -- Always delete the attacks aspect first, so that we do not end up with 100 copies of the facet
     W.modify_ai {
         side = wesnoth.current.side,
         action = "try_delete",
         path = "aspect[attacks].facet[no_healers_attack]"
     }
 
-    -- Then set the aspect to exclude healers
     W.modify_ai {
         side = wesnoth.current.side,
         action = "add",
@@ -37,8 +32,9 @@ function ca_healer_initialize:execution(ai, cfg, self)
         } }
     }
 
-    -- We also need to set the return score of healer moves to happen _after_ combat at beginning of turn
-    self.data.HS_return_score = 95000
+    -- We also need to set the score of healer moves to happen after
+    -- combat (of other units) at beginning of turn
+    self.data.HS_healer_move_score = 95000
 end
 
 return ca_healer_initialize
--- data/ai/micro_ais/cas/ca_healer_may_attack.lua
+++ data/ai/micro_ais/cas/ca_healer_may_attack.lua
@@ -3,30 +3,24 @@ local W = H.set_wml_action_metatable {}
 
 local ca_healer_may_attack = {}
 
--- After attacks by all other units are done, reset things so that healers can attack, if desired
--- This will be blacklisted after first execution each turn
 function ca_healer_may_attack:evaluation(ai)
+    -- After attacks by all other units are done, reset things so that healers can attack, if desired
+    -- This will be blacklisted after first execution each turn
+
     local score = 99990
     return score
 end
 
 function ca_healer_may_attack:execution(ai, cfg, self)
-    --print(' Letting healers participate in attacks from now on')
-
-    --local leader = wesnoth.get_units { side = wesnoth.current.side, canrecruit = 'yes' }[1]
-    --W.message { speaker = leader.id, message = "I'm done with the RCA AI combat CA for all other units, letting healers participate now (if they cannot find a support position)." }
-
-    -- Delete the attacks aspect
-    --print("Deleting attacks aspect")
     W.modify_ai {
         side = wesnoth.current.side,
         action = "try_delete",
         path = "aspect[attacks].facet[no_healers_attack]"
     }
 
-    -- We also reset the variable containing the return score of the healers CA
-    -- This will make it use its default value
-    self.data.HS_return_score = nil
+    -- Once combat (by other units) is done, set the healer move score so that it
+    -- now happens before combat (of the healers which were so far excluded from combat)
+    self.data.HS_healer_move_score = nil
 end
 
 return ca_healer_may_attack
