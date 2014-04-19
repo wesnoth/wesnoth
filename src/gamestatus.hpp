fa21189bdd70521232b124b2385abafa3753574a
gfgtdf <tischpapier@gmail.com>
2014-04-19 19:18:35 +0200

    add a comment in gamestatus.hpp

--- src/gamestatus.hpp
+++ src/gamestatus.hpp
@@ -242,6 +242,9 @@ public:
 	std::vector<std::string> campaign_xtra_defines;  /**< more customization of data */
 
 	std::string campaign;                            /**< the campaign being played */
+	//'history' is currently not referenced outside of gamestatus.cpp
+	//also in every savefile i saw is history=""
+	//TODO: can we delete it ?
 	std::string history;                             /**< ancestral IDs */
 	std::string abbrev;                              /**< the campaign abbreviation */
 //	std::string scenario;                            /**< the scenario being played */
