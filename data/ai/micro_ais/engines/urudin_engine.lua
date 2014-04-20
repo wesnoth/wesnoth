f02857b0f1e7dee822445790dbc50f0122d55fb3
mattsc <mattsc000@gmail.com>
2014-04-20 14:29:52 -0700

    Micro AIs other engines: code cleanup

--- data/ai/micro_ais/engines/urudin_engine.lua
+++ data/ai/micro_ais/engines/urudin_engine.lua
@@ -1,17 +1,16 @@
 return {
     init = function(ai)
-
         local AH = wesnoth.require "ai/lua/ai_helper.lua"
 
-        local urudin = { }
+        local urudin = {}
 
         -- This is taken almost literally from 'Ka'lian under Attack' in 'Legend of Wesmere'
         function urudin:retreat()
-            local urudin = wesnoth.get_units({side = 3, id="Urudin"})[1]
+            local urudin = wesnoth.get_units({ side = 3, id = "Urudin" })[1]
             if urudin and urudin.valid then
-                local mhp, hp = urudin.max_hitpoints, urudin.hitpoints
+                local max_hp, hp = urudin.max_hitpoints, urudin.hitpoints
                 local turn = wesnoth.current.turn
-                if (turn >= 5) or (hp < mhp / 2) then
+                if (turn >= 5) or (hp < max_hp / 2) then
                     AH.movefull_stopunit(ai, urudin, 33, 8)
                 end
             end
