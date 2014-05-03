0a8158cd551b139948a7d7d16fa1505802a10b53
Mark de Wever <koraq@xs4all.nl>
2014-05-03 19:01:21 +0200

    Remove unneeded semicolons.

--- src/synced_context.cpp
+++ src/synced_context.cpp
@@ -445,12 +445,12 @@ set_scontext_leave_for_draw::~set_scontext_leave_for_draw()
 random_seed_choice::random_seed_choice()
 {
 
-};
+}
 
 random_seed_choice::~random_seed_choice()
 {
 
-};
+}
 
 config random_seed_choice::query_user() const
 {
@@ -460,7 +460,7 @@ config random_seed_choice::query_user() const
 	config retv;
 	retv["new_seed"] = rand();
 	return retv;
-};
+}
 config random_seed_choice::random_choice() const
 {
 	//it obviously doesn't make sense to call the uninitialized random generator to generatoe a seed ofr the same random generator;
