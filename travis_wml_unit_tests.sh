987e34a3e31598e46598b4951e5fb0e2ca4bd37f
Chris Beck <render787@gmail.com>
2014-05-19 19:14:13 -0400

    fixup travis unit tests
    - travis runs tests not in verbose mode,
      *but* if there is a failure,
      then it runs in very verbose mode, with strict mode off, to get
      errors reported in the travis log.
    - run_wml_tests points out that if you don't run in strict mode,
      some tests may not fail as expected, so the count at the end
      may differ

---
+++ travis_wml_unit_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./run_wml_tests -u
+if [ "$?" -ne 0 ]; then
+  echo -e "\n*** \n*"
+  echo "* Rerunning tests to get error messages.."
+  echo -e "*\n*** \n"
+  ./run_wml_tests -u -w -s
+fi
