0111ffbcd042dcc20777968cdfc85eecae715fad
gfgtdf <f492343@rmqkr.net>
2014-05-19 23:59:34 +0200

    add "write_to" parameter to wml_message
    "write_to" lets the wmldev specify to where the message should be
    written. Defaults to the previous behaviour.
    
    The intention is for example that one might want to give a summary  as a
    chat message and a very detailed information in the stderr file.

--- src/game_events/action_wml.cpp
+++ src/game_events/action_wml.cpp
@@ -590,8 +590,9 @@ void handle_wml_log_message(const config& cfg)
 {
 	const std::string& logger = cfg["logger"];
 	const std::string& msg = cfg["message"];
+	const std::string& type = cfg["write_to"];
 
-	put_wml_message(logger,msg);
+	put_wml_message(logger,msg,type);
 }
 
 
