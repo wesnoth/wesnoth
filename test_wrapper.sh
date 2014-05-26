96d616eb4d7d0f492786f6e956c26d30aade6083
Chris Beck <render787@gmail.com>
2014-05-26 16:04:10 -0400

    travis reruns ./test in case of error code 200, 10 times max
    This is to combat what are apparently transient timeout faults
    where the travis workers choke on running ./test and never
    actually start the process... it seems not even to get to the
    "running 140 tests" message when it ends with error code 200.

---
+++ test_wrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+COUNTER=10
+./test
+ERRORCODE=$?
+while [ $COUNTER -gt 0 -a $ERRORCODE -eq 200 ]; do
+    echo "test apparently timed out with error code 200... trying again."
+    let COUNTER = COUNTER - 1
+    ./test
+    ERRORCODE=$?
+done
+exit $ERRORCODE
