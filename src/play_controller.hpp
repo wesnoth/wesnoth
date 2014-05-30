0e55026889233b16e1232dafd41bad02f5891c72
Chris Beck <render787@gmail.com>
2014-05-29 20:01:03 -0400

    remove unnecessary virtual qualifiers

--- src/play_controller.hpp
+++ src/play_controller.hpp
@@ -110,8 +110,8 @@ public:
 	virtual void search();
 	virtual void toggle_accelerated_speed();
 
-	virtual void maybe_do_init_side(const unsigned int team_index, bool is_replay = false, bool only_visual = false);
-	virtual void do_init_side(const unsigned int team_index, bool is_replay = false, bool only_visual = false);
+	void maybe_do_init_side(const unsigned int team_index, bool is_replay = false, bool only_visual = false);
+	void do_init_side(const unsigned int team_index, bool is_replay = false, bool only_visual = false);
 	virtual void play_side(const unsigned int side_number, bool save) = 0;
 
 	virtual void force_end_turn() = 0;
