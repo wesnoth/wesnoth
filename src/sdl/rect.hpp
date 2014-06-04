eea2f2dae6f9af3497e951d0d26b1027ce383dad
Boldizsár Lipka <lipkab@zoho.com>
2014-06-04 20:53:43 +0200

    Use const arguments where appropriate.

--- src/sdl/rect.hpp
+++ src/sdl/rect.hpp
@@ -142,11 +142,11 @@ inline void fill_rect(surface& dst, SDL_Rect* dst_rect, const Uint32 color)
 }
 
 #if SDL_VERSION_ATLEAST(2,0,0)
-void fill_rect(SDL_Renderer &rnd, SDL_Rect *rect, Uint8 r, Uint8 g, Uint8 b,
-			   Uint8 a);
+void fill_rect(SDL_Renderer &rnd, const SDL_Rect *rect, Uint8 r, Uint8 g,
+			   Uint8 b, Uint8 a);
 
-void draw_rect(SDL_Renderer &rnd, SDL_Rect *rect, Uint8 r, Uint8 g, Uint8 b,
-			   Uint8 a);
+void draw_rect(SDL_Renderer &rnd, const SDL_Rect *rect, Uint8 r, Uint8 g,
+			   Uint8 b, Uint8 a);
 #endif
 } // namespace sdl
 
