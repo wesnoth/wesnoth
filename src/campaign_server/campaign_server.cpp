69520c44367e0ba373d5a61857b53853265fd01b
Ignacio R. Morelle <shadowm@wesnoth.org>
2014-06-11 00:44:32 -0400

    campaignd: Write config to disk every ten minutes, not 60 seconds
    The 60 seconds value was just intended for internal testing and it
    slipped into production by accident.
    
    (Backported 6558bdc785bb31eae0973ff9e1c246c06ea9db0d from master.)

--- src/campaign_server/campaign_server.cpp
+++ src/campaign_server/campaign_server.cpp
@@ -255,7 +255,7 @@ void server::run()
 
 			const time_t cur_ts = time(NULL);
 			// Write config to disk every ten minutes.
-			if(cur_ts - last_ts >= 60) {
+			if(cur_ts - last_ts >= 10*60) {
 				write_config();
 				last_ts = cur_ts;
 			}
