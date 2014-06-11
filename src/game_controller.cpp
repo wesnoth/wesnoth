5149601c93030c3885ea3038b19c27f108596440
Ignacio R. Morelle <shadowm@wesnoth.org>
2014-06-11 16:06:18 -0400

    Improve formatting of the MP save/replay warning for Load Game

--- src/game_controller.cpp
+++ src/game_controller.cpp
@@ -540,7 +540,7 @@ bool game_controller::load_game()
 			if (side["controller"] == "network_ai")
 				side["controller"] = "ai";
 		}
-		gui2::show_message(disp().video(), _("Warning") , _("This is a multiplayer scenario. Some parts of it may not work properly in single-player. It is recommended to load this scenario through the Multiplayer -> Load Game dialog instead."));
+		gui2::show_message(disp().video(), _("Warning") , _("This is a multiplayer scenario. Some parts of it may not work properly in single-player. It is recommended to load this scenario through the <b>Multiplayer</b> → <b>Load Game</b> dialog instead."), "", true, true);
 	}
 
 	if (load.cancel_orders()) {
