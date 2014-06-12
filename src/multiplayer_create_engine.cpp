5d4135026eb9aaf45368ab200cd12cf8a0804a66
Nathan Walker <nathan.b.walker@vanderbilt.edu>
2014-06-12 16:52:08 -0500

    Implement define= for mp scenarios and eras
    Implemented reloading of configs with define= attributes in both mp_create and mp_wait::join_game().
    Added myself to about.cfg and updated changelog.

--- src/multiplayer_create_engine.cpp
+++ src/multiplayer_create_engine.cpp
@@ -414,6 +414,25 @@ void create_engine::prepare_for_new_level()
 	parameters_.mp_scenario_name = parameters_.scenario_data["name"].str();
 }
 
+void create_engine::prepare_for_scenario()
+{
+	DBG_MP << "preparing data for campaign by reloading game config\n";
+
+	state_.classification().scenario_define =
+		current_level().data()["define"].str();
+	state_.classification().era_define =
+		resources::config_manager->game_config().find_child(
+			"era", "id", get_parameters().mp_era)["define"].str();
+	
+	resources::config_manager->
+		load_game_config_for_game(state_.classification());
+
+	current_level().set_data(
+		resources::config_manager->game_config().find_child(
+		lexical_cast<std::string> (game_classification::MULTIPLAYER),
+		"id", current_level().data()["id"]));
+}
+
 void create_engine::prepare_for_campaign(const std::string& difficulty)
 {
 	DBG_MP << "preparing data for campaign by reloading game config\n";
@@ -435,6 +454,9 @@ void create_engine::prepare_for_campaign(const std::string& difficulty)
 		current_level().data()["define"].str();
 	state_.classification().campaign_xtra_defines =
 		utils::split(current_level().data()["extra_defines"]);
+	state_.classification().era_define =
+		resources::config_manager->game_config().find_child(
+			"era", "id", get_parameters().mp_era)["define"].str();
 
 	resources::config_manager->
 		load_game_config_for_game(state_.classification());
--- src/multiplayer_wait.cpp
+++ src/multiplayer_wait.cpp
@@ -235,6 +235,14 @@ void wait::join_game(bool observe)
 		const config* campaign = &resources::config_manager->
 			game_config().find_child("campaign", "id",
 				level_.child(lexical_cast<std::string>(game_classification::MULTIPLAYER))["mp_campaign"]);
+
+		const config* scenario = &resources::config_manager->
+			game_config().find_child("multiplayer", "id",
+				level_.child(lexical_cast<std::string>(game_classification::MULTIPLAYER))["id"]);
+
+		const config* era = &resources::config_manager->
+			game_config().find_child("era", "id", level_.child("era")["id"]);
+
 		if (*campaign) {
 			state_.classification().difficulty =
 				level_.child(lexical_cast<std::string>(game_classification::MULTIPLAYER))["difficulty_define"].str();
@@ -243,6 +251,14 @@ void wait::join_game(bool observe)
 			state_.classification().campaign_xtra_defines =
 				utils::split((*campaign)["extra_defines"]);
 		}
+
+		if (*scenario)
+			state_.classification().scenario_define =
+				(*scenario)["define"].str();
+
+		if (*era)
+			state_.classification().era_define =
+				(*era)["define"].str();
 
 		// Make sure that we have the same config as host, if possible.
 		resources::config_manager->
