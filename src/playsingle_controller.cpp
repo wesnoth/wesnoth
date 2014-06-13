8e3cfc8a2b30c4e0ea36308aa7dcb6480bc15d76
Ignacio R. Morelle <shadowm@wesnoth.org>
2014-06-13 19:24:00 -0400

    Call set_button_state() from playsingle_controller::init_gui()
    Fixes the minimap buttons sans the minimap unit/village drawing pair
    being in an indeterminate overlay-less (blank) state for the duration of
    the initial 'start' WML event in scenarios, unless they are interacted
    with first.
    
    set_button_state() has to be called after play_controller::init_gui()
    finishes because that method calls game_display::begin() game first,
    which in turn instantiates the theme UI GUI1 buttons. Notice that
    set_button_state() is also called when the End Turn button is enabled
    (e.g. after 'start' is finished).
    
    The reason I'm not calling this from play_controller::init_gui()
    directly is that I am concerned about possible interactions with the
    replay_controller, which currently seems to do even sloppier work of the
    GUI before the end of the 'start' event. I might move this call there if
    that issue is sorted out, since it'll be needed for the
    replay_controller anyway.

--- src/playsingle_controller.cpp
+++ src/playsingle_controller.cpp
@@ -105,6 +105,7 @@ void playsingle_controller::init_gui(){
 	gui_->scroll_to_tile(gameboard_.map().starting_position(1), game_display::WARP);
 
 	update_locker lock_display(gui_->video(),recorder.is_skipping());
+	set_button_state(*gui_);
 	events::raise_draw_event();
 	gui_->draw();
 }
