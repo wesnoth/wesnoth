fa39a1cb20ce99d4c49ccd5e2148c34035269b54
Alexander van Gessel <ai0867@gmail.com>
2014-06-17 20:55:19 +0200

    Test wrapper for jenkins
    xvfb occasionally fails to start, causing xvfb-run to return 1.
    If that happens rerun it up to 9 times.

---
+++ jenkins-wrap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+COUNTER=9
+
+export PATH="/usr/local/bin:$PATH"
+
+run_test() {
+    xvfb-run -e xvfb-err "$@"
+}
+
+run_test "$@"
+ERRORCODE=$?
+while [ $COUNTER -gt 0 -a $ERRORCODE -eq 1 ];do
+    if [ -s xvfb-err ]; then
+        echo "xvfb output:"
+        cat xvfb-err
+        rm xvfb-err
+    fi
+    echo "\n\'xvfb-run $@' returned 1, which could mean that xvfb failed. Retrying..."
+    run_test "$@"
+    ERRORCODE=$?
+    COUNTER=$(($COUNTER - 1))
+done
+
+rm -f xvfb-err
