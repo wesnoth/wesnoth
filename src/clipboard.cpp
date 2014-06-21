wesnoth/wesnoth

commit cec386ad35a1d5ddb9b721d3c66abc091e8ca68f

fixup includes in clipboard

src/clipboard.cpp
@@ -19,6 +19,7 @@
 #include "clipboard.hpp"
 #include <algorithm>
 
+#include <SDL_events.h>
 #include <SDL_version.h>
 
 #if SDL_VERSION_ATLEAST(2,0,0)

src/clipboard.hpp
@@ -18,7 +18,8 @@
 #define CLIPBOARD_HPP_INCLUDED
 
 #include <string>
-#include "SDL.h"
+
+union SDL_Event;
 
 /**
  * Copies text to the clipboard.

commit f5746958995134f68139d62ede919758af8f1337

Fix compilation with SDL2.
Commit cec386ad35a1d5ddb9b721d3c66abc091e8ca68f broke compilation due to
removing an required include.

src/clipboard.cpp
@@ -24,6 +24,8 @@
 
 #if SDL_VERSION_ATLEAST(2,0,0)
 
+#include <SDL_clipboard.h>
+
 #define CLIPBOARD_FUNCS_DEFINED
 
 /*