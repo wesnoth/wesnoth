179c9a9786bfc7b0655614ce52149b76b669b536
gfgtdf <f492343@rmqkr.net>
2014-06-25 18:01:30 +0200

    add some copyright notices.

--- src/syncmp_handler.hpp
+++ src/syncmp_handler.hpp
@@ -1,3 +1,16 @@
+/*
+   Copyright (C) 2014
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
 #ifndef SYNCMP_HANDLER_HPP_INCLUDED
 #define SYNCMP_HANDLER_HPP_INCLUDED
 
