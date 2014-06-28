089f9e923990a66e61c811a370700c11b1c9caa5
Chris Beck <render787@gmail.com>
2014-06-28 00:17:08 -0400

    add copyright notice

--- src/unit_formula_manager.cpp
+++ src/unit_formula_manager.cpp
@@ -1,3 +1,17 @@
+/*
+   Copyright (C) 2014 by Chris Beck <render787@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
 #include "unit_formula_manager.hpp"
 
 #include "callable_objects.hpp"
