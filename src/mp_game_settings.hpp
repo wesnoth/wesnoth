98cf0e54f85fb9f2264ca7b6742d4d13591cae10
Mark de Wever <koraq@xs4all.nl>
2014-06-29 21:27:31 +0200

    Fix file encoding.
    The issue was spotted by iceiceice.

--- src/mp_game_settings.hpp
+++ src/mp_game_settings.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2003 - 2014 by Jörg Hinrichs
+   Copyright (C) 2003 - 2014 by JÃ¶rg Hinrichs
    Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
