394f4e273c23c95ddc90b25686b18b194050171d
Chris Beck <render787@gmail.com>
2014-06-29 16:33:54 -0400

    add utf8 formatting check to the clang tests on travis
    This will automatically detect BOM's and such which might be
    accidentally committed.

--- check_utf8.sh
+++ check_utf8.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Install isutf8 program (from package "moreutils" at least in linux mint)
 # in order to use this script
-find src/ -type f -exec isutf8 {} \;
-find data/ -not -name "*.png" -not -name "*.ogg" -not -name "*.jpg" -not -name "*.wav" -not -name "*.gif" -not -name "*.xcf" -type f -exec isutf8 {} \;
+set -e
+find src/ -type f -print0 | xargs -0 isutf8 --
+find data/ -not -name "*.png" -not -name "*.ogg" -not -name "*.jpg" -not -name "*.wav" -not -name "*.gif" -not -name "*.xcf" -type f -print0 | xargs -0 isutf8 --
