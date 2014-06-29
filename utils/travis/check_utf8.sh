e370bfdefc838dfa4ff66396cf4c2c04e3056968
Chris Beck <render787@gmail.com>
2014-06-29 19:38:15 -0400

    add po/ to the set of folders checked for ut8 validity by travis

--- utils/travis/check_utf8.sh
+++ utils/travis/check_utf8.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Install isutf8 program (from package "moreutils" at least in linux mint)
 # in order to use this script
+#
+# This script assumes that the current working directory is the root of the
+# wesnoth repository.
 set -e
 find src/ -type f -print0 | xargs -0 isutf8 --
 find data/ -not -name "*.png" -not -name "*.ogg" -not -name "*.jpg" -not -name "*.wav" -not -name "*.gif" -not -name "*.xcf" -type f -print0 | xargs -0 isutf8 --
+find po/ -type f -print0 | xargs -0 isutf8 --
+
