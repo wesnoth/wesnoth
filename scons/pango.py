af4a0c1c049374662a49b1d4a8ab727ded7c7239
loonycyborg <loonycyborg@gmail.com>
2014-06-30 15:56:03 +0400

    Escape >= in pkg-config command in cross-platform way

--- scons/pango.py
+++ scons/pango.py
@@ -18,7 +18,7 @@ def CheckPango(context, backend, require_version = None):
         env["ENV"]["PKG_CONFIG_PATH"] = environ.get("PKG_CONFIG_PATH")
         version_arg = ""
         if require_version:
-            version_arg = " \\>= " + require_version
+            version_arg = env["ESCAPE"](" >= ") + require_version
         env.ParseConfig("pkg-config --libs --cflags $PKGCONFIG_FLAGS pango" + backend + version_arg)
         context.Result("yes")
         return True
