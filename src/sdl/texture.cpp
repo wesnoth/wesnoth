5100fb90475e96558b6ad2ad90428cf0fd43f887
Boldizsár Lipka <lipkab@zoho.com>
2014-07-02 18:06:11 +0200

    SDL_gpu implementation for sdl::ttexture.

--- src/sdl/texture.cpp
+++ src/sdl/texture.cpp
@@ -427,3 +427,247 @@ void ttexture::initialise_from_surface(SDL_Renderer& renderer, const int access)
 } // namespace sdl
 
 #endif
+
+#ifdef SDL_GPU
+#include "rect.hpp"
+namespace sdl
+{
+ttexture::ttexture(Uint16 w, Uint16 h)
+	: image_(NULL)
+	, rotation_(0)
+	, hscale_(1)
+	, vscale_(1)
+	, smooth_scaling_(false)
+	, clip_(create_gpu_rect(0, 0, w, h))
+	, color_mod_(create_color(0, 0, 0, 255))
+{
+	image_ = GPU_CreateImage(w, h, GPU_FORMAT_RGBA);
+	if (image_ == NULL) {
+		//TODO: report errorr
+	} else {
+		image_->refcount = 1;
+	}
+}
+
+ttexture::ttexture(const std::string &file)
+	: image_(GPU_LoadImage(file.c_str()))
+	, rotation_(0)
+	, hscale_(1)
+	, vscale_(1)
+	, smooth_scaling_(false)
+	, clip_()
+	, color_mod_(create_color(0, 0, 0, 255))
+{
+	if (image_ == NULL) {
+		//TODO: report error
+	} else {
+		clip_ = create_gpu_rect(0, 0, image_->w, image_->h);
+		image_->refcount = 1;
+	}
+}
+
+ttexture::ttexture(const surface &source)
+	: image_(GPU_CopyImageFromSurface(source))
+	, rotation_(0)
+	, hscale_(1)
+	, vscale_(1)
+	, smooth_scaling_(false)
+	, clip_()
+	, color_mod_(create_color(0, 0, 0, 255))
+{
+	if (image_ == NULL) {
+		//TODO: report error
+	} else {
+		clip_ = create_gpu_rect(0, 0, image_->w, image_->h);
+		image_->refcount = 1;
+	}
+}
+
+ttexture::ttexture(SDL_Surface *source)
+	: image_(GPU_CopyImageFromSurface(source))
+	, rotation_(0)
+	, hscale_(1)
+	, vscale_(1)
+	, smooth_scaling_(false)
+	, clip_()
+	, color_mod_(create_color(0, 0, 0, 255))
+{
+	if (image_ == NULL) {
+		//TODO: report error
+	} else {
+		clip_ = create_gpu_rect(0, 0, image_->w, image_->h);
+		image_->refcount = 1;
+	}
+}
+
+ttexture::ttexture()
+	: image_(NULL)
+	, rotation_(0)
+	, hscale_(1)
+	, vscale_(1)
+	, smooth_scaling_(false)
+	, clip_(create_gpu_rect(0, 0, 0, 0))
+	, color_mod_(create_color(0, 0, 0, 255))
+{
+}
+
+sdl::ttexture::~ttexture()
+{
+	if (image_ != NULL) {
+		image_->refcount -= 1;
+		if (image_->refcount == 0) {
+			GPU_FreeImage(image_);
+		}
+	}
+}
+
+ttexture::ttexture(const ttexture &texture)
+	: image_(texture.image_)
+	, rotation_(texture.rotation_)
+	, hscale_(texture.hscale_)
+	, vscale_(texture.vscale_)
+	, smooth_scaling_(texture.smooth_scaling_)
+	, clip_(texture.clip_)
+	, color_mod_(texture.color_mod_)
+{
+	if (image_ != NULL) {
+		image_->refcount += 1;
+	}
+}
+
+ttexture &ttexture::operator =(const ttexture &texture)
+{
+	if (&texture != this) {
+		this->~ttexture();
+		new (this) ttexture(texture);
+	}
+
+	return *this;
+}
+
+void ttexture::draw(GPU_Target &target, const int x, const int y)
+{
+	GPU_SetImageFilter(image_,
+					   smooth_scaling_
+					   ? GPU_FILTER_LINEAR
+					   : GPU_FILTER_NEAREST);
+	//GPU_SetColor(image_, &color_mod_);
+	GPU_BlitTransform(image_, &clip_, &target, x + image_->w/2,
+					  y + image_->h/2, rotation_, hscale_, vscale_);
+}
+
+void ttexture::set_rotation(float rotation)
+{
+	rotation_ = rotation;
+}
+
+float ttexture::rotation() const
+{
+	return rotation_;
+}
+
+void ttexture::set_hscale(float factor)
+{
+	hscale_ = factor;
+}
+
+void ttexture::set_vscale(float factor)
+{
+	vscale_ = factor;
+}
+
+void ttexture::set_scale(float hfactor, float vfactor)
+{
+	hscale_ = hfactor;
+	vscale_ = vfactor;
+}
+
+float ttexture::hscale() const
+{
+	return hscale_;
+}
+
+float ttexture::vscale() const
+{
+	return vscale_;
+}
+
+void ttexture::set_smooth_scaling(bool use_smooth)
+{
+	smooth_scaling_ = use_smooth;
+}
+
+bool ttexture::smooth_scaling() const
+{
+	return smooth_scaling_;
+}
+
+Uint16 ttexture::width() const
+{
+	return image_->w;
+}
+
+Uint16 ttexture::height() const
+{
+	return image_->h;
+}
+
+void ttexture::set_clip(const SDL_Rect &rect)
+{
+	clip_.x = rect.x;
+	clip_.y = rect.y;
+	clip_.w = rect.w;
+	clip_.h = rect.h;
+}
+
+SDL_Rect ttexture::clip() const
+{
+	SDL_Rect result;
+	result.x = clip_.x;
+	result.y = clip_.y;
+	result.w = clip_.w;
+	result.h = clip_.h;
+
+	return result;
+}
+
+void ttexture::set_alpha(Uint8 alpha)
+{
+	color_mod_.unused = alpha;
+}
+
+Uint8 ttexture::alpha() const
+{
+	return color_mod_.unused;
+}
+
+void ttexture::set_color_mod(Uint8 r, Uint8 g, Uint8 b)
+{
+	color_mod_.r = r;
+	color_mod_.g = g;
+	color_mod_.b = b;
+}
+
+Uint8 ttexture::red_mod() const
+{
+	return color_mod_.r;
+}
+
+Uint8 ttexture::green_mod() const
+{
+	return color_mod_.g;
+}
+
+Uint8 ttexture::blue_mod() const
+{
+	return color_mod_.b;
+}
+
+bool ttexture::null() const
+{
+	return image_ == NULL;
+}
+
+}
+
+#endif
--- src/sdl/texture.hpp
+++ src/sdl/texture.hpp
@@ -365,4 +365,93 @@ private:
 
 #endif
 
+#ifdef SDL_GPU
+#include "gpu.hpp"
+#include <string>
+
+struct surface;
+
+namespace sdl
+{
+
+class ttexture
+{
+public:
+	ttexture(Uint16 w, Uint16 h);
+
+	ttexture(const std::string &file);
+
+	ttexture( SDL_Surface *source);
+
+	ttexture(const surface &source);
+
+	ttexture();
+
+	~ttexture();
+
+	ttexture(const ttexture &texture);
+
+	ttexture &operator=(const ttexture &texture);
+
+	void draw(GPU_Target &target, const int x, const int y);
+
+	void set_rotation(float rotation);
+
+	float rotation() const;
+
+	void set_hscale(float factor);
+
+	void set_vscale(float factor);
+
+	void set_scale(float hfactor, float vfactor);
+
+	float hscale() const;
+
+	float vscale() const;
+
+	void set_smooth_scaling(bool use_smooth);
+
+	bool smooth_scaling() const;
+
+	Uint16 width() const;
+
+	Uint16 height() const;
+
+	void set_clip(const SDL_Rect &rect);
+
+	SDL_Rect clip() const;
+
+	void set_alpha(Uint8 alpha);
+
+	Uint8 alpha() const;
+
+	void set_color_mod(Uint8 r, Uint8 g, Uint8 b);
+
+	Uint8 red_mod() const;
+
+	Uint8 green_mod() const;
+
+	Uint8 blue_mod() const;
+
+
+	bool null() const;
+
+private:
+	GPU_Image *image_;
+
+	float rotation_;
+
+	float hscale_;
+
+	float vscale_;
+
+	bool smooth_scaling_;
+
+	GPU_Rect clip_;
+
+	SDL_Color color_mod_;
+};
+}
+#endif
+
 #endif
