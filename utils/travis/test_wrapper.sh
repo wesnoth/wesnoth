56dc1c2307d18bd380a64879825d79533817ac42
Chris Beck <render787@gmail.com>
2014-07-05 09:22:23 -0400

    fixup travis segfault handling in test executable

--- utils/travis/test_wrapper.sh
+++ utils/travis/test_wrapper.sh
@@ -3,9 +3,10 @@ COUNTER=10
 ./test
 ERRORCODE=$?
 while [ $COUNTER -gt 0 -a $ERRORCODE -eq 200 ]; do
-    echo "test apparently timed out with error code 200... trying again."
+    echo "test gave error code 200 (segfault).. trying again."
     COUNTER=$((COUNTER-1))
     ./test
     ERRORCODE=$?
 done
+export TEST_ERROR_CODE=$ERRORCODE
 exit $ERRORCODE
