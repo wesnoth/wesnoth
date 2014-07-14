ab74410e6166b0325e009e0dbd0fe15a84a11ec8
Chris Beck <render787@gmail.com>
2014-07-14 12:05:45 -0400

    fix wesnoth icon not appearing with dbus notifications

--- src/desktop/dbus_notification.cpp
+++ src/desktop/dbus_notification.cpp
@@ -144,7 +144,10 @@ uint32_t send_dbus_notification(DBusConnection *connection, uint32_t replaces_id
 			DBUS_TYPE_STRING, &event_id,
 			DBUS_TYPE_INVALID);
 	}
-	std::string app_icon_ = game_config::path + "/images/wesnoth-icon-small.png";
+
+	std::string app_icon_ = game_config::path + "images/game-icon.png";
+	LOG_DU << "app_icon_=\'" << app_icon_ << "\'\n";
+
 	const char *app_icon = app_icon_.c_str();
 	const char *summary = owner.c_str();
 	const char *body = message.c_str();
