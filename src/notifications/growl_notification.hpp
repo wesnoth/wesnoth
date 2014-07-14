19a90b43c69ff06c968aedddff55cd2860887585
Chris Beck <render787@gmail.com>
2014-07-13 22:19:27 -0400

    move growl notifications to their own file
    TODO: in the Xcode projectfile, add -DHAVE_GROWL flags to the
    targets
    
    - notifications.cpp
    - growl_notification.cpp
    
    Otherwise, when the project compiles growl notifications will not
    be enabled.

---
+++ src/notifications/growl_notification.hpp
@@ -0,0 +1,24 @@
+/*
+   Copyright (C) 2014 by Chris Beck <render787@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#ifndef GROWL_NOTIFICATION_HPP_
+#define GROWL_NOTIFICATION_HPP_
+
+#include <string>
+
+namespace growl {
+	void send_notification(const std::string& owner, const std::string& message, const std::string & note_name);
+}
+
+#endif
--- src/notifications/notifications.cpp
+++ src/notifications/notifications.cpp
@@ -31,9 +31,7 @@
 #endif
 
 #ifdef HAVE_GROWL
-#include <Growl/GrowlApplicationBridge-Carbon.h>
-#include <Carbon/Carbon.h>
-Growl_Delegate growl_obj;
+#include "growl_notification.hpp"
 #endif
 
 #ifdef _WIN32
@@ -81,25 +79,7 @@ void send_notification(const std::string& owner, const std::string& message, typ
 			break;
 	}
 
-	CFStringRef app_name = CFStringCreateWithCString(NULL, "Wesnoth", kCFStringEncodingUTF8);
-	CFStringRef cf_owner = CFStringCreateWithCString(NULL, owner.c_str(), kCFStringEncodingUTF8);
-	CFStringRef cf_message = CFStringCreateWithCString(NULL, message.c_str(), kCFStringEncodingUTF8);
-	CFStringRef cf_note_name = CFStringCreateWithCString(NULL, note_name.c_str(), kCFStringEncodingUTF8);
-
-	growl_obj.applicationName = app_name;
-	growl_obj.registrationDictionary = NULL;
-	growl_obj.applicationIconData = NULL;
-	growl_obj.growlIsReady = NULL;
-	growl_obj.growlNotificationWasClicked = NULL;
-	growl_obj.growlNotificationTimedOut = NULL;
-
-	Growl_SetDelegate(&growl_obj);
-	Growl_NotifyWithTitleDescriptionNameIconPriorityStickyClickContext(cf_owner, cf_message, cf_note_name, NULL, NULL, NULL, NULL);
-
-	CFRelease(app_name);
-	CFRelease(cf_owner);
-	CFRelease(cf_message);
-	CFRelease(cf_note_name);
+	growl::send_notification(owner, message, note_name);
 #endif
 
 #ifdef _WIN32
