0371b2ce1974bbb678e2e027be20dbcd25c335ea
Boldizsár Lipka <lipkab@zoho.com>
2014-07-22 16:39:38 +0200

    Fix compilation.

--- src/text.cpp
+++ src/text.cpp
@@ -373,6 +373,8 @@ ttext& ttext::set_foreground_color(const Uint32 color)
 ttext &ttext::set_foreground_color(const SDL_Color color)
 {
 	set_foreground_color((color.r << 16) + (color.g << 8) + color.b);
+
+	return *this;
 }
 
 ttext& ttext::set_maximum_width(int width)
