8611975db984de6b911e33e06f02ff71b2919c56
gfgtdf <tischpapier@gmail.com>
2014-10-18 00:31:28 +0200

    disable utf8 <-> ansi conversion in save_index.cpp
    with the new filesystem_boost our filesystem code now expects utf8 on windows so this code is breaks non-asci names now.

--- src/save_index.cpp
+++ src/save_index.cpp
@@ -39,7 +39,7 @@ static lg::log_domain log_engine("engine");
 static lg::log_domain log_enginerefac("enginerefac");
 #define LOG_RG LOG_STREAM(info, log_enginerefac)
 
-#ifdef _WIN32
+#if 0
 	#ifdef INADDR_ANY
 		#undef INADDR_ANY
 	#endif
