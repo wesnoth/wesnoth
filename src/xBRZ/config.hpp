ea887c48cbd7ec2c4cc522fda59cb527d78eb3bd
Chris Beck <render787@gmail.com>
2014-10-31 13:11:07 -0400

    add remarks on licensing to xBRZ headers

--- src/xBRZ/config.hpp
+++ src/xBRZ/config.hpp
@@ -1,3 +1,25 @@
+/*
+   Copyright (C) 2014 by Chris Beck <render787@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+   See the COPYING file for more details.
+
+   This is a derivative work of the xBRZ component of the HqMAME project
+   by Zenju. The original Licensing statment follows, indented with //
+   The primary changes are, syntactic to make it compile with C99+Boost,
+   and to make it handle an alpha channel in the image in a manner proper
+   for SDL.
+
+   It is not possible to extend the MAME 'special exception' to all of
+   the Battle for Wesnoth project, however, the special exception is
+   granted for my derivative forms of this work.
+*/
+
 // ****************************************************************************
 // * This file is part of the HqMAME project. It is distributed under         *
 // * GNU General Public License: http://www.gnu.org/licenses/gpl.html         *
