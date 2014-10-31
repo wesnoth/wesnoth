c2d61b3595336292d63c4f4a8371edfc789daf0a
gfgtdf <f492343@rmqkr.net>
2014-10-31 22:35:45 +0100

    fix some copyright notices

--- src/server/proxy.hpp
+++ src/server/proxy.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2007 - 2014
+   Copyright (C) 2007 - 2014 by David White <dave@whitevine.net>
    Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
