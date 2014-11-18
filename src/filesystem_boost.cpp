115c2a7985ae998f44acd396f70635d40337e939
gfgtdf <f492343@rmqkr.net>
2014-11-18 04:34:52 +0100

    use generic_string()
    this fixes a bug which caused that _final.cfg wasn't included last
    becasue we compared a string with "/_final.cfg". By using
    generic_string() we make sure that the seperators are '/'.
    
    http://gna.org/bugs/?22967

--- src/filesystem_boost.cpp
+++ src/filesystem_boost.cpp
@@ -191,9 +191,9 @@ namespace filesystem {
 static void push_if_exists(std::vector<std::string> *vec, const path &file, bool full) {
 	if (vec != NULL) {
 		if (full)
-			vec->push_back(file.string());
+			vec->push_back(file.generic_string());
 		else
-			vec->push_back(file.filename().string());
+			vec->push_back(file.filename().generic_string());
 	}
 }
 
