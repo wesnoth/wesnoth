31966fcfb836569a53b876c412b30b98036b4811
Chris Beck <render787@gmail.com>
2014-11-20 13:24:56 -0500

    suppress uninitialized variables warnings in xBRZ for gcc <= 4.7

--- src/xBRZ/xbrz.cpp
+++ src/xBRZ/xbrz.cpp
@@ -44,6 +44,12 @@
 #include <boost/bind.hpp>
 #include <boost/function.hpp>
 
+#ifdef __GNUC__
+#if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ <= 7 )
+#pragma GCC diagnostic ignored "-Wuninitialized"               //Suppress uninitialized variables warnings on GCC <= 4.7.x
+#endif
+#endif
+
 namespace
 {
 template <uint32_t N> inline
