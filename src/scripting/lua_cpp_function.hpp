1897cfed9f896df25a30274867362535a1660dbc
Chris Beck <render787@gmail.com>
2014-11-24 19:04:56 -0500

    add comments: describe motivations and caveats of lua cpp function

--- src/scripting/lua_cpp_function.hpp
+++ src/scripting/lua_cpp_function.hpp
@@ -23,6 +23,103 @@
  * function and execute it. Thus effectively all that we have to provide
  * is a "value type" user data (full userdata, not light userdata) in lua
  * which wraps the boost::function type and implements a garbage collector.
+ *
+ *
+ * -- Why? --
+ *
+ * There are a few basic approaches to connect lua and C++, with various
+ * degrees of power. Lua, being a C library, has no concept of C++ specific
+ * types, even when compiled as C++. Lua has only two functions which
+ * introduce C functions to the scripting environment:
+ * lua_pushcfunction, and lua_pushcclosure. (The helper library only provides
+ * functions which use these.) These functions can only accept C-style function
+ * pointers of type int (lua_State*). Boost bind cannot be used to make a match
+ * to this type signature, nor can C++ method functions be used.
+ *
+ * In many cases C-style functions are sufficient, but if one ever wants to
+ * refer to an instance of a class or a member variable (which one does as most
+ * of our project is written in C++ object oriented style), it's not enough.
+ *
+ * The basic lua-provided approach to this is to pass objects as "userdata".
+ * Userdata comes in two flavors, "regular" and "light", for representing
+ * value and reference types respectively. Lightuserdata is meant to hold a
+ * pointer to an object -- full userdata is meant to hold an instance of an
+ * object which is either copied with memcpy or constructed using placement new.
+ * Proper memory management may require using lua's __gc metamethod to call a
+ * destructor. In the normal idiom, every class which is passed to lua this
+ * way should have C-function shim provided for each method which may be called
+ * by lua -- the object's "this" is retrieved from the userdata type on the
+ * stack, and used to call the appopriate method. A metatable is defined, which
+ * may be the same as the "lua module" placed in the global namespace which may 
+ * also provide access to a constructor.
+ *
+ * This approach is often quite good for small objects. Wesnoth uses full userdata
+ * to represent "rng" objects used in lua random map generation, to represent lua
+ * "private units", to represent vconfigs and translatable strings. Wesnoth uses
+ * lightuserdata for many AI related objects.
+ *
+ * However it is not ideal for "large" objects like the game engine itself or its
+ * helpers. In this case full translation to userdata is out of the question --
+ * in case of lightuserdata, it is problematic because the lua api is not actually
+ * trying to directly represent a wesnoth engine object (which doesn't exist!)
+ * the wesnoth table in lua instead has a medley of callbacks implemented variously
+ * by grabbing from the gamemap, the unit map, the game_config namespace, etc. etc.
+ * for instance even the wesnoth.game_config table is not backed up by any one object,
+ * instead its callbacks may variously alter game_config namespace or the current tod
+ * manager object etc. etc.
+ *
+ * Circa 2012, the solution was to implement every callback function in the wesnoth
+ * table simply as a C function, whcih grabs whatever engine features it needs from
+ * a collection of pointers with external linkage in resources.hpp. The pointers
+ * must be reset by the play controller object whenver it is created or destroyed...
+ * or reset (replay controller), and always in a particular order because eventually
+ * all of the objects themselves are also grabbing these pointers, leading to segfaults
+ * if they are initialized in the wrong order, or left with danging pointers...
+ * in short it was very messy. While the organization of everything as pure C functions
+ * allows us to flexibly organize the wesnoth table using subtables holding functions
+ * as we like, (which wouldn't be possible if it were based on a lightuserdata), the
+ * requirement to base it all on externally-linked pointer variables comes at great cost.
+ * Further it means that we can never hope to have a correct deep-copy constructor of the
+ * gamestate, meaning that features like "replay moves since my last turn" or "AI based
+ * on exploratory simulations" are much more difficult to produce if not impossible.
+ *
+ * The lua cpp functions code permits us to refactor this by (1) passing all the engine
+ * resources needed by the lua environment, to the "lua kernel" at construction time,
+ * which holds them as private variables (2) declaring all callbacks which need these
+ * as private member functions (3) using boost bind to bind them to the lua kernel and
+ * this code to push the result into the scripting environment, at construction time
+ * of the lua kernel. Now there is no longer any question about dangling pointers in lua,
+ * or issues about making deep copies, since a lua state may be copied and the pointers
+ * in the kernel rebound, and the pointers in lua all die when the kernel is destroyed.
+ *
+ * More generally, this code makes it easy to assemble "mixins" for lua using the member
+ * functions of several different classes, if this is desired.
+ *
+ * The implementation details are also extremely simple -- whereas there are many popular
+ * lua -> C++ binding utilities like LuaPlus, luabind, etc. many of these only truly
+ * support automatic generation of bindings for *all* of the methods of an object, rely
+ * on the userdata approach, and require substantial template metaprogramming which will
+ * turn up in any associated stacktraces. This technique used here essentially delegates
+ * all of the templating work to boost, and is implemented in only a few hundred lines.
+ *
+ *
+ * -- Caveats: --
+ *
+ * Essentially, we provide C++ versions of the lua library calls 'lua_pushcfunction',
+ * 'lua_setfuncs', 'lua_pushcclosure'.
+ *
+ * - They are "C++" versions in that they take boost::function<int (lua_State*)> rather
+ * than int(lua_State*).
+ * - While for lua, "lua_pushcfunction(L, f)" is essentially the same as
+ * "lua_pushcclosure(L, f, 0)", for the functions below that is not the case.
+ * lua_cpp::push_function generates a userdata "helper" object with a _call operator,
+ * not technically a function. lua_cpp::push_closure generates a true lua closure.
+ * Therefore push_closure is the most general and most compatible form -- push_function
+ * is slightly simpler and more efficient though.
+ * - Similarly lua_cpp::set_functions(L, l) differs from lua_cpp::set_functions(L,l,nups).
+ * - Closures created by lua_cpp::push_closure are not *exactly* the same as lua closures,
+ * in that the first upvalue is used by the implementation. A closure created with two
+ * upvalues will find them at upvalue indices 2 and 3, and should not touch upvalue 1.
  */
 
 #ifndef LUA_CPP_FUNCTION_HPP_INCLUDED
