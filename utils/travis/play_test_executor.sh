6bca3fa96db46908c5914b8315024344e784e882
Chris Beck <render787@gmail.com>
2014-12-02 20:12:08 -0500

    travis: as a test, try turning off the nogui option for play test

--- utils/travis/play_test_executor.sh
+++ utils/travis/play_test_executor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-gdb -q -batch -return-child-result -ex "run" -ex "thread apply all bt" -ex "quit" --args ./wesnoth -m --controller 1:ai --controller 2:ai --nogui
+gdb -q -batch -return-child-result -ex "run" -ex "thread apply all bt" -ex "quit" --args ./wesnoth -m --controller 1:ai --controller 2:ai
