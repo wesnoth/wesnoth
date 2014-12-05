fabdf38a9eaa90c2e584c3c657510af5e85c9031
gfgtdf <f492343@rmqkr.net>
2014-12-05 16:08:01 +0100

    don't use std::pair
    instead we use a custom struct. std::pair is bad becasue you never know
    what "first" or "second" mean.
    
    (cherry-picked from commit c4aacbe31f31799861e03f4b7f7b241bee64aa65)

--- src/server/simple_wml.hpp
+++ src/server/simple_wml.hpp
@@ -115,8 +115,12 @@ public:
 	node(document& doc, node* parent);
 	node(document& doc, node* parent, const char** str, int depth=0);
 	~node();
-
-	typedef std::pair<string_span, string_span> attribute;
+	struct attribute
+	{
+		attribute(const string_span& k, const string_span& v) : key(k), value(v) {}
+		string_span key;
+		string_span value;
+	};
 	typedef std::vector<node*> child_list;
 
 	const string_span& operator[](const char* key) const;
