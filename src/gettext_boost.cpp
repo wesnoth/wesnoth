9f798cb4f118869181297608c3b812c2118acab3
gfgtdf <f492343@rmqkr.net>
2014-12-08 04:03:08 +0100

    add debug info.

--- src/gettext_boost.cpp
+++ src/gettext_boost.cpp
@@ -41,7 +41,15 @@ namespace
 			generator_.use_ansi_encoding(false);
 			update_locale();
 		}
-		void update_locale() { current_locale_ = generator_.generate(current_language_); }
+		void update_locale() 
+		{ 
+			//current_language_ = "de_DE@euro";
+			LOG_G << "attemptng to generate locale by name '" << current_language_ << "'\n";
+			current_locale_ = generator_.generate(current_language_);
+			const boost::locale::info& info = std::use_facet< boost::locale::info >(current_locale_);
+			LOG_G << "updated locale to '" << current_language_ << "' locale is now '" << current_locale_.name() << "' ( "
+			          << "name='" << info.name() << "' country='"  << info.country() << "' language='"  << info.language() << "' encoding='"  << info.encoding() << "' variant='"  << info.variant() << "')\n";
+		}
 
 		std::set<std::string> loaded_paths_;
 		std::set<std::string> loaded_domains_;
@@ -106,6 +114,7 @@ void bind_textdomain(const char* domain, const char* direcory, const char* /*enc
 
 void set_default_textdomain(const char* domain)
 {
+	LOG_G << "set_default_textdomain: '" << domain << "'\n";
 	get_manager().generator_.set_default_messages_domain(domain);
 	get_manager().update_locale();
 }
