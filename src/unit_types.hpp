2aca986bcb79b64a0befcc9266b96b9396328a79
Chris Beck <render787@gmail.com>
2014-12-26 12:49:14 -0500

    move unit_type::alignment_description impl into .cpp file

--- src/unit_types.hpp
+++ src/unit_types.hpp
@@ -235,25 +235,9 @@ public:
 		(CHAOTIC, N_("chaotic"))
 		(LIMINAL, N_("liminal"))
 	)
-	MAKE_ENUM (ALIGNMENT_FEMALE_VARIATION,
-		(FEMALE_LAWFUL, N_("female^lawful"))
-		(FEMALE_NEUTRAL, N_("female^neutral"))
-		(FEMALE_CHAOTIC, N_("female^chaotic"))
-		(FEMALE_LIMINAL, N_("female^liminal"))
-	)
 
 	ALIGNMENT alignment() const { return alignment_; }
-	inline static std::string alignment_description(ALIGNMENT align, unit_race::GENDER gender = unit_race::MALE)
-	{
-		std::string str = std::string();
-		if (gender == unit_race::FEMALE) {
-			ALIGNMENT_FEMALE_VARIATION fem = static_cast<ALIGNMENT_FEMALE_VARIATION> (align);
-			str = lexical_cast<std::string>(fem);
-		} else {
-			str = lexical_cast<std::string>(align);
-		}
-		return translation::sgettext(str.c_str());
-	}
+	static std::string alignment_description(ALIGNMENT align, unit_race::GENDER gender = unit_race::MALE);
 
 	fixed_t alpha() const { return alpha_; }
 
@@ -390,7 +374,6 @@ private:
 };
 
 MAKE_ENUM_STREAM_OPS2(unit_type, ALIGNMENT)
-MAKE_ENUM_STREAM_OPS2(unit_type, ALIGNMENT_FEMALE_VARIATION)
 
 class unit_type_data
 	: private boost::noncopyable
