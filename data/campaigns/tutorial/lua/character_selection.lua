a50225922c17a6f150b533a87369309c9874a6eb
Charles Dang <exodia339@gmail.com>
2015-01-03 15:33:39 +1100

    T: converted character selection dialog to a custom GUI2 one

---
+++ data/campaigns/tutorial/lua/character_selection.lua
@@ -0,0 +1,103 @@
+-- #textdomain wesnoth-tutorial
+
+-- Allows the player to choose whether they want to play Konrad or Li’sar
+-- for the tutorial
+
+local helper = wesnoth.require "lua/helper.lua"
+local T = helper.set_wml_tag_metatable {}
+local wml_actions = wesnoth.wml_actions
+local _ = wesnoth.textdomain "wesnoth-tutorial"
+
+function wml_actions.select_character()
+	local character_selection_dialog = {
+		maximum_height = 250,
+		maximum_width = 400,
+		T.helptip { id="tooltip_large" }, -- mandatory field
+		T.tooltip { id="tooltip_large" }, -- mandatory field
+		T.grid {
+			T.row {
+				T.column {
+					grow_factor = 1,
+					border = "all",
+					border_size = 5,
+					horizontal_alignment = "left",
+					T.label {
+						definition = "title",
+						label = _"Select Character"
+					}
+				}
+			},
+			T.row {
+				T.column {
+					grow_factor = 1,
+					border = "all",
+					border_size = 5,
+					horizontal_alignment = "left",
+					T.label {
+						label = _"Who do you want to play?"
+					}
+				}
+			},
+			T.row {
+				T.column {
+					T.grid {
+						T.row {
+							T.column {
+								grow_factor = 1,
+								border = "all",
+								border_size = 5,
+								T.image {
+									label = "units/konrad-fighter.png"
+								}
+							},
+							T.column {
+								grow_factor = 1,
+								border = "all",
+								border_size = 5,
+								T.image {
+									label = "units/human-princess.png"
+								}
+							}
+						},
+						T.row {
+							T.column {
+								grow_factor = 1,
+								border = "all",
+								border_size = 5,
+								T.button {
+									label = _"Konrad",
+									return_value = 1
+								}
+							},
+							T.column {
+								grow_factor = 1,
+								border = "all",
+								border_size = 5,
+								T.button {
+									label = _"Li’sar",
+									return_value = 2
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	local character = wesnoth.show_dialog(character_selection_dialog)
+	local unit = wesnoth.get_variable("student_store")
+	local gender = "male"
+
+	if character == 2 then
+		gender = "female"
+
+		unit.type    = "Fighteress"
+		unit.name    = "Li’sar"
+		unit.profile = "portraits/lisar.png"
+	end
+
+	wesnoth.set_variable("gender", gender)
+	wesnoth.put_unit(unit)
+	wesnoth.redraw {}
+end
