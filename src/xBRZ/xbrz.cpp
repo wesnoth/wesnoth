3f40d2f6e7a90afbc73dca56a57e1bd1cfab265d
Charles Dang <exodia339@gmail.com>
2015-01-04 17:09:28 +1100

    Fixup d38a656f41d1 , use WIN32 instead of GNUWIN32

--- src/xBRZ/xbrz.cpp
+++ src/xBRZ/xbrz.cpp
@@ -44,7 +44,7 @@
 #include <boost/bind.hpp>
 #include <boost/function.hpp>
 
-#if defined(__GNUC__) && !defined(__clang__) && !defined(__GNUWIN32__) // We only want this for gcc, not clang or tdm-gcc
+#if defined(__GNUC__) && !defined(__clang__) && !defined(__WIN32__) // We only want this for gcc, not clang or tdm-gcc
 #if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ <= 7 )
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"               //Suppress uninitialized variables warnings on GCC <= 4.7.x
 #endif
