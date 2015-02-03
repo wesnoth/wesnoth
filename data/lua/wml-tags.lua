84a7f3fb31351c5524d1287ec895b513cab7edac
Charles Dang <exodia339@gmail.com>
2015-02-03 11:03:37 +1100

    Parse [terrain] config. This fixes bug #23236

--- data/lua/wml-tags.lua
+++ data/lua/wml-tags.lua
@@ -846,7 +846,7 @@ end
 
 function wml_actions.terrain(cfg)
 	local terrain = cfg.terrain or helper.wml_error("[terrain] missing required terrain= attribute")
-	cfg = helper.shallow_literal(cfg)
+	cfg = helper.parsed(cfg)
 	cfg.terrain = nil
 	for i, loc in ipairs(wesnoth.get_locations(cfg)) do
 		wesnoth.set_terrain(loc[1], loc[2], terrain, cfg.layer, cfg.replace_if_failed)
