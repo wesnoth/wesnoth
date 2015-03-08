b9bed4ff6cf5e1622c69838ddda57fa75b9596a7
gfgtdf <tischpapier@gmail.com>
2015-03-09 00:16:19 +0100

    fix play_controller::is_browsing()
    we now check linger_, init_side_done_ and gamestate_.gamedata_.phase() before accessing current_team().
    This is saver since during linger mode or during initilisation there is not really a 'current team'.

--- src/play_controller.cpp
+++ src/play_controller.cpp
@@ -985,12 +985,11 @@ hotkey::command_executor * play_controller::get_hotkey_command_executor() {
 
 bool play_controller::is_browsing() const
 {
+	if(linger_ || !init_side_done_ || this->gamestate_.gamedata_.phase() != game_data::PLAY) {
+		return false;
+	}
 	const team& t = current_team();
-	return !t.is_local_human()
-		|| !t.is_proxy_human()
-		|| linger_
-		|| !init_side_done_
-		|| this->gamestate_.gamedata_.phase() != game_data::PLAY;
+	return !t.is_local_human() || !t.is_proxy_human();
 }
 
 void play_controller::play_slice_catch()
