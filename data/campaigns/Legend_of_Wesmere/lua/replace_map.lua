8ca687f0e9da4e662d5330adb7e52c6f30ba119a
Charles Dang <exodia339@gmail.com>
2015-03-28 16:59:58 +1100

    LoW: fix bug #22073. Don't write 'usage' and 'border_size' in replace_map.

--- data/campaigns/Legend_of_Wesmere/lua/replace_map.lua
+++ data/campaigns/Legend_of_Wesmere/lua/replace_map.lua
@@ -8,12 +8,11 @@ function wesnoth.wml_actions.replace_map(cfg)
     end
     local x1,x2 = string.match(cfg.x, "(%d+)-(%d+)")
     local y1,y2 = string.match(cfg.y, "(%d+)-(%d+)")
-    local header,map = string.match(cfg.map_data, "(.-)\n\n(.*)")
-    local b = string.match(header, "border_size=(%d+)")
+    local map = cfg.map_data
     x1 = tonumber(x1)
     y1 = tonumber(y1)
-    x2 = x2 + 2 * b
-    y2 = y2 + 2 * b
+    x2 = x2 + 2
+    y2 = y2 + 2
     local t = {}
     local y = 1
     for row in string.gmatch(map, "[^\n]+") do
@@ -28,7 +27,6 @@ function wesnoth.wml_actions.replace_map(cfg)
       end
       y = y + 1
     end
-    local s = table.concat(t, '\n')
-    local new_map = string.format("border_size=%d\nusage=map\n\n%s", b, s)
+    local new_map = table.concat(t, '\n')
     replace_map { map = new_map, expand = true, shrink = true }
 end
