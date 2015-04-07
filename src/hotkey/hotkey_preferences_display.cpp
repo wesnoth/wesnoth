8b72d6c76489b39c35c7ec2ac36f6067d8969b72
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-04-07 19:07:42 -0300

    Force xgettext to add the Add Hotkey and Clear Hotkey button tooltips
    Fixes the first issue mentioned in <http://r.wesnoth.org/p580885>.
    
    This will be backported later for version 1.12.3.

--- src/hotkey/hotkey_preferences_display.cpp
+++ src/hotkey/hotkey_preferences_display.cpp
@@ -131,9 +131,9 @@ public:
 };
 
 const char* hotkey_preferences_dialog::add_button_text =
-		"Add additional binding to \"$hotkey_description|\"";
+		N_("Add additional binding to \"$hotkey_description|\"");
 const char* hotkey_preferences_dialog::clear_button_text =
-		"Clears \"$hotkey_description|'s\" bindings";
+		N_("Clears \"$hotkey_description|'s\" bindings");
 
 class hotkey_resetter : public gui::dialog_button_action
 {
