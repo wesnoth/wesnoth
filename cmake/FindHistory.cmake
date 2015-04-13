81f2cdaf413023e0b789b71adb55aca0247ac768
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-04-13 05:00:40 -0300

    cmake: Mark history options as advanced
    These are internal options only, not the ENABLE_HISTORY toggle.

--- cmake/FindHistory.cmake
+++ cmake/FindHistory.cmake
@@ -7,3 +7,5 @@ find_library(HISTORY_LIBRARY history)
 # handle the QUIETLY and REQUIRED arguments and set XXX_FOUND to TRUE if all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(HISTORY DEFAULT_MSG HISTORY_LIBRARY HISTORY_INCLUDE_DIR)
+
+mark_as_advanced(HISTORY_INCLUDE_DIR HISTORY_LIBRARY)
