04d7c5b5870469117e536683bae76d1a046d2cbe
aquileia <sk.aquileia@gmail.com>
2015-04-25 18:34:34 +0200

    Set OMP_WAIT_POLICY in cwesnoth.cmd

--- cwesnoth.cmd
+++ cwesnoth.cmd
@@ -7,6 +7,7 @@ rem   cwesnoth <command line>
 rem
 
 set SDL_STDIO_REDIRECT=0
+set OMP_WAIT_POLICY=PASSIVE
 @wesnoth --wconsole %*
 
 rem Drop this from environment for future runs on the same session.
