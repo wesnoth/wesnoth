8d2609ff4c59b9c4713d5b2e182eab463d4bd15f
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-04-25 04:31:41 -0400

    Add --wconsole for allocating a Windows console on startup
    This is only recognized by the game client at the moment. The
    implementation is a bit hacky but it seems to work. Thanks, SDLmain.
    
    The cwesnoth.cmd Windows batch file passes --wconsole to Wesnoth in the
    command line, along with any other arguments passed to it. This is
    intended to be *the* use case for --wconsole, since otherwise the fact
    that Windows won't allocate a console for us on process initialization
    (due to Wesnoth being compiled with the GUI application flag) causes
    issues with the Command Prompt shell -- namely, the shell returns
    before Wesnoth allocates its console, allowing shell input to happen
    concurrently with Wesnoth's output to console.
    
    cwesnoth.cmd should be used in shortcuts and given Wesnoth's install
    path as its initial working directory.

---
+++ src/desktop/windows_console.hpp
@@ -0,0 +1,27 @@
+/*
+   Copyright (C) 2014 - 2015 by Ignacio Riquelme Morelle <shadowm2006@gmail.com>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#ifndef DESKTOP_WIN32_CONSOLE_HPP_INCLUDED
+#define DESKTOP_WIN32_CONSOLE_HPP_INCLUDED
+
+namespace desktop {
+
+void enable_win32_console();
+
+void disable_win32_console();
+
+}
+
+
+#endif // DESKTOP_WIN32_CONSOLE_HPP_INCLUDED
