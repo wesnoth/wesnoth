13107f343720f2682d0f41065669293ee4f4871d
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-04-26 05:49:29 -0300

    cwesnoth: Document environment assignments

--- cwesnoth.cmd
+++ cwesnoth.cmd
@@ -6,9 +6,15 @@ rem Usage:
 rem   cwesnoth <command line>
 rem
 
+rem Disable stdout.txt/stderr.txt redirection in SDLmain.
 set SDL_STDIO_REDIRECT=0
-set OMP_WAIT_POLICY=PASSIVE
-@wesnoth --wconsole %*
 
-rem Drop this from environment for future runs on the same session.
+rem OpenMP builds need to set this variable on startup and relaunch Wesnoth in
+rem order to prevent constant busy waits. Do it here so the restart doesn't
+rem cause this script to return to the shell too early.
+set OMP_WAIT_POLICY=PASSIVE
+
+wesnoth --wconsole %*
+
+rem Drop our custom environment for future runs on the same session.
 set SDL_STDIO_REDIRECT=
