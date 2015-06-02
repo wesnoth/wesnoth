d7734d8b02bc6890893bb535b4fc06ff64fbbaea
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-06-02 19:13:07 -0400

    Explicitly define admin_authentication_reset's ctor out of line
    Silences a spurious unused variable warning emitted by gcc 4.5.2.

--- src/game_preferences.cpp
+++ src/game_preferences.cpp
@@ -194,6 +194,10 @@ void parse_admin_authentication(const std::string& sender, const std::string& me
 	}
 }
 
+admin_authentication_reset::admin_authentication_reset()
+{
+}
+
 admin_authentication_reset::~admin_authentication_reset()
 {
 	authenticated = false;
