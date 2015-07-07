bcfdc85568b308b48138ee5884595f272a0795c3
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-07-06 22:21:33 -0300

    Use prefix instead of postfix increment/decrement
    Found by cppcheck.

--- src/scripting/lua_cpp_function.cpp
+++ src/scripting/lua_cpp_function.cpp
@@ -117,7 +117,7 @@ void set_functions( lua_State* L, const lua_cpp::Reg * l, int nup )
 	luaL_checkstack(L, nup+1, "too many upvalues");
 	for (; l->name != NULL; l++) {  /* fill the table with given functions */
 		int i;
-		for (i = 0; i < nup; i++)  /* copy upvalues to the top */
+		for (i = 0; i < nup; ++i)  /* copy upvalues to the top */
 			lua_pushvalue(L, -nup);
 		push_closure(L, l->func, nup);  /* closure with those upvalues */
 		lua_setfield(L, -(nup + 2), l->name);
