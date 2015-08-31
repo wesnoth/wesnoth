a247fae572cfff5d1f3e769c5065e1dd4361e6ee
gfgtdf <f492343@rmqkr.net>
2015-08-31 22:59:08 +0200

    add comments

--- src/quit_confirmation.hpp
+++ src/quit_confirmation.hpp
@@ -1,12 +1,14 @@
 #pragma once
 class CVideo;
 
+/// Any object of this type will prevent the game from quitting immediately, instad a confirmation dialog will pop up when attepmting to close. 
 class quit_confirmation
 {
 public:
 	quit_confirmation() { ++count_; }
 	quit_confirmation(const quit_confirmation&) { ++count_; }
 	~quit_confirmation() { --count_; }
+	/// Shows the quit confirmation if needed, throws CVideo::quit to exit.
 	static void quit();
 	static void quit(CVideo& video );
 private:
