900c84657b9d003eece8d341ed223a0111f5fdf2
Elias Pschernig <elias@users.sourceforge.net>
2015-09-22 16:31:28 -0400

    Upgraded wesnoth_addon_manager to Python 3
    This includes campaignserver_client.py which is also used by wescamp.py
    - however wescamp.py is still Python 2 at this point and will need to be
      upgraded at a later point in time.

--- data/tools/addon_manager/html.py
+++ data/tools/addon_manager/html.py
@@ -56,7 +56,7 @@ Select the add-on you want to install from the list and click "OK". The download
         "asc.gif", "bg.gif", "desc.gif"]:
         Popen(["cp", "-u", am_dir + name, path])
 
-    campaigns = data.get_or_create_sub("campaigns")
+    campaigns = data.get_all(tag = "campaigns")[0]
     w("<table class=\"tablesorter\" id=\"campaigns\">")
     w("<thead>")
     w("<tr>")
@@ -67,9 +67,9 @@ Select the add-on you want to install from the list and click "OK". The download
     w("<tbody>")
     root_dir = am_dir + "../../../"
     images_to_tc = []
-    for campaign in campaigns.get_all("campaign"):
+    for campaign in campaigns.get_all(tag = "campaign"):
         v = campaign.get_text_val
-        translations = campaign.get_all("translation")
+        translations = campaign.get_all(tag = "translation")
         languages = [x.get_text_val("language") for x in translations]
         w("<tr>")
         icon = v("icon", "")
