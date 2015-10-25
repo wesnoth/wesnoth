982fecdc4ae217017d853f2efcde880e9ff64d5c
gfgtdf <f492343@rmqkr.net>
2015-10-25 22:13:25 +0100

    attempt to fix crash on invalid unit images
    https://gna.org/bugs/?23943

--- src/unit_drawer.cpp
+++ src/unit_drawer.cpp
@@ -251,8 +251,8 @@ void unit_drawer::redraw_unit (const unit & u) const
 	if(draw_bars) {
 		const image::locator* orb_img = NULL;
 		const surface unit_img = image::get_image(u.default_anim_image(), image::SCALED_TO_ZOOM);
-		const int xoff = (hex_size - unit_img->w)/2;
-		const int yoff = (hex_size - unit_img->h)/2;
+		const int xoff = unit_img ? (hex_size - unit_img->w)/2 : 0;
+		const int yoff = unit_img ? (hex_size - unit_img->h)/2 : 0;
 		/*static*/ const image::locator partmoved_orb(game_config::images::orb + "~RC(magenta>" +
 						preferences::partial_color() + ")"  );
 		/*static*/ const image::locator moved_orb(game_config::images::orb + "~RC(magenta>" +
