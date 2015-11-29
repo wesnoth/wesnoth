44179a2cbb50f3ba2a22b885aeedc4f36e607896
gfgtdf <f492343@rmqkr.net>
2015-11-29 17:18:00 +0100

    add lua getter for side save_id

--- src/scripting/lua_team.cpp
+++ src/scripting/lua_team.cpp
@@ -52,6 +52,7 @@ static int impl_side_get(lua_State *L)
 
 	// Find the corresponding attribute.
 	return_int_attrib("side", t.side());
+	return_string_attrib("save_id", t.save_id());
 	return_int_attrib("gold", t.gold());
 	return_tstring_attrib("objectives", t.objectives());
 	return_int_attrib("village_gold", t.village_gold());
