98863f41f30a92902ce98ffbf15681088ec5d2da
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-12-10 21:33:39 -0300

    Version 1.13.2

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -40,11 +40,11 @@
   #undef VERSION
 #endif
 
-#define VERSION "1.13.1+dev"
+#define VERSION "1.13.2"
 
 // Used for the Windows executables' version info resource.
 #define RC_VERSION_MAJOR        1
 #define RC_VERSION_MINOR        13
-#define RC_VERSION_REVISION     1
+#define RC_VERSION_REVISION     2
 
 #endif
