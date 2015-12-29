75080d8fdc5caf174583b76e4d98c872757cc890
Charles Dang <exodia339@gmail.com>
2015-12-29 15:37:07 +1100

    Allow prefs window SIZE_CHANGED to fall through to RESIZED

--- src/preferences.cpp
+++ src/preferences.cpp
@@ -102,13 +102,10 @@ void handle_event(const SDL_Event& event)
 	if (event.type != SDL_WINDOWEVENT) return;
 
 	switch(event.window.event) {
+	case SDL_WINDOWEVENT_SIZE_CHANGED: // Fall through to RESIZED
 	case SDL_WINDOWEVENT_RESIZED:
-		_set_resolution(std::make_pair(event.window.data1,event.window.data2));
-
-		break;
-
-	case SDL_WINDOWEVENT_SIZE_CHANGED:
 		_set_maximized(false);
+		_set_resolution(std::make_pair(event.window.data1,event.window.data2));
 
 		break;
 
