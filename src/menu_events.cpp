ca8690876adbfb6cb92a57fafc2c099801dab4c6
gfgtdf <f492343@rmqkr.net>
2015-12-30 19:01:28 +0100

    fix :controller command

--- src/menu_events.cpp
+++ src/menu_events.cpp
@@ -2694,7 +2694,7 @@ void console_handler::do_controller()
 	if (!menu_handler_.teams()[side_num - 1].is_proxy_human()) {
 		report += " (" + menu_handler_.teams()[side_num - 1].proxy_controller().to_string() + ")";
 	}
-	if (!menu_handler_.teams()[side_num - 1].is_network()) {
+	if (menu_handler_.teams()[side_num - 1].is_network()) {
 		report += " (networked)";
 	}
 
