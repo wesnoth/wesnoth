5f67eee678e082b75aa67b83952572db8a096827
Charles Dang <exodia339@gmail.com>
2016-01-30 21:28:45 +1100

    ttree_view_node: bind label selection event to toggle if label is not present

--- src/gui/widgets/tree_view_node.cpp
+++ src/gui/widgets/tree_view_node.cpp
@@ -84,6 +84,11 @@ ttree_view_node::ttree_view_node(
 						&grid_, "tree_view_node_label", false, false);
 
 				label_ = dynamic_cast<tselectable_*>(widget);
+
+				if(!label_) {
+					label_ = dynamic_cast<tselectable_*>(toggle_widget);
+				}
+
 				if(label_) {
 					widget->connect_signal<event::LEFT_BUTTON_CLICK>(
 							boost::bind(
