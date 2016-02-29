ca8cb3bd6a45d4fd2da9de79d484d9ad16f6951d
Celtic Minstrel <celtic.minstrel.ca@some.place>
2016-02-29 18:38:46 -0500

    Remove the deprecated recruitment stage, candidate action, and aspect

--- src/ai/composite/stage.cpp
+++ src/ai/composite/stage.cpp
@@ -109,43 +109,6 @@ bool idle_stage::do_play_stage(){
 	return false;
 }
 
-// =======================================================================
-// COMPOSITE AI MINISTAGE
-// =======================================================================
-
-ministage::ministage(const config &cfg)
-	: cfg_(cfg),stage_()
-{
-}
-
-ministage::~ministage()
-{
-}
-
-
-stage_ptr ministage::get_stage_ptr(ai_context &context)
-{
-	if (stage_) {
-		return stage_;
-	}
-
-	std::vector<stage_ptr> stages;
-	engine::parse_stage_from_config(context,cfg_,std::back_inserter(stages));
-	if (stages.empty()) {
-		return stage_ptr();
-	}
-	stage_ = stages.front();
-	return stage_;
-}
-
-config ministage::to_config() const
-{
-	if (!stage_) {
-		return cfg_;
-	}
-	return stage_->to_config();
-}
-
 
 
 } //end of namespace ai
