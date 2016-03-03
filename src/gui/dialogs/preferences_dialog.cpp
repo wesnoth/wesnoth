wesnoth/wesnoth

commit 2b6566682386cb4e30d474765e8b5d3f8b26cab3

tpreferences: ensure all descriptions are shown

src/gui/dialogs/preferences_dialog.cpp
@@ -851,7 +851,9 @@ void tpreferences::on_advanced_prefs_list_select(tlistbox& list, twindow& window
 		// Add more options here as needed
 	}
 
-	if(selected_type != ADVANCED_PREF_TYPE::TOGGLE && selected_type != ADVANCED_PREF_TYPE::SPECIAL) {
+	const bool has_description = !adv_preferences_cfg_[selected_row]["description"].empty();
+
+	if(has_description && selected_type != ADVANCED_PREF_TYPE::SPECIAL) {
 		find_widget<twidget>(get_advanced_row_grid(list, selected_row), "prefs_setter_grid", false)
 			.set_visible(tcontrol::tvisible::visible);
 	}

commit 1e68fcdb0a17cb914550cef5c1dc7622dbdc257b

tpreferences: fixed regression where certain advanced options weren't opening
This was accidentally broken in 2b6566682386cb4e30d474765e8b5d3f8b26cab3

src/gui/dialogs/preferences_dialog.cpp
@@ -1046,7 +1046,7 @@ void tpreferences::on_advanced_prefs_list_select(tlistbox& list, twindow& window
 
 	const bool has_description = !adv_preferences_cfg_[selected_row]["description"].empty();
 
-	if(has_description && selected_type != ADVANCED_PREF_TYPE::SPECIAL) {
+	if(has_description || (selected_type != ADVANCED_PREF_TYPE::SPECIAL && selected_type != ADVANCED_PREF_TYPE::TOGGLE)) {
 		find_widget<twidget>(get_advanced_row_grid(list, selected_row), "prefs_setter_grid", false)
 			.set_visible(tcontrol::tvisible::visible);
 	}