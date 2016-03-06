653a62bc03695f92caa13ceb515be64f426e744c
loonycyborg <loonycyborg@gmail.com>
2016-03-06 16:19:10 +0300

    Fixed incorrect version strings

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -40,7 +40,7 @@
   #undef VERSION
 #endif
 
-#define VERSION "1.13.4+svn"
+#define VERSION "1.13.4+dev"
 
 // Used for the Windows executables' version info resource.
 #define RC_VERSION_MAJOR        1
