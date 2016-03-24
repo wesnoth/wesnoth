961d134ee24ff0b07cd8073816a485c6237f9552
mattsc <mattsc000@gmail.com>
2016-03-24 08:07:01 -0700

    Fix and simplify Lua AI test scenario

--- data/ai/lua/extCAexample.lua
+++ data/ai/lua/extCAexample.lua
@@ -2,15 +2,20 @@
 
 example_ca = {}
 
-function example_ca:eval(ai)
-	wesnoth.message("External eval says hi!")
-	
-	return 10000
+function example_ca:evaluation(ai)
+    wesnoth.message("External CA evaluation says hi.")
+
+    return 10000
 end
 
-function example_ca:exec(ai)
-	wesnoth.message("External CA exec attacks!")
-	ai.attack(2, 12, 3, 12, 1, 1) -- showcasing the presence of the AI table
+function example_ca:execution(ai)
+    wesnoth.message("External CA execution attacks.")
+
+    -- Note that there is no check whether these attacks are possible.
+    -- The CA will therefore be blacklisted the second time it gets called.
+    ai.attack(2, 12, 3, 12, 1, 1)
+    ai.attack(3, 13, 3, 12, 2, 1)
+    ai.attack(3, 11, 3, 12)
 end
 
 return example_ca
