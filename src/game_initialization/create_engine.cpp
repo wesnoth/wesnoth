wesnoth/wesnoth

commit 876710af8c24191ded7d9b82e5ee667aca216832

Use std::to_string() or equivalent wherever possible
This commit removes the utility srt_cast() function and replaces its calls,
along with calls to lexical_cast<std::string>() (and its boost variant),
with std::to_string().

In a few cases where the input type isn't compatible with to_string,
lexical_cast<std::string> is still used.

In other cases where lexical_cast was operating on MAKE_ENUM enums, the
call has been replaced with ENUM::enum_to_string, which is faster.

src/game_initialization/create_engine.cpp
@@ -209,7 +209,7 @@ void scenario::set_sides()
 			pos < map_positions; ++pos) {
 			config& side = data_.add_child("side");
 			side["side"] = pos + 1;
-			side["team_name"] = "Team " + lexical_cast<std::string>(pos + 1);
+			side["team_name"] = "Team " + std::to_string(pos + 1);
 			side["canrecruit"] = true;
 			side["controller"] = "human";
 		}
@@ -1059,7 +1059,7 @@ void create_engine::init_all_levels()
 	// Stand-alone scenarios.
 	BOOST_FOREACH(const config &data,
 		game_config_manager::get()->game_config().child_range(
-		lexical_cast<std::string> (game_classification::CAMPAIGN_TYPE::MULTIPLAYER)))
+		std::to_string (game_classification::CAMPAIGN_TYPE::MULTIPLAYER)))
 	{
 		if (!data["allow_new_game"].to_bool(true))
 			continue;

commit 0936066bf3e670afad845cc0b9c96cd122505d0f

Fixed initializing of mp scenarios
Was accidentally broken in 876710af8c2.

src/game_initialization/create_engine.cpp
@@ -1058,8 +1058,7 @@ void create_engine::init_all_levels()
 
 	// Stand-alone scenarios.
 	BOOST_FOREACH(const config &data,
-		game_config_manager::get()->game_config().child_range(
-		std::to_string (game_classification::CAMPAIGN_TYPE::MULTIPLAYER)))
+		game_config_manager::get()->game_config().child_range("multiplayer"))
 	{
 		if (!data["allow_new_game"].to_bool(true))
 			continue;