9bd44f3e8c2f7db35ddf884bed07dae82b255a88
ScegfOd <ScegfOd@users.noreply.github.com>
2016-04-10 21:36:15 -0700

    Update logging.hpp
    boilerplate update

--- src/gui/dialogs/logging.hpp
+++ src/gui/dialogs/logging.hpp
@@ -1,16 +1,15 @@
 /*
-   Copyright (C) 2016 - 2016 by Jonathan Combs <JofCombs@gmail.com>
-   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+ Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY.
 
-   See the COPYING file for more details.
-*/
+ See the COPYING file for more details.
+ */
 
 #ifndef GUI_LOGGING_HPP_INCLUDED
 #define GUI_LOGGING_HPP_INCLUDED
