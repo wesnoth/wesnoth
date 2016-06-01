73d243072b4760e129b3604fd568c4d3c5cc4a6a
gfgtdf <daniel.gfgtdf@gmail.com>
2016-06-01 18:29:46 +0200

    fix custom faction not available for remote players if it was changed by the host.
    or a previous player of that side.

--- src/tests/test_mp_connect.cpp
+++ src/tests/test_mp_connect.cpp
@@ -112,6 +112,7 @@ static ng::side_engine* create_side_engine(const config& defaults,
 {
 	config side_cfg = connect_engine->current_config()->child("side");
 	side_cfg.remove_attributes("faction");
+	side_cfg.clear_children("default_faction");
 	side_cfg.append(defaults);
 
 	return new ng::side_engine(side_cfg, *connect_engine, 0);
