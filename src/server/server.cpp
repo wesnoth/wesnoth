54c03ea6cb1364e41adf360a34b446bbf019468a
Alexander van Gessel <ai0867@gmail.com>
2016-06-10 20:00:03 +0200

    Don't store pointers to internal state of temporaries
    Found by coverity

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -449,10 +449,10 @@ void server::handle_termination(const boost::system::error_code& error, int sign
 {
 	assert(!error);
 
-	const char* signame;
+	std::string signame;
 	if(signal_number == SIGINT) signame = "SIGINT";
 	else if(signal_number == SIGTERM) signame = "SIGTERM";
-	else signame = lexical_cast<std::string>(signal_number).c_str();
+	else signame = lexical_cast<std::string>(signal_number);
 	LOG_SERVER << signame << " caught, exiting without cleanup immediately.\n";
 	exit(128 + signal_number);
 }
