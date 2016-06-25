dfe2f331a8fa6ec6a981e0aee58b0889515f6ddc
loonycyborg <loonycyborg@gmail.com>
2016-06-25 16:52:29 +0300

    Fixed the direction of horizontal scrolling with mousewheel

--- src/mouse_handler_base.cpp
+++ src/mouse_handler_base.cpp
@@ -268,9 +268,9 @@ void mouse_handler_base::mouse_wheel(int scrollx, int scrolly, bool browse)
 		CKey pressed;
 		// Alt + mousewheel do an 90° rotation on the scroll direction
 		if (pressed[SDLK_LALT] || pressed[SDLK_RALT]) {
-			gui().scroll(movey,movex);
+			gui().scroll(-movey,-movex);
 		} else {
-			gui().scroll(movex,-movey);
+			gui().scroll(-movex,-movey);
 		}
 	}
 
