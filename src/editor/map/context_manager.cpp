wesnoth/wesnoth

commit 6235e18bbd12751631df0d6d457b8a358354e183

Replace resources::teams with resources::gameboard->teams()

src/editor/map/context_manager.cpp
@@ -21,6 +21,7 @@
 #include "filesystem.hpp"
 #include "filechooser.hpp"
 #include "formula/string_utils.hpp"
+#include "game_board.hpp"
 #include "gettext.hpp"
 #include "generators/map_create.hpp"
 #include "generators/map_generator.hpp"
@@ -76,7 +77,7 @@ public:
 		context_manager_.gui().change_display_context(&context_manager_.get_map_context());
 
 		resources::units = &context_manager_.get_map_context().get_units();
-		resources::teams = &context_manager_.get_map_context().get_teams();
+		resources::gameboard->teams() = context_manager_.get_map_context().get_teams();
 
 		// TODO register the tod_manager with the gui?
 		resources::tod_manager = context_manager_.get_map_context().get_time_manager();

commit 312a77c075e7abedb7b5e358827f10f7e7501b28

Fixed crash on entering the editor (bug #24964)
This was introduced in 6235e18bbd12.

src/editor/map/context_manager.cpp
@@ -77,7 +77,10 @@ public:
 		context_manager_.gui().change_display_context(&context_manager_.get_map_context());
 
 		resources::units = &context_manager_.get_map_context().get_units();
-		resources::gameboard->teams() = context_manager_.get_map_context().get_teams();
+
+		if(resources::gameboard) {
+			resources::gameboard->teams() = context_manager_.get_map_context().get_teams();
+		}
 
 		// TODO register the tod_manager with the gui?
 		resources::tod_manager = context_manager_.get_map_context().get_time_manager();