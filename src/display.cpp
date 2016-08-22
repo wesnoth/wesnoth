wesnoth/wesnoth

commit 8816dc9cd9e84d15d5cecdae752dbbd880aa24b8

Fix icons of buttons under the minimap disappearing on closing a menu

src/display.cpp
@@ -24,6 +24,7 @@
 #include "game_preferences.hpp"
 #include "gettext.hpp"
 #include "halo.hpp"
+#include "hotkey/command_executor.hpp"
 #include "language.hpp"
 #include "log.hpp"
 #include "marked-up_text.hpp"
@@ -2672,6 +2673,15 @@ void display::redraw_everything()
 
 	if(!menu_buttons_.empty() || !action_buttons_.empty() || !sliders_.empty() ) {
 		create_buttons();
+	}
+
+	hotkey::command_executor* command_executor =
+		resources::controller->get_hotkey_command_executor();
+	if (command_executor != nullptr)
+	{
+		// This function adds button overlays,
+		// it needs to be run after recreating the buttons.
+		command_executor->set_button_state();
 	}
 
 	panelsDrawn_ = false;

src/controller_base.hpp
@@ -70,6 +70,11 @@ public:
 	void set_scroll_down(bool on);
 	void set_scroll_left(bool on);
 	void set_scroll_right(bool on);
+
+	/**
+	* Get (optionally) a command executor to handle context menu events
+	*/
+	virtual hotkey::command_executor * get_hotkey_command_executor() { return nullptr; }
 protected:
 	virtual bool is_browsing() const
 	{ return false; }
@@ -91,11 +96,6 @@ protected:
 	 * Get (optionally) a plugins context a derived class uses
 	 */
 	virtual plugins_context * get_plugins_context() { return nullptr; }
-
-	/**
-	 * Get (optionally) a command executor to handle context menu events
-	 */
-	virtual hotkey::command_executor * get_hotkey_command_executor() { return nullptr; }
 
 	/**
 	 * Derived classes should override this to return false when arrow keys

commit 0a0caf9104f98a9afa7a25ff2556c2511466ebce

Fix crash when starting editor introduced in 8816dc9cd9e8

src/display.cpp
@@ -2675,13 +2675,13 @@ void display::redraw_everything()
 		create_buttons();
 	}
 
-	hotkey::command_executor* command_executor =
-		resources::controller->get_hotkey_command_executor();
-	if (command_executor != nullptr)
-	{
-		// This function adds button overlays,
-		// it needs to be run after recreating the buttons.
-		command_executor->set_button_state();
+	if(resources::controller) {
+		hotkey::command_executor* command_executor = resources::controller->get_hotkey_command_executor();
+		if(command_executor != nullptr)	{
+			// This function adds button overlays,
+			// it needs to be run after recreating the buttons.
+			command_executor->set_button_state();
+		}
 	}
 
 	panelsDrawn_ = false;