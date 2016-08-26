793b8491ade1ebe1d1b2b1f3bf0b595c4bda465c
Charles Dang <exodia339@gmail.com>
2016-08-27 01:14:49 +1100

    Added GUI2-compatible versions of mp tests and plugin scripts

---
+++ utils/travis/mp_test_executor-gui2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e #Error if any line errors
+set -m #Enable job control
+set -v #Print shell commands as they are read
+
+./wesnothd --port 12345 --log-debug=server --log-warning=config &
+serverpid=$!
+
+./wesnoth --plugin=host-gui2.lua --server=localhost:12345 --username=host --mp-test --noaddons &
+hostpid=$!
+
+./wesnoth --plugin=join-gui2.lua --server=localhost:12345 --username=join --mp-test --noaddons &
+joinpid=$!
+
+wait $hostpid
+
+wait $joinpid
+
+kill $serverpid
+
+exit 0
