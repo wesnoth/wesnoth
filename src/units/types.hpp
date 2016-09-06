206096cc1e0d732c31387b2d77c795773b0a2a00
Celtic Minstrel <celtic.minstrel.ca@some.place>
2016-09-06 12:43:54 -0400

    Fix ignored qualifiers warning

--- src/units/types.hpp
+++ src/units/types.hpp
@@ -198,7 +198,7 @@ public:
 	/// The returned vector will not be empty, provided this has been built
 	/// to the HELP_INDEXED status.
 	const std::vector<unit_race::GENDER>& genders() const { return genders_; }
-	const bool has_gender_variation(unit_race::GENDER gender) const
+	bool has_gender_variation(unit_race::GENDER gender) const
 	{
 		return std::find(genders_.begin(), genders_.end(), gender) != genders_.end();
 	}
