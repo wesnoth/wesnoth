wesnoth/wesnoth

commit 2483fc80e4a57f7cdc0ff9fbeaed08d85699d0d6

scons: Don't pass a C++ compiler flag to C compilers
Needed since 0180a72573a482683485821c8d43a82a546a86c6 introduces the
requirement of a C compiler in the building process. Otherwise we get
this from GCC 6.1.1:

  cc1: error: command line option ‘-Wold-style-cast’ is valid for C++/ObjC++ but not for C [-Werror]

SConstruct
@@ -512,7 +512,8 @@ for env in [test_env, campaignd_env, client_env, env]:
             env.AppendUnique(CXXFLAGS = ["-fopenmp"], LIBS = ["gomp"])
 
         if env['strict']:
-            env.AppendUnique(CCFLAGS = Split("-Werror -Wold-style-cast $(-Wno-unused-local-typedefs$)"))
+            env.AppendUnique(CCFLAGS = Split("-Werror $(-Wno-unsused-local-typedefs$)"))
+            env.AppendUnique(CXXFLAGS = Split("-Wold-style-cast"))
         if env['sanitize']:
             env.AppendUnique(CCFLAGS = ["-fsanitize=" + env["sanitize"]], LINKFLAGS = ["-fsanitize=" + env["sanitize"]])
 

commit 3a925c85e3e262c8a1036fcdcf703302d32cda05

scons: Fix compiler switch typo
Accidentally introduced in 2483fc80e4a57f7cdc0ff9fbeaed08d85699d0d6,
oops. Didn't become evident unless hitting other compiler errors,
though.

SConstruct
@@ -512,7 +512,7 @@ for env in [test_env, campaignd_env, client_env, env]:
             env.AppendUnique(CXXFLAGS = ["-fopenmp"], LIBS = ["gomp"])
 
         if env['strict']:
-            env.AppendUnique(CCFLAGS = Split("-Werror $(-Wno-unsused-local-typedefs$)"))
+            env.AppendUnique(CCFLAGS = Split("-Werror $(-Wno-unused-local-typedefs$)"))
             env.AppendUnique(CXXFLAGS = Split("-Wold-style-cast"))
         if env['sanitize']:
             env.AppendUnique(CCFLAGS = ["-fsanitize=" + env["sanitize"]], LINKFLAGS = ["-fsanitize=" + env["sanitize"]])