c4992dcd3819d5966b2d79a2e64c00c17ba64531
Jyrki Vesterinen <sandgtx@gmail.com>
2016-09-13 21:18:50 +0300

    viesti.txt

--- utils/travis/mp_test_executor-gui2.sh
+++ utils/travis/mp_test_executor-gui2.sh
@@ -6,10 +6,10 @@ set -v #Print shell commands as they are read
 ./wesnothd --port 12345 --log-debug=server --log-warning=config &
 serverpid=$!
 
-./wesnoth --plugin=host-gui2.lua --server=localhost:12345 --username=host --mp-test --noaddons &
+./wesnoth --plugin=host-gui2.lua --server=localhost:12345 --username=host --mp-test --noaddons --nogui &
 hostpid=$!
 
-./wesnoth --plugin=join-gui2.lua --server=localhost:12345 --username=join --mp-test --noaddons &
+./wesnoth --plugin=join-gui2.lua --server=localhost:12345 --username=join --mp-test --noaddons --nogui &
 joinpid=$!
 
 wait $hostpid
