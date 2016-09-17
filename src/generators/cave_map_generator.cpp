60d114b606b305099c690a8c9d6cc0eaff9a905c
gfgtdf <daniel.gfgtdf@gmail.com>
2016-09-17 02:48:21 +0200

    finish moving cavegen (cave_map_generator.cpp)  to lua.
    This map generator is used in HttT 17 and SoF 4. which needed to be updated aswell.
    
    With this we finally have a lua generator that is used in mainline and that
    can be used an an example by wml authors who want to write other map generators.
    
    Unfortunateley there are still umc add ons in 1.12 that use
    scenario_generation=cave so i didn't remove the cpp implementation yet.

--- src/generators/cave_map_generator.cpp
+++ src/generators/cave_map_generator.cpp
@@ -20,6 +20,7 @@
 #include "global.hpp"
 
 #include "cave_map_generator.hpp"
+#include "config_assign.hpp"
 #include "log.hpp"
 #include "map/map.hpp"
 #include "pathfind/pathfind.hpp"
@@ -96,6 +97,12 @@ cave_map_generator::cave_map_generator_job::cave_map_generator_job(const cave_ma
 	, res_(params.cfg_.child_or_empty("settings"))
 	, rng_() //initialises with rand()
 {
+	res_.add_child("event", config_of
+		("name", "start")
+		("message", config_of
+			("message", "scenario_generation=cave is deprecated and will be removed soon.")
+		)
+	);
 	uint32_t seed = randomseed.get_ptr() ? *randomseed.get_ptr() : seed_rng::next_seed();
 	rng_.seed(seed);
 	LOG_NG << "creating random cave with seed: " << seed << '\n';
