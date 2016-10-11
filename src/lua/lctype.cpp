d32ab00af96d4da3965fc9ff7c9ee419e510c426
Gregory A Lundberg <gregory_lundberg@gmx.com>
2016-10-11 17:44:40 -0500

    Replace Lua 5.2.3 with Lua 5.3.3

--- src/lua/lctype.cpp
+++ src/lua/lctype.cpp
@@ -1,10 +1,14 @@
 /*
+** $Id: lctype.c,v 1.12 2014/11/02 19:19:04 roberto Exp $
 ** 'ctype' functions for Lua
 ** See Copyright Notice in lua.h
 */
 
 #define lctype_c
 #define LUA_CORE
+
+#include "lprefix.h"
+
 
 #include "lctype.h"
 
