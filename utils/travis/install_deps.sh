bc7c66d9f01aea681c8ac27c0ea35dcc8d3147de
Celtic Minstrel <celtic.minstrel.ca@some.place>
2016-11-11 20:07:55 -0500

    Travis: Wrap entire install script in travis_wait
    Using travis_wait inside the install script doesn't
    have the desired effect of increasing the timeout duration.

--- utils/travis/install_deps.sh
+++ utils/travis/install_deps.sh
@@ -4,13 +4,16 @@ if [ "$CXXSTD" == "1y" ]; then
 	sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y;
 fi
 
-travis_wait sudo apt-get update -qq
-travis_wait sudo apt-get install -qq libboost-filesystem-dev libboost-iostreams-dev libboost-random-dev libboost-program-options-dev libboost-regex-dev libboost-system-dev libboost-test-dev libboost-locale-dev libboost-thread-dev libcairo2-dev libfribidi-dev libpango1.0-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libvorbis-dev gdb moreutils xvfb
+sudo apt-get update -qq
+sudo apt-get install -qq libboost-filesystem-dev libboost-iostreams-dev libboost-random-dev libboost-program-options-dev libboost-regex-dev libboost-system-dev libboost-test-dev libboost-locale-dev libboost-thread-dev
+sudo apt-get install -qq libcairo2-dev libfribidi-dev libpango1.0-dev
+sudo apt-get install -qq libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libvorbis-dev
+sudo apt-get install gdb moreutils xvfb
 
 if [ "$USE_CMAKE" = true  ]; then
-	travis_wait sudo apt-get install -qq cmake;
+	sudo apt-get install -qq cmake;
 else
-	travis_wait sudo apt-get install -qq scons;
+	sudo apt-get install -qq scons;
 fi
 
 if [ "$CXXSTD" == "1y" ]; then
