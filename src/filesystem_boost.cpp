1fd6fb35dd1031ab55f0107675b136cbaf5aadda
gfgtdf <daniel.gfgtdf@gmail.com>
2016-12-17 22:18:36 +0100

    fix backslashes in {CURRENT_DIRECTORY}

--- src/filesystem_boost.cpp
+++ src/filesystem_boost.cpp
@@ -1262,11 +1262,11 @@ std::string get_short_wml_path(const std::string &filename)
 
 	path partial = subtract_path(full_path, get_user_data_path() / "data");
 	if (!partial.empty())
-		return "~" + partial.string();
+		return "~" + partial.generic_string();
 
 	partial = subtract_path(full_path, path(game_config::path) / "data");
 	if (!partial.empty())
-		return partial.string();
+		return partial.generic_string();
 
 	return filename;
 }
