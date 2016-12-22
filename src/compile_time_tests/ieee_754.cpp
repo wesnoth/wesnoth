7968f7ba5e1217eeff7cbe019aa07383a20133ef
Jyrki Vesterinen <sandgtx@gmail.com>
2016-12-22 20:40:15 +0200

    Check that floats are in the IEEE 754 format when building with CMake
    SCons version is coming a bit later.

---
+++ src/compile_time_tests/ieee_754.cpp
@@ -0,0 +1,17 @@
+#include <cstdint>
+
+// This test verifies that floating point numbers are represented in the IEEE 754 format.
+// Wesnoth requires that.
+int main()
+{
+	union
+	{
+		double floating_point_number;
+		uint64_t integer;
+	} number;
+
+	number.floating_point_number = 1.2;
+	// Return code zero means success.
+	// Thus, check that the bit representation is *not* what IEEE 754 specifies.
+	return number.integer != 0x3FF3333333333333ull;
+}
