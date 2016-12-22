1553cc6b6bda36c6f51b8d8b3cfbd51665028bd1
Jyrki Vesterinen <sandgtx@gmail.com>
2016-12-22 22:54:05 +0200

    CMake: print the bit representation of the test number
    I need this to investigate why the test is failing in Travis CI.

--- src/compile_time_tests/ieee_754.cpp
+++ src/compile_time_tests/ieee_754.cpp
@@ -1,4 +1,6 @@
+#include <cinttypes>
 #include <cstdint>
+#include <cstdio>
 
 // This test verifies that floating point numbers are represented in the IEEE 754 format.
 // Wesnoth requires that.
@@ -11,7 +13,13 @@ int main()
 	} number;
 
 	number.floating_point_number = 1.2;
+	bool match = (number.integer == 0x3FF3333333333333ull);
+	if (!match)
+	{
+		std::printf("Wrong binary representation. Expected 0x3FF3333333333333, got 0x%" PRIX64 "\n",
+		            number.integer);
+	}
 	// Return code zero means success.
 	// Thus, check that the bit representation is *not* what IEEE 754 specifies.
-	return number.integer != 0x3FF3333333333333ull;
+	return !match;
 }
