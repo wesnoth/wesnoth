0801402dcf66b66c1f19d14c3dbf741f055628ef
loonycyborg <loonycyborg@gmail.com>
2017-01-17 04:22:47 +0300

    Fix out of tree build issue in scons/ieee_754.py

--- scons/ieee_754.py
+++ scons/ieee_754.py
@@ -1,9 +1,10 @@
 import os.path
+from SCons.Script import File
 
 def CheckIEEE754(context):
     context.Message("Checking if floating point numbers are in the IEEE 754 format... ")
-    test_file = open(os.path.join("src", "compile_time_tests", "ieee_754.cpp"))
-    ret, _ = context.TryRun(test_file.read(), ".cpp")
+    test_file = File(os.path.join("src", "compile_time_tests", "ieee_754.cpp")).get_contents()
+    ret, _ = context.TryRun(test_file, ".cpp")
     context.Result(ret)
     return ret
 
