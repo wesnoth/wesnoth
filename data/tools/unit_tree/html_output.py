2f713046f05a521ae00af10f32c174c24169fa23
Allefant <allefant@allefant.com>
2017-01-23 19:48:01 -0500

    [wmlunits] ignore translation errors from gettext

--- data/tools/unit_tree/html_output.py
+++ data/tools/unit_tree/html_output.py
@@ -97,6 +97,8 @@ class Translation:
                 # not sure why, but this happens within the
                 # gettext.translation call sometimes
                 self.catalog[textdomain] = self.dummy
+            except ValueError:
+                self.catalog[textdoman] = self.dummy
 
         r = self.catalog[textdomain].ugettext(string)
 
