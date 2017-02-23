wesnoth/wesnoth

commit 50867795352861762ebe4b42da8e13b0ecb9b3ee

Tree View: cleaned up remove_node and made it return the removed node's position

src/gui/widgets/tree_view.cpp
@@ -67,30 +67,23 @@ ttree_view_node& ttree_view::add_node(
 	return get_root_node().add_child(id, data);
 }
 
-void ttree_view::remove_node(ttree_view_node* node)
+int ttree_view::remove_node(ttree_view_node* node)
 {
 	assert(node && node != root_node_ && node->parent_node_);
-	const tpoint node_size = node->get_size();
 
-	boost::ptr_vector<ttree_view_node>::iterator itor
-			= node->parent_node_->children_.begin();
+	ttree_view_node::node_children_vector& parent_children = node->parent_node_->children_;
 
-	for(; itor != node->parent_node_->children_.end(); ++itor) {
-		if(&*itor == node) {
-			break;
-		}
+	auto itor = std::find(parent_children.begin(), parent_children.end(), *node);
+	assert(itor != parent_children.end());
+
+	parent_children.erase(itor);
+
+	if(get_size() != tpoint()) {
+		// Don't shrink the width, need to think about a good algorithm to do so.
+		resize_content(0, -node->get_size().y);
 	}
 
-	assert(itor != node->parent_node_->children_.end());
-
-	node->parent_node_->children_.erase(itor);
-
-	if(get_size() == tpoint()) {
-		return;
-	}
-
-	// Don't shrink the width, need to think about a good algorithm to do so.
-	resize_content(0, -node_size.y);
+	return itor - parent_children.begin();
 }
 
 void ttree_view::clear()

src/gui/widgets/tree_view.hpp
@@ -59,7 +59,7 @@ public:
 	add_node(const std::string& id,
 			 const std::map<std::string /* widget id */, string_map>& data);
 
-	void remove_node(ttree_view_node* tree_view_node);
+	int remove_node(ttree_view_node* tree_view_node);
 
 	void clear();
 

commit e98531541de78317e088ecd96d849a57087fd503

Fix UB in tree_view::remove_node()
The function used a node object after deleting it and an iterator after
invalidating it. Both are undefined behavior.

Thanks to @gfgtdf for spotting the first bug.

Both bugs were introduced in commit 50867795352861762ebe4b42da8e13b0ecb9b3ee.

src/gui/widgets/tree_view.cpp
@@ -71,20 +71,23 @@ tree_view_node& tree_view::add_node(
 int tree_view::remove_node(tree_view_node* node)
 {
 	assert(node && node != root_node_ && node->parent_node_);
+	const point node_size = node->get_size();
 
 	tree_view_node::node_children_vector& siblings = node->parent_node_->children_;
 
-	auto itor = std::find(siblings.begin(), siblings.end(), *node);
-	assert(itor != siblings.end());
+	auto node_itor = std::find(siblings.begin(), siblings.end(), *node);
+	assert(node_itor != siblings.end());
 
-	siblings.erase(itor);
+	const int position = node_itor - siblings.begin();
+
+	siblings.erase(node_itor);
 
 	if(get_size() != point()) {
 		// Don't shrink the width, need to think about a good algorithm to do so.
-		resize_content(0, -node->get_size().y);
+		resize_content(0, -node_size.y);
 	}
 
-	return itor - siblings.begin();
+	return position;
 }
 
 void tree_view::clear()