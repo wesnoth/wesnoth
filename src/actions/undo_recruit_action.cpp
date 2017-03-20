55481c9949ad1edcb43e03a63796049b856dffbc
Charles Dang <exodia339@gmail.com>
2017-03-20 11:59:06 +1100

    Added a bunch of missing copyright notices
    [ci_skip]

--- src/actions/undo_recruit_action.cpp
+++ src/actions/undo_recruit_action.cpp
@@ -1,3 +1,16 @@
+/*
+   Copyright (C) 2017 the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
 #include "actions/undo_recruit_action.hpp"
 #include "actions/create.hpp"
 
