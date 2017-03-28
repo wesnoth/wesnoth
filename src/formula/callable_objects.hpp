8be08802a887951c5aa5e74e4f8e8b4606da6386
Charles Dang <exodia339@gmail.com>
2017-03-29 10:28:18 +1100

    Expand evil formula_callable class construction macros

--- src/formula/callable_objects.hpp
+++ src/formula/callable_objects.hpp
@@ -21,47 +21,9 @@
 #include "team.hpp"
 #include "units/unit.hpp"
 
-#define CALLABLE_WRAPPER_START(klass) \
-class klass##_callable : public game_logic::formula_callable { \
-	const klass& object_; \
-public: \
-	explicit klass##_callable(const klass& object) : object_(object) \
-	{} \
-	\
-	const klass& get_##klass() const { return object_; } \
-	void get_inputs(std::vector<game_logic::formula_input>* inputs) const \
-	{ \
-		using game_logic::FORMULA_READ_ONLY;
-
-#define CALLABLE_WRAPPER_INPUT(VAR) \
-	inputs->push_back(game_logic::formula_input(#VAR, FORMULA_READ_ONLY));
-
-#define CALLABLE_WRAPPER_INPUT_END \
-	} \
-	\
-	variant get_value(const std::string& key) const {
-
-#define CALLABLE_WRAPPER_VAR(VAR) \
-	if(key == #VAR) { \
-		return variant(object_.VAR); \
-	} else
-
-#define CALLABLE_WRAPPER_FN2(VAR, FN) \
-	if(key == #VAR) { \
-		return variant(object_.FN()); \
-	} else
-
-#define CALLABLE_WRAPPER_FN(VAR) CALLABLE_WRAPPER_FN2(VAR, VAR)
-
-#define CALLABLE_WRAPPER_END \
-		{ return variant(); } \
-	} \
-};
-
 template <typename T, typename K> variant convert_map( const std::map<T,K>& map );
 
 template <typename T> variant convert_vector( const std::vector<T>& input_vector );
-
 
 class terrain_callable : public game_logic::formula_callable {
 public:
@@ -81,11 +43,24 @@ private:
 	const terrain_type &t_;
 };
 
-CALLABLE_WRAPPER_START(gamemap)
-CALLABLE_WRAPPER_INPUT(terrain)
-CALLABLE_WRAPPER_INPUT(w)
-CALLABLE_WRAPPER_INPUT(h)
-CALLABLE_WRAPPER_INPUT_END
+class gamemap_callable : public game_logic::formula_callable
+{
+	const gamemap& object_;
+public:
+	explicit gamemap_callable(const gamemap& object) : object_(object)
+	{}
+
+	const gamemap& get_gamemap() const { return object_; }
+	void get_inputs(std::vector<game_logic::formula_input>* inputs) const
+	{
+		using game_logic::FORMULA_READ_ONLY;
+		inputs->push_back(game_logic::formula_input("gamemap", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("terrain", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("w", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("h", FORMULA_READ_ONLY));
+	}
+
+	variant get_value(const std::string& key) const {
 	if(key == "terrain") {
 		int w = object_.w();
 		int h = object_.h();
@@ -98,9 +73,16 @@ CALLABLE_WRAPPER_INPUT_END
 		}
 		return variant(&vars);
 	} else
-	CALLABLE_WRAPPER_FN(w)
-	CALLABLE_WRAPPER_FN(h)
-CALLABLE_WRAPPER_END
+		if(key == "w") {
+		return variant(object_.w());
+	} else
+	if(key == "h") {
+		return variant(object_.h());
+	} else
+		{ return variant(); }
+	}
+};
+
 
 class location_callable : public game_logic::formula_callable {
 	map_location loc_;
@@ -196,62 +178,120 @@ private:
 };
 
 
-CALLABLE_WRAPPER_START(team)
-CALLABLE_WRAPPER_INPUT(side)
-CALLABLE_WRAPPER_INPUT(id)
-CALLABLE_WRAPPER_INPUT(gold)
-CALLABLE_WRAPPER_INPUT(start_gold)
-CALLABLE_WRAPPER_INPUT(base_income)
-CALLABLE_WRAPPER_INPUT(total_income)
-CALLABLE_WRAPPER_INPUT(village_gold)
-CALLABLE_WRAPPER_INPUT(village_support)
-CALLABLE_WRAPPER_INPUT(recall_cost)
-CALLABLE_WRAPPER_INPUT(name)
-CALLABLE_WRAPPER_INPUT(is_human)
-CALLABLE_WRAPPER_INPUT(is_ai)
-CALLABLE_WRAPPER_INPUT(is_network)
-CALLABLE_WRAPPER_INPUT(fog)
-CALLABLE_WRAPPER_INPUT(shroud)
-CALLABLE_WRAPPER_INPUT(hidden)
-CALLABLE_WRAPPER_INPUT(flag)
-CALLABLE_WRAPPER_INPUT(flag_icon)
-CALLABLE_WRAPPER_INPUT(team_name)
-CALLABLE_WRAPPER_INPUT(faction)
-CALLABLE_WRAPPER_INPUT(faction_name)
-CALLABLE_WRAPPER_INPUT(color)
-CALLABLE_WRAPPER_INPUT(share_vision)
-CALLABLE_WRAPPER_INPUT(carryover_bonus)
-CALLABLE_WRAPPER_INPUT(carryover_percentage)
-CALLABLE_WRAPPER_INPUT(carryover_add)
-CALLABLE_WRAPPER_INPUT(recruit)
-CALLABLE_WRAPPER_INPUT(wml_vars)
-CALLABLE_WRAPPER_INPUT_END
-CALLABLE_WRAPPER_FN(side)
-CALLABLE_WRAPPER_FN2(id, save_id)
-CALLABLE_WRAPPER_FN(save_id)
-CALLABLE_WRAPPER_FN(gold)
-	if(key == "start_gold") {
-		return variant(object_.start_gold());
-	} else
-CALLABLE_WRAPPER_FN(base_income)
-CALLABLE_WRAPPER_FN(total_income)
-CALLABLE_WRAPPER_FN(village_gold)
-CALLABLE_WRAPPER_FN(village_support)
-CALLABLE_WRAPPER_FN(recall_cost)
-CALLABLE_WRAPPER_FN2(is_human, is_local_human)
-CALLABLE_WRAPPER_FN2(is_ai, is_local_ai)
-CALLABLE_WRAPPER_FN(is_network)
-CALLABLE_WRAPPER_FN2(fog, uses_fog)
-CALLABLE_WRAPPER_FN2(shroud, uses_shroud)
-CALLABLE_WRAPPER_FN(hidden)
-CALLABLE_WRAPPER_FN(flag)
-CALLABLE_WRAPPER_FN(flag_icon)
-CALLABLE_WRAPPER_FN(team_name)
-CALLABLE_WRAPPER_FN(color)
-CALLABLE_WRAPPER_FN2(share_vision, share_vision().to_string)
-CALLABLE_WRAPPER_FN(carryover_bonus)
-CALLABLE_WRAPPER_FN(carryover_percentage)
-CALLABLE_WRAPPER_FN(carryover_add)
+class team_callable : public game_logic::formula_callable {
+	const team& object_;
+public:
+	explicit team_callable(const team& object) : object_(object)
+	{}
+
+	const team& get_team() const { return object_; }
+	void get_inputs(std::vector<game_logic::formula_input>* inputs) const
+	{
+		using game_logic::FORMULA_READ_ONLY;
+
+		inputs->push_back(game_logic::formula_input("side", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("id", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("gold", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("start_gold", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("base_income", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("total_income", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("village_gold", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("village_support", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("recall_cost", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("name", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("is_human", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("is_ai", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("is_network", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("fog", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("shroud", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("hidden", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("flag", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("flag_icon", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("team_name", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("faction", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("faction_name", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("color", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("share_vision", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("carryover_bonus", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("carryover_percentage", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("carryover_add", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("recruit", FORMULA_READ_ONLY));
+		inputs->push_back(game_logic::formula_input("wml_vars", FORMULA_READ_ONLY));
+	}
+
+	variant get_value(const std::string& key) const {
+		if(key == "side") {
+			return variant(object_.side());
+		} else
+		if(key == "id") {
+			return variant(object_.save_id());
+		} else
+		if(key == "save_id") {
+			return variant(object_.save_id());
+		} else
+		if(key == "gold") {
+			return variant(object_.gold());
+		} else
+		if(key == "start_gold") {
+			return variant(object_.start_gold());
+		} else
+		if(key == "base_income") {
+			return variant(object_.base_income());
+		} else
+		if(key == "total_income") {
+			return variant(object_.total_income());
+		} else
+		if(key == "village_gold") {
+			return variant(object_.village_gold());
+		} else
+		if(key == "village_support") {
+			return variant(object_.village_support());
+		} else
+		if(key == "recall_cost") {
+			return variant(object_.recall_cost());
+		} else
+		if(key == "is_human") {
+			return variant(object_.is_local_human());
+		} else
+		if(key == "is_ai") {
+			return variant(object_.is_local_ai());
+		} else
+		if(key == "is_network") {
+			return variant(object_.is_network());
+		} else
+		if(key == "fog") {
+			return variant(object_.uses_fog());
+		} else
+		if(key == "shroud") {
+			return variant(object_.uses_shroud());
+		} else
+		if(key == "hidden") {
+			return variant(object_.hidden());
+		} else
+		if(key == "flag") {
+			return variant(object_.flag());
+		} else
+		if(key == "flag_icon") {
+			return variant(object_.flag_icon());
+		} else
+		if(key == "team_name") {
+			return variant(object_.team_name());
+		} else
+		if(key == "color") {
+			return variant(object_.color());
+		} else
+		if(key == "share_vision") {
+			return variant(object_.share_vision().to_string());
+		} else
+		if(key == "carryover_bonus") {
+			return variant(object_.carryover_bonus());
+		} else
+		if(key == "carryover_percentage") {
+			return variant(object_.carryover_percentage());
+		} else
+		if(key == "carryover_add") {
+			return variant(object_.carryover_add());
+		} else
 	if(key == "recruit") {
 		const std::set<std::string>& recruits = object_.recruits();
 		std::vector<variant> result;
@@ -262,6 +302,7 @@ CALLABLE_WRAPPER_FN(carryover_add)
 	} else if(key == "wml_vars") {
 		return variant(new config_callable(object_.variables()));
 	} else
-CALLABLE_WRAPPER_END
-
+		{ return variant(); }
+	}
+};
 #endif
