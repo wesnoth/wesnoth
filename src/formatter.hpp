30e0f14863c5a832bda967c0145280bd8efe46d9
Charles Dang <exodia339@gmail.com>
2017-04-04 11:25:05 +1100

    Updated ancient Copyright notices
    [ci ckip]
    
    Silver Tree was an ancient project of Dave's. I've updated the notices with his permission.

--- src/formatter.hpp
+++ src/formatter.hpp
@@ -1,9 +1,11 @@
 /*
-   Copyright (C) 2007 - 2017 by David White <dave.net>
-   Part of the Silver Tree Project
+   Copyright (C) 2003 - 2017 by David White <dave@whitevine.net>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by or later.
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY.
 
