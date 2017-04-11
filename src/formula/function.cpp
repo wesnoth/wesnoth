1b42378bdc83e8fccf1d78e2b46d969e708e764e
Celtic Minstrel <celtic.minstrel.ca@some.place>
2017-04-10 23:40:12 -0400

    WFL: Add colon to debug_print() output with two parameters

--- src/formula/function.cpp
+++ src/formula/function.cpp
@@ -312,7 +312,7 @@ private:
 			str1 = var1.string_cast();
 			const variant var2 = args()[1]->evaluate(variables,fdb);
 			str2 = var2.to_debug_string(true);
-			LOG_SF << str1 << str2 << std::endl;
+			LOG_SF << str1 << ": " << str2 << std::endl;
 			if(game_config::debug) {
 				game_display::get_singleton()->get_chat_manager().add_chat_message(time(nullptr), str1, 0, str2, events::chat_handler::MESSAGE_PUBLIC, false);
 			}
