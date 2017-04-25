3de10e5e61dfe07bb69b6a93743c7b71f1e28a6d
Charles Dang <exodia339@gmail.com>
2017-04-25 12:13:24 +1100

    Custom ToD: fixed bug where color sliders would continuously slide to the left when focused
    This had something to do with the value being set agin when invalidate_layout was called, which then triggered
    the callback, which then called invalidate_layout again...
    
    The purpose of the call was to redraw the window. The same can be accomplished with set_is_dirty, though this
    *might* need to be reverted if it turns out there isn't enough space to show a potential mask image. We'll see.

--- src/gui/dialogs/editor/custom_tod.cpp
+++ src/gui/dialogs/editor/custom_tod.cpp
@@ -299,7 +299,8 @@ void custom_tod::update_tod_display(window& window)
 	// redraw tiles
 	disp->draw(false);
 
-	window.invalidate_layout();
+	// NOTE: revert to invalidate_layout if necessary to display the ToD mask image.
+	window.set_is_dirty(true);
 }
 
 void custom_tod::update_lawful_bonus(window& window)
