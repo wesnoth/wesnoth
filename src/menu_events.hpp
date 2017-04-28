a4e3f54661f3013e6bc4a829d60848481826276e
Charles Dang <exodia339@gmail.com>
2017-04-28 18:36:50 +1100

    Menu Handler/Chat Handler: reformat
    No functionality changes.

--- src/menu_events.hpp
+++ src/menu_events.hpp
@@ -18,8 +18,8 @@
 
 #include "chat_events.hpp"
 #include "floating_textbox.hpp"
-#include "units/map.hpp"
 #include "lua_jailbreak_exception.hpp"
+#include "units/map.hpp"
 
 #include <vector>
 
@@ -32,22 +32,29 @@ class play_controller;
 class team;
 class unit_map;
 
-namespace events {
-	class mouse_handler;
+namespace events
+{
+class mouse_handler;
 }
 
-struct fallback_ai_to_human_exception : public lua_jailbreak_exception {IMPLEMENT_LUA_JAILBREAK_EXCEPTION(fallback_ai_to_human_exception)};
+struct fallback_ai_to_human_exception : public lua_jailbreak_exception
+{
+	IMPLEMENT_LUA_JAILBREAK_EXCEPTION(fallback_ai_to_human_exception)
+};
 
-namespace events {
-
-class menu_handler : private chat_handler {
+namespace events
+{
+class menu_handler : private chat_handler
+{
 public:
-	menu_handler(game_display* gui, play_controller & pc,
-		const config& game_config);
+	menu_handler(game_display* gui, play_controller& pc, const config& game_config);
 	virtual ~menu_handler();
 
 	gui::floating_textbox& get_textbox();
-	void set_gui(game_display* gui) { gui_ = gui; }
+	void set_gui(game_display* gui)
+	{
+		gui_ = gui;
+	}
 
 	void objectives();
 	void show_statistics(int side_num);
@@ -60,8 +67,8 @@ public:
 	void speak();
 	void whisper();
 	void shout();
-	void recruit(int side_num, const map_location &last_hex);
-	void repeat_recruit(int side_num, const map_location &last_hex);
+	void recruit(int side_num, const map_location& last_hex);
+	void repeat_recruit(int side_num, const map_location& last_hex);
 	void recall(int side_num, const map_location& last_hex);
 	void show_enemy_moves(bool ignore_units, int side_num);
 	void toggle_shroud_updates(int side_num);
@@ -77,14 +84,14 @@ public:
 	void label_terrain(mouse_handler& mousehandler, bool team_only);
 	void clear_labels();
 	void label_settings();
-	void continue_move(mouse_handler &mousehandler, int side_num);
-	void execute_gotos(mouse_handler &mousehandler, int side_num);
+	void continue_move(mouse_handler& mousehandler, int side_num);
+	void execute_gotos(mouse_handler& mousehandler, int side_num);
 	void toggle_ellipses();
 	void toggle_grid();
-	void unit_hold_position(mouse_handler &mousehandler, int side_num);
-	void end_unit_turn(mouse_handler &mousehandler, int side_num);
+	void unit_hold_position(mouse_handler& mousehandler, int side_num);
+	void end_unit_turn(mouse_handler& mousehandler, int side_num);
 	void search();
-	void request_control_change(int side_num, const std::string &player);
+	void request_control_change(int side_num, const std::string& player);
 	void user_command();
 	void custom_command();
 	void ai_formula();
@@ -93,40 +100,50 @@ public:
 
 	unit_map::iterator current_unit();
 	unit_map::const_iterator current_unit() const
-	{ return const_cast<menu_handler *>(this)->current_unit(); }
-	void move_unit_to_loc(const unit_map::iterator& ui, const map_location& target,
-		bool continue_move, int side_num, mouse_handler &mousehandler);
+	{
+		return const_cast<menu_handler*>(this)->current_unit();
+	}
+
+	void move_unit_to_loc(const unit_map::iterator& ui,
+			const map_location& target,
+			bool continue_move,
+			int side_num,
+			mouse_handler& mousehandler);
+
 	///@return Whether or not the recruit was successful
 	bool do_recruit(const std::string& name, int side_num, const map_location& last_hex);
 	void do_speak();
 	void do_search(const std::string& new_search);
-	void do_command(const std::string &str);
-	void do_ai_formula(const std::string &str, int side_num, mouse_handler &mousehandler);
+	void do_command(const std::string& str);
+	void do_ai_formula(const std::string& str, int side_num, mouse_handler& mousehandler);
 	void send_to_server(const config& cfg) override;
 
-	game_state & gamestate() const;
-	game_data & gamedata();
-	game_board & board() const;
+	game_state& gamestate() const;
+	game_data& gamedata();
+	game_board& board() const;
 	unit_map& units();
 	std::vector<team>& teams() const;
 	const gamemap& map() const;
 
 protected:
-	void add_chat_message(const time_t& time, const std::string& speaker,
-			int side, const std::string& message,
-			events::chat_handler::MESSAGE_TYPE type=events::chat_handler::MESSAGE_PRIVATE) override;
-	void send_chat_message(const std::string& message, bool allies_only=false) override;
+	void add_chat_message(const time_t& time,
+			const std::string& speaker,
+			int side,
+			const std::string& message,
+			events::chat_handler::MESSAGE_TYPE type = events::chat_handler::MESSAGE_PRIVATE) override;
+	void send_chat_message(const std::string& message, bool allies_only = false) override;
+
 private:
-	//console_handler is basically a sliced out part of menu_handler
-	//and as such needs access to menu_handler's privates
+	// console_handler is basically a sliced out part of menu_handler
+	// and as such needs access to menu_handler's privates
 	friend class console_handler;
 
-	//void do_speak(const std::string& message, bool allies_only);
-//	std::vector<std::string> create_unit_table(const statistics::stats::str_int_map& m,unsigned int team);
+	// void do_speak(const std::string& message, bool allies_only);
+	// std::vector<std::string> create_unit_table(const statistics::stats::str_int_map& m,unsigned int team);
 	bool has_friends() const;
 
 	game_display* gui_;
-	play_controller & pc_;
+	play_controller& pc_;
 
 	const config& game_config_;
 
@@ -134,6 +151,6 @@ private:
 	std::string last_search_;
 	map_location last_search_hit_;
 };
-
 }
+
 #endif
