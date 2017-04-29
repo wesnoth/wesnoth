9edf49e3af6bc87bb2c6ab21cacdfdc4c1570e4e
Jyrki Vesterinen <sandgtx@gmail.com>
2017-04-29 10:34:58 +0300

    Remove an unused variable
    Thanks to @matthiaskrgr for noticing.

--- src/scripting/lua_audio.cpp
+++ src/scripting/lua_audio.cpp
@@ -137,7 +137,7 @@ static int intf_music_play(lua_State* L) {
 	return 0;
 }
 
-static int intf_music_next(lua_State* L) {
+static int intf_music_next(lua_State*) {
 	size_t n = sound::get_num_tracks();
 	if(n > 0) {
 		sound::play_track(n);
