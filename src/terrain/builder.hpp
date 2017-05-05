66b4c30a72223ab32093ea947810992157386b4d
Charles Dang <exodia339@gmail.com>
2017-05-05 21:43:51 +1100

    Reformat terrain builder code

--- src/terrain/builder.hpp
+++ src/terrain/builder.hpp
@@ -21,13 +21,16 @@
 #define TERRAIN_BUILDER_H_INCLUDED
 
 #include "animated.hpp"
+#include "game_config.hpp"
 #include "map/location.hpp"
 #include "terrain/translation.hpp"
-#include "game_config.hpp"
 
 class config;
 class gamemap;
-namespace image{ class locator; }
+namespace image
+{
+class locator;
+}
 /**
  * The class terrain_builder is constructed from a config object, and a
  * gamemap object. On construction, it parses the configuration and extracts
@@ -45,14 +48,14 @@ class terrain_builder
 public:
 	/** Used as a parameter for the get_terrain_at function. */
 	enum TERRAIN_TYPE {
-			BACKGROUND,	/**<
-						* Represents terrains which are to be
-						* drawn behind unit sprites
-						*/
-			FOREGROUND	/**<
-						* Represents terrains which are to be
-						* drawn in front of them.
-						 */
+		BACKGROUND, /**<
+					 * Represents terrains which are to be
+					 * drawn behind unit sprites
+					 */
+		FOREGROUND  /**<
+					 * Represents terrains which are to be
+					 * drawn in front of them.
+					 */
 	};
 
 	/** The position of unit graphics in a tile. Graphics whose y
@@ -67,7 +70,7 @@ public:
 	/** A shorthand typedef for a list of animated image locators,
 	 * the base data type returned by the get_terrain_at method.
 	 */
-	typedef std::vector<animated<image::locator> > imagelist;
+	typedef std::vector<animated<image::locator>> imagelist;
 
 	/** Constructor for the terrain_builder class.
 	 *
@@ -80,8 +83,7 @@ public:
 	 *						This image automatically gets the 'terrain/' prefix
 	 *						and '.png' suffix
 	 */
-	terrain_builder(const config &level, const gamemap* map,
-			const std::string& offmap_image);
+	terrain_builder(const config& level, const gamemap* map, const std::string& offmap_image);
 
 	/**  Set the config where we will parse the global terrain rules.
 	 *   This also flushes the terrain rules cache.
@@ -91,7 +93,10 @@ public:
 	 */
 	static void set_terrain_rules_cfg(const config& cfg);
 
-	const gamemap& map() const { return *map_; }
+	const gamemap& map() const
+	{
+		return *map_;
+	}
 
 	/**
 	 * Updates internals that cache map size. This should be called when the map
@@ -116,8 +121,7 @@ public:
 	 * @return      Returns a pointer list of animated images corresponding
 	 *              to the parameters, or nullptr if there is none.
 	 */
-	const imagelist *get_terrain_at(const map_location &loc,
-			const std::string &tod, TERRAIN_TYPE const terrain_type);
+	const imagelist* get_terrain_at(const map_location& loc, const std::string& tod, TERRAIN_TYPE const terrain_type);
 
 	/** Updates the animation at a given tile.
 	 * Returns true if something has changed, and must be redrawn.
@@ -126,7 +130,7 @@ public:
 	 *
 	 * @retval      true: this tile must be redrawn.
 	 */
-	bool update_animation(const map_location &loc);
+	bool update_animation(const map_location& loc);
 
 	/** Performs a "quick-rebuild" of the terrain in a given location.
 	 * The "quick-rebuild" is no proper rebuild: it only clears the
@@ -135,7 +139,7 @@ public:
 	 *
 	 * @param loc   the location where to rebuild terrains
 	 */
-	void rebuild_terrain(const map_location &loc);
+	void rebuild_terrain(const map_location& loc);
 
 	/** Performs a complete rebuild of the list of terrain graphics
 	 * attached to a map.
@@ -150,19 +154,25 @@ public:
 	 * WML tag of the [image] WML tag. When an image only has one variant,
 	 * the [variant] tag may be omitted.
 	 */
-	struct rule_image_variant {
+	struct rule_image_variant
+	{
 		/** Constructor for the normal defaut case */
-		rule_image_variant(const std::string &image_string, const std::string& variations, bool random_start = true) :
-			image_string(image_string),
-			variations(variations),
-			images(),
-			tods(),
-			has_flag(),
-			random_start(random_start)
-			{}
+		rule_image_variant(const std::string& image_string, const std::string& variations, bool random_start = true)
+			: image_string(image_string)
+			, variations(variations)
+			, images()
+			, tods()
+			, has_flag()
+			, random_start(random_start)
+		{
+		}
 
 		/** Constructor for true [variant] cases */
-		rule_image_variant(const std::string &image_string, const std::string& variations, const std::string& tod, const std::string& has_flag, bool random_start = true);
+		rule_image_variant(const std::string& image_string,
+				const std::string& variations,
+				const std::string& tod,
+				const std::string& has_flag,
+				bool random_start = true);
 
 		/** A string representing either the filename for an image, or
 		 *  a list of images, with an optional timing for each image.
@@ -192,7 +202,7 @@ public:
 		 * This will be the image locator which will actually
 		 * be returned to the user.
 		 */
-		std::vector< animated<image::locator> > images;
+		std::vector<animated<image::locator>> images;
 
 		/** The Time of Day associated to this variant (if any)*/
 		std::set<std::string> tods;
@@ -210,11 +220,19 @@ public:
 	 * and a position for these graphics.
 	 * The rule_image structure represents one such image.
 	 */
-	struct rule_image {
-		rule_image(int layer, int x, int y, bool global_image=false, int center_x=-1, int center_y=-1, bool is_water=false);
+	struct rule_image
+	{
+		rule_image(int layer,
+				int x,
+				int y,
+				bool global_image = false,
+				int center_x = -1,
+				int center_y = -1,
+				bool is_water = false);
 
-		bool is_background() const {
-		return layer < 0 || (layer == 0 && basey < UNITPOS);
+		bool is_background() const
+		{
+			return layer < 0 || (layer == 0 && basey < UNITPOS);
 		}
 
 		/** The layer of the image for horizontal layering */
@@ -250,25 +268,27 @@ public:
 	 */
 	struct terrain_constraint
 	{
-		terrain_constraint() :
-			loc(),
-			terrain_types_match(),
-			set_flag(),
-			no_flag(),
-			has_flag(),
-			no_draw(),
-			images()
-			{}
+		terrain_constraint()
+			: loc()
+			, terrain_types_match()
+			, set_flag()
+			, no_flag()
+			, has_flag()
+			, no_draw()
+			, images()
+		{
+		}
 
-		terrain_constraint(map_location loc) :
-			loc(loc),
-			terrain_types_match(),
-			set_flag(),
-			no_flag(),
-			has_flag(),
-			no_draw(),
-			images()
-			{}
+		terrain_constraint(map_location loc)
+			: loc(loc)
+			, terrain_types_match()
+			, set_flag()
+			, no_flag()
+			, has_flag()
+			, no_draw()
+			, images()
+		{
+		}
 
 		map_location loc;
 		t_translation::ter_match terrain_types_match;
@@ -302,7 +322,7 @@ public:
 		 *
 		 * @param tod    The current time-of-day
 		 */
-		void rebuild_cache(const std::string &tod, logs* log = nullptr);
+		void rebuild_cache(const std::string& tod, logs* log = nullptr);
 
 		/** Clears all data in this tile, and resets the cache */
 		void clear();
@@ -311,14 +331,23 @@ public:
 		std::set<std::string> flags;
 
 		/** Represent a rule_image applied with a random seed.*/
-		struct rule_image_rand{
-			rule_image_rand(const rule_image* r_i, unsigned int rnd) : ri(r_i), rand(rnd) {}
+		struct rule_image_rand
+		{
+			rule_image_rand(const rule_image* r_i, unsigned int rnd)
+				: ri(r_i)
+				, rand(rnd)
+			{
+			}
 
-			const rule_image* operator->() const {return ri;}
+			const rule_image* operator->() const
+			{
+				return ri;
+			}
 			/** sort by layer first then by basey */
-			bool operator<(const rule_image_rand& o) const {
-				return ri->layer < o.ri->layer ||
-						(ri->layer == o.ri->layer && ri->basey < o.ri->basey);}
+			bool operator<(const rule_image_rand& o) const
+			{
+				return ri->layer < o.ri->layer || (ri->layer == o.ri->layer && ri->basey < o.ri->basey);
+			}
 
 			const rule_image* ri;
 			unsigned int rand;
@@ -347,15 +376,14 @@ public:
 		bool sorted_images;
 	};
 
-	tile* get_tile(const map_location &loc);
+	tile* get_tile(const map_location& loc);
 
 private:
-
 	/** The tile width used when using basex and basey. This is not,
 	 * necessarily, the tile width in pixels, this is totally
 	 * arbitrary. However, it will be set to 72 for convenience.
 	 */
-	const int tilewidth_;  // = game_config::tile_size;
+	const int tilewidth_; // = game_config::tile_size;
 
 	/**
 	 * The list of constraints attached to a terrain_graphics WML rule.
@@ -367,15 +395,16 @@ private:
 	 */
 	struct building_rule
 	{
-		building_rule() :
-			constraints(),
-			location_constraints(),
-			modulo_constraints(),
-			probability(100),
-			precedence(0),
-			local(false),
-			hash_(DUMMY_HASH)
-		{}
+		building_rule()
+			: constraints()
+			, location_constraints()
+			, modulo_constraints()
+			, probability(100)
+			, precedence(0)
+			, local(false)
+			, hash_(DUMMY_HASH)
+		{
+		}
 
 		/**
 		 * The set of [tile] constraints of this rule.
@@ -413,10 +442,13 @@ private:
 		 */
 		bool local;
 
-		bool operator<(building_rule const &that) const
-		{ return precedence < that.precedence; }
+		bool operator<(building_rule const& that) const
+		{
+			return precedence < that.precedence;
+		}
 
 		unsigned int get_hash() const;
+
 	private:
 		mutable unsigned int hash_;
 	};
@@ -430,11 +462,13 @@ private:
 		/**
 		 * Constructs a tilemap of dimensions x * y
 		 */
-		tilemap(int x, int y) :
-				tiles_((x + 4) * (y + 4)),
-				x_(x),
-				y_(y)
-			{reset();}
+		tilemap(int x, int y)
+			: tiles_((x + 4) * (y + 4))
+			, x_(x)
+			, y_(y)
+		{
+			reset();
+		}
 
 		/**
 		 * Returns a reference to the tile which is at the position
@@ -445,11 +479,11 @@ private:
 		 * @return		A reference to the tile at this location.
 		 *
 		 */
-		tile &operator[](const map_location &loc);
+		tile& operator[](const map_location& loc);
 		/**
 		 * a const variant of operator[]
 		 */
-		const tile &operator[] (const map_location &loc) const;
+		const tile& operator[](const map_location& loc) const;
 
 		/**
 		 * Tests if a location is on the map.
@@ -458,7 +492,7 @@ private:
 		 *
 		 * @return		true if loc is on the map, false otherwise.
 		 */
-		bool on_map(const map_location &loc) const;
+		bool on_map(const map_location& loc) const;
 
 		/**
 		 * Resets the whole tile map
@@ -469,6 +503,7 @@ private:
 		 * Rebuilds the map to a new set of dimensions
 		 */
 		void reload(int x, int y);
+
 	private:
 		/** The map */
 		std::vector<tile> tiles_;
@@ -493,7 +528,7 @@ private:
 	 *
 	 * @return		true if the rule is valid, false if it is not.
 	 */
-	bool load_images(building_rule &rule);
+	bool load_images(building_rule& rule);
 
 	/**
 	 * Starts the animation on a rule.
@@ -502,7 +537,7 @@ private:
 	 *
 	 * @return		true
 	 */
-	bool start_animation(building_rule &rule);
+	bool start_animation(building_rule& rule);
 
 	/**
 	 *  "Rotates" a constraint from a rule.
@@ -524,7 +559,7 @@ private:
 	 *  @param constraint  A template constraint to rotate
 	 *  @param angle       An int, from 0 to 5, representing the rotation angle.
 	 */
-	void rotate(terrain_constraint &constraint, int angle);
+	void rotate(terrain_constraint& constraint, int angle);
 
 	/**
 	 * Replaces, in a given string, rotation tokens with their values.
@@ -533,8 +568,7 @@ private:
 	 * @param angle        the angle for substituting the correct replacement.
 	 * @param replacement  the replacement strings.
 	 */
-	void replace_rotate_tokens(std::string &s, int angle,
-		const std::vector<std::string> &replacement);
+	void replace_rotate_tokens(std::string& s, int angle, const std::vector<std::string>& replacement);
 
 	/**
 	 * Replaces, in a given rule_image, rotation tokens with their values.
@@ -544,8 +578,7 @@ private:
 	 * @param angle        the angle for substituting the correct replacement.
 	 * @param replacement  the replacement strings.
 	 */
-	void replace_rotate_tokens(rule_image &image, int angle,
-		const std::vector<std::string> &replacement);
+	void replace_rotate_tokens(rule_image& image, int angle, const std::vector<std::string>& replacement);
 
 	/**
 	 * Replaces, in a given rule_variant_image, rotation tokens with their values.
@@ -556,9 +589,10 @@ private:
 	 * @param angle        the angle for substituting the correct replacement.
 	 * @param replacement  the replacement strings.
 	 */
-	void replace_rotate_tokens(rule_image_variant &variant, int angle,
-		const std::vector<std::string> &replacement)
-	{ replace_rotate_tokens(variant.image_string, angle, replacement); }
+	void replace_rotate_tokens(rule_image_variant& variant, int angle, const std::vector<std::string>& replacement)
+	{
+		replace_rotate_tokens(variant.image_string, angle, replacement);
+	}
 
 	/**
 	 * Replaces, in a given rule_imagelist, rotation tokens with their values.
@@ -569,8 +603,7 @@ private:
 	 * @param angle        the angle for substituting the correct replacement.
 	 * @param replacement  the replacement strings.
 	 */
-	void replace_rotate_tokens(rule_imagelist &list, int angle,
-		const std::vector<std::string> &replacement);
+	void replace_rotate_tokens(rule_imagelist& list, int angle, const std::vector<std::string>& replacement);
 
 	/**
 	 * Replaces, in a given building_rule, rotation tokens with their values.
@@ -583,8 +616,7 @@ private:
 	 * @param angle        the angle for substituting the correct replacement.
 	 * @param replacement  the replacement strings.
 	 */
-	void replace_rotate_tokens(building_rule &rule, int angle,
-		const std::vector<std::string> &replacement);
+	void replace_rotate_tokens(building_rule& rule, int angle, const std::vector<std::string>& replacement);
 
 	/**
 	 *  Rotates a template rule to a given angle.
@@ -622,8 +654,7 @@ private:
 	 *    r2, r3, r4, r5, r0, r1 and so on.
 	 *
 	 */
-	void rotate_rule(building_rule &rule, int angle,
-		const std::vector<std::string> &angle_name);
+	void rotate_rule(building_rule& rule, int angle, const std::vector<std::string>& angle_name);
 
 	/**
 	 * Parses a "config" object, which should contains [image] children,
@@ -641,9 +672,7 @@ private:
 	 * @param dy       The Y coordinate of the constraint those images
 	 *                 apply to.
 	 */
-	void add_images_from_config(rule_imagelist &images, const config &cfg, bool global,
-			int dx=0, int dy=0);
-
+	void add_images_from_config(rule_imagelist& images, const config& cfg, bool global, int dx = 0, int dy = 0);
 
 	/**
 	 * Creates a rule constraint object which matches a given list of
@@ -655,8 +684,9 @@ private:
 	 * @param global_images A configuration object containing [image] tags
 	 *                      describing rule-global images.
 	 */
-	terrain_constraint &add_constraints(constraint_set& constraints,
-			const map_location &loc, const t_translation::ter_match& type,
+	terrain_constraint& add_constraints(constraint_set& constraints,
+			const map_location& loc,
+			const t_translation::ter_match& type,
 			const config& global_images);
 
 	/**
@@ -670,9 +700,8 @@ private:
 	 * @param global_images A configuration object containing [image] tags
 	 *                      describing rule-global images.
 	 */
-	void add_constraints(constraint_set& constraints,
-			const map_location &loc, const config &cfg,
-			const config& global_images);
+	void add_constraints(
+			constraint_set& constraints, const map_location& loc, const config& cfg, const config& global_images);
 
 	typedef std::multimap<int, map_location> anchormap;
 
@@ -687,8 +716,8 @@ private:
 	 * @param global_images A config object representing the images defined
 	 *                      as direct children of the [terrain_graphics] rule.
 	 */
-	void parse_mapstring(const std::string &mapstring, struct building_rule &br,
-			     anchormap& anchors, const config& global_images);
+	void parse_mapstring(
+			const std::string& mapstring, struct building_rule& br, anchormap& anchors, const config& global_images);
 
 	/**
 	 * Adds a rule to a ruleset. Checks for validity before adding the rule.
@@ -696,7 +725,7 @@ private:
 	 * @param rules      The ruleset into which to add the rules.
 	 * @param rule       The rule to add.
 	 */
-	void add_rule(building_ruleset& rules, building_rule &rule);
+	void add_rule(building_ruleset& rules, building_rule& rule);
 
 	/**
 	 * Adds a set of rules to a ruleset, from a template rule which spans
@@ -708,8 +737,7 @@ private:
 	 *                   6 values for replacing rotation template
 	 *                   template strings @verbatim (@Rn) @endverbatim
 	 */
-	void add_rotated_rules(building_ruleset& rules, building_rule& tpl,
-		const std::string &rotations);
+	void add_rotated_rules(building_ruleset& rules, building_rule& tpl, const std::string& rotations);
 
 	/**
 	 * Parses a configuration object containing [terrain_graphics] rules,
@@ -719,9 +747,12 @@ private:
 	 * @param cfg       The configuration object to parse.
 	 * @param local     Mark the rules as local only.
 	 */
-	void parse_config(const config &cfg, bool local = true);
+	void parse_config(const config& cfg, bool local = true);
 
-	void parse_global_config(const config &cfg) { parse_config(cfg, false); }
+	void parse_global_config(const config& cfg)
+	{
+		parse_config(cfg, false);
+	}
 
 	/**
 	 * Adds a builder rule for the _off^_usr tile, this tile only has 1 image.
@@ -744,8 +775,10 @@ private:
 	 * @return			returns true if "tcode" matches the list or the list is empty,
 	 *					else false.
 	 */
-	bool terrain_matches(const t_translation::terrain_code & tcode, const t_translation::ter_list& terrains) const
-		{ return terrains.empty()? true : t_translation::terrain_matches(tcode, terrains); }
+	bool terrain_matches(const t_translation::terrain_code& tcode, const t_translation::ter_list& terrains) const
+	{
+		return terrains.empty() ? true : t_translation::terrain_matches(tcode, terrains);
+	}
 
 	/**
 	 * Checks whether a terrain code matches a given list of terrain tcodes.
@@ -757,8 +790,10 @@ private:
 	 * @return			returns true if "tcode" matches the list or the list is empty,
 	 *					else false.
 	 */
-	bool terrain_matches(const t_translation::terrain_code & tcode, const t_translation::ter_match &terrain) const
-		{ return terrain.is_empty ? true : t_translation::terrain_matches(tcode, terrain); }
+	bool terrain_matches(const t_translation::terrain_code& tcode, const t_translation::ter_match& terrain) const
+	{
+		return terrain.is_empty ? true : t_translation::terrain_matches(tcode, terrain);
+	}
 
 	/**
 	 * Checks whether a rule matches a given location in the map.
@@ -769,7 +804,7 @@ private:
 	 * @param type_checked The constraint which we already know that its
 	 *                  terrain types matches.
 	 */
-	bool rule_matches(const building_rule &rule, const map_location &loc, const terrain_constraint *type_checked) const;
+	bool rule_matches(const building_rule& rule, const map_location& loc, const terrain_constraint* type_checked) const;
 
 	/**
 	 * Applies a rule at a given location: applies the result of a
@@ -779,7 +814,7 @@ private:
 	 * @param rule      The rule to apply
 	 * @param loc       The location to which to apply the rule.
 	 */
-	void apply_rule(const building_rule &rule, const map_location &loc);
+	void apply_rule(const building_rule& rule, const map_location& loc);
 
 	/**
 	 * Calculates the list of terrains, and fills the tile_map_ member,
@@ -802,7 +837,7 @@ private:
 	/**
 	 * Shorthand typedef for a map associating a list of locations to a terrain type.
 	 */
-	typedef std::map<t_translation::terrain_code, std::vector<map_location> > terrain_by_type_map;
+	typedef std::map<t_translation::terrain_code, std::vector<map_location>> terrain_by_type_map;
 
 	/**
 	 * A map representing all locations whose terrain is of a given type.
