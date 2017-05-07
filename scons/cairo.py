691db62ecf3a7e262e1238df849657e646d26585
loonycyborg <loonycyborg@gmail.com>
2017-05-07 03:40:24 +0300

    scons: Add support for pkg-config files in custom path to cairo check

--- scons/cairo.py
+++ scons/cairo.py
@@ -1,8 +1,19 @@
+import sys
 from os import environ
+from os.path import join
+from SCons.Util import AppendPath
 
 def CheckCairo(context, min_version):
     context.Message("Checking for Cairo... ")
     env = context.env
+
+    gtkdir = env.get("gtkdir", environ.get("GTK_BASEPATH"))
+    if gtkdir:
+        environ["PATH"] = AppendPath(environ["PATH"], join(gtkdir, "bin"))
+        environ["PKG_CONFIG_PATH"] = AppendPath(environ.get("PKG_CONFIG_PATH", ""), join(gtkdir, "lib/pkgconfig"))
+        if sys.platform != "win32":
+            env["PKGCONFIG_FLAGS"] = "--define-variable=prefix=" + gtkdir
+
     try:
         env["ENV"]["PKG_CONFIG_PATH"] = environ.get("PKG_CONFIG_PATH", "")
         version_arg = env["ESCAPE"](" >= ") + min_version
