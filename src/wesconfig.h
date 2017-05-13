bfa5ec1619004f3ff54cc26c56a4f8c006406474
Charles Dang <exodia339@gmail.com>
2017-05-14 10:12:20 +1100

    Pre-release version bump

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -40,7 +40,7 @@
   #undef VERSION
 #endif
 
-#define VERSION "1.13.7+dev"
+#define VERSION "1.13.8"
 
 // Used for the Windows executables' version info resource.
 #define RC_VERSION_MAJOR        1
