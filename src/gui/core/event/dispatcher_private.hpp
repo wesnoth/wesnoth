wesnoth/wesnoth

commit 315f8496b61db8347b694e2efadfe6ff9ccc7702

Allow widget::find_at() to return a scrollbar container
If the pointer wasn't on top of any widget within a scrollbar container,
when GUI2 queried the widget on which a UI event occurred, the answer was
"no widget" and the whole event was discarded.

That was problematic if the player was trying to scroll. It is desired that
scrolling is possible even if the pointer isn't on a widget (as long as
it's somewhere within the scrollbar container).

Now the query will return "scrollbar container" and dispatch the event to
it. That allows the player to scroll anywhere within a scrollbar container.

Also, now the GUI2 event chain includes the target widget itself, not only
its ancestors.

Fixes #1632.

src/gui/core/event/dispatcher_private.hpp
@@ -306,12 +306,12 @@ build_event_chain(const ui_event event, widget* dispatcher, widget* w)
 	std::vector<std::pair<widget*, ui_event>> result;
 
 	while(w != dispatcher) {
-		w = w->parent();
-		assert(w);
-
 		if(w->has_event(event, dispatcher::event_queue_type(dispatcher::pre | dispatcher::post))) {
 			result.emplace_back(w, event);
 		}
+
+		w = w->parent();
+		assert(w);
 	}
 
 	return result;

src/gui/widgets/scrollbar_container.cpp
@@ -516,15 +516,23 @@ unsigned scrollbar_container::get_state() const
 widget* scrollbar_container::find_at(const point& coordinate,
 									   const bool must_be_active)
 {
-	return scrollbar_container_implementation::find_at<widget>(
+	widget* w = scrollbar_container_implementation::find_at<widget>(
 			*this, coordinate, must_be_active);
+	if(w == nullptr) {
+		w = widget::find_at(coordinate, must_be_active);
+	}
+	return w;
 }
 
 const widget* scrollbar_container::find_at(const point& coordinate,
 											 const bool must_be_active) const
 {
-	return scrollbar_container_implementation::find_at<const widget>(
+	const widget* w = scrollbar_container_implementation::find_at<const widget>(
 			*this, coordinate, must_be_active);
+	if(w == nullptr) {
+		w = widget::find_at(coordinate, must_be_active);
+	}
+	return w;
 }
 
 widget* scrollbar_container::find(const std::string& id,

commit 4c0f4c31546719c0a5fefa6973e32f0494a62f00

Attempted fix for GUI2 event firing test failures
Commit 315f8496b61db8347b694e2efadfe6ff9ccc7702 broke the test in two ways. First, I intentionally
changed pre and post events to be passed to the target widget as well, not
only its ancestors. The test verified that the events were *not* passed to
the target widget. Fixed by adjusting the test to verify that the target
widget now does receive the event.

Second, I unintentionally stopped the event dispatcher from receiving pre
and post events. Fixed.

src/gui/core/event/dispatcher_private.hpp
@@ -305,9 +305,13 @@ build_event_chain(const ui_event event, widget* dispatcher, widget* w)
 
 	std::vector<std::pair<widget*, ui_event>> result;
 
-	while(w != dispatcher) {
+	while(true) {
 		if(w->has_event(event, dispatcher::event_queue_type(dispatcher::pre | dispatcher::post))) {
 			result.emplace_back(w, event);
+		}
+
+		if(w == dispatcher) {
+			break;
 		}
 
 		w = w->parent();

src/tests/gui/fire_event.cpp
@@ -94,12 +94,13 @@ static void add_widget(gui2::grid& grid
 static std::string set_event_order()
 {
 	return
-			"pre:root\n"
-			"pre:level 1\n"
-			"child:level 2\n"
-			"post:level 1\n"
-			"post:root\n";
-
+		"pre:root\n"
+		"pre:level 1\n"
+		"pre:level 2\n"
+		"child:level 2\n"
+		"post:level 2\n"
+		"post:level 1\n"
+		"post:root\n";
 }
 
 /** @todo Add the rest of the events. */