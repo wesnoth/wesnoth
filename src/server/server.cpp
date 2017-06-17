48ed6e0c0557d6715cf235f801556ce528630216
loonycyborg <loonycyborg@gmail.com>
2017-06-17 22:53:38 +0300

    Fixed server incorrectly closing connection on hash error

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -784,6 +784,7 @@ void server::send_password_request(socket_ptr socket, const std::string& msg,
 								 "cannot log in due to an error in the hashing algorithm. "
 								 "Logging into your forum account on http://forum.wesnoth.org "
 								 "may fix this problem.");
+		login(socket);
 		return;
 	}
 
