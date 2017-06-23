60dceaaead293e6138a234ee8b83243272aa27c7
Charles Dang <exodia339@gmail.com>
2017-06-23 13:20:50 +1100

    Port floating textbox/command console to GUI2
    The old floating textbox was extremely entwined with the controller_base, play_controller, and menu_handler
    classes. controller_base::have_keyboard_focus essentially controlled whether some events were executed based
    on whether the floating textbox was open or not. Additionally, those events weren't even reached if a UI dialog
    was open at all.
    
    The new design features a singleton console class that can be called from anywhere, not just the game. I've also
    decoupled the execution object from play_controller. The relevant functions in menu_handler are now passed to
    the console as callbacks.
    
    To work around map events such as clicking not being available if the console was open, I removed the exclusionary
    is-in-dialog check from controller_base::handle_event and instead exit early out certain types of events using
    controller_base::have_keyboard_focus. As mentioned in the accompanying comment, this isn't the best solution, but
    it will do for now.
    
    The new console also isn't fully feature-comparable with the old GUI1 one. The following are still missing:
    * The checkbox, for use when sending messages.
    * Tab completion.
    * A crash occurs when existing the app if a game was exited with the console open.
    
    I'm leaving the old floating_textbox code around for now for reference.

---
+++ src/gui/dialogs/command_console.cpp
@@ -0,0 +1,89 @@
+/*
+   Copyright (C) 2017 by the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#define GETTEXT_DOMAIN "wesnoth-lib"
+
+#include "gui/dialogs/command_console.hpp"
+
+#include "gui/auxiliary/find_widget.hpp"
+#include "gui/dialogs/modal_dialog.hpp"
+#include "gui/widgets/label.hpp"
+#include "gui/widgets/settings.hpp"
+#include "gui/widgets/text_box.hpp"
+#include "gui/widgets/window.hpp"
+
+namespace gui2
+{
+namespace dialogs
+{
+REGISTER_DIALOG(command_console)
+
+std::unique_ptr<command_console> command_console::singleton_ = nullptr;
+
+command_console::command_console(const std::string& prompt, callback_t callback)
+	: input_(nullptr)
+	, prompt_(prompt)
+	, command_callback_(callback)
+{
+}
+
+void command_console::post_build(window& window)
+{
+	// Allow ESC to dismiss the console.
+	connect_signal_pre_key_press(window,
+		std::bind(&command_console::window_key_press_callback, this, _5));
+
+	input_ = find_widget<text_box>(&window, "input", false, true);
+
+	// Execute provided callback on ENTER press.
+	connect_signal_pre_key_press(*input_,
+		std::bind(&command_console::input_key_press_callback, this, _5));
+}
+
+void command_console::pre_show(window& window)
+{
+	find_widget<label>(&window, "prompt", false).set_label(prompt_);
+
+	window.keyboard_capture(input_);
+}
+
+void command_console::window_key_press_callback(const SDL_Keycode key)
+{
+	if(key == SDLK_ESCAPE) {
+		close();
+	}
+}
+
+void command_console::input_key_press_callback(const SDL_Keycode key)
+{
+	if(key == SDLK_RETURN || key == SDLK_KP_ENTER) {
+		// Execute callback.
+		if(command_callback_ != nullptr) {
+			command_callback_(input_->get_value());
+		}
+
+		// Dismiss dialog.
+		close();
+	} else if(key == SDLK_TAB) {
+		// TODO: implement
+	}
+}
+
+void command_console::close()
+{
+	// The modeless_dialog dtor will close the dialog.
+	singleton_.reset();
+}
+
+} // namespace dialogs
+} // namespace gui2
