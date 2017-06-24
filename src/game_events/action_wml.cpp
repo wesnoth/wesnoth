16671d19f3ecddd86cca17c7fdf367dc7e0651a3
Lari Nieminen <lari.nieminen@iki.fi>
2017-06-24 12:42:26 +0300

    Added support for a facing= key in [recall]
    This is necessary, as it is otherwise impossible to specify the facing of the recalled unit.

--- src/game_events/action_wml.cpp
+++ src/game_events/action_wml.cpp
@@ -484,6 +484,7 @@ WML_HANDLER_FUNCTION(recall,, cfg)
 							DBG_NG << "...valid location for the recall found. Recalling.\n";
 							avail.erase(u);	// Erase before recruiting, since recruiting can fire more events
 							actions::place_recruit(to_recruit, loc, leader->get_location(), 0, true,
+							                       map_location::parse_direction(cfg["facing"]),
 							                       cfg["show"].to_bool(true), cfg["fire_event"].to_bool(false),
 							                       true, true);
 							return;
@@ -499,8 +500,10 @@ WML_HANDLER_FUNCTION(recall,, cfg)
 						DBG_NG << "No usable leader found, but found usable location. Recalling.\n";
 						avail.erase(u);	// Erase before recruiting, since recruiting can fire more events
 						map_location null_location = map_location::null_location();
-						actions::place_recruit(to_recruit, loc, null_location, 0, true, cfg["show"].to_bool(true),
-						                       cfg["fire_event"].to_bool(false), true, true);
+						actions::place_recruit(to_recruit, loc, null_location, 0, true,
+						                       map_location::parse_direction(cfg["facing"]),
+						                       cfg["show"].to_bool(true), cfg["fire_event"].to_bool(false),
+						                       true, true);
 						return;
 					}
 				}
