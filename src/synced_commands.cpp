e42226151841ab338910cf223441fcc256cd1401
Charles Dang <exodia339@gmail.com>
2017-07-09 22:15:26 +1100

    Removed external calls to display::redraw_everything()

--- src/synced_commands.cpp
+++ src/synced_commands.cpp
@@ -581,7 +581,6 @@ SYNCED_COMMAND_HANDLER_FUNCTION(debug_turn_limit, child, use_undo, /*show*/, /*e
 	debug_notification(":turn_limit debug command was used during $player's turn");
 
 	resources::tod_manager->set_number_of_turns(child["turn_limit"].to_int(-1));
-	display::get_singleton()->redraw_everything();
 	return true;
 }
 
@@ -596,7 +595,6 @@ SYNCED_COMMAND_HANDLER_FUNCTION(debug_turn, child, use_undo, /*show*/, /*error_h
 	resources::tod_manager->set_turn(child["turn"].to_int(1), resources::gamedata);
 
 	game_display::get_singleton()->new_turn();
-	display::get_singleton()->redraw_everything();
 
 	return true;
 }
@@ -628,7 +626,6 @@ SYNCED_COMMAND_HANDLER_FUNCTION(debug_gold, child, use_undo, /*show*/, /*error_h
 	debug_notification(":gold debug command was used during $player's turn");
 
 	resources::controller->current_team().spend_gold(-child["gold"].to_int(0));
-	display::get_singleton()->redraw_everything();
 	return true;
 }
 
@@ -642,7 +639,6 @@ SYNCED_COMMAND_HANDLER_FUNCTION(debug_event, child, use_undo, /*show*/, /*error_
 	debug_notification(":throw debug command was used during $player's turn");
 
 	resources::controller->pump().fire(child["eventname"]);
-	display::get_singleton()->redraw_everything();
 
 	return true;
 }
@@ -661,7 +657,6 @@ SYNCED_COMMAND_HANDLER_FUNCTION(debug_fog, /*child*/, use_undo, /*show*/, /*erro
 	actions::recalculate_fog(current_team.side());
 
 	display::get_singleton()->recalculate_minimap();
-	display::get_singleton()->redraw_everything();
 
 	return true;
 }
@@ -680,7 +675,6 @@ SYNCED_COMMAND_HANDLER_FUNCTION(debug_shroud, /*child*/, use_undo, /*show*/, /*e
 	actions::clear_shroud(current_team.side());
 
 	display::get_singleton()->recalculate_minimap();
-	display::get_singleton()->redraw_everything();
 
 	return true;
 }
