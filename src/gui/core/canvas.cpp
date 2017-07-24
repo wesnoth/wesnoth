2f8c4eaccfa49d2017101cffd3bdbe6614346860
Charles Dang <exodia339@gmail.com>
2017-07-24 17:54:33 +1100

    GUI2/Canvas: allow the default draw routine (shape drawing) to be overridden
    Previously widgets that wanted to implement custom drawing behavior overrode styled_widget::impl_draw_background
    or impl_draw_foreground. Those functions in the base class simply called the canvas render methods. Some widgets
    such as the minimap ignored that rendered the contents on their own.
    
    This new method allows rendering directly to the canvas's texture, meaning all the caching and sizing is already
    handled by the canvas and doesn't need to be done by the widget; everything's always the right size and redrawn
    when necessary.

--- src/gui/core/canvas.cpp
+++ src/gui/core/canvas.cpp
@@ -1358,6 +1358,7 @@ void text_shape::draw(
 
 canvas::canvas()
 	: shapes_()
+	, draw_func_(nullptr)
 	, blur_depth_(0)
 	, w_(0)
 	, h_(0)
@@ -1372,6 +1373,7 @@ canvas::canvas()
 
 canvas::canvas(canvas&& c)
 	: shapes_(std::move(c.shapes_))
+	, draw_func_(c.draw_func_)
 	, blur_depth_(c.blur_depth_)
 	, w_(c.w_)
 	, h_(c.h_)
@@ -1440,11 +1442,15 @@ void canvas::draw(const bool force)
 
 	SDL_RenderClear(renderer_); // TODO: move to its own wrapper.
 
-	// Draw items.
-	for(auto& shape : shapes_) {
-		lg::scope_logger inner_scope_logging_object__(log_gui_draw, "Canvas: draw shape.");
+	// Draw items. TODO: make this cleaner
+	if(draw_func_ != nullptr) {
+		draw_func_(texture_);
+	} else {
+		for(auto& shape : shapes_) {
+			lg::scope_logger inner_scope_logging_object__(log_gui_draw, "Canvas: draw shape.");
 
-		shape->draw(w_, h_, renderer_, variables_);
+			shape->draw(w_, h_, renderer_, variables_);
+		}
 	}
 
 	// TODO: re-enable
