f730338222d7d8842f3eda3b56fce55e9a8334b3
Jyrki Vesterinen <sandgtx@gmail.com>
2017-08-28 20:38:28 +0300

    Fix build

--- src/game_initialization/lobby_info.cpp
+++ src/game_initialization/lobby_info.cpp
@@ -162,7 +162,7 @@ bool lobby_info::process_gamelist_diff(const config& data)
 		const std::string& diff_result = c[config::diff_track_attribute];
 		if(diff_result == "new" || diff_result == "modified") {
 			if(current_i == games_by_id_.end()) {
-				games_by_id_.emplace(game_id, new game_info(c, game_config_, installed_addons_));
+				games_by_id_.emplace(game_id, std::unique_ptr<game_info>(new game_info(c, game_config_, installed_addons_)));
 				continue;
 			}
 
