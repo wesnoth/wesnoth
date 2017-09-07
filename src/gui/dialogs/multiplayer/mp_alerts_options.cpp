wesnoth/wesnoth

commit e5b09de2c60627f9ec0ea543877b9ed6090c81d4

MP Alerts Options: simplify an overload disambiguation

src/gui/dialogs/multiplayer/mp_alerts_options.cpp
@@ -73,10 +73,7 @@ static toggle_button * setup_pref_toggle_button(const std::string & id, bool def
 		preferences::set(id, def);
 	}
 
-	//Needed to disambiguate overloaded function
-	void (*set) (const std::string &, bool) = &preferences::set;
-
-	connect_signal_mouse_left_click(*b, std::bind(set, id, std::bind(&toggle_button::get_value_bool, b)));
+	connect_signal_mouse_left_click(*b, std::bind([&, id]() { preferences::set(id, b->get_value_bool()); }));
 
 	return b;
 }

commit afc58c3ba9977cad9d2f79fbed374d8c794a92e6

MP Alert Options: fix crash caused by e5b09de2c606 (fixes bug #1945)
Accessing captured variable out of scope.

src/gui/dialogs/multiplayer/mp_alerts_options.cpp
@@ -73,7 +73,7 @@ static toggle_button * setup_pref_toggle_button(const std::string & id, bool def
 		preferences::set(id, def);
 	}
 
-	connect_signal_mouse_left_click(*b, std::bind([&, id]() { preferences::set(id, b->get_value_bool()); }));
+	connect_signal_mouse_left_click(*b, std::bind([b, id]() { preferences::set(id, b->get_value_bool()); }));
 
 	return b;
 }