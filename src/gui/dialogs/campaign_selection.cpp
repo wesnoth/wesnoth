4edb4e81146b607026ff95f270564ec3daf1d3dc
Charles Dang <exodia339@gmail.com>
2017-09-12 01:20:50 +1100

    GUI2/Campaign Selection: some formatting cleanup

--- src/gui/dialogs/campaign_selection.cpp
+++ src/gui/dialogs/campaign_selection.cpp
@@ -16,7 +16,6 @@
 
 #include "gui/dialogs/campaign_selection.hpp"
 
-#include "preferences/game.hpp"
 #include "gui/auxiliary/find_widget.hpp"
 #include "gui/dialogs/helper.hpp"
 #include "gui/widgets/button.hpp"
@@ -26,15 +25,16 @@
 #else
 #include "gui/widgets/listbox.hpp"
 #endif
-#include "gui/widgets/multimenu_button.hpp"
 #include "gui/widgets/multi_page.hpp"
+#include "gui/widgets/multimenu_button.hpp"
 #include "gui/widgets/scroll_label.hpp"
 #include "gui/widgets/settings.hpp"
+#include "gui/widgets/toggle_button.hpp"
 #include "gui/widgets/tree_view.hpp"
 #include "gui/widgets/tree_view_node.hpp"
-#include "gui/widgets/toggle_button.hpp"
 #include "gui/widgets/window.hpp"
 #include "lexical_cast.hpp"
+#include "preferences/game.hpp"
 #include "serialization/string_utils.hpp"
 
 #include "utils/functional.hpp"
@@ -45,7 +45,6 @@ namespace gui2
 {
 namespace dialogs
 {
-
 /*WIKI
  * @page = GUIWindowDefinitionWML
  * @order = 2_campaign_selection
@@ -89,14 +88,15 @@ REGISTER_DIALOG(campaign_selection)
 void campaign_selection::campaign_selected(window& window)
 {
 	tree_view& tree = find_widget<tree_view>(&window, "campaign_tree", false);
-
 	if(tree.empty()) {
 		return;
 	}
 
 	assert(tree.selected_item());
+
 	if(!tree.selected_item()->id().empty()) {
 		auto iter = std::find(page_ids_.begin(), page_ids_.end(), tree.selected_item()->id());
+
 		const int choice = iter - page_ids_.begin();
 		if(iter == page_ids_.end()) {
 			return;
@@ -104,57 +104,78 @@ void campaign_selection::campaign_selected(window& window)
 
 		multi_page& pages = find_widget<multi_page>(&window, "campaign_details", false);
 		pages.select_page(choice);
+
 		engine_.set_current_level(choice);
 	}
-
 }
 
 void campaign_selection::sort_campaigns(window& window, campaign_selection::CAMPAIGN_ORDER order, bool ascending)
 {
 	using level_ptr = ng::create_engine::level_ptr;
+
 	auto levels = engine_.get_levels_by_type_unfiltered(ng::level::TYPE::SP_CAMPAIGN);
+
 	switch(order) {
-		case RANK: // Already sorted by rank
-			if(!ascending) {
-				// This'll actually never happen, but who knows if that'll ever change...
-				std::reverse(levels.begin(), levels.end());
+	case RANK: // Already sorted by rank
+		// This'll actually never happen, but who knows if that'll ever change...
+		if(!ascending) {
+			std::reverse(levels.begin(), levels.end());
+		}
+
+		break;
+
+	case DATE:
+		std::sort(levels.begin(), levels.end(), [ascending](const level_ptr& a, const level_ptr& b) {
+			auto cpn_a = std::dynamic_pointer_cast<ng::campaign>(a);
+			auto cpn_b = std::dynamic_pointer_cast<ng::campaign>(b);
+
+			if(cpn_b == nullptr) {
+				return cpn_a != nullptr;
 			}
-			break;
-		case DATE:
-			std::sort(levels.begin(), levels.end(), [ascending](const level_ptr& a, const level_ptr& b) {
-				auto cpn_a = std::dynamic_pointer_cast<ng::campaign>(a), cpn_b = std::dynamic_pointer_cast<ng::campaign>(b);
-				if(cpn_b == nullptr) return cpn_a != nullptr;
-				if(cpn_a == nullptr) return false;
-				return ascending ? cpn_a->dates().first < cpn_b->dates().first : cpn_a->dates().first > cpn_b->dates().first;
-			});
-			break;
-		case NAME:
-			std::sort(levels.begin(), levels.end(), [ascending](const level_ptr& a, const level_ptr& b) {
-				int cmp = translation::icompare(a->name(), b->name());
-				return ascending ? cmp < 0 : cmp > 0;
-			});
-			break;
+
+			if(cpn_a == nullptr) {
+				return false;
+			}
+
+			return ascending
+				? cpn_a->dates().first < cpn_b->dates().first
+				: cpn_a->dates().first > cpn_b->dates().first;
+		});
+
+		break;
+
+	case NAME:
+		std::sort(levels.begin(), levels.end(), [ascending](const level_ptr& a, const level_ptr& b) {
+			const int cmp = translation::icompare(a->name(), b->name());
+			return ascending ? cmp < 0 : cmp > 0;
+		});
+
+		break;
 	}
 
 	tree_view& tree = find_widget<tree_view>(&window, "campaign_tree", false);
+
 	// Remember which campaign was selected...
 	std::string was_selected = tree.selected_item()->id();
+
 	tree.clear();
+
 	for(const auto& level : levels) {
 		add_campaign_to_tree(window, level->data());
 	}
 
 	if(!was_selected.empty()) {
-		tree_view_node& node = find_widget<tree_view_node>(&window, was_selected, false);
-		node.select_node();
+		find_widget<tree_view_node>(&window, was_selected, false).select_node();
 	}
 }
 
-void campaign_selection::toggle_sorting_selection(window& window, CAMPAIGN_ORDER order) {
+void campaign_selection::toggle_sorting_selection(window& window, CAMPAIGN_ORDER order)
+{
 	static bool force = false;
 	if(force) {
 		return;
 	}
+
 	if(current_sorting_ == order) {
 		if(currently_sorted_asc_) {
 			currently_sorted_asc_ = false;
@@ -168,16 +189,18 @@ void campaign_selection::toggle_sorting_selection(window& window, CAMPAIGN_ORDER
 	} else {
 		currently_sorted_asc_ = true;
 		current_sorting_ = order;
+
 		force = true;
+
 		if(order == NAME) {
-			toggle_button& sort_time = find_widget<toggle_button>(&window, "sort_time", false);
-			sort_time.set_value(0);
+			find_widget<toggle_button>(&window, "sort_time", false).set_value(0);
 		} else if(order == DATE) {
-			toggle_button& sort_name = find_widget<toggle_button>(&window, "sort_name", false);
-			sort_name.set_value(0);
+			find_widget<toggle_button>(&window, "sort_name", false).set_value(0);
 		}
+
 		force = false;
 	}
+
 	sort_campaigns(window, current_sorting_, currently_sorted_asc_);
 }
 
@@ -186,8 +209,7 @@ void campaign_selection::pre_show(window& window)
 	/***** Setup campaign tree. *****/
 	tree_view& tree = find_widget<tree_view>(&window, "campaign_tree", false);
 
-	tree.set_selection_change_callback(
-		std::bind(&campaign_selection::campaign_selected, this, std::ref(window)));
+	tree.set_selection_change_callback(std::bind(&campaign_selection::campaign_selected, this, std::ref(window)));
 
 	toggle_button& sort_name = find_widget<toggle_button>(&window, "sort_name", false);
 	toggle_button& sort_time = find_widget<toggle_button>(&window, "sort_time", false);
@@ -203,7 +225,7 @@ void campaign_selection::pre_show(window& window)
 	/***** Setup campaign details. *****/
 	multi_page& pages = find_widget<multi_page>(&window, "campaign_details", false);
 
-	for(const auto & level : engine_.get_levels_by_type_unfiltered(ng::level::TYPE::SP_CAMPAIGN)) {
+	for(const auto& level : engine_.get_levels_by_type_unfiltered(ng::level::TYPE::SP_CAMPAIGN)) {
 		const config& campaign = level->data();
 
 		/*** Add tree item ***/
@@ -235,7 +257,6 @@ void campaign_selection::pre_show(window& window)
 	multimenu_button& mods_menu = find_widget<multimenu_button>(&window, "mods_menu", false);
 
 	if(!engine_.get_const_extras_by_type(ng::create_engine::MOD).empty()) {
-
 		std::vector<config> mod_menu_values;
 		std::vector<std::string> enabled = engine_.active_mods();
 
@@ -329,7 +350,8 @@ void campaign_selection::post_show(window& window)
 
 void campaign_selection::mod_toggled(window& window)
 {
-	boost::dynamic_bitset<> new_mod_states = find_widget<multimenu_button>(&window, "mods_menu", false).get_toggle_states();
+	boost::dynamic_bitset<> new_mod_states =
+		find_widget<multimenu_button>(&window, "mods_menu", false).get_toggle_states();
 
 	// Get a mask of any mods that were toggled, regardless of new state
 	mod_states_ = mod_states_ ^ new_mod_states;
