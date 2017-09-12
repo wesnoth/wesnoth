wesnoth/wesnoth

commit d40a9e930b9fdf1095edf45f25885773654d0a4c

Fix [kill]ed units flashing briefly after death animation

data/lua/wml/kill.lua
@@ -70,7 +70,7 @@ function wesnoth.wml_actions.kill(cfg)
 			end
 			anim:run()
 		end
-		wesnoth.wml_actions.redraw{}
+		-- wesnoth.wml_actions.redraw{}
 
 		if can_fire then
 			wesnoth.fire_event("die", death_loc, killer_loc)
@@ -84,6 +84,8 @@ function wesnoth.wml_actions.kill(cfg)
 		end
 		-- Test that it's valid (and still on the map) first, in case the event erased (or extracted) it.
 		if unit.valid == "map" then unit:erase() end
+
+		wesnoth.wml_actions.redraw{}
 
 		number_killed = number_killed + 1
 	end

commit 3b5880f5fb07fdc986ea8efabf4918817cb16d6f

Fix #1980: [modify_unit] makes the unit disappear for a frame or two
Regression from commit d40a9e930b9fdf1095edf45f25885773654d0a4c.

In that commit I removed a redraw after the death animation of a killed
unit has been played, but just to be safe, I then added a redraw after the
unit has been erased from the map. Turns out it's not a good idea because
[modify_unit] operates by killing and recreating the unit.

This commit simply removes the redraw after erasing. Hopefully we aren't
relying on [kill] triggering a redraw somewhere.

[ci skip]

data/lua/wml/kill.lua
@@ -85,7 +85,7 @@ function wesnoth.wml_actions.kill(cfg)
 		-- Test that it's valid (and still on the map) first, in case the event erased (or extracted) it.
 		if unit.valid == "map" then unit:erase() end
 
-		wesnoth.wml_actions.redraw{}
+		-- wesnoth.wml_actions.redraw{}
 
 		number_killed = number_killed + 1
 	end