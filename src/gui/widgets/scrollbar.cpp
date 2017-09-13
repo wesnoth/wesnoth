wesnoth/wesnoth

commit acea15c312f178b2b6fe4556ca6b190b00866557

GUI2: cleaned up NOTIFY_MODIFIED event triggers
These should be in value setters instead of callback handlers so the events can be fired even when
changes are made programmatically.

Includes a change to the listbox to prevent infinite event recursion when handling toggle panels.
This expands the change from 0dbf686c4ffd to all selectable items.

Also called menu_button::set_selected in menu_button click handler to allow the event firing to be
done in a central place.

src/gui/widgets/scrollbar.cpp
@@ -104,8 +104,6 @@ void scrollbar_base::scroll(const scroll_mode scroll)
 		default:
 			assert(false);
 	}
-
-	fire(event::NOTIFY_MODIFIED, *this, nullptr);
 }
 
 void scrollbar_base::place(const point& origin, const point& size)
@@ -153,6 +151,8 @@ void scrollbar_base::set_item_position(const unsigned item_position)
 	update_canvas();
 
 	assert(item_position_ <= item_count_ - visible_items_);
+
+	fire(event::NOTIFY_MODIFIED, *this, nullptr);
 }
 
 void scrollbar_base::update_canvas()
@@ -399,11 +399,9 @@ void scrollbar_base::signal_handler_left_button_down(const event::ui_event event
 
 	if(bar == -1) {
 		scroll(HALF_JUMP_BACKWARDS);
-		fire(event::NOTIFY_MODIFIED, *this, nullptr);
 		// positioner_moved_notifier_.notify();
 	} else if(bar == 1) {
 		scroll(HALF_JUMP_FORWARD);
-		fire(event::NOTIFY_MODIFIED, *this, nullptr);
 		// positioner_moved_notifier_.notify();
 	} else {
 		assert(bar == 0);

src/gui/widgets/listbox.cpp
@@ -284,12 +284,12 @@ void listbox::list_item_clicked(widget& caller)
 
 	for(size_t i = 0; i < generator_->get_item_count(); ++i) {
 		if(generator_->item(i).has_widget(caller)) {
-			toggle_button* checkbox = dynamic_cast<toggle_button*>(&caller);
+			selectable_item* selectable = dynamic_cast<selectable_item*>(&caller);
 
-			if(checkbox != nullptr) {
-				generator_->select_item(i, checkbox->get_value_bool());
+			if(selectable != nullptr) {
+				generator_->select_item(i, selectable->get_value_bool());
 			} else {
-				generator_->toggle_item(i);
+				// TODO: only selectable items are allowed right now. Should we error here?
 			}
 
 			fire(event::NOTIFY_MODIFIED, *this, nullptr);

src/gui/widgets/menu_button.cpp
@@ -142,17 +142,13 @@ void menu_button::signal_handler_left_button_click(const event::ui_event event, 
 
 		const int selected = droplist.selected_item();
 
-		// Saftey check. If the user clicks a selection in the dropdown and moves their mouse away too
+		// Safety check. If the user clicks a selection in the dropdown and moves their mouse away too
 		// quickly, selected_ could be set to -1. This returns in that case, preventing crashes.
 		if(selected < 0) {
 			return;
 		}
 
-		selected_ = selected;
-
-		this->set_label(values_[selected_]["label"]);
-
-		fire(event::NOTIFY_MODIFIED, *this, nullptr);
+		set_selected(selected);
 
 		if(retval_ != 0) {
 			if(window* window = get_window()) {
@@ -180,6 +176,8 @@ void menu_button::set_values(const std::vector<::config>& values, int selected)
 	selected_ = selected;
 
 	set_label(values_[selected_]["label"]);
+
+	// fire(event::NOTIFY_MODIFIED, *this, nullptr); TODO: should this fire here?
 }
 
 void menu_button::set_selected(int selected)
@@ -194,6 +192,8 @@ void menu_button::set_selected(int selected)
 	selected_ = selected;
 
 	set_label(values_[selected_]["label"]);
+
+	fire(event::NOTIFY_MODIFIED, *this, nullptr);
 }
 
 // }---------- DEFINITION ---------{

src/gui/widgets/multimenu_button.cpp
@@ -241,6 +241,8 @@ void multimenu_button::set_values(const std::vector<::config>& values)
 	toggle_states_.reset();
 
 	set_label(_("multimenu^None Selected"));
+
+	// fire(event::NOTIFY_MODIFIED, *this, nullptr); TODO: should this fire here?
 }
 
 // }---------- DEFINITION ---------{

src/gui/widgets/toggle_panel.cpp
@@ -166,6 +166,14 @@ void toggle_panel::set_value(const unsigned selected)
 	}
 	state_num_ = selected % num_states();
 	set_is_dirty(true);
+
+	// Check for get_window() is here to prevent the callback from
+	// being called when the initial value is set.
+	if(!get_window()) {
+		return;
+	}
+
+	fire(event::NOTIFY_MODIFIED, *this, nullptr);
 }
 
 void toggle_panel::set_retval(const int retval)
@@ -257,8 +265,6 @@ void toggle_panel::signal_handler_left_button_click(const event::ui_event event,
 	sound::play_UI_sound(settings::sound_toggle_panel_click);
 
 	set_value(get_value() + 1);
-
-	fire(event::NOTIFY_MODIFIED, *this, nullptr);
 
 	handled = true;
 }

commit 2b3d09c177d3133a702b4b33e69a456ecf748711

GUI2/Scrollbar: fixed regression caused by acea15c312
Fixes cases where selecting a listbox item caused the viewport to jump to the top w/o a scroll.
Should fix #1979 too.

src/gui/widgets/scrollbar.cpp
@@ -104,6 +104,16 @@ void scrollbar_base::scroll(const scroll_mode scroll)
 		default:
 			assert(false);
 	}
+
+	/*
+	 * @note I'm not sure if this should be in @ref set_item_position, but right now it
+	 * causes weird viewport issues with listboxes. Selecting certain items makes the
+	 * viewport to jump to the top, likely having to do with @ref recalculate dispatching
+	 * an event (since it calls set_item_position). This seems like a safe spot for now.
+	 *
+	 * -- vultraz, 2017-09-13
+	 */
+	fire(event::NOTIFY_MODIFIED, *this, nullptr);
 }
 
 void scrollbar_base::place(const point& origin, const point& size)
@@ -152,7 +162,10 @@ void scrollbar_base::set_item_position(const unsigned item_position)
 
 	assert(item_position_ <= item_count_ - visible_items_);
 
+#if 0
+	/** See comment in @ref scroll for more info. */
 	fire(event::NOTIFY_MODIFIED, *this, nullptr);
+#endif
 }
 
 void scrollbar_base::update_canvas()