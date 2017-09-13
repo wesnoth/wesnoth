wesnoth/wesnoth

commit c5dc04c90001b95fbd3e06be3a36680371a01e53

Handle context menus on mouse up instead of mouse down
This is more consistent behavior with apps in general.

src/mouse_handler_base.hpp
@@ -135,7 +135,10 @@ public:
 	 * @returns true when the click should not process the event further.
 	 * This means do not treat the call as a start of drag movement.
 	 */
-	virtual bool right_click(int x, int y, const bool browse);
+	virtual bool right_click(int /*x*/, int /*y*/, const bool /*browse*/)
+	{
+		return true;
+	}
 
 	/**
 	 * Called in the default right_click when the context menu is about to
@@ -158,9 +161,7 @@ public:
 	}
 
 	/** Called when the right mouse button is up. */
-	virtual void right_mouse_up(int /*x*/, int /*y*/, const bool /*browse*/)
-	{
-	}
+	virtual void right_mouse_up(int /*x*/, int /*y*/, const bool /*browse*/);
 
 	/** Called when the mouse wheel is scrolled up. */
 	virtual void mouse_wheel_up(int /*x*/, int /*y*/, const bool /*browse*/)

src/controller_base.cpp
@@ -120,10 +120,6 @@ void controller_base::handle_event(const SDL_Event& event)
 	case SDL_MOUSEBUTTONDOWN:
 		process_keydown_event(event);
 		mh_base.mouse_press(event.button, is_browsing());
-		if(mh_base.get_show_menu()) {
-			show_menu(get_display().get_theme().context_menu()->items(), event.button.x, event.button.y, true,
-					get_display());
-		}
 		hotkey::mbutton_event(event, get_hotkey_command_executor());
 		break;
 

src/mouse_handler_base.cpp
@@ -123,7 +123,7 @@ bool mouse_handler_base::mouse_motion_default(int x, int y, bool /*update*/)
 		if((dragging_left_  && (SDL_GetMouseState(&mx, &my) & SDL_BUTTON_LEFT)  != 0) ||
 		   (dragging_right_ && (SDL_GetMouseState(&mx, &my) & SDL_BUTTON_RIGHT) != 0))
 		{
-			const double drag_distance = 
+			const double drag_distance =
 				std::pow(static_cast<double>(drag_from_x_ - mx), 2) +
 				std::pow(static_cast<double>(drag_from_y_ - my), 2);
 
@@ -276,10 +276,10 @@ void mouse_handler_base::mouse_wheel(int scrollx, int scrolly, bool browse)
 	}
 }
 
-bool mouse_handler_base::right_click(int x, int y, const bool browse)
+void mouse_handler_base::right_mouse_up(int x, int y, const bool browse)
 {
 	if(!right_click_show_menu(x, y, browse)) {
-		return false;
+		return;
 	}
 
 	gui().draw(); // redraw highlight (and maybe some more)
@@ -290,8 +290,6 @@ bool mouse_handler_base::right_click(int x, int y, const bool browse)
 	} else {
 		WRN_DP << "no context menu found..." << std::endl;
 	}
-
-	return true;
 }
 
 void mouse_handler_base::init_dragging(bool& dragging_flag)

commit d419caf24cdf6bd0a6fc4e68fe5f70d307cdb29d

Mouse Handler Base: fix right-click behavior regression from c5dc04c900 (fixes #1967)
The above commit moved context menu handling from right_click to right_mouse_up. However, that removed
the early-exit if right_click_show_menu returned false. It seems some areas of the code (in this case,
the editor in editor_controller::right_click - don't ask me exactly how this leads to editor_controller::mouse_motion
where the codepath that caused the exception in the above bug is actually called, I didn't bother to trace it)
relied on that early exit to control behavior.

Making right_click return the value of right_click_show_menu by default fixes things.

src/mouse_handler_base.hpp
@@ -135,9 +135,9 @@ public:
 	 * @returns true when the click should not process the event further.
 	 * This means do not treat the call as a start of drag movement.
 	 */
-	virtual bool right_click(int /*x*/, int /*y*/, const bool /*browse*/)
+	virtual bool right_click(int x, int y, const bool browse)
 	{
-		return true;
+		return right_click_show_menu(x, y, browse);
 	}
 
 	/**