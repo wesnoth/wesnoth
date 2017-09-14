615c57b8163dc6df8075baf98bc944c29cddb4b6
Charles Dang <exodia339@gmail.com>
2017-09-15 10:41:10 +1100

    Campaign Server: reformat licensing terms prompt
    Also made it a static string since it's long a'f and it doesn't make sense to recreate it every time its needed.

--- src/campaign_server/campaign_server.cpp
+++ src/campaign_server/campaign_server.cpp
@@ -587,8 +587,20 @@ void server::handle_request_terms(const server::request& req)
 		return;
 	}
 
+	// TODO: possibly move to server.cfg
+	static const std::string terms = R"""(
+		All content within add-ons uploaded to this server must be licensed under the terms of the GNU General Public License (GPL), with the sole exception of graphics and audio explicitly denoted as released under a Creative Commons license either in
+
+		A) a combined toplevel file, e.g. `add-ons/My_Addon/ART_LICENSE`, OR
+		B) a file with the same path as the asset with `.license` appended, e.g. `add-ons/My_Addon/images/units/axeman.png.license`.
+
+		By uploading content to this server, you certify that you have the right
+
+		A) to release all included art and audio explicitly denoted with a Creative Commons license in the proscribed manner under that license, AND
+		B) to release all other included content under the terms of the GPL; and that you choose to do so.)""";
+
 	LOG_CS << "sending terms " << req.addr << "\n";
-	send_message("All content within add-ons uploaded to this server must be licensed under the terms of the GNU General Public License (GPL), with the sole exception of graphics and audio explicitly denoted as released under a Creative Commons license either in a) a combined toplevel file, e.g. `add-ons/My_Addon/ART_LICENSE`, or b) a file with the same path as the asset with `.license` appended, e.g. `add-ons/My_Addon/images/units/axeman.png.license`. By uploading content to this server, you certify that you have the right a) to release all included art and audio explicitly denoted with a Creative Commons license in the proscribed manner under that license, and b) to release all other included content under the terms of the GPL; and that you choose to do so.", req.sock);
+	send_message(terms, req.sock);
 	LOG_CS << " Done\n";
 }
 
