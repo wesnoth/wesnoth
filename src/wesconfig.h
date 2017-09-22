bc86ce762dc3ce9a3f5954cf31cdd9709190cf35
Charles Dang <exodia339@gmail.com>
2017-09-22 14:22:20 -0400

    Pre-release version bump
    [ci skip]
    
    A serious issue was discovered in the 1.13.9 release, so we're releasing 1.13.10 now.

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -40,7 +40,7 @@
   #undef VERSION
 #endif
 
-#define VERSION "1.13.9+dev"
+#define VERSION "1.13.10"
 
 // Used for the Windows executables' version info resource.
 #define RC_VERSION_MAJOR        1
