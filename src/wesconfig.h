e47ea7fded7221c40a59bc96073fb13b03182823
Charles Dang <exodia339@gmail.com>
2017-09-21 23:05:15 -0400

    Pre-release version bump
    [ci skip]

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -40,7 +40,7 @@
   #undef VERSION
 #endif
 
-#define VERSION "1.13.8+dev"
+#define VERSION "1.13.9"
 
 // Used for the Windows executables' version info resource.
 #define RC_VERSION_MAJOR        1
