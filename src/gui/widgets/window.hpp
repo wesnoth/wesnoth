wesnoth/wesnoth

commit 2416f10dddaebd1629e2da711b878b1082bb60e8

GUI2/Window: minor cleanup
Simplified a setter and added some members to the ctor initializer list.

src/gui/widgets/window.hpp
@@ -463,9 +463,8 @@ public:
 		if(it != linked_size_.end()) {
 			return point(it->second.width, it->second.height);
 		}
-		else {
-			return point(-1, -1);
-		}
+
+		return point(-1, -1);
 	}
 
 	/**
@@ -482,13 +481,7 @@ public:
 
 	void set_exit_hook_ok_only(std::function<bool(window&)> func)
 	{
-		exit_hook_ = [func](window& w)->bool {
-			if(w.get_retval() == OK) {
-				return func(w);
-			}
-
-			return true;
-		};
+		exit_hook_ = [func](window& w)->bool { return (w.get_retval() == OK && func(w)) || true; };
 	}
 
 	/**
@@ -821,7 +814,7 @@ private:
 	void signal_handler_request_placement(const event::ui_event event,
 										  bool& handled);
 
-	std::function<bool(window&)> exit_hook_ = [](window&)->bool { return true; };
+	std::function<bool(window&)> exit_hook_;
 	std::function<void()> callback_next_draw_;
 };
 

src/gui/widgets/window.cpp
@@ -340,8 +340,9 @@ window::window(CVideo& video,
 #ifdef DEBUG_WINDOW_LAYOUT_GRAPHS
 	, debug_layout_(new debug_layout_graph(this))
 #endif
-	, event_distributor_(
-			  new event::distributor(*this, event::dispatcher::front_child))
+	, event_distributor_(new event::distributor(*this, event::dispatcher::front_child))
+	, exit_hook_([](window&)->bool { return true; })
+	, callback_next_draw_(nullptr)
 {
 	// We load the config in here as exception.
 	// Our caller did update the screen size so no need for us to do that again.

commit d957326828c7b037a15c9b057e6862566367499f

GUI2/Window: fixed behavior regression from 2416f10ddd
The on-OK-only exit hook wrapper was always returning true.

src/gui/widgets/window.hpp
@@ -455,7 +455,7 @@ public:
 
 	void set_exit_hook_ok_only(std::function<bool(window&)> func)
 	{
-		exit_hook_ = [func](window& w)->bool { return (w.get_retval() == OK && func(w)) || true; };
+		exit_hook_ = [func](window& w)->bool { return w.get_retval() != OK || func(w); };
 	}
 
 	/**