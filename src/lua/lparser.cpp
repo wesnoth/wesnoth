217d2e1a15a777085bf7769f0b85d8a4786580f2
Charles Dang <exodia339@gmail.com>
2017-10-09 13:15:18 +1100

    Apply Lua.org patch
    Wrong code generated for a 'goto' followed by a label inside an 'if'.
    
    reported by 云风 on 13 Apr 2017. existed since 5.2.
    
    Example:
        See https://www.lua.org/bugs.html#5.3.4-1.

--- src/lua/lparser.cpp
+++ src/lua/lparser.cpp
@@ -1392,7 +1392,7 @@ static void test_then_block (LexState *ls, int *escapelist) {
     luaK_goiffalse(ls->fs, &v);  /* will jump to label if condition is true */
     enterblock(fs, &bl, 0);  /* must enter block before 'goto' */
     gotostat(ls, v.t);  /* handle goto/break */
-    skipnoopstat(ls);  /* skip other no-op statements */
+    while (testnext(ls, ';')) {}  /* skip semicolons */
     if (block_follow(ls, 0)) {  /* 'goto' is the entire block? */
       leaveblock(fs);
       return;  /* and that is it */
