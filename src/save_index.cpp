2020b24e6545f559e98f892fc182a270451f13b9
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2017-10-22 11:18:05 +0200

    Update save_index.cpp

--- src/save_index.cpp
+++ src/save_index.cpp
@@ -405,9 +405,11 @@ void extract_summary_from_config(config& cfg_save, config& cfg_summary)
 				color_options_ = game_config::default_colors;
 				color_id_ = color_options_[color_];
 				
-				if(u["color"].to_int()){
-				color_ = u["color"].to_int() - 1;
-				color_id_ = color_options_[color_];
+				if (!cfg[ "color" ]. empty ()) {
+					if(u["color"].to_int()){
+						color_ = u["color"].to_int() - 1;
+						color_id_ = color_options_[color_];
+					}
 				}
 			
 
