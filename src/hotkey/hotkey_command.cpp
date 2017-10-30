b43194ba2e8bf69efc20dd4db9739402cebee39a
josteph <josteph@fastmail.com>
2017-10-30 01:34:44 +0000

    Fix #2135: Honour the show_all_units_in_help preference during scenarios
    Adding this overload removes a silent type conversion from 'enum scope'
    to 'hk_scopes' (std::bitset<3>) in the src/help/help_impl.cpp callsite.

--- src/hotkey/hotkey_command.cpp
+++ src/hotkey/hotkey_command.cpp
@@ -320,6 +320,11 @@ void set_active_scopes(hk_scopes s)
 	scope_active_ = s;
 }
 
+bool is_scope_active(scope s)
+{
+	return scope_active_[s];
+}
+
 bool is_scope_active(hk_scopes s)
 {
 	// s is a copy because we need one
