bf8758afde020c1edfacf320c5ab3058acbb41fd
Gregory A Lundberg <gregory_lundberg@gmx.com>
2017-11-06 13:43:18 -0600

    Tell Travis about Boost unit test name change

--- utils/travis/test_executor.sh
+++ utils/travis/test_executor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-gdb -q -batch -return-child-result -ex "run" -ex "thread apply all bt" -ex "quit" --args ./test
+gdb -q -batch -return-child-result -ex "run" -ex "thread apply all bt" -ex "quit" --args ./boost_unit_tests
