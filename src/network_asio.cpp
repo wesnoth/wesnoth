bc3c79a3e05bd9e189c22031f1f4d6d4f6b836ed
Charles Dang <exodia339@gmail.com>
2017-11-16 16:42:07 +1100

    Campaignd/Network Asio: formatting cleanup

--- src/network_asio.cpp
+++ src/network_asio.cpp
@@ -12,12 +12,14 @@
    See the COPYING file for more details.
 */
 
-#include <deque>
-#include "utils/functional.hpp"
-#include <cstdint>
-#include "log.hpp"
 #include "network_asio.hpp"
+
+#include "log.hpp"
 #include "serialization/parser.hpp"
+#include "utils/functional.hpp"
+
+#include <cstdint>
+#include <deque>
 
 static lg::log_domain log_network("network");
 #define DBG_NW LOG_STREAM(debug, log_network)
@@ -25,8 +27,8 @@ static lg::log_domain log_network("network");
 #define WRN_NW LOG_STREAM(warn, log_network)
 #define ERR_NW LOG_STREAM(err, log_network)
 
-namespace network_asio {
-
+namespace network_asio
+{
 using boost::system::system_error;
 
 connection::connection(const std::string& host, const std::string& service)
@@ -44,46 +46,38 @@ connection::connection(const std::string& host, const std::string& service)
 	, bytes_read_(0)
 {
 	resolver_.async_resolve(
-		boost::asio::ip::tcp::resolver::query(host, service),
-		std::bind(&connection::handle_resolve, this, _1, _2)
-		);
+		boost::asio::ip::tcp::resolver::query(host, service), std::bind(&connection::handle_resolve, this, _1, _2));
+
 	LOG_NW << "Resolving hostname: " << host << '\n';
 }
 
-void connection::handle_resolve(
-		const boost::system::error_code& ec,
-		resolver::iterator iterator
-		)
+void connection::handle_resolve(const boost::system::error_code& ec, resolver::iterator iterator)
 {
-	if(ec)
+	if(ec) {
 		throw system_error(ec);
+	}
 
 	connect(iterator);
 }
 
 void connection::connect(resolver::iterator iterator)
 {
-	socket_.async_connect(*iterator, std::bind(
-		&connection::handle_connect, this, _1, iterator)
-		);
+	socket_.async_connect(*iterator, std::bind(&connection::handle_connect, this, _1, iterator));
 	LOG_NW << "Connecting to " << iterator->endpoint().address() << '\n';
 }
 
-void connection::handle_connect(
-		const boost::system::error_code& ec,
-		resolver::iterator iterator
-		)
+void connection::handle_connect(const boost::system::error_code& ec, resolver::iterator iterator)
 {
 	if(ec) {
-		WRN_NW << "Failed to connect to " <<
-			iterator->endpoint().address() << ": " <<
-			ec.message() << '\n';
+		WRN_NW << "Failed to connect to " << iterator->endpoint().address() << ": " << ec.message() << '\n';
 		socket_.close();
+
 		if(++iterator == resolver::iterator()) {
 			ERR_NW << "Tried all IPs. Giving up" << std::endl;
 			throw system_error(ec);
-		} else
+		} else {
 			connect(iterator);
+		}
 	} else {
 		LOG_NW << "Connected to " << iterator->endpoint().address() << '\n';
 		handshake();
@@ -93,22 +87,20 @@ void connection::handle_connect(
 void connection::handshake()
 {
 	static const uint32_t handshake = 0;
-	boost::asio::async_write(socket_,
-		boost::asio::buffer(reinterpret_cast<const char*>(&handshake), 4),
-		std::bind(&connection::handle_write, this, _1, _2)
-		);
-	boost::asio::async_read(socket_,
-		boost::asio::buffer(&handshake_response_.binary, 4),
-		std::bind(&connection::handle_handshake, this, _1)
-		);
+
+	boost::asio::async_write(socket_, boost::asio::buffer(reinterpret_cast<const char*>(&handshake), 4),
+		std::bind(&connection::handle_write, this, _1, _2));
+
+	boost::asio::async_read(socket_, boost::asio::buffer(&handshake_response_.binary, 4),
+		std::bind(&connection::handle_handshake, this, _1));
 }
 
-void connection::handle_handshake(
-		const boost::system::error_code& ec
-		)
+void connection::handle_handshake(const boost::system::error_code& ec)
 {
-	if(ec)
+	if(ec) {
 		throw system_error(ec);
+	}
+
 	done_ = true;
 }
 
@@ -119,20 +111,21 @@ void connection::transfer(const config& request, config& response)
 
 	std::ostream os(&write_buf_);
 	write_gz(os, request);
+
 	bytes_to_write_ = write_buf_.size() + 4;
 	bytes_written_ = 0;
 	payload_size_ = htonl(bytes_to_write_ - 4);
+
 	boost::asio::streambuf::const_buffers_type gzipped_data = write_buf_.data();
 	std::deque<boost::asio::const_buffer> bufs(gzipped_data.begin(), gzipped_data.end());
+
 	bufs.push_front(boost::asio::buffer(reinterpret_cast<const char*>(&payload_size_), 4));
-	boost::asio::async_write(socket_, bufs,
-		std::bind(&connection::is_write_complete, this, _1, _2),
-		std::bind(&connection::handle_write, this, _1, _2)
-		);
-	boost::asio::async_read(socket_, read_buf_,
-		std::bind(&connection::is_read_complete, this, _1, _2),
-		std::bind(&connection::handle_read, this, _1, _2, std::ref(response))
-		);
+
+	boost::asio::async_write(socket_, bufs, std::bind(&connection::is_write_complete, this, _1, _2),
+		std::bind(&connection::handle_write, this, _1, _2));
+
+	boost::asio::async_read(socket_, read_buf_, std::bind(&connection::is_read_complete, this, _1, _2),
+		std::bind(&connection::handle_read, this, _1, _2, std::ref(response)));
 }
 
 void connection::cancel()
@@ -140,73 +133,73 @@ void connection::cancel()
 	if(socket_.is_open()) {
 		boost::system::error_code ec;
 		socket_.cancel(ec);
+
 		if(ec) {
 			WRN_NW << "Failed to cancel network operations: " << ec.message() << std::endl;
 		}
 	}
 }
 
-std::size_t connection::is_write_complete(
-		const boost::system::error_code& ec,
-		std::size_t bytes_transferred
-		)
-{
-	if(ec)
-		throw system_error(ec);
-	bytes_written_ = bytes_transferred;
-	return bytes_to_write_ - bytes_transferred;
-}
-
-void connection::handle_write(
-	const boost::system::error_code& ec,
-	std::size_t bytes_transferred
-	)
-{
-	DBG_NW << "Written " << bytes_transferred << " bytes.\n";
-	write_buf_.consume(bytes_transferred);
-	if(ec)
-		throw system_error(ec);
-}
-
-std::size_t connection::is_read_complete(
-		const boost::system::error_code& ec,
-		std::size_t bytes_transferred
-		)
+std::size_t connection::is_write_complete(const boost::system::error_code& ec, std::size_t bytes_transferred)
 {
 	if(ec) {
 		throw system_error(ec);
 	}
-	bytes_read_ = bytes_transferred;
-	if(bytes_transferred < 4) {
-		return 4;
-	} else {
-		if(!bytes_to_read_) {
-			std::istream is(&read_buf_);
-			union { char binary[4]; uint32_t num; } data_size;
-			is.read(data_size.binary, 4);
-			bytes_to_read_ = ntohl(data_size.num) + 4;
-			//Close immediately if we receive an invalid length
-			if (bytes_to_read_ < 4)
-				bytes_to_read_ = bytes_transferred;
-		}
-		return bytes_to_read_ - bytes_transferred;
+
+	bytes_written_ = bytes_transferred;
+	return bytes_to_write_ - bytes_transferred;
+}
+
+void connection::handle_write(const boost::system::error_code& ec, std::size_t bytes_transferred)
+{
+	DBG_NW << "Written " << bytes_transferred << " bytes.\n";
+	write_buf_.consume(bytes_transferred);
+
+	if(ec) {
+		throw system_error(ec);
 	}
 }
 
-void connection::handle_read(
-	const boost::system::error_code& ec,
-	std::size_t bytes_transferred,
-	config& response
-	)
+std::size_t connection::is_read_complete(const boost::system::error_code& ec, std::size_t bytes_transferred)
+{
+	if(ec) {
+		throw system_error(ec);
+	}
+
+	bytes_read_ = bytes_transferred;
+	if(bytes_transferred < 4) {
+		return 4;
+	}
+
+	if(!bytes_to_read_) {
+		std::istream is(&read_buf_);
+		data_union data_size;
+
+		is.read(data_size.binary, 4);
+		bytes_to_read_ = ntohl(data_size.num) + 4;
+
+		// Close immediately if we receive an invalid length
+		if(bytes_to_read_ < 4) {
+			bytes_to_read_ = bytes_transferred;
+		}
+	}
+
+	return bytes_to_read_ - bytes_transferred;
+}
+
+void connection::handle_read(const boost::system::error_code& ec, std::size_t bytes_transferred, config& response)
 {
 	DBG_NW << "Read " << bytes_transferred << " bytes.\n";
+
 	bytes_to_read_ = 0;
 	bytes_to_write_ = 0;
 	done_ = true;
-	if(ec && ec != boost::asio::error::eof)
+
+	if(ec && ec != boost::asio::error::eof) {
 		throw system_error(ec);
+	}
+
 	std::istream is(&read_buf_);
 	read_gz(response, is);
 }
-
 }
--- src/network_asio.hpp
+++ src/network_asio.hpp
@@ -15,34 +15,113 @@
 #pragma once
 
 #ifdef _WIN32
-# define BOOST_ASIO_DISABLE_IOCP
-# ifdef INADDR_ANY
-#  undef INADDR_ANY
-# endif
-# ifdef INADDR_BROADCAST
-#  undef INADDR_BROADCAST
-# endif
-# ifdef INADDR_NONE
-#  undef INADDR_NONE
-# endif
+
+#define BOOST_ASIO_DISABLE_IOCP
+
+#ifdef INADDR_ANY
+#undef INADDR_ANY
 #endif
 
-#include <boost/asio.hpp>
+#ifdef INADDR_BROADCAST
+#undef INADDR_BROADCAST
+#endif
+
+#ifdef INADDR_NONE
+#undef INADDR_NONE
+#endif
+
+#endif
+
 #include "exceptions.hpp"
+
+#include <boost/asio.hpp>
 
 class config;
 
-namespace network_asio {
-
+namespace network_asio
+{
 struct error : public game::error
 {
-	error(const boost::system::error_code& error) : game::error(error.message()) {}
+	error(const boost::system::error_code& error)
+		: game::error(error.message())
+	{
+	}
+};
+
+union data_union {
+	char binary[4];
+	uint32_t num;
 };
 
 /** A class that represents a TCP/IP connection. */
 class connection
 {
+public:
+	/**
+	 * Constructor.
+	 *
+	 * @param host    Name of the host to connect to
+	 * @param service Service identifier such as "80" or "http"
+	 */
+	connection(const std::string& host, const std::string& service);
+
+	void transfer(const config& request, config& response);
+
+	/** Handle all pending asynchronous events and return */
+	std::size_t poll()
+	{
+		try {
+			return io_service_.poll();
+		} catch(const boost::system::system_error& err) {
+			if(err.code() == boost::asio::error::operation_aborted) {
+				return 1;
+			}
+
+			throw error(err.code());
+		}
+	}
+
+	/**
+	 * Run asio's event loop
+	 *
+	 * Handle asynchronous events blocking until all asynchronous operations have finished.
+	 */
+	void run()
+	{
+		io_service_.run();
+	}
+
+	void cancel();
+
+	/** True if connected and no high-level operation is in progress */
+	bool done() const
+	{
+		return done_;
+	}
+
+	std::size_t bytes_to_write() const
+	{
+		return bytes_to_write_;
+	}
+
+	std::size_t bytes_written() const
+	{
+		return bytes_written_;
+	}
+
+	std::size_t bytes_to_read() const
+	{
+		return bytes_to_read_;
+	}
+
+	std::size_t bytes_read() const
+	{
+		return bytes_read_;
+	}
+
+private:
 	boost::asio::io_service io_service_;
+
 	typedef boost::asio::ip::tcp::resolver resolver;
 	resolver resolver_;
 
@@ -54,98 +133,27 @@ class connection
 	boost::asio::streambuf write_buf_;
 	boost::asio::streambuf read_buf_;
 
-	void handle_resolve(
-		const boost::system::error_code& ec,
-		resolver::iterator iterator
-		);
+	void handle_resolve(const boost::system::error_code& ec, resolver::iterator iterator);
 
 	void connect(resolver::iterator iterator);
-	void handle_connect(
-		const boost::system::error_code& ec,
-		resolver::iterator iterator
-		);
-	void handshake();
-	void handle_handshake(
-		const boost::system::error_code& ec
-		);
-	union {
-		char binary[4];
-		uint32_t num;
-	} handshake_response_;
+	void handle_connect(const boost::system::error_code& ec, resolver::iterator iterator);
 
-	std::size_t is_write_complete(
-		const boost::system::error_code& error,
-		std::size_t bytes_transferred
-		);
-	void handle_write(
-		const boost::system::error_code& ec,
-		std::size_t bytes_transferred
-		);
-	std::size_t is_read_complete(
-		const boost::system::error_code& error,
-		std::size_t bytes_transferred
-		);
-	void handle_read(
-		const boost::system::error_code& ec,
-		std::size_t bytes_transferred,
-		config& response
-		);
+	void handshake();
+	void handle_handshake(const boost::system::error_code& ec);
+
+	data_union handshake_response_;
+
+	std::size_t is_write_complete(const boost::system::error_code& error, std::size_t bytes_transferred);
+	void handle_write(const boost::system::error_code& ec, std::size_t bytes_transferred);
+
+	std::size_t is_read_complete(const boost::system::error_code& error, std::size_t bytes_transferred);
+	void handle_read(const boost::system::error_code& ec, std::size_t bytes_transferred, config& response);
+
 	uint32_t payload_size_;
+
 	std::size_t bytes_to_write_;
 	std::size_t bytes_written_;
 	std::size_t bytes_to_read_;
 	std::size_t bytes_read_;
-
-	public:
-	/**
-	 * Constructor.
-	 *
-	 * @param host    Name of the host to connect to
-	 * @param service Service identifier such as "80" or "http"
-	 */
-	connection(const std::string& host, const std::string& service);
-
-	void transfer(const config& request, config& response);
-
-	/** Handle all pending asynchonous events and return */
-	std::size_t poll()
-	{
-		try {
-			return io_service_.poll();
-		} catch(const boost::system::system_error& err) {
-			if(err.code() == boost::asio::error::operation_aborted)
-				return 1;
-			throw error(err.code());
-		}
-	}
-	/** Run asio's event loop
-	 *
-	 * Handle asynchronous events blocking until all asynchronous
-	 * operations have finished
-	 */
-	void run() { io_service_.run(); }
-
-	void cancel();
-
-	/** True if connected and no high-level operation is in progress */
-	bool done() const { return done_; }
-
-	std::size_t bytes_to_write() const
-	{
-		return bytes_to_write_;
-	}
-	std::size_t bytes_written() const
-	{
-		return bytes_written_;
-	}
-	std::size_t bytes_to_read() const
-	{
-		return bytes_to_read_;
-	}
-	std::size_t bytes_read() const
-	{
-		return bytes_read_;
-	}
 };
-
 }
