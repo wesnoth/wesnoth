ca03818fa897a558d9ad5d9c3729404f5959029f
Alexander van Gessel <ai0867@gmail.com>
2017-11-22 16:16:53 +0100

    >= 1.64, not > 1.64

--- src/serialization/string_view.hpp
+++ src/serialization/string_view.hpp
@@ -22,13 +22,13 @@ that class. */
 #include <boost/version.hpp>
 
 /*
- * Boost 1.61 already has string_view, but it fails to compile on some compilers.
- * The problem is that in string_view::at's ternary expression,
+ * Earlier versions already have string_view, but fail to compile on some
+ * compilers. The problem is that in string_view::at's ternary expression,
  * BOOST_THROW_EXCEPTION is not seen as a throw-expression. If a ternary branch
  * is not a throw-expression, it must be of the same type as the other branch,
  * necessitating the ', res[0]' workaround.
  */
-#if BOOST_VERSION > 106400
+#if BOOST_VERSION >= 106400
 
 /* Boost string_view is available, so we can just use it. */
 #include <boost/utility/string_view.hpp>
