09cdfc4c6828718e1fd1848b6e752e815ffc08d4
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2017-12-04 15:32:39 +0100

    Update unit.cpp

--- src/units/unit.cpp
+++ src/units/unit.cpp
@@ -1096,13 +1096,8 @@ color_t unit::xp_color() const
 	const bool far_advance  = static_cast<int>(experience_to_advance()) <= game_config::kill_experience*3;
 
 	color_t color = normal_color;
-	bool major_amla = false;
 	for(const config& adv:get_modification_advances()){
-		if(adv["major_amla"].to_bool ()){
-			major_amla = true;
-		}
-	}
-	if(advances_to().size() || major_amla == true){
+	if(advances_to().size() || adv["major_amla"].to_bool()){
 		if(near_advance){
 			color=near_advance_color;
 		} else if(mid_advance){
@@ -1120,6 +1115,7 @@ color_t unit::xp_color() const
 		} else {
 			color=amla_color;
 		}
+	}
 	}
 
 	return(color);
