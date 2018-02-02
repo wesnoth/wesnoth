c8abb8d5d99cc93b590fd6ae934dd8b2d9a1ba2d
Charles Dang <exodia339@gmail.com>
2018-02-02 15:40:44 +1100

    Add formula= option to [set_variable] (change by @celticminstrel)
    [ci skip]

--- data/lua/wml/set_variable.lua
+++ data/lua/wml/set_variable.lua
@@ -98,6 +98,11 @@ function wesnoth.wml_actions.set_variable(cfg)
 		wesnoth.set_variable(name, helper.rand(tostring(cfg.rand)))
 	end
 
+	if cfg.formula then
+		local fcn = wesnoth.compile_formula(cfg.formula)
+		wesnoth.set_variable(name, fcn(wesnoth.get_variable(name)))
+	end
+
 	local join_child = helper.get_child(cfg, "join")
 	if join_child then
 		local array_name = join_child.variable or helper.wml_error "missing variable= attribute in [join]"
