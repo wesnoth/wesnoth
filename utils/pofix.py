fe23166a312ef0b98965b66c19abd25d4caed210
Charles Dang <exodia339@gmail.com>
2018-02-04 02:15:16 +1100

    Updated pofix for ae33006
    [ci skip]

--- utils/pofix.py
+++ utils/pofix.py
@@ -106,6 +106,7 @@ game_stringfixes = {
 ("What secret Delfador?", "What secret, Delfador?"),
 ("I High Provost", "I, High Provost"),
 ("both a great combatant and leader", "both a great combatant and a leader"),
+("You must lead your men to the city and help defend it. Or recapture it if it falls before you arrive.", "You must lead your men to the city. Help defend it, or recapture it if it falls before you arrive!")
 ),
 
 "wesnoth-low" : (
