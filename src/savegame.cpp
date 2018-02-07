wesnoth/wesnoth

commit 220ac5f276c91b5f18ecc89918cf3864e644bea7

Disable control characters in autogenerated filenames (#2366)
On Windows, win32 programs such as Wesnoth aren't allowed to use control
characters in filenames, and therefore attempting to create a save file
whose name contains control characters will always fail. In addition, some
control characters (newlines in particular) also break Wesnoth's UI, as
regular text fields aren't intended to display multiple lines.

Fixes #2366.

src/savegame.cpp
@@ -418,7 +418,7 @@ bool savegame::check_filename(const std::string& filename)
 
 bool savegame::is_illegal_file_char(char c)
 {
-	return c == '/' || c == '\\' || c == ':'
+	return c == '/' || c == '\\' || c == ':' || c < 0x20
 #ifdef _WIN32
 	|| c == '?' || c == '|' || c == '<' || c == '>' || c == '*' || c == '"'
 #endif

commit 057a53055f74348c56d97c2c7ae0ab11a71b88cd

Fixed regression in 220ac5f276 causing em dashes to be considered invalid filename chars
Fixes #2422. Since em dashes were considered invalid, and MP scenario names almost all have
em dashes, savegames were created without them, and thus the game couldn't match any when
generating entries for the Back to Turn... menu.

src/savegame.cpp
@@ -418,7 +418,7 @@ bool savegame::check_filename(const std::string& filename)
 
 bool savegame::is_illegal_file_char(char c)
 {
-	return c == '/' || c == '\\' || c == ':' || c < 0x20
+	return c == '/' || c == '\\' || c == ':' || (c >= 0x00 && c < 0x20)
 #ifdef _WIN32
 	|| c == '?' || c == '|' || c == '<' || c == '>' || c == '*' || c == '"'
 #endif