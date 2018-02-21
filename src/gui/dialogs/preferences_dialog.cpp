wesnoth/wesnoth

commit a77652986a0b3df9a9f5cf7a454dbb3c9215a13d

Used std::distance instead of iterator arithmetic when possible

src/gui/dialogs/preferences_dialog.cpp
@@ -135,8 +135,8 @@ void preferences_dialog::set_resolution_list(menu_button& res_list, CVideo& vide
 		options.push_back(std::move(option));
 	}
 
-	const unsigned current_res = std::find(resolutions_.begin(), resolutions_.end(),
-		video.current_resolution()) - resolutions_.begin();
+	const unsigned current_res = std::distance(resolutions_.begin(), std::find(resolutions_.begin(), resolutions_.end(),
+		video.current_resolution()));
 
 	res_list.set_values(options, current_res);
 }
@@ -314,7 +314,7 @@ void preferences_dialog::post_build(window& window)
 		[&](widget& w) { disable_widget_on_toggle<slider>(window, w, "turbo_slider"); }, true);
 
 	const auto accl_load = [this]()->int {
-		return std::find(accl_speeds_.begin(), accl_speeds_.end(), turbo_speed()) - accl_speeds_.begin() + 1;
+		return std::distance(accl_speeds_.begin() + 1, std::find(accl_speeds_.begin(), accl_speeds_.end(), turbo_speed()));
 	};
 
 	const auto accl_save = [this](int i) {
@@ -652,8 +652,8 @@ void preferences_dialog::post_build(window& window)
 					option_ids.push_back(choice["id"]);
 				}
 
-				const unsigned selected = std::find(option_ids.begin(), option_ids.end(),
-					get(pref_name, option["default"].str())) - option_ids.begin();
+				const unsigned selected = std::distance(option_ids.begin(), std::find(option_ids.begin(), option_ids.end(),
+					get(pref_name, option["default"].str())));
 
 				menu_button* setter_widget = build_single_widget_instance<menu_button>("menu_button");
 				setter_widget->set_id("setter");

src/actions/advancement.cpp
@@ -193,7 +193,7 @@ namespace
 
 					for(std::vector<std::string>::const_iterator a = options.begin(); a != options.end(); ++a) {
 						if (std::find(allowed.begin(), allowed.end(), *a) != allowed.end()){
-							res = a - options.begin();
+							res = std::distance(options.begin(), a);
 							break;
 						}
 					}

src/actions/move.cpp
@@ -152,7 +152,7 @@ game_events::pump_result_t get_village(const map_location& loc, int side, bool *
 	// We strip the village off all other sides, unless it is held by an ally
 	// and our side is already defeated (and thus we can't occupy it)
 	for(std::vector<team>::iterator i = teams.begin(); i != teams.end(); ++i) {
-		int i_side = i - teams.begin() + 1;
+		int i_side = std::distance(teams.begin(), i) + 1;
 		if (!t || not_defeated || t->is_enemy(i_side)) {
 			if(i->owns_village(loc)) {
 				old_owner_side = i_side;

src/ai/simulated_actions.cpp
@@ -195,7 +195,7 @@ void helper_check_village(const map_location& loc, int side){
 	// Strip the village off all other sides.
 	int old_owner_side = 0;
 	for(std::vector<team>::iterator i = teams.begin(); i != teams.end(); ++i){
-		int i_side = i - teams.begin() + 1;
+		int i_side = std::distance(teams.begin(), i) + 1;
 		if(!t || has_leader || t->is_enemy(i_side)){
 			if(i->owns_village(loc)){
 				old_owner_side = i_side;

src/config.cpp
@@ -667,7 +667,7 @@ void config::remove_children(config_key_type key, std::function<bool(const confi
 
 	auto child_it = std::find_if(pos->second.begin(), pos->second.end(), predicate);
 	while(child_it != pos->second.end()) {
-		unsigned index = child_it - pos->second.begin();
+		unsigned index = std::distance(pos->second.begin(), child_it);
 		remove_child(pos, index);
 		child_it = std::find_if(pos->second.begin() + index, pos->second.end(), predicate);
 	}

src/display.cpp
@@ -249,7 +249,7 @@ display::display(const display_context * dc, std::weak_ptr<wb::manager> wb, repo
 
 	set_idle_anim_rate(preferences::idle_anim_rate());
 
-	zoom_index_ = std::find(zoom_levels.begin(), zoom_levels.end(), zoom_) - zoom_levels.begin();
+	zoom_index_ = std::distance(zoom_levels.begin(), std::find(zoom_levels.begin(), zoom_levels.end(), zoom_));
 
 	image::set_zoom(zoom_);
 
@@ -1737,7 +1737,7 @@ const theme::action* display::action_pressed()
 {
 	for(auto i = action_buttons_.begin(); i != action_buttons_.end(); ++i) {
 		if((*i)->pressed()) {
-			const size_t index = i - action_buttons_.begin();
+			const size_t index = std::distance(action_buttons_.begin(), i);
 			if(index >= theme_.actions().size()) {
 				assert(false);
 				return nullptr;
@@ -1753,7 +1753,7 @@ const theme::menu* display::menu_pressed()
 {
 	for(auto i = menu_buttons_.begin(); i != menu_buttons_.end(); ++i) {
 		if((*i)->pressed()) {
-			const size_t index = i - menu_buttons_.begin();
+			const size_t index = std::distance(menu_buttons_.begin(), i);
 			if(index >= theme_.menus().size()) {
 				assert(false);
 				return nullptr;
@@ -1774,7 +1774,7 @@ void display::enable_menu(const std::string& item, bool enable)
 		);
 
 		if(hasitem != menu->items().end()) {
-			const size_t index = menu - theme_.menus().begin();
+			const size_t index = std::distance(theme_.menus().begin(), menu);
 			if(index >= menu_buttons_.size()) {
 				continue;
 			}
@@ -2055,7 +2055,7 @@ bool display::set_zoom(unsigned int amount, const bool validate_value_and_set_in
 		}
 
 		new_zoom = *iter;
-		zoom_index_ = iter - zoom_levels.begin();
+		zoom_index_ = std::distance(zoom_levels.begin(), iter);
 	}
 
 	const SDL_Rect& area = map_area();

src/editor/palette/location_palette.cpp
@@ -384,7 +384,7 @@ void location_palette::add_item(const std::string& id)
 		pos = items_.size() - 1;
 	}
 	else {
-		pos = itor - items_.begin();
+		pos = std::distance(items_.begin(), itor);
 	}
 	selected_item_ = id;
 	items_start_ = std::max(pos - num_visible_items() + 1, items_start_);

src/formula/formula.cpp
@@ -971,7 +971,7 @@ public:
 			}
 
 			const std::string formula_str(i+1, j);
-			const int pos = i - str.begin();
+			const int pos = std::distance(str.begin(), i);
 			if(j - i == 2 && (i[1] == '(' || i[1] == '\'' || i[1] == ')')) {
 				// Bracket contained nothing but a quote or parenthesis.
 				// This means it was intended as a literal quote or square bracket.

commit 0b87383fd96430e0ea64e43cf356d2ddce57631f

Preferences Dialog: fixed minor regression from a77652986a0b3df9a9f
Accidentally put the +1 in the beginning range instead of adding to the resumt.

src/gui/dialogs/preferences_dialog.cpp
@@ -314,7 +314,7 @@ void preferences_dialog::post_build(window& window)
 		[&](widget& w) { disable_widget_on_toggle<slider>(window, w, "turbo_slider"); }, true);
 
 	const auto accl_load = [this]()->int {
-		return std::distance(accl_speeds_.begin() + 1, std::find(accl_speeds_.begin(), accl_speeds_.end(), turbo_speed()));
+		return std::distance(accl_speeds_.begin(), std::find(accl_speeds_.begin(), accl_speeds_.end(), turbo_speed())) + 1;
 	};
 
 	const auto accl_save = [this](int i) {