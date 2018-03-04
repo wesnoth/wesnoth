wesnoth/wesnoth

commit 45e8753cc58facabe70446f90df0f06fa3dd2ce8

Apply local ToD lighting to hex overlays when applicable (bug #22215)
This replaces map-wide ToD lighting with local ToD lighting for overlays
associated with locations that have custom local ToD lighting. The
method employed for this is more or less the same already used by
display::get_terrain_images() bar the complications associated to
dealing with terrain transitions, since overlays don't have those.

Note that for this to have an effect, local ToD lighting must be enabled
in Preferences -> Advanced. This is the default since Wesnoth 1.10.x.

This patch is more noisy than I expected, so it might require some
additional testing before landing in 1.12 to ensure it does not
introduce any regressions.

src/display.cpp
@@ -2595,13 +2595,36 @@ void display::draw_hex(const map_location& loc) {
 	if(!shrouded(loc)) {
 		typedef overlay_map::const_iterator Itor;
 		std::pair<Itor,Itor> overlays = overlays_->equal_range(loc);
+		const bool have_overlays = overlays.first != overlays.second;
+
+		bool use_local_light = local_tod_light_;
+		image::light_string lt;
+
+		if(have_overlays && use_local_light) {
+			const time_of_day& loc_tod = get_time_of_day(loc);
+			const tod_color& loc_col = loc_tod.color;
+
+			if(loc_col != get_time_of_day().color) {
+				// Continue with local light. image::get_lighted_image
+				// doesn't take the image::TOD_COLORED type, so we need
+				// to apply the color_adjust_ ourselves.
+				tod_color col = loc_col + color_adjust_;
+				lt = image::get_light_string(-1, col.r, col.g, col.b);
+			} else {
+				use_local_light = false;
+			}
+		}
+
 		for( ; overlays.first != overlays.second; ++overlays.first) {
 			if ((overlays.first->second.team_name == "" ||
 					overlays.first->second.team_name.find(dc_->teams()[playing_team()].team_name()) != std::string::npos)
 					&& !(fogged(loc) && !overlays.first->second.visible_in_fog))
 			{
-				drawing_buffer_add(LAYER_TERRAIN_BG, loc, xpos, ypos,
-						image::get_image(overlays.first->second.image,image_type));
+				const surface surf = use_local_light
+					   ? image::get_lighted_image(overlays.first->second.image, lt, image::SCALED_TO_HEX)
+					   : image::get_image(overlays.first->second.image, image_type);
+
+				drawing_buffer_add(LAYER_TERRAIN_BG, loc, xpos, ypos, surf);
 			}
 		}
 	}

commit f14481ba23fd1917a2f5b07329a12fa1577038be

Fixed color_adjust not applying to map items (fixes #2588)
Appears to be a regression from 45e8753cc58facabe70446f90df0f06fa3dd2ce8 - color adjust not
applied unless local tod was different from global - and 478c75bdb5bc914fcd71469a2ae6967cadc99e11
- changed the first argument of the get_light_string call from -1 to 0. Not sure why that has
an effect, but if it remains 0 then the item ToD seems a bit out of sync with the rest of the map
when using, say, FADE_TO_BLACK. Restoring it to -1 fixes that.

I confirmed both that map-wide ToD and local [time_area] ToD apply to items after this patch.

src/display.cpp
@@ -2616,16 +2616,9 @@ void display::draw_hex(const map_location& loc) {
 		std::pair<Itor,Itor> overlays = overlays_->equal_range(loc);
 		const bool have_overlays = overlays.first != overlays.second;
 
-		image::light_string lt;
-
 		if(have_overlays) {
-			tod_color tod_col = tod.color;
-
-			if(tod_col != get_time_of_day().color) {
-				tod_col = tod_col + color_adjust_;
-			}
-
-			lt = image::get_light_string(0, tod_col.r, tod_col.g, tod_col.b);
+			tod_color tod_col = tod.color + color_adjust_;
+			image::light_string lt = image::get_light_string(-1, tod_col.r, tod_col.g, tod_col.b);
 
 			for( ; overlays.first != overlays.second; ++overlays.first) {
 				if ((overlays.first->second.team_name.empty() ||