beed7bfe7231f3dc6d4f0b1553222ca09ab20912
Jyrki Vesterinen <sandgtx@gmail.com>
2018-03-04 19:49:21 +0200

    Use std::map::at() in ter_map::get()
    No need to roll our own bounds check when C++ standard library can do it
    for us.

--- src/terrain/translation.hpp
+++ src/terrain/translation.hpp
@@ -89,8 +89,8 @@ namespace t_translation {
 		ter_map & operator= (ter_map &&) = default;
 #endif
 
-		terrain_code& get(int x, int y) { size_t index = x * h + y; assert(index < data.size()); return data[index]; }
-		const terrain_code& get(int x, int y) const { size_t index = x * h + y; assert(index < data.size()); return data[index]; }
+		terrain_code& get(int x, int y) { size_t index = x * h + y; return data.at(index); }
+		const terrain_code& get(int x, int y) const { size_t index = x * h + y; return data.at(index); }
 
 		std::vector<terrain_code> data;
 		int w;
