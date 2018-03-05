3489dac6420c41a51ed0caf5b84c106b235fd73a
Charles Dang <exodia339@gmail.com>
2018-03-05 15:44:42 +1100

    [heal_unit]: don't play healed animation if no healing is done
    [ci skip]
    
    Addresses #2530. Not closing that though, since the behavior might still be tweaked.
    
    The healer will still play their own animation, if applicable (behavior option 2 in
    the bug report).

--- data/lua/wml/heal_unit.lua
+++ data/lua/wml/heal_unit.lua
@@ -38,8 +38,9 @@ function wesnoth.wml_actions.heal_unit(cfg)
 
 		if cfg.animate then
 			local animator = wesnoth.create_animator()
-			animator:add(u, 'healed', 'hit',
-				{value = heal_amount, text = heal_amount, color = {0, 255, 0}})
+			if heal_amount > 0 then
+				animator:add(u, 'healed', 'hit', {value = heal_amount, text = heal_amount, color = {0, 255, 0}})
+			end
 			if #healers > 0 then
 				animator:add(healers[1], 'healing', 'hit', {value = heal_amount})
 			end
