1e2512a67220195d1c0ae366013d97e1106b0d23
Celtic Minstrel <celtic.minstrel.ca@some.place>
2018-03-04 20:52:00 -0500

    Honour the regular deprecation message settings for WML deprecation messages

--- data/lua/wml-tags.lua
+++ data/lua/wml-tags.lua
@@ -893,11 +893,7 @@ function wml_actions.deprecated_message(cfg)
 		local _ = wesnoth.textdomain "wesnoth"
 		helper.wml_error(_"Invalid deprecation level (should be 1-4)")
 	end
-	wesnoth.deprecated_message(cfg.what, cfg.level, cfg.version, cfg.message or '')
-	if not wesnoth.game_config.debug then return end
-	if cfg.level > 1 then
-		wesnoth.log('wml', cfg.message)
-	end
+	wesnoth.deprecation_message(cfg.what, cfg.level, cfg.version, cfg.message or '')
 end
 
 function wml_actions.wml_message(cfg)
