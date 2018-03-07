b7ac310dc07aee5fece913b584093539c6df1d61
pentarctagon <pentarctagon@tutamail.com>
2018-03-06 19:16:30 -0600

    Moves the build-cache directory to be under $HOME.
    Moving it out of the same directory as the git repository means that the object files won't be copied into the temporary docker container when it's being created.

--- utils/travis/steps/script.sh
+++ utils/travis/steps/script.sh
@@ -20,12 +20,12 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
         
         exit $BUILD_RET
     else
-        ln -s build-cache/ build
+        ln -s $HOME/build-cache/ build
         ./utils/travis/check_utf8.sh
         ./utils/travis/utf8_bom_dog.sh
         "$CXX" --version
         scons wesnoth wesnothd campaignd boost_unit_tests build=release ctool="$CC" cxxtool="$CXX" --debug=time extra_flags_config="-pipe" extra_flags_release="$EXTRA_FLAGS_RELEASE" strict=true cxx_std="$CXXSTD" nls="$NLS" jobs=2 enable_lto=false
     fi
 else
-    docker run -v "$PWD"/build-cache:/home/wesnoth-travis/build -v "$HOME"/.ccache:/root/.ccache wesnoth-repo:16.04 bash -c './utils/travis/docker_run.sh "$@"' bash "$NLS" "$TOOL" "$CC" "$CXX" "$CXXSTD" "$EXTRA_FLAGS_RELEASE" "$WML_TESTS" "$WML_TEST_TIME" "$PLAY_TEST" "$MP_TEST" "$BOOST_TEST"
+    docker run -v "$HOME"/build-cache:/home/wesnoth-travis/build -v "$HOME"/.ccache:/root/.ccache wesnoth-repo:16.04 bash -c './utils/travis/docker_run.sh "$@"' bash "$NLS" "$TOOL" "$CC" "$CXX" "$CXXSTD" "$EXTRA_FLAGS_RELEASE" "$WML_TESTS" "$WML_TEST_TIME" "$PLAY_TEST" "$MP_TEST" "$BOOST_TEST"
 fi
