02244aaa5331ce17a5eafb023d07524c05a9a90b
pentarctagon <pentarctagon@tutamail.com>
2018-03-06 19:19:09 -0600

    Make LTO controlled by a variable export in steps/install.sh.
    Currently will remain disabled everywhere, but this allows for enabling LTO for optimized, non-xcode jobs just by changing the value of the LTO variable.

--- utils/travis/steps/install.sh
+++ utils/travis/steps/install.sh
@@ -8,12 +8,16 @@ export PLAY_TEST=true
 export MP_TEST=true
 export WML_TEST_TIME=15
 export BOOST_TEST=true
+export LTO=false
 
 if [ "$OPT" = "-O0" ]; then
     export EXTRA_FLAGS_RELEASE="-O0"
     export PLAY_TEST=false
     export MP_TEST=false
     export WML_TEST_TIME=20
+else
+    # change to true to enable LTO on optimized, non-xcode builds
+    export LTO=false
 fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
