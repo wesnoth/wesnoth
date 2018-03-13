0ed7ff100a00bb837df7060a0f18e101e6ded0ae
Alexander van Gessel <ai0867@gmail.com>
2018-03-13 15:23:28 +0100

    Factor out base64 and crypt {en,de}coding
    Base64 according to RFC4648.
    Crypt64 is whatever crypt() does.
    
    The two radix-64 encodings share less code than I would like,
    because base64 is stream-oriented and therefore big-endian,
    while crypt is little-endian.

---
+++ src/serialization/base64.hpp
@@ -0,0 +1,33 @@
+/*
+   Copyright (C) 2003 - 2018 by David White <dave@whitevine.net>
+   Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#pragma once
+
+#include "serialization/string_view.hpp"
+
+#include <vector>
+
+// Official Base64 encoding (RFC4648)
+namespace base64 {
+	std::vector<uint8_t> decode(utils::string_view encoded);
+	std::string encode(utils::byte_string_view bytes);
+}
+// crypt()-compatible radix-64 encoding
+namespace crypt64 {
+	std::vector<uint8_t> decode(utils::string_view encoded);
+	std::string encode(utils::byte_string_view bytes);
+	// Single character functions. For special use only
+	int decode(char encoded_char);
+	char encode(int value);
+}
