4b08a40181d913f2c3f035daaafa6ce1c4948dde
Celtic Minstrel <celtic.minstrel.ca@some.place>
2018-03-17 10:41:04 -0400

    Deprecate wesnoth.set_music

--- data/lua/backwards-compatibility.lua
+++ data/lua/backwards-compatibility.lua
@@ -2,9 +2,7 @@
 
 -- This file may provide an implementation of Lua functions removed from the engine.
 
-function wesnoth.set_music(cfg)
-	wesnoth.wml_actions.music(cfg)
-end
+wesnoth.set_music = wesnoth.deprecate_api('wesnoth.set_music', 'wesnoth.music_list', 1, nil, wesnoth.wml_actions.music)
 
 -- Calling wesnoth.fire isn't the same as calling wesnoth.wml_actions[name] due to the passed vconfig userdata
 -- which also provides "constness" of the passed wml object from the point of view of the caller.
