e37d511d2fa06d5aa1854c334d46cdbd898fa550
gfgtdf <daniel.gfgtdf@gmail.com>
2018-03-17 16:27:37 +0100

    add bar_offset_x/y to [unit_type]
    this should probably be opimized later so that it does not need the config lookup on each redraw, but that can be done 1.13.12

--- src/units/drawer.cpp
+++ src/units/drawer.cpp
@@ -224,9 +224,22 @@ void unit_drawer::redraw_unit (const unit & u) const
 	}
 	if(draw_bars) {
 		const image::locator* orb_img = nullptr;
-		const surface unit_img = image::get_image(u.default_anim_image(), image::SCALED_TO_ZOOM);
-		const int xoff = unit_img.null() ? hex_size_by_2 : (hex_size - unit_img->w)/2;
-		const int yoff = unit_img.null() ? hex_size_by_2 : (hex_size - unit_img->h)/2;
+
+		const auto& type_cfg = u.type().get_cfg();
+		const auto& cfg_offset_x = type_cfg["bar_offset_x"];
+		const auto& cfg_offset_y = type_cfg["bar_offset_y"];
+		int xoff;
+		int yoff;
+		if(cfg_offset_x.empty() && cfg_offset_y.empty()) {
+			const surface unit_img = image::get_image(u.default_anim_image(), image::SCALED_TO_ZOOM);
+			const int xoff = unit_img.null() ? hex_size_by_2 : (hex_size - unit_img->w)/2;
+			const int yoff = unit_img.null() ? hex_size_by_2 : (hex_size - unit_img->h)/2;
+		}
+		else {
+			const int xoff = hex_size_by_2 + cfg_offset_x;
+			const int yoff = hex_size_by_2 + cfg_offset_y;
+		}
+
 		/*static*/ const image::locator partmoved_orb(game_config::images::orb + "~RC(magenta>" +
 						preferences::partial_color() + ")"  );
 		/*static*/ const image::locator moved_orb(game_config::images::orb + "~RC(magenta>" +
