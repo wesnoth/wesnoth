b8ecf7116f663e026dc7fb6021696b1d2839bc8b
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2018-03-23 10:26:34 +0000

    Update attack_predictions.cpp

--- src/gui/dialogs/attack_predictions.cpp
+++ src/gui/dialogs/attack_predictions.cpp
@@ -217,7 +217,7 @@ void attack_predictions::set_data(window& window, const combatant_data& attacker
 	}
 
 	// Leadership bonus.
-	const int leadership_bonus = under_leadership(resources::gameboard->units(), attacker.unit_.get_location()).first;
+	const int leadership_bonus = under_leadership(resources::gameboard->units(), attacker.unit_.get_location(), weapon).first;
 
 	if(leadership_bonus != 0) {
 		set_label_helper("leadership_modifier", utils::signed_percent(leadership_bonus));
