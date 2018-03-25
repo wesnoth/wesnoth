32892ffd96ccacc8a020bba251388bb17edc0497
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2018-03-25 11:21:14 +0200

    Update abilities.cpp

--- src/units/abilities.cpp
+++ src/units/abilities.cpp
@@ -176,47 +176,6 @@ bool unit::get_ability_bool(const std::string& tag_name, const map_location& loc
 
 	return false;
 }
-unit_ability_list unit::get_abilities(const std::string& tag_name, const map_location& loc) const
-{
-	unit_ability_list res(loc_);
-
-	for (const config &i : this->abilities_.child_range(tag_name)) {
-		if (ability_active(tag_name, i, loc) &&
-			ability_affects_self(tag_name, i, loc))
-		{
-			res.push_back(unit_ability(&i, loc));
-		}
-	}
-
-	assert(display::get_singleton());
-	const unit_map& units = display::get_singleton()->get_units();
-
-	adjacent_loc_array_t adjacent;
-	get_adjacent_tiles(loc,adjacent.data());
-	for(unsigned i = 0; i < adjacent.size(); ++i) {
-		const unit_map::const_iterator it = units.find(adjacent[i]);
-		if (it == units.end() || it->incapacitated())
-			continue;
-		// Abilities may be tested at locations other than the unit's current
-		// location. This is intentional to allow for less messing with the unit
-		// map during calculations, particularly with regards to movement.
-		// Thus, we need to make sure the adjacent unit (*it) is not actually
-		// ourself.
-		if ( &*it == this )
-			continue;
-		for (const config &j : it->abilities_.child_range(tag_name)) {
-			if (affects_side(j, resources::gameboard->teams(), side(), it->side()) &&
-			    it->ability_active(tag_name, j, adjacent[i]) &&
-			    ability_affects_adjacent(tag_name, j, i, loc, *it))
-			{
-				res.push_back(unit_ability(&j, adjacent[i]));
-			}
-		}
-	}
-
-
-	return res;
-}
 
 unit_ability_list unit::get_abilities(const std::string& tag_name, const map_location& loc, const_attack_ptr weapon) const
 {
