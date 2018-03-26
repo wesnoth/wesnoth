0c2ce018786079260d6924fb8d05b055812b812c
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2018-03-26 18:20:20 +0200

    Update unit.hpp

--- src/units/unit.hpp
+++ src/units/unit.hpp
@@ -1543,9 +1543,7 @@ private:
 	 */
 	bool ability_affects_self(const std::string& ability, const config& cfg, const map_location& loc) const;
 
-	bool ability_affects_weapon(const std::string& ability,const config& cfg,const map_location& loc, const_attack_ptr weapon) const;
-	
-	bool ability_affects_second_weapon(const std::string& ability,const config& cfg,const map_location& loc, const_attack_ptr opp_weapon) const;
+	bool ability_affects_weapon(const std::string& ability,const config& cfg,const map_location& loc, const_attack_ptr weapon, const_attack_ptr opp_weapon) const;
 	
 public:
 	/** Get the unit formula manager. */
