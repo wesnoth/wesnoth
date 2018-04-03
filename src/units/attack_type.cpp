46f4b514822aadfed173f705ec401439b6aae74d
Celtic Minstrel <celtic.minstrel.ca@some.place>
2018-04-03 19:49:33 -0400

    SWF: Support lists for special= and special_active=

--- src/units/attack_type.cpp
+++ src/units/attack_type.cpp
@@ -103,8 +103,8 @@ static bool matches_simple_filter(const attack_type & attack, const config & fil
 	const std::string& filter_movement = filter["movement_used"];
 	const std::vector<std::string> filter_name = utils::split(filter["name"]);
 	const std::vector<std::string> filter_type = utils::split(filter["type"]);
-	const std::string filter_special = filter["special"];
-	const std::string filter_special_active = filter["special_active"];
+	const std::vector<std::string> filter_special = utils::split(filter["special"]);
+	const std::vector<std::string> filter_special_active = utils::split(filter["special_active"]);
 	const std::string filter_formula = filter["formula"];
 
 	if ( !filter_range.empty() && std::find(filter_range.begin(), filter_range.end(), attack.range()) == filter_range.end() )
@@ -131,11 +131,31 @@ static bool matches_simple_filter(const attack_type & attack, const config & fil
 	if ( !filter_type.empty() && std::find(filter_type.begin(), filter_type.end(), attack.type()) == filter_type.end() )
 		return false;
 
-	if ( !filter_special.empty() && !attack.get_special_bool(filter_special, true) )
-		return false;
+	if(!filter_special.empty()) {
+		bool found = false;
+		for(auto& special : filter_special) {
+			if(attack.get_special_bool(special, true)) {
+				found = true;
+				break;
+			}
+		}
+		if(!found) {
+			return false;
+		}
+	}
 
-	if ( !filter_special_active.empty() && !attack.get_special_bool(filter_special_active, false) )
-		return false;
+	if(!filter_special_active.empty()) {
+		bool found = false;
+		for(auto& special : filter_special_active) {
+			if(attack.get_special_bool(special, false)) {
+				found = true;
+				break;
+			}
+		}
+		if(!found) {
+			return false;
+		}
+	}
 
 	if (!filter_formula.empty()) {
 		try {
