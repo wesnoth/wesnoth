wesnoth/wesnoth

commit 6b52d16faedaf6499f4777ed8156b1eaa09ab554

Unit Create: made Race the default sorting option, as was in 1.12
Fixes the second issue described in #2510.

src/gui/dialogs/unit_create.cpp
@@ -154,6 +154,8 @@ void unit_create::pre_show(window& window)
 	list.register_sorting_option(0, [this](const int i) { return (*units_[i]).race()->plural_name().str(); });
 	list.register_sorting_option(1, [this](const int i) { return (*units_[i]).type_name().str(); });
 
+	list.set_active_sorting_option({0, listbox::SORT_ASCENDING}, true);
+
 	list_item_clicked(window);
 }
 

commit ccce10511a2e74e23448e54e652874711bf6bad8

Unit Create: fixed unit selection not persisting between uses (fixes #2822)
I had been unconditionally selecting the first entry after setting the active
sorting option. This overrode the previous unit selection.

Regression from 6b52d16faedaf6499f4777ed8156b1eaa09ab554.

src/gui/dialogs/unit_create.cpp
@@ -137,7 +137,7 @@ void unit_create::pre_show(window& window)
 		list.add_row(row_data);
 
 		// Select the previous choice, if any.
-		if(choice_.empty() != true && choice_ == i.first) {
+		if(!choice_.empty() && choice_ == i.first) {
 			list.select_last_row();
 		}
 	}
@@ -150,7 +150,8 @@ void unit_create::pre_show(window& window)
 	list.register_translatable_sorting_option(0, [this](const int i) { return (*units_[i]).race()->plural_name().str(); });
 	list.register_translatable_sorting_option(1, [this](const int i) { return (*units_[i]).type_name().str(); });
 
-	list.set_active_sorting_option({0, listbox::SORT_ASCENDING}, true);
+	// Select the first entry on sort if no previous selection was provided.
+	list.set_active_sorting_option({0, listbox::SORT_ASCENDING}, choice_.empty());
 
 	list_item_clicked(window);
 }