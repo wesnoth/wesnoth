fee641516b3f3ac954aa787d3625e254d652a86c
Charles Dang <exodia339@gmail.com>
2018-04-13 10:49:38 +1100

    Fixup 7767902

--- src/tests/gui/test_gui2.cpp
+++ src/tests/gui/test_gui2.cpp
@@ -858,7 +858,7 @@ struct dialog_tester<mp_lobby>
 	wesnothd_connection_init init;
 	std::vector<std::string> installed_addons;
 	mp::lobby_info li;
-	dialog_tester() : connection("", ""), init(*connection), li(game_config, installed_addons)
+	dialog_tester() : connection("", ""), init(connection), li(game_config, installed_addons)
 	{
 	}
 	mp_lobby* create()
