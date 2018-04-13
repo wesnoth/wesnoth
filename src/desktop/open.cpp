wesnoth/wesnoth

commit b95c9996bf435daa4d7e64fd1ef41425ac182b95

Fix freeze when launching web browser on Linux/macOS
POSIX has a feature to automatically reap zombie processes, which makes it
unnecessary to wait for the child process to exit. Activate the feature
and stop waiting for exit.

"POSIX.1-2001 specifies that if the disposition of SIGCHLD is set to
SIG_IGN or the SA_NOCLDWAIT flag is set for SIGCHLD (see sigaction(2)),
then children that terminate do not become zombies"

src/desktop/open.cpp
@@ -73,10 +73,10 @@ bool open_object(const std::string& path_or_url)
 	} else if(child == 0) {
 		execlp(launcher, launcher, path_or_url.c_str(), nullptr);
 		_exit(1); // This shouldn't happen.
-	} else if(waitpid(child, &child_status, 0) == -1) {
-		ERR_DU << "open_object(): waitpid() failed" << std::endl;
-		return false;
 	}
+
+	// Waiting for the child process to exit is unnecessary because we ignore SIGCHLD.
+	// See the manpage for wait(2).
 
 	if(child_status) {
 //Those status check macros seem to trigger old style casts on some compiler versions

src/wesnoth.cpp
@@ -1015,6 +1015,11 @@ int main(int argc, char** argv)
 	sigemptyset(&terminate_handler.sa_mask);
 	sigaction(SIGTERM, &terminate_handler, nullptr);
 	sigaction(SIGINT, &terminate_handler, nullptr);
+
+	// Explicitly ignore SIGCHLD. This allows us to launch child processes without waiting
+	// for them to exit. See the manpage for wait(2).
+	terminate_handler.sa_handler = SIG_IGN;
+	sigaction(SIGCHLD, &terminate_handler, nullptr);
 #endif
 
 	//declare this here so that it will always be at the front of the event queue.

commit 846bf8f208ae41381e133e96b24e6b9876ea2a01

Fix "host networked game" option
It was broken by b95c9996bf435daa4d7e64fd1ef41425ac182b95 because
ignoring SIGCHLD makes it impossible to query exit status of wesnothd
subprocess. Fix this by spawning a thread to wait for the browser
instead of ignoring SIGCHLD.

src/desktop/open.cpp
@@ -19,6 +19,8 @@
 
 #if defined(_X11) || defined(__APPLE__)
 
+#include <thread>
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h> // fork(), exec family
@@ -74,8 +76,8 @@ bool open_object(const std::string& path_or_url)
 		_exit(1); // This shouldn't happen.
 	}
 
-	// Waiting for the child process to exit is unnecessary because we ignore SIGCHLD.
-	// See the manpage for wait(2).
+	std::thread t { [child](){ int status; waitpid(child, &status, 0); } };
+	t.detach();
 
 	return true;
 

src/wesnoth.cpp
@@ -1100,11 +1100,6 @@ int main(int argc, char** argv)
 	sigemptyset(&terminate_handler.sa_mask);
 	sigaction(SIGTERM, &terminate_handler, nullptr);
 	sigaction(SIGINT, &terminate_handler, nullptr);
-
-	// Explicitly ignore SIGCHLD. This allows us to launch child processes without waiting
-	// for them to exit. See the manpage for wait(2).
-	terminate_handler.sa_handler = SIG_IGN;
-	sigaction(SIGCHLD, &terminate_handler, nullptr);
 #endif
 
 	SDL_StartTextInput();
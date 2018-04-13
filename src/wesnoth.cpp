846bf8f208ae41381e133e96b24e6b9876ea2a01
loonycyborg <loonycyborg@gmail.com>
2018-04-13 15:35:43 +0300

    Fix "host networked game" option
    It was broken by b95c9996bf435daa4d7e64fd1ef41425ac182b95 because
    ignoring SIGCHLD makes it impossible to query exit status of wesnothd
    subprocess. Fix this by spawning a thread to wait for the browser
    instead of ignoring SIGCHLD.

--- src/wesnoth.cpp
+++ src/wesnoth.cpp
@@ -1100,11 +1100,6 @@ int main(int argc, char** argv)
 	sigemptyset(&terminate_handler.sa_mask);
 	sigaction(SIGTERM, &terminate_handler, nullptr);
 	sigaction(SIGINT, &terminate_handler, nullptr);
-
-	// Explicitly ignore SIGCHLD. This allows us to launch child processes without waiting
-	// for them to exit. See the manpage for wait(2).
-	terminate_handler.sa_handler = SIG_IGN;
-	sigaction(SIGCHLD, &terminate_handler, nullptr);
 #endif
 
 	SDL_StartTextInput();
