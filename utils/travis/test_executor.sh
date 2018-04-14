33ad6be326c234b173d680ce34589464f88f8418
pentarctagon <pentarctagon@tutamail.com>
2018-04-14 04:55:22 -0500

    Adds a c++17 build using the pre-release Ubuntu 18.04.
    Though given 00d87f8fe4078ffd84fcdc0d73fcc2104efef1e7 I would say this is better than nothing.

--- utils/travis/test_executor.sh
+++ utils/travis/test_executor.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-set -e
-gdb -q -batch -return-child-result -ex "run" -ex "thread apply all bt" -ex "quit" --args ./boost_unit_tests
+gdb -q -batch -return-child-result -ex "run" -ex "thread apply all bt" -ex "quit" --args ./boost_unit_tests 2> error.log
+error_code="$?"
+while grep -q 'Could not initialize SDL_video' error.log; do
+  echo "Could not initialize SDL_video error, retrying..."
+  gdb -q -batch -return-child-result -ex "run" -ex "thread apply all bt" -ex "quit" --args ./boost_unit_tests 2> error.log
+  error_code="$?"
+done
+cat error.log
+
+exit $error_code
+
