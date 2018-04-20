bea9de088514a720277b80730df0b03861bae4d7
Iris Morelle <shadowm@wesnoth.org>
2018-04-20 04:13:21 -0300

    wesnothd: Initial support for checking forum bans during login
    This adds a user_is_banned() method to the user_handler classes that
    returns whether a given username (and optionally IP address) is banned
    by the user_handler platform. Obviously right now this is only intended
    to work with forum_user_handler and phpBB.
    
    Forum bans are checked against entries in the banlist table using
    username (actually user id), IP address, and email address where
    applicable. A user matching a ban on any of those three items will not
    be permitted into the server *unless* they have the moderator flag set.
    It might be worth making an exception for board founders as well,
    although that is probably orthogonal to this patchset.
    
    Right now there are a few missing items:
    
     * The server sends clients an error that allows them to try again with
       a different username/password combination immediately. Nothing stops
       them from causing noise in the server logs this way, so we probably
       need to ensure this counts as an authentication failure for the
       purpose of temporarily and automatically banning the IP address.
    
     * The user handler doesn't allow retrieving details about the ban, so
       all that the main server code can do is report back to the client as
       their nickname being banned, when this is not necessarily the case
       (email or IP address bans). I need to figure out a better API for
       retrieving this info.
    
     * Likewise, the server does not log the specifics about the matched ban
       yet unless the mp_user_handler log domain is set to the info log
       level.
    
     * There's no i18n support on the client side for the error message sent
       by the server -- which is going to change anyway.
    
     * Testing this patch uncovered an issue with the MP client not
       displaying messages sent during the login sequence, including the mod
       authentication notice.

--- src/server/user_handler.hpp
+++ src/server/user_handler.hpp
@@ -108,6 +108,15 @@ class user_handler {
 		/** Mark this user as a moderator */
 		virtual void set_is_moderator(const std::string& name, const bool& is_moderator) =0;
 
+		/**
+		 * Returns true if this user account or IP address is banned.
+		 *
+		 * @note The IP address is only used by the @a forum_user_handler
+		 *       subclass. Regular IP ban checks are done by @a server_base
+		 *       instead.
+		 */
+		virtual bool user_is_banned(const std::string& name, const std::string& addr="") = 0;
+
 		struct error : public game::error {
 			error(const std::string& message) : game::error(message) {}
 		};
