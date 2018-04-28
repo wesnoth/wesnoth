2417af262688d1eca00c6dce972de6cae0563518
Charles Dang <exodia339@gmail.com>
2018-04-29 05:58:44 +1100

    AI: deployed typesafe_aspect_ptr consistently
    No reason this couldn't have used aspect_type<t>typesafe_ptr before, so this just
    makes uses of this specific type easier to find.
    
    (cherry-picked from commit 1e846aced28b5b1d1b0ae9890d326ed3a875b96d)

--- src/ai/contexts.cpp
+++ src/ai/contexts.cpp
@@ -174,7 +174,7 @@ synced_command_result_ptr readonly_context_impl::check_synced_command_action(con
 
 
 template<typename T>
-void readonly_context_impl::add_known_aspect(const std::string &name, std::shared_ptr< typesafe_aspect <T>> &where)
+void readonly_context_impl::add_known_aspect(const std::string &name, typesafe_aspect_ptr<T>& where)
 {
 	std::shared_ptr< typesafe_known_aspect <T>> ka_ptr(new typesafe_known_aspect<T>(name,where,aspects_));
 	known_aspects_.emplace(name,ka_ptr);
