a4f7f5c37c2bc7208651527678b56f070327b2c3
Iris Morelle <shadowm@wesnoth.org>
2018-05-08 02:24:50 -0300

    wam: Allow using embedded data URIs as add-on icons
    [ci skip]
    
    (cherry-picked from commit 4335bedeff9dbf1b147c0ef260171dd3eb83c795)

--- data/tools/addon_manager/html.py
+++ data/tools/addon_manager/html.py
@@ -4,6 +4,7 @@
 import html
 import glob
 import os
+import re
 import sys
 import time
 import urllib.parse
@@ -260,32 +261,40 @@ def output(path, url, datadir, data):
 
         if icon:
             icon = icon.strip()
-            tilde = icon.find("~")
-            if tilde >= 0:
-                icon = icon[:tilde]
-            if "\\" in icon:
-                icon = icon.replace("\\", "/")
-            try:
-                os.mkdir(path + "/icons")
-            except OSError:
-                pass
-            if "." not in icon:
-                icon += ".png"
-            src = root_dir + icon
-            imgurl = "icons/" + os.path.basename(icon)
-            if not os.path.exists(src):
-                src = root_dir + "data/core/images/" + icon
-            if not os.path.exists(src):
-                src = root_dir + "images/" + icon
-            if not os.path.exists(src):
-                src = glob.glob(root_dir + "data/campaigns/*/images/" + icon)
-                if src:
-                    src = src[0]
-                if not src or not os.path.exists(src):
-                    sys.stderr.write("Cannot find icon " + icon + "\n")
-                    src = root_dir + "images/misc/missing-image.png"
-                    imgurl = "icons/missing-image.png"
-            images_to_tc.append((src, path + "/" + imgurl))
+            uri_manifest = re.match('^data:(image/(?:png|jpeg));base64,', icon)
+
+            if uri_manifest:
+                if uri_manifest.group(1) not in ('image/png', 'image/jpeg'):
+                    sys.stderr.write("Data URI icon using unsupported content type " + uri_manifest.group(1))
+                else:
+                    imgurl = icon
+            else:
+                tilde = icon.find("~")
+                if tilde >= 0:
+                    icon = icon[:tilde]
+                if "\\" in icon:
+                    icon = icon.replace("\\", "/")
+                try:
+                    os.mkdir(path + "/icons")
+                except OSError:
+                    pass
+                if "." not in icon:
+                    icon += ".png"
+                src = root_dir + icon
+                imgurl = "icons/" + os.path.basename(icon)
+                if not os.path.exists(src):
+                    src = root_dir + "data/core/images/" + icon
+                if not os.path.exists(src):
+                    src = root_dir + "images/" + icon
+                if not os.path.exists(src):
+                    src = glob.glob(root_dir + "data/campaigns/*/images/" + icon)
+                    if src:
+                        src = src[0]
+                    if not src or not os.path.exists(src):
+                        sys.stderr.write("Cannot find icon " + icon + "\n")
+                        src = root_dir + "images/misc/missing-image.png"
+                        imgurl = "icons/missing-image.png"
+                images_to_tc.append((src, path + "/" + imgurl))
 
         w('<tr>')
 
