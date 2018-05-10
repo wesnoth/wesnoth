c8d8993bbe201a1d3e196c9607a95966a7764feb
Pentarctagon <pentarctagon@tutamail.com>
2018-05-10 18:41:09 -0500

    Add osx+cmake support to travis.

--- utils/travis/steps/script.sh
+++ utils/travis/steps/script.sh
@@ -20,16 +20,23 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
 
         exit $BUILD_RET
     else
-        ln -s $HOME/build-cache/ build
-
         ./utils/travis/check_utf8.sh || exit 1
         ./utils/travis/utf8_bom_dog.sh || exit 1
 
         "$CXX" --version
-        scons wesnoth wesnothd campaignd boost_unit_tests build=release \
-              ctool="$CC" cxxtool="$CXX" cxx_std="$CXXSTD" \
-              extra_flags_config="-pipe" opt="$OPT" strict=true \
-              nls="$NLS" enable_lto="$LTO" jobs=2 --debug=time
+        if [ "$TOOL" == "scons" ]; then
+            ln -s $HOME/build-cache/ build
+
+            scons wesnoth wesnothd campaignd boost_unit_tests build=release \
+                  ctool="$CC" cxxtool="$CXX" cxx_std="$CXXSTD" \
+                  extra_flags_config="-pipe" opt="$OPT" strict=true \
+                  nls="$NLS" enable_lto="$LTO" jobs=2 --debug=time
+        else
+            cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_GAME=true -DENABLE_SERVER=true -DENABLE_CAMPAIGN_SERVER=true -DENABLE_TESTS=true -DENABLE_NLS=false \
+                  -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCXX_STD="$CXXSTD"\
+                  -DEXTRA_FLAGS_CONFIG="-pipe" -DOPT="$OPT" -DENABLE_STRICT_COMPILATION="$STRICT" && \
+                  make VERBOSE=1 -j2
+        fi
     fi
 else
     docker run --volume "$HOME"/build-cache:/home/wesnoth-travis/build \
