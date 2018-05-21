787bf47225c37f4dd5a8ec733d16529d8d9e2a1d
Martin Hrubý (hrubymar10) <hrubymar10@gmail.com>
2018-05-21 14:23:16 +0200

    Switch travis Xcode to DEBUG scheme

--- utils/travis/steps/script.sh
+++ utils/travis/steps/script.sh
@@ -11,7 +11,7 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
         export CCACHE_MAXSIZE=2G
         export CCACHE_COMPILERCHECK=content
 
-        xcodebuild -project Wesnoth.xcodeproj -target Wesnoth
+        xcodebuild -project Wesnoth.xcodeproj -target Wesnoth -configuration Debug
 
         BUILD_RET=$?
 
