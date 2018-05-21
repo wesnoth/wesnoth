039e60ce0e9d3bb0316426775031ad7eb096021e
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2018-05-21 13:17:22 +0200

    Update attack.cpp

--- src/actions/attack.cpp
+++ src/actions/attack.cpp
@@ -127,7 +127,7 @@ battle_context_unit_stats::battle_context_unit_stats(const unit& u,
 	boost::optional<decltype(ctx)> opp_ctx;
 
 	if(opp_weapon) {
-		opp_ctx = opp_weapon->specials_context(&opp, &u, opp_loc, u_loc, !attacking, weapon);
+		opp_ctx.emplace(opp_weapon->specials_context(&opp, &u, opp_loc, u_loc, !attacking, weapon));
 	}
 
 	slows = bool_leadership("slow", units, u_loc, weapon, opp_weapon, attacking);
