0ca822795903e7f0ac93e410b7dde0b8012048fe
Iris Morelle <shadowm@wesnoth.org>
2018-05-27 23:00:42 -0400

    pofix: Update with 1.14.1 -> 1.14.2 rules for the website
    [ci skip]

--- utils/pofix.py
+++ utils/pofix.py
@@ -201,17 +201,17 @@ website_stringfixes = {
 "1.14-announcement" : (
 ("better ingrate them", "better integrate them"),
 
-("wesnoth-1.14.0/wesnoth-1.14.0.tar.bz2", "wesnoth-1.14.1/wesnoth-1.14.1.tar.bz2"),
-("wesnoth-1.14.0/wesnoth-1.14.0-win32.exe", "wesnoth-1.14.1/wesnoth-1.14.1-win32.exe"),
-("wesnoth-1.14.0/Wesnoth_1.14.0.dmg", "wesnoth-1.14.1/Wesnoth_1.14.1a.dmg"),
+("wesnoth-1.14.1/wesnoth-1.14.1.tar.bz2", "wesnoth-1.14.2/wesnoth-1.14.2.tar.bz2"),
+("wesnoth-1.14.1/wesnoth-1.14.1-win32.exe", "wesnoth-1.14.2/wesnoth-1.14.2-win32.exe"),
+("wesnoth-1.14.1/Wesnoth_1.14.1a.dmg", "wesnoth-1.14.2/Wesnoth_1.14.2.dmg"),
 
 # Yes, these are really flimsy. In hindsight it was a bad idea to include
 # download sizes in the announcement. Hopefully we won't end up with
 # files that have the same size in a release/in the last two releases
 # any time soon.
-("442.2", "442.4"),
-("403.1", "403.2"),
-("447.0", "450.5"),
+("442.4", "449.9"),
+("403.2", "407.7"),
+("450.5", "456.4"),
 ),
 
 }
