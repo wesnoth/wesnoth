9c9fbe464950690c8f31ac0e9a8c9a09e0e65477
Charles Dang <exodia339@gmail.com>
2018-06-16 14:03:01 +1100

    Faction Select: fixed changes persisting even if you cancel the dialog
    (cherry-picked from commit c2c613325b075a57728e4bf87659fd2606817630)

--- src/gui/dialogs/multiplayer/faction_select.hpp
+++ src/gui/dialogs/multiplayer/faction_select.hpp
@@ -41,11 +41,16 @@ private:
 
 	group<std::string> gender_toggle_;
 
+	const int last_faction_, last_leader_, last_gender_;
+
 	/** Inherited from modal_dialog, implemented by REGISTER_DIALOG. */
 	virtual const std::string& window_id() const override;
 
 	/** Inherited from modal_dialog. */
 	virtual void pre_show(window& window) override;
+
+	/** Inherited from modal_dialog. */
+	virtual void post_show(window& window) override;
 
 	/** Callbacks */
 	void on_faction_select(window& window);
