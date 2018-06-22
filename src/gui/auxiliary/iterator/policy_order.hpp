cd3f7fd8e92daacace55baaa04c097e2931c012b
Charles Dang <exodia339@gmail.com>
2018-06-23 07:43:53 +1100

    Revert "Fixup c3ed9d2"
    This reverts commit 39efb7a2e4a28bff0f3b90a45aea8031b8cf7348.

--- src/gui/auxiliary/iterator/policy_order.hpp
+++ src/gui/auxiliary/iterator/policy_order.hpp
@@ -16,7 +16,6 @@
 
 #include "gui/auxiliary/iterator/exception.hpp"
 #include "gui/auxiliary/iterator/policy_visit.hpp"
-#include "gui/auxiliary/iterator/walker.hpp"
 #include "gui/core/log.hpp"
 #include "gui/widgets/widget.hpp"
 
