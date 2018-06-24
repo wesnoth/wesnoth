8763e2f2b16ff0c86560342673d825fe83498021
Iris Morelle <shadowm@wesnoth.org>
2018-06-24 00:35:19 -0400

    sound: Halt sound sources that go inaudible instead of fading them out
    Fixes #3280.
    
    (cherry-picked from commit e46c05162fdab578d172cf06e7b35792b045223a)

--- src/sound.cpp
+++ src/sound.cpp
@@ -829,17 +829,7 @@ void reposition_sound(int id, unsigned int distance)
 		}
 
 		if(distance >= DISTANCE_SILENT) {
-			// Don't call Mix_FadeOutChannel if the channel's volume is set to
-			// zero. It doesn't do anything in that case and the channel will
-			// resume playing as soon as its volume is reset to a non-zero
-			// value, which results in issues like sound sources deleted while
-			// their volume is zero coming back to life and escaping Wesnoth's
-			// sound source management code.
-			if(Mix_Volume(ch, -1) == 0) {
-				Mix_HaltChannel(ch);
-			} else {
-				Mix_FadeOutChannel(ch, 100);
-			}
+			Mix_HaltChannel(ch);
 		} else {
 			Mix_SetDistance(ch, distance);
 		}
