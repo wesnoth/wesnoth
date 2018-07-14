2adac53461b62d3f1aa30b496759451555c5361e
Charles Dang <exodia339@gmail.com>
2018-07-14 12:02:44 +1100

    GUI2/Tree View: use NOTIFY_MODIFIED events instead of manual callbacks
    Unlike other widgets where `this` is the event target, the tree nodes fire the event
    with the tree widget itself as the target.
    
    I had added a fire-event call in f97dc8ae12be1b8982ab87b748266f915a84e15d, but with
    the target as the node. I don't think there was any way that would have worked...
    changed.

--- src/gui/widgets/tree_view.hpp
+++ src/gui/widgets/tree_view.hpp
@@ -102,11 +102,6 @@ public:
 		return selected_item_;
 	}
 
-	void set_selection_change_callback(std::function<void(widget&)> callback)
-	{
-		selection_change_callback_ = callback;
-	}
-
 	const std::vector<node_definition>& get_node_definitions() const
 	{
 		return node_definitions_;
@@ -142,8 +137,6 @@ private:
 	tree_view_node* root_node_;
 
 	tree_view_node* selected_item_;
-
-	std::function<void(widget&)> selection_change_callback_;
 
 	/**
 	 * Resizes the content.
