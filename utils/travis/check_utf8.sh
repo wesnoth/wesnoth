b869b760c4bdcb273ddc86c7307e4690ea58cb7c
Jyrki Vesterinen <sandgtx@gmail.com>
2018-07-27 23:49:24 +0300

    check_utf8.sh: don't check the second Lua bytecode test scenario
    This avoids the other file with binary data.

--- utils/travis/check_utf8.sh
+++ utils/travis/check_utf8.sh
@@ -6,7 +6,8 @@
 # wesnoth repository.
 set -e
 find src/ -type f -print0 | xargs -0 isutf8 --
-find data/ -not -name "*.png" -not -name "*.ogg" -not -name "*.jpg" -not -name "*.wav" -not -name "*.gif" -not -name "*.xcf" -not -name "*.bin" -type f -print0 | xargs -0 isutf8 --
+find data/ -not -name "*.png" -not -name "*.ogg" -not -name "*.jpg" -not -name "*.wav" -not -name "*.gif" -not -name "*.xcf" -not -name "*.bin" \
+    -not -name "test_cve_2018_1999023_2.cfg" -type f -print0 | xargs -0 isutf8 --
 find po/ -type f -print0 | xargs -0 isutf8 --
 isutf8 changelog
 isutf8 RELEASE_NOTES
