7cfd22097625be550ddaa3333125df04be8e249d
Jyrki Vesterinen <sandgtx@gmail.com>
2018-08-26 13:42:41 +0300

    Fix another pofix mistake
    That comma wasn't supposed to be replaced with a period, and I didn't
    notice the mistake before pushing the commit.
    
    [ci skip]
    
    (cherry-picked from commit 20bfb35bdda3dca06a9e997812d50bea0e24f341)

--- utils/pofix.py
+++ utils/pofix.py
@@ -87,7 +87,7 @@ game_stringfixes = {
 "wesnoth-aoi": (
 # conversion added in 1.14.4+dev
 ("the bodies of both sides, ", "the bodies of both sides."),
-("there was no hale orc in sight,", "There was no hale orc in sight."),
+("there was no hale orc in sight,", "There was no hale orc in sight,"),
 ),
 
 "wesnoth-editor" : (
