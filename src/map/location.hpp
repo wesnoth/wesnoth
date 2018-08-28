d10230026a4bfd41a286af74760f4004d62f51f4
Gregory A Lundberg <gregory_lundberg@gmx.com>
2018-08-27 19:35:04 -0500

    Fix error: missing noexcept
    GCC 8.2.0 -Wnoexcept flags two functions which should be marked as not throwing.

--- src/map/location.hpp
+++ src/map/location.hpp
@@ -210,7 +210,7 @@ std::ostream &operator<<(std::ostream &s, const std::vector<map_location>& v);
 namespace std {
 template<>
 struct hash<map_location> {
-	size_t operator()(const map_location& l) const {
+	size_t operator()(const map_location& l) const noexcept {
 		// The 2000 bias supposedly ensures that the correct x is recovered for negative y
 		// This implementation copied from the Lua location_set
 		return (l.wml_x()) * 16384 + (l.wml_y()) + 2000;
