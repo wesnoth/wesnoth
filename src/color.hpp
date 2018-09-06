wesnoth/wesnoth

commit 3826c263b1ac203c5487c1a81000c3111bda263b

Fix error: missing noexcept
GCC 8.2.0 -Wnoexcept flags two functions which should be marked as not throwing.

src/color.hpp
@@ -254,7 +254,7 @@ namespace std
 	template<>
 	struct hash<color_t>
 	{
-		std::size_t operator()(const color_t& c) const
+		std::size_t operator()(const color_t& c) const noexcept
 		{
 			return c.to_rgba_bytes();
 		}

src/map/location.hpp
@@ -210,7 +210,7 @@ std::ostream &operator<<(std::ostream &s, const std::vector<map_location>& v);
 namespace std {
 template<>
 struct hash<map_location> {
-	std::size_t operator()(const map_location& l) const {
+	std::size_t operator()(const map_location& l) const noexcept {
 		// The 2000 bias supposedly ensures that the correct x is recovered for negative y
 		// This implementation copied from the Lua location_set
 		return (l.wml_x()) * 16384 + (l.wml_y()) + 2000;

commit 5512eb6a14499fc1646d93f021d180317870e985

Fix build with Visual Studio 2013
Regression from commit 3826c263b1ac203c5487c1a81000c3111bda263b.

src/color.hpp
@@ -13,6 +13,8 @@
 
 #pragma once
 
+#include "global.hpp"
+
 #include <algorithm> // for max
 #include <cstdint>
 #include <ostream>
@@ -250,7 +252,7 @@ namespace std
 	template<>
 	struct hash<color_t>
 	{
-		size_t operator()(const color_t& c) const noexcept
+		size_t operator()(const color_t& c) const NOEXCEPT
 		{
 			return c.to_rgba_bytes();
 		}

src/map/location.hpp
@@ -19,6 +19,8 @@
 class config;
 class variable_set;
 
+#include "global.hpp"
+
 #include <array>
 #include <cmath>
 #include <cstdlib>
@@ -210,7 +212,7 @@ std::ostream &operator<<(std::ostream &s, const std::vector<map_location>& v);
 namespace std {
 template<>
 struct hash<map_location> {
-	size_t operator()(const map_location& l) const noexcept {
+	size_t operator()(const map_location& l) const NOEXCEPT {
 		// The 2000 bias supposedly ensures that the correct x is recovered for negative y
 		// This implementation copied from the Lua location_set
 		return (l.wml_x()) * 16384 + (l.wml_y()) + 2000;