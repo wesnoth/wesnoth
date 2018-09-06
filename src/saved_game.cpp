wesnoth/wesnoth

commit ca6e7aef7db9e5625e4b5db8e9b30f863b83c582

fix #3515 : random start time

src/saved_game.cpp
@@ -134,7 +134,9 @@ void saved_game::set_random_seed()
 		return;
 	}
 
-	carryover_["random_seed"] = randomness::generator->get_random_int(0, INT_MAX);
+	std::stringstream stream;
+	stream << std::setfill('0') << std::setw(8) << std::hex << randomness::generator->get_random_int(0, INT_MAX);
+	carryover_["random_seed"] = stream.str();
 	carryover_["random_calls"] = 0;
 }
 

commit 40b5e8da01c7c4aef928f999da0f77ce2c402b5c

Fix build
Regression from commit ca6e7aef7db9e5625e4b5db8e9b30f863b83c582.

src/saved_game.cpp
@@ -77,6 +77,7 @@
 #include "variable_info.hpp"
 
 #include <cassert>
+#include <iomanip>
 
 static lg::log_domain log_engine("engine");
 #define ERR_NG LOG_STREAM(err, log_engine)