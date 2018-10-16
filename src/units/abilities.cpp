3e1f1f69cc0e859ccf7298dfd559041a4584f2b2
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2018-10-16 10:19:09 +0200

    Update abilities.cpp

--- src/units/abilities.cpp
+++ src/units/abilities.cpp
@@ -824,8 +824,8 @@ void attack_type::modified_attacks(bool is_backstab, unsigned & min_attacks,
 	unit_abilities::effect attacks_effect(get_specials("attacks"),
 	                                      num_attacks(), is_backstab);
 	int attacks_value = attacks_effect.get_composite_value();
-	if(combat_ability("attacks", attacks_value).second){
-            attacks_value = combat_ability("attacks", attacks_value).first;
+	if(combat_ability("attacks", attacks_value, is_backstab).second){
+            attacks_value = combat_ability("attacks", attacks_value, is_backstab).first;
     }
 	if ( attacks_value < 0 ) {
 		attacks_value = num_attacks();
@@ -850,8 +850,8 @@ int attack_type::modified_damage(bool is_backstab) const
 {
 	unit_abilities::effect dmg_effect(get_specials("damage"), damage(), is_backstab);
 	int damage_value = dmg_effect.get_composite_value();
- 	if(combat_ability("damage", damage_value).second){
-    damage_value = combat_ability("damage", damage_value).first;
+ 	if(combat_ability("damage", damage_value, is_backstab).second){
+    damage_value = combat_ability("damage", damage_value, is_backstab).first;
     }
     return damage_value;
 }
