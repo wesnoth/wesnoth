d4bb9babe640838ada56ae6ec05933c3b1b7af5b
Steve Cotton <steve@octalot.co.uk>
2018-10-27 16:45:26 +0200

    Allow players to choose Kaleh or Nym in the tutorial
    Could be separated in to two branches, one making the strings use
    "$student_name" to allow any character, and one for adding Kaleh
    and Nym to the selectable characters.
    
    Inspired by LienRag's work on black protagonists for HttT.
    https://r.wesnoth.org/t48990

--- data/campaigns/tutorial/lua/character_selection.lua
+++ data/campaigns/tutorial/lua/character_selection.lua
@@ -1,7 +1,7 @@
 -- #textdomain wesnoth-tutorial
 
--- Allows the player to choose whether they want to play Konrad or Li’sar
--- for the tutorial
+-- Allows the player to choose whether they want to play Konrad, Li’sar,
+-- Kaleh or Nym for the tutorial
 
 local T = wml.tag
 local wml_actions = wesnoth.wml_actions
@@ -10,7 +10,7 @@ local _ = wesnoth.textdomain "wesnoth-tutorial"
 function wml_actions.select_character()
 	local character_selection_dialog = {
 		maximum_height = 250,
-		maximum_width = 400,
+		maximum_width = 600,
 		T.helptip { id="tooltip_large" }, -- mandatory field
 		T.tooltip { id="tooltip_large" }, -- mandatory field
 		T.grid {
@@ -46,6 +46,14 @@ function wml_actions.select_character()
 								border = "all",
 								border_size = 5,
 								T.image {
+									label = "units/kaleh.png"
+								}
+							},
+							T.column {
+								grow_factor = 1,
+								border = "all",
+								border_size = 5,
+								T.image {
 									label = "units/konrad-fighter.png"
 								}
 							},
@@ -56,9 +64,26 @@ function wml_actions.select_character()
 								T.image {
 									label = "units/human-princess.png~TC(1,magenta)"
 								}
+							},
+							T.column {
+								grow_factor = 1,
+								border = "all",
+								border_size = 5,
+								T.image {
+									label = "units/nym.png"
+								}
 							}
 						},
 						T.row {
+							T.column {
+								grow_factor = 1,
+								border = "all",
+								border_size = 5,
+								T.button {
+									label = _"Kaleh",
+									return_value = 3
+								}
+							},
 							T.column {
 								grow_factor = 1,
 								border = "all",
@@ -75,6 +100,15 @@ function wml_actions.select_character()
 								T.button {
 									label = _"Li’sar",
 									return_value = 2
+								}
+							},
+							T.column {
+								grow_factor = 1,
+								border = "all",
+								border_size = 5,
+								T.button {
+									label = _"Nym",
+									return_value = 4
 								}
 							}
 						}
@@ -100,9 +134,37 @@ function wml_actions.select_character()
 		wesnoth.sides[1].side_name = _"Li’sar"
 		-- enable the help to display this unit's page
 		wesnoth.add_known_unit("Fighteress")
+	elseif character == 3 then
+		wesnoth.put_unit({
+			type = "Tutorial_Quenoth_Fighter",
+			id = unit.id,
+			name = _"Kaleh",
+			unrenamable = true,
+			profile = "portraits/kaleh.png",
+			canrecruit = true,
+			facing = unit.facing,
+		}, unit.x, unit.y )
+		wesnoth.sides[1].side_name = _"Kaleh"
+		-- enable the help to display this unit's page
+		wesnoth.add_known_unit("Tutorial_Quenoth_Fighter")
+	elseif character == 4 then
+		wesnoth.put_unit({
+			type = "Tutorial_Quenoth_Fighteress",
+			id = unit.id,
+			name = _"Nym",
+			unrenamable = true,
+			profile = "portraits/nym_no_bow.png",
+			canrecruit = true,
+			facing = unit.facing,
+		}, unit.x, unit.y )
+		wesnoth.sides[1].side_name = _"Nym"
+		-- enable the help to display this unit's page
+		wesnoth.add_known_unit("Tutorial_Quenoth_Fighteress")
 	else
 		wesnoth.put_unit(unit)
 	end
+
+	wml.variables.student_name = wesnoth.sides[1].side_name
 
 	wesnoth.redraw {}
 end
