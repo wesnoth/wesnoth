wesnoth/wesnoth

commit e2a64541002f0b4ee885f699c4f0901e441f938a

Made use of config_attribute_value::to_time_t
(cherry-picked from commit 6dfb87103c629cd91d57fc87934a3fb2d1177834)

src/campaign_server/campaign_server.cpp
@@ -587,14 +587,14 @@ void server::handle_request_campaign_list(const server::request& req)
 	bool before_flag = false;
 	time_t before = epoch;
 	try {
-		before = before + lexical_cast<time_t>(req.cfg["before"]);
+		before += req.cfg["before"].to_time_t();
 		before_flag = true;
 	} catch(bad_lexical_cast) {}
 
 	bool after_flag = false;
 	time_t after = epoch;
 	try {
-		after = after + lexical_cast<time_t>(req.cfg["after"]);
+		after += req.cfg["after"].to_time_t();
 		after_flag = true;
 	} catch(bad_lexical_cast) {}
 
@@ -611,12 +611,12 @@ void server::handle_request_campaign_list(const server::request& req)
 			continue;
 		}
 
-		const std::string& tm = i["timestamp"];
+		const auto& tm = i["timestamp"];
 
-		if(before_flag && (tm.empty() || lexical_cast_default<time_t>(tm, 0) >= before)) {
+		if(before_flag && (tm.empty() || tm.to_time_t(0) >= before)) {
 			continue;
 		}
-		if(after_flag && (tm.empty() || lexical_cast_default<time_t>(tm, 0) <= after)) {
+		if(after_flag && (tm.empty() || tm.to_time_t(0) <= after)) {
 			continue;
 		}
 

src/save_index.cpp
@@ -85,7 +85,7 @@ config& save_index_class::get(const std::string& name)
 	time_t m = modified_[name];
 
 	config::attribute_value& mod_time = result["mod_time"];
-	if(mod_time.empty() || static_cast<time_t>(mod_time.to_int()) != m) {
+	if(mod_time.empty() || mod_time.to_time_t() != m) {
 		rebuild(name, m);
 	}
 

src/server/ban.cpp
@@ -174,9 +174,9 @@ static lg::log_domain log_server("server");
 		}
 		nick_ = cfg["nick"].str();
 		if (cfg.has_attribute("end_time"))
-			end_time_ = lexical_cast_default<time_t>(cfg["end_time"], 0);
+			end_time_ = cfg["end_time"].to_time_t(0);
 		if (cfg.has_attribute("start_time"))
-			start_time_ = lexical_cast_default<time_t>(cfg["start_time"], 0);
+			start_time_ = cfg["start_time"].to_time_t(0);
 		reason_ = cfg["reason"].str();
 
 		// only overwrite defaults if exists

src/server/server.cpp
@@ -405,7 +405,7 @@ void server::load_config() {
 
 	admin_passwd_ = cfg_["passwd"].str();
 	motd_ = cfg_["motd"].str();
-	lan_server_ = lexical_cast_default<time_t>(cfg_["lan_server"], 0);
+	lan_server_ = cfg_["lan_server"].to_time_t(0);
 	uh_name_ = cfg_["user_handler"].str();
 
 	deny_unregistered_login_ = cfg_["deny_unregistered_login"].to_bool();

commit 318a6242ee855bca754404ad369d39069e4c594d

campaignd: Explicitly check for before= and after= in add-ons list requests
A regression introduced in e2a64541002f0b4ee885f699c4f0901e441f938a
causes the timestamp-based list filtering to be forcibly enabled in ALL
cases because the author missed that the old code would bail out due to
lexical_cast() throwing an exception if the provided input was empty,
such as when before/after aren't provided in the list request. As a
result, the add-ons list would come up empty for clients because nothing
was matching the resulting time criteria.

The old pattern was a bit oblique to say the least, so I decided it'd be
best to replace it with an explicit check for the attributes' existence
and contents.

src/campaign_server/campaign_server.cpp
@@ -586,17 +586,17 @@ void server::handle_request_campaign_list(const server::request& req)
 
 	bool before_flag = false;
 	std::time_t before = epoch;
-	try {
+	if(!req.cfg["before"].empty()) {
 		before += req.cfg["before"].to_time_t();
 		before_flag = true;
-	} catch(const bad_lexical_cast&) {}
+	}
 
 	bool after_flag = false;
 	std::time_t after = epoch;
-	try {
+	if(!req.cfg["after"].empty()) {
 		after += req.cfg["after"].to_time_t();
 		after_flag = true;
-	} catch(const bad_lexical_cast&) {}
+	}
 
 	const std::string& name = req.cfg["name"];
 	const std::string& lang = req.cfg["language"];