wesnoth/wesnoth

commit 05cd1521ffb13f33a2879dc660be5a9164afdbc9

Dark Forecast: Enemies that spawn on a village now take ownership of that village.
Fixes #3890

data/multiplayer/scenarios/2p_Dark_Forecast.lua
@@ -239,6 +239,7 @@ local function place_units(unittypes, x, y)
 		})
 		local dst_x, dst_y = wesnoth.find_vacant_tile(x, y, u)
 		u:to_map(dst_x, dst_y)
+		wesnoth.set_village_owner(dst_x, dst_y, 1)
 	end
 end
 

commit 04e840b0254458e5e117e5b175542820985abf64

Dark Forecast: add placed units to the help
Resolved conflict caused by 05cd1521ffb13f33a2879dc660be5a9164afdbc9 on
master.

data/multiplayer/scenarios/2p_Dark_Forecast.lua
@@ -239,6 +239,7 @@ local function place_units(unittypes, x, y)
 		})
 		local dst_x, dst_y = wesnoth.find_vacant_tile(x, y, u)
 		u:to_map(dst_x, dst_y)
+		wesnoth.add_known_unit(v)
 	end
 end
 