wesnoth/wesnoth

commit 8ee6ed786cc42defb0bc20d3a3b5891479d5ddea

Sidebar: Add jamming to the vision tooltip.
See #3327.

src/reports.cpp
@@ -618,7 +618,12 @@ static config unit_vision(const unit* u)
 	std::ostringstream str, tooltip;
 	if (u->vision() != u->total_movement()) {
 		str << _("vision:") << ' ' << u->vision();
-		tooltip << _("vision:") << ' ' << u->vision();
+		tooltip << _("vision:") << ' ' << u->vision() << '\n';
+	}
+	if (u->jamming() != 0) {
+		if (str.tellp() == 0)
+			str << _("jamming:") << ' ' << u->jamming();
+		tooltip << _("jamming:") << ' ' << u->jamming() << '\n';
 	}
 	return text_report(str.str(), tooltip.str());
 }

commit 0379e2e8329b62dce6d4a7723641cba673c66740

Fix build with Visual Studio
Regression from commit 8ee6ed786cc42defb0bc20d3a3b5891479d5ddea.

src/reports.cpp
@@ -621,7 +621,7 @@ static config unit_vision(const unit* u)
 		tooltip << _("vision:") << ' ' << u->vision() << '\n';
 	}
 	if (u->jamming() != 0) {
-		if (str.tellp() == 0)
+		if (static_cast<std::streamoff>(str.tellp()) == 0)
 			str << _("jamming:") << ' ' << u->jamming();
 		tooltip << _("jamming:") << ' ' << u->jamming() << '\n';
 	}