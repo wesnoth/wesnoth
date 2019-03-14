45a0e887e47d4bcdbb04ccf7149c0a7f5afa136c
nemaara <nemaara@gmail.com>
2019-03-14 10:38:47 -0400

    Revert "Dark Forecast: fix boss spawns not occurring"
    This reverts commit 4f33408c96bee9ef7fa2d5e90052c51ad3223771.

--- data/multiplayer/scenarios/2p_Dark_Forecast.lua
+++ data/multiplayer/scenarios/2p_Dark_Forecast.lua
@@ -313,8 +313,6 @@ on_event("new turn", function()
 		canrecruit=true,
 		message= _ "The last and most powerful of these creatures are almost upon us. I feel that if we can finish them off in time, we shall be victorious.",
 	}
-
-	wml.variables["next_final_spawn"] = wesnoth.current.turn + wesnoth.random(1,2)
 end)
 
 -- after the first final spawn, spawn a new final spawn every 1 or 2 turns.
