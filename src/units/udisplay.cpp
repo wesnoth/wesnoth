wesnoth/wesnoth

commit 83ba09a5a98b817a3e50a89dd6b41d2f779af5ff

Unit Display: When the recruiting or recruited units is invisible, don't scroll to it.
When the enemy leader and the unit it recruits are both invisible,
scrolling to them would leak their location to the player.

src/units/udisplay.cpp
@@ -698,20 +698,34 @@ void reset_helpers(const unit *attacker,const unit *defender)
 void unit_recruited(const map_location& loc,const map_location& leader_loc)
 {
 	game_display* disp = game_display::get_singleton();
+	const display_context& dc = disp->get_disp_context();
+	const team& viewing_team = dc.get_team(disp->viewing_side());
 	if(!disp || disp->video().update_locked() || disp->video().faked() ||disp->fogged(loc)) return;
+
 	unit_map::const_iterator u = disp->get_units().find(loc);
 	if(u == disp->get_units().end()) return;
+	const bool unit_visible = u->is_visible_to_team(viewing_team, dc, false);
+
+	unit_map::const_iterator leader = disp->get_units().find(leader_loc); // may be null_location
+	const bool leader_visible = (leader != disp->get_units().end()) && leader->is_visible_to_team(viewing_team, dc, false);
+
 	u->set_hidden(true);
 
 	unit_animator animator;
-	if(leader_loc != map_location::null_location()) {
-		unit_map::const_iterator leader = disp->get_units().find(leader_loc);
-		if(leader == disp->get_units().end()) return;
+	if (leader_visible && unit_visible) {
 		disp->scroll_to_tiles(loc,leader_loc,game_display::ONSCREEN,true,0.0,false);
-		leader->set_facing(leader_loc.get_relative_dir(loc));
-		animator.add_animation(&*leader, "recruiting", leader_loc, loc, 0, true);
-	} else {
+	} else if (leader_visible) {
+		disp->scroll_to_tile(leader_loc,game_display::ONSCREEN,true,false);
+	} else if (unit_visible) {
 		disp->scroll_to_tile(loc,game_display::ONSCREEN,true,false);
+	} else {
+		return;
+	}
+	if (leader != disp->get_units().end()) {
+		leader->set_facing(leader_loc.get_relative_dir(loc));
+		if (leader_visible) {
+			animator.add_animation(&*leader, "recruiting", leader_loc, loc, 0, true);
+		}
 	}
 
 	disp->draw();

commit 9f4030ab3807afa25e0ed785ed78a0b06547f962

Unhide recruited unit when both the recruiter and recruitee are invisible to the viewing team.
This looks wrong, as though it would be a regression introduced by
83ba09a5a98b817a3e50a89dd6b41d2f779af5ff, but I found no difference in my testing.

src/units/udisplay.cpp
@@ -746,6 +746,7 @@ void unit_recruited(const map_location& loc,const map_location& leader_loc)
 	unit_map::const_iterator leader = disp->get_units().find(leader_loc); // may be null_location
 	const bool leader_visible = (leader != disp->get_units().end()) && leader->is_visible_to_team(viewing_team, false);
 
+	// TODO: Use a RAII object such as wb::temporary_unit_hider
 	u->set_hidden(true);
 
 	unit_animator animator;
@@ -757,6 +758,7 @@ void unit_recruited(const map_location& loc,const map_location& leader_loc)
 	} else if (unit_visible) {
 		disp->scroll_to_tile(loc,game_display::ONSCREEN,true,false);
 	} else {
+		u->set_hidden(false);
 		return;
 	}
 	if (leader != disp->get_units().end()) {