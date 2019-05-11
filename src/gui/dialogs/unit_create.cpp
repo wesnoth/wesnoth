c56245958d10cf70ee18c520f53100c7ba8e3305
josteph <josteph@fastmail.com>
2019-05-11 10:49:18 +0000

    unit_create: Remove debug check
    It's not needed in this debug-only dialog.
    
    Thanks @ProditorMagnus

--- src/gui/dialogs/unit_create.cpp
+++ src/gui/dialogs/unit_create.cpp
@@ -126,7 +126,7 @@ void unit_create::pre_show(window& window)
 		row_data.emplace("race", column);
 
 		column["label"] = units_.back()->type_name();
-		if(game_config::debug && strcmp(units_.back()->type_name().c_str(), units_.back()->id().c_str()))
+		if(strcmp(units_.back()->type_name().c_str(), units_.back()->id().c_str()))
 			column["label"] += " (" + units_.back()->id() + ")";
 		row_data.emplace("unit_type", column);
 
