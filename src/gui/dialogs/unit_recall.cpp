32f39f8000cd532008c62a1eca060495fd5c0ed7
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2019-05-16 00:08:39 +0200

    add other overlay

--- src/gui/dialogs/unit_recall.cpp
+++ src/gui/dialogs/unit_recall.cpp
@@ -185,6 +185,10 @@ void unit_recall::pre_show(window& window)
 			mods += "~BLIT(" + overlay + ")";
 		}
 
+		if(!unit->image_overlay().empty()) {
+			mods += "~BLIT(" + unit->image_overlay() + ")";
+		}
+
 		column["use_markup"] = "true";
 
 		column["label"] = unit->absolute_image() + mods;
