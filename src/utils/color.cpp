a78466c059a468b78f9023bdf2f952e04d48f749
Alexander van Gessel <ai0867@gmail.com>
2019-06-21 16:58:43 +0200

    Fix a floating-point edge-case
    We're trying to do division + modulo.
    floor + fmod can have inconsistent results around integers.
    (floor rounding less than one unit up, with fmod returning slightly less than the divisor)
    lround + remainder will return negative remainders when rounding up, which needs to be compensated for,
    but is at least consistent.
    
    Details at: https://stackoverflow.com/q/56580411/145413

--- src/utils/color.cpp
+++ src/utils/color.cpp
@@ -33,19 +33,24 @@ namespace utils {
 
 		value = utils::clamp(value, 0., 100.);
 
+		// lround + remainder will return negative remainders when rounding up, which needs to be compensated for,
+		// but using floor + fmod can have inconsistent results around integers.
+		// (floor rounding less than one unit up, with fmod returning slightly less than the divisor)
+		// For details, see: https://stackoverflow.com/q/56580411/145413
 		const double stepsize = 100.0 / (color_scale.size() - 1);
-		const double idx_d = value / stepsize;
-		const long idx_l = std::lround(std::floor(idx_d));
-		const int idx_i = static_cast<int>(idx_l);
-		const double remainder = std::fmod(value, stepsize);
-		const double spline_fraction = remainder / stepsize;
+		const double whole = std::lround(value / stepsize);
+		const double remain = std::remainder(value, stepsize);
 
-		if(idx_i >= static_cast<signed>(color_scale.size()) - 1){
+		const bool off_by_one = remain < 0;
+		const int index = off_by_one ? static_cast<int>(whole - 1) : static_cast<int>(whole);
+		const double spline_fraction = off_by_one ? (remain / stepsize) + 1 : remain / stepsize;
+
+		if(index >= static_cast<signed>(color_scale.size()) - 1){
 			return color_scale.back();
 		}
 
-		const color_t& left = color_scale[idx_i];
-		const color_t& right = color_scale[idx_i + 1];
+		const color_t& left = color_scale[index];
+		const color_t& right = color_scale[index + 1];
 		return {
 			static_cast<uint8_t>(std::lround(left.r * (1 - spline_fraction) + right.r * spline_fraction)),
 			static_cast<uint8_t>(std::lround(left.g * (1 - spline_fraction) + right.g * spline_fraction)),
