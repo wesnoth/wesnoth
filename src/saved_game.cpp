533facbe83f2317ed4a7bbc7aaf3d82726e6a057
Charles Dang <exodia339@gmail.com>
2019-07-01 18:29:58 +1100

    Ensure invalid savename characters are stripped from scenario names earlier (fixes #4116)
    This ensures the invalid characters are never saved internally in the first place, so this should
    cover any case besides the aforementioned bug that might crop up.

--- src/saved_game.cpp
+++ src/saved_game.cpp
@@ -85,6 +85,38 @@ static lg::log_domain log_engine("engine");
 #define LOG_NG LOG_STREAM(info, log_engine)
 #define DBG_NG LOG_STREAM(debug, log_engine)
 
+namespace
+{
+bool variable_to_bool(const config& vars, const std::string& expression)
+{
+	std::string res = utils::interpolate_variables_into_string(expression, config_variable_set(vars));
+	return res == "true" || res == "yes" || res == "1";
+}
+
+// helper objects for saved_game::expand_mp_events()
+struct modevents_entry
+{
+	modevents_entry(const std::string& _type, const std::string& _id)
+		: type(_type)
+		, id(_id)
+	{
+	}
+
+	std::string type;
+	std::string id;
+};
+
+bool is_illegal_file_char(char c)
+{
+	return c == '/' || c == '\\' || c == ':' || (c >= 0x00 && c < 0x20)
+#ifdef _WIN32
+	|| c == '?' || c == '|' || c == '<' || c == '>' || c == '*' || c == '"'
+#endif
+	;
+}
+
+} // end anon namespace
+
 saved_game::saved_game()
 	: has_carryover_expanded_(false)
 	, carryover_(carryover_info().to_config())
@@ -274,29 +306,6 @@ void saved_game::check_require_scenario()
 
 	mp_settings_.update_addon_requirements(required_scenario);
 }
-
-namespace
-{
-bool variable_to_bool(const config& vars, const std::string& expression)
-{
-	std::string res = utils::interpolate_variables_into_string(expression, config_variable_set(vars));
-	return res == "true" || res == "yes" || res == "1";
-}
-
-// helper objects for saved_game::expand_mp_events()
-struct modevents_entry
-{
-	modevents_entry(const std::string& _type, const std::string& _id)
-		: type(_type)
-		, id(_id)
-	{
-	}
-
-	std::string type;
-	std::string id;
-};
-
-} // end anon namespace
 
 void saved_game::load_mod(const std::string& type, const std::string& id, size_t pos)
 {
@@ -620,11 +629,16 @@ bool saved_game::not_corrupt() const
 
 void saved_game::update_label()
 {
+	std::string& label = classification().label;
+
 	if(classification().abbrev.empty()) {
-		classification().label = starting_point_["name"].str();
+		label = starting_point_["name"].str();
 	} else {
-		classification().label = classification().abbrev + "-" + starting_point_["name"];
+		label = classification().abbrev + "-" + starting_point_["name"];
 	}
+
+	label.erase(std::remove_if(label.begin(), label.end(), is_illegal_file_char), label.end());
+	std::replace(label.begin(), label.end(), '_', ' ');
 }
 
 void saved_game::cancel_orders()
