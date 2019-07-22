bcef6c52f852be9fd9ff5614c3c1483163469e61
Steve Cotton <steve@octalot.co.uk>
2019-07-22 12:44:44 +0200

    Add [find_path] option "nearest_by", and simple_find_path test
    Adding this is issue 2 of #4177, changing the behavior when [find_path]
    is given a SLF which matches multiple hexes.
    
    The map and tests here should be easy enough for manually editing them. It
    duplicates some of the functionality of the existing characterize_pathfinding
    tests, however those tests need their expected values to be calculated and
    can't be changed by hand.
    
    '''nearest_by''': {DevFeature1.15|2} possible values "movement_cost"
    (default), "steps", "hexes". If the [destination] SLF matches multiple hexes,
    the one that would need the least movement points to reach may not be the one
    that's closest as measured by '''hexes''', or closest as measured by steps,
    from the starting point.
    
    Behavior in 1.14 depended on which hex was checked first.

--- data/lua/wml/find_path.lua
+++ data/lua/wml/find_path.lua
@@ -26,6 +26,19 @@ function wesnoth.wml_actions.find_path(cfg)
 	local allow_multiple_turns = cfg.allow_multiple_turns
 	local viewing_side
 
+	local nearest_by_cost = true
+	local nearest_by_distance = false
+	local nearest_by_steps = false
+	if (cfg.nearest_by or "movement_cost") == "hexes" then
+		nearest_by_cost = false
+		nearest_by_distance = true
+		nearest_by_steps = false
+	elseif (cfg.nearest_by or "movement_cost") == "steps" then
+		nearest_by_cost = false
+		nearest_by_distance = false
+		nearest_by_steps = true
+	end
+
 	if not cfg.check_visibility then viewing_side = 0 end -- if check_visiblity then shroud is taken in account
 
 	-- only the first location with the lowest distance and lowest movement cost will match.
@@ -33,32 +46,57 @@ function wesnoth.wml_actions.find_path(cfg)
 
 	local max_cost = nil
 	if not allow_multiple_turns then max_cost = unit.moves end --to avoid wrong calculation on already moved units
-	local current_distance, current_cost = math.huge, math.huge
+	local current_distance, current_cost, current_steps = math.huge, math.huge, math.huge
 	local current_location = {}
 
 	local width,heigth = wesnoth.get_map_size() -- data for test below
 
 	for index, location in ipairs(locations) do
 		-- we test if location passed to pathfinder is invalid (border);
-		-- if it is, do not return and continue the cycle
+		-- if it is, do not use it, and continue the cycle
 		if location[1] == 0 or location[1] == ( width + 1 ) or location[2] == 0 or location[2] == ( heigth + 1 ) then
 		else
 			local distance = wesnoth.map.distance_between ( unit.x, unit.y, location[1], location[2] )
-			-- if we pass an unreachable locations an high value will be returned
+			-- if we pass an unreachable location then an empty path and high value cost will be returned
 			local path, cost = wesnoth.find_path( unit, location[1], location[2], { max_cost = max_cost, ignore_units = ignore_units, ignore_teleport = ignore_teleport, viewing_side = viewing_side } )
 
-			if distance < current_distance and cost <= current_cost
-				or cost < current_cost and distance <= current_distance
-			then
-				-- avoid changing the hex with one less distance and more cost, or vice versa
-				current_distance = distance
-				current_cost = cost
-				current_location = location
+			if #path == 0 or cost >= 42424241 then
+				-- it's not a reachable hex. 42424242 is the high value returned for unwalkable or busy terrains
+			else
+				local steps = #path
+
+				local is_better = false
+				if nearest_by_cost and cost < current_cost then
+					is_better = true
+				elseif nearest_by_distance and distance < current_distance then
+					is_better = true
+				elseif nearest_by_steps and steps < current_steps then
+					is_better = true
+				elseif cost == current_cost and distance == current_distance and steps == current_steps then
+					-- the two options are equivalent. Treating this as not-better probably creates a bias for
+					-- choosing the north-west option, treating it as better probably biases to south-east.
+					-- Choosing false is more likely to match the option that 1.14 would choose.
+					is_better = false
+				elseif cost <= current_cost and distance <= current_distance and steps <= current_steps then
+					is_better = true
+				end
+
+				if is_better then
+					current_distance = distance
+					current_cost = cost
+					current_steps = steps
+					current_location = location
+				end
 			end
 		end
 	end
 
-	if #current_location == 0 then wesnoth.message("WML warning","[find_path]'s filter didn't match any location")
+	if #current_location == 0 then
+		-- either no matching locations, or only inaccessible matching locations (maybe enemy units are there)
+		if #locations == 0 then
+			wesnoth.message("WML warning","[find_path]'s filter didn't match any location")
+		end
+		wml.variables[tostring(variable)] = { hexes = 0 } -- set only hexes, nil all other values
 	else
 		local path, cost = wesnoth.find_path(
 			unit,
@@ -77,7 +115,7 @@ function wesnoth.wml_actions.find_path(cfg)
 			turns = math.ceil( ( ( cost - unit.moves ) / unit.max_moves ) + 1 )
 		end
 
-		if cost >= 42424242 then -- it's the high value returned for unwalkable or busy terrains
+		if cost >= 42424241 then -- it's the high value returned for unwalkable or busy terrains
 			wml.variables[tostring(variable)] = { hexes = 0 } -- set only length, nil all other values
 			-- support for $this_unit
 			wml.variables["this_unit"] = nil -- clearing this_unit
