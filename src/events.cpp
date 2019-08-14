wesnoth/wesnoth

commit 4139b43cc900b72756f07267dbb887c469d6f4c2

Fix -Wdeprecated-copy compiler warnings with gcc 9
(partial cherry pick from commit 05d109215d71e692d2bdcd1420f8bbe065788fb8)

src/widgets/widget.cpp
@@ -30,14 +30,6 @@ namespace gui {
 
 bool widget::mouse_lock_ = false;
 
-widget::widget(const widget &o)
-	: events::sdl_handler(), focus_(o.focus_), video_(o.video_), restorer_(o.restorer_), rect_(o.rect_),
-	   needs_restore_(o.needs_restore_), state_(o.state_), hidden_override_(o.hidden_override_),
-	  enabled_(o.enabled_), clip_(o.clip_), clip_rect_(o.clip_rect_), volatile_(o.volatile_),
-	  help_text_(o.help_text_), tooltip_text_(o.tooltip_text_), help_string_(o.help_string_), id_(o.id_), mouse_lock_local_(o.mouse_lock_local_)
-{
-}
-
 widget::widget(CVideo& video, const bool auto_join)
 	: events::sdl_handler(auto_join), focus_(true), video_(&video), rect_(EmptyRect), needs_restore_(false),
 	  state_(UNINIT), hidden_override_(false), enabled_(true), clip_(false),

src/widgets/widget.hpp
@@ -69,7 +69,6 @@ public:
 	virtual void process_tooltip_string(int mousex, int mousey);
 
 protected:
-	widget(const widget& o);
 	widget(CVideo& video, const bool auto_join=true);
 	virtual ~widget();
 

src/game_classification.cpp
@@ -28,27 +28,6 @@ static lg::log_domain log_engine("engine");
 /// The default difficulty setting for campaigns.
 const std::string DEFAULT_DIFFICULTY("NORMAL");
 
-game_classification::game_classification()
-	: label()
-	, version()
-	, campaign_type()
-	, campaign_define()
-	, campaign_xtra_defines()
-	, scenario_define()
-	, era_define()
-	, mod_defines()
-	, campaign()
-	, campaign_name()
-	, abbrev()
-	, end_credits(true)
-	, end_text()
-	, end_text_duration()
-	, difficulty(DEFAULT_DIFFICULTY)
-	, random_mode("")
-	, oos_debug(false)
-{
-}
-
 game_classification::game_classification(const config& cfg)
 	: label(cfg["label"])
 	, version(cfg["version"])
@@ -68,27 +47,6 @@ game_classification::game_classification(const config& cfg)
 	, difficulty(cfg["difficulty"].empty() ? DEFAULT_DIFFICULTY : cfg["difficulty"].str())
 	, random_mode(cfg["random_mode"])
 	, oos_debug(cfg["oos_debug"].to_bool(false))
-{
-}
-
-game_classification::game_classification(const game_classification& gc)
-	: label(gc.label)
-	, version(gc.version)
-	, campaign_type(gc.campaign_type)
-	, campaign_define(gc.campaign_define)
-	, campaign_xtra_defines(gc.campaign_xtra_defines)
-	, scenario_define(gc.scenario_define)
-	, era_define(gc.era_define)
-	, mod_defines(gc.mod_defines)
-	, campaign(gc.campaign)
-	, campaign_name(gc.campaign_name)
-	, abbrev(gc.abbrev)
-	, end_credits(gc.end_credits)
-	, end_text(gc.end_text)
-	, end_text_duration(gc.end_text_duration)
-	, difficulty(gc.difficulty)
-	, random_mode(gc.random_mode)
-	, oos_debug(gc.oos_debug)
 {
 }
 

src/game_classification.hpp
@@ -27,9 +27,8 @@ extern const std::string DEFAULT_DIFFICULTY;
 class game_classification
 {
 public:
-	game_classification();
+	game_classification() = default;
 	explicit game_classification(const config& cfg);
-	game_classification(const game_classification& gc);
 
 	config to_config() const;
 	std::string get_tagname() const;
@@ -54,10 +53,10 @@ public:
 	std::string campaign_name;                       /**< The name of the campaign being played. */
 
 	std::string abbrev;                              /**< the campaign abbreviation */
-	bool end_credits;                                /**< whether to show the standard credits at the end */
+	bool end_credits = true;                                /**< whether to show the standard credits at the end */
 	std::string end_text;                            /**< end-of-campaign text */
 	unsigned int end_text_duration;                  /**< for how long the end-of-campaign text is shown */
-	std::string difficulty; /**< The difficulty level the game is being played on. */
-	std::string random_mode;
-	bool oos_debug;
+	std::string difficulty = DEFAULT_DIFFICULTY; /**< The difficulty level the game is being played on. */
+	std::string random_mode = "";
+	bool oos_debug = false;
 };

src/picture.cpp
@@ -358,17 +358,6 @@ locator& locator::operator=(const locator& a)
 	return *this;
 }
 
-locator::value::value(const locator::value& a)
-	: type_(a.type_)
-	, is_data_uri_(a.is_data_uri_)
-	, filename_(a.filename_)
-	, loc_(a.loc_)
-	, modifications_(a.modifications_)
-	, center_x_(a.center_x_)
-	, center_y_(a.center_y_)
-{
-}
-
 locator::value::value()
 	: type_(NONE)
 	, is_data_uri_(false)

src/picture.hpp
@@ -42,7 +42,6 @@ namespace image {
 		void parse_arguments();
 		struct value {
 			value();
-			value(const value &a);
 			value(const char *filename);
 			value(const std::string& filename);
 			value(const std::string& filename, const std::string& modifications);

commit ecec2c5188df2acc4fc9ccf181e9586d54202176

Rule of Five for event::sdl_handler
This fixes the default implementations of widget's copy constructor and copy
assignment, which was root cause of the regression in 4139b43cc900 (#4215).

This reverts commit f46ed66f2c9a8acf8d88338d8433fc35689d4c39.

src/events.cpp
@@ -86,6 +86,14 @@ void context::add_handler(sdl_handler* ptr)
 	staging_handlers.push_back(ptr);
 }
 
+bool context::has_handler(const sdl_handler* ptr) const
+{
+	if(handlers.cend() != std::find(handlers.cbegin(), handlers.cend(), ptr)) {
+		return true;
+	}
+	return staging_handlers.cend() != std::find(staging_handlers.cbegin(), staging_handlers.cend(), ptr);
+}
+
 bool context::remove_handler(sdl_handler* ptr)
 {
 	static int depth = 0;
@@ -234,6 +242,49 @@ sdl_handler::sdl_handler(const bool auto_join)
 	}
 }
 
+sdl_handler::sdl_handler(const sdl_handler &that)
+	: has_joined_(that.has_joined_)
+	, has_joined_global_(that.has_joined_global_)
+{
+	if(has_joined_global_) {
+		assert(!event_contexts.empty());
+		event_contexts.front().add_handler(this);
+	} else if(has_joined_) {
+		bool found_context = false;
+		for(auto &context : boost::adaptors::reverse(event_contexts)) {
+			if(context.has_handler(&that)) {
+				found_context = true;
+				context.add_handler(this);
+				break;
+			}
+		}
+
+		if (!found_context) {
+			throw std::logic_error("Copy-constructing a sdl_handler that has_joined_ but can't be found by searching contexts");
+		}
+	}
+}
+
+sdl_handler &sdl_handler::operator=(const sdl_handler &that)
+{
+	if(that.has_joined_global_) {
+		join_global();
+	} else if(that.has_joined_) {
+		for(auto &context : boost::adaptors::reverse(event_contexts)) {
+			if(context.has_handler(&that)) {
+				join(context);
+				break;
+			}
+		}
+	} else if(has_joined_) {
+		leave();
+	} else if(has_joined_global_) {
+		leave_global();
+	}
+
+	return *this;
+}
+
 sdl_handler::~sdl_handler()
 {
 	if(has_joined_) {
@@ -281,8 +332,7 @@ void sdl_handler::join_same(sdl_handler* parent)
 	}
 
 	for(auto& context : boost::adaptors::reverse(event_contexts)) {
-		handler_list& handlers = context.handlers;
-		if(std::find(handlers.begin(), handlers.end(), parent) != handlers.end()) {
+		if(context.has_handler(parent)) {
 			join(context);
 			return;
 		}

src/events.hpp
@@ -51,6 +51,8 @@ public:
 	context(const context&) = delete;
 
 	void add_handler(sdl_handler* ptr);
+	/** Returns true if @a ptr is found in either the handlers or staging_handlers lists */
+	bool has_handler(const sdl_handler* ptr) const;
 	bool remove_handler(sdl_handler* ptr);
 	void cycle_focus();
 	void set_focus(const sdl_handler* ptr);
@@ -95,8 +97,19 @@ public:
 
 	virtual bool has_joined() { return has_joined_;}
 	virtual bool has_joined_global() { return has_joined_global_;}
+
+	/**
+	 * Moving would require two instances' context membership to be handled,
+	 * it's simpler to delete these and require the two instances to be
+	 * separately constructed / destructed.
+	 */
+	sdl_handler &operator=(sdl_handler &&) = delete;
+	sdl_handler(sdl_handler &&) = delete;
+
 protected:
 	sdl_handler(const bool auto_join=true);
+	sdl_handler(const sdl_handler &);
+	sdl_handler &operator=(const sdl_handler &);
 	virtual ~sdl_handler();
 	virtual std::vector<sdl_handler*> handler_members()
 	{

src/widgets/widget.cpp
@@ -30,27 +30,6 @@ namespace gui {
 
 bool widget::mouse_lock_ = false;
 
-widget::widget(const widget& o)
-	: events::sdl_handler()
-	, focus_(o.focus_)
-	, video_(o.video_)
-	, restorer_(o.restorer_)
-	, rect_(o.rect_)
-	, needs_restore_(o.needs_restore_)
-	, state_(o.state_)
-	, hidden_override_(o.hidden_override_)
-	, enabled_(o.enabled_)
-	, clip_(o.clip_)
-	, clip_rect_(o.clip_rect_)
-	, volatile_(o.volatile_)
-	, help_text_(o.help_text_)
-	, tooltip_text_(o.tooltip_text_)
-	, help_string_(o.help_string_)
-	, id_(o.id_)
-	, mouse_lock_local_(o.mouse_lock_local_)
-{
-}
-
 widget::widget(CVideo& video, const bool auto_join)
 	: events::sdl_handler(auto_join), focus_(true), video_(&video), rect_(EmptyRect), needs_restore_(false),
 	  state_(UNINIT), hidden_override_(false), enabled_(true), clip_(false),

src/widgets/widget.hpp
@@ -69,7 +69,6 @@ public:
 	virtual void process_tooltip_string(int mousex, int mousey);
 
 protected:
-	widget(const widget& o);
 	widget(CVideo& video, const bool auto_join=true);
 	virtual ~widget();
 