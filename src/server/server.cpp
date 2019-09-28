2e15a3f644291bc5fb340be93579d0eccd1a91a6
pentarctagon <pentarctagon@tutamail.com>
2019-09-27 23:13:14 -0500

    Use BIT(1) for fields with only 2 possible non-NULL values.

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1620,7 +1620,7 @@ void server::handle_player_in_game(socket_ptr socket, std::shared_ptr<simple_wml
 			const simple_wml::node::child_list& sides = g.get_sides_list();
 			for(unsigned side_index = 0; side_index < sides.size(); ++side_index) {
 				const simple_wml::node& side = *sides[side_index];
-				user_handler_->db_insert_game_player_info(uuid_, g.id(), side["player_id"].to_string(), side["side"].to_int(), side["is_host"].to_string(), side["faction"].to_string());
+				user_handler_->db_insert_game_player_info(uuid_, g.id(), side["player_id"].to_string(), side["side"].to_int(), side["is_host"].to_bool(), side["faction"].to_string());
 			}
 
 			const std::string mods = multiplayer["active_mods"].to_string();
