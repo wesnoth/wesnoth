5028556d43998ab818ac9c4bade36dd0a98fe255
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2019-10-03 17:38:07 +0200

    Update attack_type.hpp

--- src/units/attack_type.hpp
+++ src/units/attack_type.hpp
@@ -73,7 +73,7 @@ public:
 
 	// In unit_abilities.cpp:
 
-	bool get_special_bool(const std::string& special, bool simple_check=false) const;
+	bool get_special_bool(const std::string& special, bool simple_check=false, bool special_id=true, bool special_tags=true) const;
 	unit_ability_list get_specials(const std::string& special) const;
 	std::vector<std::pair<t_string, t_string>> special_tooltips(boost::dynamic_bitset<>* active_list = nullptr) const;
 	std::string weapon_specials(bool only_active=false, bool is_backstab=false) const;
