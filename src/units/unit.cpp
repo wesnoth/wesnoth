5e7225e7d402ad51d4a7d9e3852a6fe65e6154b0
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2019-11-24 00:29:50 +0100

    Update unit.cpp

--- src/units/unit.cpp
+++ src/units/unit.cpp
@@ -1681,9 +1681,11 @@ int unit::resistance_against(const std::string& damage_name,bool attacker,const 
 {
 	int res = movement_type_.resistance_against(damage_name);
 
-	unit_ability_list resistance_abilities = get_abilities("resistance",loc, weapon, opp_weapon);
+	unit_ability_list resistance_abilities = get_abilities("resistance", loc);
 	for(unit_ability_list::iterator i = resistance_abilities.begin(); i != resistance_abilities.end();) {
-		if(!resistance_filter_matches(*i->first, attacker, damage_name, 100-res)) {
+		bool affect_weapon = !ability_affects_weapon(*i->first, weapon, false) || !ability_affects_weapon(*i->first, opp_weapon, true);
+		bool filter_student = !ability_filter_fighter("resistance", "filter_student" , *i->first, loc, weapon);
+		if(!resistance_filter_matches(*i->first, attacker, damage_name, 100-res) || affect_weapon || filter_student) {
 			i = resistance_abilities.erase(i);
 		} else {
 			++i;
