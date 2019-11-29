1a4b0a5de3d7a76297bf000c2828142b3896095c
Steve Cotton <steve@octalot.co.uk>
2019-11-29 04:00:14 +0100

    WIP: Use the Python run_wml_tests on Windows too

--- projectfiles/VC16/WML_tests.cmd
+++ projectfiles/VC16/WML_tests.cmd
@@ -4,46 +4,21 @@
 @echo off
 setlocal enabledelayedexpansion
 
+if "%SKIP_WMLTESTS%" == "TRUE" exit 0
+echo running WML tests:
+
 :: if OMP_WAIT_POLICY isn't set, the process restarts itself and !ERRORLEVEL!=0
 set OMP_WAIT_POLICY=PASSIVE
 
-:: save file paths and command line arguments
+:: Argument 1 (accessed via %~f1%) is the build's OutDir, but it might be
+:: empty, in which case use run_wml_tests built-in logic for working out where
+:: the binary should be.
 cd ..\..\
-set LoadFile=wml_test_schedule
-set binary=%~f1%wesnoth.exe
-set opt=--log-strict=warning --noaddons
-
-echo running WML tests:
-set tSTART=%time%
-
-:: ignore lines beginning with #
-:: %%G contains whether the test should pass (0), timeout (2) or fail (1,4)
-:: %%H is the name of the WML unit test to be executed
-for /f "eol=# tokens=1,2 delims= " %%G in (%LoadFile%) do (
-    WindowsTimeout.exe "%binary% %opt% -u%%H" 20000
-    if !ERRORLEVEL! neq %%G (
-        if !ERRORLEVEL! equ 2 (
-            echo(
-            echo WML_tests.cmd: Warning WML2: Test '%%H' timed out, expected return value %%G
-        ) else (
-            echo(
-            echo WML_tests.cmd: Error WML1: Test '%%H' returned !ERRORLEVEL!, expected %%G
-        )
-        set /a "fail_num+=1"
-    )
-    :: minimalistic progress bar
-    <nul (set/p progress=.)
-    set /a "test_num+=1"
+if "%~f1%" == "" (
+	py -3 .\run_wml_tests -c -v
+) else (
+	py -3 .\run_wml_tests -p "%~f1%" -c -v
 )
-echo(
-if not DEFINED fail_num ( set "fail_num=none" )
-set /a "minutes = 1%time:~3,2% - 1%tSTART:~3,2%"
-set /a "seconds = 1%time:~6,2% - 1%tSTART:~6,2%"
-if %seconds% LSS 0 (
-    set /a "seconds+=60"
-    set /a "minutes-=1"
-)
-echo %test_num% WML tests completed in %minutes%m %seconds%s, %fail_num% of them failed
 
 :: restore the state before execution
 cd %~p0
