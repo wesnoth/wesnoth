cbd88814c3d7174f81f54f55edf89233672d887f
Celtic Minstrel <celtic.minstrel.ca@some.place>
2019-12-09 22:12:39 -0500

    Fix #4622

--- data/lua/wml-flow.lua
+++ data/lua/wml-flow.lua
@@ -240,7 +240,7 @@ function wml_actions.switch(cfg)
 
 	-- Execute all the [case]s where the value matches.
 	for v in wml.child_range(cfg, "case") do
-		for _,w in ipairs(v.value:split()) do
+		for _,w in ipairs(tostring(v.value):split()) do
 			if w == tostring(var_value) then
 				local action = utils.handle_event_commands(v, "switch")
 				found = true
