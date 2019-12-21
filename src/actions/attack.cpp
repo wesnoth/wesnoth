f8c73962f9746a5436157b62071a443595f0cdce
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2019-12-21 12:42:46 +0100

    Update attack.cpp

--- src/actions/attack.cpp
+++ src/actions/attack.cpp
@@ -52,7 +52,6 @@
 #include "whiteboard/manager.hpp"
 #include "wml_exception.hpp"
 
-
 static lg::log_domain log_engine("engine");
 #define DBG_NG LOG_STREAM(debug, log_engine)
 #define LOG_NG LOG_STREAM(info, log_engine)
