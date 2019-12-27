0a7f483c72679cbb9ddccb2e5e433bc3af71b80e
pentarctagon <pentarctagon@tutamail.com>
2019-12-26 23:02:25 -0600

    Add debug and release Windows jobs to travis.
    This also removes the separate WML_tests.cmd scripts from VC14 and VC16, now instead using the same run_wml_tests script as the linux jobs.
    
    There is currently no caching of the Windows jobs, since msbuild unfortunately uses timestamps rather than more complete information like scons/ccache in order to determine if things need to be rebuilt. Since git doesn't preserve any sort of last modified timestamp, the cloned repo into the travis job is always "newer" than the cached compiled output, and therefore wesnoth is always fully rebuilt.
    
    Ideally, assuming this sticks around and gets out of early access from travis, we could then use just travis instead of travis and appveyor.

--- utils/travis/steps/install.sh
+++ utils/travis/steps/install.sh
@@ -26,6 +26,18 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
         HOMEBREW_NO_AUTO_UPDATE=1 brew install scons cairo pango moreutils sdl2_image sdl2_ttf sdl2_mixer glew ccache
         HOMEBREW_NO_AUTO_UPDATE=1 brew reinstall libffi
     fi
+elif [ "$TRAVIS_OS_NAME" = "windows" ]; then
+    start=`pwd`
+    choco install python --version=3.6.8
+    cd /c/Python36
+    ln -s python.exe python3.exe
+    cd $start
+    cd ..
+    wget https://github.com/aquileia/external/archive/VC15.zip -O VC15.zip
+    7z x VC15.zip
+    mv external-VC15 external
+    cd $start
+    export PATH="/c/Python36:"$PATH":/c/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin/amd64:$start/../external/dll"
 else
     if [ "$NLS" != "true" ]; then
         echo "po/" >> .dockerignore
