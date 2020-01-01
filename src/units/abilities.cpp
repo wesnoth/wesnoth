ae6fdbabbeb664ca46c039c620f62c4c0c57432d
gfgtdf <daniel.gfgtdf@gmail.com>
2020-01-01 20:47:11 +0100

    why do you say does this not work?

--- src/units/abilities.cpp
+++ src/units/abilities.cpp
@@ -1225,37 +1225,12 @@ unit_ability_list attack_type::list_ability(const std::string& ability) const
 	unit_ability_list abil_list(self_loc_);
 	// leadership_affect_self/opponent are used there for determine is special_affects_self/opponent
 	// or none must be used in list_leadership
-	bool affect_self;
-	if(self){affect_self= leadership_affects_self(ability, *self, self_loc_, is_attacker_);}
-	bool affect_opponent;
-	if(other){affect_opponent = leadership_affects_opponent(ability, *other, other_loc_, !is_attacker_);}
 	if(self){
-		if(!other){
-			if(affect_self){
-				abil_list = list_leadership(ability, self, other, self_loc_, other_loc_, is_attacker_, shared_from_this(), other_attack_, true, false);
-			}
-		}
-		if(other){
-			if(affect_self && !affect_opponent){
-				abil_list = list_leadership(ability, self, other, self_loc_, other_loc_, is_attacker_, shared_from_this(), other_attack_, true, false);
-			}
-			if(affect_self && affect_opponent) {
-				abil_list = list_leadership(ability, self, other, self_loc_, other_loc_, is_attacker_, shared_from_this(), other_attack_, true, true);
-			}
-		}
+		abil_list.append(list_leadership(ability, self, other, self_loc_, other_loc_, is_attacker_, shared_from_this(), other_attack_, true, false));
 	}
 
-	if(!other_attack_){
-		return abil_list;
-	}
-	
-	if(other){
-		if(!affect_self && affect_opponent) {
-			abil_list = list_leadership(ability, other, self, other_loc_, self_loc_, !is_attacker_, other_attack_, shared_from_this(), false, true);
-		}
-		if(affect_self && affect_opponent) {
-			abil_list = list_leadership(ability, other, self, other_loc_, self_loc_, !is_attacker_, other_attack_, shared_from_this(), true, true);
-		}
+	if(other && other_attack_) {
+		abil_list.append(list_leadership(ability, other, self, other_loc_, self_loc_, !is_attacker_, other_attack_, shared_from_this(), false, true));
 	}
 	return abil_list;
 }
