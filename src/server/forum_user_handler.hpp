a71c576cde448f601f074ca2eca563b2dfef7d85
Pentarctagon <pentarctagon@tutamail.com>
2020-01-17 13:25:52 -0600

    Only store game information once the game starts.

--- src/server/forum_user_handler.hpp
+++ src/server/forum_user_handler.hpp
@@ -70,8 +70,7 @@ class fuh : public user_handler {
 		bool use_phpbb_encryption() const { return true; }
 
 		std::string get_uuid();
-		void db_insert_game_info(const std::string& uuid, int game_id, const std::string& version, const std::string& name);
-		void db_update_game_start(const std::string& uuid, int game_id, const std::string& map_name, const std::string& era_name, int reload, int observers, int is_public, int has_password);
+		void db_insert_game_info(const std::string& uuid, int game_id, const std::string& version, const std::string& name, const std::string& map_name, const std::string& era_name, int reload, int observers, int is_public, int has_password);
 		void db_update_game_end(const std::string& uuid, int game_id, const std::string& replay_location);
 		void db_insert_game_player_info(const std::string& uuid, int game_id, const std::string& username, int side_number, int is_host, const std::string& faction, const std::string& version, const std::string& source, const std::string& current_user);
 		void db_insert_modification_info(const std::string& uuid, int game_id, const std::string& modification_name);
