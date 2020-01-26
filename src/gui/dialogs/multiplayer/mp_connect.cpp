1104d3756fb21ab56679d8813308ad27ccc5df4f
Iris Morelle <shadowm@wesnoth.org>
2020-01-26 02:05:14 -0300

    gui2/mp_connect: Fix adding new servers under built-ins
    If the current selection was a built-in server, the new server would be
    added right below it even if it wasn't the last built-in server,
    resulting in a new entry that can't be deleted without closing the
    dialog and launching it again to force the server list to be
    re-enumerated correctly.

--- src/gui/dialogs/multiplayer/mp_connect.cpp
+++ src/gui/dialogs/multiplayer/mp_connect.cpp
@@ -197,7 +197,7 @@ void mp_connect::on_server_add()
 
 	const std::size_t mem_pos = selection.user_defined() && !user_servers_.empty()
 			? 1 + selection.relative_index() : 0;
-	const unsigned int ui_pos = 1 + selection.row();
+	const unsigned int ui_pos = selection.user_defined() ? 1 + selection.row() : builtin_servers_.size();
 
 	std::string name;
 
