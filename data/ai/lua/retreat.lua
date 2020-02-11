9020c92498604c82415d28e0bc81b7eb26083c78
mattsc <mattsc000@gmail.com>
2020-02-11 12:33:39 -0800

    AI retreat CA: bug fix for cure-only abilities
    That is, those that cure poison, but do not provide any healing.

--- data/ai/lua/retreat.lua
+++ data/ai/lua/retreat.lua
@@ -86,7 +86,7 @@ function retreat_functions.get_healing_locations()
             local cure = 0
             local abilities = wml.get_child(u.__cfg, "abilities") or {}
             for ability in wml.child_range(abilities, "heals") do
-                heal_amount = ability.value
+                heal_amount = ability.value or 0
                 if ability.poison == "slowed" then
                     cure = 1
                 elseif ability.poison == "cured" then
