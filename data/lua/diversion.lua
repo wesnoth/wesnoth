a01e0369fa17af4695e7cee11d70a2b6296842e1
doofus-01 <dk0045@comcast.net>
2020-03-21 16:42:21 -0700

    units/abilities - dunefolk falconer diversion ability-related animations mechanism

---
+++ data/lua/diversion.lua
@@ -0,0 +1,94 @@
+
+local _ = wesnoth.textdomain 'wesnoth-help'
+local T = wml.tag
+local on_event = wesnoth.require("on_event")
+
+local u_pos_filter = function(u_id)
+
+        local output = "initial"
+        local hex_dirs = {"n", "ne", "se", "s", "sw", "nw"}
+        local diversion_unit = wesnoth.units.get(u_id)
+        if not diversion_unit then
+            return nil
+        end
+        for i, dir in ipairs(hex_dirs) do
+            if diversion_unit:matches {
+              id = u_id, 
+              T.filter_adjacent {
+                  is_enemy = "yes",
+                  adjacent = dir,
+                  T.filter_adjacent {
+                      is_enemy = "yes",
+                      adjacent = dir
+                  }
+              }
+            } then
+                output = "diverter"
+                break 
+            end
+        end
+        if output ~= "initial" then
+            return output
+        else
+        -- either nothing passed filter, or there was an error
+            return nil
+        end
+end
+
+
+local status_anim_update = function()
+
+	local ec = wesnoth.current.event_context
+
+	if not ec.x1 or not ec.y1 then
+                return
+        end
+
+        -- find all units on map with ability = diversion but not status.diversion = true
+        local div_candidates = wesnoth.units.find_on_map({
+                ability = "diversion",
+                {"not", { status = "diversion" }}
+                })
+        -- for those that pass the filter now, change status and fire animation
+        for index, ec_unit in ipairs(div_candidates) do
+                local filter_result = u_pos_filter(ec_unit.id)
+                if filter_result then
+                    ec_unit.status.diversion = true
+                    ec_unit:extract()
+                    ec_unit:to_map()
+                    wesnoth.wml_actions.animate_unit {
+                            flag = "launching",
+                            with_bars = true,
+                            T.filter { id = ec_unit.id }
+                    }
+                end
+        end
+        
+        -- find all units on map with ability = diversion and status.diversion = true
+        local stop_candidates = wesnoth.units.find_on_map({
+                ability = "diversion",
+                status = "diversion"
+                })
+        -- for those that fail the filter now, change status and fire animation
+        for index, ec_unit in ipairs(stop_candidates) do
+                local filter_result = u_pos_filter(ec_unit.id)
+                if not filter_result then
+                    ec_unit.status.diversion = false
+                    ec_unit:extract()
+                    ec_unit:to_map()
+                    wesnoth.wml_actions.animate_unit {
+                            flag = "landing",
+                            with_bars = true,
+                            T.filter { id = ec_unit.id }
+                    }
+                end
+        end
+end
+
+on_event("moveto", function()
+        status_anim_update()
+end)
+
+on_event("die", function()
+        status_anim_update()
+end)
