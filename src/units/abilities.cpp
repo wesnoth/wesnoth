2bfe70a93b4f2cb8662237d4ec4b46deb9540033
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2020-04-04 18:03:17 +0200

    simplify get_abilities_weapons code

--- src/units/abilities.cpp
+++ src/units/abilities.cpp
@@ -225,46 +225,14 @@ unit_ability_list unit::get_abilities(const std::string& tag_name, const map_loc
 
 unit_ability_list unit::get_abilities_weapons(const std::string& tag_name, const map_location& loc, const_attack_ptr weapon, const_attack_ptr opp_weapon) const
 {
-	unit_ability_list res(loc_);
-
-	for(const config& i : this->abilities_.child_range(tag_name)) {
-		if(ability_active(tag_name, i, loc)
-			&& ability_affects_self(tag_name, i, loc)
-			&& ability_affects_weapon(i, weapon, false)
-			&& ability_affects_weapon(i, opp_weapon, true)
-		) {
-			res.emplace_back(&i, loc);
+	unit_ability_list res = get_abilities(tag_name, loc);
+	for(unit_ability_list::iterator i = res.begin(); i != res.end();) {
+		if((!ability_affects_weapon(*i->first, weapon, false) || !ability_affects_weapon(*i->first, opp_weapon, true))) {
+			i = res.erase(i);
+		} else {
+			++i;
 		}
 	}
-
-	assert(display::get_singleton());
-	const unit_map& units = display::get_singleton()->get_units();
-
-	adjacent_loc_array_t adjacent;
-	get_adjacent_tiles(loc,adjacent.data());
-	for(unsigned i = 0; i < adjacent.size(); ++i) {
-		const unit_map::const_iterator it = units.find(adjacent[i]);
-		if (it == units.end() || it->incapacitated())
-			continue;
-		// Abilities may be tested at locations other than the unit's current
-		// location. This is intentional to allow for less messing with the unit
-		// map during calculations, particularly with regards to movement.
-		// Thus, we need to make sure the adjacent unit (*it) is not actually
-		// ourself.
-		if ( &*it == this )
-			continue;
-		for(const config& j : it->abilities_.child_range(tag_name)) {
-			if(affects_side(j, side(), it->side())
-				&& it->ability_active(tag_name, j, adjacent[i])
-				&& ability_affects_adjacent(tag_name, j, i, loc, *it) && ability_affects_weapon(j, weapon, false)
-				&& ability_affects_weapon(j, opp_weapon, true)
-			) {
-				res.emplace_back(&j, adjacent[i]);
-			}
-		}
-	}
-
-
 	return res;
 }
 
