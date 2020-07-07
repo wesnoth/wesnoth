da4ee6b0762e28125ba9b46fd172415900641075
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2020-07-07 20:20:16 +0200

    Update unit.hpp

--- src/units/unit.hpp
+++ src/units/unit.hpp
@@ -1722,7 +1722,8 @@ private:
 	/**
 	 * used for abilities who affect unit distant to teacher (radius 10 maximum)
 	 */
-	bool get_abilities_bool_radius(const std::string& tag_name, const config& cfg, const map_location& loc, int max_radius = 0) const;
+	bool get_abilities_bool_radius(const std::string& tag_name, const config& cfg, const map_location& loc) const;
+	unit_ability_list get_abilities_radius(const std::string& tag_name, const config& cfg, const map_location& loc) const;
 
 	/**
 	 * Check if an ability affects adjacent units.
