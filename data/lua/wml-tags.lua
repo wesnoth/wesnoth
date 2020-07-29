3df569ae74095729449980964b2d484b9837f017
gfgtdf <daniel.gfgtdf@gmail.com>
2020-07-29 18:52:41 +0200

    fix missing chunk name in [lua] tags with no name= attribute
    reported here https://forums.wesnoth.org/viewtopic.php?p=656867#p656867

--- data/lua/wml-tags.lua
+++ data/lua/wml-tags.lua
@@ -261,7 +261,7 @@ end
 function wml_actions.lua(cfg)
 	cfg = wml.shallow_literal(cfg)
 	--fixme untested
-	local bytecode, message = load(cfg.code or "", cfg.name or "")
+	local bytecode, message = load(cfg.code or "", cfg.name or nil)
 	if not bytecode then error("~lua:" .. message, 0) end
 	bytecode(wml.get_child(cfg, "args"))
 end
