400a169bbc98cc1877152ef18a1d996e926d68db
gfgtdf <daniel.gfgtdf@gmail.com>
2020-07-30 21:52:06 +0200

    Make [feeding] ability available
    Now feeding is reconized by tagname instead of
    id. This means in particular that umc authors
    can use [feeding] abilities with filters now.
    
    Also [feeding] is now technicially a weapon
    special, but since weapon specials can be used
    in [abilities] it will work as before.
    
    This commit in partiuclar gives a test case for
    the new active_specials() lua api in mainline
    (which is probably the main reason why i did
    this).
    
    Lastly [feeding] now takes a value, for example
    `[feeding] value=(opponent.level)` should
    increase the units max hp by the opponents level
    (not tested).

--- data/lua/feeding.lua
+++ data/lua/feeding.lua
@@ -14,21 +14,36 @@ on_event("die", function()
 	local u_killer = wesnoth.units.get(ec.x2, ec.y2)
 	local u_victim = wesnoth.units.get(ec.x1, ec.y1)
 
-	if not u_killer or not u_killer:matches { ability = "feeding" } then
+	if not u_killer or not u_killer.active_attack then
 		return
 	end
+
+	local feeding = u_killer.active_attack:active_specials {
+		tag = "feeding",
+		base_value = 0,
+	}
+
+	if feeding.value <= 0 then
+		return
+	end
+
 	if not u_victim or u_victim:matches { status = "unplagueable" } then
 		return
 	end
+
+	-- po: Floating text shown when a unit with the "feeding" ability gets a kill
+	local text = _(_"+$value max HP", _"+$value max HP", feeding.value)
+	local text = text:vformat { value = feeding.value }
+
 	local u_killer_cfg = u_killer.__cfg
 	for i,v in ipairs(wml.get_child(u_killer_cfg, "modifications"))do
 		if v[1] == "object" and v[2].feeding == true then
 			local effect = wml.get_child(v[2], "effect")
-			effect.increase_total = effect.increase_total + 1
-			u_killer_cfg.max_hitpoints = u_killer_cfg.max_hitpoints + 1
-			u_killer_cfg.hitpoints = u_killer_cfg.hitpoints + 1
+			effect.increase_total = effect.increase_total + feeding.value
+			u_killer_cfg.max_hitpoints = u_killer_cfg.max_hitpoints + feeding.value
+			u_killer_cfg.hitpoints = u_killer_cfg.hitpoints + feeding.value
 			wesnoth.units.to_map(u_killer_cfg)
-			wesnoth.interface.float_label(ec.x2, ec.y2, _ "+1 max HP", "0,255,0")
+			wesnoth.interface.float_label(ec.x2, ec.y2, text, "0,255,0")
 			return
 		end
 	end
@@ -37,9 +52,9 @@ on_event("die", function()
 		feeding = true,
 		T.effect {
 			apply_to = "hitpoints",
-			increase_total = 1,
+			increase_total = feeding.value,
 		},
 	})
-	u_killer.hitpoints = u_killer.hitpoints + 1
-	wesnoth.interface.float_label(ec.x2, ec.y2, _ "+1 max HP", "0,255,0")
+	u_killer.hitpoints = u_killer.hitpoints + feeding.value
+	wesnoth.interface.float_label(ec.x2, ec.y2, text, "0,255,0")
 end)
