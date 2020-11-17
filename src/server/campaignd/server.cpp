cc087491f1072f20abe83276fb1c0ceea02c19fd
Iris Morelle <shadowm@wesnoth.org>
2020-11-17 20:07:18 -0300

    campaignd: Drop redundant includes

--- src/server/campaignd/server.cpp
+++ src/server/campaignd/server.cpp
@@ -46,9 +46,6 @@
 #include <ctime>
 #include <iomanip>
 
-#include <boost/random.hpp>
-#include <boost/generator_iterator.hpp>
-
 // the fork execute is unix specific only tested on Linux quite sure it won't
 // work on Windows not sure which other platforms have a problem with it.
 #if !(defined(_WIN32))
