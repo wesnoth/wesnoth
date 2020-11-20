c09e95a20ef5fc8fda7d227e75dbff9c68ebb0b3
Pentarctagon <pentarctagon@tutamail.com>
2020-11-20 11:19:27 -0600

    Github Actions ubuntu compile test.

--- .github/workflows/ci-scripts/ubuntu.sh
+++ .github/workflows/ci-scripts/ubuntu.sh
@@ -2,6 +2,8 @@
 
 date
 
+docker image ls
+
 echo FROM wesnoth/wesnoth:2004-master > utils/dockerbuilds/travis/Dockerfile-travis-2004-master
 echo COPY ./ /home/wesnoth-travis/ >> utils/dockerbuilds/travis/Dockerfile-travis-2004-master
 echo WORKDIR /home/wesnoth-travis >> utils/dockerbuilds/travis/Dockerfile-travis-2004-master
@@ -10,4 +12,4 @@ docker build -t wesnoth-repo:2004-master -f utils/dockerbuilds/travis/Dockerfile
 
 docker image ls
 
-
+docker run --cap-add=ALL --privileged wesnoth-repo:2004-master ./.github/workflows/ci-scripts/docker.sh
