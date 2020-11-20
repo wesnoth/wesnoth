eb361bdccd0e2958ed3ab95e3d6e09159f87a5c7
Pentarctagon <pentarctagon@tutamail.com>
2020-11-20 11:25:37 -0600

    Use correct sanitize value.
    [ci skip]

--- .github/workflows/ci-scripts/docker.sh
+++ .github/workflows/ci-scripts/docker.sh
@@ -3,6 +3,6 @@
 scons wesnoth wesnothd campaignd boost_unit_tests build=release \
     ctool=gcc cxxtool=g++ cxx_std=17 \
     extra_flags_config="-pipe" strict=true forum_user_handler=true \
-    nls=false enable_lto=true sanitize=false jobs=2 --debug=time
+    nls=false enable_lto=true sanitize="" jobs=2 --debug=time
 
 ls -Al
