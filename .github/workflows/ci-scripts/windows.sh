4dd02a0df384f44eacde3e64c2b8a901732b4749
Pentarctagon <pentarctagon@tutamail.com>
2020-11-21 21:37:54 -0600

    Github Actions let's fine MSBuild [ci skip]

--- .github/workflows/ci-scripts/windows.sh
+++ .github/workflows/ci-scripts/windows.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
-echo "choco: $(which choco)"
-echo "python: $(which python)"
-echo "python2: $(which python2)"
 echo "python3: $(which python3)"
-echo "sqlite3: $(which sqlite3)"
-echo "sqlite3: $(which sqlite3.exe)"
+echo "sqlite3.exe: $(which sqlite3.exe)"
+echo "cmd.exe: $(which cmd.exe)"
+echo "MSBuild.exe: $(which MSBuild.exe)"
+ls -Al /mnt/c
