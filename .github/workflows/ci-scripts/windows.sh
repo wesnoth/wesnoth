a6caadea69bad17c39a91d6138e563aea22a3fce
Pentarctagon <pentarctagon@tutamail.com>
2020-11-21 18:45:51 -0600

    Github Actions where is chocolatey [ci skip]

--- .github/workflows/ci-scripts/windows.sh
+++ .github/workflows/ci-scripts/windows.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
-echo "hello world"
-echo "CFG: $CFG"
 echo "choco: $(which choco)"
-echo "python $(which python)"
-echo "python2 $(which python2)"
-echo "python3 $(which python3)"
-echo "sqlite $(which sqlite)"
+echo "python: $(which python)"
+echo "python2: $(which python2)"
+echo "python3: $(which python3)"
+echo "sqlite: $(which sqlite)"
