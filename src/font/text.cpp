3a2410afba321e14863f9cb5cd013ca37cc44be9
Iris Morelle <shadowm@wesnoth.org>
2021-02-18 01:27:26 -0300

    font/sdl_ttf_compat: Add a Pango-based word wrap implementation
    This lets Pango do all of the work for us and pulls the text back from
    the renderer. It shouldn't be much more expensive than it needs to be --
    the only real expensive step is copying the characters back into a list
    of strings, and it's not by much.
    
    Note: I haven't quite figured out what the partial_line parameter does
    or what to do about it. It's probably not trivial to implement since it
    seems to involve the decision of whether to break a line within word
    boundaries or not.

--- src/font/text.cpp
+++ src/font/text.cpp
@@ -868,6 +868,32 @@ void pango_text::copy_layout_properties(PangoLayout& src, PangoLayout& dst)
 	pango_layout_set_ellipsize(&dst, pango_layout_get_ellipsize(&src));
 }
 
+std::vector<std::string> pango_text::get_lines() const
+{
+	this->recalculate();
+
+	PangoLayout* const layout = layout_.get();
+	std::vector<std::string> res;
+	int count = pango_layout_get_line_count(layout);
+
+	if(count < 1) {
+		return res;
+	}
+
+	using layout_iterator = std::unique_ptr<PangoLayoutIter, std::function<void(PangoLayoutIter*)>>;
+	layout_iterator i{pango_layout_get_iter(layout), pango_layout_iter_free};
+
+	res.reserve(count);
+
+	do {
+		PangoLayoutLine* ll = pango_layout_iter_get_line_readonly(i.get());
+		const char* begin = &pango_layout_get_text(layout)[ll->start_index];
+		res.emplace_back(begin, ll->length);
+	} while(pango_layout_iter_next_line(i.get()));
+
+	return res;
+}
+
 pango_text& get_text_renderer()
 {
 	static pango_text text_renderer;
