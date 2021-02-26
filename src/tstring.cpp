a98204a0fdff42330a74bffe57be65c541a3a903
Celtic Minstrel <celtic.minstrel.ca@some.place>
2021-02-26 02:32:57 -0500

    Add a Wesnoth natvis file
    The change to t_string is just to make the textdomain map visible to the debugger, since it can't see variables with static linkage.

--- src/tstring.cpp
+++ src/tstring.cpp
@@ -42,10 +42,10 @@ const char UNTRANSLATABLE_PART = 0x02;
 const char TEXTDOMAIN_SEPARATOR = 0x03;
 const char ID_TRANSLATABLE_PART = 0x04;
 const char PLURAL_PART = 0x05;
-
-std::vector<std::string> id_to_textdomain;
-std::map<std::string, unsigned int> textdomain_to_id;
 }
+
+std::vector<std::string> t_string_base::id_to_textdomain;
+std::map<std::string, unsigned int> t_string_base::textdomain_to_id;
 
 std::size_t t_string_base::hash_value() const
 {
