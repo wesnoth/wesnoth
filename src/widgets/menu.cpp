wesnoth/wesnoth

commit d1f026d22b9d0a989e72d1b0c31ac25fe6def8c8

gui1: Move more code to using the compatibility API

src/widgets/menu.cpp
@@ -966,26 +966,11 @@ void menu::draw_row(const std::size_t row_index, const SDL_Rect& rect, ROW_TYPE 
 				}
 			} else {
 				column.x = xpos;
-				const bool has_wrap = (str.find_first_of("\r\n") != std::string::npos);
-				//prevent ellipsis calculation if there is any line wrapping
-				std::string to_show = str;
-				if (use_ellipsis_ && !has_wrap)
-				{
-					int fs = style_->get_font_size();
-					int style = TTF_STYLE_NORMAL;
-					int w = rect.w - (xpos - rect.x) - 2 * style_->get_thickness();
-					std::string::const_iterator it2_beg = to_show.begin(), it2_end = to_show.end(),
-						it2 = font::parse_markup(it2_beg, it2_end, &fs, nullptr, &style);
-					if (it2 != it2_end) {
-						std::string tmp(it2, it2_end);
-						to_show.erase(it2 - it2_beg, it2_end - it2_beg);
-						to_show += font::make_text_ellipsis(tmp, fs, w, style);
-					}
-				}
-				const SDL_Rect& text_size = font::text_area(str,style_->get_font_size());
+
+				const SDL_Rect& text_size = font::pango_text_area(str,style_->get_font_size());
 				const std::size_t y = rect.y + (rect.h - text_size.h)/2;
 				const std::size_t padding = 2;
-				font::pango_draw_text(&video(),column,style_->get_font_size(),font::NORMAL_COLOR,to_show,
+				font::pango_draw_text(&video(),column,style_->get_font_size(),font::NORMAL_COLOR,str,
 					(type == HEADING_ROW ? xpos+padding : xpos), y);
 
 				if(type == HEADING_ROW && sortby_ == int(i)) {

src/display_chat_manager.cpp
@@ -21,6 +21,7 @@
 #include "preferences/game.hpp"
 #include "log.hpp"
 #include "font/marked-up_text.hpp"
+#include "font/sdl_ttf_compat.hpp"
 #include "mp_ui_alerts.hpp"
 #include "serialization/string_utils.hpp"
 #include "color.hpp"
@@ -104,7 +105,7 @@ void display_chat_manager::add_chat_message(const std::time_t& time, const std::
 	try {
 		// We've had a joker who send an invalid utf-8 message to crash clients
 		// so now catch the exception and ignore the message.
-		msg = my_disp_.video().faked() ? "" : font::word_wrap_text(msg,font::SIZE_15,my_disp_.map_outside_area().w*3/4);
+		msg = my_disp_.video().faked() ? "" : font::pango_word_wrap(msg,font::SIZE_15,my_disp_.map_outside_area().w*3/4);
 	} catch (utf8::invalid_utf8_exception&) {
 		ERR_NG << "Invalid utf-8 found, chat message is ignored." << std::endl;
 		return;

src/widgets/button.cpp
@@ -208,24 +208,6 @@ void button::calculate_size()
 		set_location(loc_image);
 		return;
 	}
-	const SDL_Rect& loc = location();
-	bool change_size = loc.h == 0 || loc.w == 0;
-
-	if (!change_size) {
-		unsigned w = loc.w - (type_ == TYPE_PRESS || type_ == TYPE_TURBO ? horizontal_padding : checkbox_horizontal_padding + base_width_);
-		if (type_ != TYPE_IMAGE)
-		{
-			int fs = font_size;
-			int style = TTF_STYLE_NORMAL;
-			std::string::const_iterator i_beg = label_text_.begin(), i_end = label_text_.end(),
-				i = font::parse_markup(i_beg, i_end, &fs, nullptr, &style);
-			if (i != i_end) {
-				std::string tmp(i, i_end);
-				label_text_.erase(i - i_beg, i_end - i_beg);
-				label_text_ += font::make_text_ellipsis(tmp, fs, w, style);
-			}
-		}
-	}
 
 	if (type_ != TYPE_IMAGE){
 		textRect_ = font::pango_draw_text(nullptr, video().screen_area(), font_size, font::BUTTON_COLOR, label_text_, 0, 0);

commit 57b89d225a1f46907b5b69fdc57ab5260a7218e1

gui1: Fix incorrect menu item size constraints
Regression from d1f026d22b9d0a989e72d1b0c31ac25fe6def8c8.

src/widgets/menu.cpp
@@ -921,7 +921,10 @@ void menu::draw_row(const std::size_t row_index, const SDL_Rect& rect, ROW_TYPE 
 				const SDL_Rect& text_size = font::pango_text_area(str,style_->get_font_size());
 				const std::size_t y = rect.y + (rect.h - text_size.h)/2;
 				const std::size_t padding = 2;
-				font::pango_draw_text(&video(),column,style_->get_font_size(),font::NORMAL_COLOR,str,
+				SDL_Rect text_rect = column;
+				text_rect.w = rect.w - (xpos - rect.x) - 2 * style_->get_thickness();
+				text_rect.h = text_size.h;
+				font::pango_draw_text(&video(), text_rect, style_->get_font_size(), font::NORMAL_COLOR, str,
 					(type == HEADING_ROW ? xpos+padding : xpos), y);
 
 				if(type == HEADING_ROW && sortby_ == int(i)) {