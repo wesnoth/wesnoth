ae5a27c996e5c245bfb2c915f993ce9adb55cdbf
Celtic Minstrel <celtic.minstrel.ca@some.place>
2021-04-04 00:53:12 -0400

    Support passing an arbitrary Lua function as the filter when the event is registered from Lua.

--- src/scripting/game_lua_kernel.hpp
+++ src/scripting/game_lua_kernel.hpp
@@ -251,9 +251,10 @@ public:
 	 * @param ref The unique index into the EVENT_TABLE within the Lua registry
 	 * @param args Arguments to pass to the event function, as a config
 	 * @param ev The event data for the event being fired
+	 * @param out If non-null, receives the result of the called function (provided it is a boolean value)
 	 * @return Whether the function was successfully called; could be false if @a ref was invalid or if the function raised an error
 	 */
-	bool run_wml_event(int ref, const vconfig& args, const game_events::queued_event& ev);
+	bool run_wml_event(int ref, const vconfig& args, const game_events::queued_event& ev, bool* out = nullptr);
 
 	virtual void log_error(char const* msg, char const* context = "Lua error") override;
 
