667cb7f5716bcea6be543801e85ee5180163ad5e
Iris Morelle <shadowm@wesnoth.org>
2021-05-01 02:55:03 -0400

    Remove orphaned declarations of removed functions

--- src/scripting/lua_gui2.hpp
+++ src/scripting/lua_gui2.hpp
@@ -22,18 +22,7 @@ class game_state;
 
 namespace lua_gui2 {
 
-int intf_set_dialog_value(lua_State *L);
-int intf_set_dialog_tooltip(lua_State *L);
-int intf_get_dialog_value(lua_State *L);
-int intf_set_dialog_callback(lua_State *L);
-int intf_set_dialog_markup(lua_State *L);
-int intf_set_dialog_canvas(lua_State *L);
-int intf_set_dialog_focus(lua_State *L);
-int intf_set_dialog_active(lua_State *L);
-int intf_set_dialog_visible(lua_State *L);
-int intf_add_dialog_tree_node(lua_State *L);
 int intf_add_widget_definition(lua_State *L);
-int show_dialog(lua_State *L);
 int show_message_dialog(lua_State *L);
 int show_popup_dialog(lua_State *L);
 int show_menu(lua_State* L);
@@ -41,7 +30,6 @@ int show_story(lua_State* L);
 int show_message_box(lua_State* L);
 int show_lua_console(lua_State*L, lua_kernel_base * lk);
 int show_gamestate_inspector(const vconfig& cfg, const game_data& data, const game_state& state);
-int intf_remove_dialog_item(lua_State *L);
 int luaW_open(lua_State *L);
 
 } // end namespace lua_gui2
