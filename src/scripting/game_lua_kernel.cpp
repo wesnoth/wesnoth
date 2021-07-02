wesnoth/wesnoth

commit 5a9c24c4e79d624e34aa475949a4aa49b1984322

Updated a few places that didn't handle locations in the unified way

src/scripting/game_lua_kernel.cpp
@@ -1931,18 +1931,7 @@ int game_lua_kernel::intf_find_vision_range(lua_State *L)
 		vision_set.insert(d.curr);
 	}
 
-	lua_createtable(L, vision_set.size(), 0);
-	int i = 1;
-	for (const map_location& loc : vision_set)
-	{
-		lua_createtable(L, 2, 0);
-		lua_pushinteger(L, loc.wml_x());
-		lua_rawseti(L, -2, 1);
-		lua_pushinteger(L, loc.wml_y());
-		lua_rawseti(L, -2, 2);
-		lua_rawseti(L, -2, i);
-		++i;
-	}
+	luaW_push_locationset(L, vision_set);
 	return 1;
 }
 
@@ -2158,6 +2147,18 @@ int game_lua_kernel::intf_find_cost_map(lua_State *L)
 
 		lua_pushinteger(L, cost_map.get_pair_at(loc).second);
 		lua_rawseti(L, -2, 4);
+		
+		lua_pushinteger(L, loc.wml_x());
+		lua_setfield(L, -2, "x");
+
+		lua_pushinteger(L, loc.wml_y());
+		lua_setfield(L, -2, "y");
+
+		lua_pushinteger(L, cost_map.get_pair_at(loc).first);
+		lua_setfield(L, -2, "cost");
+
+		lua_pushinteger(L, cost_map.get_pair_at(loc).second);
+		lua_setfield(L, -2, "reach");
 
 		lua_rawseti(L, -2, counter);
 		++counter;
@@ -3006,18 +3007,7 @@ int game_lua_kernel::intf_get_locations(lua_State *L)
 		t_filter.get_locations(res, true);
 	}
 
-	lua_createtable(L, res.size(), 0);
-	int i = 1;
-	for (const map_location& loc : res)
-	{
-		lua_createtable(L, 2, 0);
-		lua_pushinteger(L, loc.wml_x());
-		lua_rawseti(L, -2, 1);
-		lua_pushinteger(L, loc.wml_y());
-		lua_rawseti(L, -2, 2);
-		lua_rawseti(L, -2, i);
-		++i;
-	}
+	luaW_push_locationset(L, res);
 	return 1;
 }
 
@@ -4095,8 +4085,7 @@ int game_lua_kernel::intf_toggle_fog(lua_State *L, const bool clear)
 			sides.insert(t.side()+1);
 		}
 	}
-	const auto& v_locs = lua_check<std::vector<map_location>>(L, lua_istable(L, 2) ? 2 : 1);
-	std::set<map_location> locs(v_locs.begin(), v_locs.end());
+	const auto& locs = luaW_check_locationset(L, lua_istable(L, 2) ? 2 : 1);
 
 	for(const int &side_num : sides) {
 		if(side_num < 1 || static_cast<std::size_t>(side_num) > teams().size()) {

src/scripting/lua_common.cpp
@@ -671,6 +671,12 @@ void luaW_pushlocation(lua_State *L, const map_location& ml)
 
 	lua_pushinteger(L, ml.wml_y());
 	lua_rawseti(L, -2, 2);
+	
+	lua_pushinteger(L, ml.wml_x());
+	lua_setfield(L, -2, "x");
+
+	lua_pushinteger(L, ml.wml_y());
+	lua_setfield(L, -2, "y");
 }
 
 bool luaW_tolocation(lua_State *L, int index, map_location& loc) {
@@ -686,7 +692,7 @@ bool luaW_tolocation(lua_State *L, int index, map_location& loc) {
 
 	index = lua_absindex(L, index);
 
-	if (lua_istable(L, index) || luaW_tounit(L, index) || luaW_tovconfig(L, index, dummy_vcfg)) {
+	if (lua_istable(L, index) || lua_isuserdata(L, index)) {
 		map_location result;
 		int x_was_num = 0, y_was_num = 0;
 		lua_getfield(L, index, "x");
@@ -696,7 +702,7 @@ bool luaW_tolocation(lua_State *L, int index, map_location& loc) {
 		lua_pop(L, 2);
 		if (!x_was_num || !y_was_num) {
 			// If we get here and it was userdata, checking numeric indices won't help
-			// (It won't help if it was a config either, but there's no easy way to check that.)
+			// (It won't help if it was a WML table either, but there's no easy way to check that.)
 			if (lua_isuserdata(L, index)) {
 				return false;
 			}
@@ -735,11 +741,7 @@ int luaW_push_locationset(lua_State* L, const std::set<map_location>& locs)
 	lua_createtable(L, locs.size(), 0);
 	int i = 1;
 	for(const map_location& loc : locs) {
-		lua_createtable(L, 2, 0);
-		lua_pushinteger(L, loc.wml_x());
-		lua_rawseti(L, -2, 1);
-		lua_pushinteger(L, loc.wml_y());
-		lua_rawseti(L, -2, 2);
+		luaW_pushlocation(L, loc);
 		lua_rawseti(L, -2, i);
 		++i;
 	}

src/ai/lua/core.cpp
@@ -275,10 +275,7 @@ static int ai_recruit(lua_State *L, bool exec)
 	const char *unit_name = luaL_checkstring(L, 1);
 	int side = get_readonly_context(L).get_side();
 	map_location where;
-	if (!lua_isnoneornil(L, 2)) {
-		where.set_wml_x(lua_tonumber(L, 2));
-		where.set_wml_y(lua_tonumber(L, 3));
-	}
+	luaW_tolocation(L, 2, where);
 	map_location from = map_location::null_location();
 	ai::recruit_result_ptr recruit_result = ai::actions::execute_recruit_action(side,exec,std::string(unit_name),where,from);
 	return transform_ai_action(L,recruit_result);
@@ -299,10 +296,7 @@ static int ai_recall(lua_State *L, bool exec)
 	const char *unit_id = luaL_checkstring(L, 1);
 	int side = get_readonly_context(L).get_side();
 	map_location where;
-	if (!lua_isnoneornil(L, 2)) {
-		where.set_wml_x(lua_tonumber(L, 2));
-		where.set_wml_y(lua_tonumber(L, 3));
-	}
+	luaW_tolocation(L, 2, where);
 	map_location from = map_location::null_location();
 	ai::recall_result_ptr recall_result = ai::actions::execute_recall_action(side,exec,std::string(unit_id),where,from);
 	return transform_ai_action(L,recall_result);
@@ -396,7 +390,7 @@ static int cfun_ai_get_avoid(lua_State *L)
 	std::set<map_location> locs;
 	terrain_filter avoid = get_readonly_context(L).get_avoid();
 	avoid.get_locations(locs);
-	lua_push(L, locs);
+	luaW_push_locationset(L, locs);
 
 	return 1;
 }

commit d04bd2bf5d2ad932ca9bb3203f4ff9f91411e261

Lua API: Add a named tuple idiom, used mainly for locations
This should fix the issue introduced in 5a9c24c4e79d624e34aa475949a4aa49b1984322

I also spotted a couple more cases missed in the prior commit.

src/scripting/game_lua_kernel.cpp
@@ -1803,11 +1803,7 @@ int game_lua_kernel::intf_find_path(lua_State *L)
 	lua_createtable(L, nb, 0);
 	for (int i = 0; i < nb; ++i)
 	{
-		lua_createtable(L, 2, 0);
-		lua_pushinteger(L, res.steps[i].wml_x());
-		lua_rawseti(L, -2, 1);
-		lua_pushinteger(L, res.steps[i].wml_y());
-		lua_rawseti(L, -2, 2);
+		luaW_pushlocation(L, res.steps[i]);
 		lua_rawseti(L, -2, i + 1);
 	}
 	lua_pushinteger(L, res.move_cost);
@@ -1877,7 +1873,7 @@ int game_lua_kernel::intf_find_reach(lua_State *L)
 	for (int i = 0; i < nb; ++i)
 	{
 		pathfind::paths::step &s = res.destinations[i];
-		lua_createtable(L, 2, 0);
+		luaW_push_namedtuple(L, {"x", "y", "moves_left"});
 		lua_pushinteger(L, s.curr.wml_x());
 		lua_rawseti(L, -2, 1);
 		lua_pushinteger(L, s.curr.wml_y());
@@ -2134,7 +2130,7 @@ int game_lua_kernel::intf_find_cost_map(lua_State *L)
 	int counter = 1;
 	for (const map_location& loc : location_set)
 	{
-		lua_createtable(L, 4, 0);
+		luaW_push_namedtuple(L, {"x", "y", "cost", "reach"});
 
 		lua_pushinteger(L, loc.wml_x());
 		lua_rawseti(L, -2, 1);
@@ -2147,18 +2143,6 @@ int game_lua_kernel::intf_find_cost_map(lua_State *L)
 
 		lua_pushinteger(L, cost_map.get_pair_at(loc).second);
 		lua_rawseti(L, -2, 4);
-		
-		lua_pushinteger(L, loc.wml_x());
-		lua_setfield(L, -2, "x");
-
-		lua_pushinteger(L, loc.wml_y());
-		lua_setfield(L, -2, "y");
-
-		lua_pushinteger(L, cost_map.get_pair_at(loc).first);
-		lua_setfield(L, -2, "cost");
-
-		lua_pushinteger(L, cost_map.get_pair_at(loc).second);
-		lua_setfield(L, -2, "reach");
 
 		lua_rawseti(L, -2, counter);
 		++counter;

src/scripting/lua_common.cpp
@@ -662,21 +662,61 @@ void luaW_filltable(lua_State *L, const config& cfg)
 	}
 }
 
+static int impl_namedtuple_get(lua_State* L)
+{
+	if(lua_isstring(L, 2)) {
+		std::string k = lua_tostring(L, 2);
+		luaL_getmetafield(L, 1, "__names");
+		auto names = lua_check<std::vector<std::string>>(L, -1);
+		auto iter = std::find(names.begin(), names.end(), k);
+		if(iter != names.end()) {
+			int i = std::distance(names.begin(), iter) + 1;
+			lua_rawgeti(L, 1, i);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+static int impl_namedtuple_tostring(lua_State* L)
+{
+	std::vector<std::string> elems;
+	for(unsigned i = 1; i <= lua_rawlen(L, 1); i++) {
+		lua_getglobal(L, "tostring");
+		lua_rawgeti(L, 1, i);
+		lua_call(L, 1, 1);
+		elems.push_back(lua_tostring(L, -1));
+	}
+	lua_push(L, "(" + utils::join(elems) + ")");
+	return 1;
+}
+
+void luaW_push_namedtuple(lua_State* L, const std::vector<std::string>& names)
+{
+	lua_createtable(L, names.size(), 0);
+	lua_createtable(L, 0, 4);
+	static luaL_Reg callbacks[] = {
+		{ "__index", &impl_namedtuple_get },
+		{ "__tostring", &impl_namedtuple_tostring },
+		{ nullptr, nullptr }
+	};
+	luaL_setfuncs(L, callbacks, 0);
+	lua_pushliteral(L, "named tuple");
+	lua_setfield(L, -2, "__metatable");
+	lua_push(L, names);
+	lua_setfield(L, -2, "__names");
+	lua_setmetatable(L, -2);
+}
+
 void luaW_pushlocation(lua_State *L, const map_location& ml)
 {
-	lua_createtable(L, 2, 0);
+	luaW_push_namedtuple(L, {"x", "y"});
 
 	lua_pushinteger(L, ml.wml_x());
 	lua_rawseti(L, -2, 1);
 
 	lua_pushinteger(L, ml.wml_y());
 	lua_rawseti(L, -2, 2);
-	
-	lua_pushinteger(L, ml.wml_x());
-	lua_setfield(L, -2, "x");
-
-	lua_pushinteger(L, ml.wml_y());
-	lua_setfield(L, -2, "y");
 }
 
 bool luaW_tolocation(lua_State *L, int index, map_location& loc) {

src/scripting/lua_common.hpp
@@ -94,6 +94,13 @@ bool luaW_iststring(lua_State* L, int index);
 void luaW_filltable(lua_State *L, const config& cfg);
 
 /**
+ * Push an empty "named tuple" onto the stack.
+ * A named tuple is an array where each index can also be accessed by name.
+ * Once it's pushed, you can set the elements, eg with lua_rawseti.
+ */
+void luaW_push_namedtuple(lua_State* L, const std::vector<std::string>& names);
+
+/**
  * Converts a map location object to a Lua table pushed at the top of the stack.
  */
 void luaW_pushlocation(lua_State *L, const map_location& loc);