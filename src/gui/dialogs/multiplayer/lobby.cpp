wesnoth/wesnoth

commit 0e3faee8031610a1e7db2cd484e4c65cfbe292ea

Add a method to clear and then add multiple items to a tree view.
This addresses the lobby performance issue as far as the playerlist updating. Previously for every player an item was added and the size of the tree view recalculated, which led to an increasingly unresponsive UI when the number of players in the lobby got to around 150+. With this change, the replace_children() method adds all the players and then after that recalculates the tree view's size once.

src/gui/dialogs/multiplayer/lobby.cpp
@@ -104,7 +104,7 @@ void sub_player_list::update_player_count_label()
 void player_list::init(window& w)
 {
 	active_game.init(w, _("Selected Game"), true);
-	other_rooms.init(w, _("Lobby"), true);
+	lobby_players.init(w, _("Lobby"), true);
 	other_games.init(w, _("Other Games"));
 
 	tree = find_widget<tree_view>(&w, "player_tree", false, true);
@@ -585,35 +585,34 @@ void mp_lobby::update_playerlist()
 
 	assert(player_list_.active_game.tree);
 	assert(player_list_.other_games.tree);
-	assert(player_list_.other_rooms.tree);
+	assert(player_list_.lobby_players.tree);
 
 	unsigned scrollbar_position = player_list_.tree->get_vertical_scrollbar_item_position();
 
 	player_list_.active_game.tree->clear();
 	player_list_.other_games.tree->clear();
-	player_list_.other_rooms.tree->clear();
+	player_list_.lobby_players.tree->clear();
 
-	for(auto& user : lobby_info_.users()) {
-		sub_player_list* target_list(nullptr);
-
+	std::map<std::string, std::map<std::string, string_map>> lobby_player_items;
+	std::map<std::string, std::map<std::string, string_map>> active_game_items;
+	std::map<std::string, std::map<std::string, string_map>> other_game_items;
+	for(const auto& user : lobby_info_.users()) {
 		std::string name = user.name;
 
 		std::stringstream icon_ss;
+
 		icon_ss << "lobby/status";
 		switch(user.state) {
 			case mp::user_info::user_state::LOBBY:
 				icon_ss << "-lobby";
-				target_list = &player_list_.other_rooms;
 				break;
 			case mp::user_info::user_state::SEL_GAME:
 				name = colorize(name, {0, 255, 255});
 				icon_ss << (user.observing ? "-obs" : "-playing");
-				target_list = &player_list_.active_game;
 				break;
 			case mp::user_info::user_state::GAME:
 				name = colorize(name, font::GRAY_COLOR);
 				icon_ss << (user.observing ? "-obs" : "-playing");
-				target_list = &player_list_.other_games;
 				break;
 			default:
 				ERR_LB << "Bad user state in lobby: " << user.name << ": " << static_cast<int>(user.state) << "\n";
@@ -639,8 +638,6 @@ void mp_lobby::update_playerlist()
 
 		icon_ss << ".png";
 
-		assert(target_list->tree);
-
 		string_map tree_group_field;
 		std::map<std::string, string_map> tree_group_item;
 
@@ -652,14 +649,37 @@ void mp_lobby::update_playerlist()
 		tree_group_field["use_markup"] = "true";
 		tree_group_item["name"] = tree_group_field;
 
-		tree_view_node& player = target_list->tree->add_child("player", tree_group_item);
+		switch(user.state) {
+			case mp::user_info::user_state::LOBBY:
+				lobby_player_items[user.name] = tree_group_item;
+				break;
+			case mp::user_info::user_state::SEL_GAME:
+				active_game_items[user.name] = tree_group_item;
+				break;
+			case mp::user_info::user_state::GAME:
+				other_game_items[user.name] = tree_group_item;
+				break;
+			default:
+				ERR_LB << "Bad user state in lobby: " << user.name << ": " << static_cast<int>(user.state) << "\n";
+				continue;
+		}
+	}
 
-		connect_signal_mouse_left_double_click(find_widget<toggle_panel>(&player, "tree_view_node_label", false),
-			std::bind(&mp_lobby::user_dialog_callback, this, &user));
+	for(const auto& player : player_list_.active_game.tree->replace_children("player", active_game_items)) {
+		 connect_signal_mouse_left_double_click(find_widget<toggle_panel>(player.second.get(), "tree_view_node_label", false),
+		 	std::bind(&mp_lobby::user_dialog_callback, this, lobby_info_.get_user(player.first)));
+	}
+	for(const auto& player : player_list_.lobby_players.tree->replace_children("player", lobby_player_items)) {
+		 connect_signal_mouse_left_double_click(find_widget<toggle_panel>(player.second.get(), "tree_view_node_label", false),
+		 	std::bind(&mp_lobby::user_dialog_callback, this, lobby_info_.get_user(player.first)));
+	}
+	for(const auto& player : player_list_.other_games.tree->replace_children("player", other_game_items)) {
+		 connect_signal_mouse_left_double_click(find_widget<toggle_panel>(player.second.get(), "tree_view_node_label", false),
+		 	std::bind(&mp_lobby::user_dialog_callback, this, lobby_info_.get_user(player.first)));
 	}
 
 	player_list_.active_game.update_player_count_label();
-	player_list_.other_rooms.update_player_count_label();
+	player_list_.lobby_players.update_player_count_label();
 	player_list_.other_games.update_player_count_label();
 
 	// Don't attempt to restore the scroll position if the window hasn't been laid out yet

src/gui/dialogs/multiplayer/lobby.hpp
@@ -54,7 +54,7 @@ struct player_list
 	void init(window& w);
 
 	sub_player_list active_game;
-	sub_player_list other_rooms;
+	sub_player_list lobby_players;
 	sub_player_list other_games;
 
 	tree_view* tree;

src/gui/widgets/tree_view_node.cpp
@@ -126,7 +126,7 @@ void tree_view_node::clear_before_destruct()
 	}
 }
 
-tree_view_node& tree_view_node::add_child_impl(ptr_t&& new_node, const int index)
+tree_view_node& tree_view_node::add_child_impl(std::shared_ptr<tree_view_node>&& new_node, const int index)
 {
 	auto itor = children_.end();
 
@@ -184,6 +184,82 @@ tree_view_node& tree_view_node::add_child_impl(ptr_t&& new_node, const int index
 	get_tree_view().resize_content(width_modification, height_modification, -1, node.calculate_ypos());
 
 	return node;
+}
+
+std::map<std::string, std::shared_ptr<gui2::tree_view_node>> tree_view_node::replace_children(const std::string& id, const std::map<std::string, std::map<std::string /* widget id */, string_map>>& data)
+{
+	std::map<std::string, std::shared_ptr<gui2::tree_view_node>> nodes;
+	clear();
+
+	if(data.size() == 0)
+	{
+		return nodes;
+	}
+
+	int width_modification = 0;
+
+	for(const auto& d : data)
+	{
+		std::shared_ptr<gui2::tree_view_node> new_node = std::make_shared<tree_view_node>(id, this, get_tree_view(), d.second);
+		std::shared_ptr<gui2::tree_view_node> node = *children_.insert(children_.end(), std::move(new_node));
+
+		// NOTE: we currently don't support moving nodes between different trees, so this
+		// just ensures that wasn't tried. Remove this if we implement support for that.
+		assert(node->tree_view_ == tree_view_);
+
+		// Safety check. Might only fail if someone accidentally removed the parent_node_ setter in add_child().
+		assert(node->parent_node_ == this);
+
+		nodes[d.first] = node;
+
+		if(is_folded()) {
+			continue;
+		}
+
+		assert(get_tree_view().content_grid());
+		const point current_size = get_tree_view().content_grid()->get_size();
+
+		// Calculate width modification.
+		// This increases tree width if the width of the new node is greater than the current width.
+		int best_size = node->get_best_size().x;
+		best_size += get_indentation_level() * get_tree_view().indentation_step_size_;
+
+		int new_width = best_size > current_size.x
+			? best_size - current_size.x
+			: 0;
+
+		if(new_width > width_modification)
+		{
+			width_modification = new_width;
+		}
+	}
+
+	if(is_folded()) {
+		return nodes;
+	}
+
+	// Calculate height modification.
+	// For this, we only increase height if the best size of the tree (that is, the size with the new node)
+	// is larger than its current size. This prevents the scrollbar being reserved even when there's obviously
+	// enough visual space.
+
+	// Throw away cached best size to force a recalculation.
+	get_tree_view().layout_initialize(false);
+
+	const point current_size = get_tree_view().content_grid()->get_size();
+	const point tree_best_size = get_tree_view().get_best_size();
+
+	const int height_modification = tree_best_size.y > current_size.y && get_tree_view().layout_size() == point()
+		? tree_best_size.y - current_size.y
+		: 0;
+
+	assert(height_modification >= 0);
+
+	// Request new size.
+	auto& last_node = children_.at(children_.size()-1);
+	get_tree_view().resize_content(width_modification, height_modification, -1, last_node->calculate_ypos());
+
+	return nodes;
 }
 
 unsigned tree_view_node::get_indentation_level() const

src/gui/widgets/tree_view_node.hpp
@@ -35,8 +35,7 @@ class tree_view_node : public widget
 	friend class tree_view;
 
 public:
-	using ptr_t = std::shared_ptr<tree_view_node>;
-	using node_children_vector = std::vector<ptr_t>;
+	using node_children_vector = std::vector<std::shared_ptr<tree_view_node>>;
 
 	bool operator==(const tree_view_node& node)
 	{
@@ -74,13 +73,26 @@ public:
 	}
 
 	/**
+	 * Replaces all children of this tree with new children.
+	 * The motivation here is to provide a way to add multiple children without calculating the trees size for each child added.
+	 * This is a waste of time since the results of that resizing will be immediately thrown out except for the final child added.
+	 *
+	 * @param id The id of the node definition to use for the new nodes.
+	 * @param data data.first is a unique identifying value to be associated to the respective tree_view_node that's returned.
+	 * 			   data.second is the data to provide to the tree_node_view's constructor.
+	 * @return return_value.first is data.first
+	 * 		   return_value.second is the tree_view_node created from data.second
+	 */
+	std::map<std::string, std::shared_ptr<gui2::tree_view_node>> replace_children(const std::string& id, const std::map<std::string, std::map<std::string /* widget id */, string_map>>& data);
+
+	/**
 	 * Adds a previously-constructed node as a child of this node at the given position.
 	 *
 	 * @param new_node            A smart pointer to the node object to insert.
 	 * @param index               The item before which to add the new item,
 	 *                            0 == begin, -1 == end.
 	 */
-	tree_view_node& add_child(ptr_t new_node, const int index = -1)
+	tree_view_node& add_child(std::shared_ptr<tree_view_node> new_node, const int index = -1)
 	{
 		new_node->parent_node_ = this;
 		return add_child_impl(std::move(new_node), index);
@@ -109,7 +121,7 @@ public:
 
 private:
 	/** Implementation detail for @ref add_child. */
-	tree_view_node& add_child_impl(ptr_t&& new_node, const int index);
+	tree_view_node& add_child_impl(std::shared_ptr<tree_view_node>&& new_node, const int index);
 
 public:
 	/**

src/gui/widgets/tree_view.cpp
@@ -59,7 +59,7 @@ tree_view_node& tree_view::add_node(
 	return get_root_node().add_child(id, data, index);
 }
 
-std::pair<tree_view_node::ptr_t, int> tree_view::remove_node(tree_view_node* node)
+std::pair<std::shared_ptr<tree_view_node>, int> tree_view::remove_node(tree_view_node* node)
 {
 	assert(node && node != root_node_ && node->parent_node_);
 	const point node_size = node->get_size();

commit 1f994775c208de706940a946d8aad8e77401157e

MP/Lobby: split handling of user list updating into separate class
This also fixes an issue introduced in 0e3faee8031610a1e7db2cd484e4c65cfbe292ea where, since the newly added
nodes were sorted by player name, the default sorting of players (first by relation, *then* by name) was ignored.

This also changes tree_view_node::replace_children to take a vector instead of a map for its data.

src/gui/dialogs/multiplayer/lobby.cpp
@@ -65,52 +65,7 @@ static lg::log_domain log_lobby("lobby");
 
 namespace gui2::dialogs
 {
-
 REGISTER_DIALOG(mp_lobby)
-
-void sub_player_list::init(window& w, const std::string& lbl, const bool unfolded)
-{
-	tree_view& parent_tree = find_widget<tree_view>(&w, "player_tree", false);
-
-	std::map<std::string, string_map> tree_group_item;
-	tree_group_item["tree_view_node_label"]["label"] = lbl;
-
-	tree = &parent_tree.add_node("player_group", tree_group_item);
-
-	if(unfolded) {
-		tree->unfold();
-	}
-
-	tree_label = find_widget<label>(tree, "tree_view_node_label", false, true);
-	label_player_count = find_widget<label>(tree, "player_count", false, true);
-
-	assert(tree_label);
-	assert(label_player_count);
-}
-
-void sub_player_list::update_player_count_label()
-{
-	assert(tree);
-	assert(label_player_count);
-
-	/**
-	 * @todo Make sure setting visible resizes the widget.
-	 *
-	 * It doesn't work here since invalidate_layout is blocked, but the
-	 * widget should also be able to handle it itself. Once done the
-	 * setting of the label text can also be removed.
-	 */
-	label_player_count->set_label((formatter() << "(" << tree->count_children() << ")").str());
-}
-
-void player_list::init(window& w)
-{
-	active_game.init(w, _("Selected Game"), true);
-	lobby_players.init(w, _("Lobby"), true);
-	other_games.init(w, _("Other Games"));
-
-	tree = find_widget<tree_view>(&w, "player_tree", false, true);
-}
 
 bool mp_lobby::logout_prompt()
 {
@@ -148,7 +103,7 @@ mp_lobby::mp_lobby(mp::lobby_info& info, wesnothd_connection& connection, int& j
 		  std::bind(&mp_lobby::game_filter_change_callback, this)))
 	, filter_text_(nullptr)
 	, selected_game_id_()
-	, player_list_()
+	, player_list_(std::bind(&mp_lobby::user_dialog_callback, this, std::placeholders::_1))
 	, player_list_dirty_(true)
 	, gamelist_dirty_(true)
 	, last_lobby_update_(0)
@@ -591,109 +546,7 @@ void mp_lobby::update_playerlist()
 	DBG_LB << "Playerlist update: " << lobby_info_.users().size() << "\n";
 	lobby_info_.update_user_statuses(selected_game_id_);
 
-	assert(player_list_.active_game.tree);
-	assert(player_list_.other_games.tree);
-	assert(player_list_.lobby_players.tree);
-
-	unsigned scrollbar_position = player_list_.tree->get_vertical_scrollbar_item_position();
-
-	player_list_.active_game.tree->clear();
-	player_list_.other_games.tree->clear();
-	player_list_.lobby_players.tree->clear();
-
-	std::map<std::string, std::map<std::string, string_map>> lobby_player_items;
-	std::map<std::string, std::map<std::string, string_map>> active_game_items;
-	std::map<std::string, std::map<std::string, string_map>> other_game_items;
-	for(const auto& user : lobby_info_.users()) {
-		std::string name = user.name;
-
-		std::stringstream icon_ss;
-
-		icon_ss << "lobby/status";
-		switch(user.state) {
-			case mp::user_info::user_state::LOBBY:
-				icon_ss << "-lobby";
-				break;
-			case mp::user_info::user_state::SEL_GAME:
-				name = colorize(name, {0, 255, 255});
-				icon_ss << (user.observing ? "-obs" : "-playing");
-				break;
-			case mp::user_info::user_state::GAME:
-				name = colorize(name, font::GRAY_COLOR);
-				icon_ss << (user.observing ? "-obs" : "-playing");
-				break;
-			default:
-				ERR_LB << "Bad user state in lobby: " << user.name << ": " << static_cast<int>(user.state) << "\n";
-				continue;
-		}
-
-		switch(user.relation) {
-			case mp::user_info::user_relation::ME:
-				icon_ss << "-s";
-				break;
-			case mp::user_info::user_relation::NEUTRAL:
-				icon_ss << "-n";
-				break;
-			case mp::user_info::user_relation::FRIEND:
-				icon_ss << "-f";
-				break;
-			case mp::user_info::user_relation::IGNORED:
-				icon_ss << "-i";
-				break;
-			default:
-				ERR_LB << "Bad user relation in lobby: " << static_cast<int>(user.relation) << "\n";
-		}
-
-		icon_ss << ".png";
-
-		string_map tree_group_field;
-		std::map<std::string, string_map> tree_group_item;
-
-		/*** Add tree item ***/
-		tree_group_field["label"] = icon_ss.str();
-		tree_group_item["icon"] = tree_group_field;
-
-		tree_group_field["label"] = name;
-		tree_group_field["use_markup"] = "true";
-		tree_group_item["name"] = tree_group_field;
-
-		switch(user.state) {
-			case mp::user_info::user_state::LOBBY:
-				lobby_player_items[user.name] = tree_group_item;
-				break;
-			case mp::user_info::user_state::SEL_GAME:
-				active_game_items[user.name] = tree_group_item;
-				break;
-			case mp::user_info::user_state::GAME:
-				other_game_items[user.name] = tree_group_item;
-				break;
-			default:
-				ERR_LB << "Bad user state in lobby: " << user.name << ": " << static_cast<int>(user.state) << "\n";
-				continue;
-		}
-	}
-
-	for(const auto& player : player_list_.active_game.tree->replace_children("player", active_game_items)) {
-		 connect_signal_mouse_left_double_click(find_widget<toggle_panel>(player.second.get(), "tree_view_node_label", false),
-		 	std::bind(&mp_lobby::user_dialog_callback, this, lobby_info_.get_user(player.first)));
-	}
-	for(const auto& player : player_list_.lobby_players.tree->replace_children("player", lobby_player_items)) {
-		 connect_signal_mouse_left_double_click(find_widget<toggle_panel>(player.second.get(), "tree_view_node_label", false),
-		 	std::bind(&mp_lobby::user_dialog_callback, this, lobby_info_.get_user(player.first)));
-	}
-	for(const auto& player : player_list_.other_games.tree->replace_children("player", other_game_items)) {
-		 connect_signal_mouse_left_double_click(find_widget<toggle_panel>(player.second.get(), "tree_view_node_label", false),
-		 	std::bind(&mp_lobby::user_dialog_callback, this, lobby_info_.get_user(player.first)));
-	}
-
-	player_list_.active_game.update_player_count_label();
-	player_list_.lobby_players.update_player_count_label();
-	player_list_.other_games.update_player_count_label();
-
-	// Don't attempt to restore the scroll position if the window hasn't been laid out yet
-	if(player_list_.tree->get_origin() != point{-1, -1}) {
-		player_list_.tree->set_vertical_scrollbar_item_position(scrollbar_position);
-	}
+	player_list_.update(lobby_info_.users());
 
 	player_list_dirty_ = false;
 	last_lobby_update_ = SDL_GetTicks();

src/gui/dialogs/multiplayer/lobby.hpp
@@ -15,12 +15,13 @@
 
 #pragma once
 
-#include "gui/dialogs/modal_dialog.hpp"
-#include "gui/widgets/tree_view.hpp"
 #include "chat_events.hpp"
 #include "game_initialization/lobby_info.hpp"
-#include "gui/dialogs/multiplayer/plugin_executor.hpp"
 #include "game_initialization/multiplayer.hpp"
+#include "gui/dialogs/modal_dialog.hpp"
+#include "gui/dialogs/multiplayer/lobby_player_list_helper.hpp"
+#include "gui/dialogs/multiplayer/plugin_executor.hpp"
+#include "gui/widgets/tree_view.hpp"
 #include "quit_confirmation.hpp"
 
 class wesnothd_connection;
@@ -28,7 +29,6 @@ class game_config_view;
 
 namespace gui2
 {
-
 class grid;
 class label;
 class listbox;
@@ -40,27 +40,6 @@ class chatbox;
 
 namespace dialogs
 {
-
-struct sub_player_list
-{
-	void init(window& w, const std::string& label, const bool unfolded = false);
-	void update_player_count_label();
-	tree_view_node* tree;
-	label* tree_label;
-	label* label_player_count;
-};
-
-struct player_list
-{
-	void init(window& w);
-
-	sub_player_list active_game;
-	sub_player_list lobby_players;
-	sub_player_list other_games;
-
-	tree_view* tree;
-};
-
 class mp_lobby : public modal_dialog, public quit_confirmation, private plugin_executor
 {
 public:
@@ -185,7 +164,7 @@ private:
 
 	int selected_game_id_;
 
-	player_list player_list_;
+	player_list_helper player_list_;
 
 	bool player_list_dirty_;
 

src/gui/dialogs/multiplayer/lobby_player_list_helper.cpp
@@ -1,0 +1,196 @@
+/*
+	Copyright (C) 2009 - 2021
+	by Tomasz Sniatowski <kailoran@gmail.com>
+	Part of the Battle for Wesnoth Project https://www.wesnoth.org/
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY.
+
+	See the COPYING file for more details.
+*/
+
+#include "gui/dialogs/multiplayer/lobby_player_list_helper.hpp"
+
+#include "font/text_formatting.hpp"
+#include "game_initialization/lobby_data.hpp"
+#include "gettext.hpp"
+#include "gui/auxiliary/find_widget.hpp"
+#include "gui/core/event/handler.hpp"
+#include "gui/widgets/label.hpp"
+#include "gui/widgets/toggle_panel.hpp"
+#include "gui/widgets/tree_view.hpp"
+#include "gui/widgets/window.hpp"
+
+static lg::log_domain log_lobby("lobby");
+#define ERR_LB LOG_STREAM(err, log_lobby)
+
+namespace gui2
+{
+player_list_helper::sub_list::sub_list(tree_view* parent_tree, const std::string& lbl, const bool unfolded)
+{
+	std::map<std::string, string_map> tree_group_item;
+	tree_group_item["tree_view_node_label"]["label"] = lbl;
+
+	root = &parent_tree->add_node("player_group", tree_group_item);
+
+	if(unfolded) {
+		root->unfold();
+	}
+
+	tree_label = find_widget<label>(root, "tree_view_node_label", false, true);
+	label_player_count = find_widget<label>(root, "player_count", false, true);
+
+	assert(tree_label);
+	assert(label_player_count);
+}
+
+void player_list_helper::sub_list::update_player_count_label()
+{
+	assert(root);
+	assert(label_player_count);
+
+	/**
+	 * @todo Make sure setting visible resizes the widget.
+	 *
+	 * It doesn't work here since invalidate_layout is blocked, but the
+	 * widget should also be able to handle it itself. Once done the
+	 * setting of the label text can also be removed.
+	 */
+	label_player_count->set_label((formatter() << "(" << root->count_children() << ")").str());
+}
+
+namespace
+{
+struct update_pod
+{
+	/** The raw data used to mass-construct player tree nodes. */
+	std::vector<std::map<std::string, string_map>> node_data;
+
+	/** The associated user data for each node, index-to-index. */
+	std::vector<mp::user_info*> user_data;
+};
+} // namespace
+
+void player_list_helper::update(std::vector<mp::user_info>& user_info)
+{
+	unsigned scrollbar_position = tree->get_vertical_scrollbar_item_position();
+
+	for(auto& list : player_lists) {
+		list.root->clear();
+	}
+
+	std::array<update_pod, std::tuple_size<decltype(player_lists)>::value> inputs{};
+
+	for(auto& user : user_info) {
+		std::string name = user.name;
+
+		std::stringstream icon_ss;
+		icon_ss << "lobby/status";
+
+		switch(user.state) {
+		case mp::user_info::user_state::LOBBY:
+			icon_ss << "-lobby";
+			break;
+		case mp::user_info::user_state::SEL_GAME:
+			name = font::span_color({0, 255, 255}, name);
+			icon_ss << (user.observing ? "-obs" : "-playing");
+			break;
+		case mp::user_info::user_state::GAME:
+			name = font::span_color(font::GRAY_COLOR, name);
+			icon_ss << (user.observing ? "-obs" : "-playing");
+			break;
+		default:
+			ERR_LB << "Bad user state in lobby: " << user.name << ": " << static_cast<int>(user.state) << "\n";
+			continue;
+		}
+
+		switch(user.relation) {
+		case mp::user_info::user_relation::ME:
+			icon_ss << "-s";
+			break;
+		case mp::user_info::user_relation::NEUTRAL:
+			icon_ss << "-n";
+			break;
+		case mp::user_info::user_relation::FRIEND:
+			icon_ss << "-f";
+			break;
+		case mp::user_info::user_relation::IGNORED:
+			icon_ss << "-i";
+			break;
+		default:
+			ERR_LB << "Bad user relation in lobby: " << static_cast<int>(user.relation) << "\n";
+		}
+
+		icon_ss << ".png";
+
+		string_map tree_group_field;
+		std::map<std::string, string_map> tree_group_item;
+
+		/*** Add tree item ***/
+		tree_group_field["label"] = icon_ss.str();
+		tree_group_item["icon"] = tree_group_field;
+
+		tree_group_field["label"] = name;
+		tree_group_field["use_markup"] = "true";
+		tree_group_item["name"] = tree_group_field;
+
+		// Indices here must match the order of the lists in the player_lists array (see `init`)
+		switch(user.state) {
+		case mp::user_info::user_state::SEL_GAME:
+			inputs[0].node_data.push_back(std::move(tree_group_item));
+			inputs[0].user_data.push_back(&user);
+
+			break;
+		case mp::user_info::user_state::LOBBY:
+			inputs[1].node_data.push_back(std::move(tree_group_item));
+			inputs[1].user_data.push_back(&user);
+
+			break;
+		case mp::user_info::user_state::GAME:
+			inputs[2].node_data.push_back(std::move(tree_group_item));
+			inputs[2].user_data.push_back(&user);
+
+			break;
+		default:
+			ERR_LB << "Bad user state in lobby: " << user.name << ": " << static_cast<int>(user.state) << "\n";
+			continue;
+		}
+	}
+
+	for(std::size_t i = 0; i < player_lists.size(); ++i) {
+		assert(inputs[i].node_data.size() == inputs[i].user_data.size());
+
+		// Add the player nodes
+		const auto new_nodes = player_lists[i].root->replace_children("player", inputs[i].node_data);
+
+		for(std::size_t k = 0; k < new_nodes.size(); ++k) {
+			connect_signal_mouse_left_double_click(
+				find_widget<toggle_panel>(new_nodes[k].get(), "tree_view_node_label", false),
+				std::bind(user_callback, inputs[i].user_data[k])
+			);
+		}
+
+		player_lists[i].update_player_count_label();
+	}
+
+	// Don't attempt to restore the scroll position if the window hasn't been laid out yet
+	if(tree->get_origin() != point{-1, -1}) {
+		tree->set_vertical_scrollbar_item_position(scrollbar_position);
+	}
+}
+
+void player_list_helper::init(window& w)
+{
+	tree = find_widget<tree_view>(&w, "player_tree", false, true);
+
+	player_lists = {
+		sub_list{tree, _("Selected Game"), true},
+		sub_list{tree, _("Lobby"), true},
+		sub_list{tree, _("Other Games"), false}
+	};
+}
+} // namespace gui2

src/gui/dialogs/multiplayer/lobby_player_list_helper.hpp
@@ -1,0 +1,73 @@
+/*
+	Copyright (C) 2009 - 2021
+	by Tomasz Sniatowski <kailoran@gmail.com>
+	Part of the Battle for Wesnoth Project https://www.wesnoth.org/
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY.
+
+	See the COPYING file for more details.
+*/
+
+#pragma once
+
+#include <array>
+#include <functional>
+#include <string>
+
+namespace mp
+{
+struct user_info;
+}
+
+namespace gui2
+{
+class label;
+class tree_view;
+class tree_view_node;
+class window;
+
+class player_list_helper
+{
+public:
+	void init(window& w);
+
+	/** Updates the tree contents based on the given user data. */
+	void update(std::vector<mp::user_info>& user_info);
+
+private:
+	struct sub_list
+	{
+		sub_list() = default;
+		sub_list(tree_view* parent_tree, const std::string& label, const bool unfolded);
+
+		void update_player_count_label();
+
+		tree_view_node* root;
+
+		label* tree_label;
+		label* label_player_count;
+	};
+
+	/** Main subsections of the player tree. This should never change at runtime so the number is hardcoded. */
+	std::array<sub_list, 3> player_lists;
+
+	/** The parent tree. */
+	tree_view* tree;
+
+	/** The double click callback bound to each player's tree node. */
+	std::function<void(mp::user_info*)> user_callback;
+
+public:
+	player_list_helper(decltype(user_callback) ucb)
+		: player_lists()
+		, tree(nullptr)
+		, user_callback(ucb)
+	{
+	}
+};
+} // namespace gui2

src/gui/widgets/tree_view_node.cpp
@@ -182,21 +182,19 @@ tree_view_node& tree_view_node::add_child_impl(std::shared_ptr<tree_view_node>&&
 	return node;
 }
 
-std::map<std::string, std::shared_ptr<gui2::tree_view_node>> tree_view_node::replace_children(const std::string& id, const std::map<std::string, std::map<std::string /* widget id */, string_map>>& data)
+std::vector<std::shared_ptr<gui2::tree_view_node>> tree_view_node::replace_children(const std::string& id, const std::vector<std::map<std::string /* widget id */, string_map>>& data)
 {
-	std::map<std::string, std::shared_ptr<gui2::tree_view_node>> nodes;
+	std::vector<std::shared_ptr<gui2::tree_view_node>> nodes;
 	clear();
 
-	if(data.size() == 0)
-	{
+	if(data.size() == 0) {
 		return nodes;
 	}
 
 	int width_modification = 0;
 
-	for(const auto& d : data)
-	{
-		std::shared_ptr<gui2::tree_view_node> new_node = std::make_shared<tree_view_node>(id, this, get_tree_view(), d.second);
+	for(const auto& d : data) {
+		std::shared_ptr<gui2::tree_view_node> new_node = std::make_shared<tree_view_node>(id, this, get_tree_view(), d);
 		std::shared_ptr<gui2::tree_view_node> node = *children_.insert(children_.end(), std::move(new_node));
 
 		// NOTE: we currently don't support moving nodes between different trees, so this
@@ -206,7 +204,7 @@ std::map<std::string, std::shared_ptr<gui2::tree_view_node>> tree_view_node::rep
 		// Safety check. Might only fail if someone accidentally removed the parent_node_ setter in add_child().
 		assert(node->parent_node_ == this);
 
-		nodes[d.first] = node;
+		nodes.push_back(node);
 
 		if(is_folded()) {
 			continue;

src/gui/widgets/tree_view_node.hpp
@@ -78,13 +78,11 @@ public:
 	 * The motivation here is to provide a way to add multiple children without calculating the trees size for each child added.
 	 * This is a waste of time since the results of that resizing will be immediately thrown out except for the final child added.
 	 *
-	 * @param id The id of the node definition to use for the new nodes.
-	 * @param data data.first is a unique identifying value to be associated to the respective tree_view_node that's returned.
-	 * 			   data.second is the data to provide to the tree_node_view's constructor.
-	 * @return return_value.first is data.first
-	 * 		   return_value.second is the tree_view_node created from data.second
+	 * @param id                  The id of the node definition to use for the new nodes.
+	 * @param data                A vector of the data to provide to the tree_node_view's constructor.
+	 * @return                    A vector of pointers to the newly created nodes.
 	 */
-	std::map<std::string, std::shared_ptr<gui2::tree_view_node>> replace_children(const std::string& id, const std::map<std::string, std::map<std::string /* widget id */, string_map>>& data);
+	std::vector<std::shared_ptr<gui2::tree_view_node>> replace_children(const std::string& id, const std::vector<std::map<std::string /* widget id */, string_map>>& data);
 
 	/**
 	 * Adds a previously-constructed node as a child of this node at the given position.