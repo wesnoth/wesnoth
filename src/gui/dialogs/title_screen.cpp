ce08eda7f8b5f37b2b39dfe2e2dcc7cebd52299d
Iris Morelle <shadowm@wesnoth.org>
2021-10-15 03:42:19 -0300

    gui2/title_screen: Fix edge case with "c.UTF-8" on the Language button
    This also refactors boost_name out of a lambda expression so we don't
    create the same string a dozen times.

--- src/gui/dialogs/title_screen.cpp
+++ src/gui/dialogs/title_screen.cpp
@@ -316,19 +316,27 @@ void title_screen::pre_show(window& win)
 
 	if(auto* lang_button = find_widget<button>(&win, "language", false, false); lang_button) {
 		const auto& locale = translation::get_effective_locale_info();
+		// Just assume everything is UTF-8 (it should be as long as we're called Wesnoth)
+		// and strip the charset from the Boost locale identifier.
+		const auto& boost_name = boost::algorithm::erase_first_copy(locale.name(), ".UTF-8");
 		const auto& langs = get_languages(true);
 
 		auto lang_def = std::find_if(langs.begin(), langs.end(), [&](language_def const& lang) {
-			// Just assume everything is UTF-8 (it should be as long as we're called Wesnoth)
-			// and strip the charset from the Boost locale identifier.
-			const auto& boost_name = boost::algorithm::erase_first_copy(locale.name(), ".UTF-8");
-			// std::cerr << lang.localename << '/' << boost_name << '\n';
 			return lang.localename == boost_name;
 		});
 
-		// If somehow the locale doesn't match a known translation, use the
-		// locale identifier as a last resort
-		lang_button->set_label(lang_def != langs.end() ? lang_def->language.str() : locale.name());
+		if(lang_def != langs.end()) {
+			lang_button->set_label(lang_def->language.str());
+		} else if(boost_name == "c" || boost_name == "C") {
+			// HACK: sometimes System Default doesn't match anything on the list. If you fork
+			// Wesnoth and change the neutral language to something other than US English, you
+			// want to change this too.
+			lang_button->set_label("English (US)");
+		} else {
+			// If somehow the locale doesn't match a known translation, use the
+			// locale identifier as a last resort
+			lang_button->set_label(boost_name);
+		}
 	}
 
 	//
