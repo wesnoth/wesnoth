dde6f2ba6adb4eed1af2f9ecceb467132b54e64e
Pentarctagon <pentarctagon@tutamail.com>
2022-01-09 21:05:17 -0600

    Run the WML unit tests on macOS.
    Not really sure why they're able to run now.

--- .github/workflows/ci-scripts/macos.sh
+++ .github/workflows/ci-scripts/macos.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo ~
-echo $PWD
-
 HOMEBREW_NO_AUTO_UPDATE=1 brew install ccache scons
 export PATH="/usr/local/opt/gettext/bin:/usr/local/opt/ccache/libexec:$PWD/utils/CI:$PATH"
 export CC=ccache-clang
@@ -19,9 +16,15 @@ cd ./projectfiles/Xcode
 xcodebuild CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -project "The Battle for Wesnoth.xcodeproj" -target "The Battle for Wesnoth" -configuration "$CFG"
 EXIT_VAL=$?
 
+hdiutil create -volname "Wesnoth_${CFG}" -fs 'HFS+' -srcfolder "build/$CFG" -ov -format UDBZ "Wesnoth_${CFG}.dmg"
+
 ccache -s
 ccache -z
 
-hdiutil create -volname "Wesnoth_${CFG}" -fs 'HFS+' -srcfolder "build/$CFG" -ov -format UDBZ "Wesnoth_${CFG}.dmg"
+if [ $EXIT_VAL == 0 ] && [ "$CFG" == "Release" ]; then
+		cd ../..
+		./run_wml_tests -g -c -t 20 -p "./projectfiles/Xcode/build/$CFG/The Battle for Wesnoth.app/Contents/MacOS/The Battle for Wesnoth"
+		EXIT_VAL=$?
+fi
 
 exit $EXIT_VAL
