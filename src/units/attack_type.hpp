3fe24cade94b23c8b60616d41b865ad6dd513571
newfrenchy83 <eric.belleux@gmail.com>
2022-02-10 18:10:16 +0100

    (1.16) remove only_active argument to weapon_specials()

--- src/units/attack_type.hpp
+++ src/units/attack_type.hpp
@@ -82,7 +82,7 @@ public:
 	bool has_special(const std::string& special, bool simple_check=false, bool special_id=true, bool special_tags=true) const;
 	unit_ability_list get_specials(const std::string& special) const;
 	std::vector<std::pair<t_string, t_string>> special_tooltips(boost::dynamic_bitset<>* active_list = nullptr) const;
-	std::string weapon_specials(bool only_active=false, bool is_backstab=false) const;
+	std::string weapon_specials(bool is_backstab=false) const;
 
 	/** Calculates the number of attacks this weapon has, considering specials. */
 	void modified_attacks(bool is_backstab, unsigned & min_attacks,
