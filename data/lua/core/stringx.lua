7ec64a902e041c0b972d3de6811ae874f2c6d2c0
Celtic Minstrel <celtic.minstrel.ca@some.place>
2022-02-13 13:10:29 -0500

    Document parameters in stringx.split wrappers

--- data/lua/core/stringx.lua
+++ data/lua/core/stringx.lua
@@ -2,9 +2,9 @@
 print("Loading stringx module...")
 
 ---Split a string on a separator, skipping separators that have been escaped
----@param str string
----@param sep? string
----@param esc? string
+---@param str string string to split
+---@param sep? string separator character
+---@param esc? string escape character
 ---@return string[]
 function stringx.escaped_split(str, sep, esc)
 	esc = esc or '\\'
@@ -12,10 +12,10 @@ function stringx.escaped_split(str, sep, esc)
 end
 
 ---Split a string on a separator, skipping separatorss that are enclosed in quotes
----@param str string
----@param sep? string
----@param left? string
----@param right? string
+---@param str string string to split
+---@param sep? string separator character
+---@param left? string left quote characters
+---@param right? string right quote characters
 ---@return string[]
 function stringx.quoted_split(str, sep, left, right)
 	right = right or left
@@ -27,8 +27,8 @@ function stringx.quoted_split(str, sep, left, right)
 end
 
 ---Split a progressive string on a separator
----@param str string
----@param sep string
+---@param str string string to split
+---@param sep string separator character
 ---@return string[]
 function stringx.anim_split(str, sep)
 	return stringx.split(str, sep, {expand_anim = true, strip_spaces = true, remove_empty = true});
