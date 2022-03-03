36a90ce4c45acdd8441c3ddd7a64737ac8106c98
Pentarctagon <pentarctagon@tutamail.com>
2022-03-03 12:46:55 -0600

    Make lua a submodule.
    No changes required to the lua source anymore.

--- src/nowarn/llimits.h
+++ src/nowarn/llimits.h
@@ -1,13 +1,13 @@
 #if defined(__clang__)
     #pragma clang diagnostic push
     #pragma clang diagnostic ignored "-Wold-style-cast"
-    #include "lua/llimits.h"
+    #include "modules/lua/llimits.h"
     #pragma clang diagnostic pop
 #elif defined(__GNUG__)
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wold-style-cast"
-    #include "lua/llimits.h"
+    #include "modules/lua/llimits.h"
     #pragma GCC diagnostic pop
 #elif defined(_MSC_VER)
-    #include "lua/llimits.h"
+    #include "modules/lua/llimits.h"
 #endif
