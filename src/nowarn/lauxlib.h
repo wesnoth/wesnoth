c6bed72a3b6bf842b572df699f0034ac2f72523d
Pentarctagon <pentarctagon@tutamail.com>
2022-03-03 20:16:47 -0600

    Rename variable.

--- src/nowarn/lauxlib.h
+++ src/nowarn/lauxlib.h
@@ -4,15 +4,15 @@
     #pragma clang diagnostic push
     #pragma clang diagnostic ignored "-Wold-style-cast"
     #include "modules/lua/lauxlib.h"
-    constexpr int lual_buffersize = LUAL_BUFFERSIZE;
+    constexpr int luaL_buffersize = LUAL_BUFFERSIZE;
     #pragma clang diagnostic pop
 #elif defined(__GNUG__)
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wold-style-cast"
     #include "modules/lua/lauxlib.h"
-    constexpr int lual_buffersize = LUAL_BUFFERSIZE;
+    constexpr int luaL_buffersize = LUAL_BUFFERSIZE;
     #pragma GCC diagnostic pop
 #elif defined(_MSC_VER)
     #include "modules/lua/lauxlib.h"
-    constexpr int lual_buffersize = LUAL_BUFFERSIZE;
+    constexpr int luaL_buffersize = LUAL_BUFFERSIZE;
 #endif
