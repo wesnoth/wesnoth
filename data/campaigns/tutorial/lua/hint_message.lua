acee69bd053ab498769b6eb3a9a7f6c84579f400
Celtic Minstrel <celtic.minstrel.ca@some.place>
2022-04-10 15:09:34 -0400

    The hint messages now persist in saved games

--- data/campaigns/tutorial/lua/hint_message.lua
+++ data/campaigns/tutorial/lua/hint_message.lua
@@ -1,17 +1,18 @@
 
 local hint_message = {valid = false}
+local hint_text = ""
 function wesnoth.wml_actions.hint_message(cfg)
     if hint_message.valid then
         hint_message:remove()
     end
     if cfg.message and not cfg.remove then
-        local text = cfg.message
+        hint_text = cfg.message
         if cfg.caption and cfg.caption ~= "" then
-            text = "<b><big>" .. cfg.caption .. "</big></b>\n" .. cfg.message
+            hint_text = "<b><big>" .. cfg.caption .. "</big></b>\n" .. hint_text
         else
-            text = cfg.message
+            hint_text = cfg.message
         end
-        hint_message = wesnoth.interface.add_overlay_text(text, {
+        hint_message = wesnoth.interface.add_overlay_text(hint_text, {
             size = 18,
             location = {5,5},
             color = {255, 255, 255},
@@ -22,3 +23,10 @@ function wesnoth.wml_actions.hint_message(cfg)
         })
     end
 end
+
+wesnoth.persistent_tags.hint_message.read = wesnoth.wml_actions.hint_message
+
+function wesnoth.persistent_tags.hint_message.write(add)
+    add{message = hint_text}
+end
+    
