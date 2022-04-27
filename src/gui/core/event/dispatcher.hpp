8d8fa2dbebbbf94bd5ff8b5d238af1d7c9c7b64b
Pentarctagon <pentarctagon@tutamail.com>
2022-04-27 17:44:44 -0500

    Revert "Fix clang template error."
    This reverts commit 3595951e7ecebf8b5cefba1902cae8d3a1255c4b.

--- src/gui/core/event/dispatcher.hpp
+++ src/gui/core/event/dispatcher.hpp
@@ -623,8 +623,7 @@ private:
 		} else if constexpr(cat == event_category::text_input) {
 			return signal_text_input_queue_;
 		} else {
-			// "No matching signal queue for category"
-			utils::static_assert_false();
+			static_assert(utils::dependent_false_v<cat>, "No matching signal queue for category");
 		}
 	}
 };
