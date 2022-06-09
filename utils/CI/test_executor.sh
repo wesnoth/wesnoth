0a0dc90f20e10d11b6fba6b9b786ccb4d3684386
Tommy <mesilliac@tomanui.nz>
2022-06-09 16:28:55 +1200

    CI: Report which boost test suite is being executed.

--- utils/CI/test_executor.sh
+++ utils/CI/test_executor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-gdb -q -batch -return-child-result -ex "set disable-randomization off" -ex "set style enabled on" -ex "run" -ex "thread apply all bt" -ex "quit" --args ./boost_unit_tests 2> error.log
+gdb -q -batch -return-child-result -ex "set disable-randomization off" -ex "set style enabled on" -ex "run" -ex "thread apply all bt" -ex "quit" --args ./boost_unit_tests --color_output --log_level=test_suite 2> error.log
 error_code="$?"
 while grep -q 'Could not initialize SDL_video' error.log; do
   echo "Could not initialize SDL_video error, retrying..."
-  gdb -q -batch -return-child-result -ex "set style enabled on" -ex "run" -ex "thread apply all bt" -ex "quit" --args ./boost_unit_tests 2> error.log
+  gdb -q -batch -return-child-result -ex "set style enabled on" -ex "run" -ex "thread apply all bt" -ex "quit" --args ./boost_unit_tests --color_output --log_level=test_suite 2> error.log
   error_code="$?"
 done
 cat error.log
