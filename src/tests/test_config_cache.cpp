c6b236374e34cbc5e8a873673b1031856cdc1a77
Celtic Minstrel <CelticMinstrel@users.noreply.github.com>
2022-07-04 22:18:16 -0400

    Create a conflict

--- src/tests/test_config_cache.cpp
+++ src/tests/test_config_cache.cpp
@@ -34,7 +34,7 @@ static preproc_map setup_test_preproc_map()
 	preproc_map defines_map;
 
 #if defined(__APPLE__)
-	defines_map["APPLE"] = preproc_define();
+	defines_map["APPLE" ] = preproc_define();
 #endif
 
 	defines_map["WESNOTH_VERSION"] = preproc_define(game_config::wesnoth_version.str());
