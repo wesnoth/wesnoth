debaf5662ec6a81fb4c0330cb5b6d777bcab705c
Celtic Minstrel <CelticMinstrel@users.noreply.github.com>
2022-07-05 20:47:00 -0400

    Ohhh, it broke because Release changed directory while Debug didn't...!

--- .github/workflows/ci-scripts/macos.sh
+++ .github/workflows/ci-scripts/macos.sh
@@ -13,13 +13,12 @@ EXIT_VAL=$?
 
 hdiutil create -volname "Wesnoth_${CFG}" -fs 'HFS+' -srcfolder "build/$CFG" -ov -format UDBZ "Wesnoth_${CFG}.dmg"
 
+cd ../..
 if [ $EXIT_VAL == 0 ] && [ "$CFG" == "Release" ]; then
-		cd ../..
 		./run_wml_tests -g -c -t 30 -p "./projectfiles/Xcode/build/$CFG/The Battle for Wesnoth.app/Contents/MacOS/The Battle for Wesnoth"
 		EXIT_VAL=$?
 fi
 
-cd ../..
 "projectfiles/Xcode/build/$CFG/unit_tests" --color_output --log_level=test_suite
 EXIT_VAL=$?
 
