be1a347618fd501904e00647d0c1d16334681197
Celtic Minstrel <CelticMinstrel@users.noreply.github.com>
2022-07-05 22:43:13 -0400

    Run Boost unit tests on MacOS CI (#6827)
    This also fixes the architecture not specified warning

--- .github/workflows/ci-scripts/macos.sh
+++ .github/workflows/ci-scripts/macos.sh
@@ -8,15 +8,18 @@ scons translations build=release --debug=time nls=true jobs=2 || exit 1
 
 cd ./projectfiles/Xcode
 
-xcodebuild -project "The Battle for Wesnoth.xcodeproj" -target "The Battle for Wesnoth" -configuration "$CFG"
+xcodebuild ARCHS=x86_64 -project "The Battle for Wesnoth.xcodeproj" -target "The Battle for Wesnoth" -target "unit_tests" -configuration "$CFG"
 EXIT_VAL=$?
 
 hdiutil create -volname "Wesnoth_${CFG}" -fs 'HFS+' -srcfolder "build/$CFG" -ov -format UDBZ "Wesnoth_${CFG}.dmg"
 
+cd ../..
 if [ $EXIT_VAL == 0 ] && [ "$CFG" == "Release" ]; then
-		cd ../..
 		./run_wml_tests -g -c -t 30 -p "./projectfiles/Xcode/build/$CFG/The Battle for Wesnoth.app/Contents/MacOS/The Battle for Wesnoth"
 		EXIT_VAL=$?
 fi
+
+"projectfiles/Xcode/build/$CFG/unit_tests" --color_output --log_level=test_suite
+EXIT_VAL=$?
 
 exit $EXIT_VAL
