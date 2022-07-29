073b468ddecd0f530e08ffc567f185deab361f59
Tommy <mesilliac@tomanui.nz>
2022-07-29 18:25:14 +1200

    Remove halo TODOs as done or out of scope

--- src/halo.cpp
+++ src/halo.cpp
@@ -39,10 +39,6 @@ static lg::log_domain log_halo("halo");
 namespace halo
 {
 
-// TODO: draw_manager - GH#1354 - halo_mod doesn't mod halos - maybe not hard
-// TODO: draw_manager - GH#1960 - halos inherit alpha from unit - maybe not hard
-// TODO: draw_manager - GH#6738 - (1) done already (2) halos off screen maybe done
-
 class halo_impl
 {
 
@@ -173,7 +169,6 @@ halo_impl::effect::effect(int xpos, int ypos,
 
 void halo_impl::effect::set_location(int x, int y)
 {
-	// TODO: draw_manager - tidy...
 	int new_x = x - disp->get_location_x(map_location::ZERO());
 	int new_y = y - disp->get_location_y(map_location::ZERO());
 	if (new_x != abs_mid_.x || new_y != abs_mid_.y) {
@@ -189,7 +184,6 @@ rect halo_impl::effect::get_draw_location()
 }
 
 
-// TODO: draw_manager - maybe rename in line with TLD interface.
 /** Update the current location, animation frame, etc. */
 void halo_impl::effect::update()
 {
@@ -225,7 +219,6 @@ void halo_impl::effect::update()
 	int w(tex_.w() * disp->get_zoom_factor());
 	int h(tex_.h() * disp->get_zoom_factor());
 
-	// TODO: draw_manager - should these be baked in?
 	const int zero_x = disp->get_location_x(map_location::ZERO());
 	const int zero_y = disp->get_location_y(map_location::ZERO());
 
@@ -245,8 +238,8 @@ void halo_impl::effect::update()
 bool halo_impl::effect::visible()
 {
 	// Source is shrouded
-	// TODO: draw_manager - what if part of the halo peeks out of the shroud?
-	// TODO: draw_manager - is this related to GH#6738?
+	// The halo will be completely obscured here, even if it would
+	// technically be large enough to peek out of the shroud.
 	if(map_loc_.x != -1 && map_loc_.y != -1 && disp->shrouded(map_loc_)) {
 		return false;
 	}
