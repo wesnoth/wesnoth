4305e2a73a65161f166fa41b9eedd98bcef31768
newfrenchy83 <eric.belleux@gmail.com>
2022-10-14 20:49:17 +0200

    [1.17] add [filter_ability] in [filter] events and [effect]remove_ability and [filter_ability_active] in [filter] events
    see https://forums.wesnoth.org/viewtopic.php?p=681371#p681371 in forum
    
    the type of ability used is also filtered.

--- src/units/filter.cpp
+++ src/units/filter.cpp
@@ -806,6 +806,49 @@ void unit_filter_compound::fill(vconfig cfg)
 					return side_filter(c, args.fc).match(args.u.side());
 				});
 			}
+			else if (child.first == "filter_ability") {
+				create_child(child.second, [](const vconfig& c, const unit_filter_args& args) {
+					for(const config::any_child ab : args.u.abilities().all_children_range()) {
+						if(args.u.ability_matches_filter(ab.cfg, ab.key, c.get_parsed_config())) {
+							return true;
+						}
+					}
+					return false;
+				});
+			}
+			else if (child.first == "filter_ability_active") {
+				create_child(child.second, [](const vconfig& c, const unit_filter_args& args) {
+					if(!display::get_singleton()){
+						return false;
+					}
+					const unit_map& units = display::get_singleton()->get_units();
+					for(const config::any_child ab : args.u.abilities().all_children_range()) {
+						if(args.u.ability_matches_filter(ab.cfg, ab.key, c.get_parsed_config())) {
+							if (args.u.get_self_ability_bool(ab.cfg, ab.key, args.loc)) {
+								return true;
+							}
+						}
+					}
+
+					const auto adjacent = get_adjacent_tiles(args.loc);
+					for(unsigned i = 0; i < adjacent.size(); ++i) {
+						const unit_map::const_iterator it = units.find(adjacent[i]);
+						if (it == units.end() || it->incapacitated())
+							continue;
+						if (&*it == (args.u.shared_from_this()).get())
+							continue;
+
+						for(const config::any_child ab : it->abilities().all_children_range()) {
+							if(it->ability_matches_filter(ab.cfg, ab.key, c.get_parsed_config())) {
+								if (args.u.get_adj_ability_bool(ab.cfg, ab.key, i, args.loc, *it)) {
+									return true;
+								}
+							}
+						}
+					}
+				return false;
+				});
+			}
 			else if (child.first == "has_attack") {
 				create_child(child.second, [](const vconfig& c, const unit_filter_args& args) {
 					for(const attack_type& a : args.u.attacks()) {
