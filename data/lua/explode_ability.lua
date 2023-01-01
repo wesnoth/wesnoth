60c1d489125b7815d5a11c41e19f89c53640a747
doofus-01 <dk0045@comcast.net>
2023-01-01 09:40:51 -0800

    use formula in value key

--- data/lua/explode_ability.lua
+++ data/lua/explode_ability.lua
@@ -21,7 +21,27 @@ wesnoth.game_events.add_repeating("die", function()
 	for i,v in ipairs(wml.get_child(u_exploder.__cfg, "abilities")) do
 		if v.tag == "explode" and v.contents.id == "explode" then
 			local dmg = v.contents.value
+                        if type(dmg) == 'string' then
+                            if string.match( dmg, '^%s*%(.*%)%s*$') then
+                                local params = {
+                                    unit = u_exploder,
+                                    other_unit = wesnoth.units.get(ec.x2, ec.y2)
+                                    }
+                                local result = wesnoth.eval_formula(dmg, params)
+                                dmg = math.floor(tonumber(result))
+                            else
+                                wml.error('invalid formula')
+                            end
+                        end
+                        if type(dmg) ~= 'number' then
+                            wml.error('invalid value')
+                        end
                         local dmg_type = v.contents.apply_to
+                        -- more useful would be to check for one of the six damage types to catch mispellings,
+                        -- but UMC might have more damage types, so string type it is
+                        if type(dmg_type) ~= 'string' then
+                            wml.error('invalid apply_to data')
+                        end
 			for i = 1, #(adjacent_units) do
 			        local affected_unit_cfg = adjacent_units[i].__cfg
 			        wml.fire("harm_unit", {
