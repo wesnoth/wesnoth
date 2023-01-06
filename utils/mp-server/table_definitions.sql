859a56f4c29edf154d423d4ce0e360a7a836b3b2
Pentarctagon <pentarctagon@tutamail.com>
2023-01-06 10:42:21 -0600

    Shrink max column length.
    Required for the updated primary key - the max allowed primary key length is 3072 bytes which isn't enough when there are three VARCHAR(255) columns and each character uses 4 bytes. The max length of the data in these columns so far is also well under 100 characters.

--- utils/mp-server/table_definitions.sql
+++ utils/mp-server/table_definitions.sql
@@ -117,9 +117,9 @@ create table game_content_info
 (
     INSTANCE_UUID     CHAR(36) NOT NULL,
     GAME_ID           INT UNSIGNED NOT NULL,
-    TYPE              VARCHAR(255) NOT NULL,
-    ID                VARCHAR(255) NOT NULL,
-    NAME              VARCHAR(255),
+    TYPE              VARCHAR(100) NOT NULL,
+    ID                VARCHAR(100) NOT NULL,
+    NAME              VARCHAR(100),
     SOURCE            VARCHAR(255) NOT NULL,
     VERSION           VARCHAR(255) NOT NULL,
     PRIMARY KEY (INSTANCE_UUID, GAME_ID, TYPE, ID, SOURCE)
