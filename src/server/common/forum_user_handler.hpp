b5f4a24e3965a0eea3b4f298385978811ca5c5fd
Pentarctagon <pentarctagon@tutamail.com>
2023-01-06 10:53:52 -0600

    Rename database column SOURCE to ADDON_ID.
    Also rename VERSION to ADDON_VERSION.

--- src/server/common/forum_user_handler.hpp
+++ src/server/common/forum_user_handler.hpp
@@ -178,11 +178,11 @@ public:
 	 * @param type The add-on content's type (ie: era, scenario, etc).
 	 * @param name The name of the content.
 	 * @param id The id of the content.
-	 * @param source The source add-on for the content.
-	 * @param version The version of the source add-on.
+	 * @param addon_id The id of the addon that the content is from.
+	 * @param addon_version The version of the add-on.
 	 * @return The number of rows inserted which should always be 1.
 	 */
-	unsigned long long db_insert_game_content_info(const std::string& uuid, int game_id, const std::string& type, const std::string& name, const std::string& id, const std::string& source, const std::string& version);
+	unsigned long long db_insert_game_content_info(const std::string& uuid, int game_id, const std::string& type, const std::string& name, const std::string& id, const std::string& addon_id, const std::string& addon_version);
 
 	/**
 	 * Sets the OOS flag in the database if wesnothd is told by a client it has detected an OOS error.
