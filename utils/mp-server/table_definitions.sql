2c3e674e896a5c9d44e599e4790b91dc36dd797d
Pentarctagon <pentarctagon@tutamail.com>
2023-01-06 11:01:55 -0600

    Skip inserting rows that would cause a duplicate primary key error.
    The cause is that somehow, during regular play, MP campaigns can end up with duplicate values in the [content] data for each [addon]. A very similar issue was fixed in 6c980d1, but there's apparently some other way to trigger it that I haven't been able to reproduce. Since the error itself is harmless, it'll just be avoided server-side to avoid the spurious errors in the server log file.

--- utils/mp-server/table_definitions.sql
+++ utils/mp-server/table_definitions.sql
@@ -108,7 +108,7 @@ create table game_player_info
 CREATE INDEX USER_ID_IDX ON game_player_info(USER_ID);
 
 -- information about the scenario/era/modifications for the game
--- TYPE: one of era/scenario/modification
+-- TYPE: one of era/scenario/modification/campaign
 -- ID: the id of the content
 -- NAME: the content's user-visible name
 -- SOURCE: the id of the add-on that the particular content came from
@@ -117,12 +117,12 @@ create table game_content_info
 (
     INSTANCE_UUID     CHAR(36) NOT NULL,
     GAME_ID           INT UNSIGNED NOT NULL,
-    TYPE              VARCHAR(255) NOT NULL,
-    ID                VARCHAR(255) NOT NULL,
+    TYPE              VARCHAR(100) NOT NULL,
+    ID                VARCHAR(100) NOT NULL,
     NAME              VARCHAR(255),
-    SOURCE            VARCHAR(255) NOT NULL,
+    SOURCE            VARCHAR(100) NOT NULL,
     VERSION           VARCHAR(255) NOT NULL,
-    PRIMARY KEY (INSTANCE_UUID, GAME_ID, TYPE, ID)
+    PRIMARY KEY (INSTANCE_UUID, GAME_ID, TYPE, ID, SOURCE)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- information about an uploaded addon
