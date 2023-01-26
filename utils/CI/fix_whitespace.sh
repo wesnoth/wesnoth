b025be9b7849b5c5aafc7c16e6e58384689c0f23
Steve Cotton <steve@octalot.co.uk>
2023-01-26 10:52:51 +0100

    Make fix_whitespace convert tabs in Lua files
    There was exactly one .lua file that contained any tabs at all,
    so we seem to already have a coding standard.
    
    This will also convert tabs inside strings - I didn't anchor the
    regexp to the start of the line because it should convert all
    tabs used in the indent, not just the first tab on the line.

--- utils/CI/fix_whitespace.sh
+++ utils/CI/fix_whitespace.sh
@@ -7,7 +7,9 @@ make -C data/tools reindent
 if [[ "$OS" == "Linux" ]]; then
   find src/ -name \*.\[ch\]pp -print0 | xargs -0 sed -i 's/[[:blank:]]*$//'
   find data/ -name \*.lua -print0 | xargs -0 sed -i 's/[[:blank:]]*$//'
+  find data/ -name \*.lua -print0 | xargs -0 sed -i 's/\t/    /g'
 else
   find src/ -name \*.\[ch\]pp -print0 | xargs -0 sed -i '' 's/[[:blank:]]*$//'
   find data/ -name \*.lua -print0 | xargs -0 sed -i '' 's/[[:blank:]]*$//'
+  find data/ -name \*.lua -print0 | xargs -0 sed -i '' 's/\t/    /g'
 fi
