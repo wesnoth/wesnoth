a01c50879df849d6547f46fb5ac08c78e4e3d8b6
SyedSaifuddin045 <syedsaifuddin.vlcro@gmail.com>
2023-03-05 12:07:44 +0530

    Removed mutex and fixed formatting

--- src/gettext.cpp
+++ src/gettext.cpp
@@ -1,8 +1,8 @@
 /*
 	Copyright (C) 2003 - 2022
 	by David White <dave@whitevine.net>
-
 	Part of the Battle for Wesnoth Project https://www.wesnoth.org/
+	
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation; either version 2 of the License, or
