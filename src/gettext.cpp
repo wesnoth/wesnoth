47d9a9cf06e38e8068b7fdf161470d46719be5cb
SyedSaifuddin045 <syedsaifuddin.vlcro@gmail.com>
2023-03-05 12:04:29 +0530

    Removed mutex and fixed formatting

--- src/gettext.cpp
+++ src/gettext.cpp
@@ -1,6 +1,7 @@
 /*
 	Copyright (C) 2003 - 2022
 	by David White <dave@whitevine.net>
+
 	Part of the Battle for Wesnoth Project https://www.wesnoth.org/
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -8,6 +9,7 @@
 	(at your option) any later version.
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY.
+
 	See the COPYING file for more details.
 */
 #include "gettext.hpp"
