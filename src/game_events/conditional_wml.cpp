f90a0927e5b1a5eb674df03c227af8001d8ff18a
newfrenchy83 <eric.belleux@gmail.com>
2023-04-02 00:11:26 +0200

    Fix a bug in [event][filter_attack] when trying to use special_id/type_active to detect a special whose activity is conditional.
    When we try to trigger an event conditioned by the use of a special whose activity is subject to a condition, we cannot use special_active because the specials are always considered inactive. Additionally, abilities used as weapons possessed or taught by a leadership are also not detectable in [event][filter_attack]. This PR is there to remedy this double problem.

--- src/game_events/conditional_wml.cpp
+++ src/game_events/conditional_wml.cpp
@@ -212,16 +212,15 @@ bool conditional_passed(const vconfig& cond)
 	return matches;
 }
 
-bool matches_special_filter(optional_const_config cfg, const vconfig& filter)
+bool matches_special_filter(const_attack_ptr attack, const vconfig& filter)
 {
-	if (!cfg) {
+	if (!attack) {
 		WRN_NG << "attempt to filter attack for an event with no attack data.";
 		// better to not execute the event (so the problem is more obvious)
 		return false;
 	}
 	// Though it may seem wasteful to put this on the heap, it's necessary.
 	// matches_filter() could potentially call a WFL formula, which would call shared_from_this().
-	auto attack = std::make_shared<const attack_type>(*cfg);
 	return attack->matches_filter(filter.get_parsed_config());
 }
 
--- src/game_events/conditional_wml.hpp
+++ src/game_events/conditional_wml.hpp
@@ -24,11 +24,12 @@
 class vconfig;
 
 #include "config.hpp"
+#include "units/unit.hpp"
 
 namespace game_events
 {
 	bool conditional_passed(const vconfig& cond);
-	bool matches_special_filter(optional_const_config cfg, const vconfig& filter);
+	bool matches_special_filter(const_attack_ptr attack, const vconfig& filter);
 
 	namespace builtin_conditions {
 		bool have_unit(const vconfig& cfg);
