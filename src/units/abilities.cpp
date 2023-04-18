97e6dc286508bc703efe01126572ffba8b6b67a6
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2023-04-18 15:19:36 +0200

    fix ability used like weapon affect placeholder weapon used in defense when not attack of same range what attacker (#7539)

--- src/units/abilities.cpp
+++ src/units/abilities.cpp
@@ -1535,6 +1535,12 @@ bool attack_type::has_weapon_ability(const std::string& special, bool special_id
 
 bool attack_type::has_special_or_ability(const std::string& special, bool special_id, bool special_tags) const
 {
+	//Now that filter_(second)attack in event supports special_id/type_active, including abilities used as weapons,
+	//these can be detected even in placeholder attacks generated to compensate for the lack of attack in defense against an attacker using a range attack not possessed by the defender.
+	//It is therefore necessary to check if the range is not empty (proof that the weapon is not a placeholder) to decide if has_weapon_ability can be returned or not.
+	if(range().empty()){
+		return false;
+	}
 	return (has_special(special, false, special_id, special_tags) || has_weapon_ability(special, special_id, special_tags));
 }
 //end of emulate weapon special functions.
