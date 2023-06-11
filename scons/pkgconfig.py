be29ec317c3ba34c5de22d4f6795a5e17e7549bf
loonycyborg <loonycyborg@gmail.com>
2023-06-11 13:20:40 +0300

    Don't mess with pkgconfig path in tools other than cross-compile.py

--- scons/pkgconfig.py
+++ scons/pkgconfig.py
@@ -5,7 +5,6 @@ import os, sys
 def run_pkg_config(context, name):
     env = context.env
     try:
-        env["ENV"]["PKG_CONFIG_PATH"] = os.environ.get("PKG_CONFIG_PATH", "")
         env.ParseConfig("pkg-config --libs --cflags $PKG_CONFIG_FLAGS --silence-errors $PKGCONFIG_FLAGS \"" + name + "\"")
         context.Log("Found '" + name + "' with pkg-config.\n")
         return True
