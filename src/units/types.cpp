72945cdc6e697a4776b236be7523eca3f11a48f9
Cody Burchell <cody.burchell@gmail.com>
2023-06-29 18:20:45 -0500

    types: Do not add advancement if it already exists

--- src/units/types.cpp
+++ src/units/types.cpp
@@ -1367,7 +1367,9 @@ void unit_type::apply_scenario_fix(const config& cfg)
 	}
 	if(auto attr = cfg.get("add_advancement")) {
 		for(const auto& str : utils::split(attr->str())) {
-			advances_to_.push_back(str);
+			if(std::none_of(advances_to.begin(), advances_to.end(), compare(str))) {
+				advances_to_.push_back(str);
+			}
 		}
 	}
 	if(auto attr = cfg.get("remove_advancement")) {
