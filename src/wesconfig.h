1bf5b2b1fb337848a4f631981d7dc736cbf27c5d
Pentarctagon <pentarctagon@tutamail.com>
2023-07-15 22:25:39 -0500

    1.17.19+dev

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -40,11 +40,11 @@
   #undef VERSION
 #endif
 
-#define VERSION "1.17.19"
+#define VERSION "1.17.19+dev"
 
 // Used for the Windows executables' version info resource.
 #define RC_VERSION_MAJOR        1
 #define RC_VERSION_MINOR        17
-#define RC_VERSION_REVISION     19
+#define RC_VERSION_REVISION     20
 
 #endif
