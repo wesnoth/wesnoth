182d0ff548b0149a17a6fefcf061074db05c82de
Pentarctagon <pentarctagon@tutamail.com>
2023-08-17 23:16:09 -0500

    Compile fix for boost 1.83.
    Fixes #7849

--- src/gettext.cpp
+++ src/gettext.cpp
@@ -166,7 +166,11 @@ namespace
 			return msg;
 		}
 
+#if BOOST_VERSION < 108300
 		const char* get(int domain_id, const char* ctx, const char* sid, int n) const override
+#else
+		const char* get(int domain_id, const char* ctx, const char* sid, bl::count_type n) const override
+#endif
 		{
 			auto& base = get_base();
 			const char* msg = base.get(domain_id, ctx, sid, n);
