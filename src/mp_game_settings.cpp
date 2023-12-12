ba03cd13723cdf9ce3870e704f23c20693af5a9b
gfgtdf <daniel.gfgtdf@gmail.com>
2023-12-12 21:48:24 +0100

    fix invalid mp addon version info

--- src/mp_game_settings.cpp
+++ src/mp_game_settings.cpp
@@ -164,7 +164,7 @@ void mp_game_settings::addon_version_info::write(config & cfg) const {
 	}
 
 	cfg["name"]	= name;
-	cfg["require"] = required;
+	cfg["required"] = required;
 	for(const auto& item : content) {
 		config& c = cfg.add_child("content");
 		c["id"] = item.id;
