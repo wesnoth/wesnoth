4738455f14b6937c6517f628a3b700242589d413
Rafael Fillipe Silva <rfsc.mori@gmail.com>
2023-12-20 01:11:27 -0300

    add tests for mandatory key validations considering super tags

--- src/tests/wml/schema/test_schema_validator.cpp
+++ src/tests/wml/schema/test_schema_validator.cpp
@@ -153,4 +153,71 @@ BOOST_AUTO_TEST_CASE( test_super_missing_only_if_used )
 	BOOST_CHECK_NO_THROW(read(result, *stream, &validator));
 }
 
+BOOST_AUTO_TEST_CASE( test_super_mandatory )
+{
+	constexpr auto schema_path = "src/tests/wml/schema/test_schema_validator/test_schema_super_mandatory_missing.cfg";
+	constexpr auto config_path = "src/tests/wml/schema/test_schema_validator/test_super_mandatory.cfg";
+
+	auto validator = schema_validation::schema_validator(schema_path, false);
+
+	validator.set_create_exceptions(true);
+
+	preproc_map defines_map;
+	defines_map["WESNOTH_VERSION"] = preproc_define(game_config::wesnoth_version.str());
+	defines_map["SCHEMA_VALIDATION"] = preproc_define();
+
+	auto stream = preprocess_file(config_path, &defines_map);
+
+	config result;
+	BOOST_CHECK_NO_THROW(read(result, *stream, &validator));
+}
+
+BOOST_AUTO_TEST_CASE( test_super_mandatory_missing )
+{
+	constexpr auto schema_path = "src/tests/wml/schema/test_schema_validator/test_schema_super_mandatory_missing.cfg";
+	constexpr auto config_path = "src/tests/wml/schema/test_schema_validator/test_super_mandatory_missing.cfg";
+
+	auto validator = schema_validation::schema_validator(schema_path, false);
+
+	validator.set_create_exceptions(true);
+
+	preproc_map defines_map;
+	defines_map["WESNOTH_VERSION"] = preproc_define(game_config::wesnoth_version.str());
+	defines_map["SCHEMA_VALIDATION"] = preproc_define();
+
+	auto stream = preprocess_file(config_path, &defines_map);
+
+	config result;
+
+	BOOST_CHECK_EXCEPTION(
+		read(result, *stream, &validator),
+		wml_exception,
+		[] (const wml_exception& e) {
+			return boost::algorithm::contains(
+				e.dev_message,
+				"Missing key 'id=' in tag [campaign]"
+			);
+		}
+	);
+}
+
+BOOST_AUTO_TEST_CASE( test_super_cycle_mandatory )
+{
+	constexpr auto schema_path = "src/tests/wml/schema/test_schema_validator/test_schema_super_cycle_mandatory.cfg";
+	constexpr auto config_path = "src/tests/wml/schema/test_schema_validator/test_super_cycle_mandatory.cfg";
+
+	auto validator = schema_validation::schema_validator(schema_path, false);
+
+	validator.set_create_exceptions(true);
+
+	preproc_map defines_map;
+	defines_map["WESNOTH_VERSION"] = preproc_define(game_config::wesnoth_version.str());
+	defines_map["SCHEMA_VALIDATION"] = preproc_define();
+
+	auto stream = preprocess_file(config_path, &defines_map);
+
+	config result;
+	BOOST_CHECK_NO_THROW(read(result, *stream, &validator));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
