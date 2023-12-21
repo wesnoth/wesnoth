70369b7f0bc2c168cedfaf3ab1bc1f0d50545d65
P. J. McDermott <pj@pehjota.net>
2023-12-20 21:15:08 -0500

    Make mouse handler use same coordinate signs as map
    Also fix mouse_wheel_*() virtual method calls, which have been wrong in
    the X axis since commit dfe2f33 (and unused since commit c912f7e).

--- src/controller_base.cpp
+++ src/controller_base.cpp
@@ -232,8 +232,7 @@ void controller_base::handle_event(const SDL_Event& event)
 		break;
 
 	case SDL_MOUSEWHEEL:
-		// Down and right are positive in Wesnoth's map, but
-		// mouse_handler_base::mouse_wheel() inverts both axes.
+		// Down and right are positive in Wesnoth's map.
 		// Up and right are positive in SDL_MouseWheelEvent on all
 		// platforms:
 		//     https://wiki.libsdl.org/SDL2/SDL_MouseWheelEvent
@@ -245,20 +244,20 @@ void controller_base::handle_event(const SDL_Event& event)
 		// Fixes issue #7404, which is a regression caused by pull #2481
 		// that fixed issue #2218.
 		{
-			int xmul = -1;
+			int xmul = 1;
 #if !defined(_WIN32) && !defined(__APPLE__)
 			const char* video_driver = SDL_GetCurrentVideoDriver();
 			SDL_version ver;
 			SDL_GetVersion(&ver);
 			if(video_driver != nullptr && ver.major <= 2 && ver.minor <= 0) {
 				if(std::strcmp(video_driver, "x11") == 0 && ver.patch < 18) {
-					xmul = 1;
+					xmul = -1;
 				} else if(std::strcmp(video_driver, "wayland") == 0 && ver.patch < 20) {
-					xmul = 1;
+					xmul = -1;
 				}
 			}
 #endif
-			mh_base.mouse_wheel(xmul * event.wheel.x, event.wheel.y, is_browsing());
+			mh_base.mouse_wheel(xmul * event.wheel.x, -event.wheel.y, is_browsing());
 		}
 		break;
 
