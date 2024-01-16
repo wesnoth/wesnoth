c6f44b1cf034fde2ba31ec7ea8d885cbc0abbf2a
demario-w <97009180+demario-w@users.noreply.github.com>
2024-01-16 15:34:18 +0100

    Store core ID into save

--- src/game_classification.cpp
+++ src/game_classification.cpp
@@ -17,6 +17,7 @@
 
 #include "config.hpp"
 #include "log.hpp"
+#include "preferences/general.hpp"
 #include "serialization/string_utils.hpp"
 #include "game_version.hpp"
 #include "game_config_manager.hpp"
@@ -77,6 +78,7 @@ config game_classification::to_config() const
 	cfg["difficulty"] = difficulty;
 	cfg["random_mode"] = random_mode;
 	cfg["oos_debug"] = oos_debug;
+	cfg["core"] = preferences::core_id();
 
 	return cfg;
 }
