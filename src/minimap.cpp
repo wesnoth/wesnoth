cc4932ea46f517862933a868a778f2fd17a116d4
Charles Dang <exodia339@gmail.com>
2024-01-29 23:41:39 -0500

    Make Ubuntu happier

--- src/minimap.cpp
+++ src/minimap.cpp
@@ -186,7 +186,7 @@ surface getMinimap(int w, int h, const gamemap &map, const team *vw, const std::
 					// Despite its name, game_config::team_rgb_range isn't just team colors,
 					// it has "red", "lightblue", "cave", "reef", "fungus", etc.
 					color_t col;
-					std::map<std::string, color_range>::const_iterator it = game_config::team_rgb_range.find(terrain_info.id());
+					auto it = game_config::team_rgb_range.find(terrain_info.id());
 					if (it == game_config::team_rgb_range.end()) {
 						col = color_t(0,0,0,0);
 					} else
