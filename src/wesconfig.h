4906f3cc7a04eedbd85a3b2a30c9b5252755442a
pentarctagon <pentarctagon@tutamail.com>
2024-02-19 21:10:15 -0600

    1.17.26+dev

--- src/wesconfig.h
+++ src/wesconfig.h
@@ -40,11 +40,11 @@
   #undef VERSION
 #endif
 
-#define VERSION "1.17.26"
+#define VERSION "1.17.26+dev"
 
 // Used for the Windows executables' version info resource.
 #define RC_VERSION_MAJOR        1
 #define RC_VERSION_MINOR        17
-#define RC_VERSION_REVISION     26
+#define RC_VERSION_REVISION     27
 
 #endif
