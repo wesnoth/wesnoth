c25d87bc9ae2b4962bfe068c9a00293d99685385
Hejnewar <41817546+Hejnewar@users.noreply.github.com>
2024-03-17 00:51:18 +0100

    Improve default settings for the timer
    Action bonus - is often thought as inconsisnent and generally not very well understood not only by new players but aslo by players with mid tier experience, so removing it from initial setting should improve overall experience of a new player.
    Turn bonus - 60 seconds is way too low and initial 270 is also not much for the whole game. 240/240 does better job at keeping the game in good pace while not being too opressive in my opinion, 360 reserve is aslo ok if you can save some time for more intensive turns.

--- src/preferences/game.cpp
+++ src/preferences/game.cpp
@@ -607,7 +607,7 @@ void set_countdown(bool value)
 
 int countdown_init_time()
 {
-	return std::clamp<int>(lexical_cast_default<int>(preferences::get("mp_countdown_init_time"), 270), 0, 1500);
+	return std::clamp<int>(lexical_cast_default<int>(preferences::get("mp_countdown_init_time"), 240), 0, 1500);
 }
 
 void set_countdown_init_time(int value)
@@ -617,7 +617,7 @@ void set_countdown_init_time(int value)
 
 int countdown_reservoir_time()
 {
-	return std::clamp<int>(lexical_cast_default<int>(preferences::get("mp_countdown_reservoir_time"), 330), 30, 1500);
+	return std::clamp<int>(lexical_cast_default<int>(preferences::get("mp_countdown_reservoir_time"), 360), 30, 1500);
 }
 
 void set_countdown_reservoir_time(int value)
@@ -627,7 +627,7 @@ void set_countdown_reservoir_time(int value)
 
 int countdown_turn_bonus()
 {
-	return std::clamp<int>(lexical_cast_default<int>(preferences::get("mp_countdown_turn_bonus"), 60), 0, 300);
+	return std::clamp<int>(lexical_cast_default<int>(preferences::get("mp_countdown_turn_bonus"), 240), 0, 300);
 }
 
 void set_countdown_turn_bonus(int value)
@@ -637,7 +637,7 @@ void set_countdown_turn_bonus(int value)
 
 int countdown_action_bonus()
 {
-	return std::clamp<int>(lexical_cast_default<int>(preferences::get("mp_countdown_action_bonus"), 13), 0, 30);
+	return std::clamp<int>(lexical_cast_default<int>(preferences::get("mp_countdown_action_bonus"), 0), 0, 30);
 }
 
 void set_countdown_action_bonus(int value)
