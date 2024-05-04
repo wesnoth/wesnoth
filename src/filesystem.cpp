ebd9b43fa9699f8acab0e2b85f54c2332e1186d4
pentarctagon <pentarctagon@tutamail.com>
2024-05-04 13:20:02 -0500

    Remove special handling of config directory on linux
    Instead store the preferences, credentials, and lua command history files in the userdata folder, same as is done on windows and macos.
    
    Also removes another instance of _X11 for #8806

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -892,27 +892,7 @@ static const bfs::path& get_user_data_path()
 std::string get_user_config_dir()
 {
 	if(user_config_dir.empty()) {
-#if defined(_X11) && !defined(PREFERENCES_DIR)
-		char const* xdg_config = getenv("XDG_CONFIG_HOME");
-
-		if(!xdg_config || xdg_config[0] == '\0') {
-			xdg_config = getenv("HOME");
-			if(!xdg_config) {
-				user_config_dir = get_user_data_path();
-				return user_config_dir.string();
-			}
-
-			user_config_dir = xdg_config;
-			user_config_dir /= ".config";
-		} else {
-			user_config_dir = xdg_config;
-		}
-
-		user_config_dir /= "wesnoth";
-		set_user_config_path(user_config_dir);
-#else
 		user_config_dir = get_user_data_path();
-#endif
 	}
 
 	return user_config_dir.string();
