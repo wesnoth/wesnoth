8f527e61c5ca038ecbacd9b49cdb2a7b5795c676
Subhraman Sarkar <suvrax@gmail.com>
2024-06-07 16:44:30 +0530

    change > to >=

--- src/network_download_file.cpp
+++ src/network_download_file.cpp
@@ -53,7 +53,7 @@ namespace network
             curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 1L);
             curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
             curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, 5000L);
-            #if LIBCURL_VERSION_NUM > 0x075500
+            #if LIBCURL_VERSION_NUM >= 0x075500
 	            curl_easy_setopt(curl, CURLOPT_PROTOCOLS_STR, CURLPROTO_HTTPS);
 	        #else
 	            curl_easy_setopt(curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
