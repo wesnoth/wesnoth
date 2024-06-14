1c78ab366691f42e060525fcfb69f9210754f268
Subhraman Sarkar <suvrax@gmail.com>
2024-06-14 18:14:36 +0530

    replaces tabs with 4 spaces

--- src/network_download_file.cpp
+++ src/network_download_file.cpp
@@ -1,15 +1,15 @@
 /*
-	Copyright (C) 2003 - 2024
-	Part of the Battle for Wesnoth Project https://www.wesnoth.org/
+    Copyright (C) 2003 - 2024
+    Part of the Battle for Wesnoth Project https://www.wesnoth.org/
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY.
 
-	See the COPYING file for more details.
+    See the COPYING file for more details.
 */
 
 #include "network_download_file.hpp"
@@ -54,10 +54,10 @@ namespace network
             curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
             curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, 5000L);
             #if LIBCURL_VERSION_NUM >= 0x075500
-	            curl_easy_setopt(curl, CURLOPT_PROTOCOLS_STR, CURLPROTO_HTTPS);
-	        #else
-	            curl_easy_setopt(curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
-	        #endif
+                curl_easy_setopt(curl, CURLOPT_PROTOCOLS_STR, CURLPROTO_HTTPS);
+            #else
+                curl_easy_setopt(curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
+            #endif
 
             CURLcode res = curl_easy_perform(curl);
 
