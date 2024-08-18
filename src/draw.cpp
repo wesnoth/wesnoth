wesnoth/wesnoth

commit 283e52222a7c022a5ad529826de1ac3a9eeab512

Restore old clip rect when reverting render target (fixes #7087, fixes #8607)
This specifically manifested with the new minimap rendering code. Since we were creating a new temp texture onto which to draw the minimap before drawing it to the window texture, the formerly set clip rect was incorrect when the old viewport was restored:

For a minimap texture of 738 x 912:
Clip before changing target: `[0,0|330,200]`
Clip after changing target: `[0,0|738,912]` (covers whole of new target)
Clip after old target is restored: `[476,55|330,200]`

It's unclear where the extra x,y values came from, but I suspect it's some issue with SDL's internal viewport handling.

src/draw.cpp
@@ -625,10 +625,10 @@ SDL_Rect draw::get_viewport()
 	return viewport;
 }
 
-
 draw::render_target_setter::render_target_setter(const texture& t)
 	: target_()
 	, viewport_()
+	, clip_()
 {
 	// Validate we can render to this texture.
 	assert(!t || t.get_access() == SDL_TEXTUREACCESS_TARGET);
@@ -640,6 +640,7 @@ draw::render_target_setter::render_target_setter(const texture& t)
 
 	target_ = video::get_render_target();
 	SDL_RenderGetViewport(renderer(), &viewport_);
+	SDL_RenderGetClipRect(renderer(), &clip_);
 
 	if (t) {
 		video::force_render_target(t);
@@ -656,6 +657,7 @@ draw::render_target_setter::~render_target_setter()
 	}
 	video::force_render_target(target_);
 	SDL_RenderSetViewport(renderer(), &viewport_);
+	SDL_RenderSetClipRect(renderer(), &clip_);
 }
 
 draw::render_target_setter draw::set_render_target(const texture& t)

src/draw.hpp
@@ -475,6 +475,7 @@ public:
 private:
 	texture target_;
 	::rect viewport_;
+	::rect clip_;
 };
 
 /**

commit f9e27009912ac8316082d460c4182ac805527554

Fix clip rect regression from 283e52222a7c022a5ad529826de1ac3a9eeab512

src/draw.cpp
@@ -657,6 +657,7 @@ draw::render_target_setter::~render_target_setter()
 	}
 	video::force_render_target(target_);
 	SDL_RenderSetViewport(renderer(), &viewport_);
+	if(clip_ == sdl::empty_rect) return;
 	SDL_RenderSetClipRect(renderer(), &clip_);
 }
 