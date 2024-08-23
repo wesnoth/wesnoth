e1c14ac2a72a16fffbd9966b9be90cd20eeb9398
Nils Kneuper <crazy-ivanovic@gmx.net>
2024-08-23 14:50:50 +0200

    applied "update_copyrights" script

--- src/tests/wml/schema/test_schema_validator.cpp
+++ src/tests/wml/schema/test_schema_validator.cpp
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2003 - 2023
+	Copyright (C) 2003 - 2024
 	Part of the Battle for Wesnoth Project https://www.wesnoth.org/
 
 	This program is free software; you can redistribute it and/or modify
