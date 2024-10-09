79fca79d67da300bd9a40353773e2197bc03acee
Aardvark <93877353+AskAardvark@users.noreply.github.com>
2024-10-08 22:30:04 -0500

    Update custom_bonus.lua

--- data/campaigns/World_Conquest/lua/map/scenario_utils/custom_bonus.lua
+++ data/campaigns/World_Conquest/lua/map/scenario_utils/custom_bonus.lua
@@ -1,10 +1,9 @@
 local on_event = wesnoth.require("on_event")
 local custom_bonus = wesnoth.require('bonus_points') -- world_conquest_tek_bonus_points function
-local custom_bonus = wesnoth.require('./../postgeneration_utils/events') -- oceanic function
+local oceanic_events = wesnoth.require('./../postgeneration_utils/events') -- oceanic function
 local engine = wesnoth.require('./../postgeneration_utils/engine') -- f map function
-local engine = wesnoth.require('./../wct_map_generator.lua') -- f map function
-local engine = wesnoth.require('./../scenarios/WC_II_1p_scenario1.lua') -- start map function
-
+local map_gen = wesnoth.require('./../wct_map_generator.lua') -- f map function
+local map_scen_1 = wesnoth.require('./../scenarios/WC_II_1p_scenario1.lua') -- start map function
 
 on_event("prestart", function(ec)
     print(wml.variables["custom_map_bonus_per_player"])
