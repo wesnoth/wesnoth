0a602e1c7c844ab8f5db6046df5476b4997300ac
Subhraman Sarkar <suvrax@gmail.com>
2024-10-11 20:20:20 +0530

    addon: reorder types and remove comment and remove "UI " from strings

--- src/gui/dialogs/editor/edit_pbl.cpp
+++ src/gui/dialogs/editor/edit_pbl.cpp
@@ -179,7 +179,7 @@ void editor_edit_pbl::pre_show()
 	type_list.emplace_back("label", _("Map Pack"));
 	type_list.emplace_back("label", _("Modification"));
 	type_list.emplace_back("label", _("Media"));
-	type_list.emplace_back("label", _("UI Theme"));
+	type_list.emplace_back("label", _("Theme"));
 	type_list.emplace_back("label", _("Other"));
 	types.set_values(type_list);
 	types.set_selected(selected);
