94b075d720ae79ff2bbcb1c390196c4c7400384b
Subhraman Sarkar <suvrax@gmail.com>
2024-10-11 18:37:15 +0530

    addons: remove commented out constants related to unused addon type

--- src/addon/validation.hpp
+++ src/addon/validation.hpp
@@ -111,7 +111,6 @@ enum ADDON_TYPE {
 	ADDON_MP_FACTION,	/**< Multiplayer faction. */
 	// NOTE: following two still require proper engine support
 	ADDON_MOD,		/**< Modification of the game. */
-	//ADDON_GUI,			// GUI add-ons/themes.
 	ADDON_MEDIA,		/**< Miscellaneous content/media (unit packs, terrain packs, music packs, etc.). */
 	ADDON_OTHER,		/**< an add-on that fits in no other category */
 	ADDON_THEME,		/** GUI2 Themes */
