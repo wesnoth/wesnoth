6dc65f6483a50b78c14482874c84507e14fd115b
Charles Dang <exodia339@gmail.com>
2024-11-04 01:04:19 -0500

    Campaignd: use chrono::system_clock instead of std::time_t
    Includes adjustments to client code to likewise use chrono types. I decided to keep using time_t
    (which is just an alias for long_long) for serializing time points. This avoids compatibility issues,
    since we keep the same resolution (seconds since Unix epoch), and unless we're going to "properly"
    store time points in config, the time_t's unitless value is a good a representation as any.

---
+++ src/serialization/chrono.hpp
@@ -0,0 +1,32 @@
+/*
+	Copyright (C) 2024
+	Part of the Battle for Wesnoth Project https://www.wesnoth.org/
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY.
+
+	See the COPYING file for more details.
+*/
+
+#pragma once
+
+#include "config_attribute_value.hpp"
+
+#include <chrono>
+
+namespace chrono
+{
+inline auto parse_timestamp(const config_attribute_value& val)
+{
+	return std::chrono::system_clock::from_time_t(val.to_long_long());
+}
+
+inline auto serialize_timestamp(const std::chrono::system_clock::time_point& time)
+{
+	return std::chrono::system_clock::to_time_t(time);
+}
+} // namespace chrono
