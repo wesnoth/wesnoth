25b9a4a61fa24a6e4d0f1f44e6cc961b526e12ab
loonycyborg <loonycyborg@gmail.com>
2024-12-05 21:20:22 +0300

    Revert "Add support for android paths and loading datadir with playcore api"
    This reverts commit 313b5824f59df9e1e1dd587a404c7126ed5f84bd.

--- src/wesnoth.cpp
+++ src/wesnoth.cpp
@@ -104,10 +104,6 @@
 
 #ifdef __ANDROID__
 #include <android/log.h>
-#include <jni.h>
-
-#include <play/asset_pack.h>
-
 #endif
 
 #ifdef DEBUG_WINDOW_LAYOUT_GRAPHS
@@ -975,7 +971,6 @@ static int do_gameloop(commandline_options& cmdline_opts)
  * Try to autodetect the location of the game data dir. Note that
  * the root of the source tree currently doubles as the data dir.
  */
-#ifndef __ANDROID__
 static std::string autodetect_game_data_dir(std::string exe_dir)
 {
 	std::string auto_dir;
@@ -1006,7 +1001,6 @@ static std::string autodetect_game_data_dir(std::string exe_dir)
 
 	return auto_dir;
 }
-#endif
 
 #ifdef _WIN32
 #define error_exit(res)                                                                                                \
@@ -1031,53 +1025,6 @@ int main(int argc, char** argv)
 	events::set_main_thread();
 #ifdef __ANDROID__
 	__android_log_write(ANDROID_LOG_INFO, "wesnoth", "Wesnoth started");
-
-	JNIEnv* env = reinterpret_cast<JNIEnv*>(SDL_AndroidGetJNIEnv());
-	JavaVM* jvm;
-	env->GetJavaVM(&jvm);
-
-	jobject activity = reinterpret_cast<jobject>(SDL_AndroidGetActivity());
-
-	AssetPackManager_init(jvm, activity);
-
-	const char* asset_pack_name = "wesnoth_data";
-	auto result = AssetPackManager_requestInfo(&asset_pack_name, 1);
-	assert(result == ASSET_PACK_NO_ERROR);
-
-	AssetPackDownloadState* state;
-	AssetPackDownloadStatus status;
-	do {
-		auto error_code = AssetPackManager_getDownloadState(asset_pack_name, &state);
-		assert(error_code == ASSET_PACK_NO_ERROR);
-		status = AssetPackDownloadState_getStatus(state);
-		__android_log_print(ANDROID_LOG_INFO, "wesnoth", "Download status: %d\n", status);
-		AssetPackDownloadState_destroy(state);
-		SDL_Delay(16);
-	} while(status == ASSET_PACK_INFO_PENDING);
-
-	result = AssetPackManager_requestDownload(&asset_pack_name, 1);
-	assert(result == ASSET_PACK_NO_ERROR);
-
-	do {
-		auto error_code = AssetPackManager_getDownloadState(asset_pack_name, &state);
-		assert(error_code == ASSET_PACK_NO_ERROR);
-		status = AssetPackDownloadState_getStatus(state);
-		__android_log_print(ANDROID_LOG_INFO, "wesnoth", "Download status: %d\n", status);
-		AssetPackDownloadState_destroy(state);
-		SDL_Delay(16);
-	} while(status < 4);
-	assert(status == ASSET_PACK_DOWNLOAD_COMPLETED);
-
-	AssetPackLocation* location;
-
-	result = AssetPackManager_getAssetPackLocation(asset_pack_name, &location);
-	assert(result == ASSET_PACK_NO_ERROR);
-
-	std::string android_asset_path;
-	android_asset_path = AssetPackLocation_getAssetsPath(location);
-	__android_log_print(ANDROID_LOG_INFO, "wesnoth", "Download path: %s\n", android_asset_path.c_str());
-	AssetPackLocation_destroy(location);
-
 #endif
 	auto args = read_argv(argc, argv);
 	assert(!args.empty());
@@ -1096,9 +1043,6 @@ int main(int argc, char** argv)
 		commandline_options cmdline_opts = commandline_options(args);
 		int finished = process_command_args(cmdline_opts);
 
-#ifdef __ANDROID__
-		game_config::path = std::move(android_asset_path);
-#else
 		if(std::string exe_dir = filesystem::get_exe_dir(); !exe_dir.empty()) {
 			if(std::string auto_dir = autodetect_game_data_dir(std::move(exe_dir)); !auto_dir.empty()) {
 				if(!nobanner) {
@@ -1121,7 +1065,6 @@ int main(int argc, char** argv)
 #endif
 			safe_exit(finished);
 		}
-#endif
 
 		SDL_SetHint(SDL_HINT_NO_SIGNAL_HANDLERS, "1");
 		// Is there a reason not to just use SDL_INIT_EVERYTHING?
