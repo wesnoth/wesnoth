db8873e2aa16027faa90ebace9c64f41d592dc62
Subhraman Sarkar <suvrax@gmail.com>
2025-01-03 10:08:34 +0530

    mp method selection: improve accessibility
    tab ordering has been added to the username textbox and the list of methods.
    this serves two purposes:
    1. the listbox is initially selected, so the user can select entries using keyboard.
    2. navigation between the listbox and the username textbox can be done using TAB.
    
    additionally,
    3. the message and control layout has been tweaked to hopefully make it a bit user friendly.
    4. the last selected method is remembered via a preference key.
    5. a [X] button has been added on top-right so that the window can be
       closed via mouse. (previously, it was only possible with the keyboard
       Esc option or by clicking on one of the methods.)

--- src/gui/dialogs/multiplayer/mp_method_selection.hpp
+++ src/gui/dialogs/multiplayer/mp_method_selection.hpp
@@ -27,27 +27,13 @@ public:
 	enum class choice { JOIN = 0, CONNECT, HOST, LOCAL };
 
 	mp_method_selection()
-		: modal_dialog(window_id()) , user_name_(), choice_()
+		: modal_dialog(window_id())
 	{
 	}
 
-	const std::string& user_name() const
-	{
-		return user_name_;
-	}
-
-	choice get_choice() const
-	{
-		return choice_;
-	}
+	choice get_choice() const;
 
 private:
-	/** The name to use on the MP server. */
-	std::string user_name_;
-
-	/** The selected method to `connect' to the MP server. */
-	choice choice_;
-
 	virtual const std::string& window_id() const override;
 
 	virtual void pre_show() override;
