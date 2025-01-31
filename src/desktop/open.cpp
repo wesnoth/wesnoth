8149f015f71de6ad96155506b50da1fa7a966524
Subhraman Sarkar <suvrax@gmail.com>
2025-01-31 20:12:10 +0530

    implement desktop::open and desktop::open_object_is_supported() for Android

--- src/desktop/open.cpp
+++ src/desktop/open.cpp
@@ -35,6 +35,10 @@
 #include <windows.h>
 #include <shellapi.h> // ShellExecute()
 
+#elif defined(__ANDROID__)
+#include <SDL2/SDL.h> // For SDL_AndroidGetJNIEnv()
+#include <jni.h>
+
 #endif
 
 static lg::log_domain log_desktop("desktop");
@@ -86,6 +90,24 @@ bool open_object([[maybe_unused]] const std::string& path_or_url)
 
 	return true;
 
+#elif defined(__ANDROID__)
+	LOG_DU << "open_object(): on Android, will use Intent.GET_ACTION_VIEW with path as extra data. URL: " << path_or_url;
+	// call the helper method WesnothActivity.open(String url) using JNI
+	JNIEnv* env = reinterpret_cast<JNIEnv*>(SDL_AndroidGetJNIEnv());
+	jobject wesnoth_instance = reinterpret_cast<jobject>(SDL_AndroidGetActivity());
+	jclass wesnoth_activity(env->GetObjectClass(wesnoth_instance));
+	jmethodID open = env->GetMethodID(wesnoth_activity, "open", "(Ljava/lang/String;)V");
+	env->CallVoidMethod(wesnoth_instance, open, env->NewStringUTF(path_or_url.c_str()));
+
+	if(env->ExceptionCheck() == JNI_TRUE) {
+		env->ExceptionDescribe();
+		env->ExceptionClear();
+	}
+
+	env->DeleteLocalRef(wesnoth_instance);
+	env->DeleteLocalRef(wesnoth_activity);
+
+	return true;
 #else
 
 	ERR_DU << "open_object(): unsupported platform";
