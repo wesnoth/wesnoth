50485625d96070d604baff45c367b968a1c23f54
Subhraman Sarkar <suvrax@gmail.com>
2025-01-31 20:12:10 +0530

    implement desktop::open and desktop::open_object_is_supported() for Android

--- packaging/android/app/src/main/java/org/wesnoth/Wesnoth/WesnothActivity.java
+++ packaging/android/app/src/main/java/org/wesnoth/Wesnoth/WesnothActivity.java
@@ -1,7 +1,19 @@
 package org.wesnoth.Wesnoth;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
 import org.libsdl.app.SDLActivity;
 
 public class WesnothActivity extends SDLActivity
 {
+
+// Needs to be inside an activity so we can use `startActivity`.
+public void open(String url) {
+	Log.d("WesnothActivity", "opening " + url);
+	Intent openIntent = new Intent(Intent.ACTION_VIEW);
+	openIntent.setData(Uri.parse(url));
+	startActivity(openIntent);
+}
 }
