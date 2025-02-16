d542fa2731eca3bbe10686c79bab6790158aaed5
loonycyborg <loonycyborg@gmail.com>
2025-02-16 06:28:58 +0300

    Fix build with newest sdk/ndk from android studio making riscv64 work too

--- packaging/android/build-android-dependencies/build-android-deps.sh
+++ packaging/android/build-android-dependencies/build-android-deps.sh
@@ -21,8 +21,8 @@ https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.6.3.tar.gz
 https://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.xz
 https://ftp.osuosl.org/pub/xiph/releases/vorbis/libvorbis-1.3.7.tar.xz
 https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.3/SDL2_mixer-2.6.3.tar.gz
-https://archives.boost.io/release/1.81.0/source/boost_1_81_0.tar.bz2
-https://www.openssl.org/source/openssl-3.1.0.tar.gz
+https://archives.boost.io/release/1.87.0/source/boost_1_87_0.tar.bz2
+https://www.openssl.org/source/openssl-3.4.1.tar.gz
 https://curl.se/download/curl-8.1.1.tar.xz
 )
 PACKAGES=()
@@ -148,6 +148,10 @@ do
 			then
 				BCABI="aapcs"
 				BOOSTARCH="arm"
+			elif [[ $ANDROID_ARCH == *"risc"* ]]
+			then
+				BCABI="sysv"
+				BOOSTARCH="riscv"
 			else
 				BCABI="sysv"
 				BOOSTARCH="x86"
