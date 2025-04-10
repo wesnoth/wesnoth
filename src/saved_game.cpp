6f230846ee3eb58902fad3d58c4e4a046ec2ab9a
newfrenchy83 <eric.belleux@gmail.com>
2025-04-10 14:28:49 +0200

    ensure that in the event of several liminal bonuses, the first one loaded takes priority (scenario, campaign then resource)
    So if someone wants to set a liminal_bonus for a specific scenario that is different from the rest of the campaign, the scenario value will be taken into account. Similarly, campaign will take priority over resource.

--- src/saved_game.cpp
+++ src/saved_game.cpp
@@ -372,6 +372,11 @@ void saved_game::load_non_scenario(const std::string& type, const std::string& i
 
 		mp_settings_.update_addon_requirements(non_scenario);
 
+		//copy liminal_bonus value
+		if(cfg->has_attribute("liminal_bonus") && !starting_point_.has_attribute("liminal_bonus")) {
+			starting_point_["liminal_bonus"] = cfg["liminal_bonus"].to_int();
+		}
+
 		// Copy events
 		for(const config& modevent : cfg->child_range("event")) {
 			if(modevent["enable_if"].empty()
@@ -394,11 +399,6 @@ void saved_game::load_non_scenario(const std::string& type, const std::string& i
 		// Copy load_resource
 		for(const config& load_resource : cfg->child_range("load_resource")) {
 			starting_point_.add_child_at_total("load_resource", load_resource, pos++);
-		}
-
-		//copy liminal_bonus value
-		if(cfg->has_attribute("liminal_bonus")) {
-			starting_point_["liminal_bonus"] = cfg["liminal_bonus"].to_int();
 		}
 	} else {
 		// TODO: A user message instead?
