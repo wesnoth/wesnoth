1a2c58a3ae7eeee54095df4a9d7f41f7defe3bad
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2025-05-28 23:10:41 +0200

    fix id=poison can poison opponent same if in another type of special (#10226)
    if for example, someone has fun giving a special [chance_to_hit] an id=poison, then the target will be poisoned once hit, this PR solves this problem
    
    Fixes #10225

--- src/units/attack_type.hpp
+++ src/units/attack_type.hpp
@@ -81,7 +81,7 @@ public:
 	 * @param special_id If true, match @a special against the @c id of special tags.
 	 * @param special_tags If true, match @a special against the tag name of special tags.
 	 */
-	bool has_special(const std::string& special, bool simple_check=false, bool special_id=true, bool special_tags=true) const;
+	bool has_special(const std::string& special, bool simple_check = false, bool special_id = false, bool special_tags = true) const;
 	unit_ability_list get_specials(const std::string& special) const;
 	std::vector<std::pair<t_string, t_string>> special_tooltips(boost::dynamic_bitset<>* active_list = nullptr) const;
 	std::string weapon_specials() const;
@@ -124,14 +124,14 @@ public:
 	 * @param special_id If true, match @a special against the @c id of special tags.
 	 * @param special_tags If true, match @a special against the tag name of special tags.
 	 */
-	bool has_weapon_ability(const std::string& special, bool special_id=true, bool special_tags=true) const;
+	bool has_weapon_ability(const std::string& special, bool special_id = false, bool special_tags = true) const;
 	/** used for abilities used like weapon and true specials
 	 * @return True if the ability @a special is active.
 	 * @param special The special being checked.
 	 * @param special_id If true, match @a special against the @c id of special tags.
 	 * @param special_tags If true, match @a special against the tag name of special tags.
 	 */
-	bool has_special_or_ability(const std::string& special, bool special_id=true, bool special_tags=true) const;
+	bool has_special_or_ability(const std::string& special, bool special_id = false, bool special_tags = true) const;
 	/**
 	 * Returns true if this is a dummy attack_type, for example the placeholder that the unit_attack dialog
 	 * uses when a defender has no weapon for a given range.
