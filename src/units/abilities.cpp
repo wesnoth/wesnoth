0e3985d86d7c3d9f34ad6b69718a691d9c28ecea
Charles Dang <exodia339@gmail.com>
2025-06-02 01:55:40 -0400

    plague only

--- src/units/abilities.cpp
+++ src/units/abilities.cpp
@@ -990,7 +990,9 @@ std::vector<std::pair<t_string, t_string>> attack_type::special_tooltips(
 				continue;
 			}
 
-			auto iter = unit_types.types().find(cfg["type"].str());
+			const auto iter = key == "plague" // Only [plague] supports a type key
+				? unit_types.types().find(cfg["type"].str());
+				: unit_types.types().end();
 
 			// TODO: warn if an invalid type is specified?
 			if(iter != unit_types.types().end()) {
