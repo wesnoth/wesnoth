8ddaa290cedcf9edd1e18b7843a1375f3a16d479
Dalas121 <33790750+Dalas121@users.noreply.github.com>
2025-06-04 13:26:39 -0500

    TSG: replace {SAY_COMPANION} macro with lua [companion_message] tag (#10252)
    Resolves #10227. Replaces #10237

---
+++ data/campaigns/The_South_Guard/lua/companion_message.lua
@@ -0,0 +1,24 @@
+-- function for saying a particular message, depending on which companion the player has chosen
+function wesnoth.wml_actions.companion_message(cfg)
+	-- cfg.message_mari
+	-- cfg.message_gerrick
+	-- cfg.message_hylas
+	-- cfg.message_generic
+	-- cfg.speaker
+
+	if wml.variables["companion_id"]=="Mari" and cfg.message_mari and wesnoth.units.find_on_map{id="Mari"}[1] then
+		local speaker = cfg.speaker or "Mari"
+		wesnoth.wml_actions.message{ speaker=speaker, message=cfg.message_mari }
+
+	elseif wml.variables["companion_id"]=="Sir Gerrick" and cfg.message_gerrick and wesnoth.units.find_on_map{id="Sir Gerrick"}[1] then
+		local speaker = cfg.speaker or "Sir Gerrick"
+		wesnoth.wml_actions.message{ speaker=speaker, message=cfg.message_gerrick }
+
+	elseif wml.variables["companion_id"]=="Minister Hylas" and cfg.message_hylas and wesnoth.units.find_on_map{id="Minister Hylas"}[1] then
+		local speaker = cfg.speaker or "Minister Hylas"
+		wesnoth.wml_actions.message{ speaker=speaker, message=cfg.message_hylas }
+
+	elseif cfg.message_generic then
+		wesnoth.wml_actions.message{ speaker="Mari,Sir Gerrick,Minister Hylas", message=cfg.message_generic }
+	end
+end
