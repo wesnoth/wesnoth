0b7d8cb24f629a1f867f43b09ebeefb33b549e33
loonycyborg <loonycyborg@gmail.com>
2025-06-14 15:26:18 +0300

    Bump boost to 1.88 in android deps to make boost.process build

--- packaging/android/build-android-dependencies/build-android-deps.sh
+++ packaging/android/build-android-dependencies/build-android-deps.sh
@@ -21,7 +21,7 @@ https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.6.3.tar.gz
 https://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.xz
 https://ftp.osuosl.org/pub/xiph/releases/vorbis/libvorbis-1.3.7.tar.xz
 https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.3/SDL2_mixer-2.6.3.tar.gz
-https://archives.boost.io/release/1.87.0/source/boost_1_87_0.tar.bz2
+https://archives.boost.io/release/1.88.0/source/boost_1_88_0.tar.bz2
 https://www.openssl.org/source/openssl-3.4.1.tar.gz
 https://curl.se/download/curl-8.1.1.tar.xz
 )
