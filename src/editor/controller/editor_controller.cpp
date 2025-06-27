wesnoth/wesnoth

commit 87db953829f657e740c9c2a20be201f2a4a3a656

Editor: simplify menu expansion

src/editor/controller/editor_controller.cpp
@@ -1172,97 +1172,92 @@ void editor_controller::show_menu(const std::vector<config>& items_arg, int xloc
 		}
 	}
 
-	std::vector<config> items;
+	std::vector<config> filtered;
 	for(const auto& c : items_arg) {
 		const std::string& id = c["id"];
-
-		const hotkey::ui_command cmd = hotkey::ui_command(hotkey::get_hotkey_command(id));
+		const auto cmd = hotkey::ui_command(id);
 
 		if((can_execute_command(cmd) && (!context_menu || in_context_menu(cmd)))
 			|| cmd.hotkey_command == hotkey::HOTKEY_NULL)
 		{
-			items.emplace_back("id", id);
+			filtered.emplace_back("id", id);
 		}
 	}
 
 	// No point in showing an empty menu.
-	if(items.empty()) {
+	if(filtered.empty()) {
 		return;
 	}
 
 	// Based on the ID of the first entry, we fill the menu contextually.
-	const std::string& first_id = items.front()["id"];
+	const std::string& first_id = filtered.front()["id"];
+	std::vector<config> items;
 
 	if(first_id == "EDITOR-LOAD-MRU-PLACEHOLDER") {
 		active_menu_ = editor::LOAD_MRU;
-		context_manager_->expand_load_mru_menu(items, 0);
+		context_manager_->expand_load_mru_menu(items);
 	}
 
-	if(first_id == "editor-switch-map") {
+	else if(first_id == "editor-switch-map") {
 		active_menu_ = editor::MAP;
-		context_manager_->expand_open_maps_menu(items, 0);
+		context_manager_->expand_open_maps_menu(items);
 	}
 
-	if(first_id == "editor-palette-groups") {
+	else if(first_id == "editor-palette-groups") {
 		active_menu_ = editor::PALETTE;
-		toolkit_->get_palette_manager()->active_palette().expand_palette_groups_menu(items, 0);
+		toolkit_->get_palette_manager()->active_palette().expand_palette_groups_menu(items);
 	}
 
-	if(first_id == "editor-switch-side") {
+	else if(first_id == "editor-switch-side") {
 		active_menu_ = editor::SIDE;
-		context_manager_->expand_sides_menu(items, 0);
+		context_manager_->expand_sides_menu(items);
 	}
 
-	if(first_id == "editor-switch-area") {
+	else if(first_id == "editor-switch-area") {
 		active_menu_ = editor::AREA;
-		context_manager_->expand_areas_menu(items, 0);
+		context_manager_->expand_areas_menu(items);
 	}
 
-	if(first_id == "editor-pbl") {
+	else if(first_id == "editor-pbl") {
 		active_menu_ = editor::ADDON;
 	}
 
-	if(!items.empty() && items.front()["id"] == "editor-switch-time") {
+	else if(first_id == "editor-switch-time") {
 		active_menu_ = editor::TIME;
-		context_manager_->expand_time_menu(items, 0);
+		context_manager_->expand_time_menu(items);
 	}
 
-	if(first_id == "editor-assign-local-time") {
-		active_menu_ = editor::LOCAL_TIME;
-		context_manager_->expand_local_time_menu(items, 0);
-	}
-
-	if(first_id == "menu-unit-facings") {
+	else if(first_id == "menu-unit-facings") {
 		active_menu_ = editor::UNIT_FACING;
-		auto pos = items.erase(items.begin());
-		int dir = 0;
-		std::generate_n(std::inserter<std::vector<config>>(items, pos), static_cast<int>(map_location::direction::indeterminate), [&dir]() -> config {
-			return config {"label", map_location::write_translated_direction(map_location::direction(dir++))};
+		auto count = static_cast<int>(map_location::direction::indeterminate);
+		std::generate_n(std::back_inserter(items), count, [dir = 0]() mutable {
+			return config{"label", map_location::write_translated_direction(map_location::direction(dir++))};
 		});
 	}
 
-	if(first_id == "editor-playlist") {
+	else if(first_id == "editor-playlist") {
 		active_menu_ = editor::MUSIC;
-		auto pos = items.erase(items.begin());
-		std::transform(music_tracks_.begin(), music_tracks_.end(), std::inserter<std::vector<config>>(items, pos), [](const sound::music_track& track) -> config {
-			return config {"label", track.title().empty() ? track.id() : track.title()};
-		});
+		std::transform(music_tracks_.begin(), music_tracks_.end(), std::back_inserter(items),
+			[](const sound::music_track& track) {
+				return config{"label", track.title().empty() ? track.id() : track.title()};
+			});
 	}
 
-	if(first_id == "editor-assign-schedule") {
+	else if(first_id == "editor-assign-schedule") {
 		active_menu_ = editor::SCHEDULE;
-		auto pos = items.erase(items.begin());
-		std::transform(tods_.begin(), tods_.end(), std::inserter<std::vector<config>>(items, pos), [](const tods_map::value_type& tod) -> config {
-			return config {"label", tod.second.first};
-		});
+		std::transform(tods_.begin(), tods_.end(), std::back_inserter(items),
+			[](const tods_map::value_type& tod) { return config{"label", tod.second.first}; });
 	}
 
-	if(first_id == "editor-assign-local-schedule") {
+	else if(first_id == "editor-assign-local-schedule") {
 		active_menu_ = editor::LOCAL_SCHEDULE;
-		auto pos = items.erase(items.begin());
-		std::transform(tods_.begin(), tods_.end(), std::inserter<std::vector<config>>(items, pos), [](const tods_map::value_type& tod) -> config {
-			return config {"label", tod.second.first};
-		});
+		std::transform(tods_.begin(), tods_.end(), std::back_inserter(items),
+			[](const tods_map::value_type& tod) { return config{"label", tod.second.first}; });
+	}
+
+	// Splice the lists
+	if(filtered.size() > 1) {
+		std::move(filtered.begin() + 1, filtered.end(), std::back_inserter(items));
 	}
 
 	command_executor::show_menu(items, xloc, yloc, context_menu);

src/editor/map/context_manager.cpp
@@ -329,11 +329,8 @@ void context_manager::new_scenario_dialog()
 	}
 }
 
-void context_manager::expand_open_maps_menu(std::vector<config>& items, int i)
+void context_manager::expand_open_maps_menu(std::vector<config>& items) const
 {
-	auto pos = items.erase(items.begin() + i);
-	std::vector<config> contexts;
-
 	for(std::size_t mci = 0; mci < map_contexts_.size(); ++mci) {
 		map_context& mc = *map_contexts_[mci];
 
@@ -366,47 +363,32 @@ void context_manager::expand_open_maps_menu(std::vector<config>& items, int i)
 		const std::string label = ss.str();
 		const std::string details = get_menu_marker(changed);
 
-		contexts.emplace_back("label", label, "details", details);
+		items.emplace_back("label", label, "details", details);
 	}
-
-	items.insert(pos, contexts.begin(), contexts.end());
 }
 
-void context_manager::expand_load_mru_menu(std::vector<config>& items, int i)
+void context_manager::expand_load_mru_menu(std::vector<config>& items) const
 {
 	std::vector<std::string> mru = prefs::get().recent_files();
-
-	auto pos = items.erase(items.begin() + i);
-
 	if(mru.empty()) {
-		items.insert(pos, config {"label", _("No Recent Files")});
+		items.emplace_back("label", _("No Recent Files"));
 		return;
 	}
 
-	for(std::string& path : mru) {
+	for(const std::string& path : mru) {
 		// TODO: add proper leading ellipsization instead, since otherwise
 		// it'll be impossible to tell apart files with identical names and
 		// different parent paths.
-		path = filesystem::base_name(path);
+		items.emplace_back("label", filesystem::base_name(path));
 	}
-
-	std::vector<config> temp;
-	std::transform(mru.begin(), mru.end(), std::back_inserter(temp), [](const std::string& str) {
-		return config {"label", str};
-	});
-
-	items.insert(pos, temp.begin(), temp.end());
 }
 
-void context_manager::expand_areas_menu(std::vector<config>& items, int i)
+void context_manager::expand_areas_menu(std::vector<config>& items) const
 {
-	tod_manager* tod = get_map_context().get_time_manager();
+	const tod_manager* tod = get_map_context().get_time_manager();
 	if(!tod) {
 		return;
 	}
-
-	auto pos = items.erase(items.begin() + i);
-	std::vector<config> area_entries;
 
 	std::vector<std::string> area_ids = tod->get_area_ids();
 
@@ -429,17 +411,12 @@ void context_manager::expand_areas_menu(std::vector<config>& items, int i)
 		const std::string label = ss.str();
 		const std::string details = get_menu_marker(changed);
 
-		area_entries.emplace_back("label", label, "details", details);
+		items.emplace_back("label", label, "details", details);
 	}
-
-	items.insert(pos, area_entries.begin(), area_entries.end());
 }
 
-void context_manager::expand_sides_menu(std::vector<config>& items, int i)
+void context_manager::expand_sides_menu(std::vector<config>& items) const
 {
-	auto pos = items.erase(items.begin() + i);
-	std::vector<config> contexts;
-
 	for(std::size_t mci = 0; mci < get_map_context().teams().size(); ++mci) {
 
 		const team& t = get_map_context().teams()[mci];
@@ -453,46 +430,29 @@ void context_manager::expand_sides_menu(std::vector<config>& items, int i)
 			label << teamname;
 		}
 
-		contexts.emplace_back("label", label.str());
+		items.emplace_back("label", label.str());
 	}
-
-	items.insert(pos, contexts.begin(), contexts.end());
 }
 
-void context_manager::expand_time_menu(std::vector<config>& items, int i)
+void context_manager::expand_time_menu(std::vector<config>& items) const
 {
-	auto pos = items.erase(items.begin() + i);
-	std::vector<config> times;
-
-	tod_manager* tod_m = get_map_context().get_time_manager();
-
+	const tod_manager* tod_m = get_map_context().get_time_manager();
 	assert(tod_m != nullptr);
 
 	for(const time_of_day& time : tod_m->times()) {
-		times.emplace_back(
-			"details", time.name, // Use 'details' field here since the image will take the first column
-			"image", time.image
-		);
+		// Use 'details' field here since the image will take the first column
+		items.emplace_back("details", time.name, "image", time.image);
 	}
-
-	items.insert(pos, times.begin(), times.end());
 }
 
-void context_manager::expand_local_time_menu(std::vector<config>& items, int i)
+void context_manager::expand_local_time_menu(std::vector<config>& items) const
 {
-	auto pos = items.erase(items.begin() + i);
-	std::vector<config> times;
-
-	tod_manager* tod_m = get_map_context().get_time_manager();
+	const tod_manager* tod_m = get_map_context().get_time_manager();
 
 	for(const time_of_day& time : tod_m->times(get_map_context().get_active_area())) {
-		times.emplace_back(
-			"details", time.name, // Use 'details' field here since the image will take the first column
-			"image", time.image
-		);
+		// Use 'details' field here since the image will take the first column
+		items.emplace_back("details", time.name, "image", time.image);
 	}
-
-	items.insert(pos, times.begin(), times.end());
 }
 
 void context_manager::apply_mask_dialog()

src/editor/map/context_manager.hpp
@@ -103,22 +103,22 @@ public:
 	void rename_area_dialog();
 
 	/** Menu expanding for open maps list */
-	void expand_open_maps_menu(std::vector<config>& items, int i);
+	void expand_open_maps_menu(std::vector<config>& items) const;
 
 	/** Menu expanding for most recent loaded list */
-	void expand_load_mru_menu(std::vector<config>& items, int i);
+	void expand_load_mru_menu(std::vector<config>& items) const;
 
 	/** Menu expanding for the map's player sides */
-	void expand_sides_menu(std::vector<config>& items, int i);
+	void expand_sides_menu(std::vector<config>& items) const;
 
 	/** Menu expanding for the map's defined areas */
-	void expand_areas_menu(std::vector<config>& items, int i);
+	void expand_areas_menu(std::vector<config>& items) const;
 
 	/** Menu expanding for the map's defined areas */
-	void expand_time_menu(std::vector<config>& items, int i);
+	void expand_time_menu(std::vector<config>& items) const;
 
 	/** Menu expanding for the map's defined areas */
-	void expand_local_time_menu(std::vector<config>& items, int i);
+	void expand_local_time_menu(std::vector<config>& items) const;
 
 	/** Display a load map dialog and process user input. */
 	void load_map_dialog(bool force_same_context = false);

src/editor/palette/common_palette.hpp
@@ -73,7 +73,7 @@ public:
 	virtual const std::vector<item_group>& get_groups() const = 0;
 
 	/** Menu expanding for palette group list */
-	virtual void expand_palette_groups_menu(std::vector<config>& items, int i) = 0;
+	virtual void expand_palette_groups_menu(std::vector<config>& items) = 0;
 
 	//item
 	virtual std::size_t num_items() = 0;

src/editor/palette/editor_palettes.cpp
@@ -37,11 +37,8 @@ sdl_handler_vector editor_palette<Item>::handler_members()
 }
 
 template<class Item>
-void editor_palette<Item>::expand_palette_groups_menu(std::vector<config>& items, int i)
+void editor_palette<Item>::expand_palette_groups_menu(std::vector<config>& items)
 {
-	auto pos = items.erase(items.begin() + i);
-
-	std::vector<config> groups;
 	const std::vector<item_group>& item_groups = get_groups();
 
 	for (std::size_t mci = 0; mci < item_groups.size(); ++mci) {
@@ -61,13 +58,8 @@ void editor_palette<Item>::expand_palette_groups_menu(std::vector<config>& items
 			img += ".png";
 		}
 
-		groups.emplace_back(
-			"label", groupname,
-			"icon", img
-		);
+		items.emplace_back("label", groupname, "icon", img);
 	}
-
-	items.insert(pos, groups.begin(), groups.end());
 }
 
 template<class Item>

src/editor/palette/editor_palettes.hpp
@@ -55,7 +55,7 @@ public:
 	std::size_t start_num(void) override { return items_start_; }
 
 	/** Menu expanding for palette group list */
-	void expand_palette_groups_menu(std::vector<config>& items, int i) override;
+	void expand_palette_groups_menu(std::vector<config>& items) override;
 
 	void set_group(std::size_t index) override;
 

src/editor/palette/empty_palette.hpp
@@ -63,9 +63,8 @@ public:
 	virtual const std::vector<item_group>& get_groups() const override { static const std::vector<item_group> empty; return empty; }
 
 	/** Menu expanding for palette group list */
-	virtual void expand_palette_groups_menu(std::vector<config>& items, int i) override
+	virtual void expand_palette_groups_menu(std::vector<config>& /*items*/) override
 	{
-		items.erase(items.begin() + i);
 	}
 
 	//item

src/editor/palette/location_palette.hpp
@@ -43,9 +43,8 @@ public:
 	std::size_t start_num(void) override { return items_start_; }
 
 	/** Menu expanding for palette group list */
-	void expand_palette_groups_menu(std::vector<config>& items, int i) override
+	void expand_palette_groups_menu(std::vector<config>& /*items*/) override
 	{
-		items.erase(items.begin() + i);
 	}
 
 	virtual void set_group(std::size_t /*index*/) override {}

commit 59c7562322e69f00fe1120641e37c8267157c686

Fix logic error in 87db953829f657e740c9c2a20be201f2a4a3a656
Accidentally removed one generator and forgot to account for the case where there's no expansion.

src/editor/controller/editor_controller.cpp
@@ -1172,7 +1172,7 @@ void editor_controller::show_menu(const std::vector<config>& items_arg, int xloc
 		}
 	}
 
-	std::vector<config> filtered;
+	std::vector<config> items;
 	for(const auto& c : items_arg) {
 		const std::string& id = c["id"];
 		const auto cmd = hotkey::ui_command(id);
@@ -1180,42 +1180,44 @@ void editor_controller::show_menu(const std::vector<config>& items_arg, int xloc
 		if((can_execute_command(cmd) && (!context_menu || in_context_menu(cmd)))
 			|| cmd.hotkey_command == hotkey::HOTKEY_NULL)
 		{
-			filtered.emplace_back("id", id);
+			items.emplace_back("id", id);
 		}
 	}
 
 	// No point in showing an empty menu.
-	if(filtered.empty()) {
+	if(items.empty()) {
 		return;
 	}
 
 	// Based on the ID of the first entry, we fill the menu contextually.
-	const std::string& first_id = filtered.front()["id"];
-	std::vector<config> items;
+	const std::string& first_id = items.front()["id"];
+
+	// All generated items (might be empty).
+	std::vector<config> generated;
 
 	if(first_id == "EDITOR-LOAD-MRU-PLACEHOLDER") {
 		active_menu_ = editor::LOAD_MRU;
-		context_manager_->expand_load_mru_menu(items);
+		context_manager_->expand_load_mru_menu(generated);
 	}
 
 	else if(first_id == "editor-switch-map") {
 		active_menu_ = editor::MAP;
-		context_manager_->expand_open_maps_menu(items);
+		context_manager_->expand_open_maps_menu(generated);
 	}
 
 	else if(first_id == "editor-palette-groups") {
 		active_menu_ = editor::PALETTE;
-		toolkit_->get_palette_manager()->active_palette().expand_palette_groups_menu(items);
+		toolkit_->get_palette_manager()->active_palette().expand_palette_groups_menu(generated);
 	}
 
 	else if(first_id == "editor-switch-side") {
 		active_menu_ = editor::SIDE;
-		context_manager_->expand_sides_menu(items);
+		context_manager_->expand_sides_menu(generated);
 	}
 
 	else if(first_id == "editor-switch-area") {
 		active_menu_ = editor::AREA;
-		context_manager_->expand_areas_menu(items);
+		context_manager_->expand_areas_menu(generated);
 	}
 
 	else if(first_id == "editor-pbl") {
@@ -1224,20 +1226,25 @@ void editor_controller::show_menu(const std::vector<config>& items_arg, int xloc
 
 	else if(first_id == "editor-switch-time") {
 		active_menu_ = editor::TIME;
-		context_manager_->expand_time_menu(items);
+		context_manager_->expand_time_menu(generated);
+	}
+
+	else if(first_id == "editor-assign-local-time") {
+		active_menu_ = editor::LOCAL_TIME;
+		context_manager_->expand_local_time_menu(generated);
 	}
 
 	else if(first_id == "menu-unit-facings") {
 		active_menu_ = editor::UNIT_FACING;
 		auto count = static_cast<int>(map_location::direction::indeterminate);
-		std::generate_n(std::back_inserter(items), count, [dir = 0]() mutable {
+		std::generate_n(std::back_inserter(generated), count, [dir = 0]() mutable {
 			return config{"label", map_location::write_translated_direction(map_location::direction(dir++))};
 		});
 	}
 
 	else if(first_id == "editor-playlist") {
 		active_menu_ = editor::MUSIC;
-		std::transform(music_tracks_.begin(), music_tracks_.end(), std::back_inserter(items),
+		std::transform(music_tracks_.begin(), music_tracks_.end(), std::back_inserter(generated),
 			[](const sound::music_track& track) {
 				return config{"label", track.title().empty() ? track.id() : track.title()};
 			});
@@ -1245,22 +1252,28 @@ void editor_controller::show_menu(const std::vector<config>& items_arg, int xloc
 
 	else if(first_id == "editor-assign-schedule") {
 		active_menu_ = editor::SCHEDULE;
-		std::transform(tods_.begin(), tods_.end(), std::back_inserter(items),
+		std::transform(tods_.begin(), tods_.end(), std::back_inserter(generated),
 			[](const tods_map::value_type& tod) { return config{"label", tod.second.first}; });
 	}
 
 	else if(first_id == "editor-assign-local-schedule") {
 		active_menu_ = editor::LOCAL_SCHEDULE;
-		std::transform(tods_.begin(), tods_.end(), std::back_inserter(items),
+		std::transform(tods_.begin(), tods_.end(), std::back_inserter(generated),
 			[](const tods_map::value_type& tod) { return config{"label", tod.second.first}; });
 	}
 
-	// Splice the lists
-	if(filtered.size() > 1) {
-		std::move(filtered.begin() + 1, filtered.end(), std::back_inserter(items));
+	else {
+		// No placeholders, show everything
+		command_executor::show_menu(items, xloc, yloc, context_menu);
+		return;
 	}
 
-	command_executor::show_menu(items, xloc, yloc, context_menu);
+	// Splice the lists, excluding placeholder entry
+	if(items.size() > 1) {
+		std::move(items.begin() + 1, items.end(), std::back_inserter(generated));
+	}
+
+	command_executor::show_menu(generated, xloc, yloc, context_menu);
 }
 
 void editor_controller::preferences()