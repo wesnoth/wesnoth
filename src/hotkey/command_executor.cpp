950881430d368fac9debc3afae2da3d774c3e10c
Charles Dang <exodia339@gmail.com>
2025-06-29 05:07:36 -0400

    Hotkey/Command Executor: reduce excessive switch statement indent
    No functional changes

--- src/hotkey/command_executor.cpp
+++ src/hotkey/command_executor.cpp
@@ -68,21 +68,21 @@ bool command_executor::do_execute_command(const hotkey::ui_command& cmd, bool pr
 	// hotkey release handling
 	if (release) {
 		switch(cmd.hotkey_command) {
-			// release a scroll key, un-apply scrolling in the given direction
-			case HOTKEY_SCROLL_UP:
-				scroll_up(false);
-				break;
-			case HOTKEY_SCROLL_DOWN:
-				scroll_down(false);
-				break;
-			case HOTKEY_SCROLL_LEFT:
-				scroll_left(false);
-				break;
-			case HOTKEY_SCROLL_RIGHT:
-				scroll_right(false);
-				break;
-			default:
-				return false; // nothing else handles a hotkey release
+		// release a scroll key, un-apply scrolling in the given direction
+		case HOTKEY_SCROLL_UP:
+			scroll_up(false);
+			break;
+		case HOTKEY_SCROLL_DOWN:
+			scroll_down(false);
+			break;
+		case HOTKEY_SCROLL_LEFT:
+			scroll_left(false);
+			break;
+		case HOTKEY_SCROLL_RIGHT:
+			scroll_right(false);
+			break;
+		default:
+			return false; // nothing else handles a hotkey release
 		}
 
 		return true;
@@ -90,23 +90,23 @@ bool command_executor::do_execute_command(const hotkey::ui_command& cmd, bool pr
 
 	// handling of hotkeys which activate even on hold events
 	switch(cmd.hotkey_command) {
-		case HOTKEY_REPEAT_RECRUIT:
-			repeat_recruit();
-			return true;
-		case HOTKEY_SCROLL_UP:
-			scroll_up(true);
-			return true;
-		case HOTKEY_SCROLL_DOWN:
-			scroll_down(true);
-			return true;
-		case HOTKEY_SCROLL_LEFT:
-			scroll_left(true);
-			return true;
-		case HOTKEY_SCROLL_RIGHT:
-			scroll_right(true);
-			return true;
-		default:
-			break;
+	case HOTKEY_REPEAT_RECRUIT:
+		repeat_recruit();
+		return true;
+	case HOTKEY_SCROLL_UP:
+		scroll_up(true);
+		return true;
+	case HOTKEY_SCROLL_DOWN:
+		scroll_down(true);
+		return true;
+	case HOTKEY_SCROLL_LEFT:
+		scroll_left(true);
+		return true;
+	case HOTKEY_SCROLL_RIGHT:
+		scroll_right(true);
+		return true;
+	default:
+		break;
 	}
 
 	if(!press) {
@@ -115,285 +115,285 @@ bool command_executor::do_execute_command(const hotkey::ui_command& cmd, bool pr
 
 	// hotkey press handling
 	switch(cmd.hotkey_command) {
-		case HOTKEY_CYCLE_UNITS:
-			cycle_units();
-			break;
-		case HOTKEY_CYCLE_BACK_UNITS:
-			cycle_back_units();
-			break;
-		case HOTKEY_ENDTURN:
-			end_turn();
-			break;
-		case HOTKEY_UNIT_HOLD_POSITION:
-			unit_hold_position();
-			break;
-		case HOTKEY_END_UNIT_TURN:
-			end_unit_turn();
-			break;
-		case HOTKEY_LEADER:
-			goto_leader();
-			break;
-		case HOTKEY_UNDO:
-			undo();
-			break;
-		case HOTKEY_REDO:
-			redo();
-			break;
-		case HOTKEY_TERRAIN_DESCRIPTION:
-			terrain_description();
-			break;
-		case HOTKEY_UNIT_DESCRIPTION:
-			unit_description();
-			break;
-		case HOTKEY_RENAME_UNIT:
-			rename_unit();
-			break;
-		case HOTKEY_SAVE_GAME:
-			save_game();
-			break;
-		case HOTKEY_SAVE_REPLAY:
-			save_replay();
-			break;
-		case HOTKEY_SAVE_MAP:
-			save_map();
-			break;
-		case HOTKEY_LOAD_GAME:
-			load_game();
-			break;
-		case HOTKEY_TOGGLE_ELLIPSES:
-			toggle_ellipses();
-			break;
-		case HOTKEY_TOGGLE_GRID:
-			toggle_grid();
-			break;
-		case HOTKEY_STATUS_TABLE:
-			status_table();
-			break;
-		case HOTKEY_RECALL:
-			recall();
-			break;
-		case HOTKEY_LABEL_SETTINGS:
-			label_settings();
-			break;
-		case HOTKEY_RECRUIT:
-			recruit();
-			break;
-		case HOTKEY_SPEAK:
-			speak();
-			break;
-		case HOTKEY_SPEAK_ALLY:
-			whisper();
-			break;
-		case HOTKEY_SPEAK_ALL:
-			shout();
-			break;
-		case HOTKEY_CREATE_UNIT:
-			create_unit();
-			break;
-		case HOTKEY_CHANGE_SIDE:
-			change_side();
-			break;
-		case HOTKEY_KILL_UNIT:
-			kill_unit();
-			break;
-		case HOTKEY_TELEPORT_UNIT:
-			select_teleport();
-			break;
-		case HOTKEY_PREFERENCES:
-			preferences();
-			break;
-		case HOTKEY_OBJECTIVES:
-			objectives();
-			break;
-		case HOTKEY_UNIT_LIST:
-			unit_list();
-			break;
-		case HOTKEY_STATISTICS:
-			show_statistics();
-			break;
-		case HOTKEY_STOP_NETWORK:
-			stop_network();
-			break;
-		case HOTKEY_START_NETWORK:
-			start_network();
-			break;
-		case HOTKEY_LABEL_TEAM_TERRAIN:
-			label_terrain(true);
-			break;
-		case HOTKEY_LABEL_TERRAIN:
-			label_terrain(false);
-			break;
-		case HOTKEY_CLEAR_LABELS:
-			clear_labels();
-			break;
-		case HOTKEY_SHOW_ENEMY_MOVES:
-			show_enemy_moves(false);
-			break;
-		case HOTKEY_BEST_ENEMY_MOVES:
-			show_enemy_moves(true);
-			break;
-		case HOTKEY_DELAY_SHROUD:
-			toggle_shroud_updates();
-			break;
-		case HOTKEY_UPDATE_SHROUD:
-			update_shroud_now();
-			break;
-		case HOTKEY_CONTINUE_MOVE:
-			continue_move();
-			break;
-		case HOTKEY_SEARCH:
-			search();
-			break;
-		case HOTKEY_HELP:
-			show_help();
-			break;
-		case HOTKEY_HELP_ABOUT_SAVELOAD:
-			// although HOTKEY_HELP uses a virtual call to allow context-specific help, this one is already a specific topic
-			help::show_help("saveload");
-			break;
-		case HOTKEY_CHAT_LOG:
-			show_chat_log();
-			break;
-		case HOTKEY_USER_CMD:
-			user_command();
-			break;
-		case HOTKEY_CUSTOM_CMD:
-			custom_command();
-			break;
-		case HOTKEY_AI_FORMULA:
-			ai_formula();
-			break;
-		case HOTKEY_CLEAR_MSG:
-			clear_messages();
-			break;
-		case HOTKEY_LANGUAGE:
-			change_language();
-			break;
-		case HOTKEY_REPLAY_PLAY:
-			play_replay();
-			break;
-		case HOTKEY_REPLAY_RESET:
-			reset_replay();
-			break;
-		case HOTKEY_REPLAY_STOP:
-			stop_replay();
-			break;
-		case HOTKEY_REPLAY_NEXT_TURN:
-			replay_next_turn();
-			break;
-		case HOTKEY_REPLAY_NEXT_SIDE:
-			replay_next_side();
-			break;
-		case HOTKEY_REPLAY_NEXT_MOVE:
-			replay_next_move();
-			break;
-		case HOTKEY_REPLAY_SHOW_EVERYTHING:
-			replay_show_everything();
-			break;
-		case HOTKEY_REPLAY_SHOW_EACH:
-			replay_show_each();
-			break;
-		case HOTKEY_REPLAY_SHOW_TEAM1:
-			replay_show_team1();
-			break;
-		case HOTKEY_REPLAY_SKIP_ANIMATION:
-			replay_skip_animation();
-			break;
-		case HOTKEY_REPLAY_EXIT:
-			replay_exit();
-			break;
-		case HOTKEY_WB_TOGGLE:
-			whiteboard_toggle();
-			break;
-		case HOTKEY_WB_EXECUTE_ACTION:
-			whiteboard_execute_action();
-			break;
-		case HOTKEY_WB_EXECUTE_ALL_ACTIONS:
-			whiteboard_execute_all_actions();
-			break;
-		case HOTKEY_WB_DELETE_ACTION:
-			whiteboard_delete_action();
-			break;
-		case HOTKEY_WB_BUMP_UP_ACTION:
-			whiteboard_bump_up_action();
-			break;
-		case HOTKEY_WB_BUMP_DOWN_ACTION:
-			whiteboard_bump_down_action();
-			break;
-		case HOTKEY_WB_SUPPOSE_DEAD:
-			whiteboard_suppose_dead();
-			break;
-		case HOTKEY_SELECT_HEX:
-			select_hex();
-			break;
-		case HOTKEY_DESELECT_HEX:
-			deselect_hex();
-			break;
-		case HOTKEY_MOVE_ACTION:
-			move_action();
-			break;
-		case HOTKEY_SELECT_AND_ACTION:
-			select_and_action();
-			break;
-		case HOTKEY_TOUCH_HEX:
-			touch_hex();
-			break;
-		case HOTKEY_ACCELERATED:
-			toggle_accelerated_speed();
-			break;
-		case LUA_CONSOLE:
-			lua_console();
-			break;
-		case HOTKEY_ZOOM_IN:
-			zoom_in();
-			break;
-		case HOTKEY_ZOOM_OUT:
-			zoom_out();
-			break;
-		case HOTKEY_ZOOM_DEFAULT:
-			zoom_default();
-			break;
-		case HOTKEY_MAP_SCREENSHOT:
-			map_screenshot();
-			break;
-		case HOTKEY_QUIT_TO_DESKTOP:
-			quit_confirmation::quit_to_desktop();
-			break;
-		case HOTKEY_QUIT_GAME:
-			gui2::switch_theme(prefs::get().gui2_theme());
-			quit_confirmation::quit_to_title();
-			break;
-		case HOTKEY_SURRENDER:
-			surrender_game();
-			break;
-		case HOTKEY_MINIMAP_DRAW_TERRAIN:
-			prefs::get().set_minimap_draw_terrain(!prefs::get().minimap_draw_terrain());
-			recalculate_minimap();
-			break;
-		case HOTKEY_MINIMAP_CODING_TERRAIN:
-			prefs::get().set_minimap_terrain_coding(!prefs::get().minimap_terrain_coding());
-			recalculate_minimap();
-			break;
-		case HOTKEY_MINIMAP_CODING_UNIT:
-			prefs::get().set_minimap_movement_coding(!prefs::get().minimap_movement_coding());
-			recalculate_minimap();
-			break;
-		case HOTKEY_MINIMAP_DRAW_UNITS:
-			prefs::get().set_minimap_draw_units(!prefs::get().minimap_draw_units());
-			recalculate_minimap();
-			break;
-		case HOTKEY_MINIMAP_DRAW_VILLAGES:
-			prefs::get().set_minimap_draw_villages(!prefs::get().minimap_draw_villages());
-			recalculate_minimap();
-			break;
-		case HOTKEY_ACHIEVEMENTS:
-			{
-				gui2::dialogs::achievements_dialog ach;
-				ach.show();
-			}
-			break;
-		default:
-			return false;
+	case HOTKEY_CYCLE_UNITS:
+		cycle_units();
+		break;
+	case HOTKEY_CYCLE_BACK_UNITS:
+		cycle_back_units();
+		break;
+	case HOTKEY_ENDTURN:
+		end_turn();
+		break;
+	case HOTKEY_UNIT_HOLD_POSITION:
+		unit_hold_position();
+		break;
+	case HOTKEY_END_UNIT_TURN:
+		end_unit_turn();
+		break;
+	case HOTKEY_LEADER:
+		goto_leader();
+		break;
+	case HOTKEY_UNDO:
+		undo();
+		break;
+	case HOTKEY_REDO:
+		redo();
+		break;
+	case HOTKEY_TERRAIN_DESCRIPTION:
+		terrain_description();
+		break;
+	case HOTKEY_UNIT_DESCRIPTION:
+		unit_description();
+		break;
+	case HOTKEY_RENAME_UNIT:
+		rename_unit();
+		break;
+	case HOTKEY_SAVE_GAME:
+		save_game();
+		break;
+	case HOTKEY_SAVE_REPLAY:
+		save_replay();
+		break;
+	case HOTKEY_SAVE_MAP:
+		save_map();
+		break;
+	case HOTKEY_LOAD_GAME:
+		load_game();
+		break;
+	case HOTKEY_TOGGLE_ELLIPSES:
+		toggle_ellipses();
+		break;
+	case HOTKEY_TOGGLE_GRID:
+		toggle_grid();
+		break;
+	case HOTKEY_STATUS_TABLE:
+		status_table();
+		break;
+	case HOTKEY_RECALL:
+		recall();
+		break;
+	case HOTKEY_LABEL_SETTINGS:
+		label_settings();
+		break;
+	case HOTKEY_RECRUIT:
+		recruit();
+		break;
+	case HOTKEY_SPEAK:
+		speak();
+		break;
+	case HOTKEY_SPEAK_ALLY:
+		whisper();
+		break;
+	case HOTKEY_SPEAK_ALL:
+		shout();
+		break;
+	case HOTKEY_CREATE_UNIT:
+		create_unit();
+		break;
+	case HOTKEY_CHANGE_SIDE:
+		change_side();
+		break;
+	case HOTKEY_KILL_UNIT:
+		kill_unit();
+		break;
+	case HOTKEY_TELEPORT_UNIT:
+		select_teleport();
+		break;
+	case HOTKEY_PREFERENCES:
+		preferences();
+		break;
+	case HOTKEY_OBJECTIVES:
+		objectives();
+		break;
+	case HOTKEY_UNIT_LIST:
+		unit_list();
+		break;
+	case HOTKEY_STATISTICS:
+		show_statistics();
+		break;
+	case HOTKEY_STOP_NETWORK:
+		stop_network();
+		break;
+	case HOTKEY_START_NETWORK:
+		start_network();
+		break;
+	case HOTKEY_LABEL_TEAM_TERRAIN:
+		label_terrain(true);
+		break;
+	case HOTKEY_LABEL_TERRAIN:
+		label_terrain(false);
+		break;
+	case HOTKEY_CLEAR_LABELS:
+		clear_labels();
+		break;
+	case HOTKEY_SHOW_ENEMY_MOVES:
+		show_enemy_moves(false);
+		break;
+	case HOTKEY_BEST_ENEMY_MOVES:
+		show_enemy_moves(true);
+		break;
+	case HOTKEY_DELAY_SHROUD:
+		toggle_shroud_updates();
+		break;
+	case HOTKEY_UPDATE_SHROUD:
+		update_shroud_now();
+		break;
+	case HOTKEY_CONTINUE_MOVE:
+		continue_move();
+		break;
+	case HOTKEY_SEARCH:
+		search();
+		break;
+	case HOTKEY_HELP:
+		show_help();
+		break;
+	case HOTKEY_HELP_ABOUT_SAVELOAD:
+		// although HOTKEY_HELP uses a virtual call to allow context-specific help, this one is already a specific topic
+		help::show_help("saveload");
+		break;
+	case HOTKEY_CHAT_LOG:
+		show_chat_log();
+		break;
+	case HOTKEY_USER_CMD:
+		user_command();
+		break;
+	case HOTKEY_CUSTOM_CMD:
+		custom_command();
+		break;
+	case HOTKEY_AI_FORMULA:
+		ai_formula();
+		break;
+	case HOTKEY_CLEAR_MSG:
+		clear_messages();
+		break;
+	case HOTKEY_LANGUAGE:
+		change_language();
+		break;
+	case HOTKEY_REPLAY_PLAY:
+		play_replay();
+		break;
+	case HOTKEY_REPLAY_RESET:
+		reset_replay();
+		break;
+	case HOTKEY_REPLAY_STOP:
+		stop_replay();
+		break;
+	case HOTKEY_REPLAY_NEXT_TURN:
+		replay_next_turn();
+		break;
+	case HOTKEY_REPLAY_NEXT_SIDE:
+		replay_next_side();
+		break;
+	case HOTKEY_REPLAY_NEXT_MOVE:
+		replay_next_move();
+		break;
+	case HOTKEY_REPLAY_SHOW_EVERYTHING:
+		replay_show_everything();
+		break;
+	case HOTKEY_REPLAY_SHOW_EACH:
+		replay_show_each();
+		break;
+	case HOTKEY_REPLAY_SHOW_TEAM1:
+		replay_show_team1();
+		break;
+	case HOTKEY_REPLAY_SKIP_ANIMATION:
+		replay_skip_animation();
+		break;
+	case HOTKEY_REPLAY_EXIT:
+		replay_exit();
+		break;
+	case HOTKEY_WB_TOGGLE:
+		whiteboard_toggle();
+		break;
+	case HOTKEY_WB_EXECUTE_ACTION:
+		whiteboard_execute_action();
+		break;
+	case HOTKEY_WB_EXECUTE_ALL_ACTIONS:
+		whiteboard_execute_all_actions();
+		break;
+	case HOTKEY_WB_DELETE_ACTION:
+		whiteboard_delete_action();
+		break;
+	case HOTKEY_WB_BUMP_UP_ACTION:
+		whiteboard_bump_up_action();
+		break;
+	case HOTKEY_WB_BUMP_DOWN_ACTION:
+		whiteboard_bump_down_action();
+		break;
+	case HOTKEY_WB_SUPPOSE_DEAD:
+		whiteboard_suppose_dead();
+		break;
+	case HOTKEY_SELECT_HEX:
+		select_hex();
+		break;
+	case HOTKEY_DESELECT_HEX:
+		deselect_hex();
+		break;
+	case HOTKEY_MOVE_ACTION:
+		move_action();
+		break;
+	case HOTKEY_SELECT_AND_ACTION:
+		select_and_action();
+		break;
+	case HOTKEY_TOUCH_HEX:
+		touch_hex();
+		break;
+	case HOTKEY_ACCELERATED:
+		toggle_accelerated_speed();
+		break;
+	case LUA_CONSOLE:
+		lua_console();
+		break;
+	case HOTKEY_ZOOM_IN:
+		zoom_in();
+		break;
+	case HOTKEY_ZOOM_OUT:
+		zoom_out();
+		break;
+	case HOTKEY_ZOOM_DEFAULT:
+		zoom_default();
+		break;
+	case HOTKEY_MAP_SCREENSHOT:
+		map_screenshot();
+		break;
+	case HOTKEY_QUIT_TO_DESKTOP:
+		quit_confirmation::quit_to_desktop();
+		break;
+	case HOTKEY_QUIT_GAME:
+		gui2::switch_theme(prefs::get().gui2_theme());
+		quit_confirmation::quit_to_title();
+		break;
+	case HOTKEY_SURRENDER:
+		surrender_game();
+		break;
+	case HOTKEY_MINIMAP_DRAW_TERRAIN:
+		prefs::get().set_minimap_draw_terrain(!prefs::get().minimap_draw_terrain());
+		recalculate_minimap();
+		break;
+	case HOTKEY_MINIMAP_CODING_TERRAIN:
+		prefs::get().set_minimap_terrain_coding(!prefs::get().minimap_terrain_coding());
+		recalculate_minimap();
+		break;
+	case HOTKEY_MINIMAP_CODING_UNIT:
+		prefs::get().set_minimap_movement_coding(!prefs::get().minimap_movement_coding());
+		recalculate_minimap();
+		break;
+	case HOTKEY_MINIMAP_DRAW_UNITS:
+		prefs::get().set_minimap_draw_units(!prefs::get().minimap_draw_units());
+		recalculate_minimap();
+		break;
+	case HOTKEY_MINIMAP_DRAW_VILLAGES:
+		prefs::get().set_minimap_draw_villages(!prefs::get().minimap_draw_villages());
+		recalculate_minimap();
+		break;
+	case HOTKEY_ACHIEVEMENTS:
+		{
+			gui2::dialogs::achievements_dialog ach;
+			ach.show();
+		}
+		break;
+	default:
+		return false;
 	}
 	return true;
 }
