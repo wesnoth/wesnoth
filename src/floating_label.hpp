06776a31752c8528900d122ac45cb8f11a4d82a8
Charles Dang <exodia339@gmail.com>
2025-07-01 09:55:44 -0400

    Floating Label: use native clock duration in most cases
    We don't need to do the duration cast except in get_pos since the movement rate is defined as pixels per millisecond.

--- src/floating_label.hpp
+++ src/floating_label.hpp
@@ -111,7 +111,7 @@ public:
 
 private:
 
-	std::chrono::milliseconds get_time_alive(const clock::time_point& current_time) const;
+	clock::duration get_time_alive(const clock::time_point& current_time) const;
 	int xpos(std::size_t width) const;
 	point get_pos(const clock::time_point& time) const;
 	uint8_t get_alpha(const clock::time_point& time) const;
