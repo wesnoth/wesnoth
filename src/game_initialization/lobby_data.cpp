9ddfbb5c9a2e2c152648ffa05473f20d70a0625e
Pentarctagon <Pentarctagon@users.noreply.github.com>
2025-07-05 10:21:32 -0500

    convert game presets into preferences (#10289)

--- src/game_initialization/lobby_data.cpp
+++ src/game_initialization/lobby_data.cpp
@@ -142,6 +142,7 @@ game_info::game_info(const config& game, const std::vector<std::string>& install
 	, has_friends(false)
 	, has_ignored(false)
 	, auto_hosted(game["auto_hosted"].to_bool())
+	, game_preset(game["game_preset"].to_bool())
 	, display_status(disp_status::NEW)
 	, required_addons()
 	, addons_outcome(addon_req::SATISFIED)
