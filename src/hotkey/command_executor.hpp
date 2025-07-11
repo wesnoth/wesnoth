wesnoth/wesnoth

commit ab372ed37f2e1bfa88f97250367256896668ca27

Hotkey/Command: take args by string_view when possible
Allows ui_command's ctor to be simplified slightly

src/hotkey/command_executor.hpp
@@ -57,15 +57,16 @@ struct ui_command
 		: hotkey_command(hotkey_command)
 		, id(id)
 		, index(index)
-	{ }
+	{
+	}
 	explicit ui_command(const hotkey::hotkey_command& cmd, int index = -1)
 		: ui_command(cmd.command, cmd.id, index)
-	{ }
+	{
+	}
 	// the string @param id references must live longer than this object.
 	explicit ui_command(std::string_view id, int index = -1)
-		: ui_command(hotkey::HOTKEY_NULL, id, index)
+		: ui_command(hotkey::get_hotkey_command(id), index)
 	{
-		hotkey_command = hotkey::get_hotkey_command(std::string(id)).command;
 	}
 };
 

src/editor/toolkit/editor_toolkit.cpp
@@ -86,7 +86,7 @@ void editor_toolkit::init_mouse_actions(context_manager& cmanager)
 
 	for (const theme::menu& menu : gui_.get_theme().menus()) {
 		if (menu.items().size() == 1) {
-			hotkey::HOTKEY_COMMAND hk = hotkey::get_hotkey_command(menu.items().front()["id"]).command;
+			hotkey::HOTKEY_COMMAND hk = hotkey::get_hotkey_command(menu.items().front()["id"].str()).command;
 			mouse_action_map::iterator i = mouse_actions_.find(hk);
 			if (i != mouse_actions_.end()) {
 				i->second->set_toolbar_button(&menu);

src/hotkey/hotkey_command.cpp
@@ -365,7 +365,7 @@ bool is_scope_active(hk_scopes s)
 	return s.any();
 }
 
-const hotkey_command& get_hotkey_command(const std::string& command)
+const hotkey_command& get_hotkey_command(std::string_view command)
 {
 	try {
 		return registered_hotkeys.at(command);
@@ -379,7 +379,7 @@ const std::map<std::string_view, hotkey::hotkey_command>& get_hotkey_commands()
 	return registered_hotkeys;
 }
 
-bool has_hotkey_command(const std::string& id)
+bool has_hotkey_command(std::string_view id)
 {
 	return get_hotkey_command(id).command != hotkey::HOTKEY_NULL;
 }

src/hotkey/hotkey_command.hpp
@@ -314,12 +314,12 @@ private:
 const std::map<std::string_view, hotkey::hotkey_command>& get_hotkey_commands();
 
 /** returns the hotkey_command with the given name */
-NOT_DANGLING const hotkey_command& get_hotkey_command(const std::string& command);
+NOT_DANGLING const hotkey_command& get_hotkey_command(std::string_view command);
 
 bool is_scope_active(scope s);
 bool is_scope_active(hk_scopes s);
 
-bool has_hotkey_command(const std::string& id);
+bool has_hotkey_command(std::string_view id);
 
 /**
  * RAII helper class to control the lifetime of a WML hotkey_command.

src/theme.cpp
@@ -519,7 +519,7 @@ theme::menu::menu(std::size_t sw, std::size_t sh, const config& cfg)
 		items_.emplace_back("id", item);
 	}
 
-	const auto& cmd = hotkey::get_hotkey_command(items_[0]["id"]);
+	const auto& cmd = hotkey::get_hotkey_command(items_[0]["id"].str());
 	if(cfg["auto_tooltip"].to_bool() && tooltip_.empty() && items_.size() == 1) {
 		tooltip_ = cmd.description + hotkey::get_names(items_[0]["id"]) + "\n" + cmd.tooltip;
 	} else if(cfg["tooltip_name_prepend"].to_bool() && items_.size() == 1) {

commit 667bf3b352ce53a802acc2e2ce1c9ee603894798

Fix Load Turn menu items not working
Regression from ab372ed37f2e1bfa88f97250367256896668ca27. Added comment explaining the problem and removed an inaccurate comment.

src/hotkey/command_executor.hpp
@@ -63,10 +63,13 @@ struct ui_command
 		: ui_command(cmd.command, cmd.id, index)
 	{
 	}
-	// the string @param id references must live longer than this object.
 	explicit ui_command(std::string_view id, int index = -1)
-		: ui_command(hotkey::get_hotkey_command(id), index)
+		: ui_command(hotkey::HOTKEY_NULL, id, index)
 	{
+		// Only set the command from the associated hotkey_command, not the ID.
+		// There are cases (specifically, autoload items) with which no command
+		// is associated, yet whose ID should be stored unmodified.
+		hotkey_command = hotkey::get_hotkey_command(id).command;
 	}
 };
 