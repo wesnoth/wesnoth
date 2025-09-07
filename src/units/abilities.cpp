f2e81ff5a9d59f8ae619594a40bab3388f6b170d
newfrenchy83 <eric.belleux@gmail.com>
2025-09-07 14:26:09 +0200

    add a 'base_cth' attribute in [chance_to_hit]
    I know, i already suggested this, but if another weapon special is an error, perhaps what this method will be approved(i expect!)

--- src/units/abilities.cpp
+++ src/units/abilities.cpp
@@ -1379,7 +1379,15 @@ int attack_type::modified_chance_to_hit(int cth, bool special_only) const
 {
 	int parry = other_attack_ ? other_attack_->parry() : 0;
 	unit_ability_list chance_to_hit_list = special_only ? get_specials("chance_to_hit") : get_specials_and_abilities("chance_to_hit");
-	cth = std::clamp(cth + accuracy_ - parry, 0, 100);
+	unit_ability_list base_cth_list;
+	//erase [chance_to_hit] with base_cth attribute, add to base_cth_list.
+	utils::erase_if(chance_to_hit_list, [&](const unit_ability& i) {
+		if((*i.ability_cfg)["base_cth"].to_bool()) {
+			base_cth_list.emplace_back(i);
+		}
+		return (*i.ability_cfg)["base_cth"].to_bool();
+	});
+	cth = std::clamp(composite_value(base_cth_list, cth) + accuracy_ - parry, 0, 100);
 	return composite_value(chance_to_hit_list, cth);
 }
 
