e864a89ea15b4d043672849e7b2aaa453d88a6fb
Dalas121 <33790750+Dalas121@users.noreply.github.com>
2025-09-23 11:26:05 -0500

    Add a couple companion lines for Seimus

--- data/campaigns/Heir_To_The_Throne/lua/companion_message.lua
+++ data/campaigns/Heir_To_The_Throne/lua/companion_message.lua
@@ -11,6 +11,7 @@ function wesnoth.wml_actions.companion_message(cfg)
 	-- cfg.message_Harper
 	-- cfg.message_Ulfdain
 	-- cfg.message_Jeniver
+	-- cfg.message_Seimus
 	-- cfg.message_Dosh
 	-- cfg.fallback_Konrad
 
@@ -20,7 +21,7 @@ function wesnoth.wml_actions.companion_message(cfg)
 	-- use this list to track which companion last spoke, so we can spread lines around evenly
 	-- initialize this list once per campaign, and rotate through the companions each time one speaks
 	if next(wml.array_variables["companion_ids"])==nil then
-		wml.array_variables["companion_ids"] = { {id="Delfador"}, {id="Kalenz"}, {id="Chantal"}, {id="Moremirmu"}, {id="Harper"}, {id="Jeniver"}, {id="Ulfdain"}, {id="Dosh"} }
+		wml.array_variables["companion_ids"] = { {id="Delfador"}, {id="Kalenz"}, {id="Chantal"}, {id="Moremirmu"}, {id="Harper"}, {id="Jeniver"}, {id="Seimus"}, {id="Ulfdain"}, {id="Dosh"} }
 	end
 
 	--###########################
