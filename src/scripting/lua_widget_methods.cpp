wesnoth/wesnoth

commit 9ac7e7dc2122ec517207e3e29ca2e011a47a15d0

Catch wml_exception from gui in lua.show_dialog
This stops a lua error from exiting the scenario.

src/scripting/lua_widget_methods.cpp
@@ -44,6 +44,7 @@
 #include "serialization/string_utils.hpp"
 #include "tstring.hpp"
 #include "utils/scope_exit.hpp"
+#include "wml_exception.hpp"
 #include <functional>
 
 #include <type_traits>
@@ -66,9 +67,18 @@ static lg::log_domain log_scripting_lua("scripting/lua");
 int intf_show_dialog(lua_State* L)
 {
 	config def_cfg = luaW_checkconfig(L, 1);
-	gui2::builder_window::window_resolution def(def_cfg);
 
-	std::unique_ptr<gui2::window> wp(gui2::build(def));
+	std::unique_ptr<gui2::window> wp;
+	try {
+		gui2::builder_window::window_resolution def(def_cfg);
+		wp = std::make_unique<gui2::window>(def);
+	} catch (const wml_exception& error) {
+		error.caught();
+		error.show();
+		// luaL_argerror is effectively a throw statement
+		// It exits the current lua context, but returns control to its caller
+		return luaL_argerror(L, 1, error.dev_message.c_str());
+	}
 
 	if(!lua_isnoneornil(L, 2)) {
 		lua_pushvalue(L, 2);

commit e9d10a3c418bb40bde151ed9e1f5cc036c168ef4

Fix missing call to gui2::build in gui.show_dialog implementation
A mistake introduced by commit 9ac7e7dc2122ec517207e3e29ca2e011a47a15d0
made it so dialogs have no widgets at all. The reason for the mistake
being present in the patch is that it's a backport from master, which
itself has a change in commit a96ce07e35efb644038c6a0e5481ddf535eab092
that removed gui2::build. Since 1.18 is supposed to be a stable branch,
it does not include this change, resulting in a broken backport.

Closes #10654.

src/scripting/lua_widget_methods.cpp
@@ -71,7 +71,7 @@ int intf_show_dialog(lua_State* L)
 	std::unique_ptr<gui2::window> wp;
 	try {
 		gui2::builder_window::window_resolution def(def_cfg);
-		wp = std::make_unique<gui2::window>(def);
+		wp = gui2::build(def);
 	} catch (const wml_exception& error) {
 		error.caught();
 		error.show();