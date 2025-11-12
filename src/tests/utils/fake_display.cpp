wesnoth/wesnoth

commit 0a47d7db73bb3485184006858ca7ca35638c5031

Tests: remove fake display

src/tests/gui/test_gui2.cpp
@@ -209,7 +209,7 @@ namespace {
 	void test_resolutions(const resolution_list& resolutions)
 	{
 		for(const resolution& resolution : resolutions) {
-			test_utils::get_fake_display(resolution.first, resolution.second);
+			test_utils::set_test_resolution(resolution.first, resolution.second);
 
 			dialog_tester<T> ctor;
 			const std::unique_ptr<modal_dialog> dlg(ctor.create());
@@ -249,7 +249,7 @@ namespace {
 		bool interact = false;
 		for(int i = 0; i < 2; ++i) {
 			for(const resolution& resolution : resolutions) {
-				test_utils::get_fake_display(resolution.first, resolution.second);
+				test_utils::set_test_resolution(resolution.first, resolution.second);
 
 				dialog_tester<T> ctor;
 				const std::unique_ptr<modeless_dialog> dlg(ctor.create());
@@ -297,7 +297,7 @@ namespace {
 			, const std::string& id)
 	{
 		for(const auto& resolution : resolutions) {
-			test_utils::get_fake_display(resolution.first, resolution.second);
+			test_utils::set_test_resolution(resolution.first, resolution.second);
 
 			filesystem::write_file(test_gui2_fixture::widgets_file, ","+id, std::ios_base::app);
 
@@ -730,7 +730,7 @@ BOOST_AUTO_TEST_CASE(test_last)
 
 BOOST_AUTO_TEST_CASE(test_make_test_fake)
 {
-	test_utils::get_fake_display(10, 10);
+	test_utils::set_test_resolution(10, 10);
 
 	try {
 		message dlg("title", "message", true, false, false);
@@ -797,7 +797,7 @@ struct dialog_tester<addon_manager>
 {
 	dialog_tester()
 	{
-		test_utils::get_fake_display(10, 10);
+		test_utils::set_test_resolution(10, 10);
 	}
 	addon_manager* create()
 	{

src/tests/utils/fake_display.cpp
@@ -17,74 +17,22 @@
 
 #include "tests/utils/fake_display.hpp"
 
-#include "game_board.hpp"
-#include "game_config_view.hpp"
-#include "game_display.hpp"
-#include "reports.hpp"
 #include "video.hpp"
-
-namespace wb
-{
-class manager;
-}
 
 namespace test_utils
 {
-class fake_display_manager
+void set_test_resolution(const int width, const int height)
 {
-	static fake_display_manager* manager_;
+	static bool video_initialized = false;
 
-	config dummy_cfg_;
-	config dummy_cfg2_;
-	game_board dummy_board_;
-	reports dummy_reports;
-	const events::event_context main_event_context_;
-
-	game_display disp_;
-
-public:
-	static fake_display_manager* get_manager();
-	game_display& get_display();
-
-	fake_display_manager();
-	//		~fake_display_manager();
-};
-
-fake_display_manager* fake_display_manager::manager_ = nullptr;
-
-fake_display_manager* fake_display_manager::get_manager()
-{
-	if(!manager_) {
-		manager_ = new fake_display_manager();
+	if(!video_initialized) {
+		video_initialized = true;
+		video::init(video::fake::no_draw);
 	}
-
-	return manager_;
-}
-
-fake_display_manager::fake_display_manager()
-	: dummy_cfg_()
-	, dummy_cfg2_()
-	, dummy_board_(dummy_cfg2_)
-	, main_event_context_()
-	, disp_(dummy_board_, std::shared_ptr<wb::manager>(), dummy_reports, "", dummy_cfg_)
-{
-	video::init(video::fake::no_draw);
-}
-
-game_display& fake_display_manager::get_display()
-{
-	return disp_;
-}
-
-game_display& get_fake_display(const int width, const int height)
-{
-	game_display& display = fake_display_manager::get_manager()->get_display();
 
 	if(width >= 0 && height >= 0) {
 		video::set_resolution({width, height});
 	}
-
-	return display;
 }
 
 } // namespace test_utils

src/tests/main.cpp
@@ -86,7 +86,7 @@ struct wesnoth_global_fixture {
 
 		// Initialize unit tests
 		SDL_Init(SDL_INIT_TIMER);
-		test_utils::get_fake_display(1024, 768);
+		test_utils::set_test_resolution(1024, 768);
 
 		gui2::init();
 		gui2::switch_theme("default");

src/tests/test_mp_connect.cpp
@@ -52,7 +52,7 @@ struct mp_connect_fixture {
 		hotkey_manager(),
 		config_manager()
 	{
-		test_utils::get_fake_display(1000, 500);
+		test_utils::set_test_resolution(1000, 500);
 		config_manager.reset(new game_config_manager(cmdline_opts));
 		config_manager->init_game_config(game_config_manager::NO_FORCE_RELOAD);
 

src/tests/utils/fake_display.hpp
@@ -15,23 +15,17 @@
 
 #pragma once
 
-class game_display;
-
-namespace test_utils {
-
+namespace test_utils
+{
 	/**
-	 * Gets a fake test display.
+	 * Sets the dummy display resolution for use by the tests.
 	 *
 	 * The width and height parameter are ignored if either of them is less
 	 * than zero.
 	 *
 	 * @param width               The width of the display.
 	 * @param height              The height of the display.
-	 *
-	 * @returns                   The display.
 	 */
-	game_display& get_fake_display(
-			const int width, const int height);
+	void set_test_resolution(const int width, const int height);
 
-
-}
+} // namespace test_utils

commit dfd549f1b6684276e217aa30851db1389c3c4a91

Fixup tests (#10689)
Fixes breakage caused by 0a47d7db73bb3485184006858ca7ca35638c5031 which I accidentally pushed to master.

The custom_tod dialog test has been disabled since it asserts without a display pointer. That can be adjusted, but I'll handle that separately.

boost_test_schedule
@@ -9,7 +9,6 @@ test_gui2/modal_dialog_test_campaign_difficulty
 test_gui2/modal_dialog_test_chat_log
 test_gui2/modal_dialog_test_editor_choose_addon
 test_gui2/modal_dialog_test_core_selection
-test_gui2/modal_dialog_test_custom_tod
 test_gui2/modal_dialog_test_depcheck_confirm_change
 test_gui2/modal_dialog_test_depcheck_select_new
 test_gui2/modal_dialog_test_edit_label

src/tests/gui/test_gui2.cpp
@@ -419,10 +419,6 @@ BOOST_AUTO_TEST_CASE(modal_dialog_test_core_selection)
 {
 	test<core_selection>();
 }
-BOOST_AUTO_TEST_CASE(modal_dialog_test_custom_tod)
-{
-	test<custom_tod>();
-}
 BOOST_AUTO_TEST_CASE(modal_dialog_test_depcheck_confirm_change)
 {
 	test<depcheck_confirm_change>();
@@ -707,6 +703,7 @@ BOOST_AUTO_TEST_CASE(test_last)
 		"game_load",// segfault after disabling the above tests
 		"file_progress",
 		"fps_report", // needs something to report...
+		"custom_tod", // needs to be adapted to handle a null display
 	};
 	filesystem::delete_file(test_gui2_fixture::widgets_file);
 

src/tests/utils/fake_display.cpp
@@ -17,18 +17,31 @@
 
 #include "tests/utils/fake_display.hpp"
 
+#include "events.hpp"
 #include "video.hpp"
 
 namespace test_utils
 {
-void set_test_resolution(const int width, const int height)
+namespace
 {
-	static bool video_initialized = false;
-
-	if(!video_initialized) {
-		video_initialized = true;
+class context_manager
+{
+public:
+	context_manager()
+	{
 		video::init(video::fake::no_draw);
 	}
+
+private:
+	const events::event_context main_event_context_;
+};
+
+} // namespace
+
+void set_test_resolution(const int width, const int height)
+{
+	// Single object for the lifetime of the unit tests.
+	static const context_manager main_event_context;
 
 	if(width >= 0 && height >= 0) {
 		video::set_resolution({width, height});