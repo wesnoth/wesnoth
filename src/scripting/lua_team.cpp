73a6c813f3ce2f98cc5978a6f2e496cfa02a02f0
Charles Dang <exodia339@gmail.com>
2025-11-20 23:38:35 -0500

    Fixup bf32d3a613663d9e3d57958fdb5d2fa7233f5080

--- src/scripting/lua_team.cpp
+++ src/scripting/lua_team.cpp
@@ -160,7 +160,7 @@ SIDE_SETTER("scroll_to_leader", bool) {
 	t.set_scroll_to_leader(value);
 }
 
-static void reinit_flag_for_team(lua_State* L, const team& t) {
+static void reinit_flag_for_team(const team& t) {
 	if(auto disp = game_display::get_singleton()) {
 		disp->reinit_flags_for_team(t);
 	}
@@ -172,7 +172,7 @@ SIDE_GETTER("color", std::string) {
 
 SIDE_SETTER("color", std::string) {
 	t.set_color(value);
-	reinit_flag_for_team(L, t);
+	reinit_flag_for_team(t);
 }
 
 SIDE_GETTER("flag", std::string) {
@@ -181,7 +181,7 @@ SIDE_GETTER("flag", std::string) {
 
 SIDE_SETTER("flag", std::string) {
 	t.set_flag(value);
-	reinit_flag_for_team(L, t);
+	reinit_flag_for_team(t);
 }
 
 SIDE_GETTER("flag_icon", std::string) {
