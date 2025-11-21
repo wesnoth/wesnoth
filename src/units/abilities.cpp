50d2f9658eba1737cabde74702b206d2d9b4ecf1
newfrenchy83 <eric.belleux@gmail.com>
2025-11-21 16:46:20 +0100

    deprecate overwrite_specials

--- src/units/abilities.cpp
+++ src/units/abilities.cpp
@@ -238,6 +238,10 @@ void unit_ability_t::do_compat_fixes(config& cfg, const std::string& tag, bool i
 		cfg.remove_children("filter_weapon");
 	}
 
+	if (!cfg["overwrite_specials"].blank() || cfg.optional_child("overwrite")) {
+		deprecated_message("overwrite_specials= or [overwrite] in weapon specials", DEP_LEVEL::INDEFINITE, "", "Use [overwrite_specials] instead.");
+	}
+
 }
 
 
@@ -1738,6 +1742,9 @@ namespace
 		if(!bool_matches_if_present(filter, cfg, "cumulative", false))
 			return false;
 
+		if (!cfg["overwrite_specials"].blank() || cfg.optional_child("overwrite")) {
+			deprecated_message("overwrite_specials= or [overwrite] in weapon specials", DEP_LEVEL::INDEFINITE, "", "Use [overwrite_specials] instead.");
+		}
 		if(!string_matches_if_present(filter, cfg, "overwrite_specials", "none"))
 			return false;
 
