1f8f091a5a5c7c02f8e2f12c3c35b61bcf16dd7b
Subhraman Sarkar <suvrax@gmail.com>
2025-12-17 08:53:48 +0530

    checkout target tag, remove realpath

--- packaging/android/create_patch.sh
+++ packaging/android/create_patch.sh
@@ -3,15 +3,18 @@
 # Check number of arguments, print usage message
 if (($# != 2)); then
 	echo "Usage: ./create_patch.sh tag1 tag2"
+	echo "This will checkout tag2, backup any critical files."
 	exit 0
 fi
+
+git checkout $2
 
 mkdir patch
 
 # find out what changed between two given tags
 for file in `git diff tags/$1..tags/$2 --name-only --diff-filter=AM`; do
 	if [[ $file =~ ^(data|images|sounds|music|fonts|translations).* ]]; then
-		install -Dv `realpath ../../$file` "patch/$file"
+		install -Dv "../../$file" "patch/$file"
 	fi
 done
 
@@ -29,3 +32,4 @@ zip -r ../patch.zip *
 cd ..
 rm -rf patch
 
+git checkout HEAD@{1}
