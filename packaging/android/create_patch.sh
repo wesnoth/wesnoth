f21eeded60a9ece0925fafe723bfb9ea27ae5ae9
Subhraman Sarkar <suvrax@gmail.com>
2025-12-18 18:53:45 +0530

    Manifest generation code

--- packaging/android/create_patch.sh
+++ packaging/android/create_patch.sh
@@ -6,6 +6,7 @@ if (($# != 2)); then
 	exit 0
 fi
 
+# Create patch.zip
 mkdir patch
 
 # find out what changed between two given tags, then copy those files
@@ -16,7 +17,7 @@ for file in `git diff tags/$1..tags/$2 --name-only --diff-filter=AM`; do
 			mkdir -pv "patch/`dirname $file`"
 		fi
 		
-		git show $2:$file > "patch/$file"
+		git show "$2:$file" > "patch/$file"
 	fi
 done
 
@@ -33,3 +34,23 @@ cd patch
 zip -r ../patch.zip *
 cd ..
 rm -rf patch
+
+# Create server package manifest
+if [[ -f manifest.txt ]]; then
+	mv -fv manifest.txt manifest.txt.old
+fi
+
+cat > manifest.txt <<EOF
+packages=patch,core,music
+patch.name=Patch
+patch.version=$2
+patch.url=https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-%s/android-data/patch.zip/download
+patch.depends=core==$1
+patch.excludes=core==$2
+core.name=Core Data
+core.version=$2
+core.url=https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-%s/android-data/master.zip/download
+music.name=Music
+music.version=1.19.18 # Intentional, this prevents redownload of same music assets
+music.url=https://sourceforge.net/projects/wesnoth/files/android-music/music.zip/download
+EOF
