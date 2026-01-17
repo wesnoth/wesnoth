5d0dfeb6012e0ee27dbf30e0bae466ed5eafc885
Toom <public.ravana@gmail.com>
2026-01-17 12:49:15 +0200

    show ability and special unique_id in unit tree
    Temporary workaround to at least show ability identifier in unit tree.

--- data/tools/unit_tree/html_output.py
+++ data/tools/unit_tree/html_output.py
@@ -775,6 +775,13 @@ class HTMLOutput:
                 # complex ones.
                 if name:
                     anames.append(name)
+        abilities_list = u.get_text_val("abilities_list")
+        if abilities_list:
+            abilities_list = abilities_list.split(",")
+            for ability_ref in abilities_list:
+                ability_ref = ability_ref.strip()
+                # TODO load translatable name from self.wesnoth.(new implementation in helpers.py)
+                anames.append(ability_ref)
         return anames
 
     def get_recursive_attacks(self, this_unit):
@@ -956,6 +963,13 @@ class HTMLOutput:
                                         sname = T(special, "name")
                                         if sname:
                                             s.append(sname)
+                                specials_list = attack.get_text_val("specials_list")
+                                if specials_list:
+                                    specials_list = specials_list.split(",")
+                                    for special_ref in specials_list:
+                                        special_ref = specials_ref.strip()
+                                        # TODO load translatable name from self.wesnoth.(new implementation in helpers.py)
+                                        s.append(special_ref)
                                 accuracy = attack.get_text_val("accuracy", default="0")
                                 parry = attack.get_text_val("parry", default="0")
                                 if accuracy != "0":
