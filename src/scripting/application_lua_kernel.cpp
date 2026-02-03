096b89366bad10d51bfcb4fcb83e8c61dbf997c7
Stefan Fuhrmann <stefan.fuhrmann@efficientware.net>
2026-02-03 19:13:28 +0100

    LUA: 5x speedup for lua_push_namedtuple
    Since we are using only a few different tuple configurations, we
    can just store the respective meta tables in the LUA registry.
    
    Intruduce a builder class that allows for convenient declaration
    of reusable tuple types. Move static callback functions into
    the builder class as it provides a natural namespace.

--- src/scripting/application_lua_kernel.cpp
+++ src/scripting/application_lua_kernel.cpp
@@ -468,7 +468,7 @@ bool luaW_copy_upvalues(lua_State* L, const config& cfg)
 					for(const auto& cfg : children) {
 						names.push_back(cfg["name"]);
 					}
-					luaW_push_namedtuple(L, names);
+					lua_named_tuple_builder{ names }.push(L);
 					for(const auto& cfg : children) {
 						luaW_pushscalar(L, cfg["value"]);
 						lua_rawseti(L, -2, lua_rawlen(L, -2) + 1);
