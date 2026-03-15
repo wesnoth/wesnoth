wesnoth/wesnoth

commit e6369fee385ad5e89d219d36c7ea823b7906d703

use different variable to remember to autosave

src/play_controller.cpp
@@ -163,7 +163,8 @@ play_controller::play_controller(const config& level, saved_game& state_of_game,
 	, replay_(new replay(state_of_game.get_replay()))
 	, skip_replay_(skip_replay)
 	, skip_story_(state_of_game.skip_story())
-	, init_side_done_now_(false)
+	, did_autosave_this_turn_(true)
+	, did_tod_sound_this_turn_(false)
 	, map_start_()
 	, start_faded_(start_faded)
 	, victory_when_enemies_defeated_(level["victory_when_enemies_defeated"].to_bool(true))
@@ -476,7 +477,6 @@ void play_controller::do_init_side()
 	// In case we might end up calling sync:network during the side turn events,
 	// and we don't want do_init_side to be called when a player drops.
 	gamestate().gamedata_.set_phase(game_data::TURN_STARTING);
-	init_side_done_now_ = true;
 
 	const std::string turn_num = std::to_string(turn());
 	const std::string side_num = std::to_string(current_side());
@@ -538,16 +538,18 @@ void play_controller::do_init_side()
 	// Make sure vision is accurate.
 	actions::clear_shroud(current_side(), true);
 
-	init_side_end();
 	check_victory();
 	sync.do_final_checkup();
+
+	init_side_end();
 }
 
 void play_controller::init_side_end()
 {
-	const time_of_day& tod = gamestate().tod_manager_.get_time_of_day();
 
-	if(current_side() == 1 || !init_side_done_now_) {
+	if(	did_tod_sound_this_turn_) {
+		did_tod_sound_this_turn_ = true;
+		const time_of_day& tod = gamestate().tod_manager_.get_time_of_day();
 		sound::play_sound(tod.sounds, sound::SOUND_SOURCES);
 	}
 
@@ -608,6 +610,7 @@ void play_controller::finish_side_turn()
 	}
 	mouse_handler_.deselect_hex();
 	gamestate().gamedata_.set_phase(game_data::TURN_STARTING_WAITING);
+	did_autosave_this_turn_ = false;
 }
 
 void play_controller::finish_turn()
@@ -617,6 +620,7 @@ void play_controller::finish_turn()
 	pump().fire("turn_end");
 	pump().fire("turn_" + turn_num + "_end");
 	sync.do_final_checkup();
+	did_tod_sound_this_turn_ = false;
 }
 
 bool play_controller::enemies_visible() const

src/play_controller.hpp
@@ -394,7 +394,8 @@ protected:
 	 * Whether we did init sides in this session
 	 * (false = we did init sides before we reloaded the game).
 	 */
-	bool init_side_done_now_;
+	bool did_autosave_this_turn_;
+	bool did_tod_sound_this_turn_;
 	//the displayed location when we load a game.
 	map_location map_start_;
 	// Whether to start with the display faded to black

src/playsingle_controller.cpp
@@ -398,8 +398,6 @@ void playsingle_controller::play_side_impl()
 		end_turn_requested_ = false;
 	}
 	if(replay_controller_.get() != nullptr) {
-		init_side_done_now_ = false;
-
 		REPLAY_RETURN res = replay_controller_->play_side_impl();
 		if(res == REPLAY_FOUND_END_TURN) {
 			gamestate().gamedata_.set_phase(game_data::TURN_ENDED);
@@ -454,7 +452,8 @@ void playsingle_controller::before_human_turn()
 		return;
 	}
 
-	if(init_side_done_now_ && !game_config::disable_autosave && preferences::autosavemax() > 0) {
+	if(!did_autosave_this_turn_ && !game_config::disable_autosave && preferences::autosavemax() > 0) {
+		did_autosave_this_turn_ = true;
 		scoped_savegame_snapshot snapshot(*this);
 		savegame::autosave_savegame save(saved_game_, preferences::save_compression_format());
 		save.autosave(game_config::disable_autosave, preferences::autosavemax(), preferences::INFINITE_AUTO_SAVES);

commit 66432bc545f6a99d75552a30c91926cc0c8c4bcd

Fix ToD sound playing broken in e6369fe

src/play_controller.cpp
@@ -576,9 +576,10 @@ void play_controller::do_init_side()
 
 void play_controller::init_side_end()
 {
-	if(	did_tod_sound_this_turn_) {
+	if(!did_tod_sound_this_turn_) {
 		did_tod_sound_this_turn_ = true;
 		const time_of_day& tod = gamestate().tod_manager_.get_time_of_day();
+		LOG_NG << "playing ToD sound: " << tod.sounds;
 		sound::play_sound(tod.sounds, sound::SOUND_SOURCES);
 	}
 	whiteboard_manager_->on_init_side();