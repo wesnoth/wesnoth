wesnoth/wesnoth

commit 16f5980a3cb201c90a770c228d09da610d8e1872

config::all_children_range(), attribute_range() now return iterator_range
This also fixed compilation with boost veryion < 1.56 by using
iterator_range::pop_front() instead of iterator_range::drop_front()
which was added in boost 1.56

This also makes config::all_children_iterator a random access iterator.

src/units/filter.cpp
@@ -461,12 +461,11 @@ bool basic_unit_filter_impl::internal_matches_filter(const unit & u, const map_l
 			config fwml = wmlcfg.get_parsed_config();
 			/* Check if the filter only cares about variables.
 			   If so, no need to serialize the whole unit. */
-			config::const_attr_itors ai = fwml.attribute_range();
 			config::all_children_itors ci = fwml.all_children_range();
-			if (std::distance(ai.first, ai.second) == 0 &&
-			    std::distance(ci.first, ci.second) == 1 &&
-			    ci.first->key == "variables") {
-				if (!u.variables().matches(ci.first->cfg))
+			if (fwml.all_children_count() == 1 && 
+				fwml.attribute_count() == 1 &&
+			    ci.front().key == "variables") {
+				if (!u.variables().matches(ci.front().cfg))
 					return false;
 			} else {
 				if (unit_cfg.empty())

src/config.cpp
@@ -1532,11 +1532,11 @@ bool operator==(const config& a, const config& b)
 		return false;
 
 	config::all_children_itors x = a.all_children_range(), y = b.all_children_range();
-	for (; x.first != x.second && y.first != y.second; ++x.first, ++y.first) {
-		if (x.first->key != y.first->key || x.first->cfg != y.first->cfg) {
+	for (; !x.empty() && !y.empty(); x.pop_front(), y.pop_front()) {
+		if (x.front().key != y.front().key || x.front().cfg != y.front().cfg) {
 			return false;
 		}
 	}
 
-	return x.first == x.second && y.first == y.second;
+	return x.empty() && y.empty();
 }

src/config.hpp
@@ -412,7 +412,7 @@ public:
 		Itor i_;
 	};
 
-	typedef std::pair<const_attribute_iterator,const_attribute_iterator> const_attr_itors;
+	typedef boost::iterator_range<const_attribute_iterator> const_attr_itors;
 
 	child_itors child_range(const std::string& key);
 	const_child_itors child_range(const std::string& key) const;
@@ -637,15 +637,18 @@ public:
 		};
 
 		typedef any_child value_type;
-		typedef std::forward_iterator_tag iterator_category;
+		typedef std::random_access_iterator_tag iterator_category;
 		typedef int difference_type;
 		typedef const arrow_helper pointer;
 		typedef const any_child reference;
 		typedef std::vector<child_pos>::const_iterator Itor;
+		typedef all_children_iterator this_type;
 		explicit all_children_iterator(const Itor &i): i_(i) {}
 
 		all_children_iterator &operator++() { ++i_; return *this; }
 		all_children_iterator operator++(int) { return all_children_iterator(i_++); }
+		this_type &operator--() { --i_; return *this; }
+		this_type operator--(int) { return this_type(i_--); }
 
 		reference operator*() const;
 		pointer operator->() const { return *this; }
@@ -653,13 +656,27 @@ public:
 		bool operator==(const all_children_iterator &i) const { return i_ == i.i_; }
 		bool operator!=(const all_children_iterator &i) const { return i_ != i.i_; }
 
+		friend bool operator<(const this_type& a, const this_type& b) { return a.i_ < b.i_; }
+		friend bool operator<=(const this_type& a, const this_type& b) { return a.i_ <= b.i_; }
+		friend bool operator>=(const this_type& a, const this_type& b) { return a.i_ >= b.i_; }
+		friend bool operator>(const this_type& a, const this_type& b) { return a.i_ > b.i_; }
+
+		this_type& operator+=(difference_type n) { i_ += n; return *this; }
+		this_type& operator-=(difference_type n) { i_ -= n; return *this; }
+
+		reference operator[](difference_type n) const { return any_child(&i_[n].pos->first, i_[n].pos->second[i_->index]); }
+		friend difference_type operator-(const this_type& a, const this_type& b) { return a.i_ - b.i_; }
+		friend this_type operator-(const this_type& a, difference_type n) { return this_type(a.i_ - n); }
+		friend this_type operator+(const this_type& a, difference_type n) { return this_type(a.i_ + n); }
+		friend this_type operator+(difference_type n, const this_type& a) { return this_type(a.i_ + n); }
+
 	private:
 		Itor i_;
 
 		friend class config;
 	};
 
-	typedef std::pair<all_children_iterator, all_children_iterator> all_children_itors;
+	typedef boost::iterator_range<all_children_iterator> all_children_itors;
 
 	/** In-order iteration over all children. */
 	all_children_itors all_children_range() const;

src/display.cpp
@@ -3400,7 +3400,7 @@ void display::refresh_report(std::string const &report_name, const config * new_
 	SDL_Rect ellipsis_area = rect;
 
 	for (config::const_child_itors elements = report.child_range("element");
-		 elements.begin() != elements.end(); elements.drop_front())
+		 elements.begin() != elements.end(); elements.pop_front(1))
 	{
 		SDL_Rect area = sdl::create_rect(x, y, rect.w + rect.x - x, rect.h + rect.y - y);
 		if (area.h <= 0) break;

src/gui/core/window_builder.cpp
@@ -155,7 +155,7 @@ register_builder_widget(const std::string& id,
 tbuilder_widget_ptr create_builder_widget(const config& cfg)
 {
 	config::all_children_itors children = cfg.all_children_range();
-	size_t nb_children = std::distance(children.first, children.second);
+	size_t nb_children = std::distance(children.begin(), children.end());
 	VALIDATE(nb_children == 1, "Grid cell does not have exactly 1 child.");
 
 	for(const auto & item : builder_widget_lookup())

src/playturn.cpp
@@ -126,7 +126,7 @@ turn_info::PROCESS_DATA_RESULT turn_info::process_network_data(const config& cfg
 {
 	// the simple wesnothserver implementation in wesnoth was removed years ago.
 	assert(cfg.all_children_count() == 1);
-	assert(cfg.attribute_range().first == cfg.attribute_range().second);
+	assert(cfg.attribute_range().empty());
 	if(!resources::recorder->at_end())
 	{
 		ERR_NW << "processing network data while still having data on the replay." << std::endl;

src/playturn_network_adapter.cpp
@@ -48,7 +48,7 @@ void playturn_network_adapter::read_from_network()
 		back.remove_attribute("side_drop");
 		back.remove_attribute("controller");
 	}
-	else if(back.attribute_range().first != back.attribute_range().second )
+	else if(!back.attribute_range().empty() )
 	{
 		ERR_NW << "found unexpected attribute:" <<back.debug() << std::endl;
 		this->data_.pop_back();

src/replay.cpp
@@ -698,7 +698,7 @@ REPLAY_RETURN do_replay_handle(bool one_move)
 
 		const config::all_children_itors ch_itors = cfg->all_children_range();
 		//if there is an empty command tag or a start tag
-		if (ch_itors.first == ch_itors.second || cfg->has_child("start"))
+		if (ch_itors.empty() || cfg->has_child("start"))
 		{
 			//this shouldn't happen anymore because replaycontroller now moves over the [start] with get_next_action
 			//also we removed the the "add empty replay entry at scenario reload" behavior.
@@ -821,7 +821,7 @@ REPLAY_RETURN do_replay_handle(bool one_move)
 			// but we are called from
 			// the only other option for "dependent" command is checksum wich is already checked.
 			assert(cfg->all_children_count() == 1);
-			std::string child_name = cfg->all_children_range().first->key;
+			std::string child_name = cfg->all_children_range().front().key;
 			DBG_REPLAY << "got an dependent action name = " << child_name <<"\n";
 			resources::recorder->revert_action();
 			return REPLAY_FOUND_DEPENDENT;

commit 562036ebd1b002bba9dd2bd7723d6cda207e2749

Fix unit filter ignoring sole attribute in [variables] (bug #11211)
This addresses a regression introduced in 16f5980.

src/units/filter.cpp
@@ -711,7 +711,7 @@ void unit_filter_compound::fill(const vconfig& cfg)
 					/* Check if the filter only cares about variables.
 					   If so, no need to serialize the whole unit. */
 					config::all_children_itors ci = fwml.all_children_range();
-					if (fwml.all_children_count() == 1 && fwml.attribute_count() == 1 && ci.front().key == "variables") {
+					if (fwml.all_children_count() == 1 && fwml.attribute_count() == 0 && ci.front().key == "variables") {
 						return args.u.variables().matches(ci.front().cfg);
 					} else {
 						config ucfg;