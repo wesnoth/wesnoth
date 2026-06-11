114cf72be6ff5029a9da81010e6902d2852a1339
willca <37680486+will-ca@users.noreply.github.com>
2026-06-11 00:43:44 +0000

    Adjust default reachmap opacities

--- src/game_config.cpp
+++ src/game_config.cpp
@@ -383,9 +383,9 @@ void load_config(const config &v)
 	shroud_prefix = v["shroud_prefix"].str();
 	fog_prefix    = v["fog_prefix"].str();
 	reach_map_prefix 	= v["reach_map_prefix"].str();
-	reach_map_border_opacity = v["reach_map_border_opacity"].to_int(100);
-	reach_map_tint_opacity   = v["reach_map_tint_opacity"].to_int(50);//tint is at 50% by default instead of 100% to allow players to make it more opaque than normal
-	reach_map_darken_opacity = v["reach_map_darken_opacity"].to_int(100);
+	reach_map_border_opacity = v["reach_map_border_opacity"].to_int(60);
+	reach_map_tint_opacity   = v["reach_map_tint_opacity"].to_int(30);
+	reach_map_darken_opacity = v["reach_map_darken_opacity"].to_int(60);
 
 	add_color_info(game_config_view::wrap(v), true);
 
