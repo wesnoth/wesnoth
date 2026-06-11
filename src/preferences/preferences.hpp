9691532aa8f4b87362013cd41ce6c619553f410c
willca <37680486+will-ca@users.noreply.github.com>
2026-06-11 00:43:44 +0000

    Allow configuring reach map darkening opacity

--- src/preferences/preferences.hpp
+++ src/preferences/preferences.hpp
@@ -322,6 +322,9 @@ public:
 	int reach_map_tint_opacity();
 	void set_reach_map_tint_opacity(const int new_opacity);
 
+	int reach_map_darken_opacity();
+	void set_reach_map_darken_opacity(const int new_opacity);
+
 	int scroll_speed();
 	void set_scroll_speed(const int scroll);
 
@@ -839,6 +842,7 @@ private:
 		prefs_list::addon_icons,
 		prefs_list::reach_map_border_opacity,
 		prefs_list::reach_map_tint_opacity,
+		prefs_list::reach_map_darken_opacity,
 		prefs_list::reach_map_color,
 		prefs_list::reach_map_enemy_color,
 	};
