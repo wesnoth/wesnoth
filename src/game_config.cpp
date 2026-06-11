448ed4ffa0963c89f055ab9ace4cd7736bbe92fe
willca <37680486+will-ca@users.noreply.github.com>
2026-06-11 01:31:33 +0000

    Add option for untextured reachmap darkening

--- src/game_config.cpp
+++ src/game_config.cpp
@@ -133,6 +133,7 @@ bool show_unmoved_orb;
 int reach_map_border_opacity;
 int reach_map_tint_opacity;
 int reach_map_darken_opacity;
+bool reach_map_darken_tex;
 
 //
 // Music constants
@@ -385,6 +386,7 @@ void load_config(const config &v)
 	reach_map_border_opacity = v["reach_map_border_opacity"].to_int(60);
 	reach_map_tint_opacity   = v["reach_map_tint_opacity"].to_int(30);
 	reach_map_darken_opacity = v["reach_map_darken_opacity"].to_int(60);
+	reach_map_darken_tex = v["reach_map_darken_tex"].to_bool(true);
 
 	add_color_info(game_config_view::wrap(v), true);
 
--- src/gui/dialogs/reachmap_options.cpp
+++ src/gui/dialogs/reachmap_options.cpp
@@ -57,6 +57,7 @@ void reachmap_options::pre_show()
 	find_widget<slider>("reachmap_opacity_border").set_value(prefs::get().reach_map_border_opacity());
 	find_widget<slider>("reachmap_opacity_tint").set_value(prefs::get().reach_map_tint_opacity());
 	find_widget<slider>("reachmap_opacity_darken").set_value(prefs::get().reach_map_darken_opacity());
+	find_widget<toggle_button>("reachmap_darken_tex").set_value_bool(prefs::get().reach_map_darken_tex());
 
 	connect_signal_mouse_left_click(
 		find_widget<button>("reachmap_defaults"), std::bind(&reachmap_options::reset_reachmap_callback, this));
@@ -77,6 +78,7 @@ void reachmap_options::post_show()
 	prefs::get().set_reach_map_border_opacity(find_widget<slider>("reachmap_opacity_border").get_value());
 	prefs::get().set_reach_map_tint_opacity(find_widget<slider>("reachmap_opacity_tint").get_value());
 	prefs::get().set_reach_map_darken_opacity(find_widget<slider>("reachmap_opacity_darken").get_value());
+	prefs::get().set_reach_map_darken_tex(find_widget<toggle_button>("reachmap_darken_tex").get_value_bool());
 }
 
 void reachmap_options::setup_reachmap_group(const std::string& base_id, const std::string& initial)
@@ -118,6 +120,7 @@ void reachmap_options::reset_reachmap_callback()
 	reset_reachmap_slider("reachmap_opacity_border", game_config::reach_map_border_opacity);
 	reset_reachmap_slider("reachmap_opacity_tint", game_config::reach_map_tint_opacity);
 	reset_reachmap_slider("reachmap_opacity_darken", game_config::reach_map_darken_opacity);
+	find_widget<toggle_button>("reachmap_darken_tex").set_value_bool(game_config::reach_map_darken_tex);
 }
 
 } // namespace gui2::dialogs
