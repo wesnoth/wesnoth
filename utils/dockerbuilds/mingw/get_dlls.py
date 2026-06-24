23115d249cdb3556f89041f4b5db57c63f80fc82
pentarctagon <pentarctagon@tutamail.com>
2026-06-24 16:45:24 -0500

    get this fixed for now at least

--- utils/dockerbuilds/mingw/get_dlls.py
+++ utils/dockerbuilds/mingw/get_dlls.py
@@ -6,19 +6,20 @@ dlls = set()
 dlls.add('/msys64/mingw64/bin/libwebp-7.dll')
 dlls.add('/msys64/mingw64/bin/libturbojpeg.dll')
 dllpath = pathlib.Path('/msys64/mingw64/bin')
+executables = ['wesnoth.exe', 'wesnothd.exe']
 pe_modules = set([pefile.PE('wesnoth.exe'), pefile.PE('wesnothd.exe')])
 
 while pe_modules:
     pe = pe_modules.pop()
     for entry in pe.DIRECTORY_ENTRY_IMPORT:
         path = dllpath / pathlib.Path(entry.dll.decode())
-        if path.exists():
-            print("Found dll path `"+str(path)+"` for executable `"+pe+"`")
-        else:
-            print("Did not find dll path `"+str(path)+"` for executable `"+pe+"`")
-        if path not in dlls and path.exists():
-            dlls.add(path)
-            pe_modules.add(pefile.PE(path))
+        if path not in dlls:
+            if path.exists():
+                print("Found dll path `"+str(path)+"`")
+                dlls.add(path)
+                pe_modules.add(pefile.PE(path))
+            else:
+                print("Did not find dll path `"+str(path)+"`")
 
 for dll in dlls:
     shutil.copy(dll, ".")
