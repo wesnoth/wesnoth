d91b6afc06d69123d8ebdb0f028dcd0b70f2c0db
pentarctagon <pentarctagon@tutamail.com>
2026-06-24 14:02:51 -0500

    log dll search results

--- utils/dockerbuilds/mingw/get_dlls.py
+++ utils/dockerbuilds/mingw/get_dlls.py
@@ -10,6 +10,10 @@ while pe_modules:
     pe = pe_modules.pop()
     for entry in pe.DIRECTORY_ENTRY_IMPORT:
         path = dllpath / pathlib.Path(entry.dll.decode())
+        if path.exists():
+            print("Found dll path `"+path+"` for executable `"+pe+"`")
+        else:
+            print("Did not find dll path `"+path+"` for executable `"+pe+"`")
         if path not in dlls and path.exists():
             dlls.add(path)
             pe_modules.add(pefile.PE(path))
