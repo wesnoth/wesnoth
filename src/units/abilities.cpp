7887786efbb2bf3f30d94cbb98c98082eb90de97
Éric Belleux <eric.belleux@gmail.com>
2026-07-27 10:31:51 +0200

    Remove two deprecation warnings regarding the `count=` attribute called within two already-deprecated filters and fix count strictly equals to 6.
    Adding these two messages is redundant and implies that using `count` is becoming mandatory for two filters then what 'count' was mandatory until 1.19, but not anymore.
    
    Worse, it could suggest that the SUF variants of `[filter_adjacent]` might require a mandatory `count`, which is not the case.
    
    and impose max direction size to 'count' what number of adjacent unit who match must be equals to max_direction.size and not a nimber bteween 1 and max_direction.size and now what count by default anew specified in SUF, it is not necessary to make thhis in abilities.cpp

--- src/units/abilities.cpp
+++ src/units/abilities.cpp
@@ -217,20 +217,10 @@ void unit_ability_t::do_compat_fixes(config& cfg, const std::string& tag, bool i
 
 	//These tags are were never supported inside [specials] according to the wiki.
 	for (config& filter_adjacent : cfg.child_range("filter_adjacent")) {
-		if (filter_adjacent["count"].empty()) {
-			//Previously count= behaved differenty in abilities.cpp and in filter.cpp according to the wiki
-			deprecated_message("omitting count= in [filter_adjacent] in abilities", DEP_LEVEL::FOR_REMOVAL, version_info("1.21"), "specify count explicitly");
-			filter_adjacent["count"] = map_location::parse_directions(filter_adjacent["adjacent"]).size();
-		}
 		cfg.child_or_add(filter_teacher).add_child("filter_adjacent", filter_adjacent);
 	}
 	cfg.remove_children("filter_adjacent");
 	for (config& filter_adjacent : cfg.child_range("filter_adjacent_location")) {
-		if (filter_adjacent["count"].empty()) {
-			//Previously count= bahves differenty in abilities.cpp and in filter.cpp according to the wiki
-			deprecated_message("omitting count= in [filter_adjacent_location] in abilities", DEP_LEVEL::FOR_REMOVAL, version_info("1.21"), "specify count explicitly");
-			filter_adjacent["count"] = map_location::parse_directions(filter_adjacent["adjacent"]).size();
-		}
 		cfg.child_or_add(filter_teacher).add_child("filter_location").add_child("filter_adjacent_location", filter_adjacent);
 	}
 	cfg.remove_children("filter_adjacent_location");
