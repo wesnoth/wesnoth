f2487b7b1f70a99cc6758c386905844c57a93f3a
gfgtdf <daniel.gfgtdf@gmail.com>
2026-08-01 20:13:50 +0200

    dont copy team objects 3

--- src/scripting/lua_formula_bridge.cpp
+++ src/scripting/lua_formula_bridge.cpp
@@ -160,7 +160,7 @@ void luaW_pushfaivariant(lua_State* L, const variant& val) {
 			luaW_pushweapon(L, atk.shared_from_this());
 		} else if(auto team_ref = callable_cast<team_callable*>(val)) {
 			auto& t = team_ref->get_team();
-			luaW_pushteam(L, t);
+			luaW_pushteam(L, const_cast<team&>(t));
 		} else if(auto loc_ref = callable_cast<location_callable*>(val)) {
 			luaW_pushlocation(L, loc_ref->loc());
 		} else {
